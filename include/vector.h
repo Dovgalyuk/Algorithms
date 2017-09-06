d02d3c53a68abdaa18652be029f4753c1d3fd06b
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2017-09-06 11:54:03 +0300

    Added vector header

---
+++ include/vector.h
@@ -0,0 +1,25 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// ¬ списке хран€тс€ данные типа int,
+// поэтому функции принимают и возвращают 
+// данные этого типа
+
+struct Vector;
+
+// создание вектора
+Vector *vector_create();
+
+// удаление вектора
+void vector_delete(Vector *vector);
+
+// получить заданный элемент вектора
+int vector_get(Vector *vector, size_t index);
+
+// получить текущий размер вектора
+size_t vector_size(Vector *vector);
+
+// изменить размер вектора
+void vector_resize(Vector *vector, size_t size);
+
+#endif
