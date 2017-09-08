78fdd535a01d6df2a842d00a50f3647942588269
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2017-09-08 11:59:06 +0300

    Added stack test

--- Library/stack.cpp
+++ Library/stack.cpp
@@ -1,2 +1,39 @@
+#include <stdlib.h>
 #include "stack.h"
 
+struct Stack
+{
+};
+
+// создание пустого стека
+Stack *stack_create()
+{
+    return NULL;
+}
+
+// удаление стека
+void stack_delete(Stack *stack)
+{
+}
+
+// включение элемента в стек
+void stack_push(Stack *stack, int Data)
+{
+}
+
+// получение последнего элемента стека
+int stack_get(Stack *stack)
+{
+    return 0;
+}
+
+// выталкивание элемента из стека
+void stack_pop(Stack *stack)
+{
+}
+
+// проверка стека на наличие элементов
+bool stack_empty(Stack *stack)
+{
+    return true;
+}
--- TestStack/main.cpp
+++ TestStack/main.cpp
@@ -3,4 +3,24 @@
 
 int main()
 {
+    Stack *stack = stack_create();
+
+    stack_push(stack, 1);
+    stack_push(stack, 2);
+    stack_push(stack, 3);
+
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_push(stack, 4);
+    stack_push(stack, 5);
+    while (!stack_empty(stack))
+    {
+        std::cout << "Get: " << stack_get(stack) << "\n";
+        stack_pop(stack);
+    }
+
+    stack_delete(stack);
 }
