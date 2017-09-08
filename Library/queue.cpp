259cb586c3f6eca84c0daf1e461a127c050d76de
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2017-09-08 09:59:43 +0300

    Added queue test

--- Library/queue.cpp
+++ Library/queue.cpp
@@ -1,1 +1,39 @@
+#include <stdlib.h>
 #include "queue.h"
+
+struct Queue
+{
+};
+
+// создание пустой очереди
+Queue *queue_create()
+{
+    return NULL;
+}
+
+// удаление очереди
+void queue_delete(Queue *queue)
+{
+}
+
+// включение элемента в очередь
+void queue_insert(Queue *queue, int data)
+{
+}
+
+// получение первого элемента очереди
+int queue_get(Queue *queue)
+{
+    return 0;
+}
+
+// удаление первого элемента из очереди
+void queue_remove(Queue *queue)
+{
+}
+
+// проверка очереди на наличие элементов
+bool queue_empty(Queue *queue)
+{
+    return true;
+}
--- TestQueue/main.cpp
+++ TestQueue/main.cpp
@@ -3,4 +3,19 @@
 
 int main()
 {
+    Queue *queue = queue_create();
+
+    queue_insert(queue, 1);
+    queue_insert(queue, 2);
+    queue_insert(queue, 3);
+
+    std::cout << "Get: " << queue_get(queue) << "\n";
+    queue_remove(queue);
+    std::cout << "Get: " << queue_get(queue) << "\n";
+    queue_remove(queue);
+    std::cout << "Get: " << queue_get(queue) << "\n";
+    queue_insert(queue, 4);
+    std::cout << "Get: " << queue_get(queue) << "\n";
+
+    queue_delete(queue);
 }
