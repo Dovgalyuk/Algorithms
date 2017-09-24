98ecf084ce46909ae46d1f13f5715f3f7c319ffe
Alex Vanchikov <Alexbvbv@yandex.ru>
2017-09-24 22:05:43 +0300

    VectorRealise

--- Library/vector.cpp
+++ Library/vector.cpp
@@ -1,39 +1,56 @@
 #include <stdlib.h>
 #include "vector.h"
 
+
+
+
 struct Vector
 {
+	int *Element = new int[0];
+	size_t size = 0;
 };
 
 // создание вектора
 Vector *vector_create()
 {
-    return NULL;
+	Vector *NewVector = new Vector;
+	return NewVector;
 }
 
 // удаление вектора
 void vector_delete(Vector *vector)
 {
+	delete  vector;
 }
 
 // получить заданный элемент вектора
 int vector_get(Vector *vector, size_t index)
 {
-    return 0;
+	return vector->Element[index];
 }
 
 // установить заданный элемент вектора
 void vector_set(Vector *vector, size_t index, int value)
 {
+	vector->Element[index] = value;
 }
 
 // получить текущий размер вектора
 size_t vector_size(Vector *vector)
 {
-    return 0;
+	return vector->size;
 }
 
 // изменить размер вектора
 void vector_resize(Vector *vector, size_t size)
 {
+	int *buffer = new int[size];
+	vector->size = size;
+	for (int i = 0; i < size; i++)
+		buffer[i] = vector->Element[i];
+	delete[] vector->Element;
+	vector->Element = buffer;
+
+
 }
+
