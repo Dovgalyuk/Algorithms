ee690fca8303d8abedbf7b3a233797939ec15701
Alex Vanchikov <Alexbvbv@yandex.ru>
2017-10-08 13:00:30 +0300

    Второе задание выполнено

--- Zadanie2/Source.cpp
+++ Zadanie2/Source.cpp
@@ -5,31 +5,132 @@
 	Операндами в выражении являются неотрицательные целые числа.
 */
 
+
+
+
 #include <iostream>
 #include <conio.h>
 #include <string>
-#include <string.h>
+//#include <string.h>
 #include "../Library/stack.h"
 
-
-
+using namespace std;
+// преверяет букву на то - символ ли это
+bool is_znak(string insertString, int key);
+// возвращает код знака либо -1
+int get_znak(string insertString, int& key);
+// возвращает число из строки inserString начиная с номера элемента key
+int get_number(string insertString, int& key);
 
 int main() {
 
 	//std::string test = "1+2*3-1";
 
-	Stack * testStack = stack_create();
-	std::string kek;
+	Stack * mainStack = stack_create();
+	
+		
+	string insertString; int buffer = 0,buffer1 = 0,buffer2 = 0;
+	getline(cin, insertString);
+	
+
+
+
+	int i = 0;
+	while (i < insertString.size()) 
+	{
+		if (is_znak(insertString, i)) 
+		{
+		buffer2 =	get_znak(insertString, i);
+		i++;
+		}
+
+		switch (buffer2) 
+		{
+			case 1: 
+					buffer = get_number(insertString, i);
+					stack_push(mainStack, buffer);
+				break;
+			case 2:	
+					buffer = get_number(insertString, i);
+					buffer = buffer * -1;
+					stack_push(mainStack, buffer);
+				break;
+			case 3: 
+					buffer1 = stack_get(mainStack);
+					stack_pop(mainStack);
+					buffer = get_number(insertString, i);
+					buffer = buffer * buffer1;
+					stack_push(mainStack, buffer);
+				break;	
+			case 4:
+					buffer1 = stack_get(mainStack);
+					stack_pop(mainStack);
+					buffer = get_number(insertString, i);
+					buffer = buffer / buffer1;
+					stack_push(mainStack, buffer);
+				break;
+			default: buffer = get_number(insertString, i);	stack_push(mainStack, buffer); break;
+		
+		}
+		
+	
+	
+	//i++;
+	}
+
 
 	
 
-	while (!stack_empty(testStack))
+	int  answer = 0;
+
+
+	while (!stack_empty(mainStack))
 	{
-		std::cout << "Get: " << stack_get(testStack) << "\n";
-		stack_pop(testStack);
+		answer = answer + stack_get(mainStack);
+		stack_pop(mainStack);
 	}
 
-	stack_delete(testStack);
+	cout << answer;
 
-
+	stack_delete(mainStack);
+	
+	
 }
+
+    bool is_znak(string insertString, int key)
+	{
+		if (insertString[key] == '-' || insertString[key] == '+' || insertString[key] == '*' || insertString[key] == '/')
+			return true;
+		else
+			return false;	
+	}
+
+	int get_number(string insertString, int & key) 
+	{
+		int buffer = 0;
+		
+		while (isdigit(insertString[key]))
+		{
+			buffer = ((buffer) * 10 + insertString[key]) - 48;
+			key++;
+		}
+		return buffer;
+	}
+
+	int get_znak(string insertString, int &key) {
+		switch (insertString[key]) {
+		case '+': return 1;break;
+		case '-': return 2;break;
+		case '*': return 3;break;
+		case '/': return 4;break;
+		default: return -1; break;
+		}
+		}
+	
+	
+	
+	
+	
+	
+	
+	
