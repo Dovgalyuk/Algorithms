717f94cdc7d5fb58e41337f886687c87aec90c73
Vitaljaz <vitalja@github.com>
2018-10-08 19:30:34 +0300

    update list.cpp
    update list.cpp

--- Library/list.cpp
+++ Library/list.cpp
@@ -3,57 +3,116 @@
 
 struct ListItem
 {
+	Data item;
+	ListItem *next;
+	ListItem *prev;
 };
 
 struct List
 {
+	ListItem *head;
 };
 
 List *list_create()
 {
-    return NULL;
+	List *tmp = new List;
+	tmp->head = NULL;
+    return tmp;
 }
 
 void list_delete(List *list)
 {
+	ListItem *tmp = (list)->head;
+	ListItem *next = NULL;
+
+	while (tmp) {
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	free(list);
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return list->head;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return Data();
+	return item->item;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return item->prev;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+	ListItem *tmp = new ListItem;
+	if (list->head == NULL) {
+		tmp->item = data;
+		tmp->next = NULL;
+		tmp->prev = NULL;
+		list->head = tmp;
+	}
+	else {
+		tmp->item = data;
+		tmp->next = list->head;
+		tmp->prev = NULL;
+		list->head = tmp;
+	}
+	return tmp;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+	ListItem *tmp = new ListItem;
+	if (item == NULL)
+	{
+		return NULL;
+	}
+	tmp->item = data;
+	tmp->prev = item;
+	tmp->next = item->next;
+	if (item->next)
+	{
+		item->next->prev = tmp;
+	}
+	item->next = tmp;
+	return NULL;
 }
 
 ListItem *list_erase(List *list, ListItem *item)
 {
-    return NULL;
+	if (item->next) {
+		item->next->prev = item->prev;
+	}
+
+	if (!item->prev) {
+		list->head = item->next;
+
+	}
+
+	free(item);
+	return item;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+	if (item->next == NULL) {
+		return NULL;
+	}
+	ListItem *tmp = item->next;
+	ListItem *tmpNext = item->next->next;
+	tmpNext->prev = item;
+	item->next = tmpNext;
+	delete tmp;
+	return tmpNext;
 }
