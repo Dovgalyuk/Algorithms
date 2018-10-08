eb4cd9c9a2bcc5c6cf955c06f20b521a2e4f0342
Vitaljaz <vitalja@github.com>
2018-10-08 19:45:22 +0300

    Lab 01
    Check to correct sequence html tags

---
+++ Lab 01/stack.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "stack.h"
+#include "list.h"
+
+struct Stack
+{
+	List *list;
+};
+
+Stack *stack_create()
+{
+	Stack *tmp = new Stack;
+	tmp->list = list_create();
+	return tmp;
+}
+
+void stack_delete(Stack *stack)
+{
+	list_delete(stack->list);
+}
+
+void stack_push(Stack *stack, data data)
+{
+	List *tmp = stack->list;
+	list_insert(tmp, data);
+}
+
+data stack_get(const Stack *stack)
+{
+	return list_item_data(list_first(stack->list));
+}
+
+void stack_pop(Stack *stack)
+{
+	List *tmp = stack->list;
+	list_erase(tmp, list_first(stack->list));
+}
+
+bool stack_empty(const Stack *stack)
+{
+	if (list_first(stack->list) == NULL)
+	{
+		return true;
+	}
+	else
+		return false;
+}
