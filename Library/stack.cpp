11e9b0cb3507f17a96d3b6ad5ca96390186e0d33
Vitaljaz <vitalja@github.com>
2018-10-08 19:30:25 +0300

    Update stack.cpp
    Update stack.cpp

--- Library/stack.cpp
+++ Library/stack.cpp
@@ -1,33 +1,47 @@
 #include <stdlib.h>
 #include "stack.h"
+#include "list.h"
 
 struct Stack
 {
+	List *list;
 };
 
 Stack *stack_create()
 {
-    return NULL;
+	Stack *tmp = new Stack;
+	tmp->list = list_create();
+	return tmp;
 }
 
 void stack_delete(Stack *stack)
 {
+	list_delete(stack->list);
 }
 
 void stack_push(Stack *stack, Data data)
 {
+	List *tmp = stack->list;
+	list_insert(tmp, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return Data();
+	return list_item_data(list_first(stack->list));
 }
 
 void stack_pop(Stack *stack)
 {
+	List *tmp = stack->list;
+	list_erase(tmp, list_first(stack->list));
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+	if (list_first(stack->list) == NULL)
+	{
+		return true;
+	}
+	else
+		return false;
 }
