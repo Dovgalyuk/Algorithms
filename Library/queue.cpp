4f05dc5a9dfa5b1ad44658556858fd89a40be4c6
Vitaljaz <vitalja@github.com>
2018-11-19 18:27:14 +0300

    Update queue.cpp

--- Library/queue.cpp
+++ Library/queue.cpp
@@ -27,13 +27,10 @@ void queue_insert(Queue *queue, Data data)
 	stack_push(queue->s1, data);
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(Queue *queue)
 {
 	if (stack_empty(queue->s2)) {
-		while (!stack_empty(queue->s1)) {
-			stack_push(queue->s2, stack_get(queue->s1));
-			stack_pop(queue->s1);
-		}
+		queue_copy(queue);
 	}
 	if (stack_empty(queue->s2)) {
 		return 2;
@@ -46,10 +43,7 @@ Data queue_get(const Queue *queue)
 void queue_remove(Queue *queue)
 {
 	if (stack_empty(queue->s2)) {
-		while (!stack_empty(queue->s1)) {
-			stack_push(queue->s2, stack_get(queue->s1));
-			stack_pop(queue->s1);
-		}
+		queue_copy(queue);
 	}
 	if (!stack_empty(queue->s2)) {
 		stack_pop(queue->s2);
@@ -60,4 +54,11 @@ bool queue_empty(const Queue *queue)
 {
 	if (stack_empty(queue->s1) && stack_empty(queue->s2)) return true;
 	return false;
+}
+
+void queue_copy(Queue *queue) {
+	while (!stack_empty(queue->s1)) {
+		stack_push(queue->s2, stack_get(queue->s1));
+		stack_pop(queue->s1);
+	}
 }
