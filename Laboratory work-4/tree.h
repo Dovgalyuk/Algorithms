b4d9f14b0e14287f2fdb90ecb3347bd4d8226194
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2018-12-09 12:16:20 +0300

    Create tree.h

---
+++ Laboratory work-4/tree.h
@@ -0,0 +1,30 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+#include <string>
+
+typedef std::string Data;
+
+struct Leaf;
+struct Tree;
+
+//Создаёт дерево.
+Tree* tree_create();
+
+//Добавляет пару ключ - значение в дерево.
+void insert_leaf(Tree* tree, Data key, Data value);
+
+//Получает элемент дерева по указанному ключу.
+Data get_value_of_leaf(const Tree* tree, Data key);
+
+//Удаляет элемент дерева.
+void delete_leaf(Tree* tree, Data key);
+
+//Возвращает степень баланска корня.
+bool balance_tree(Leaf* leaf);
+
+//Возвращает корень дерева.
+Leaf* get_root(Tree* tree);
+
+//Возвращает ключ корня дерева
+Data get_root_key(Tree* tree);
+#endif
