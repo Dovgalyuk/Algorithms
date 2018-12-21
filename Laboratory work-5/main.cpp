c4acb7bcfde7fc70cf779b5cb3ff7b2b710847dc
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2018-12-21 13:20:54 +0300

    Update main.cpp

--- Laboratory work-5/main.cpp
+++ Laboratory work-5/main.cpp
@@ -12,11 +12,13 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
-/*
+
 vector<int> output;
 vector<int> tmp;								//Вектор возможной последовательности.
+vector<int> Lengths;
 
 //Перебирает все возможные последовательсноти.
 void processing(vector<int>& input, int i)
@@ -41,7 +43,7 @@ void processing(vector<int>& input, int i)
 			output[i] = tmp[i];
 		}
 	}
-}*/
+}
 
 //Считает максимальную длину возрастающей последовательности для каждого элемента в массиве.
 int processing2(vector<int>& input, int i)
@@ -49,20 +51,23 @@ int processing2(vector<int>& input, int i)
 	int max = 1;
 	int Length = 1;
 
-	if (i + 1 >= input.size())
-	{
-		return 1;
-	}
-
 	for (int j = i + 1; j < input.size(); j++)
 	{
 		if (input[i] < input[j])
 		{
-			Length += processing2(input, j);
+			if (Lengths[j] != 1)
+			{
+				Length += Lengths[j];
+			}
+			else
+			{
+				Length += processing2(input, j);
+			}
 
 			if (max < Length)
 			{
 				max = Length;
+				Lengths[i] = Length;
 			}
 
 			Length = 1;
@@ -80,18 +85,50 @@ int main()
 
 	vector<int> input(N);
 
+	srand(time(NULL));
+
 	for (size_t i = 0; i < N; i++)
 	{
 		cin >> input[i];
 	}
 
+	cout << endl;
+
+	chrono::time_point<chrono::high_resolution_clock> startProcessing2, endProcessing2;
+
 	for (size_t i = 0; i < N; i++)
 	{
-		cout << processing2(input, i) << " ";
+		Lengths.resize(N, 1);
+		startProcessing2 = chrono::high_resolution_clock::now();
 
-		/*processing(input, i);
-		tmp.clear();*/
+		processing2(input, i);
+
+		endProcessing2 = chrono::high_resolution_clock::now();
+
+		double delta = chrono::duration_cast<chrono::nanoseconds> (endProcessing2 - startProcessing2).count();
+
+		cout << i + 1 << ": " << "Processing2(): " << delta << " nanoseconds" << endl;
+		Lengths.clear();
 	}
+
+	cout << endl;
+	cout << endl;
+
+	chrono::time_point<chrono::high_resolution_clock> startProcessing, endProcessing;
+
+	for (size_t i = 0; i < N; i++)
+	{
+		startProcessing = chrono::high_resolution_clock::now();
+
+		processing(input, i);
+
+		endProcessing = chrono::high_resolution_clock::now();
+
+		double delta = chrono::duration_cast<chrono::nanoseconds> (endProcessing - startProcessing).count();
+
+		cout << i + 1 << ": " <<"Processing():" << delta << " nanoseconds" << endl;
+	}
+
 	/*
 	cout << output.size() << endl;
 
@@ -100,6 +137,7 @@ int main()
 		cout << output[i] << " ";
 	}
 	*/
+
 	cout << endl;
 	system("Pause");
 	return 0;
