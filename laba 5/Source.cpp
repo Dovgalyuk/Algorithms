df8a73457be88d4c6f0b0a7af8db5bb451b76f71
HereCouldBeName <ivanovrkasha@gmail.com>
2019-01-10 14:08:56 +0300

    LABA 5 (changed)

--- laba 5/Source.cpp
+++ laba 5/Source.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
 
-
 /*recursion method*/
+
+int mas[2][20][20];
+
 int next(int step, int n, char c)
 {
-	int otv = 0;
 	if (n == step)
 	{
 		if (c != 'a')
 			return 3;
 		else
 			return 2;
-	}		
-	else
-	{
-		step++;
-		if(c != 'a')
-			return next(step,n,'a') + 2 * next(step, n, 'b');
-		else
-			return 2 * next(step, n, 'b');
 	}
+	if (mas[c-'a'][step][n] >= 0)
+		return mas[c - 'a'][step][n];
+	int res = 0;
+	step++;
+	if(c != 'a')
+		res = next(step,n,'a') + 2 * next(step, n, 'b');
+	else
+		res =  2 * next(step, n, 'b');
+	mas[c - 'a'][step-1][n] = res;
+	return res;
 }
 
 int main()
 {
 	int n; 
 	cin >> n;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < n; j++)
+			memset(mas[i][j], -1, sizeof(int)*n);
 	if (n < 1)
 		cout << "0";
 	else
@@ -37,17 +43,15 @@ int main()
 {
 	int n;
 	cin >> n;
-	int *mem1 = new int[n];
-	int *mem2 = new int[n];
-	mem1[0] = 1;
-	mem2[0] = 3;
+	int last1 = 1;
+	int last2 = 3;
 	for (int i = 1; i < n; i++)
 	{
-		mem1[i] = mem2[i - 1] - mem1[i - 1];
-		mem2[i] = mem1[i] + 2 * mem2[i - 1];
+		last1 = last2 - last1;
+		last2 = last1 + 2 * last2;
 	}
 	if (n < 1)
 		cout << "0";
 	else
-		cout << mem2[n - 1];
+		cout << last2;
 }
