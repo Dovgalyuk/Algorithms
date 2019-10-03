4229f690db01a986feb8745e01a015e0bee4e61e
root <vladimir.popov2222@gmail.com>
2019-10-03 17:40:50 +0300

    fix

--- Library/list.cpp
+++ Library/list.cpp
@@ -28,8 +28,6 @@ void list_delete(List *list)
 		list->head = list->head->next;
 		delete tmp;
 	}
-	//ListItem *tmp = list->head;
-	//delete tmp;
 	delete list;
 }
 
@@ -60,21 +58,11 @@ ListItem *list_item_prev(ListItem *item)
 
 ListItem *list_insert(List *list, Data data)
 {
-	if (list->head == NULL)
-	{
-		ListItem *temp = new ListItem;
-		list->head = temp;
-		temp->data = data;
-		temp->next = NULL;
-		return temp;
-	}
-	else {
 		ListItem *a = new ListItem;
 		a->next = list->head;
 		a->data = data;
 		list->head = a;
 		return a;
-	}
 
 }
 
@@ -83,7 +71,6 @@ ListItem *list_insert_after(List *list, ListItem *item, Data data)
 	ListItem *tmp = new ListItem;
 	ListItem *mp;
 	tmp->data = data;
-	tmp->next = item;
 	mp = item->next;
 	item->next = tmp;
 	tmp->next = mp;
@@ -102,26 +89,15 @@ ListItem *list_erase(List *list, ListItem *item)
 		return mp;
 	}
 	else {
-	if (item->next == NULL)
-	{
-		ListItem *tmp = list->head;
-		ListItem *mp;
-		mp = tmp->next;
-		list->head = NULL;
-		delete tmp;
-		return mp;
-	}
 
 		ListItem *tmp = list->head;
 		while (tmp->next != item)
 		{
 			tmp = tmp->next;
 		}
-		ListItem *mp;
-		mp = item->next;
 		tmp->next = item->next;
 		delete item;
-		return mp;
+		return tmp->next;
 	}
 
 }
