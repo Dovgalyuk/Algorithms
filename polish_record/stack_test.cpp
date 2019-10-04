224708fcd259668f98a1106b85bb47056706fb03
abubaca4 <abubaca4@gmail.com>
2019-10-04 14:47:04 +0300

    test function style changed

--- polish_record/stack_test.cpp
+++ polish_record/stack_test.cpp
@@ -5,28 +5,28 @@
 void Stack_test()
 {
 	Stack<int>* test = new Stack<int>;
-	(*test).push(1);
-	(*test).push(2);
-	(*test).push(3);
-	if ((*test).get() != 3)
+	test->push(1);
+	test->push(2);
+	test->push(3);
+	if (test->get() != 3)
 		std::cout << "Invalid stack top after push\n";
-	std::cout << "Get: " << (*test).get() << "\n";
-	(*test).pop();
-	if ((*test).get() != 2)
+	std::cout << "Get: " << test->get() << "\n";
+	test->pop();
+	if (test->get() != 2)
 		std::cout << "Invalid stack top after pop\n";
-	std::cout << "Get: " << (*test).get() << "\n";
-	(*test).pop();
-	if ((*test).get() != 1)
+	std::cout << "Get: " << test->get() << "\n";
+	test->pop();
+	if (test->get() != 1)
 		std::cout << "Invalid stack top after pop\n";
-	std::cout << "Get: " << (*test).get() << "\n";
-	(*test).push(4);
-	(*test).push(5);
-	if ((*test).get() != 5)
+	std::cout << "Get: " << test->get() << "\n";
+	test->push(4);
+	test->push(5);
+	if (test->get() != 5)
 		std::cout << "Invalid stack top after push\n";
-	while (!(*test).empty())
+	while (!test->empty())
 	{
-		std::cout << "Get: " << (*test).get() << "\n";
-		(*test).pop();
+		std::cout << "Get: " << test->get() << "\n";
+		test->pop();
 	}
 	delete test;
 }
