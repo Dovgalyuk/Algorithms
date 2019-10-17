812cfe3076ce6fd45aca9d84bf6ce87aebeff4de
Fedor <54978262+Void-spirit@users.noreply.github.com>
2019-10-18 00:41:01 +0300

    Add files via upload

--- Library/list.cpp
+++ Library/list.cpp
@@ -3,57 +3,102 @@
 
 struct ListItem
 {
+	Data D;
+	ListItem *next = NULL;
+	ListItem *prev = NULL;
 };
 
 struct List
 {
+	ListItem *head;
 };
 
 List *list_create()
 {
-    return NULL;
+	List *list = new List;
+	list->head = NULL;
+	return list;
 }
 
 void list_delete(List *list)
 {
+	ListItem *p = list->head;
+	ListItem *next = NULL;
+	while (p) {
+		next = p->next;
+		delete p;
+		p = next;
+	}
+	delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+	return list->head;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return Data();
+	return item->D;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+	return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+	return item->prev;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+	ListItem *p = new ListItem;
+	p->D = data;
+	p->next = list->head;
+	if (list->head)
+		list->head->prev = p;
+	list->head = p;
+	return p;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+	ListItem *p = new ListItem;
+	if (item == NULL) {
+		return NULL;
+	}
+	p->D = data;
+	p->prev = item;
+	p->next = item->next;
+	if (item->next)
+		item->next->prev = p;
+	item->next = p;
+	return p;
 }
 
 ListItem *list_erase(List *list, ListItem *item)
 {
-    return NULL;
+	if (item->next && item->prev) {
+		item->prev->next = item->next;
+		item->next->prev = item->prev;
+	}
+	else if (item->next) {
+		list->head = item->next;
+		item->next->prev = NULL;
+	}
+	else if (item->prev) {
+		item->prev->next = NULL;
+	}
+	else
+		list->head = NULL;
+	ListItem *next = item->next;
+	delete item;
+	return next;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+	return list_erase(list, item->next);
 }
--- Library/stack.cpp
+++ Library/stack.cpp
@@ -1,33 +1,48 @@
 #include <stdlib.h>
 #include "stack.h"
+#include "list.h"
 
 struct Stack
 {
+	List *list;
 };
 
 Stack *stack_create()
 {
-    return NULL;
+	Stack *p = new Stack;
+	p->list = list_create();
+	return p;
 }
 
 void stack_delete(Stack *stack)
 {
+	list_delete(stack->list);
+	delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+	List *p = stack->list;
+	list_insert(p, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return Data();
+	if (stack_empty(stack) == false)
+		return list_item_data(list_first(stack->list));
+	return NULL;
 }
 
 void stack_pop(Stack *stack)
 {
+	List* p = stack->list;
+	list_erase(p, list_first(stack->list));
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+	if (list_first(stack->list) == NULL) {
+		return true;
+	}
+	else return false;
 }
