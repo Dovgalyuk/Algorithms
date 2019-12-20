fc8b3ee4316f953999254dcb67a1c74f9420500c
Fedor <54978262+Void-spirit@users.noreply.github.com>
2019-12-20 09:10:19 +0300

    Library

--- Library/queue.cpp
+++ Library/queue.cpp
@@ -1,33 +1,72 @@
 #include <stdlib.h>
 #include "queue.h"
+#include "vector.h"
 
 struct Queue
 {
+	Vector* vec;
+	size_t head, tail;
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
-    return NULL;
+	Queue* q = new Queue;
+	q->vec = vector_create();
+	q->head = q->tail = 0;
+	q->tail = 0;
+	return q;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
+	vector_delete(queue->vec);
+	delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+	if (queue->head == 0 && queue->tail + 1 == vector_size(queue->vec))
+	{
+		vector_resize(queue->vec, vector_size(queue->vec) * 2);
+	}
+	else
+		if (queue->tail + 1 == queue->head)
+		{
+			Vector* t = vector_create();
+			vector_resize(t, vector_size(queue->vec) * 2);
+			for (size_t i = queue->head; i < vector_size(queue->vec); i++)
+				vector_set(t, i - queue->head, vector_get(queue->vec, i));
+			for (size_t i = 0; i < queue->tail; i++)
+				vector_set(t, i + (vector_size(queue->vec) - queue->head), vector_get(queue->vec, i));
+			vector_delete(queue->vec);
+			queue->vec = t;
+			queue->tail = vector_size(t) / 2 - 1;
+			queue->head = 0;
+		}
+	vector_set(queue->vec, queue->tail, data);
+	if (queue->tail + 1 >= vector_size(queue->vec))
+		queue->tail = 0;
+	else
+		queue->tail++;
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-    return Data();
+	return vector_get(queue->vec, queue->head);
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+	if (queue->head == vector_size(queue->vec) - 1)
+		queue->head = 0;
+	else queue->head += 1;
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    return true;
+	if (queue->head == queue->tail)
+	{
+		return true;
+	}
+	else return false;
 }
--- Library/vector.cpp
+++ Library/vector.cpp
@@ -3,31 +3,81 @@
 
 struct Vector
 {
+	Data* arr;
+	size_t size, real_size;
 };
 
-Vector *vector_create()
+Vector* vector_create()
 {
-    return NULL;
+	Vector* vec = new Vector;
+	vec->arr = new Data[1];
+	vec->arr[0] = 0;
+	vec->size = 1;
+	vec->real_size = 1;
+	return vec;
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
+	delete[] vector->arr;
+	delete[] vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
+Data vector_get(const Vector* vector, size_t index)
 {
-    return Data();
+	return vector->arr[index];
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
+void vector_set(Vector* vector, size_t index, Data value)
 {
+	if (index < vector->real_size)
+	{
+		vector->arr[index] = value;
+	}
+	else if (index < vector->size)
+	{
+		vector->real_size = index;
+		vector->arr[index] = value;
+	}
+	else
+	{
+		vector_resize(vector, index * 2);
+		vector->real_size = index;
+		vector->arr[index] = value;
+	}
 }
 
-size_t vector_size(const Vector *vector)
+size_t vector_size(const Vector* vector)
 {
-    return 0;
+	return vector->real_size;
 }
 
-void vector_resize(Vector *vector, size_t size)
+void vector_resize(Vector* vector, size_t size)
 {
+	if (size == 0) {
+		vector->real_size = 0;
+		vector->arr[0] = 0;
+	}
+	else if (size < vector->size)
+	{
+		vector->real_size = size;
+	}
+	else
+	{
+		Data* arr2 = new Data[size];
+		for (int i = 0; i < vector->size; i++)
+		{
+			arr2[i] = vector->arr[i];
+		}
+		for (int i = vector->size; i < size; i++)
+		{
+			arr2[i] = 0;
+		}
+		Data* del = vector->arr;
+		vector->arr = arr2;
+		delete[] del;
+		vector->size = size;
+		vector->real_size = size;
+	}
+
 }
