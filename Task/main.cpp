f03739b0ba3519b8c2afaf1d66089e9a5aa45ff1
Dmitriy <dimka.fed01@mail.ru>
2020-09-04 17:04:49 +0300

    Удалён дополнительный массив

--- Task/main.cpp
+++ Task/main.cpp
@@ -6,30 +6,26 @@
 void print(Array* arr, size_t size)
 {	
 	for(int i = 0; i < size; i++)
-		std::cout << array_get(arr, i) << " ";
+		std::cout << array_get(arr, i) << " "; 
 	std::cout << std::endl;
 }
 
 void arrayCompressor(Array* arr, int a, int b, size_t arrSize)
 {
-	Array* newArray = array_create(arrSize);
 	size_t extraNumbers = 0;
-
 	for (int i = 0; i < arrSize; i++)
 	{
 		if (array_get(arr, i) >= a && array_get(arr, i) <= b)
 		{
 			extraNumbers++;
-			array_set(newArray, arrSize - extraNumbers, 0);
 			continue;
 		}
-		array_set(newArray, i - extraNumbers, array_get(arr, i));
+		array_set(arr, i - extraNumbers, array_get(arr, i));
 	}
+	
+	for (int i = arrSize - extraNumbers; i < arrSize; i++)
+		array_set(arr, i, 0);
 
-	for(int i = 0; i < arrSize; i++)
-		array_set(arr, i, array_get(newArray, i));
-
-	array_delete(newArray);
 }
 
 int main()
