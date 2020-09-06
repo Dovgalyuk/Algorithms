76260a17d670be2ee6bbfa8e6893ce08d866af8f
SirS2JeT <Slava_s53@mail.ru>
2020-09-06 19:31:31 +0300

    Индивидуальное Задание 1

---
+++ IndividualTask1/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+void ArrRandFill(Array* arr)
+{
+	for (size_t i = 0; i < array_size(arr); i++)
+		array_set(arr, i, rand() % 201 - 100);
+}
+
+void ArrPrint(Array* arr)
+{
+	for (size_t i = 0; i < array_size(arr); i++)
+		cout << array_get(arr, i) << " ";
+	cout << endl;
+}
+
+int ArrCheck(Array* arr)
+{
+	int count = 0;
+	for (size_t i = 0; i < array_size(arr); i++)
+		for (Data j = 2; j <= 9; j++)
+			if (array_get(arr, i) % j == 0)
+			{
+				count++;
+				break;
+			}
+	return count;
+}
+
+int main()
+{
+	int count;
+	size_t ArrSize;
+	cout << "Type array size: ";
+	cin >> ArrSize;
+	Array* arr = array_create(ArrSize);
+	ArrRandFill(arr);
+	ArrPrint(arr);
+	count = ArrCheck(arr);
+	cout << "Amount of numbers: " << count;
+	return 0;
+}
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -3,33 +3,41 @@
 
 struct Array
 {
+	size_t ArrSize;
+	Data* ArrData;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+	Array* NewArr = new Array;
+	NewArr->ArrSize = size;
+	NewArr->ArrData = new Data[size];
+	return NewArr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
-    delete arr;
+	delete[] arr->ArrData;
+	delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+	return arr->ArrData[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+	if (arr->ArrSize > index && index >= 0 )
+		arr->ArrData[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->ArrSize;
 }
