76a20784c523bd1b163ffbd6c0994c93ed2725d9
Znoleg <59749011+Znoleg@users.noreply.github.com>
2020-09-07 01:46:44 +0300

    Array class done in OOP style

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -7,32 +7,47 @@ public:
     // create array
     Array(size_t size)
     {
+        _elem = new Data[size];
+        _size = size;
+        for (int i = 0; i < size; i++)
+            _elem[i] = Data();
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] _elem;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return _elem[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        _elem[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return _size;
     }
 
+    Data& operator[](size_t index)
+    {
+        return _elem[index];
+    }
+    Data operator[](size_t index) const
+    {
+        return _elem[index];
+    }
 private:
-    // private data should be here
+    Data* _elem;
+    size_t _size;
 };
 
 #endif
--- TestArrayCPPTemplate/main.cpp
+++ TestArrayCPPTemplate/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+#include <algorithm>
 #include "array.h"
 
 typedef Array<int> MyArray;
+
+template<typename Data>
+void PrintArray(Array<Data>& toPrint)
+{
+    size_t arraySize = toPrint.size();
+    for (int i = 0; i < arraySize; i++)
+        std::cout << toPrint[i] << " ";
+}
+
+void Exec()
+{
+    srand(4541);
+    int n = -1;
+    while (n <= 0)
+    {
+        std::cout << "Enter size of your array: ";
+        std::cin >> n;
+        std::cout << std::endl;
+    }
+    MyArray arr(n);
+    for (int i = 0; i < n; i++)
+        arr[i] = rand();
+    int min = arr[0], max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        min = std::min(min, arr[i]);
+        max = std::max(max, arr[i]);
+    }
+    std::cout << "Minimal element: " << min << ' ' << "Maximum element: " << max << std::endl;
+}
 
 int main()
 {
@@ -10,6 +42,8 @@ int main()
     if (arr->size() != 10)
         std::cout << "Invalid array size\n";
 
+    PrintArray(*arr);
+
     for (int i = 0 ; i < 10 ; ++i)
         arr->set(i, i * 2);
 
@@ -17,5 +51,9 @@ int main()
         if (arr->get(i) != i * 2)
             std::cout << "Invalid array element " << i << "\n";
     
+    PrintArray(*arr);
+
     delete arr;
+
+    Exec();
 }
