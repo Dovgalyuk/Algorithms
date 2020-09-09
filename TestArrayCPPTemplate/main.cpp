fce0eb4c4f4c40fed83b74c4bccc436fe7310be3
Znoleg <59749011+Znoleg@users.noreply.github.com>
2020-09-09 23:52:52 +0300

    Fixed previous problems

--- TestArrayCPPTemplate/main.cpp
+++ TestArrayCPPTemplate/main.cpp
@@ -13,28 +13,6 @@ void PrintArray(Array<Data>& toPrint)
         std::cout << toPrint[i] << " ";
 }
 
-void Exec()
-{
-    srand(4541);
-    int n = -1;
-    while (n <= 0)
-    {
-        std::cout << "Enter size of your array: ";
-        std::cin >> n;
-        std::cout << std::endl;
-    }
-    MyArray arr(n);
-    for (int i = 0; i < n; i++)
-        arr[i] = rand();
-    int min = arr[0], max = arr[0];
-    for (int i = 0; i < n; i++)
-    {
-        min = std::min(min, arr[i]);
-        max = std::max(max, arr[i]);
-    }
-    std::cout << "Minimal element: " << min << ' ' << "Maximum element: " << max << std::endl;
-}
-
 int main()
 {
     MyArray *arr = new MyArray(10);
@@ -54,6 +32,4 @@ int main()
     PrintArray(*arr);
 
     delete arr;
-
-    Exec();
 }
