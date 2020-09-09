4d11c474569ca24acba08d82d233c8f51db1b1b6
3minbb <3minbb@users.noreply.github.com>
2020-09-09 12:28:18 +0300

    Update main.cpp

--- papka/main.cpp
+++ papka/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "array.h";
 int main()
 {
+	int size;
+	int max = 0;
+	int arr_e;
+	srand(time(0));
+	std::cout << "Enter the size:";
+	std::cin >> size;
+	Array* arr = array_create(size);
+	for (int i = 0; i < size; i++)
+		array_set(arr, i, rand() % 100 - 50);
+	for (int i = 0; i < size; i += 2) {
+		arr_e = array_get(arr, i);
+		if (arr_e > max)
+			max = arr_e;
+	}
+	std::cout << "Max element = " << max;
+	array_delete(arr);
 	return 0;
 }
