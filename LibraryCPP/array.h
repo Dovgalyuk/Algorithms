bdb73ed887329f4fba1c0dd52c118291b9162af2
IlyaSukharev01 <60602324+IlyaSukharev01@users.noreply.github.com>
2020-10-26 00:28:08 +0300

    Add files via upload

---
+++ LibraryCPP/array.h
@@ -0,0 +1,44 @@
+#ifndef ARRAY_TEMPLATE_H
+#define ARRAY_TEMPLATE_H
+
+template <typename Data> class Array
+{
+public:
+    // create array
+    Array(size_t size)
+    {
+        _elem = new Data[size];
+        _size = size;
+        for (int i = 0; i < size; i++)
+            _elem[i] = 0;
+    }
+
+    // delete array, free memory
+    ~Array()
+    {
+        delete[] _elem;
+    }
+
+    // returns specified array element
+    Data get(size_t index) const
+    {
+        return _elem[index];
+    }
+
+    // sets the specified array element to the value
+    void set(size_t index, Data value)
+    {
+        _elem[index] = value;
+    }
+
+    // returns array size
+    size_t size() const
+    {
+        return _size;
+    }
+private:
+    Data* _elem;
+    size_t _size;
+};
+
+#endif
