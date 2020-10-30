1a1cfd75c2653d1474112b9447dccd2799de2229
Godovarov-vn <bogovarov.senya@yandex.ru>
2020-10-30 19:14:43 +0300

    Homework dynamic array

--- LibraryArseny/main.cpp
+++ LibraryArseny/main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include "array.h"
+using namespace std;
+
 int main()
 {
- 
+	setlocale(LC_ALL, "Russian");
+	int size;
+	int positive = 0, negative = 0, zero = 0;
+
+	cout << "¬ведите размер массива : ";
+	cin >> size;
+	Array* arr = array_create(size);
+
+	cout << endl << "ћассив : " << endl;
+	for (int i = 0, x; i < size; i++)
+	{
+		x = rand() % 201 - 100;     // диапазон[-100;100]
+		cout << x << ' ';
+		array_set(arr, i, x);
+		if (x > 0)
+			positive++;
+		if (x < 0)
+			negative++;
+		if (x == 0)
+			zero++;
+	}
+
+	cout << endl << endl << "¬ массиве содержитс€ [" << positive << "] положительных, [" << negative << "] отрицательных и [" << zero << "] нулевых значений. " << endl;
+
+	array_delete(arr);
+
+	return 0;
 }
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -3,33 +3,43 @@
 
 struct Array
 {
+    size_t size;
+    Data* pArr;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    Array* new_arr = new Array;
+    new_arr->size = size;
+    new_arr->pArr = new Data[size];
+    return new_arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    delete[] arr->pArr;
     delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
+    if (0 <= index && index < arr->size)
+        return (arr->pArr)[index];
     return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (0 <= index && index < arr->size)
+        (arr->pArr)[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
