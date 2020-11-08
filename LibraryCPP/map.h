08107ff6d9516cd9bf6d6b78b6bbec674092828e
Dmitriy <dimka.fed01@mail.ru>
2020-11-08 20:28:50 +0300

    Добавил комментарии

--- LibraryCPP/map.h
+++ LibraryCPP/map.h
@@ -5,14 +5,19 @@
 
 struct Map;
 
+// Create new map
 Map* map_create();
 
+// Inserts a new data
 void map_insert(Map* mp, const Data& data, const Key& key);
 
+// Deletes a data by key
 void map_remove(Map* mp, const Key& key);
 
+// Returns data from map by key
 Data map_getData(Map* mp, const Key& key);
 
+// Delete map, free memory
 void map_delete(Map* mp);
 
 #endif
