c9815b99456ae928aae64090380d7acc921d03c8
IlyaSukharev01 <60602324+IlyaSukharev01@users.noreply.github.com>
2020-12-04 12:17:40 +0300

    Release Laba 1 v.2

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,5 +1,5 @@
-#ifndef ARRAY_TEMPLATE_H
-#define ARRAY_TEMPLATE_H
+#ifndef ARRAY_H
+#define ARRAY_H
 
 template <typename Data> class Array
 {
@@ -7,32 +7,38 @@ public:
     // create array
     Array(size_t size)
     {
+        _elem = new Data[size];
+        _size = size;
+        for (int i = 0; i < size; i++)
+            _elem[i] = 0;
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] _elem;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return _elem[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        _elem[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return _size;
     }
-
 private:
-    // private data should be here
+    Data* _elem;
+    size_t _size;
 };
 
 #endif
