12d768b99c411bcab0b4b08ecce35fd4d42b4ffb
3minbb <3minbb@users.noreply.github.com>
2020-12-22 16:53:18 +0300

    Update main.cpp

--- TestListC/main.cpp
+++ TestListC/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include "list.h"
-
+ 
 int main()
 {
-    List *list = list_create();
-
+    List *list = new List();
+ 
     if (!list)
         std::cout << "List creation error\n";
-
-    list_insert(list, 1);
-    list_insert(list, 2);
-    list_insert(list, 3);
-
-    if (list_item_data(list_first(list)) != 3)
+ 
+    list->push_front(1);
+    list->push_front(2);
+    list->push_front(3);
+ 
+    if (list->list_first()->list_item_data() != 3)
         std::cout << "list_insert error\n";
-
-    list_insert_after(list, list_first(list), 4);
-
-    if (list_item_data(list_item_next(list_first(list))) != 4)
+ 
+    list->push_front(4);
+ 
+    if (list->list_first()->list_item_data() != 4)
         std::cout << "list_insert_after error\n";
-
-    list_erase(list, list_first(list));
-
-    if (list_item_data(list_first(list)) != 4)
+ 
+    list->list_erase_next(list->list_first());
+ 
+    if (list->list_first()->list_item_data() != 4)
         std::cout << "list_erase error\n";
-
+ 
     std::cout << "List: ";
-    for (ListItem *item = list_first(list) ; item ; item = list_item_next(item))
+    for (ListItem *item = list->list_first() ; item ; item = item->list_item_next())
     {
-        std::cout << list_item_data(item) << " ";
+        std::cout << item->list_item_data() << " ";
     }
     std::cout << "\n";
     
-    list_delete(list);
+    delete list;
 }
