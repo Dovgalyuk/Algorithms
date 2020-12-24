0a402ba0750c91d213a975160edfa7098e696a54
IlyaSukharev01 <60602324+IlyaSukharev01@users.noreply.github.com>
2020-12-24 16:50:35 +0300

    Примечание
    Вынес определение в array.h, потому что были ошибки линковки

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -5,19 +5,37 @@ template <typename Data> class Array
 {
 public:
     // create array
-    Array(size_t size);
+    Array(size_t size)
+    {
+        _elem = new Data[size];
+        _size = size;
+        for (int i = 0; i < size; i++)
+            _elem[i] = 0;
+    }
 
     // delete array, free memory
-    ~Array();
+    ~Array()
+    {
+        delete[] _elem;
+    }
 
     // returns specified array element
-    Data get(size_t index) const;
+    Data get(size_t index) const
+    {
+        return _elem[index];
+    }
 
     // sets the specified array element to the value
-    void set(size_t index, Data value);
+    void set(size_t index, Data value)
+    {
+        _elem[index] = value;
+    }
 
     // returns array size
-    size_t size() const;
+    size_t size() const
+    {
+        return _size;
+    }
 
 private:
     Data* _elem;
