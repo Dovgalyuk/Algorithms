d39f5585c39dc5df83828ec04004858db167b341
NewExample <timurmanilov12@mail.ru>
2020-12-24 22:35:26 +0300

    Laba1
    Laba1

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -3,33 +3,48 @@
 
 struct Array
 {
+	int *data;
+	int size;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+	Array* arr = new Array;
+	arr->size = size;
+	arr->data = new int[size];
+	return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
-    delete arr;
+	delete[] arr->data;
+	delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+	if (0 <= index && index < arr->size)
+	{
+		return (arr->data)[index];
+	}
+	else return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+	if (0 <= index && index < arr->size)
+	{
+		(arr->data)[index] = value;
+	}
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
+
