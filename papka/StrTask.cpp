bd02c36b82d843979f76c05682a5908f32a69f86
3minbb <3minbb@users.noreply.github.com>
2020-12-29 18:55:41 +0300

    Update StrTask.cpp

--- papka/StrTask.cpp
+++ papka/StrTask.cpp
@@ -2,56 +2,56 @@
 #include <string>
 #include "stack.h"
 using namespace std;
-
+ 
 bool CheckRightStr(string str, const string openDelim = "([{", const string closeDelim = ")]}")
 {
-	Stack* stack = new Stack();
-	bool right = true;
-	for (int i = 0; i < str.size(); i++)
-	{
-		auto index = openDelim.find(str[i]);
-		if (index != string::npos)
-		{
-			stack->Push(str[i]);
-		}
-		else
-		{
-			if (!stack->Empty())
-			{
-				auto index = closeDelim.find(str[i]);
-				if (stack->Get() != openDelim[index])
-				{
-					right = false;
-					break;
-				}
-				else
-				{
-					stack->Pop();
-				}
-			}
-			else
-			{
-				right = false;
-				break;
-			}
-		}
-	}
-	
-	delete stack;
-	return right;
+    Stack* stack = new Stack();
+    bool right = true;
+    for (int i = 0; i < str.size(); i++)
+    {
+        auto index = openDelim.find(str[i]);
+        if (index != string::npos)
+        {
+            stack->Push(str[i]);
+        }
+        else
+        {
+            if (!stack->Empty())
+            {
+                auto index = closeDelim.find(str[i]);
+                if (stack->Get() != openDelim[index])
+                {
+                    right = false;
+                    break;
+                }
+                else
+                {
+                    stack->Pop();
+                }
+            }
+            else
+            {
+                right = false;
+                break;
+            }
+        }
+    }
+ 
+    right = right && stack->Empty();
+    delete stack;
+    return right;
 }
-
+ 
 void Say(bool value)
 {
-	if (value) cout << "YES";
-	else cout << "NO";
+    if (value) cout << "YES";
+    else cout << "NO";
 }
-
+ 
 int main()
 {
-	string toCheck;
-	cout << "Enter string to check: ";
-	cin >> toCheck;
-	Say(CheckRightStr(toCheck));
-	//Fixed for "(][)" it says YES, is it right? i think so because there was no condition about mark order
+    string toCheck;
+    cout << "Enter string to check: ";
+    cin >> toCheck;
+    Say(CheckRightStr(toCheck));
 }
