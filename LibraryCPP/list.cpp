7484bd3abd348aeca618b34a0bde645f4a6cb27a
guper100 <orestgupper100@gmail.com>
2020-12-29 14:10:00 +0300

    Реализован двусвязный список
    Не обратил внимания сразу

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -4,6 +4,7 @@
 struct ListItem
 {
     ListItem* next;
+    ListItem* prev;
     Data value;
 };
 
@@ -40,17 +41,19 @@ ListItem *list_item_next(ListItem *item)
     return item->next;
 }
 
-//ListItem *list_item_prev(ListItem *item)
-//{
-//    return NULL;
-//}
+ListItem *list_item_prev(ListItem *item)
+{
+    return item->prev;
+}
 
 ListItem *list_insert(List *list, Data data)
 {
     ListItem* item = new ListItem;
     item->value = data;
 
-    item->next = list->head;
+    ListItem* current_head = list->head;
+    if (current_head != nullptr) current_head->prev = item;
+    item->next = current_head;
     list->head = item;
 
     return item;
@@ -62,23 +65,35 @@ ListItem *list_insert_after(List *list, ListItem *item, Data data)
 	new_item->value = data;
 
 	new_item->next = item->next;
+    new_item->prev = item;
+    item->next->prev = new_item;
     item->next = new_item;
 
     return new_item;
 }
 
-//ListItem *list_erase(List *list, ListItem *item)
-//{
-//    return NULL;
-//}
+ListItem *list_erase(List *list, ListItem *item)
+{
+    ListItem* item_prev = item->prev;
+    ListItem* item_next = item->next;
+    if (item_prev != nullptr && item_next != nullptr)
+    {
+        item_prev->next = item->next;
+        item->next->prev = item->prev;
+    }
+    else if (item == list->head)
+    {
+        list_erase_first(list);
+    }
+
+	delete item;
+    return nullptr;
+}
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    ListItem* delete_item = item->next;
-    item->next = delete_item->next;
-
-	delete delete_item;
-    return item;
+    list_erase(list, item->next);
+    return nullptr;
 }
 
 ListItem *list_erase_first(List *list)
@@ -87,5 +102,5 @@ ListItem *list_erase_first(List *list)
     list->head = list->head->next;
 
 	delete delete_item;
-	return list->head;
+	return nullptr;
 }
