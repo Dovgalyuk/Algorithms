d56bfef9dffe705ebd9c83cbf7931b55ef40cc4e
Dmitriy <dimka.fed01@mail.ru>
2020-12-31 18:30:53 +0300

    fixes1

--- LibraryCPP/binaryHeap.h
+++ LibraryCPP/binaryHeap.h
@@ -3,30 +3,9 @@
 
 #include <stdlib.h>
 #include <iostream>
-
-struct HuffmanNode;
+#include "huffmanTree.h"
 
 struct BinaryHeap;
-
-HuffmanNode* huffman_createLeafNode(unsigned char symbol, unsigned long long int weight);
-
-HuffmanNode* huffman_createInternalNode(HuffmanNode* leftNode, HuffmanNode* rightNode);
-
-HuffmanNode* huffman_getLeftNode(HuffmanNode* node);
-
-HuffmanNode* huffman_getRightNode(HuffmanNode* node);
-
-void huffman_setLeftNode(HuffmanNode* node, HuffmanNode* newNode);
-
-void huffman_setRightNode(HuffmanNode* node, HuffmanNode* newNode);
-
-bool huffman_nodeIsLeaf(HuffmanNode* node);
-
-unsigned char huffman_getNodeChar(HuffmanNode* node);
-
-unsigned long long int huffman_getNodeWeight(HuffmanNode* node);
-
-HuffmanNode* huffman_deleteTree(HuffmanNode* node);
 
 BinaryHeap* binaryHeap_create(const size_t size);
 
@@ -49,7 +28,5 @@ unsigned char binaryHeap_getNodeSymbol(BinaryHeap* heap);
 size_t binaryHeap_getSize(BinaryHeap* heap);
 
 void binaryHeap_delete(BinaryHeap* heap);
-
-void binaryHeap_printTree(HuffmanNode* node, unsigned int height);
 
 #endif
---
+++ LibraryCPP/huffmanTree.h
@@ -0,0 +1,31 @@
+#ifndef HUFFMANTREE_H
+#define HUFFMANTREE_H
+
+#include <stdlib.h>
+#include <iostream>
+
+struct HuffmanNode;
+
+HuffmanNode* huffman_createLeafNode(unsigned char symbol, unsigned long long int weight);
+
+HuffmanNode* huffman_createInternalNode(HuffmanNode* leftNode, HuffmanNode* rightNode);
+
+HuffmanNode* huffman_getLeftNode(HuffmanNode* node);
+
+HuffmanNode* huffman_getRightNode(HuffmanNode* node);
+
+void huffman_setLeftNode(HuffmanNode* node, HuffmanNode* newNode);
+
+void huffman_setRightNode(HuffmanNode* node, HuffmanNode* newNode);
+
+bool huffman_nodeIsLeaf(HuffmanNode* node);
+
+unsigned char huffman_getNodeChar(HuffmanNode* node);
+
+unsigned long long int huffman_getNodeWeight(HuffmanNode* node);
+
+HuffmanNode* huffman_deleteTree(HuffmanNode* node);
+
+void huffman_printTree(HuffmanNode* node, unsigned int height);
+
+#endif
