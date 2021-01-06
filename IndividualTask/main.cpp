a2afbf29f35fd992a33b0e3809f36f5a7ba32fb8
Werwolf2085 <70657064+Werwolf2085@users.noreply.github.com>
2021-01-06 16:53:54 +0300

    Индивидуальное 2

--- IndividualTask/main.cpp
+++ IndividualTask/main.cpp
@@ -1,23 +1,71 @@
 #include <iostream>
-#include "array.h"
+#include <map>
+#include "stack.h"
+#include <fstream>
 
-typedef Array<int> MyArray;
 using namespace std;
 
-int main()
-{
-    srand(4541);
-    MyArray nums(size);
-    int size;
-    cin >> size;
-    for (int i = 0; i < size; i++)
-    {
-        nums[i] = rand();
+bool has_only_digits(const string& s){
+    return s.find_first_not_of( "0123456789" ) == string::npos;
+}
+
+void push_into_stack(Stack<int>& stack, map<char, int>& map, const string& second_part, const size_t& str_num){
+    if (second_part.length() == 1 && map.find(second_part[0]) != map.end()){
+        stack.push(map[second_part[0]]);
+    } else if(has_only_digits(second_part)) {
+        char* end;
+        stack.push(strtol(second_part.data(), &end, 10));
+    } else {
+        cout << "Unknown symbol at " << str_num <<" PUSH \"" + second_part + " \"" << endl;
     }
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-        sum += nums[i];
+}
+
+void pop_from_stack(Stack<int>& stack, map<char, int>& map, const string& second_part, const size_t& str_num){
+    if (second_part.length() == 1 && map.find(second_part[0]) != map.end()){
+        map[second_part[0]] = stack.top();
+        stack.pop();
+    } else {
+        cout << "Unknown symbol at " << str_num <<" POP \"" + second_part + " \"" << endl;
     }
-    cout << "Summa: " << sum;
+}
+
+int main() {
+
+    map<char, int> registers = map<char, int>{
+            {'A', 0},
+            {'B', 0},
+            {'C', 0},
+            {'D', 0}
+    };
+    Stack<int> processor_stack = Stack<int>();
+
+    ifstream input_file_stream("input.txt");
+    if (!input_file_stream){
+        perror("File \"input.txt\" missing");
+    }
+
+    for (std::size_t i = 1; !input_file_stream.eof(); ++i){
+        string buffer;
+        getline(input_file_stream, buffer);
+        int space_index = buffer.find(' ');
+        string words[] = {
+            buffer.substr(0, space_index),
+            buffer.substr(space_index + 1, buffer.length() - 1)
+        };
+
+        if (words[0] == "PUSH"){
+            push_into_stack(processor_stack, registers, words[1], i);
+        } else if (words[0] == "POP"){
+            pop_from_stack(processor_stack, registers, words[1], i);
+        } else {
+            cout << "Unknown symbol at " << i << " " << words[0] << " " <<  words[1] << endl;
+        }
+    }
+    for (auto& s : registers)
+    {
+        cout << s.first << " = " << s.second << endl;
+    }
+    input_file_stream.close();
+    system("pause");
+    return 0;
 }
