4e5b2178662d547e29ebf1a80095640ff9505f3b
Znol <olezhka.210@gmail.com>
2021-01-06 14:02:49 +0300

    Map fix&boost

--- LibraryCPPTemplate/HashMap.h
+++ LibraryCPPTemplate/HashMap.h
@@ -2,128 +2,147 @@
 #include <stdexcept>
 #include <array>
 #include <vector>
+#include <functional>
 #include "IContainer.h"
 
 using std::string;
 
+
 class HashMap : IContainer<string>
 {
 public:
-	HashMap(size_t capacity): _capacity(capacity)
-	{
-		_nodes.resize(capacity);
-	}
+    HashMap(size_t capacity) : _capacity(capacity)
+    {
+        _nodes.resize(capacity);
 
-	HashMap(): HashMap(10){ }
+        _conditions[0] = [](Node& node, string& key) {return node.status != NodeStatus::Full; }; // could do this in find_index() but doing it here gives us 0.001 boost :)
+        _conditions[1] = [](Node& node, string& key) {return node.key == key && node.status == NodeStatus::Full; };
+        _conditions[2] = [](Node& node, string& key) {return node.key == key && node.status == NodeStatus::Full; };
+    }
 
-	~HashMap() override = default;
-	
-	string Add(string key, string value) override
-	{
-		bool passed = false;
-		size_t index = get_index(key);
-		size_t indexR = index;
-		size_t end = _nodes.size();
+    HashMap() : HashMap(10) { }
 
-		for (index; index < end; index++)
-		{
-			Node* node = &_nodes[index];
-			if (node->status != NodeStatus::Full)
-			{
-				node->key = key;
-				node->value = value;
+    ~HashMap() override = default;
 
-				node->status = NodeStatus::Full;
-				return key;
-			}
+    string Add(string key, string value) override
+    {
+        size_t index = find_index(key, FindType::Add);
+        if (index == -1)
+        {
+            rehash();
+            index = find_index(key, FindType::Add);
+        }
 
-			if (index == end - 1)
-			{
-				if (!passed)
-				{
-					passed = true;
-					end = indexR;
-					index = 0;
-				}
-				else rehash();
-			}
-		}
+        Node* node = &_nodes[index];
+        node->key = key;
+        node->value = value;
 
-	}
+        node->status = NodeStatus::Full;
+        return key;
+    }
 
-	string Remove(string key) override
-	{
-		size_t index = get_index(key);
+    string Remove(string key) override
+    {
+        size_t index = find_index(key, FindType::Remove);
+        if (index == -1) throw std::logic_error("No proposed key in Delete() of hash_map");
 
-		for (index; index < _nodes.size(); index++)
-		{
-			Node* node = &_nodes[index];
-			if (node->key == key && node->status == NodeStatus::Full)
-			{
-				node->key = "";
-				node->value = "";
+        Node* node = &_nodes[index];
+        node->key = "";
+        node->value = "";
 
-				node->status = NodeStatus::Erased;
-				return "";
-			}
-		}
+        node->status = NodeStatus::Erased;
+        return "";
+    }
 
-		throw std::logic_error("No proposed key in Delete() of hash_map");
-	}
+    string Find(string key) override
+    {
+        size_t index = find_index(key, FindType::Find);
+        if (index == -1) throw std::logic_error("No proposed key in Find() of hash_map");
 
-	string Find(string key) override
-	{
-		size_t index = get_index(key);
+        return _nodes[index].value;
+    }
 
-		for (index; index < _nodes.size(); index++)
-		{
-			Node node = _nodes[index];
-			if (node.key == key && node.status == NodeStatus::Full)
-			{
-				return node.value;
-			}
-		}
-
-		throw std::logic_error("No proposed key in Find() of hash_map");
-	}
-	
 private:
-	enum class NodeStatus
-	{
-		Full, Empty, Erased
-	};
-	
-	struct Node
-	{
-		string key;
-		string value;
-		NodeStatus status = NodeStatus::Empty;
-	};
-	
-	size_t get_index(const string key) const
-	{
-		return (str_hash(key) *22543) % _capacity;
-	}
+    enum class NodeStatus
+    {
+        Full, Empty, Erased
+    };
 
-	void rehash()
-	{
-		auto old_nodes = _nodes;
+    enum class FindType
+    {
+        Add, Remove, Find
+    };
 
-		_nodes.clear();
-		_capacity *= 2;
-		_nodes.resize(_capacity);
-		
-		for (int i = 0; i < _capacity / 2; i++)
-		{
-			if (old_nodes[i].status == NodeStatus::Full)
-			{
-				Add(old_nodes[i].key, old_nodes[i].value);
-			}
-		}
-	}
+    struct Node
+    {
+        string key;
+        string value;
+        NodeStatus status = NodeStatus::Empty;
+    };
+
+    typedef std::function<bool(Node&, string&)> MyFunc;
+
+    size_t find_index(string& key, const FindType find_type)
+    {
+        size_t index = get_index(key);
+
+        MyFunc* condition;
+        if (find_type == FindType::Add)
+        {
+            condition = &_conditions[0];
+        }
+        else if (find_type == FindType::Remove)
+        {
+            condition = &_conditions[1];
+        }
+        else
+        {
+            condition = &_conditions[2];
+        }
+
+        size_t indexZ = do_circle((*condition), key, index, _nodes.size());
+        if (indexZ == -1)
+        {
+            indexZ = do_circle((*condition), key, 0, index);
+        }
+
+        return indexZ;
+    }
 	
-	size_t _capacity;
+    size_t get_index(const string key) const
+    {
+        return (str_hash(key) * 22543) % _capacity;
+    }
 
-	std::vector<Node> _nodes;
-	std::hash<string> str_hash = std::hash<string>{};
+    size_t do_circle(MyFunc& condition, string& key, size_t index, size_t end) // another 0.001 boost
+    {
+        for (index; index < end; index++)
+        {
+            Node& node = _nodes[index];
+            if (condition(node, key)) return index;
+        }
+        return -1;
+    }
+	
+    void rehash()
+    {
+        auto old_nodes = _nodes;
+
+        _nodes.clear();
+        _capacity *= 2;
+        _nodes.resize(_capacity);
+
+        for (int i = 0; i < _capacity / 2; i++)
+        {
+            if (old_nodes[i].status == NodeStatus::Full)
+            {
+                Add(old_nodes[i].key, old_nodes[i].value);
+            }
+        }
+    }
+
+    size_t _capacity;
+    std::vector<Node> _nodes;
+    MyFunc _conditions[3];
+    std::hash<string> str_hash = std::hash<string>{};
 };
