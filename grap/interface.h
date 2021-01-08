db332436eeb10eef3d168f1a92e19c6b735cdc57
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-01-08 13:37:19 +0300

    Update interface.h

--- grap/interface.h
+++ grap/interface.h
@@ -9,28 +9,28 @@ using namespace std;
 //1. ÈÍÒÅÐÔÅÉÑ
 class Interface {
 private:
-	vector <unsigned int> vertexList;				//Ñïèñîê âåðøèí ãðàôà
-	vector <List*> adjacencyList;					//Ñïèñîê ñìåæíîñòè
-	vector <unsigned int> labelVertex;				//Ìåòêà âåðøèí
+	vector <unsigned int> vertexList;				
+	vector <List*> adjacencyList;					
+	vector <unsigned int> labelVertex;			
 
 public:
-	Interface() {}									//Êîíñòðóêòîð
-	virtual ~Interface() {}							//Äåñòðóêòîð 
+	Interface() {}								
+	virtual ~Interface() {}						
 
 	//Ôóíêöèè
 	virtual unsigned int sizeGraph() = 0;
 	virtual int posVertex(unsigned int vertex) = 0;	
 	virtual unsigned int vertexGraph(unsigned int) = 0;	
-	virtual void printGraph() {};										//Âûâîä ãðàôà
-	virtual void addVertex(unsigned int) = 0;;										//Äîáàâèòü âåðøèíó
-	virtual void removeVertex(unsigned int) = 0;;										//Óäàëèòü âåðøèíó
-	virtual void addEdge(unsigned int, unsigned int, unsigned int) = 0;											//Äîáàâèòü ðåáðî
-	virtual void removeEdge(unsigned int, unsigned int) = 0;										//Óäàëèòü ðåáðî
-	virtual void existEdge(unsigned int, unsigned int) = 0;										//Ïðîâåðêà íà ñóùåñòâîâàíèå ðåáðà
-	virtual void addLabelVertex() {};									//Çàäàòü ìåòêó âåðøèíå
-	virtual unsigned int readLabelVertex(unsigned int) = 0;				//Ñ÷èòàòü ìåòêó âåðøèíû
-	virtual void addLabelEdge() {};										//Çàäàòü ìåòêó ðåáðó
-	virtual unsigned int readLabelEdge(unsigned int, unsigned int) = 0;	//Ñ÷èòàòü ìåòêó ðåáðà
+	virtual void printGraph() {};										
+	virtual void addVertex(unsigned int) = 0;;										
+	virtual void removeVertex(unsigned int) = 0;;										
+	virtual void addEdge(unsigned int, unsigned int, unsigned int) = 0;			
+	virtual void removeEdge(unsigned int, unsigned int) = 0;									
+	virtual void existEdge(unsigned int, unsigned int) = 0;										
+	virtual void addLabelVertex() {};									
+	virtual unsigned int readLabelVertex(unsigned int) = 0;				
+	virtual void addLabelEdge() {};										
+	virtual unsigned int readLabelEdge(unsigned int, unsigned int) = 0;	
 	virtual ListItem* neighborsVertex(unsigned int, ListItem*) = 0;
 };
 
