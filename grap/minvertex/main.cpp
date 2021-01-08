96633c998155bd3ae7f797b31b928368f3e5144e
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-01-08 13:53:31 +0300

    Update main.cpp

--- grap/minvertex/main.cpp
+++ grap/minvertex/main.cpp
@@ -1,35 +1,30 @@
 #include <locale>
 #include <iostream>
-
+-finput-charset=cp866 -fexec-charset=utf-8
 #include "../Graphs/graph.h"
 #include "../Graphs/graph.cpp"
 #include "../Graphs/list.cpp"
 
 using namespace std;
 
-//Àëãîðèòì Äåéêñòðû(êðàò÷àéøåãî ïóòè)
 void minimWay(Graph graph, unsigned int vertex) {
-	//Íà÷àëüíàÿ èíèöèàëèçàöèÿ
 	if (graph.posVertex(vertex) == -1) {
-		cout << "Данная вершина не существует в графе." << endl;
+		cout << "versh otsutstvuet." << endl;
 		return;
 	}
-	unsigned int sizeGraph = graph.sizeGraph();			//Ðàçìåð ãðàôà
-	vector<bool> visitVertex(sizeGraph, 0);				//Ïîñåùåííûå âåðøèíû
-	vector<vector<unsigned int>> minWay(sizeGraph);		//Êðàò÷àéøèå ïóòè
+	unsigned int sizeGraph = graph.sizeGraph();			
+	vector<bool> visitVertex(sizeGraph, 0);			
+	vector<vector<unsigned int>> minWay(sizeGraph);		
 	
-	//Ïðèñâîåíèå âñåì ýëåìåíòàì, êðîìå èñõîäíîé, ìàêñèìàëüíî áîëüøîå çíà÷åíèå
 		graph.addLabelVertex(vertex, 0);
 	for (unsigned int numVertex = 0; numVertex < sizeGraph;numVertex++) {
 		if (graph.vertexGraph(numVertex) != vertex) {
 		graph.addLabelVertex(graph.vertexGraph(numVertex), UINT_MAX);
 		}
 	}
-	//Îñíîâíîé öèêë
 	unsigned int vertexMinLabel;
 	unsigned int valueVertexMinLabel;
 	for (unsigned int numVertex = 0; numVertex < sizeGraph; numVertex++) {
-		//Âûáîð íå ïîñåùåííîé âåðøèíû ñ íàèìåíüøèì çíà÷åíèåì ìåòêè
 		valueVertexMinLabel = UINT_MAX;
 		for (unsigned int j = 0; j < sizeGraph;j++) {
 			if (visitVertex[j] == 0 && graph.readLabelVertex(graph.vertexGraph(j)) < valueVertexMinLabel) {
@@ -39,7 +34,6 @@ void minimWay(Graph graph, unsigned int vertex) {
 		}
 		visitVertex[graph.posVertex(vertexMinLabel)] = 1;
 
-		//Ïåðåáîð âñåõ ñîñåäíèõ âåðøèí è ïîäñ÷¸ò
 		ListItem *item = NULL;
 		for (item = graph.neighborsVertex(vertexMinLabel, item); item != NULL; item = graph.neighborsVertex(vertexMinLabel, item)) {
 			if (graph.readlabelVertex(list_item_data(item)[0]) > graph.readlabelVertex(vertexMinLabel) + list_item_data(item)[1]) {
@@ -59,7 +53,6 @@ void minimWay(Graph graph, unsigned int vertex) {
 		}
 	}
 
-	//Âûâîä ðåçóëüòàòîâ
 	for (unsigned int i = 0; i < sizeGraph; i++) {
 		if (graph.vertexList[i] != vertex) {
 			if (graph.readlabelVertex(graph.vertexGraph(i))!= UINT_MAX) {
@@ -76,15 +69,13 @@ void minimWay(Graph graph, unsigned int vertex) {
 	}
 }
 
-//ÃËÀÂÍÀß ÔÓÍÊÖÈß
 void main() {
 	setlocale(LC_ALL, "Russian");
 
-	//Èíèöèàëèçàöèÿ ãðàôà
-	cout << "ÍÀ×ÀËÜÍÀß ÈÍÈÖÈÀËÈÇÀÖÈß ÃÐÀÔÀ" << endl;
-	cout << "Ââåäèòå êîëè÷åñòâî âåðøèí ãðàôà: ";
-	Graph graph;						//Ñîçäà¸ì ãðàô
-	unsigned int vertex;				//Íîìåð âåðøèíû
+	cout << "initialising graph" << endl;
+	cout << "kol-vo vershin in graph: ";
+	Graph graph;						
+	unsigned int vertex;				
         graph.addVertex(1);
 	graph.addVertex(2);
 	graph.addVertex(3);
@@ -100,67 +91,92 @@ void main() {
 	graph.addEdge(5, 4, 4);*/
 
 	//Ìåíþ
-	unsigned int numItemMenu;	//Ïóíêò â ìåíþ
-	unsigned int firstVertex;	//Èñõîäíàÿ âåðøèíà ðåáðà
-	unsigned int secondVertex;	//Êîíå÷íàÿ âåðøèíà ðåáðà
-	unsigned int valueEdge;		//Çíà÷åíèå ìåòêè ðåáðà
+	unsigned int numItemMenu;	
+	unsigned int firstVertex;	
+	unsigned int secondVertex;	
+	unsigned int valueEdge;		
 	for (;;) {
 		system("cls");
-		cout << "ÏÐÅÄÑÒÀÂËÅÍÈÅ ÃÐÀÔÀ Â ÂÈÄÅ ÌÀÒÐÈÖÛ ÑÌÅÆÍÎÑÒÈ:" << endl;
+		cout << "graph matr smegnosti:" << endl;
 		graph.printGraph();
 		cout << endl;
 		cout << "ÌÅÍÞ" << endl;
-		cout << "1. Äîáàâèòü âåðøèíó." << endl;
-		cout << "2. Óäàëèòü âåðøèíó." << endl;
-		cout << "3. Äîáàâèòü ðåáðî." << endl;
-		cout << "4. Óäàëèòü ðåáðî" << endl;
-		cout << "5. Ïðîâåðèòü ðåáðî íà ñóùåñòâîâàíèå â ãðàôå." << endl;
-		cout << "6. Íàéòè êðàò÷àéøèé ïóòü îò îäíîé âåðøèíû äî âñåõ äðóãèõ." << endl;
-		cout << "7. Âûõîä." << endl;
+		cout << "1. add versh." << endl;
+		cout << "2. delete versh." << endl;
+		cout << "3. add rebro." << endl;
+		cout << "4. delete rebro" << endl;
+		cout << "5. proverka sushestvovania." << endl;
+		cout << "6. krath put ot vershini do drugih." << endl;
+		cout << "7. exit." << endl;
 		cout << ">";
 		cin >> numItemMenu;
 		switch (numItemMenu)
 		{
 		case 1:
-			cout << "Ââåäèòå íîìåð äëÿ íîâîé âåðøèíû: ";
+			cout << "vvedite nomer novoy vershini: ";
 			cin >> vertex;
-			graph.addVertex(vertex);
+			if(graph.addVertex(vertex)){
+		        cout << "vershina add." << endl;
+			}
+			else {
+			cout << "vershina est." << endl;
+			}
 			system("pause");
 			break;
 		case 2:
-			cout << "Ââåäèòå íîìåð âåðøèíû äëÿ óäàëåíèÿ: ";
+			cout << "vvedite nomer dla udalenia: ";
 			cin >> vertex;
-			graph.removeVertex(vertex);
+			if(graph.removeVertex(vertex)){
+				cout << "vershina udalena." << endl;
+			}
+			else {
+				cout << "vershina otsutstvuet." << endl;
+			}
 			system("pause");
 			break;
 		case 3:
-			cout << "Èñõîäíàÿ âåðøèíà: ";
+			cout << "ishodnaya vershina: ";
 			cin >> firstVertex;
-			cout << "Êîíå÷íàÿ âåðøèíà: ";
+			cout << "konechnaya vershina: ";
 			cin >> secondVertex;
-			cout << "Ìåòêà äëÿ äàííîãî ðåáðà: ";
+			cout << "metka dla dannogo rebra: ";
 			cin >> valueEdge;
-			graph.addEdge(firstVertex, secondVertex, valueEdge);
+			if (graph.addEdge(firstVertex, secondVertex, valueEdge)){
+				 cout << "rebro meshdu vershinami add." << endl;
+			}
+			else {
+				cout << "rebro meshdu vershinami uze est." << endl;
+			}
 			system("pause");
 			break;
 		case 4:
-			cout << "Èñõîäíàÿ âåðøèíà: ";
+			cout << "ishodnaya vershina: ";
 			cin >> firstVertex;
-			cout << "Êîíå÷íàÿ âåðøèíà: ";
+			cout << "konechnaya vershina: ";
 			cin >> secondVertex;
-			graph.removeEdge(firstVertex, secondVertex);
+		 if (graph.removeEdge(firstVertex, secondVertex)){
+			 cout << "rebro meshdu vershinami delete." << endl;
+			}
+			else {
+				cout << "rebro meshdu vershinami otsutstvuet." << endl;
+			}
 			system("pause");
 			break;
 		case 5:
-			cout << "Èñõîäíàÿ âåðøèíà: ";
+			cout << "ishodnaya vershina: ";
 			cin >> firstVertex;
-			cout << "Êîíå÷íàÿ âåðøèíà: ";
+			cout << "konechnaya vershina: ";
 			cin >> secondVertex;
-			graph.existEdge(firstVertex, secondVertex);
+		if (graph.existEdge(firstVertex, secondVertex)){
+			cout << "rebro meshdu vershinami est." << endl;
+			}
+			else {
+				cout << "rebro meshdu vershinami otsutstvuet." << endl;
+			}
 			system("pause");
 			break;
 		case 6:
-			cout << "Èñõîäíàÿ âåðøèíà: ";
+			cout << "ishodnaya vershina: ";
 			cin >> vertex;
 			minimWay(graph, vertex);
 			system("pause");
