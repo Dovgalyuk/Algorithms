243631ff2daa478efb95f1ffd28a3b9dfcd106bc
sergeychaliy2 <70662544+sergeychaliy2@users.noreply.github.com>
2021-01-10 21:26:01 +0300

    Update graph.h

--- grap/graph.h
+++ grap/graph.h
@@ -5,32 +5,31 @@
 #include "interface.h"
 
 class Graph :public Interface {
-public:
-	vector <unsigned int> vertexList;				//Список вершин графа
-	vector <List*> adjacencyList;					//Список смежности
-	vector <unsigned int> labelVertex;				//Метка вершин
-	vector <vector<unsigned int>> labelEdge;		//Метки рёбер
-
-	//Вспомогательные функции
-	unsigned int posVertex(unsigned int vertex);	//Получить позицию вершины в списке
-
-	//Конструктор
+private:
+	vector <unsigned int> vertexList;			
+	vector <List*> adjacencyList;				
+	vector <unsigned int> labelVertex;			
+	public:
+	//ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г°
 	Graph() {}
-	//Деструктор
+	//Г„ГҐГ±ГІГ°ГіГЄГІГ®Г°
 	virtual ~Graph() {}
 
-	//Функции
-	virtual unsigned int sizeGraph();																			//Размер графа
-	virtual void printGraph();																					//Вывод графа
-	virtual void addVertex(unsigned int vertex);																//Добавить вершину
-	virtual void removeVertex(unsigned int vertex);																//Удалить вершину
-	virtual void addEdge(unsigned int firstVertex, unsigned int secondVertex, unsigned int valueEdge);			//Добавить ребро
-	virtual void removeEdge(unsigned int firstVertex, unsigned int secondVertex);								//Удалить ребро
-	virtual void existEdge(unsigned int firstVertex, unsigned int secondVertex);								//Проверка на существование ребра
-	virtual void addLabelVertex(unsigned int vertex, unsigned int valueVertex);									//Задать метку вершине
-	virtual unsigned int readLabelVertex(unsigned int vertex);													//Считать метку вершины
-	virtual void addLabelEdge(unsigned int firstVertex, unsigned int secondVertex, unsigned int valueEdge);		//Задать метку ребру
-	virtual unsigned int readLabelEdge(unsigned int firstVertex, unsigned int secondVertex);					//Считать метку ребра
+	//Г”ГіГ­ГЄГ¶ГЁГЁ
+	virtual unsigned int sizeGraph();
+	virtual int posVertex(unsigned int vertex);																
+	virtual unsigned int vertexGraph(unsigned int);	//ГђГ Г§Г¬ГҐГ° ГЈГ°Г ГґГ 
+	virtual void printGraph();																
+	virtual void addVertex(unsigned int vertex);													
+	virtual void removeVertex(unsigned int vertex);											
+	virtual void addEdge(unsigned int firstVertex, unsigned int secondVertex, unsigned int valueEdge);		
+	virtual void removeEdge(unsigned int firstVertex, unsigned int secondVertex);							
+	virtual void existEdge(unsigned int firstVertex, unsigned int secondVertex);							
+	virtual void addLabelVertex(unsigned int vertex, unsigned int valueVertex);								
+	virtual unsigned int readLabelVertex(unsigned int vertex);									
+	virtual void addLabelEdge(unsigned int firstVertex, unsigned int secondVertex, unsigned int valueEdge);		
+	virtual unsigned int readLabelEdge(unsigned int firstVertex, unsigned int secondVertex);
+	virtual List<ListItem>::Iterator firstNeighborVertex(unsigned int vertex);
 };
 
 #endif
