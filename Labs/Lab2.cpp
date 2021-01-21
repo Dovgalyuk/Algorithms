ba13356eb9b6d0b370d24cfc586df703815a9eaf
Foxy1835 <foxy1835@bk.ru>
2021-01-21 15:13:15 +0300

    Лабораторная работа 2

---
+++ Labs/Lab2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include "stack.h"
+using namespace std;
+
+int main()
+{
+	string filename;
+	cout << "Enter filename: ";
+	cin >> filename;
+	fstream file(filename);
+	
+	Stack* stack = stack_create();
+	while (!file.eof())
+	{
+		string str;
+		file >> str;
+		for (int i = 0; i < str.size(); i++)
+		{
+			stack_push(stack, str[i]);
+		}
+		while (!stack_empty(stack))
+		{
+			cout << (char)stack_get(stack);
+			stack_pop(stack);
+		}
+		cout << endl;
+	}
+}
--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,86 @@
 
 struct ListItem
 {
+    Data value;
+    ListItem* next;
 };
 
 struct List
 {
+    ListItem* head;
 };
 
-List *list_create()
+List* list_create()
 {
-    return new List;
+    List* list = new List;
+    list->head = NULL;
+    return list;
 }
 
-void list_delete(List *list)
+void list_delete(List* list)
 {
-    // TODO: free items
+    ListItem* current = list->head;
+
+    while (current != NULL)
+    {
+        ListItem* prev = current;
+        current = list_item_next(prev);
+        delete prev;
+    }
+
     delete list;
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)
 {
+    return list->head;
+}
+
+Data list_item_data(const ListItem* item)
+{
+    return item->value;
+}
+
+ListItem* list_item_next(ListItem* item)
+{
+    return item->next;
+}
+
+ListItem* list_insert(List* list, Data data)
+{
+    ListItem* item = new ListItem;
+    item->value = data;
+
+    item->next = list->head;
+    list->head = item;
+
+    return item;
+}
+
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
+{
+    ListItem* new_item = new ListItem;
+    new_item->value = data;
+    new_item->next = item->next;
+    item->next = new_item;
+
+    return new_item;
+}
+
+ListItem* list_erase_next(List* list, ListItem* item)
+{
+    ListItem* delete_elem = item->next;
+    item->next = delete_elem->next;
+    delete delete_elem;
+
     return NULL;
 }
 
-Data list_item_data(const ListItem *item)
+ListItem* list_erase_head(List* list)
 {
-    return (Data)0;
-}
+    ListItem* delete_elem = list->head;
+    list->head = list_item_next(list->head);
+    delete delete_elem;
 
-ListItem *list_item_next(ListItem *item)
-{
-    return NULL;
-}
-
-ListItem *list_item_prev(ListItem *item)
-{
-    return NULL;
-}
-
-ListItem *list_insert(List *list, Data data)
-{
-    return NULL;
-}
-
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
-{
-    return NULL;
-}
-
-ListItem *list_erase(List *list, ListItem *item)
-{
-    return NULL;
-}
-
-ListItem *list_erase_next(List *list, ListItem *item)
-{
     return NULL;
 }
