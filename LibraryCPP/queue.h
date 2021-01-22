5cfba9cd6660cd9dddfda7443781f09f3490846f
Werwolf2085 <70657064+Werwolf2085@users.noreply.github.com>
2021-01-22 23:45:20 +0300

    Add files via upload

--- LibraryCPP/queue.h
+++ LibraryCPP/queue.h
@@ -1,30 +1,74 @@
-#ifndef QUEUE_H
-#define QUEUE_H
 
-// Queue
-// Stores integer values inside
-typedef int Data;
 
-struct Queue;
+#ifndef VECTORQUEUE_QUEUE_H
+#define VECTORQUEUE_QUEUE_H
 
-// Create empty queue
-Queue *queue_create();
+#include "Vector.h"
 
-// Deletes queue
-void queue_delete(Queue *queue);
+template<typename T>
+class Queue{
 
-// Includes new element into the queue
-// Should be O(1) on average
-void queue_insert(Queue *queue, Data data);
+    private:
+    //[_inner_vector] - вектор, на основе которого сделана очередь.
+    Vector<T>* _inner_vector;
+    //[_head_position] - индекс первогоэлемента очереди.
+    size_t _head_position;
 
-// Retrieves first element from the queue
-Data queue_get(const Queue *queue);
+    public:
+    //Стандартный конструктор.
+    Queue() : _inner_vector(new Vector<T>()), _head_position(0){}
 
-// Removes first element from the queue
-// Should be O(1) on average
-void queue_remove(Queue *queue);
+    //Деструктор с удалением вектора.
+    ~Queue() { delete _inner_vector;};
 
-// Returns true if the queue is empty
-bool queue_empty(const Queue *queue);
+    //Операция вставки в конец очереди.
+    //
+    //Суть метода заключается в том, что мы отслеживаем позицию первого элемента в очереди, вместо прямого его удаления.
+    //Когда происходит удаление из очереди, мы просто передвигаем этот индекс на единицу вперёд, если это возможно.
+    //(Когда есть, что удалять. Т.е. когда размер вектора != позиции первого элемента очереди)
+    //Когда же вектор заполнен, происходит перезаписывание вектора. Во время перезаписи перезаписываются только те элементы,
+    //которые имеют смысл (Т.е. стоят "правее" первого элемента очереди). А индекс вершины очереди становиться = 0, потому что
+    //в новом векторе больше не осталось ненужного мусора "слева".
+    //Среднеее время исполнения = О(1) - просто вставка по индексу или + 1 к индексу первого элемента в очереди, или доступ к элементу по индексу.
+    //Иногда будет происходить перезапись О(Размер вектора - Индекс первого элемента очереди).
+    //При малом размере очереди О Перезаписи -> О(1).
+    //При большом размере очереди необходимость перезаписи снижается, т.к. есть ещё место в буфере. Следовательно О Манипуляций -> О(1).
+    void insert(const T& value){
+        if (_inner_vector->is_full() && _head_position != 0){
+            Vector<T>* new_inner_vector = new Vector<T>(_inner_vector->size());
+            for (int i = _head_position; i < _inner_vector->size(); ++i){
+                new_inner_vector->push_back(_inner_vector->get(i));
+            }
+            _head_position = 0;
+            delete _inner_vector;
+            _inner_vector = new_inner_vector;
+        }
+        _inner_vector->push_back(value);
+    }
 
-#endif
+    //Удаление первого элемента очереди.
+    void remove(){
+        if (_inner_vector->size() != _head_position){++_head_position;}
+    }
+
+    //Доступ к первому элементу очереди.
+    T& get(){
+        return _inner_vector->get(_head_position);
+    }
+
+    //Проверка, пуста ли очередь.
+    bool empty(){
+        return _inner_vector->size() == _head_position;
+    }
+
+    //Очистка очереди.
+    void clear(){
+        _inner_vector->clear();
+    }
+
+    //Геттер текущего размера очереди.
+    size_t size(){return _inner_vector->size() - _head_position;}
+
+};
+
+#endif //VECTORQUEUE_QUEUE_H
