ddfdb9c65c5810764af0e6846d63aa1871396488
Kirill <kirillirikvideo@gmail.com>
2021-09-04 12:44:56 +0300

    feat: solution of the circuit problem

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,38 @@
 #include "array.h"
 
-struct Array
-{
+struct Array {
+    Data *data;
+    size_t arraySize;
+    Array(size_t arraySize) {
+        this->arraySize = arraySize;
+        this->data = new Data[arraySize];
+    }
+    ~Array() {
+        delete[] data;
+    }
 };
 
 // create array
-Array *array_create(size_t size)
-{
-    return new Array;
+Array *array_create(size_t size) {
+    return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
+void array_delete(Array *arr) {
     delete arr;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array *arr, size_t index) {
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array *arr, size_t index, Data value) {
+    arr->data[index] = value;
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array *arr) {
+    return arr->arraySize;
 }
---
+++ Result/circuit.cpp
@@ -0,0 +1,36 @@
+//
+// Created by KirilliriK on 04.09.2021.
+//
+
+#include <iostream>
+#include <random>
+#include "../LibraryCPP/array.h"
+
+void generateCircuit(Array *array);
+
+int main() {
+    int arraySize = 0;
+    std::cout << "Enter circuit size: " << std::endl;
+    std::cin >> arraySize;
+    Array *array = array_create(arraySize);
+    generateCircuit(array);
+
+    double result = 0;
+    for (size_t i = 0; i < array_size(array); i++) {
+        result += 1.0 / array_get(array, i);
+    }
+    result = std::pow(result, -1);
+
+    std::cout << "Total circuit resistance: " << result << std::endl;
+    return 0;
+}
+
+void generateCircuit(Array *array) {
+    std::random_device randomDevice;
+    std::mt19937 randomAlgorithm(randomDevice());
+    std::uniform_int_distribution<int> dist(1, 50);
+
+    for (size_t i = 0; i < array_size(array); i++) {
+        array_set(array, i, dist(randomAlgorithm));
+    }
+}
