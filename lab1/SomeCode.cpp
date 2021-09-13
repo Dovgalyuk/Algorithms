60095a5323ab2402db9dc5b3e82e9afeddd25bbf
Potapenko Egor <toptricker8@gmail.com>
2021-09-13 14:55:18 +0300

    Update SomeCode.cpp

--- lab1/SomeCode.cpp
+++ lab1/SomeCode.cpp
@@ -1,34 +1,37 @@
 ﻿#include <iostream>
-
 #include "array.h"
-int decade = 10;
-int sum = 0;
-int size = 0;
+#include <stdlib.h>
 
-void forecast(Array *arr) {  
-    int j = 20;
-    int c = 3;
-    for (int i = 0; i < 3; i++) {
-        for (j; j < size; j++) {
-            sum += array_get(arr, j);
+const int decade = 10;
+
+void forecast(Array* arr) {
+    int sum = 0;
+    int c = 1;
+    for (int j = 0; j < array_size(arr); j++) {
+        sum += array_get(arr, j);
+        if (j == 0) {
+            continue;
         }
-        std::cout << "decade " << c << ": " << sum / decade << std::endl;
-        c--;
-        j -= 20;
-        size -= 10;
-        sum = 0;
+        if ((j % 10) == 0) {
+            std::cout << "decade " << c << ": " << sum / decade << std::endl;
+            sum = 0;
+            c++;
+        }
     }
+
 }
 
 int main()
 {
+    int size = 0;
     srand(time(0));
     std::cout << "write down size of array" << std::endl;
     std::cin >> size;
-
-    Array *arr = array_create(size);
+    size++;
+    Array* arr = array_create(size);
     for (int i = 0; i < size; i++) {
         array_set(arr, i, 1 + rand() % 100);
     }
     forecast(arr);
+    array_delete(arr);
 }
