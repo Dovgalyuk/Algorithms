fea2997fe283cf2d9b39cd61ae062a1c26d57820
Golod-ru <tomredbrony@gmail.com>
2021-09-21 15:05:34 +0300

    Update lab1.cpp

--- laba/Lab1/lab1.cpp
+++ laba/Lab1/lab1.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <ctime>
-#include "array.cpp"
+#include "array.h"
 
 using namespace std;
 
@@ -36,50 +36,57 @@ void print(Array* array)
     cout << endl;
 }
 
-void searchA(Array* array)//Search for two-digit numbers
+void select(int* max, int* min, int mark)
+{
+    if (mark == 2)
+    {
+        cout << "search Search for two-digit numbers\n";
+        *min = 10;
+        *max = 100;
+    }
+
+    else if (mark == 3)
+    {
+        cout << "search Search for three-digit numbers\n";
+        *min = 100;
+        *max = 1000;
+    }
+}
+
+void search(Array* array, int* max, int* min)
 {
     int check = 0;
-    cout << "search two-digit numbers" << endl;
+
     for (int i = 0; i < array_size(array); i++)
     {
-        if ((array_get(array, i)>=10)&&(array_get(array, i) < 100))
+        if ((array_get(array, i) >= *min) && (array_get(array, i) < *max))
         {
             cout << "(" << i << ")" << "=" << array_get(array, i) << endl;
             check++;
         }
     }
+
     if (check == 0)
     {
         cout << "There are no two-digit numbers" << endl;
-    }
-}
-void searchB(Array* array)//Search for three-digit numbers
-{
-    int check = 0;
-    cout << "search three-digit numbers" << endl;
-    for (int i = 0; i < array_size(array); i++)
-    {
-        if ((array_get(array, i) >= 100) && (array_get(array, i) < 1000))
-        {
-            cout << "(" << i << ")" << "=" << array_get(array, i) << endl;
-            check++;
-        }
-    }
-    if (check == 0)
-    {
-        cout << "There are no three-digit numbers" << endl;
     }
 }
 
 
 int main()
 {
+    int maxS;
+    int minS;
     int a = size_for_array();
     Array* array = array_create(a);
     full(array);
     print(array);
-    searchA(array);
-    searchB(array);
+    select(&maxS, &minS, 2);
+    search(array, &maxS, &minS);
+
+    select(&maxS, &minS, 3);
+    search(array, &maxS, &minS);
+
     array_delete(array);
     return 0;
 }
