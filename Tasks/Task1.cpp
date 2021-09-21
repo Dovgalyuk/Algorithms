39180a6fa19ffdb3db011557cd183ebd700cc917
Programmesto <segga.sgg@gmail.com>
2021-09-21 15:04:29 +0300

    Task1 completed

---
+++ Tasks/Task1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "../LibraryCPP/array.h"
+
+void fill_arr(Array *arr) {   //fill array with students heighst
+    std::srand(532);
+    for (size_t i = 0; i < array_size(arr); i++) {
+        int h = std::rand()%40 + 150;
+        array_set(arr, i, h);
+    }
+}
+void print_arr(Array *arr) {
+    for (size_t i = 0; i < array_size(arr); i++) {
+        std::cout<<array_get(arr, i);
+        std::cout<<" ";
+    }
+    std::cout<<std::endl;
+}
+int count_higher_than_average_students(Array *arr) {
+    int avg = 0;
+    for (size_t i = 0; i < array_size(arr); i++) {
+        avg += array_get(arr, i);
+    }
+    avg /= array_size(arr);
+    int count = 0;
+    for (size_t i = 0; i < array_size(arr); i++) {
+        int h = array_get(arr, i);
+        if (h > avg) 
+            count++;
+    }
+    return count;
+}
+int main() {
+    std::cout<<"array size: ";
+    int size;
+    std::cin>>size;
+    Array *arr = array_create(size);
+    fill_arr(arr);
+    std::cout<<"array generated: \n";
+    print_arr(arr);
+    std::cout<< count_higher_than_average_students(arr) 
+        << " students are above average tall\n";
+    system("pause");
+    array_delete(arr);
+}
