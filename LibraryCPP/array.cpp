5f65d8aa44a7d4bb150d79f783286670f1cb3a72
onisken <aniskinartyom2002@gmail.com>
2021-09-28 00:20:29 +0300

    Revert "lab1"
    This reverts commit 29736668aa904f1513accec808cdc7ffe1a40f32.

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,26 +1,13 @@
 #include "array.h"
-#include <iostream>
+
 struct Array
 {
-    int *data;
-    size_t size;
-
-    Array(size_t size) 
-    {
-            this->size = size;
-        data = new int[size];
-    }
-
-    ~Array()
-    {
-        delete[] data;
-    }
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array(size);
+    return new Array;
 }
 
 // delete array, free memory
@@ -32,17 +19,16 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return arr->data[index];
+    return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
-    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return arr->size;
+    return 0;
 }
