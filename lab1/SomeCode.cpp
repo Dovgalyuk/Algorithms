8000019d1278ba6f2a0df1e80652bec181123d1c
Potapenko Egor <toptricker8@gmail.com>
2021-09-29 11:18:46 +0300

    Update
    Используется константа Decade;
    Убрано условие j==0;
    Исправлен выход за границы массива;

--- lab1/SomeCode.cpp
+++ lab1/SomeCode.cpp
@@ -1,18 +1,15 @@
 ﻿#include <iostream>
 #include "array.h"
 #include <stdlib.h>
-
 const int decade = 10;
+
 
 void forecast(Array* arr) {
     int sum = 0;
     int c = 1;
-    for (int j = 0; j <= array_size(arr); j++) {
+    for (int j = 0; j < array_size(arr); j++) {
         sum += array_get(arr, j);
-        if (j == 0) {
-            continue;
-        }
-        if ((j + 1) % 10) == 0) {
+        if (((j + 1) % decade) == 0) {
             std::cout << "decade " << c << ": " << sum / decade << std::endl;
             sum = 0;
             c++;
@@ -28,9 +25,10 @@ int main()
     std::cout << "write down size of array" << std::endl;
     std::cin >> size;
     Array* arr = array_create(size);
-    for (int i = 0; i <= size; i++) {
+    for (int i = 0; i < size; i++) {
         array_set(arr, i, 1 + rand() % 100);
     }
+
     forecast(arr);
     array_delete(arr);
 }
