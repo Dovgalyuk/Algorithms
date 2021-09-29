4e2f1b6dd29fbd8ef42f1b8b1117d53c55bc69df
onisken <aniskinartyom2002@gmail.com>
2021-09-30 00:06:49 +0300

    'lab1.1'

---
+++ results/lab1.cpp
@@ -0,0 +1,37 @@
+#include "../LibraryCPP/array.h"
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	int count1 = 0;
+	int count2 = 0;
+
+	int size;
+	std::cout << "Введите количество элементов: ";
+	std::cin >> size;
+
+	Array* arr = array_create(size);
+
+	for (int i = 0; i < size; i++)
+	{
+		array_set(arr, i, rand());
+
+		if (array_get(arr, i) % 2 == 0)
+			count1 += 1;
+		else if (array_get(arr, i) % 10 == 5)
+			count2 += 1;
+
+		std::cout << "Элемент номер " << (i + 1) << ": " << array_get(arr, i) << std::endl;
+
+	}
+
+	std::cout << std::endl << "Количество чётных чисел: " << count1 << std::endl;
+	std::cout << "Количество чисел с цифрой 5 на конце: " << count2 << std::endl;
+
+	array_delete(arr);
+
+	return 0;
+}
