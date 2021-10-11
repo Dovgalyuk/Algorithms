d2b6f2546c8eaa29fdc91be508a39dbd36395e36
Даниил <bacydaniil@gmail.com>
2021-10-11 18:13:32 +0300

    lab 1 finished

--- LibraryCPPTemplate/Tests/array.cpp
+++ LibraryCPPTemplate/Tests/array.cpp
@@ -1,28 +1,29 @@
+#include <iostream>
 #include "array.h"
-#include "iostream"
-#include "math.h"
-#include "time.h"
-#include "cstddef"
 
-int main() {
-    setlocale(LC_ALL, "cp1151");
-    srand(time(NULL));
-    int k = 0; // k - счетчик
-    int sum = 0; // sum - сумма
-    float x; // x - среднее арифметическое
-    Array<int> arr(10);
-    for (int i = 0; i < 10; i++) {
-        arr.set(i, rand()%100);
+typedef Array<int> MyArray;
+
+int main()
+{
+    MyArray *arr = new MyArray(10);
+
+    if (arr->size() != 10)
+    {
+        std::cout << "Invalid array size\n";
+        return 1;
     }
 
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr.get(i)>10) {
-            sum += arr.get(i);
-            k = k + 1;
+    for (int i = 0 ; i < 10 ; ++i)
+        arr->set(i, i * 2);
+
+    for (int i = 0 ; i < 10 ; ++i)
+    {
+        if (arr->get(i) != i * 2)
+        {
+            std::cout << "Invalid array element " << i << "\n";
+            return 1;
         }
     }
-    x = sum/k;
-    std::cout << "Avg " << x;
 
-    return 0;
+    delete arr;
 }
