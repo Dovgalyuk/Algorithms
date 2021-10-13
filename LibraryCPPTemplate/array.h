4781f1e78cda783d6ea4bda78a302c55c70fe67e
Даниил <bacydaniil@gmail.com>
2021-10-13 16:37:50 +0300

    updated folder lab1 with 1 file array.cpp array.h is moved to TestArrayCPPTemplate

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -11,39 +11,29 @@ public:
     {
 
     }
-
     // delete array, free memory
     ~Array()
     {
         delete []arr;
     }
-
     // returns specified array element
     Data get(size_t index) const
     {
         return arr[index];
     }
-
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
         arr[index] = value;
     }
-
     // returns array size
     size_t size() const
     {
         return _size;
     }
-
 private:
     // private data should be here
-
     Data* arr;
     size_t _size;
-
 };
-
-
-
 #endif
