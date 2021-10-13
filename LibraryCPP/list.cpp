36ca57a0528cc12bc4e4da17f141fa863956c83e
Lostmindd <coffeebrain666@gmail.com>
2021-10-13 20:55:51 +0300

    Update list.cpp

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,78 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem *next;
 };
 
 struct List
 {
+    ListItem *head;
+    List()
+    {
+        head = nullptr;
+    }
 };
 
-List *list_create()
+List* list_create()
 {
     return new List;
 }
 
-void list_delete(List *list)
+void list_delete(List* list)
 {
-    // TODO: free items
     delete list;
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)
 {
+    return list->head;
+}
+
+Data list_item_data(const ListItem* item)
+{
+    if(item)
+        return item->data; 
+}
+
+ListItem* list_item_next(ListItem* item)
+{
+    return item->next;
+}
+
+ListItem* list_item_prev(ListItem* item) 
+{
+    return NULL; // Not applicable for the singly linked lists.
+}
+
+ListItem* list_insert(List* list, Data data)
+{
+    ListItem* new_element = new ListItem;
+    new_element->data = data;
+    new_element->next = list->head;
+    list->head = new_element;
     return NULL;
 }
 
-Data list_item_data(const ListItem *item)
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
 {
-    return (Data)0;
-}
-
-ListItem *list_item_next(ListItem *item)
-{
+    ListItem* new_element = new ListItem;
+    new_element->data = data;
+    new_element->next = item->next;
+    item->next = new_element;
     return NULL;
 }
 
-ListItem *list_item_prev(ListItem *item)
+ListItem* list_erase(List* list, ListItem* item)
 {
-    return NULL;
+    return NULL; // Not applicable for the singly linked lists.
 }
 
-ListItem *list_insert(List *list, Data data)
+ListItem* list_erase_next(List* list, ListItem* item)
 {
-    return NULL;
-}
-
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
-{
-    return NULL;
-}
-
-ListItem *list_erase(List *list, ListItem *item)
-{
-    return NULL;
-}
-
-ListItem *list_erase_next(List *list, ListItem *item)
-{
+    ListItem* item_next = new ListItem;
+    item_next = item->next;
+    item->next = item->next->next;
+    delete item_next;
     return NULL;
 }
