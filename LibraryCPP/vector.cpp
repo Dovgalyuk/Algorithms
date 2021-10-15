57418eb9c2db77babc4aaab363a0afb90e1b0a79
Potapenko Egor <toptricker8@gmail.com>
2021-10-15 15:12:23 +0300

    Update vector.cpp
    Vector realisation

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,34 +1,63 @@
 #include "vector.h"
+#include <algorithm>
 
 struct Vector
 {
+    size_t vecSize = 0;
+    size_t vecCap = 0;
+    Data* vecData = 0;
+    Vector() {
+        vecData = new Data[1];
+        vecSize = 1;
+        vecCap = 0;
+    }
 };
 
-Vector *vector_create()
+Vector* vector_create()
 {
-    return new Vector;
+    return new Vector();
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
     // TODO: free vector internals
-    delete vector; 
+    delete[] vector->vecData;
+    delete[] vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
+Data vector_get(const Vector* vector, size_t index)
 {
-    return (Data)0;
+    return vector->vecData[index];
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
+void vector_set(Vector* vector, size_t index, Data value)
 {
+    vector->vecData[index] = value;
 }
 
-size_t vector_size(const Vector *vector)
+size_t vector_size(const Vector* vector)
 {
-    return 0;
+    return vector->vecSize;
 }
 
-void vector_resize(Vector *vector, size_t size)
+void vector_resize(Vector* vector, size_t size)
 {
+    if (size < vector->vecSize) {
+        vector->vecSize = size;
+    }
+    else {
+        while (vector->vecCap < size) {
+            if (vector->vecCap == 0) {
+                vector->vecCap = 1;
+            }
+            vector->vecCap *= 2;
+        }
+        Data* temp = new Data[vector->vecCap];
+        for (int i = 0; i < vector->vecSize; i++) {
+            temp[i] = vector->vecData[i];
+        }
+        delete[] vector->vecData;
+        vector->vecData = temp;
+        vector->vecSize = size;
+    }
 }
