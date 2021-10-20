1c20e6e6a6b3531bb8af137f0a80fb2e14888c69
MorozowSergey <90004812+MorozowSergey@users.noreply.github.com>
2021-10-20 13:27:21 +0300

    Revert "Revert "Update list.cpp""
    This reverts commit a172c99a95ce9dd32a5bd9515c11f3373382dcff.

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -34,28 +34,64 @@ ListItem *list_item_next(ListItem *item)
 {
     return NULL;
 }
+#include <cstddef>
+#include "list.h"
 
-ListItem *list_item_prev(ListItem *item)
+struct ListItem
 {
-    return NULL;
+};
+
+struct List
+{
+};
+
+List* list_create()
+{
+	return new List;
 }
 
-ListItem *list_insert(List *list, Data data)
+void list_delete(List* list)
 {
-    return NULL;
+	// TODO: free items
+	delete list;
 }
 
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
+ListItem* list_first(List* list)
 {
-    return NULL;
+	return NULL;
 }
 
-ListItem *list_erase(List *list, ListItem *item)
+Data list_item_data(const ListItem* item)
 {
-    return NULL;
+	return (Data)0;
 }
 
-ListItem *list_erase_next(List *list, ListItem *item)
+ListItem* list_item_next(ListItem* item)
 {
-    return NULL;
+	return NULL;
+}
+
+ListItem* list_item_prev(ListItem* item)
+{
+	return NULL;
+}
+
+ListItem* list_insert(List* list, Data data)
+{
+	return NULL;
+}
+
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
+{
+	return NULL;
+}
+
+ListItem* list_erase(List* list, ListItem* item)
+{
+	return NULL;
+}
+
+ListItem* list_erase_next(List* list, ListItem* item)
+{
+	return NULL;
 }
