90d2b5b7be2c254c049940f3a0f9014943f94315
Lostmindd <coffeebrain666@gmail.com>
2021-10-22 15:10:13 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,50 @@
 #include "stack.h"
+#include "list.h"
 
 struct Stack
 {
+    List* stack;
+    Stack()
+    {
+        stack = list_create();
+    }
 };
 
-Stack *stack_create()
+Stack* stack_create()
 {
     return new Stack;
 }
 
-void stack_delete(Stack *stack)
+void stack_delete(Stack* stack)
 {
-    // TODO: free stack elements
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
+void stack_push(Stack* stack, Data data)
 {
+    list_insert(stack->stack, data);
 }
 
-Data stack_get(const Stack *stack)
+Data stack_get(const Stack* stack)
 {
-    return (Data)0;
+    return list_item_data(list_first(stack->stack));
 }
 
-void stack_pop(Stack *stack)
+void stack_pop(Stack* stack)
 {
+    if (stack_empty(stack) == false)
+    {
+        ListItem* second_element = list_item_next(list_first(stack->stack));
+        delete list_first(stack->stack);
+        stack->stack->head = second_element;
+
+    }
 }
 
-bool stack_empty(const Stack *stack)
+bool stack_empty(const Stack* stack)
 {
-    return true;
+    if (list_first(stack->stack) == nullptr)
+        return true;
+    else
+        return false;
 }
