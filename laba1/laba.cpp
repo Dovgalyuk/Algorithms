2a84ec66e139cfc74c75fe92e79d5f91aa770ffe
1pampuz1 <Maksmen38@yandex.ru>
2021-10-26 15:22:32 +0300

    1 algorithm

--- laba1/laba.cpp
+++ laba1/laba.cpp
@@ -5,26 +5,43 @@
 #include <climits>
 #include <ctime>
 #include "array.h"
-int main()
-{
-    srand(time (0));
+
+int main() {
+    srand(time(0));
     int a = 0, b = 0;
     std::cin >> a >> b;
-    Array* arr = array_create(10);
-    for (int i = 0; i <  array_size(arr); i++)
-    {
+    Array *arr = array_create(10);
+    for (int i = 0; i < array_size(arr); i++) {
         array_set(arr, i, rand() % 1000 - 100);
     }
 
-    for(int i = 0; i <  array_size(arr); i++)
-    {
-        if(array_get(arr,i) >= a && array_get(arr,i) <= b)
-        {
+    for (int i = 0; i < array_size(arr); i++) {
+        if (array_get(arr, i) >= a && array_get(arr, i) <= b) {
             array_set(arr, i, 0);
         }
     }
-    for (int i = 0; i < array_size(arr); i++)
-    {
+
+    int size = array_size(arr);
+    for (int i = 0; i < size - 1; ++i) {
+
+        if (array_get(arr, i) == 0) {
+
+            int firstNonZero = i + 1;
+            for (int j = firstNonZero; j < size; ++j) {
+                if (array_get(arr, j) != 0) {
+                    firstNonZero = j;
+                    break;
+                }
+            }
+            int num = array_get(arr, firstNonZero);
+            array_set(arr, i, num);
+            array_set(arr, firstNonZero, 0);
+        }
+
+
+    }
+
+    for (int i = 0; i < array_size(arr); i++) {
         std::cout << array_get(arr, i) << " ";
     }
     array_delete(arr);
