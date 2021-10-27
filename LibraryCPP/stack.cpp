b789d735fb41837469524efcc6f47950607904aa
Lostmindd <coffeebrain666@gmail.com>
2021-10-27 22:19:15 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,12 +1,40 @@
 #include "stack.h"
 #include "list.h"
 
+struct ListItem
+{
+    ListItem* next;
+    void clear()
+    {
+        if (next != nullptr)
+        {
+            next->clear();
+            delete next;
+        }
+    }
+};
+
+struct List   //пришлось добавить сюда прототипы, иначе деструкторы не
+{             //вызываються и список не удаляется 
+    ListItem* head;
+    ~List()
+    {
+        if (head != nullptr)
+            head->clear();
+        delete head;
+    }
+};
+
 struct Stack
 {
     List* stack;
     Stack()
     {
         stack = list_create();
+    }
+    ~Stack()
+    {
+        delete stack;
     }
 };
 
@@ -34,16 +62,11 @@ void stack_pop(Stack* stack)
 {
     if (stack_empty(stack) == false)
     {
-        ListItem* second_element = list_item_next(list_first(stack->stack));
-        delete list_first(stack->stack);
-        stack->stack->head = second_element;
+        list_erase_first(stack->stack);
     }
 }
 
 bool stack_empty(const Stack* stack)
 {
-    if (list_first(stack->stack) == nullptr)
-        return true;
-    else
-        return false;
+        return list_first(stack->stack) == nullptr;
 }
