8de1fcf27f03622e4d66f8702ede89066e257132
Golod-ru <tomredbrony@gmail.com>
2021-10-31 14:24:26 +0300

    Lab2
    2

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,34 +1,59 @@
+#include <stdlib.h>
 #include "vector.h"
+#include <iostream>
 
 struct Vector
 {
+    Data* data;
+    size_t size;
 };
 
-Vector *vector_create()
+Vector* vector_create()
 {
-    return new Vector;
+    Vector* vec = new Vector;
+    vec->size = 0;
+    vec->data = new Data[0];
+    return vec;
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
-    // TODO: free vector internals
-    delete vector; 
+    delete[] vector->data;
+    delete vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
+Data vector_get(const Vector* vector, size_t index)
 {
-    return (Data)0;
+    return vector->data[index];
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
+void vector_set(Vector* vector, size_t index, Data value)
 {
+    if (index >= 0 && index <= vector->size)
+    {
+        vector->data[index] = value;
+    }
 }
 
-size_t vector_size(const Vector *vector)
+size_t vector_size(const Vector* vector)
 {
-    return 0;
+    return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
+void vector_resize(Vector* vector, size_t size)
 {
+    if (size < vector->size)
+        vector->size = size;
+    else {
+        size_t newSize = size * 2;
+        Data* newVector = new Data[newSize];
+        for (int i = 0; i < vector->size; i++)
+        {
+            newVector[i] = vector->data[i];
+        }
+        delete[] vector->data;
+        vector->data = newVector;
+        vector->size = size;
+        //vector->rSize = newSize;
+    }
 }
---
+++ laba/lab2/Laba2v2.2.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include "vector.h"
+#include "stack.h"
+
+using namespace std;
+
+void cheak(Stack* str,string st);
+
+
+int main()
+{
+	Stack *str = stack_create();
+	string cha;
+	cout << "Give me string. Only ()[]{}''\"\"" << endl;
+	cin >> cha;
+	//stack_push(str, cha[0]);
+	cheak(str, cha);
+	if (stack_empty(str))
+		cout << "YES";
+	else
+		cout << "NO";
+	stack_delete(str);
+
+}
+
+void cheak(Stack* str, string st)
+{
+	bool c = 0;
+	bool b = 0;
+	for (int i = 0; i < st.size(); i++)
+	{
+		if (st[i] == '(' || st[i] == '[' || st[i] == '{')
+		{
+			stack_push(str, st[i]);
+		}
+
+		else if(st[i]==')')
+		{
+			if (stack_get(str) == '(')
+				stack_pop(str);
+			else
+				break;
+		}
+
+		else if (st[i] == ']')
+		{
+			if (stack_get(str) == '[')
+				stack_pop(str);
+			else
+				break;
+		}
+
+		else if (st[i] == '}')
+		{
+			if (stack_get(str) == '{')
+				stack_pop(str);
+			else
+				break;
+		}
+
+		else if (st[i] == '\'')
+		{
+			if (c!=true)
+			{
+				stack_push(str, st[i]);
+				c=true;
+			}
+			else if (stack_get(str) == '\'')
+			{
+				stack_pop(str);
+				c = false;
+			}
+			else
+				break;
+		}
+
+		else if (st[i] == '\"')
+		{
+			if (b !=true)
+			{
+				stack_push(str, st[i]);
+				b=true;
+			}
+			else if (stack_get(str) == '\"')
+			{
+				stack_pop(str);
+				b = false;
+			}
+			else
+				break;
+		}
+	}
+}
