5511c274deff381ddc53310856fd099cfe7e7f73
Lostmindd <coffeebrain666@gmail.com>
2021-11-07 23:14:00 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,52 @@
 #include "queue.h"
+#include "vector.h"
 
 struct Queue
 {
+    size_t begin;
+    size_t end;
+    Vector* Q;
+    Queue()
+    {
+        begin = 0;
+        end = 0;
+        Q = vector_create();
+    }
+    ~Queue()
+    {
+        vector_delete(Q);
+    }
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
     return new Queue;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
-    // TODO: free queue items
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    vector_resize(queue->Q, vector_size(queue->Q) + 1);
+    queue->end++;
+    vector_set(queue->Q, queue->end - 1, data);
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-    return (Data)0;
+    return vector_get(queue->Q, queue->begin);
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+    if (queue->begin < queue->end)
+        queue->begin++;
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    return true;
+    return vector_get(queue->Q, queue->begin) == vector_get(queue->Q, queue->end);
 }
