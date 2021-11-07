9329958bc33ec564b539fc15a8c93cecf6ca47a0
Lostmindd <coffeebrain666@gmail.com>
2021-11-07 23:14:07 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,33 +2,64 @@
 
 struct Vector
 {
+    size_t size;
+    size_t capacity;
+    Data* pointer;
+    Vector()
+    {
+        size = 0;
+        capacity = 0;
+        pointer = nullptr;
+    }
+    ~Vector()
+    {
+        delete pointer;
+        pointer = nullptr;
+    }
 };
 
-Vector *vector_create()
+Vector* vector_create()
 {
     return new Vector;
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
-    // TODO: free vector internals
-    delete vector; 
+    delete vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
+Data vector_get(const Vector* vector, size_t index)
 {
-    return (Data)0;
+    return vector->pointer[index];
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
+void vector_set(Vector* vector, size_t index, Data value)
 {
+    vector->pointer[index] = value;
 }
 
-size_t vector_size(const Vector *vector)
+size_t vector_size(const Vector* vector)
 {
-    return 0;
+    return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
+void vector_resize(Vector* vector, size_t size)
 {
+    if (size == vector->size) return;
+    else if (size > vector->size)
+    {
+        if (size > vector->capacity)
+        {
+            if (size < 5) vector->capacity = size;
+            else vector->capacity = size + ((size - 3) / 2);
+            int* ptr = new int[vector->capacity]();
+            for (int i = 0; i < vector->size; i++)
+                ptr[i] = vector->pointer[i];
+            vector->size = size;
+            delete vector->pointer;
+            vector->pointer = ptr;
+        }
+        else vector->size = size;
+    }
+    else vector->size = size;
 }
