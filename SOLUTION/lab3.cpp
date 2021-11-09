13fa9a816df75a49ca01bb6b26b35875cc85ff25
Lostmindd <coffeebrain666@gmail.com>
2021-11-09 13:26:33 +0300

    Update lab3.cpp

--- SOLUTION/lab3.cpp
+++ SOLUTION/lab3.cpp
@@ -15,25 +15,25 @@ int main()
 	bool* was_in_queue = new bool[n]();
 	int* distance = new int[n]();
 	queue_insert(queue, 0);
-	was_in_queue[0] = 1;
+	was_in_queue[0] = true;
 	while (!queue_empty(queue))
 	{
 		int line = queue_get(queue);
 		queue_remove(queue);
 		for (int j = 0; j < n; j++)
 		{
-			if (arr[(line * n) + j] == 1 && was_in_queue[j] == 0)
+			if (arr[(line * n) + j] == 1 && was_in_queue[j] == false)
 			{
 				queue_insert(queue, j);
 				distance[j] += distance[line] + 1;
-				was_in_queue[j] = 1;
+				was_in_queue[j] = true;
 			}
 		}
 	}
 	std::cout << arr[0] << "\n";
 	for (int i = 1; i < n; i++)
 	{
-		if (was_in_queue[i] == 0) std::cout << "none" << "\n";
+		if (was_in_queue[i] == false) std::cout << "none" << "\n";
 		else std::cout << distance[i] << "\n";
 	}
 	delete[] was_in_queue;
