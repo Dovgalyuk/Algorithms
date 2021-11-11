1dcd2a1b00cd03dce6030618ce2f8febcbb94be6
MorozowSergey <90004812+MorozowSergey@users.noreply.github.com>
2021-11-11 13:40:07 +0300

    1

--- out/build/x64-Debug/LibraryCPPTemplate/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Сергей/Documents/GitHub/Algorithms/LibraryCPPTemplate
-# Build directory: C:/Users/Сергей/Documents/GitHub/Algorithms/out/build/x64-Debug/LibraryCPPTemplate
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
