47b2feb3138117d9bddf2f4a185bb7e5bc73e3c9
Vasilyev_Dmitry <podumattolkodmitry@mail.ru>
2021-11-11 16:15:19 +0300

    Lab is complete

--- Third/main.cpp
+++ Third/main.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
 #include <fstream>
-#include <queue.h>
-
+#include "queue.h"
 using namespace std;
 
+bool contains(Queue<char> *queue, string secondStr) {
+    int i = 0;
+    while (!queue->empty()){
+        if (queue->get() == secondStr[i])
+            {
+                queue->remove();
+                
+                if (i == secondStr.length() -1) {
+                    return true;
+                }
+                i++;
+            }
+            else {
+                queue->remove();
+                i = 0;
+            }
+    }
+    return false;
+}
+
 int main()
-{   auto* queue = new Queue<string>;
+{   auto* queue = new Queue<char>;
     char sim;
-    ifstream myfile("file.txt");
-    if (!myfile.is_open())
+    string secondStr;
+    ifstream myfile("V:/COD/VS/Algorithms/Third/file.txt");
+    if (myfile.is_open())
     {
         while (myfile.get(sim))
         {
-            queue->insert(sim);
+            if (sim == '\n'){
+                break;
+            }
+            queue->insert(sim); 
+        }
+        while (myfile.get(sim)) {
+            secondStr += sim;
         }
         myfile.close();
+        cout << secondStr + "\n";
+        if (contains(queue, secondStr)) {
+            cout << "\nYES" << endl;
+        }
+        else {
+            cout << "\nNO" << endl;
+        }            
     }
-    else cout << "Unable to open file";
+    else {
+        cout << "Unable to open file";
+    }
     
     return 0;
 }
