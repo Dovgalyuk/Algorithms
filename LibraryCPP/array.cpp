9724f72369bcf107e9cbff70fb27bcfc741f6d12
MrMike1209 <gutik4913@ya.ru>
2021-11-15 16:24:04 +0300

    Fix

---
+++ LibraryCPP/array.cpp
@@ -0,0 +1,49 @@
+#include "array.h"
+#include <iostream>
+struct Array
+{
+	int* data;
+	size_t size;
+
+
+	Array(size_t size)
+	{
+		this->size = size;
+		data = new int[size];
+	}
+
+	~Array()
+	{
+		delete[] data;
+	}
+};
+
+// create array
+Array* array_create(size_t size)
+{
+	return new Array(size);
+}
+
+// delete array, free memory
+void array_delete(Array* arr)
+{
+	delete arr;
+}
+
+// returns specified array element
+Data array_get(const Array* arr, size_t index)
+{
+	return arr->data[index];
+}
+
+// sets the specified array element to the value
+void array_set(Array* arr, size_t index, Data value)
+{
+	arr->data[index] = value;
+}
+
+// returns array size
+size_t array_size(const Array* arr)
+{
+	return arr->size;
+}
