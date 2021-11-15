089343e5fc624d88254727beda55d8b4458519e6
Даниил <bacydaniil@gmail.com>
2021-11-15 15:29:17 +0300

    updated testStack.cpp

--- lab2/testStack.cpp
+++ lab2/testStack.cpp
@@ -5,14 +5,21 @@
 
 typedef Stack<int> steck;
 
-void readFile(steck registr, int A[], int B[], int C[], int D[]){
+int main() {
+    steck registr;
+    int A[1] = {0},
+        B[1] = {0},
+        C[1] = {0},
+        D[1] = {0};
+
     std::string _string;
     std::ifstream in("E:\\text.txt");
+
     if(in.is_open()) {
         while (!in.eof()) {
             std::getline(in, _string);
             if (!_string.find("PUSH ")) {
-                _string.erase(0, 5);
+                _string = _string[5];
                 if (!_string.find('A'))
                     registr.push(A[0]);
                 else if (!_string.find('B'))
@@ -24,41 +31,31 @@ void readFile(steck registr, int A[], int B[], int C[], int D[]){
                 else
                     registr.push(atoi(_string.c_str()));
             }
-            if (!_string.find("POP")) {
-                _string.erase(0,4);
-                if(!_string.find('A')) {
-                    A[0] = registr.get();
-                    registr.pop();
-                }
-                else if (!_string.find('B')) {
-                    B[0] = registr.get();
-                    registr.pop();
-                }
-                else if (!_string.find('C')) {
-                    C[0] = registr.get();
-                    registr.pop();
-                }
-                else if (!_string.find('D')) {
-                    D[0] = registr.get();
-                    registr.pop();
-                }
-                else {
-                    std::cout << "Error";
-                    in.eof();
-                }
+            if(registr.empty()) {
+                std::cout << "Stack is empty";
+                break;
             }
+                if (!_string.find("POP")) {
+                    _string = _string[4];
+                    if (!_string.find('A')) {
+                        A[0] = registr.get();
+                        registr.pop();
+                    } else if (!_string.find('B')) {
+                        B[0] = registr.get();
+                        registr.pop();
+                    } else if (!_string.find('C')) {
+                        C[0] = registr.get();
+                        registr.pop();
+                    } else if (!_string.find('D')) {
+                        D[0] = registr.get();
+                        registr.pop();
+                    } else {
+                        std::cout << "Error";
+                        in.eof();
+                    }
+                }
         }
     }
-}
-
-int main() {
-    steck registr;
-    int A[1] = {0},
-        B[1] = {},
-        C[1] = {0},
-        D[1] = {0};
-
-    readFile(registr, A, B, C, D);
     std::cout << "A = " << *A << std::endl;
     std::cout << "B = " << *B << std::endl;
     std::cout << "C = " << *C << std::endl;
