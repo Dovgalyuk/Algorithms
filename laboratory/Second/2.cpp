c5e7ae4144917c287c6c0a4d10e798763fc885ad
gerundv <gerundv@yandex.ru>
2021-11-16 17:23:31 +0300

    remaster

--- laboratory/Second/2.cpp
+++ laboratory/Second/2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "stack.h"
+#include "stack.cpp"
 #include <string>
 int main(){
 
@@ -7,26 +7,29 @@ int main(){
     //std::string NO = (")"
     //std::string YES = ([]"")
     std::string b;
-        getline(std::cin,b);
-    for (int i= 0; i <= b.length();++i){
-        if (stack_empty(stack)) stack_push(stack,b[i]);
+    getline(std::cin,b);
+    int a = b.length();
+    for (int i= 0; i <= a;++i){
+        if ((stack_empty(stack)) && (b[i] != 0)) stack_push(stack,b[i]);
         else if (b[i] == ']'){
             if (stack_get(stack) == '[') stack_pop(stack);
-            if (stack_empty(stack)) break;
+            else  stack_push(stack,b[i]);
+            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else if (b[i]=='"'){
             if (stack_get(stack) == '"') stack_pop(stack);
-            if (stack_empty(stack)) break;
+            else  stack_push(stack,b[i]);
+            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else if (b[i]==')'){
             if (stack_get(stack) == '(') stack_pop(stack);
-            if (stack_empty(stack)) break;
+            else  stack_push(stack,b[i]);
+            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else  stack_push(stack,b[i]);
-    }         
+    } 
     if (stack_empty(stack)) std::cout << "YES\n";
     else std::cout << "NO\n";
-    system("pause");
     stack_delete(stack);
     return 0;
 }
