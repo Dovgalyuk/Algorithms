6c1c11831f0bcdd1da6184fea6872d00e7b8e6c1
gerundv <gerundv@yandex.ru>
2021-11-18 11:41:07 +0300

    remaster

--- laboratory/Second/2.cpp
+++ laboratory/Second/2.cpp
@@ -9,22 +9,19 @@ int main(){
     std::string b;
     getline(std::cin,b);
     int a = b.length();
-    for (int i= 0; i <= a;++i){
+    for (int i= 0; i < a;++i){
         if (stack_empty(stack)) stack_push(stack,b[i]);
         else if (b[i] == ']'){
             if (stack_get(stack) == '[') stack_pop(stack);
             else  stack_push(stack,b[i]);
-            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else if (b[i]=='"'){
             if (stack_get(stack) == '"') stack_pop(stack);
             else  stack_push(stack,b[i]);
-            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else if (b[i]==')'){
             if (stack_get(stack) == '(') stack_pop(stack);
             else  stack_push(stack,b[i]);
-            if ((stack_empty(stack)) && (b[i+1] == 0)) break;
         }
         else  stack_push(stack,b[i]);
     } 
