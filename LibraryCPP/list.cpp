232dbf31f1eaa24a53a2bb45ecf93e418f62ea76
gerundv <gerundv@yandex.ru>
2021-11-23 11:41:36 +0300

    third lab

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,109 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* next = nullptr;
+    ListItem* prev = nullptr;
+
+    ListItem(Data data): data(data) {};
 };
 
 struct List
 {
+    ListItem* first = nullptr;
 };
 
+//Создает новый список
 List *list_create()
 {
     return new List;
 }
 
+//Уничтожает список и освобождает память
 void list_delete(List *list)
 {
-    // TODO: free items
+    ListItem*item=list->first;
+    while(item){
+        ListItem* del = item;
+        item = item->next;
+        delete del;
+    }
     delete list;
 }
 
+//Извлекает первый элемент из списка
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return list->first;
 }
 
+//Извлекает данные из элемента списка
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    return item->data;
 }
 
+//Возвращает элемент списка, следующий за указанным
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
+//Возвращает предыдущий элемент для указанного элемента.
+//Не применимо для односвязных списков.
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return item->prev;
 }
 
+//Вставляет новый элемент списка в начало
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    ListItem* item = new ListItem(data);
+    item->next = list->first;
+    if (list->first){
+        list->first->prev = item;
+    }
+    list->first=item;
+    return item;
 }
 
+//Вставляет новый элемент списка после указанного элемента
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    ListItem* new_item = new ListItem(data);
+    new_item->next=item->next;
+    if (new_item->next){
+        new_item->next->prev =new_item;
+    }
+    item->next=new_item;
+    new_item->prev=item;
+    return new_item;
 }
 
+//Удаляет указанный элемент списка.
+//Не применимо для односвязных списков.
+//Должно быть O(1)
 ListItem *list_erase(List *list, ListItem *item)
 {
-    return NULL;
+    if(list->first == item){
+        list->first = item->next;
+    }
+    if(item->next){
+        item->next->prev=item->prev;
+    }
+    if(item->prev){
+        item->prev->next=item->next;
+    }
+    ListItem*next=item->next;
+    delete item;
+    return next;
 }
 
+
+//Удаляет элемент списка, следующий за указанным
+//Должно быть O(1)
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    return list_erase(list,item->next);
 }
--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,7 +1,12 @@
 #include "queue.h"
+#include "list.h"
 
 struct Queue
 {
+    List* list;
+    ListItem* last;
+    Queue(): list(list_create()),last(0) {}
+
 };
 
 Queue *queue_create()
@@ -11,24 +16,36 @@ Queue *queue_create()
 
 void queue_delete(Queue *queue)
 {
-    // TODO: free queue items
+    list_delete(queue->list);
     delete queue;
 }
 
 void queue_insert(Queue *queue, Data data)
 {
+    if (!queue->last){
+        queue->last = list_insert(queue->list,data);
+    }
+    else {
+        queue->last = list_insert_after(queue->list,queue->last,data); 
+    }
 }
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    return list_item_data(list_first(queue->list));
 }
 
 void queue_remove(Queue *queue)
 {
+    list_erase(queue->list,list_first(queue->list));
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    if (list_first(queue->list) == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
