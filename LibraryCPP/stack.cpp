f9892214ba2425b16534c8150b745cd150e5de25
Danya Alekseev <lashywork@gmail.com>
2021-11-25 11:33:45 +0300

    Laba2

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,7 +1,17 @@
 #include "stack.h"
+#include "vector.h"
 
 struct Stack
 {
+    Vector *vector;
+
+    Stack() {
+        vector = vector_create();
+    }
+
+    ~Stack() {
+        vector_delete(vector);
+    }
 };
 
 Stack *stack_create()
@@ -17,18 +27,21 @@ void stack_delete(Stack *stack)
 
 void stack_push(Stack *stack, Data data)
 {
+    vector_resize(stack->vector, vector_size(stack->vector) + 1);
+    vector_set(stack->vector, vector_size(stack->vector) - 1, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    return vector_get(stack->vector, vector_size(stack->vector) - 1);
 }
 
 void stack_pop(Stack *stack)
 {
+    vector_resize(stack->vector, vector_size(stack->vector) - 1);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return !((bool)vector_size(stack->vector));
 }
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,6 +2,19 @@
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t capacity;
+
+    Vector() {
+        size = 0;
+        capacity = 0;
+        data = nullptr;
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -17,18 +30,40 @@ void vector_delete(Vector *vector)
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    return vector->data[index];
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    vector->data[index] = value;
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size <= vector->capacity) {
+        vector->size = size;
+    }
+    else {
+        if (vector->capacity == 0) {
+            vector->capacity = 1;
+        }
+
+        while (vector->capacity < size) {
+            vector->capacity *= 2;
+        }
+
+        Data* new_data = new Data[vector->capacity];
+        for (size_t i = 0;  i < vector->size; i++) {
+            new_data[i] = vector_get(vector, i);
+        }
+
+        delete[] vector->data;
+        vector->data = new_data;
+        vector->size = size;
+    }
 }
