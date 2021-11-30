149056f38743ad26664d44a5ea47bc31fd40e9f9
Иван Важенин <ivan.vazhenin34@gmail.com>
2021-11-30 23:05:59 +0300

    Написал код. Работает даже для таких лабиринтов ############## ###.........## ##.Y####.##### ##.###X......# #......####### ############## Vazhenin Ivan 0091

--- testQueue/third.cpp
+++ testQueue/third.cpp
@@ -11,7 +11,7 @@ int main(){
     char arr[row][col];
     int num = 1, num2, num3, num4;
     char *str = new char[1];
-    str = itoa(num, str, 10);
+    str[1] = '1';
     bool full = true;
     bool imposible = true;
     ifstream file("C:\\Users\\vasya\\OneDrive\\Desktop\\file2.txt");
