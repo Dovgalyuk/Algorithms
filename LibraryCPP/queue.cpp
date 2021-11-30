a59266500e5f50b7e20826a49ea10a481884103f
Egor Potapenko <toptricker8@gmail.com>
2021-11-30 11:53:47 +0300

    Added solution on a third lab
    Crutch

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,60 @@
 #include "queue.h"
+#include "stack.h"
 
 struct Queue
 {
+    Stack* inStack;
+    Stack* stack;
+    Queue() {
+        inStack = stack_create();
+        stack = stack_create();
+    }
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
     return new Queue;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
     // TODO: free queue items
+    stack_delete(queue->inStack);
+    stack_delete(queue->stack);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    if (queue_empty(queue)) {
+        stack_push(queue->stack, data);
+    }
+    else {
+        while (!stack_empty(queue->stack)) {
+            stack_push(queue->inStack, stack_get(queue->stack));
+            stack_pop(queue->stack);
+        }
+        stack_push(queue->stack, data);
+        while (!stack_empty(queue->inStack)) {
+            stack_push(queue->stack, stack_get(queue->inStack));
+            stack_pop(queue->inStack);
+        }
+
+    }
+
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-    return (Data)0;
+    return stack_get(queue->stack);
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+    stack_pop(queue->stack);
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    return true;
+    return ((stack_empty(queue->stack)));
 }
---
+++ lab3/Labirintum.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "queue.h"
+
+void checker(int &startX, int &startY, int columns, int rows) {
+
+    if (startY > rows - 1) {
+        startY--;
+    }
+    else if (startY < 0) {
+        startY++;
+    }
+
+    if (startX > columns - 1) {
+        startX--;
+    }
+    else if (startX < 0) {
+        startX++;
+    }
+}
+
+enum labirintElements {X = 88, dot = 46, cage = 35,};
+
+int main()
+{
+    std::ifstream in;
+    std::ofstream out;
+    std::vector<std::string> labirint;
+    Queue* queue = queue_create();
+    std::string line;
+
+    in.open("labirint.txt");
+    out.open("output.txt");
+    if (!in.is_open()) {
+        std::cout << "Критическая ошибка" << std::endl;
+        exit(1);
+    }
+    else {
+        while (!in.eof()) {
+            std::getline(in, line);
+            labirint.push_back(line + "\n");
+        }
+    }
+
+    int columns = 0;
+    int rows = 0;
+
+    int startX = 0;
+    int startY = 0;
+  
+
+    for (int i = 0; i < labirint.size(); i++) {
+        rows++;
+        for (int j = 0; j < labirint.size(); j++) {
+            columns++;
+            if (labirint[i][j] == 'X') {
+                startY = i;
+                startX = j;
+            }
+        }
+    }
+    int lastX = startX;
+    int lastY = startY;
+
+    queue_insert(queue, labirint[startY][startX]);
+
+    while (!queue_empty(queue)) {
+
+        queue_remove(queue);
+
+        for (int i = 0; i < 4; i++) {
+
+            switch (i)
+            {
+            case 0: 
+                startY--;
+                checker(startX, startY, columns, rows);
+                queue_insert(queue, labirint[startY][startX]);
+                break;
+            case 1:
+                startY++;
+                checker(startX, startY, columns, rows);
+                queue_insert(queue, labirint[startY][startX]);
+                break;
+            case 2:
+                startX++;
+                checker(startX, startY, columns, rows);
+                queue_insert(queue, labirint[startY][startX]);
+                break;
+            case 3:
+                startX--;
+                checker(startX, startY, columns, rows);
+                queue_insert(queue, labirint[startY][startX]);
+                break;
+            default:
+                break;
+            }
+
+            switch (labirint[startY][startX]) { 
+            case cage:
+                startX = lastX;
+                startY = lastY;
+                queue_remove(queue);
+                break;
+            case dot:
+                labirint[startY][startX] = 'X';
+                lastX = startX; 
+                lastY = startY;                
+                break;
+            case X:
+                startX = lastX;
+                startY = lastY;
+                queue_remove(queue);
+                break;
+            default:
+                break;
+            }
+        
+        }      
+
+    }
+    int count = 0;
+
+    for (int i = 0; i < labirint.size(); i++) {
+        for (int j = 0; j < labirint.size(); j++) {
+            if (labirint[i][j] == 'X') {
+                count++;
+            }
+        }
+         out << labirint[i];        
+    }
+    out << std::endl << count;
+    in.close();
+    out.close();
+
+}
