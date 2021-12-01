bd1dd3be475e8708cda0ec77528a2d0ceb09a268
Lostmindd <coffeebrain666@gmail.com>
2021-12-02 00:12:22 +0300

    Update directed_graph.cpp

--- LibraryCPP/directed_graph.cpp
+++ LibraryCPP/directed_graph.cpp
@@ -1,80 +1,4 @@
-#ifndef DIRECTED_GRAPH_H
-#define DIRECTED_GRAPH_H
-
-struct directed_graph
-{
-	struct vertex
-	{
-		int label = -1;
-	};
-
-	struct edge
-	{
-		bool exists = 0;
-		int label = -1;
-	};
-
-	struct iterator
-	{
-		int	CurrentNeighbor = 0;
-		int GivenVertex = -1;
-		directed_graph* DirectedGraphPtr;
-
-		bool NeighborListEmpty(int vertex)
-		{
-			GivenVertex = vertex;
-			for (int i = CurrentNeighbor; i < DirectedGraphPtr->vertexes_amount; i++)
-			{
-				if (DirectedGraphPtr->matrix[(vertex * DirectedGraphPtr->vertexes_amount) + i].exists == 1)
-					return false;
-			}
-			CurrentNeighbor = 0;
-			return true;
-		}
-
-		int NextNeighborIndex(int vertex)
-		{
-			GivenVertex = vertex;
-			while (DirectedGraphPtr->matrix[(vertex * DirectedGraphPtr->vertexes_amount) + CurrentNeighbor].exists == 0)
-			{
-				CurrentNeighbor++;
-			}
-			CurrentNeighbor++;
-			return CurrentNeighbor - 1;
-		}
-		~iterator()
-		{
-			DirectedGraphPtr->iter = nullptr;
-		}
-	};
-
-	vertex* vertex_labels;
-	edge* matrix;
-	iterator* iter;
-	int vertexes_amount;
-
-	iterator* createIterator()
-	{
-		iter = new iterator;
-		iter->DirectedGraphPtr = this;
-		return iter;
-	}
-
-	directed_graph(int vertexes_amount)
-	{
-		this->vertexes_amount = vertexes_amount;
-		vertex_labels = new vertex[vertexes_amount];
-		matrix = new edge[vertexes_amount * vertexes_amount];
-	}
-	~directed_graph()
-	{
-		delete[] vertex_labels;
-		delete[] matrix;
-		delete iter;
-	}
-
-};
-
+#include <C:\Users\vasill\source\repos\mendeleev8\mendeleev8\directed_graph.h>
 
 directed_graph* create_directed_graph(int vertexes_amount)
 {
@@ -86,7 +10,7 @@ void delete_directed_graph(directed_graph* graph)
 	delete graph;
 }
 
-void add_vertex(struct directed_graph** graph) //add an vertex by copying the old array to the new array (where is 1 element more)
+void add_vertex(struct directed_graph** graph)
 {
 	directed_graph* new_graph = new directed_graph((*graph)->vertexes_amount + 1);
 	for (int i = 0; i < (*graph)->vertexes_amount; i++)
@@ -112,7 +36,7 @@ void add_edge(directed_graph* graph, int vertex1, int vertex2)
 		graph->matrix[(vertex1 * graph->vertexes_amount) + vertex2].exists = 1;
 }
 
-void remove_vertex(struct directed_graph** graph, int vertex) //add an vertex by copying the old array to the new array (where is 1 element less)
+void remove_vertex(struct directed_graph** graph, int vertex)
 {
 	if (vertex < (*graph)->vertexes_amount && (*graph)->vertexes_amount > 1)
 	{
@@ -182,5 +106,3 @@ directed_graph::iterator* new_iterator(directed_graph* graph)
 {
 	return graph->createIterator();
 }
-
-#endif
