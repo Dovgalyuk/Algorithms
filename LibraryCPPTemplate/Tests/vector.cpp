297778d96c20da49d5458b3ecc436674d4bc0543
Даниил <bacydaniil@gmail.com>
2021-12-05 21:12:42 +0300

    lab3 finished

--- LibraryCPPTemplate/Tests/vector.cpp
+++ LibraryCPPTemplate/Tests/vector.cpp
@@ -60,6 +60,15 @@ int main()
     std::cout << "\n";
 
     // Performance test
-    for (int i = 0 ; i < 10000000 ; ++i)
+    for (int i = 1 ; i <= 10000000 ; ++i)
+    {
         vector.resize(i);
+        vector.set(i - 1, i);
+    }
+
+    long long sum = 0;
+    for (int i = 0 ; i < 10000000 ; ++i)
+        sum += vector.get(i);
+
+    std::cout << sum << "\n";
 }
