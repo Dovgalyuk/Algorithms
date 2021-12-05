a5273c46cfecd2c7b71ffca73e96aabc3b1d8143
Lostmindd <coffeebrain666@gmail.com>
2021-12-05 19:29:16 +0300

    Update array.h

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,38 +1,45 @@
 #ifndef ARRAY_TEMPLATE_H
 #define ARRAY_TEMPLATE_H
 
-template <typename Data> class Array
+template <class Data> 
+class Array
 {
 public:
     // create array
     Array(size_t size)
     {
+        array_size = size;
+        pointer = new Data[size];
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] pointer;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return pointer[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        pointer[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return array_size;
     }
 
 private:
     // private data should be here
+    size_t array_size;
+    Data* pointer;
 };
 
 #endif
