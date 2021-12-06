3863c47fdf51511abda82150103cb7673ae82b53
Lubov260902 <luba260902@gmail.com>
2021-12-06 21:54:51 +0300

    3 задание

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -1,45 +1,79 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <vector.h>
+#include <cstddef>
+
 template <typename Data> class Queue
 {
 public:
     // Create empty queue
-    Queue()
+    Queue(): _vector(new Vector<Data>())
     {
     }
 
     // Deletes queue
     ~Queue()
     {
+        delete _vector;
     }
 
     // Includes new element into the queue
     // Should be O(1) on average
     void insert(Data data)
     {
+        if(end != _vector->size())
+        {
+            _vector->set(end,data);
+            end++;
+        }
+        else
+        {
+            if (start != 0)
+            {
+                for (int i = 0; i < end - start; ++i) {
+                    _vector->set(i, _vector->get(start + i));
+                }
+                end -= start;
+                start = 0;
+                insert(data);
+            } else {
+                _vector->resize(end + 1);
+                insert(data);
+            }
+        }
     }
+
 
     // Retrieves first element from the queue
     Data get() const
     {
-        return Data();
+        return _vector->get(start);
     }
 
     // Removes first element from the queue
     // Should be O(1) on average
     void remove()
     {
+        if(!empty())
+        {
+            start++;
+        }
+        //return 0;
     }
 
     // Returns true if the queue is empty
     bool empty() const
     {
-        return true;
+        //return true;
+        return end == start;
     }
 
 private:
     // private data should be here
+    Vector<Data> *_vector;
+    size_t start = 0;
+    size_t end = 0;
 };
 
 #endif
---
+++ lab3/Test.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "queue.h"
+using namespace std;
+unsigned board[100][100];
+typedef Queue<int> queue;
+int main()
+{
+    cout << "Enter N M: " << endl;
+    int N, M;
+    cin >> N >> M;
+    queue x_queue ;
+    queue y_queue ;
+    cout << "Start spots:(0 0 to stop)" << endl;
+    while (true)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a == 0) break;
+        x_queue.insert( a - 1);
+        y_queue.insert( b - 1);
+
+        board[a - 1][b - 1] = 1;
+    }
+
+    unsigned k = 0;
+    while (!x_queue.empty())
+    {
+        int x_start = x_queue.get();
+        int y_start = y_queue.get();
+        unsigned intensity = board[x_start][y_start];
+
+        x_queue.remove();
+        y_queue.remove();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i * i + j * j != 0 && i * j == 0)
+                {
+                    int x = x_start + i;
+                    int y = y_start + j;
+                    if (x < 0 || y < 0 || x >= N || y >= M)
+                    {
+                        continue;
+                    }
+                    if (board[x][y] == 0)
+                    {
+                        k = intensity + 1;
+                        board[x][y] = k;
+
+                        x_queue.insert( x);
+                        y_queue.insert( y);
+                    }
+                }
+
+            }
+        }
+    }
+    cout << k;
+}
