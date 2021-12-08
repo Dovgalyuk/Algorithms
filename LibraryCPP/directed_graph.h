94297197281ffe0021f07fcd3576347a36f7ec71
Lostmindd <coffeebrain666@gmail.com>
2021-12-08 23:55:40 +0300

    Update directed_graph.h

--- LibraryCPP/directed_graph.h
+++ LibraryCPP/directed_graph.h
@@ -3,81 +3,12 @@
 
 #include "array.h" //else it uses the wrong array.h
 
-struct directed_graph
-{
-	struct vertex
-	{
-		int label = -1;
-	};
+struct iterator;
+struct directed_graph;
 
-	struct edge
-	{
-		bool exists = 0;
-		int label = -1;
-	};
+bool NeighborListEmpty(iterator* iter);
 
-	struct iterator
-	{
-		int	CurrentNeighbor = 0;
-		int GivenVertex = -1;
-		directed_graph* DirectedGraphPtr;
-		int vertex;
-
-		bool NeighborListEmpty()
-		{
-			for (int i = CurrentNeighbor; i < DirectedGraphPtr->vertexes_amount; i++)
-			{
-				if (DirectedGraphPtr->matrix->get((vertex * DirectedGraphPtr->vertexes_amount) + i).exists == 1)
-					return false;
-			}
-			CurrentNeighbor = 0;
-			return true;
-		}
-
-		int NextNeighborIndex()
-		{
-			while (DirectedGraphPtr->matrix->get((vertex * DirectedGraphPtr->vertexes_amount) + CurrentNeighbor).exists == 0)
-			{
-				CurrentNeighbor++;
-			}
-			CurrentNeighbor++;
-			return CurrentNeighbor - 1;
-		}
-		iterator(int vertex)
-		{
-			this->vertex = vertex;
-		}
-		~iterator()
-		{
-			DirectedGraphPtr = nullptr;
-		}
-	};
-
-	typedef Array<vertex> vertex_arr;
-	vertex_arr* vertex_labels;
-	typedef Array<edge> matrix_arr;
-	matrix_arr* matrix;
-	int vertexes_amount;
-
-	iterator* createIterator(int vertex)
-	{
-		iterator* iter = new iterator(vertex);
-		iter->DirectedGraphPtr = this;
-		return iter;
-	}
-
-	directed_graph(int vertexes_amount)
-	{
-		vertex_labels = new vertex_arr(vertexes_amount);
-		matrix = new matrix_arr(vertexes_amount * vertexes_amount);
-		this->vertexes_amount = vertexes_amount;
-	}
-	~directed_graph()
-	{
-		delete vertex_labels;
-		delete matrix;
-	}
-};
+int NextNeighborIndex(iterator* iter);
 
 directed_graph* create_directed_graph(int vertexes_amount);
 
@@ -101,5 +32,5 @@ int get_edge_label(directed_graph* graph, int vertex1, int vertex2);
 
 bool edge_exists(directed_graph* graph, int vertex1, int vertex2);
 
-directed_graph::iterator* new_iterator(directed_graph* graph, int vertex);
+iterator* new_iterator(directed_graph* graph, int vertex);
 #endif
