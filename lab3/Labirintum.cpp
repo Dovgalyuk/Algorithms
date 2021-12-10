f896ed6f6bca1d070cb6f14f255463f06177c73d
Egor Potapenko <toptricker8@gmail.com>
2021-12-10 10:28:41 +0300

    Update Labirintum.cpp

--- lab3/Labirintum.cpp
+++ lab3/Labirintum.cpp
@@ -4,31 +4,33 @@
 #include <fstream>
 #include "queue.h"
 
-void checker(int &startX, int &startY, int columns, int rows) {
+void putCoordinates(Queue*& queueX, Queue*& queueY) {
+    queue_insert(queueY, queue_get(queueY) - 1);
+    queue_insert(queueX, queue_get(queueX));
 
-    if (startY > rows - 1) {
-        startY--;
-    }
-    else if (startY < 0) {
-        startY++;
-    }
+    queue_insert(queueY, queue_get(queueY) + 1);
+    queue_insert(queueX, queue_get(queueX));
 
-    if (startX > columns - 1) {
-        startX--;
-    }
-    else if (startX < 0) {
-        startX++;
-    }
+    queue_insert(queueX, queue_get(queueX) + 1);
+    queue_insert(queueY, queue_get(queueY));
+
+    queue_insert(queueX, queue_get(queueX) - 1);
+    queue_insert(queueY, queue_get(queueY));
+
+    queue_remove(queueY);
+    queue_remove(queueX);
 }
 
-enum labirintElements {X = 88, dot = 46, cage = 35,};
+
+enum labirintElements { X = 88, dot = 46, cage = 35, };
 
 int main()
 {
     std::ifstream in;
     std::ofstream out;
     std::vector<std::string> labirint;
-    Queue* queue = queue_create();
+    Queue* queueY = queue_create();
+    Queue* queueX = queue_create();
     std::string line;
 
     in.open("labirint.txt");
@@ -44,81 +46,52 @@ int main()
         }
     }
 
-    int columns = 0;
-    int rows = 0;
 
     int startX = 0;
     int startY = 0;
-  
+
 
     for (int i = 0; i < labirint.size(); i++) {
-        rows++;
         for (int j = 0; j < labirint.size(); j++) {
-            columns++;
             if (labirint[i][j] == 'X') {
                 startY = i;
                 startX = j;
             }
         }
     }
-    int lastX = startX;
-    int lastY = startY;
 
-    queue_insert(queue, labirint[startY][startX]);
+    queue_insert(queueY, startY);
+    queue_insert(queueX, startX);
 
-    while (!queue_empty(queue)) {
+    while (!queue_empty(queueY)) {
 
-        queue_remove(queue);
+
+        putCoordinates(queueX, queueY);
 
         for (int i = 0; i < 4; i++) {
-
-            switch (i)
-            {
-            case 0: 
-                startY--;
-                checker(startX, startY, columns, rows);
-                queue_insert(queue, labirint[startY][startX]);
-                break;
-            case 1:
-                startY++;
-                checker(startX, startY, columns, rows);
-                queue_insert(queue, labirint[startY][startX]);
-                break;
-            case 2:
-                startX++;
-                checker(startX, startY, columns, rows);
-                queue_insert(queue, labirint[startY][startX]);
-                break;
-            case 3:
-                startX--;
-                checker(startX, startY, columns, rows);
-                queue_insert(queue, labirint[startY][startX]);
-                break;
-            default:
-                break;
-            }
-
-            switch (labirint[startY][startX]) { 
+            switch (labirint[queue_get(queueY)][queue_get(queueX)]) {
             case cage:
-                startX = lastX;
-                startY = lastY;
-                queue_remove(queue);
+                queue_remove(queueY);
+                queue_remove(queueX);
                 break;
             case dot:
-                labirint[startY][startX] = 'X';
-                lastX = startX; 
-                lastY = startY;                
+                labirint[queue_get(queueY)][queue_get(queueX)] = 'X';
+                queue_insert(queueY, queue_get(queueY));
+                queue_insert(queueX, queue_get(queueX));
+                queue_remove(queueY);
+                queue_remove(queueX);
                 break;
             case X:
-                startX = lastX;
-                startY = lastY;
-                queue_remove(queue);
+                queue_remove(queueY);
+                queue_remove(queueX);
                 break;
             default:
                 break;
             }
-        
-        }      
+        }
+
+
+
 
     }
     int count = 0;
@@ -129,7 +102,7 @@ int main()
                 count++;
             }
         }
-         out << labirint[i];        
+        out << labirint[i];
     }
     out << std::endl << count;
     in.close();
