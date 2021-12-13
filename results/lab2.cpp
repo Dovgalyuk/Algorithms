5b98f9e2ea339f92c1c79e88b12b5b1a77574a69
onisken <aniskinartyom2002@gmail.com>
2021-12-13 19:43:06 +0300

    Изменена логика алгоритма

--- results/lab2.cpp
+++ results/lab2.cpp
@@ -8,9 +8,6 @@
 int main() {
     int size = 0;
     std::cin >> size;
-
-    Stack<int> stackPlus;
-    Stack<int> stackMinus;
     /*
 5
 1 -
@@ -19,45 +16,21 @@ int main() {
 9 +
 10 -
      */
+    Stack<int> order; // Storage minus
     for (int i = 0; i < size; i++) {
         int tmp = 0;
         std::cin >> tmp;
-        char tmpChar = '-';
+        char tmpChar;
         std::cin >> tmpChar;
-        if (tmpChar == '+') {
-            stackPlus.push(tmp);
-        } else stackMinus.push(tmp);
+        if (tmpChar == '-') {
+            if (order.size() <= 0) continue;
+
+            const int plus = order.get();
+            if (tmp < plus) continue;
+
+            std::cout << plus << " " << tmp << std::endl;
+            order.pop();
+        } else order.push(tmp);
     }
-
-    int maxIndex = std::min(stackMinus.size(), stackPlus.size());
-
-    Stack<int> order; // Storage minus
-
-    for (int i = 0; i < maxIndex; i++) {
-        int plus = stackPlus.get();
-        int minus = stackMinus.get();
-
-        if (plus > minus) {
-            for (int j = 0; j < order.size(); j++) {
-                int tmpMinus = order.get();
-                if (plus > minus) continue;
-                order.pop();
-                stackPlus.pop();
-                std::cout << plus << " " << tmpMinus << std::endl;
-            }
-            order.push(minus);
-            stackMinus.pop();
-        } else {
-            for(;;) {
-                order.push(minus);
-                stackMinus.pop();
-                if (plus < stackMinus.get()) minus = stackMinus.get();
-                else break;
-            }
-            stackPlus.pop();
-            std::cout << plus << " " << minus << std::endl;
-        }
-    }
-
     return 0;
 }
