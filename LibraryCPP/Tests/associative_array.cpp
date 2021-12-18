ace0565ebd393bd89ee9f60ca512b18b9aa920a4
Lostmindd <coffeebrain666@gmail.com>
2021-12-19 00:17:54 +0300

    Create associative_array.cpp

---
+++ LibraryCPP/Tests/associative_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include "associative_array.h"
+
+
+int main()
+{
+	associative_array* associative_arr = associative_array_create();
+	int load_test_count = 100000; 
+
+	for (int i = 0; i < load_test_count; i++)
+	{
+		insert_pair(std::to_string(i), std::to_string(i), associative_arr);
+	}
+
+	for (int i = 100; i < 120; i++)
+	{
+		if (find(std::to_string(i), associative_arr) != std::to_string(i))
+		{
+			std::cout << "Search or insert error";
+			return 1;
+		}
+	}
+
+	for (int i = 0; i < load_test_count; i++)
+	{
+		delete_pair(std::to_string(i), associative_arr);
+	}
+
+	for (int i = 100; i < 120; i++)
+	{
+		if (find(std::to_string(i), associative_arr) != "")
+		{
+			std::cout << "Delete error";
+			return 1;
+		}
+	}
+
+	associative_array_delete(associative_arr);
+}
