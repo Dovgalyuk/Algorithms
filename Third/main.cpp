3d929499c5dbd1a37da1fa58c0387fb78fd3542f
Nikolay0092 <78650140+Nikolay0092@users.noreply.github.com>
2021-12-20 10:09:51 +0300

    main.cpp|fix1
    Принял во внимание ваши комментарии и постарался реализовать.

--- Third/main.cpp
+++ Third/main.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 #include "queue.h"
 
-
-
 int main()
 {
 	std::ifstream fin;
@@ -11,28 +9,16 @@ int main()
 
 	int n = 0;
 	//read n
-	char ch;
-	bool enter = true;
-	while (fin.get(ch)) {
-		if (ch == '\n') break;
-		n = ch - '0';
-	}
+	fin >> n;
+
 	//pointer to start
-	fin.seekg(0, fin.beg);
 	int* arr = new int[n * n];
+
 	//filling the array
 	if (fin.is_open()) {
-		enter = true;
-		int i = 0;
-		while (fin.get(ch)) {
-			int i_ch = ch - '0';
-			if (!enter) {
-				if (ch != ' ' && ch != '\n') {
-					arr[i] = i_ch;
-					i++;
-				}
-			}
-			if (ch == '\n') enter = false;
+		for (int i = 0; i < n * n; i++)
+		{
+			fin >> arr[i];
 		}
 	}
 	else {
@@ -54,7 +40,7 @@ int main()
 			if (arr[(line * n) + j] == 1 && was_in_queue[j] == false)
 			{
 				queue_insert(queue, j);
-				distance[j] += distance[line] + 1;
+				distance[j] = distance[line] + 1;
 				was_in_queue[j] = true;
 			}
 		}
@@ -63,8 +49,8 @@ int main()
 	std::ofstream fout;
 	fout.open("second.txt");
 
-	fout << arr[0] << "\n";
 	//output of results
+	fout << arr[0] << "\n";
 	for (int i = 1; i < n; i++)
 	{
 		if (was_in_queue[i] == false) fout << "none" << "\n";
