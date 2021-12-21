3e9a0f81e46c9144d9351a5b474cd3d72f3f0209
Lubov260902 <luba260902@gmail.com>
2021-12-21 23:57:13 +0300

    3  задание исправление

--- lab3/Test.cpp
+++ lab3/Test.cpp
@@ -12,48 +12,43 @@ int main()
     for (int i = 0; i < N; ++i) {
         board[i] = new int[M] {0};
     }
-    cout << "Start spots:(-1 -1 to stop)" << endl;
+    cout << "Start spots:(0 0 to stop)" << endl;
     while (true)
     {
         int a, b;
         cin >> a >> b;
-        if (a < 0) break;
+        if (a == 0) break;
+        a--;
+        b--;
         coords.insert( pair<int,int>(a, b));
 
         board[a][b] = 1;
     }
-
-    unsigned k = 0;
-    while (!coords.empty())
-    {
+    int k=0;
+    while (!coords.empty()) {
         pair<int, int> curr = coords.get();
         unsigned intensity = board[curr.first][curr.second];
         coords.remove();
-        k++;
+
         if (curr.first > 0 && board[curr.first - 1][curr.second] == 0) {
-           board[curr.first - 1][curr.second] = intensity + 1;
-           k++;
+            board[curr.first - 1][curr.second] = intensity + 1;
+            coords.insert( pair<int,int>(curr.first-1, curr.second));
         }
         if (curr.first < N - 1 && board[curr.first + 1][curr.second] == 0) {
-          board[curr.first + 1][curr.second] = intensity  + 1;
-            k++;
+            board[curr.first + 1][curr.second] = intensity + 1;
+            coords.insert( pair<int,int>(curr.first+1, curr.second));
         }
         if (curr.second > 0 && board[curr.first][curr.second - 1] == 0) {
             board[curr.first][curr.second - 1] = intensity + 1;
-            k++;
+            coords.insert( pair<int,int>(curr.first, curr.second-1));
         }
         if (curr.second < M - 1 && board[curr.first][curr.second + 1] == 0) {
             board[curr.first][curr.second + 1] = intensity + 1;
-            k++;
+            coords.insert( pair<int,int>(curr.first, curr.second+1));
         }
-
+        if(k<intensity){
+            k=intensity;
+        }
     }
     cout << k;
 }
-/*Enter N M:
-3 3
-Start spots:(-1 -1 to stop)
-1 1
--1 -1
-5
- */
