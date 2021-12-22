b726e86333e86dd3ef9ebcab51f508d1079faf03
Lostmindd <coffeebrain666@gmail.com>
2021-12-22 23:35:21 +0300

    Update lab5.cpp

--- SOLUTION/lab5.cpp
+++ SOLUTION/lab5.cpp
@@ -24,9 +24,10 @@ void info(associative_array* associative_arr, std::unordered_map<std::string, st
 	insert_pair(rand_str(), rand_str(), associative_arr);
 	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
 	std::chrono::duration<double> sec = end - start;
-	std::cout << "insert after "<< after <<" elements| My own Hash table:" << sec.count() << " sec. | unordered_map: ";
+	std::cout << "insert after " << after << " elements \t| My own Hash table:" << sec.count() << " sec. \t| unordered_map: ";
 	start = std::chrono::system_clock::now();
 	map[rand_str()] = rand_str();
+	end = std::chrono::system_clock::now();
 	sec = end - start;
 	std::cout << sec.count() << " sec.\n";
 }
@@ -37,47 +38,85 @@ int main()
 	std::unordered_map<std::string, std::string> map;
 
 	std::string str;
-	for (int i = 0; i < 100; i++) // 100
+	for (int i = 0; i < 100; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 100);
-	for (int i = 0; i < 1000; i++) //1101
+
+	for (int i = 0; i < 899; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 1000);
-	for (int i = 0; i < 5000; i++) //5102
+
+	for (int i = 0; i < 3999; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 5000);
-	for (int i = 0; i < 10000; i++) //15103
+
+	for (int i = 0; i < 4999; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 10000);
-	for (int i = 0; i < 50000; i++) //65104
+
+	for (int i = 0; i < 39999; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 50000);
-	for (int i = 0; i < 100000; i++) //165105
+
+	for (int i = 0; i < 49999; i++)
 	{
 		str = rand_str();
 		map[str] = str;
 		insert_pair(str, str, associative_arr);
 	}
 	info(associative_arr, map, 100000);
+
+	for (int i = 0; i < 99999; i++)
+	{
+		str = rand_str();
+		map[str] = str;
+		insert_pair(str, str, associative_arr);
+	}
+	info(associative_arr, map, 200000);
+
+	for (int i = 0; i < 299999; i++)
+	{
+		str = rand_str();
+		map[str] = str;
+		insert_pair(str, str, associative_arr);
+	}
+	info(associative_arr, map, 500000);
+
+	for (int i = 0; i < 499999; i++)
+	{
+		str = rand_str();
+		map[str] = str;
+		insert_pair(str, str, associative_arr);
+	}
+	info(associative_arr, map, 1000000);
+
+	for (int i = 0; i < 199999; i++)
+	{
+		str = rand_str();
+		map[str] = str;
+		insert_pair(str, str, associative_arr);
+	}
+	info(associative_arr, map, 1200000);
+
 	associative_array_delete(associative_arr);
 }
