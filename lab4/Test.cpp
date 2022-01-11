d591f8d5c8fb30ac9173809b81a8e5daf0a1d324
Egor Potapenko <toptricker8@gmail.com>
2022-01-11 11:31:40 +0300

    fixes

--- lab4/Test.cpp
+++ lab4/Test.cpp
@@ -9,7 +9,7 @@ int main() {
 	std::vector<Vertex*> vs;
 
 	for (int i = 0; i < 4; i++) {
-		vs.push_back(graph.AddVertex(i)) ;
+		vs.push_back(graph->AddVertex(i)) ;
 	}
 	/*
 	v0->v0
@@ -18,31 +18,31 @@ int main() {
 	v2->v3
 	v2->v3 (test, duplicates are not allowed)
 	*/
-	graph.AddEdge(*vs.at(0), *vs.at(0));
-	graph.AddEdge(*vs.at(0), *vs.at(1));
-	graph.AddEdge(*vs.at(0), *vs.at(3));
-	graph.AddEdge(*vs.at(2), *vs.at(3));
+	graph->AddEdge(*vs.at(0), *vs.at(0));
+	graph->AddEdge(*vs.at(0), *vs.at(1));
+	graph->AddEdge(*vs.at(0), *vs.at(3));
+	graph->AddEdge(*vs.at(2), *vs.at(3));
 
 	if (graph.AddEdge(*vs.at(2), *vs.at(3))) {
 		exit(1);
 	}
 
-	graph.GetSuccessors(*vs.at(0));
-	graph.RemoveEdge(*vs.at(0), *vs.at(1));
-	if (graph.GetEdge(*vs.at(0), *vs.at(1))) {
+	graph->GetSuccessors(*vs.at(0));
+	graph->RemoveEdge(*vs.at(0), *vs.at(1));
+	if (graph->GetEdge(*vs.at(0), *vs.at(1))) {
 		exit(1);
 	}
 
-	graph.AddEdge(*vs.at(0), *vs.at(1));
-	if (!graph.GetEdge(*vs.at(0), *vs.at(1))) {
+	graph->AddEdge(*vs.at(0), *vs.at(1));
+	if (!graph->GetEdge(*vs.at(0), *vs.at(1))) {
 		exit(1);
 	}
 
-	graph.GetSuccessors(*vs.at(0));
+	graph->GetSuccessors(*vs.at(0));
 
 
-	graph.GetVertices();
-	graph.RemoveVertex(*vs.at(3));
-	graph.GetVertices();
+	graph->GetVertices();
+	graph->RemoveVertex(*vs.at(3));
+	graph->GetVertices();
 
 }
