b751a8f25ba20c0a71fb0a186edfb1fbf8aed4d4
Egor Potapenko <toptricker8@gmail.com>
2022-01-13 11:43:48 +0300

    added test for graph

--- lab4/Test.cpp
+++ lab4/Test.cpp
@@ -3,68 +3,34 @@
 
 #include "graph.h"
 
+Graph* Prima(Graph* graph, Graph* prima){
+    auto* Graph = graph;
+    auto* Prima = prima;
+    std::vector<Graph::Vertex*> vs;
+    vs = Graph->GetVertices();
+
+
+
+    return Prima;
+}
+
 int main() {
 	
-	auto* graph = new Graph();
 
-	//graph->AddVertex(1);
+ auto* graph2 = new Graph();
+    std::vector<Graph::Vertex*> graph2_vs;
+    graph2_vs.reserve(6);
 
-	std::vector<Graph::Vertex*> vs;
-    vs.reserve(4);
-	for (int i = 0; i < 4; i++) {
-		vs.push_back(graph->AddVertex(i)) ;
-	}
-    
-	/*
-	v0->v0
-	v0->v1
-	v0->v3
-	v2->v3
-	v2->v3 (test, duplicates are not allowed)
-	*/
-	//graph->AddEdge(*vs.at(0), *vs.at(0), 5);
-	graph->AddEdge(*vs.at(0), *vs.at(1), 3);
+    auto* prima = new Graph();
 
-	graph->AddEdge(*vs.at(0), *vs.at(3), 6);
-	graph->AddEdge(*vs.at(2), *vs.at(3), 2);
-
-	if (!graph->AddEdge(*vs.at(2), *vs.at(3),2)) {
-		return 1;
-	}
-
-    if(!graph->IsEdge(*vs.at(0), *vs.at(1))){
-        return 1;
+    for(int i = 0; i < 6; ++i){
+        graph2_vs.push_back(graph2->AddVertex(i));
     }
+    graph2->AddEdge(*graph2_vs);
 
-	graph->GetSuccessors(*vs.at(0));
-
-	graph->RemoveEdge(*vs.at(0), *vs.at(1));
-	if (graph->IsEdge(*vs.at(0), *vs.at(1))) {
-		return 1;
-	}
-
-	graph->AddEdge(*vs.at(0), *vs.at(1), 1);
-	if (!graph->IsEdge(*vs.at(0), *vs.at(1))) {
-		exit(1);
-	}
-
-    std::vector<Graph::Vertex*> suc;
-	suc = graph->GetSuccessors(*vs.at(0));
-    std::cout << "Successors of 0: " << std::endl;
-    for (auto& vert: suc) {
-        std::cout << vert->GetData() << std::endl;
-    }
+    Prima(graph2, prima);
 
 
-	graph->GetVertices();
-	graph->RemoveVertex(*vs.at(3));
-    if (graph->IsEdge(*vs.at(2), *vs.at(3))) {
-        return 1;
-    }
-	vs = graph->GetVertices();
-    std::cout << "Vertices: "<<std::endl;
-    for (auto& vert: vs) {
-        std::cout << vert->GetData() << std::endl;
-    }
+
     return 0;
 }
