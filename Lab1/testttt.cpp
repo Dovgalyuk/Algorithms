706b12f60ef151ebce5c81b73de133606c34a54d
GAZON17 <95870147+GAZON17@users.noreply.github.com>
2022-09-23 17:38:20 +0300

    Create testttt.cpp

---
+++ Lab1/testttt.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <ctime>
+#include "array.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void task1(Array* Numbers, const int size)
+{
+	int kpol=0;
+	int kotr=0;
+	int sumpol=0;
+	int sumotr=0;
+	cout << "Полученный массив:" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		array_set(Numbers, i, rand() % 201 - 100);
+		cout << array_get(Numbers, i) << " ";
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (array_get(Numbers, i) > 0)
+		{
+			kpol++;
+			sumpol+= array_get(Numbers, i);
+		}
+		if (array_get(Numbers, i) < 0)
+		{
+			kotr++;
+			sumotr+= array_get(Numbers, i);
+		}
+	}
+	cout << endl << endl << "Среднее арифметическое положительных элементов: " << (float)sumpol / kpol << endl;
+	cout << "Среднее арифметическое отрицательных элементов: " << (float)sumotr / kotr << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	int k;
+	cout << "Введите размер массива: ";
+	cin >> k;
+	cout << endl;
+
+	Array *Numbers = array_create(k);
+
+	task1(Numbers, k);
+	
+	array_delete(Numbers);
+	system("pause");
+}
