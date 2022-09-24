f0c77ac1067b9e35eefbe3a1f7f86585140914fe
Dmitriy-Sazonov <58565484+Dmitriy-Sazonov@users.noreply.github.com>
2022-09-24 15:02:32 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+	int M;
+	std::cin >> M;
+	int* A = new int[M];
+	for (int i = 0; i < M; i++)
+	{
+		std::cin >> A[i];
+	}
+	int ch = 0;
+	for (int i = 1; i < M; i++)
+	{
+		if ((A[i - 1] >= 0) != (A[i] >= 0))
+		{
+			ch++;
+		}
+	}
+	std::cout << ch;
+}
