147dd9bc263848bf5b51444dca20dc7a0545e768
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-10-06 18:57:47 +0300

    Update vector.h

--- LibraryCPP/vector.h
+++ LibraryCPP/vector.h
@@ -3,76 +3,30 @@
 
 #include <cstddef>
 
-template <typename Data>
-class Vector {
-public:
+// Vector (dynamic array)
+// Stores integer values inside
+typedef int Data;
 
-    // Creates vector
-    Vector() {
-        data = nullptr;
-        length = 0;
-        capacity = 0;
-    }
+struct Vector;
 
-    // Deletes vector structure and internal data
-    ~Vector() {
-        delete[] data;
-    }
+// Creates vector
+Vector *vector_create();
 
-    // Retrieves vector element with the specified index
-    Data get(size_t index) const {
-        return data[index];
-    }
+// Deletes vector structure and internal data
+void vector_delete(Vector *vector);
 
-    // Sets vector element with the specified index
-    void set(size_t index, Data value) {
-        if (index >= length) {
-            resize(index);
-        }
-        data[index] = value;
-    }
+// Retrieves vector element with the specified index
+Data vector_get(const Vector *vector, size_t index);
 
-    // Retrieves current vector size
-    size_t size() const {
-        return length;
-    }
+// Sets vector element with the specified index
+void vector_set(Vector *vector, size_t index, Data value);
 
-    // Reserve memory
-    void reserve(size_t size) {
-        if (capacity >= size) return;
+// Retrieves current vector size
+size_t vector_size(const Vector *vector);
 
-        if (capacity == 0) capacity = 1;
+// Changes the vector size (may increase or decrease)
+// Should be O(1) on average
+void vector_resize(Vector *vector, size_t size);
 
-        while (capacity < size)
-            capacity *= MULTIPLIER;
-
-        Data* copy = new Data[capacity];
-
-        for (int i = 0; i < length; i++) {
-            copy[i] = data[i];
-        }
-
-        delete[] data;
-        data = copy;
-    }
-
-    // Changes the vector size (may increase or decrease)
-    // Should be O(1) on average
-    void resize(size_t size) {
-        if (capacity < size)
-            reserve(size);
-        length = size;
-    }
-
-protected:
-    // private data should be here
-    Data* data;
-
-    size_t length; // Count of elements
-    size_t capacity; // Count of reserved elements
-
-    /// Константа для увеличения размера массива
-    const int MULTIPLIER = 2;
-};
 #endif
 
