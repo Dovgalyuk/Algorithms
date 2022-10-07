e8ade8319541c70647f28be0adb9214687669de0
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-10-07 16:16:12 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,20 +2,6 @@
 
 struct Vector
 {
-    size_t size;
-    size_t capacity;
-    Data* pointer;
-    Vector()
-    {
-        size = 0;
-        capacity = 0;
-        pointer = nullptr;
-    }
-    ~Vector()
-    {
-        delete[] pointer;
-        pointer = nullptr;
-    }
 };
 
 Vector *vector_create()
@@ -31,36 +17,18 @@ void vector_delete(Vector *vector)
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return vector->pointer[index];
+    return (Data)0;
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
-    vector->pointer[index] = value;
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return vector->size;
+    return 0;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
-    if (size == vector->size) return;
-    else if (size > vector->size)
-    {
-        if (size > vector->capacity)
-        {
-            if (size < 5) vector->capacity = size;
-            else vector->capacity = size + ((size - 3) / 2);
-            int* ptr = new int[vector->capacity]();
-            for (int i = 0; i < vector->size; i++)
-                ptr[i] = vector->pointer[i];
-            vector->size = size;
-            delete[] vector->pointer;
-            vector->pointer = ptr;
-        }
-        else vector->size = size;
-    }
-    else vector->size = size;
 }
