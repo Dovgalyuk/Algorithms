c5d0ab96ed32996e5a93c5c6bb704c6113c41c4a
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-10-07 16:12:57 +0300

    Update stack.h

--- LibraryCPP/stack.h
+++ LibraryCPP/stack.h
@@ -1,44 +1,30 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include "vector.h"
+// Stack
+// Stores integer values inside
+typedef int Data;
 
-template <typename Data>
-class Stack : Vector<Data> {
-public:
+struct Stack;
 
-    // Creates empty stack
-    Stack() = default;
+// Creates empty stack
+Stack *stack_create();
 
-    // Deletes the stack
-    ~Stack() = default;
+// Deletes the stack
+void stack_delete(Stack *stack);
 
-    // Pushes data on top of the stack
-    // Should be O(1) on average
-    void push(Data data) {
-        this->resize(this->length + 1);
-        this->data[this->length - 1] = data;
-    }
+// Pushes data on top of the stack
+// Should be O(1) on average
+void stack_push(Stack *stack, Data data);
 
-    // Retrives the last element from the stack
-    Data get() const {
-        return this->data[this->length - 1];
-    }
+// Retrives the last element from the stack
+Data stack_get(const Stack *stack);
 
-    // Removes the last element from the stack
-    // Should be O(1)
-    void pop() {
-        this->resize(this->length - 1);
-    }
+// Removes the last element from the stack
+// Should be O(1)
+void stack_pop(Stack *stack);
 
-    // Returns true if the stack is empty
-    bool empty() const {
-        return this->length == 0;
-    }
-
-    int size() {
-        return this->length;
-    }
-};
+// Returns true if the stack is empty
+bool stack_empty(const Stack *stack);
 
 #endif
