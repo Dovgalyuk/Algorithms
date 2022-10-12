42bd818e154b94514daf87cc03abc07894e9ce0d
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-10-12 23:15:57 +0300

    Update Source.cpp

--- Laba2/Source.cpp
+++ Laba2/Source.cpp
@@ -1,71 +1,60 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <cctype>
 #include "stack.h"
-
-void function(Stack<int> number, Stack<char> symbol) {
-    bool ZERO = true;
-    while (!symbol.empty()) {
-        char c = symbol.get();
-        symbol.pop();
-        int a = number.get();
-        number.pop();
-        int b = 0;
-        if (!number.empty()) {
-            b = number.get();
-            number.pop();
-            if (c == '+') {
-                number.push(a + b);
-            }
-            else if (c == '-') {
-                number.push(a - b);
-            }
-            else if (c == '*') {
-                number.push(a * b);
-            }
-            else if (c == '/') {
-                if (b != 0) {
-                    number.push(a + b);
-                }
-                else {
-                    //std::cout << "ZERO" << std::endl;
-                    ZERO = false;
-                }
-            }
-        }
-        else {
-            number.push(a);
-        }
-    }
+using namespace std;
+void check(Stack<int> number, char ch[], bool& ZERO)
+{
     int i = 0;
-    for (i = 0; !number.empty(); i++) {
+    int a = number.get();
+    number.pop();
+    int b = 0;
+    if (!number.empty()) {
+        b = number.get();
         number.pop();
-    }
-    if (ZERO == true) {
-        if (i < 2) std::cout << "UNDERFLOW" << std::endl;
-        else if (i > 1) std::cout << "OVERFLOW" << std::endl;
+        if (ch[i] == '+') {
+            number.push(a + b);
+            i++;
+        }
+        else if (ch[i] == '-') {
+            number.push(a - b);
+            i++;
+        }
+        else if (ch[i] == '*') {
+            number.push(a * b);
+            i++;
+        }
+        else if (ch[i] == '/') {
+            if (b != 0) {
+                number.push(a + b);
+                i++;
+            }
+            else {
+                ZERO = false;
+            }
+        }
     }
     else {
-        std::cout << "ZERO" << std::endl;
+        number.push(a);
     }
 }
-
 int main() {
     //  10 9 - + (UNDERFLOW)
     //  1 2 + 3 (OVERFLOW)
     //  90 0 / (ZERO)
-    Stack<int> number,number1;
-    Stack<char> symbol,symbol1;
-
-    std::ifstream fin;
-    fin.open("test.txt");
+    Stack<int> number, number1;
+    char arr[2]={' ',' '};
+    int i = 0;
+    bool ZERO = true;
+    ifstream fin;
+    fin.open("first.txt");
 
     if (fin.is_open()) {
         char ch, last_ch = '.';
         while (fin.get(ch)) {
             if (isdigit(ch)) {
                 if (isdigit(last_ch)) {
-                    int a = ch - '0',b = number.get();
+                    int a = ch - '0', b = number.get();
                     number.pop();
                     number.push(10 * b + a);
                 }
@@ -75,23 +64,30 @@ int main() {
                 }
             }
             else if (ch != '\n') {
-                symbol.push(ch);
+                
+                arr[i] = ch;
+                i++;
             }
             last_ch = ch;
         }
-        while (!number.empty()) {
-            number1.push(number.get());
-            number.pop();
-        }
-        while (!symbol.empty()) {
-            symbol1.push(symbol.get());
-            symbol.pop();
-        }
-
-        function(number1, symbol1);
     }
     else {
         std::cout << "Open error!\n";
+    }
+    while (!number.empty()) {
+        number1.push(number.get());
+        number.pop();
+    }
+    check(number1, arr, ZERO);
+    int j = 0;
+    for (j = 0; !number1.empty(); j++)
+        number1.pop();
+    if (ZERO == true) {
+        if (j <= 2) cout << "UNDERFLOW" << std::endl;
+        else if (j > 1) cout << "OVERFLOW" << std::endl;
+    }
+    else {
+        cout << "ZERO" << std::endl;
     }
     fin.close();
     return 0;
