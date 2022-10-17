fbf259cdfb091b8684f7eeec83269a9d0fc1b561
Pavel <90069034+Riba4koff@users.noreply.github.com>
2022-10-17 17:49:41 +0300

    Add files via upload

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,61 @@
-#include "array.h"
+#pragma once
+#pragma once
 
-struct Array
-{
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <cstddef>
+
+// Non-resizeable array
+// Stores integer values inside
+typedef int Data;
+
+struct Array {
+	Data* data;
+	size_t size;
+
+	Array(int size) {
+		this->size = size;
+		this->data = new Data[size];
+	}
+
+	~Array() {
+		delete[] data;
+	}
 };
 
+// returns array size
+size_t array_size(const Array* arr) {
+	return arr->size;
+}
+
 // create array
-Array *array_create(size_t size)
-{
-    return new Array;
+Array* array_create(size_t size) {
+	return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
-    delete arr;
+void array_delete(Array* arr) {
+	delete arr;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array* arr, size_t index) {
+	size_t size = array_size(arr);
+	if (index < size && index >= 0) {
+		return arr->data[index];
+	}
+	else {
+		return Data();
+	}
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+	size_t size = array_size(arr);
+	if (index < size && index >= 0) {
+		arr->data[index] = value;
+	}
 }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
-}
+#endif
