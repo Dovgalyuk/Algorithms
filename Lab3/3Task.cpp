1afca6b72ab69fb64768daf33529f9b7e1b99368
IPOMPONI <93474657+IPOMPONI@users.noreply.github.com>
2022-10-19 16:20:10 +0300

    Update 3Task.cpp

--- Lab3/3Task.cpp
+++ Lab3/3Task.cpp
@@ -8,7 +8,7 @@ using std::cin; using std::cout; using std::string;  using std::ifstream;
 int main()
 {
 	Queue* queue = queue_create();
-	int width, height = 0, distance = 0, X, Y, z = 0;
+	int width, height = 0, distance = 0, X = 0, Y = 0, z = 0;
 	bool flag = true;
 	string Patch, Str, BufStr;
 
@@ -28,7 +28,7 @@ int main()
 			++height;                                                       // Считаем высоту лабиринта
 			for (int i = 0; i < BufStr.size(); i++)                         // Записываем элементы лабиринта из буферной строки в выходящую      
 			{                                                            // P.S. я не знаю почему, но простые способы записи из файла в строку у меня рботали до конца первой строчки лабиринта (######)
-				Str += BufStr[i];                                // Поэтому пришлось выдумывать свой цикл... Зато рабочий :)
+					Str += BufStr[i];                                // Поэтому пришлось выдумывать свой цикл... Зато рабочий :)
 			}
 		}
 		cout << "\tA maze in the form of a string: " << Str << "\n\n";  // Получаем нашь лабирит в виде строки
@@ -38,13 +38,6 @@ int main()
 	cout << "\tWidth: " << width << "\tHeight: " << height << "\n\n";
 	file.close();                                                     // Закрываем файл
 	BufStr.clear();                                                 // Чистим буферную строку
-
-	string** maze = new string * [height];                         // Создаем двумерный масиив строк (матрицу) для лабиринта (При дебаге кода легко отслеживать текущее местоположение в лабиринте и запоминать уже посещенные места)
-	for (int i = 0; i < height; i++)
-	{
-		maze[i] = new string[width];
-	}
-
 
 	bool** mark = new bool* [height];                            // Создаем двумерный булевый массив для фиксации уже посещенных мест     
 	for (int i = 0; i < height; i++)
@@ -56,78 +49,97 @@ int main()
 		}
 	}
 
+	char** maze = new char * [height];                         // Создаем двумерный масиив символов (матрицу) для лабиринта (При дебаге кода легко отслеживать текущее местоположение в лабиринте и запоминать уже посещенные места)
 	for (int i = 0; i < height; i++)
 	{
+		maze[i] = new char[width];
 		for (int j = 0; j < width; j++)
 		{
-			maze[i][j] = Str[z];                            // Переносим все элементы лабиринта (который сейчас в виде строки Str) в двумерный массив строк (матрицу) maze[i][j]
+			maze[i][j] = Str[z];                            // Переносим все элементы лабиринта (который сейчас в виде строки Str) в двумерный массив символов (матрицу) maze[i][j]
 
-			if (Str[z] == 'X')                             // Находим стартовую позицию в лабиринте
+			if (maze[i][j] == 'X')                             // Находим стартовую позицию в лабиринте
 			{
 				queue_insert(queue, i);
 				queue_insert(queue, j);
+				mark[i][j] = true;                        // Помечаем текущее местоположение как посещенное
 			}
 			++z;
 		}
 	}
 
-	while (!queue_empty(queue))       // Пока очередь НЕ пуста
+	while (!queue_empty(queue) && flag)       // Пока очередь НЕ пуста и flag == true (число не найдено)
 	{
-		X = queue_get(queue);      // Берем из очереди элемент (наимен строки)
+		X = queue_get(queue);      // Берем из очереди элемент (наимен строки влабиринте)
 		queue_remove(queue);       // Удаляем взятый элемент из очереди
-		Y = queue_get(queue);       // Берем следующий элемент в очереди
+		Y = queue_get(queue);       // Берем следующий элемент в очереди   (наимен столбца влабиринте)
 		queue_remove(queue);      // Удаляем взятый элемент из очереди
-		mark[X][Y] = true;       // Помечаем текущее местоположение как посещенное
+		mark[X][Y] = true;
 
-		// Пока flag = true и в текщем месте НЕ '.' (в которую можно попасть), НЕ '#' и НЕ 'X' (старт), а искомое число, которое располагается ближе всего к старту
-		while (flag && maze[X][Y] != "." && (maze[X][Y] != "#") && (maze[X][Y] != "X"))
+		// Если найденный элемент - число 
+		if (isdigit(maze[X][Y]))
 		{
 			cout << "\tThe shortest number found: " << maze[X][Y] << "\n";              // Печатаем это число
 			flag = false;                                                     // Меняем flag на false, чтобы выйти из цикла
 		}
-		// Если место в которое мы хотим попасть доступно или если это искомое число (хочу пометить позицию искомого числа как посещенное место)
-		if (maze[X - 1][Y] == "." || maze[X - 1][Y] != "." && (maze[X - 1][Y] != "#") && (maze[X - 1][Y] != "X")) // Вверх в лабиринте
+		else
 		{
-			if (mark[X - 1][Y] == false)       // Если мы еще не были в этом месте
+			// Если место в которое мы хотим попасть доступно или если это искомое число (хочу пометить позицию искомого числа как посещенное место)
+			if (maze[X - 1][Y] == '.' || isdigit(maze[X - 1][Y])) // Вверх в лабиринте
 			{
-				mark[X - 1][Y] = true;        // Помечаем его как посещенное
-				queue_insert(queue, X - 1);  // Заносим его координаты, чтобы
-				queue_insert(queue, Y);     // далее его посетить 
-				++distance;                // Считаем пройденный путь
-			}
-		}      // Для остальных направлений движения по лабиринту все аналогично
+				if (mark[X - 1][Y] == false)       // Если мы еще не были в этом месте
+				{
+					if (isdigit(maze[X - 1][Y]))   // Если в этом месте число,
+					{
+						queue_remove(queue);      // То забываем про все предыдущие возможные пути,
+						queue_remove(queue);     // Ведь ближайшее число найдено
+					}
+					queue_insert(queue, X - 1);  // Заносим его координаты, чтобы
+					queue_insert(queue, Y);     // далее его посетить 
+				}
+			}      // Для остальных направлений движения по лабиринту все аналогично
 
-		if (maze[X + 1][Y] == "." || maze[X + 1][Y] != "." && (maze[X + 1][Y] != "#") && (maze[X + 1][Y] != "X")) {    // Вниз в лабиринте
-			if (mark[X + 1][Y] == false)
-			{
-				mark[X + 1][Y] = true;
-				queue_insert(queue, X + 1);
-				queue_insert(queue, Y);
-				++distance;
+			if (maze[X + 1][Y] == '.' || isdigit(maze[X + 1][Y])) {    // Вниз в лабиринте
+				if (mark[X + 1][Y] == false)
+				{
+					if (isdigit(maze[X + 1][Y]))
+					{
+						queue_remove(queue);
+						queue_remove(queue);
+					}
+					queue_insert(queue, X + 1);
+					queue_insert(queue, Y);
+				}
 			}
-		}
 
-		if (maze[X][Y + 1] == "." || maze[X][Y + 1] != "." && (maze[X][Y + 1] != "#") && (maze[X][Y + 1] != "X")) {    // Вправо в лабиринте
-			if (mark[X][Y + 1] == false)
-			{
-				mark[X][Y + 1] = true;
-				queue_insert(queue, X);
-				queue_insert(queue, Y + 1);
-				++distance;
+			if (maze[X][Y + 1] == '.' || isdigit(maze[X][Y + 1])) {    // Вправо в лабиринте
+				if (mark[X][Y + 1] == false)
+				{
+					if (isdigit(maze[X][Y + 1]))
+					{
+						queue_remove(queue);
+						queue_remove(queue);
+					}
+					queue_insert(queue, X);
+					queue_insert(queue, Y + 1);
+				}
 			}
-		}
 
-		if (maze[X][Y - 1] == "." || maze[X][Y + 1] != "." && (maze[X][Y + 1] != "#") && (maze[X][Y + 1] != "X")) {    // Влево в лабиринте
-			if (mark[X][Y - 1] == false)
-			{
-				mark[X][Y - 1] = true;
-				queue_insert(queue, X);
-				queue_insert(queue, Y - 1);
-				++distance;
+			if (maze[X][Y - 1] == '.' || isdigit(maze[X][Y - 1])) {    // Влево в лабиринте
+				if (mark[X][Y - 1] == false)
+				{
+					if (isdigit(maze[X][Y - 1]))
+					{
+						queue_remove(queue);
+						queue_remove(queue);
+					}
+					queue_insert(queue, X);
+					queue_insert(queue, Y - 1);
+				}
 			}
+			++distance;               // Считаем количество посещенных доступных мест при поиске числа
 		}
 	}
-	cout << "\n\tA complete bypass of all available (not counting the start) cells took: " << distance << "\n";  // Выводим количество посещенных доступных (кроме старта) мест на экран
+	cout << "\n\tThe path from the start: " << distance << " cells.\n";  // Выводим количество посещенных доступных (кроме старта) мест, на экран
 	cout << "\n\tMaze:\tVisited places in the maze:\n\n\n\t";
 
 	for (int i = 0; i < height; i++)                   // Выводим матрицу лабиринта и посещенных мест на экран
