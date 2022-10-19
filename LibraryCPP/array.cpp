2fb56d7516eb19e142c5b487861510120040ae93
RickCodeProg <98888624+RickCodeProg@users.noreply.github.com>
2022-10-19 06:44:01 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,12 +2,24 @@
 
 struct Array
 {
+    Data *data;
+    size_t sizeOfArray;
+    Array(const size_t size) 
+    {
+        arraySize = size;
+        data = new Data[arraySize];
+    }
+    ~Array
+    {
+        delete[] data
+    }
+    
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    return new Array(size);
 }
 
 // delete array, free memory
@@ -19,16 +31,27 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (index<array_size(arr))
+    {
+        return arr->data[index];
+    }
+    else
+    {
+        return null;
+    }    
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index<array_size(arr))
+    {
+        arr->data[index]=value;
+    }
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->arraySize;
 }
