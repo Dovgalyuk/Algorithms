3c587bb0ad065b69c72c9bce6318e9320a05930b
Pavel <90069034+Riba4koff@users.noreply.github.com>
2022-10-20 23:58:40 +0300

    реализация модуля вектор

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,34 +1,71 @@
 #include "vector.h"
+#include <memory.h>
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t capacity;
+
+    Vector() {
+        this->size = 0; //Initial size
+        this->capacity = 10;//Initial capacity
+        this->data = new Data[capacity];
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
-Vector *vector_create()
+Vector* vector_create()
 {
-    return new Vector;
+    return new Vector();
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
-    // TODO: free vector internals
-    delete vector; 
+    delete vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
+Data vector_get(const Vector* vector, size_t index)
 {
-    return (Data)0;
+    size_t size = vector_size(vector);
+    if (index < size)
+        return vector->data[index];
+    else
+        return Data();
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
+void vector_set(Vector* vector, size_t index, Data value)
 {
+    size_t size = vector_size(vector);
+    if (index < size)
+        vector->data[index] = value;
 }
 
-size_t vector_size(const Vector *vector)
+size_t vector_size(const Vector* vector)
 {
-    return 0;
+    return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
+void vector_resize(Vector* vector, size_t size)
 {
+    if (size <= vector->capacity) {
+        vector->size = size;
+    }
+    else {
+
+        size_t new_capacity = size * 2;
+
+        Data* new_data = new Data[new_capacity];
+
+        memcpy(new_data, vector->data, vector_size(vector) * sizeof(Data));
+
+        delete[] vector->data;
+
+        vector->size = size;
+        vector->capacity = new_capacity;
+        vector->data = new_data;
+    }
 }
