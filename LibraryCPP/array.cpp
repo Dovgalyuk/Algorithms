ddfe6edeed5878eeccdbfee403a9bf244efd313b
Nechis <90858272+Nechis@users.noreply.github.com>
2022-10-22 01:20:30 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,32 +1,74 @@
 #include "array.h"
-#include <stdlib.h>
-#include <iostream>
-using namespace std;
-typedef int Data;
-typedef Array<Data> MyArray;
 
-int main(){
-	srand(time(NULL));
-	setlocale(LC_ALL, "RUS");
-	int n;
-	int sum = 0;
-	cout << "Введите размерность массива: ";
-	cin >> n;
-	MyArray* array = new MyArray(n);
-	for (int i = 0; i < n; i++) {
-		array->set(i, rand() % 100);
-	}
-	cout << "Массив: ";
-	array->output();
-	cout << endl;
-	array->sort();
-	cout << "Массив(отсортированный): ";
-	array->output();
-	cout << endl;
-	for (int i = 0; i < array->size(); i++) {
-		if (array->get(i) != array->max() and array->get(i) != array->min()) {
-			sum += array->get(i);
-		}
-	}
-	cout << "Сумма элементов массива(без минимального и максимального элементов): " << sum << endl;
+struct Array
+{
+    Data* arr_1;
+    size_t count;
+
+    Array(size_t count)
+    {
+        _n = count;
+        _arr = new Data[count];
+    }
+
+    ~Array()
+    {
+        delete[] arr_1;
+    }
+};
+
+// create array
+Array *array_create(size_t size)
+{
+    return new Array(size);
+}
+
+// delete array, free memory
+void array_delete(Array *arr)
+{
+    delete arr;
+}
+
+// returns specified array element
+Data array_get(const Array *arr, size_t index)
+{
+    if (index >= array_size(arr))
+        return 0;
+    else
+        return arr->arr_1[index];
+}
+
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value)
+{
+    if (index >= array_size(arr))
+ 	    return;
+    else
+        arr->arr_1[index] = value;
+}
+
+// returns array size
+size_t array_size(const Array *arr)
+{
+    return arr->count;
+}
+
+int array_min(Array* arr) {
+    int min = arr->arr_1[0];
+    for (int i = 0; i < array_size(arr); ++i) {
+        if (min > arr->arr_1[i]) {
+            min = arr->arr_1[i];
+        }
+    }
+    return min;
+}
+
+int array_max(Array* arr) {
+    int max = arr->arr_1[0];
+    for (int i = 0; i < array_size(arr); ++i) {
+        if (max < arr->arr_1[i]) {
+            max = arr->arr_1[i];
+        }
+    }
+    return max;
 }
