260508b4ce522be245085b3012014140ca3b7e16
Nechis <90858272+Nechis@users.noreply.github.com>
2022-10-25 13:37:25 +0300

    Update arrayss.cpp

--- Laba_1/arrayss.cpp
+++ Laba_1/arrayss.cpp
@@ -3,17 +3,32 @@
 #include <iostream>
 using namespace std;
 
-int Zad(Array* zar, int const SIZE) {
+int Zad(Array* zar) {
+	int min = 0;
+	int max = 0;
+	bool ser_min = false;
+	bool ser_max = false;
 	int sum = 0;
-	int count = 0;
-	for (int i = 0; i < SIZE; i++) {
+	int count_min = 0;
+	int count_max = 0;
+	for (int i = 0; i < array_size(zar); i++) {
 		array_set(zar, i, rand() % 100);
 		cout << array_get(zar, i) << " ";
 	}
-	for (int i = 0; i < SIZE; i++) {
-		if (array_get(zar, i) == array_min(zar) or array_get(zar, i) == array_max(zar))
-			count++;
-		if (count == 1 and array_get(zar, i) != array_min(zar) and array_get(zar, i) != array_max(zar))
+	min = array_min(zar);
+	max = array_max(zar);
+	for (int i = 0; i < array_size(zar); i++) {
+		if (array_get(zar, i) == max) {
+			ser_max = true;
+			count_max++;
+		}
+		if (array_get(zar, i) == min) {
+			ser_min = true;
+			count_min++;
+		}
+		if (ser_max == true and ser_min == true)
+			return;
+		if ((ser_max == true and count_max > 1) or (ser_min == true and count_min > 1) or (array_get(zar, i) != max and array_get(zar, i) != min and (ser_max == true or ser_min == true))
 			sum += array_get(zar, i);
 	}
 	return sum;
@@ -26,6 +41,6 @@ int main(){
 	cout << "Введите размерность массива: ";
 	cin >> n;
 	Array* zar = array_create(n);
-	cout << "Сумма элементов массива(без минимального и максимального элементов): " << Zad(zar, n) << endl;
+	cout << "Сумма элементов массива(без минимального и максимального элементов): " << Zad(zar) << endl;
 	array_delete(zar);
 }
