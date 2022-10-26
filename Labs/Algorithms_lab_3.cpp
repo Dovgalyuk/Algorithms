3444634921f89e8b1fe1e08a61b7dc1ba438bf3d
Pavel <90069034+Riba4koff@users.noreply.github.com>
2022-10-27 02:42:12 +0300

    empty lab

---
+++ Labs/Algorithms_lab_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "list.h"
+#include "queue.h"
+
+using namespace std;
+
+void show_queue(Queue *queue){
+	while (queue_empty(queue)) {
+		cout << queue_get(queue) << " ";
+		queue_remove(queue);
+	}
+}
+
+
+int main()
+{
+	Queue *queue = queue_create();
+
+	queue_insert(queue, 1);
+	queue_insert(queue, 2);
+	queue_insert(queue, 3);
+	queue_insert(queue, 4);
+	queue_insert(queue, 5);
+
+	show_queue(queue);
+}
