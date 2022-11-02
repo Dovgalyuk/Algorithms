40ac2b93bdffe2efd65b9b9c77edba2ef65ac521
IPOMPONI <93474657+IPOMPONI@users.noreply.github.com>
2022-11-02 18:33:07 +0300

    Update graph.h

--- LibraryCPPTemplate/graph.h
+++ LibraryCPPTemplate/graph.h
@@ -4,241 +4,243 @@
 #include "list.h"
 #include <vector>
 
-template<typename Data> class Graph 
+template<typename Data> class Graph
 {
-    public:
+public:
 
-        struct Edge;
+    struct Edge;
 
-        struct Vertex 
+    struct Vertex
+    {
+        Data data;
+        List<Edge*> edges;
+
+        Vertex(Data VertexData) : data(VertexData) {}
+     
+        ~Vertex()
         {
-            Data data;
-            List<Edge*> edges = List<Edge*>();
-      
-            Vertex(Data VertexData): data(VertexData) {}
+            auto Del = edges.first();
 
-            ~Vertex() 
+            while (Del != nullptr)
             {
-                auto Del = edges.first();
-
-                while(Del != nullptr) 
-                {
-                    delete Del->data();
-                    Del = Del->next();
-                }
-            }
-
-            void SetData(Data data_) 
-            {
-                data = data_;
-            }
-
-            Data GetData() 
-            {
-                return data;
-            }
-
-            void AddEdge(Vertex* toVertex) 
-            {
-                edges.insert(new Edge(toVertex));
-            }
-
-            Edge* GetEdge(Vertex* toVertex) 
-            {
-                for (auto i = edges.first(); i; i = i->next()) 
-                {
-                    if (i->data()->GetToVertex() == toVertex) 
-                    {
-                        return i->data();
-                    }
-                }
-                return nullptr;
-            }
-
-            void RemoveEdge(Vertex* toVertex) 
-            {
-                for (auto i = edges.first(); i; i = i->next()) 
-                {
-                    if (i->data()->GetToVertex() == toVertex) 
-                    {
-                        delete i->data();
-                        edges.erase(i);
-                        break;
-                    }
-                }
-            }
-        };
-
-        struct Edge 
-        {
-            Vertex* toVertex;
-            int weight = 0;
-
-            Edge(Vertex* toVertex): toVertex(toVertex) {}
-
-            Vertex* GetToVertex() 
-            {
-                return toVertex;
-            }
-
-            void SetWeight(int weight_) 
-            {
-                weight = weight_;
-            }
-
-            int GetWeight() 
-            {
-                return weight;
-            }
-        };
-
-        struct EdgeIterator 
-        {
-            typename List<Edge*>::Item * edgeElem;
-
-            EdgeIterator(Vertex* head) 
-            {
-                edgeElem = head->edges.first();
-            }
-
-            void operator ++() 
-            {
-                if (edgeElem != nullptr)
-                {
-                    edgeElem = edgeElem->next();
-                }
-            }
-
-            Edge* operator *() 
-            {
-                if (edgeElem != nullptr)
-                {
-                    return edgeElem->data();
-                }
-                else 
-                {
-                    return nullptr;
-                }
-            }
-        };
-
-        Graph(size_t VertexNumber, Data data)
-        {
-            VertexVec.resize(VertexNumber);
-            for (size_t i = 0; i < VertexNumber; i++)
-            {
-                VertexVec[i] = new Vertex(data);
+                delete Del->data();
+                Del = Del->next();
             }
         }
 
-        ~Graph() 
+        void SetData(Data data_)
+        {
+            data = data_;
+        }
+
+        Data GetData()
+        {
+            return data;
+        }
+
+        void AddEdge(Vertex* toVertex)
+        {
+            edges.insert(new Edge(toVertex));
+        }
+
+        Edge* GetEdge(Vertex* toVertex)
+        {
+            for (auto i = edges.first(); i; i = i->next())
+            {
+                if (i->data()->GetToVertex() == toVertex)
+                {
+                    return i->data();
+                }
+            }
+            return nullptr;
+        }
+
+        void RemoveEdge(Vertex* toVertex)
+        {
+            for (auto i = edges.first(); i; i = i->next())
+            {
+                if (i->data()->GetToVertex() == toVertex)
+                {
+                    delete i->data();
+                    edges.erase(i);
+                    break;
+                }
+            }
+        }
+    };
+
+    struct Edge
+    {
+        Vertex* toVertex;
+        int weight = 0;
+
+        Edge(Vertex* toVertex) : toVertex(toVertex) {}
+
+        Vertex* GetToVertex()
+        {
+            return toVertex;
+        }
+
+        void SetWeight(int weight_)
+        {
+            weight = weight_;
+        }
+
+        int GetWeight()
+        {
+            return weight;
+        }
+    };
+
+    struct EdgeIterator
+    {
+        typename List<Edge*>::Item* edgeElem;
+
+        EdgeIterator(Vertex* head)
+        {
+            edgeElem = head->edges.first();
+        }
+
+        void operator ++()
+        {
+            if (edgeElem != nullptr)
+            {
+                edgeElem = edgeElem->next();
+            }
+        }
+
+        Edge* operator *()
+        {
+            if (edgeElem != nullptr)
+            {
+                return edgeElem->data();
+            }
+            else
+            {
+                return nullptr;
+            }
+        }
+    };
+
+    Graph(size_t VertexNumber, Data data)
+    {
+        VertexVec.resize(VertexNumber);
+        for (size_t i = 0; i < VertexNumber; i++)
+        {
+            VertexVec[i] = new Vertex(data);
+        }
+    }
+
+    ~Graph()
+    {
+        for (int i = 0; i < VertexVec.size(); i++)
+        {
+            delete VertexVec[i];
+        }
+    }
+
+    size_t SizeVertexVec()
+    {
+        return VertexVec.size();
+    }
+
+    bool AvailabilityLink(Vertex* fromVertex, Vertex* toVertex)
+    {
+        for (auto i = fromVertex->edges.first(); i != nullptr; i = i->next())
+        {
+            if (i->data()->GetToVertex() == toVertex)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Vertex* AddVertex(Vertex* data)
+    {
+        auto* vertex = new Vertex(data);
+        VertexVec.push_back(vertex);
+
+        return vertex;
+    }
+
+    Vertex* GetVertex(size_t index)
+    {
+        return VertexVec[index];
+    }
+
+    void RemoveVertex(size_t index)
+    {
+        if (index < VertexVec.size())
         {
             for (int i = 0; i < VertexVec.size(); i++)
             {
-                delete VertexVec[i];
+                VertexVec[i]->RemoveEdge(VertexVec[index]);
             }
-        }
 
-        size_t SizeVertexVec()
-        {
-            return VertexVec.size();
-        }
-        
-        bool AvailabilityLink(Vertex* fromVertex, Vertex* toVertex)                 
-        {
-            for (auto i = fromVertex->edges.first(); i != nullptr; i = i->next()) 
+            delete VertexVec[index];
+            for (int i = index + 1; i < VertexVec.size(); i++)
             {
-                if (i->data()->GetToVertex() == toVertex) 
-                {
-                    return true;
-                }
+                VertexVec[i - 1] = VertexVec[i];
             }
-            return false;
+            VertexVec.pop_back();
         }
+    }
 
-        Vertex* AddVertex(Vertex* data) 
+    void SetVertexData(size_t index, Data data)
+    {
+        VertexVec[index]->SetData(data);
+    }
+
+    Data GetVertexData(size_t index)
+    {
+        return VertexVec[index]->GetData();
+    }
+   
+    void AddEdge(Vertex* fromVertex, Vertex* toVertex, int weightEdge)
+    {
+
+        if (AvailabilityLink(fromVertex, toVertex))
         {
-            auto* vertex = new Vertex(data);
-            VertexVec.push_back(vertex);
-
-            return vertex;
+            fromVertex->GetEdge(toVertex)->SetWeight(weightEdge);
         }
-
-        Vertex* GetVertex(size_t index) 
+        else
         {
-            return VertexVec[index];
+            fromVertex->AddEdge(toVertex);
+            fromVertex->GetEdge(toVertex)->SetWeight(weightEdge);
         }
+    }
 
-        void RemoveVertex(size_t index) 
+    Edge* GetEdge(Vertex* fromVertex, Vertex* toVertex)
+    {
+        return fromVertex->GetEdge(toVertex);
+    }
+
+    int GetWeightEdge(Vertex* fromVertex, Vertex* toVertex)
+    {
+        return fromVertex->GetEdge(toVertex)->GetWeight();
+    }
+
+    void RemoveEdge(Vertex* fromVertex, Vertex* toVertex)
+    {
+        fromVertex->RemoveEdge(toVertex);
+    }
+
+    size_t GetIndexVertex(Vertex* vertex)
+    {
+        for (size_t i = 0; i < VertexVec.size(); i++)
         {
-            if (index < VertexVec.size())
+            if (VertexVec[i] == vertex)
             {
-                for (int i = 0; i < VertexVec.size(); i++)
-                {
-                    VertexVec[i]->RemoveEdge(VertexVec[index]);
-                }
-
-                delete VertexVec[index];
-                for (int i = index + 1; i < VertexVec.size(); i++)
-                {
-                    VertexVec[i-1] = VertexVec[i];
-                }
-                VertexVec.pop_back();
+                return i;
             }
         }
 
-        void SetVertexData(size_t index, Data data)
-        {
-            VertexVec[index]->SetData(data);
-        }
+        return -1;
+    }
 
-        Data GetVertexData(size_t index)
-        {
-            return VertexVec[index]->GetData();
-        }
-        
-        void AddEdge(Vertex* fromVertex, Vertex* toVertex, int weightEdge) 
-        {
-
-            if (AvailabilityLink(fromVertex, toVertex))
-            {
-                fromVertex->GetEdge(toVertex)->SetWeight(weightEdge);
-            }
-            else 
-            {
-                fromVertex->AddEdge(toVertex);
-                fromVertex->GetEdge(toVertex)->SetWeight(weightEdge);
-            }
-        }
-
-        Edge* GetEdge (Vertex* fromVertex, Vertex* toVertex) 
-        {
-            return fromVertex->GetEdge(toVertex);
-        }
-
-        int GetWeightEdge(Vertex* fromVertex, Vertex* toVertex) 
-        {
-            return fromVertex->GetEdge(toVertex)->GetWeight();
-        }
-
-        void RemoveEdge(Vertex* fromVertex, Vertex* toVertex) 
-        {
-            fromVertex->RemoveEdge(toVertex);
-        }
-
-        size_t GetIndexVertex(Vertex* vertex) 
-        {
-            for (size_t i = 0; i < VertexVec.size(); i++)
-            {
-                if (VertexVec[i] == vertex)
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-    private:
-        std::vector<Vertex*> VertexVec;
+private:
+    std::vector<Vertex*> VertexVec;
 };
 #endif
+
