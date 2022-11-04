50b0d63e38a23ad0e1dae0beec838328cee03411
Artem Raykh <77747763+jscelle@users.noreply.github.com>
2022-11-04 14:33:49 +0300

    lab1 solution

--- LibraryCPPTemplate/Tests/array.cpp
+++ LibraryCPPTemplate/Tests/array.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "array.h"
+#include <ctime>
+#include "stdlib.h"
 
 typedef Array<int> MyArray;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void solution();
 
 int main()
 {
@@ -26,4 +34,52 @@ int main()
     }
 
     delete arr;
+
+    solution();
+}
+
+void solution() {
+    srand(time(NULL));
+    int size, shift, direction;
+    cout << "Input size: " << endl;
+    cin >> size;
+    cout << "Choose shift count an direction (0 -> direction left, 1 -> direction right): " << endl;
+    cin >> shift >> direction;
+    if (direction != 0 && direction != 1) {
+        cout << "Wrong direction input" << endl;
+    }
+
+    Array<int>* array = new Array<int>(size);
+
+    for (size_t i = 0; i < size; ++i) {
+        array->set(i, rand());
+    }
+
+    cout << "Non shifted array: " << endl;
+    cout << array << endl;
+
+    if (direction == 0) {
+        cout << "Shifting to left: " << endl;
+        for (int i = 0; i < array->size(); i++) {
+            if (i + shift < array->size())
+                array->set(i, array->get(i+shift));
+            else
+                array->set(i, 0);
+            cout << array << endl;
+        }
+    } else if (direction == 1) {
+        cout << "Shifting to right: " << endl;
+        for (int i = array->size() - 1; i >= 0; i--) {
+            if (i - shift < array->size())
+                array->set(i, array->get(i-shift));
+            else
+                array->set(i, 0);
+            cout << array << endl;
+        }
+    }
+
+    cout << "Shifted array: " << endl;
+    cout << array << endl;
+
+    delete array;
 }
--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,16 +1,17 @@
 #ifndef ARRAY_TEMPLATE_H
 #define ARRAY_TEMPLATE_H
 
+using std::ostream;
+using std::endl;
+
 template <typename Data> class Array
 {
 public:
     // create array
-    explicit Array(size_t size)
-    {
-    }
+    explicit Array(size_t size): _size(size), _array(new Data[size]){}
 
     // copy constructor
-    Array(const Array &a)
+    Array(const Array &a): _size(a._size), _array(std::copy(a._array))
     {
         // implement or disable this function
     }
@@ -18,33 +19,64 @@ public:
     // assignment operator
     Array &operator=(const Array &a)
     {
-        // implement or disable this function
+        _size = a._size;
+        _array = std::copy(a._array);
         return *this;
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] _array;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return _array[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        _array[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return _size;
+    }
+
+    friend ostream& operator<<(ostream& os, const Array<Data>& dt)
+    {
+        os << '[';
+        for (int i = 0; i < dt._size; ++i) {
+            os << dt.get(i);
+            if (i + 1 != dt._size) {
+                os << ',';
+            }
+        }
+        os << ']' << endl;
+        return os;
+    }
+
+    friend ostream& operator<<(ostream& os, const Array<Data>* dt)
+    {
+        os << '[';
+        for (int i = 0; i < dt->_size; ++i) {
+            os << dt->get(i);
+            if (i + 1 != dt->_size) {
+                os << ',';
+            }
+        }
+        os << ']' << endl;
+        return os;
     }
 
 private:
+    Data* _array;
+    size_t _size;
     // private data should be here
 };
 
