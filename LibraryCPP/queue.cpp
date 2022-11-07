ea4272c1253e55b69b663b87b4c70395294523b8
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-07 16:00:10 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -10,39 +10,39 @@ struct Queue
     }
 
     ~Queue() {
-        list_delete(this->list);
+        list_delete(this->list); 
     }
 };
 
-Queue* queue_create()
+Queue *queue_create()
 {
     return new Queue();
 }
 
-void queue_delete(Queue* queue)
+void queue_delete(Queue *queue)
 {
     delete queue;
 }
 
-void queue_insert(Queue* queue, Data data)
+void queue_insert(Queue *queue, Data data)
 {
     if (queue_empty(queue))
         list_insert(queue->list, data);
     else
-        list_insert_after(queue->list, list_insert(queue->list,data), data);
+        list_insert_after(queue->list, list_last(queue->list), data);
 }
 
-Data queue_get(const Queue* queue)
+Data queue_get(const Queue *queue)
 {
     return list_item_data(list_first(queue->list));
 }
 
-void queue_remove(Queue* queue)
+void queue_remove(Queue *queue)
 {
     list_erase(queue->list, list_first(queue->list));
 }
 
-bool queue_empty(const Queue* queue)
+bool queue_empty(const Queue *queue)
 {
     return !list_first(queue->list);
 }
