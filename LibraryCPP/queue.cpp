1f1b3f63a26feee03e9861b1d52417b4b731712c
ErikMak <47Poligons@gmail.com>
2022-11-10 00:54:06 +0300

    Lab3; minor fixes

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -4,14 +4,14 @@
 struct Queue
 {
     Vector *vector;
-    size_t head;
-    size_t rear;
+    int head;
+    int rear;
 
     Queue() {
         vector = vector_create();
-        vector_resize(vector, 2);
-        head = 0;
-        rear = 0;
+        vector_resize(vector, 20);
+        head = -1;
+        rear = -1;
     }
     ~Queue() {
         vector_delete(vector);
@@ -31,13 +31,22 @@ void queue_delete(Queue *queue)
 void queue_insert(Queue *queue, Data data)
 {
     size_t size = vector_size(queue->vector);
-    size_t rear = queue->rear;
 
-    if(rear == size - 1) {
-        vector_resize(queue->vector, size*2);
+    if(queue_empty(queue)) {
+        queue->rear = 0;
+        queue->head = 0;
+    } else if (queue->rear % size == queue->head) {
+        throw "Error: Queue is full!";
     }
+
+    int rear = queue->rear % size;
+    // if(rear == size - 1)
+
+    // if(rear == size - 1) {
+    //     vector_resize(queue->vector, size*2);
+
     vector_set(queue->vector, rear, data);
-    queue->rear++;
+    queue->rear = rear+1;
 }
 
 Data queue_get(const Queue *queue)
@@ -50,14 +59,23 @@ Data queue_get(const Queue *queue)
 }
 
 void queue_remove(Queue *queue)
-{
+{   
     size_t size = vector_size(queue->vector);
-    if(!queue_empty(queue) && queue->head <= size) {
-        queue->head++;
+    int head = queue->head;
+
+    if(!queue_empty(queue)) {
+        head++;
+
+        if(queue->rear == head) {
+            queue->head = -1;
+            queue->rear = -1;
+        } else {
+            queue->head = head % size;
+        }
     }
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return (queue->rear == queue->head);
+    return (queue->rear == -1 && queue->head == -1);
 }
