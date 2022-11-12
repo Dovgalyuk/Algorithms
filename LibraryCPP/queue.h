c7a804b9d08a0cac4eb39b47e821e5e0c9c49b68
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-13 00:24:24 +0300

    Update queue.h

--- LibraryCPP/queue.h
+++ LibraryCPP/queue.h
@@ -1,30 +1,59 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <list.h>
 
-// Queue
-// Stores integer values inside
-typedef int Data;
+template <typename Data> class Queue
+{
+public:
+    // Create empty queue
+    Queue()
+    {
+        _list = new List<Data>();
+    }
 
-struct Queue;
+    // Deletes queue
+    ~Queue()
+    {
+        delete _list;
+    }
 
-// Create empty queue
-Queue *queue_create();
+    // Includes new element into the queue
+    // Should be O(1) on average
+    void insert(Data data)
+    {
+        if (empty()) {
+            _list->insert(data);
+            _last = _list->first();
+        } else {
+            _list->insert(data);
+        }
+    }
 
-// Deletes queue
-void queue_delete(Queue *queue);
+    // Retrieves first element from the queue
+    Data get() const
+    {
+        return _last->data();
+    }
 
-// Includes new element into the queue
-// Should be O(1) on average
-void queue_insert(Queue *queue, Data data);
+    // Removes first element from the queue
+    // Should be O(1) on average
+    void remove()
+    {
+        auto *prev = _last->prev();
+        _list->erase(_last);
+        _last = prev;
+    }
 
-// Retrieves first element from the queue
-Data queue_get(const Queue *queue);
+    // Returns true if the queue is empty
+    bool empty() const
+    {
+        return _list->first() == nullptr;
+    }
 
-// Removes first element from the queue
-// Should be O(1) on average
-void queue_remove(Queue *queue);
-
-// Returns true if the queue is empty
-bool queue_empty(const Queue *queue);
+private:
+    // private data should be here
+    List<Data>* _list;
+    typename List<Data>::Item* _last;
+};
 
 #endif
