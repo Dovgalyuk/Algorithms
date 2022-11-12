839dd690a07ad47fa7ed37f845f147beb9c2ab82
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-13 00:25:07 +0300

    Update list.h

--- LibraryCPP/list.h
+++ LibraryCPP/list.h
@@ -1,47 +1,108 @@
 #ifndef LIST_H
 #define LIST_H
 
-// List
-// Stores integer values inside
-typedef int Data;
+template <typename Data> class List
+{
+public:
+    class Item
+    {
+    public:
+        Item *nextItem = nullptr;
+        Item *prevItem = nullptr;
+        Item *next() { return nextItem; }
+        Item *prev() { return prevItem; }
+        Data data() const { return data_; }
+        void setData(Data data_) {
+            this->data_ = data_;
+        }
+    private:
+        // internal data here
+        Data data_;
+    };
 
-struct List;
-struct ListItem;
+    // Creates new list
+    List()
+    {
+        firstItem = nullptr;
+    }
 
-// Creates new list
-List* list_create();
+    // Destroys the list and frees the memory
+    ~List()
+    {
+        Item* item = firstItem;
+        while (item) {
+            Item* deletedItem = item;
+            item = item->next();
+            delete deletedItem;
+        }
+    }
 
-// Destroys the list and frees the memory
-void list_delete(List* list);
+    // Retrieves the first item from the list
+    Item *first()
+    {
+        return firstItem;
+    }
 
-// Retrieves the first item from the list
-ListItem* list_first(List* list);
+    // Inserts new list item into the beginning
+    Item *insert(Data data)
+    {
+        Item* item = new Item();
+        item->setData(data);
+        item->nextItem = firstItem;
+        if (firstItem) {
+            firstItem->prevItem = item;
+        }
+        firstItem = item;
+        return item;
+    }
 
-// Extracts data from the list item
-Data list_item_data(const ListItem* item);
+    // Inserts new list item after the specified item
+    Item *insert_after(Item *item, Data data)
+    {
 
-// Returns list item following after the specified one
-ListItem* list_item_next(ListItem* item);
+        Item* newItem = new Item();
+        newItem->setData(data);
+        newItem->nextItem = item->next();
+        if (newItem->next()) {
+            newItem->next()->prevItem = newItem;
+        }
+        item->nextItem = newItem;
+        newItem->prevItem = item;
+        return newItem;
+    }
 
-// Returns previous element for the specified item.
-// Not applicable for the singly linked lists.
-ListItem* list_item_prev(ListItem* item);
+    // Deletes the specified list item.
+    // Returns pointer to the item next to the deleted one.
+    // Not applicable for the singly linked lists.
+    // Should be O(1)
+    Item *erase(Item *item)
+    {
 
-// Inserts new list item into the beginning
-ListItem* list_insert(List* list, Data data);
+        if (firstItem == item) {
+            firstItem = item->next();
+        }
+        if (item->next()) {
+            item->next()->prevItem = item->prevItem;
+        }
+        if (item->prev()) {
+            item->prev()->nextItem = item->nextItem;
+        }
+        Item* newItem = item->next();
+        delete item;
+        return newItem;
 
-// Inserts new list item after the specified item
-ListItem* list_insert_after(List* list, ListItem* item, Data data);
+    }
 
-// Deletes the specified list item.
-// Returns pointer to the item next to the deleted one.
-// Not applicable for the singly linked lists.
-// Should be O(1)
-ListItem* list_erase(List* list, ListItem* item);
-
-// Deletes the list item following the specified one.
-// Returns pointer to the item next to the deleted one.
-// Should be O(1)
-ListItem* list_erase_next(List* list, ListItem* item);
+    // Deletes the list item following the specified one.
+    // Returns pointer to the item next to the deleted one.
+    // Should be O(1)
+    Item *erase_next(Item *item)
+    {
+        return erase(item->next());
+    }
+private:
+    // private data should be here
+    Item *firstItem;
+};
 
 #endif
