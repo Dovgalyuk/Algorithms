415575bedc516600d8304f5b048be72f150bcdf8
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-13 00:35:07 +0300

    Update list.h

--- LibraryCPP/list.h
+++ LibraryCPP/list.h
@@ -1,108 +1,33 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <typename Data> class List
-{
-public:
-    class Item
-    {
-    public:
-        Item *nextItem = nullptr;
-        Item *prevItem = nullptr;
-        Item *next() { return nextItem; }
-        Item *prev() { return prevItem; }
-        Data data() const { return data_; }
-        void setData(Data data_) {
-            this->data_ = data_;
-        }
-    private:
-        // internal data here
-        Data data_;
-    };
+// List
+// Stores integer values inside
+typedef int Data;
 
-    // Creates new list
-    List()
-    {
-        firstItem = nullptr;
-    }
+struct DLList;
+struct DLListItem;
 
-    // Destroys the list and frees the memory
-    ~List()
-    {
-        Item* item = firstItem;
-        while (item) {
-            Item* deletedItem = item;
-            item = item->next();
-            delete deletedItem;
-        }
-    }
+DLList* list_create();
 
-    // Retrieves the first item from the list
-    Item *first()
-    {
-        return firstItem;
-    }
+void list_delete(DLList* list);
 
-    // Inserts new list item into the beginning
-    Item *insert(Data data)
-    {
-        Item* item = new Item();
-        item->setData(data);
-        item->nextItem = firstItem;
-        if (firstItem) {
-            firstItem->prevItem = item;
-        }
-        firstItem = item;
-        return item;
-    }
+DLListItem* list_first(DLList* list);
 
-    // Inserts new list item after the specified item
-    Item *insert_after(Item *item, Data data)
-    {
+Data list_item_data(const DLListItem* item);
 
-        Item* newItem = new Item();
-        newItem->setData(data);
-        newItem->nextItem = item->next();
-        if (newItem->next()) {
-            newItem->next()->prevItem = newItem;
-        }
-        item->nextItem = newItem;
-        newItem->prevItem = item;
-        return newItem;
-    }
+DLListItem* list_item_next(DLListItem* item);
 
-    // Deletes the specified list item.
-    // Returns pointer to the item next to the deleted one.
-    // Not applicable for the singly linked lists.
-    // Should be O(1)
-    Item *erase(Item *item)
-    {
+DLListItem* list_item_prev(DLListItem* item);
 
-        if (firstItem == item) {
-            firstItem = item->next();
-        }
-        if (item->next()) {
-            item->next()->prevItem = item->prevItem;
-        }
-        if (item->prev()) {
-            item->prev()->nextItem = item->nextItem;
-        }
-        Item* newItem = item->next();
-        delete item;
-        return newItem;
+DLListItem* list_insert(DLList* list, Data data);
 
-    }
+DLListItem* list_insert_after(DLList* list, DLListItem* item, Data data);
 
-    // Deletes the list item following the specified one.
-    // Returns pointer to the item next to the deleted one.
-    // Should be O(1)
-    Item *erase_next(Item *item)
-    {
-        return erase(item->next());
-    }
-private:
-    // private data should be here
-    Item *firstItem;
-};
+DLListItem* list_erase(DLList* list, DLListItem* item);
+
+DLListItem* list_erase_next(DLList* list, DLListItem* item);
+
+DLListItem* list_erase_first(DLList* list);
 
 #endif
