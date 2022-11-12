a8c21ef145fbf3a0e4155dd3fcb2dba98d76c905
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-13 00:31:16 +0300

    Update queue.h

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -1,5 +1,6 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <list.h>
 
 template <typename Data> class Queue
 {
@@ -7,52 +8,52 @@ public:
     // Create empty queue
     Queue()
     {
-    }
-
-    // copy constructor
-    Queue(const Queue &a)
-    {
-        // implement or disable this function
-    }
-
-    // assignment operator
-    Queue &operator=(const Queue &a)
-    {
-        // implement or disable this function
-        return *this;
+        _list = new List<Data>();
     }
 
     // Deletes queue
     ~Queue()
     {
+        delete _list;
     }
 
     // Includes new element into the queue
     // Should be O(1) on average
     void insert(Data data)
     {
+        if (empty()) {
+            _list->insert(data);
+            _last = _list->first();
+        } else {
+            _list->insert(data);
+        }
     }
 
     // Retrieves first element from the queue
     Data get() const
     {
-        return Data();
+        return _last->data();
     }
 
     // Removes first element from the queue
     // Should be O(1) on average
     void remove()
     {
+        auto *prev = _last->prev();
+        _list->erase(_last);
+        _last = prev;
     }
 
     // Returns true if the queue is empty
     bool empty() const
     {
-        return true;
+        return _list->first() == nullptr;
     }
 
 private:
     // private data should be here
+    List<Data>* _list;
+    typename List<Data>::Item* _last;
 };
 
 #endif
