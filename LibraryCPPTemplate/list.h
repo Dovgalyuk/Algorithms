a223d8bb76b91b91a18531b608135060d43e707e
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-13 00:32:00 +0300

    Update list.h

--- LibraryCPPTemplate/list.h
+++ LibraryCPPTemplate/list.h
@@ -7,52 +7,68 @@ public:
     class Item
     {
     public:
-        Item *next() { return nullptr; }
-        Item *prev() { return nullptr; }
-        Data data() const { return Data(); }
+        Item *nextItem = nullptr;
+        Item *prevItem = nullptr;
+        Item *next() { return nextItem; }
+        Item *prev() { return prevItem; }
+        Data data() const { return data_; }
+        void setData(Data data_) {
+            this->data_ = data_;
+        }
     private:
         // internal data here
+        Data data_;
     };
 
     // Creates new list
     List()
     {
-    }
-
-    // copy constructor
-    List(const List &a)
-    {
-        // implement or disable this function
-    }
-
-    // assignment operator
-    List &operator=(const List &a)
-    {
-        // implement or disable this function
-        return *this;
+        firstItem = nullptr;
     }
 
     // Destroys the list and frees the memory
     ~List()
     {
+        Item* item = firstItem;
+        while (item) {
+            Item* deletedItem = item;
+            item = item->next();
+            delete deletedItem;
+        }
     }
 
     // Retrieves the first item from the list
     Item *first()
     {
-        return nullptr;
+        return firstItem;
     }
 
     // Inserts new list item into the beginning
     Item *insert(Data data)
     {
-        return nullptr;
+        Item* item = new Item();
+        item->setData(data);
+        item->nextItem = firstItem;
+        if (firstItem) {
+            firstItem->prevItem = item;
+        }
+        firstItem = item;
+        return item;
     }
 
     // Inserts new list item after the specified item
     Item *insert_after(Item *item, Data data)
     {
-        return nullptr;
+
+        Item* newItem = new Item();
+        newItem->setData(data);
+        newItem->nextItem = item->next();
+        if (newItem->next()) {
+            newItem->next()->prevItem = newItem;
+        }
+        item->nextItem = newItem;
+        newItem->prevItem = item;
+        return newItem;
     }
 
     // Deletes the specified list item.
@@ -61,7 +77,20 @@ public:
     // Should be O(1)
     Item *erase(Item *item)
     {
-        return nullptr;
+
+        if (firstItem == item) {
+            firstItem = item->next();
+        }
+        if (item->next()) {
+            item->next()->prevItem = item->prevItem;
+        }
+        if (item->prev()) {
+            item->prev()->nextItem = item->nextItem;
+        }
+        Item* newItem = item->next();
+        delete item;
+        return newItem;
+
     }
 
     // Deletes the list item following the specified one.
@@ -69,10 +98,11 @@ public:
     // Should be O(1)
     Item *erase_next(Item *item)
     {
-        return nullptr;
+        return erase(item->next());
     }
 private:
     // private data should be here
+    Item *firstItem;
 };
 
 #endif
