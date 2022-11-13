bb0ba85ddf7e3ea55c125b17479eb65ad1eb4ecf
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-11-13 16:28:01 +0300

    Add files via upload

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <ctime>
+#include "array.h"
+
+
+
+using namespace std;
+
+int counting(Array* arr, int const SIZE)
+{
+	int shet = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		array_set(arr, i, rand() % 51);
+		if (array_get(arr, i) % 2 == 0)
+		{
+			shet++;
+		}
+	}
+	return shet;
+}
+
+void completion(Array* arr, Array* arr2, int const SIZE)
+{
+	int shet = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (array_get(arr, i) % 2 == 0)
+		{
+			array_set(arr2, shet, i);
+			shet++;
+		}
+	}
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+    int size;
+    cout << "Введите длину массива: ";
+    cin >> size;
+	Array* mass = array_create(size); //создаём первый массив с заданным размером
+
+	int size2 = counting(mass, size); // заполняем первый массив, считая при этом кол-во четных
+
+	Array* mass2 = array_create(size2);// создаём второй массив с размером раным кол-ву четных значений из первого
+
+	completion(mass, mass2, size);// заполняем второй массив
+
+	for (int i = 0; i < size2; i++) //вывод индексов четных значений среди элементов первого массива
+	{
+		cout << array_get(mass2, i) << " , ";
+	}
+	
+}
