f7d72ad45cef21445a480180c1f897e29795a5fa
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-14 23:02:03 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,26 +1,22 @@
+#include <stdlib.h>
 #include "queue.h"
 #include "list.h"
 
 struct Queue
 {
-    List* list;
-
-    Queue() {
-        this->list = list_create();
-    }
-
-    ~Queue() {
-        list_delete(this->list);
-    }
+    DLList* list;
 };
 
 Queue* queue_create()
 {
-    return new Queue();
+    Queue* queue = new Queue;
+    queue->list = list_create();
+    return queue;
 }
 
 void queue_delete(Queue* queue)
 {
+    list_delete(queue->list);
     delete queue;
 }
 
@@ -31,15 +27,15 @@ void queue_insert(Queue* queue, Data data)
 
 Data queue_get(const Queue* queue)
 {
-    return list_item_data(list_first(queue->list));
+    return list_item_data(list_item_prev(list_first(queue->list)));
 }
 
 void queue_remove(Queue* queue)
 {
-    list_erase(queue->list, list_first(queue->list));
+    list_erase(queue->list, list_item_prev(list_first(queue->list)));
 }
 
 bool queue_empty(const Queue* queue)
 {
-    return !list_first(queue->list);
+    return (queue->list) == 0;
 }
