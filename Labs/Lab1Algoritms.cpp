93ba6fa11a5425bd3169c5a95a2540609ba01fe2
igor81258 <96629556+igor81258@users.noreply.github.com>
2022-11-17 01:23:05 +0300

    Lab1
    Решение 1 лабораторной

---
+++ Labs/Lab1Algoritms.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include "array.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int size;
+	int sum = 0;
+	int composition = 1;
+	Data putArray; 
+	cin >> size;
+	Array* arr = array_create(size);
+	for (int i = 0; i < size; i++)
+	{
+		cin >> putArray;
+		array_set(arr, i, putArray);
+	}
+	
+
+	for (int i = 0; i <size; i++)
+	{
+		composition *= array_get(arr, i);
+		sum += array_get(arr, i);
+
+	}
+	cout <<"Сумма " << sum << ' ' << "Произведение " << composition;
+}
+
