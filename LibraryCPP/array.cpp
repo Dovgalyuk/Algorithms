b292facc65585bdb36fdbd2774bdcf0b145bd614
KozlikGoat <96651218+KozlikGoat@users.noreply.github.com>
2022-11-17 18:44:03 +0300

    Add files via upload

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,44 @@
+#pragma once
 #include "array.h"
 
-struct Array
-{
+struct Array{
+    Data* data;
+    size_t size;
+
+    Array(size_t size) {
+        this->data = new Data[size];
+    }
+    ~Array() {
+        delete[] data;
+    }
 };
 
 // create array
-Array *array_create(size_t size)
-{
-    return new Array;
+Array* array_create(size_t size){
+    return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
+void array_delete(Array* arr){
     delete arr;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array* arr, size_t index){
+    size_t size = array_size(arr);
+    if (index < size && index >= 0) return arr->data[index];
+    return Data();
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+    size_t size = array_size(arr);
+    if (index < size && index >= 0) {
+        arr->data[index] = value;
+    }
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr){
+    return arr->size;
 }
