a669f4b91eb8110e59bd95f5cb4eb3f639060e78
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-17 23:52:19 +0300

    Update queue.h

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -9,7 +9,6 @@ public:
     Queue()
     {
         list = new List<Data>();
-        size = 0;
     }
 
     // Deletes queue
@@ -22,43 +21,40 @@ public:
     // Should be O(1) on average
     void insert(Data data)
     {
-        if (lastItem) {
-            lastItem = list->insert_after(lastItem, data);
+        if (empty()) {
+            list->insert(data);
+            lastItem = list->first();
         }
         else {
-            lastItem = list->insert(data);
+            list->insert(data);
         }
-        size++;
     }
 
     // Retrieves first element from the queue
     Data get() const
     {
-        return list->first()->data();
+        return lastItem->data();
     }
 
     // Removes first element from the queue
     // Should be O(1) on average
     void remove()
     {
-        list->erase(list->first());
-        if (size == 1) {
-            lastItem == nullptr;
-        }
-        size--;
+        auto* prev = lastItem->prev();
+        list->erase(lastItem);
+        lastItem = prev;
     }
 
     // Returns true if the queue is empty
     bool empty() const
     {
-        return (size == 0);
+        return list->first() == nullptr;
     }
 
 private:
     // private data should be here
     List<Data>* list;
-    size_t size;
-    typename List<Data>::Item* lastItem = nullptr;
+    typename List<Data>::Item* lastItem;
 };
 
 #endif
