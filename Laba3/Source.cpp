9268caefe24d8a7ed531b55eed0ee8a018febd51
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-11-17 20:10:32 +0300

    Update Source.cpp

--- Laba3/Source.cpp
+++ Laba3/Source.cpp
@@ -7,40 +7,39 @@ using namespace std;
 
 int main()
 {
-
-    const int COUNT = 1000000;
-    int n, s, f,t;
-    cin >> n >> s >> f; 
-    vector<vector<int>>a(n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-        {
-            cin >> t;
-            if (t == 1) 
-                a[i].push_back(j);
-        }
-    Queue* q = queue_create();
-    vector<int>b(n, 1e6);
-    s--;
-    f--;
-    queue_insert(q, s);
-    b[s] = 0;
-    while (!queue_empty(q))
-    {
-        int v = queue_get(q);
-        queue_remove(q);
-        for (int i = 0; i < a[v].size(); i++)
-        {
-            int next = a[v][i];
-            if (b[next] > b[v] + 1)
-            {
-                b[next] = b[v] + 1;
-                queue_insert(q, next);
-            }
-        }
-    }
-    if (b[f] < COUNT) cout << b[f];
-    else cout <<"IMPOSSIBLE";
-    queue_delete(q);
-    return 0;
+   const int COUNT = 1000000;
+   int n, s, f,t;
+   cin >> n >> s >> f; 
+   vector<vector<int>>a(n);
+   for (int i = 0; i < n; i++)
+       for (int j = 0; j < n; j++)
+       {
+           cin >> t;
+           if (t == 1) 
+               a[i].push_back(j);
+       }
+   auto queue = new Queue<char>;
+   vector<int>b(n, 1e6);
+   s--;
+   f--;
+   queue->insert(s);
+   b[s] = 0;
+   while (! queue->empty())
+   {
+       int v =  queue->get();
+       queue->remove();
+       for (int i = 0; i < a[v].size(); i++)
+       {
+           int next = a[v][i];
+           if (b[next] > b[v] + 1)
+           {
+               b[next] = b[v] + 1;
+                queue->insert(next);
+           }
+       }
+   }
+   if (b[f] < COUNT) cout << b[f];
+   else cout <<"IMPOSSIBLE";
+    queue->remove();
+   return 0;
 }
