21127752d551b4cec47e52020b70453d8692e9ce
IPOMPONI <93474657+IPOMPONI@users.noreply.github.com>
2022-11-20 12:44:36 +0300

    Add files via upload

---
+++ LibraryCPPTemplate/Tests/associative_array.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+#include "associative_array.h"
+using namespace std;
+int main() {
+    RBAssociativeArray<string, string> array(to_string(NULL), to_string(NULL));
+    int count = 8000000;
+    for (int i = 0; i < count; ++i) {
+        array.Insert(to_string(i), to_string(i));
+    }
+    srand(time(0));
+    for (int i = 0; i < 5; ++i) {
+        int id = rand() % count;
+        if (array.SearchTree(to_string(id)) == false)
+        {
+            cout << "Data storage error" << id << "\n\n";
+            return 1;
+        }
+    }
+    for (int i = 0; i < 5; ++i) {
+        int id = rand() % count;
+        array.DeleteNode(to_string(id));
+
+        if (array.SearchTree(to_string(id))==true)
+        {
+            cout << "Data deletion was not successful\t id: " << id << "\n\n";
+            return 1;
+        }
+    }
+}
