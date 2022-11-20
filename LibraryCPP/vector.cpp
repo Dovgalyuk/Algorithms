db0a1f3ff11df8c9a3daed93cbde160745236689
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-11-20 16:53:01 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,7 +1,20 @@
 #include "vector.h"
+#include <memory.h>
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t allsize;
+    Vector() {
+        this->size = 0; //Initial size
+        this->allsize = 10;//Initial capacity
+        this->data = new Data[allsize];
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -11,24 +24,50 @@ Vector *vector_create()
 
 void vector_delete(Vector *vector)
 {
-    // TODO: free vector internals
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    if (index < vector_size(vector))
+    {
+        return vector->data[index];
+    }
+    else
+        return false;
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    if (index < vector_size(vector))
+    {
+        vector->data[index] = value;
+    }
+    
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size <= vector->allsize) {
+        vector->size = size;
+    }
+    else {
+
+        size_t new_capacity = size * 2;
+
+        Data* new_data = new Data[new_capacity];
+
+        memcpy(new_data, vector->data, vector_size(vector) * sizeof(Data));
+
+        delete[] vector->data;
+
+        vector->size = size;
+        vector->allsize = size;
+        vector->data = new_data;
+    }
 }
