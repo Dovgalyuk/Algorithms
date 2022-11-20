81022070d2b5a020cf1a45dba0f4e54115d19a5e
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-11-20 16:55:11 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,7 +1,16 @@
 #include "stack.h"
-
+#include "vector.h"
 struct Stack
 {
+    Vector* vector;
+
+    Stack() {
+        this->vector = vector_create();
+    }
+
+    ~Stack() {
+        vector_delete(this->vector);
+    }
 };
 
 Stack *stack_create()
@@ -11,24 +20,65 @@ Stack *stack_create()
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
     delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    size_t size = vector_size(stack->vector);
+    vector_resize(stack->vector, size + 1);
+    vector_set(stack->vector, size, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    if (!stack_empty(stack))
+    {
+        return vector_get(stack->vector, vector_size(stack->vector) - 1);
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void stack_pop(Stack *stack)
 {
+    if (!stack_empty(stack))
+    {
+        vector_resize(stack->vector, vector_size(stack->vector) - 1);
+    }
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return vector_size(stack->vector) == 0;
+}
+
+int stack_prior(Stack* stack)
+{
+    if (stack_get(stack) == '(')
+    {
+        return 1;
+    }
+    if (stack_get(stack) == ')')
+    {
+        return 1;
+    }
+    if (stack_get(stack) == '+')
+    {
+        return 2;
+    }
+    if (stack_get(stack) == '-')
+    {
+        return 2;
+    }
+    if (stack_get(stack) == '*')
+    {
+        return 3;
+    }
+    if (stack_get(stack) == '/')
+    {
+        return 3;
+    }
 }
