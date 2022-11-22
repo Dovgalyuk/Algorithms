5ef9734b07a7d1d10ef608850f472684f4b83c1b
igor81258 <96629556+igor81258@users.noreply.github.com>
2022-11-22 23:34:25 +0300

    fix

--- Labs/Lab1Algoritms.cpp
+++ Labs/Lab1Algoritms.cpp
@@ -3,28 +3,53 @@
 #include <iostream>
 #include "array.h"
 using namespace std;
+//int main()
+//{
+//	setlocale(LC_ALL, "rus");
+//	int size;
+//	int sum = 0;
+//	int composition = 1;
+//	Data putArray; 
+//	cin >> size;
+//	Array* arr = array_create(size);
+//	for (int i = 0; i < size; i++)
+//	{
+//		cin >> putArray;
+//		array_set(arr, i, putArray);
+//	}
+//	
+//
+//	for (int i = 0; i <size; i++)
+//	{
+//		composition *= array_get(arr, i);
+//		sum += array_get(arr, i);
+//
+//	}
+//	cout <<"Сумма " << sum << ' ' << "Произведение " << composition;
+//}
+
+
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	int size;
-	int sum = 0;
-	int composition = 1;
-	Data putArray; 
-	cin >> size;
-	Array* arr = array_create(size);
-	for (int i = 0; i < size; i++)
-	{
-		cin >> putArray;
-		array_set(arr, i, putArray);
-	}
-	
+    Array* arr = array_create(10);
 
-	for (int i = 0; i <size; i++)
-	{
-		composition *= array_get(arr, i);
-		sum += array_get(arr, i);
+    if (array_size(arr) != 10)
+    {
+        std::cout << "Invalid array size\n";
+        return 1;
+    }
 
-	}
-	cout <<"Сумма " << sum << ' ' << "Произведение " << composition;
+    for (int i = 0; i < 10; ++i)
+        array_set(arr, i, i * 2);
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (array_get(arr, i) != i * 2)
+        {
+            std::cout << "Invalid array element " << i << "\n";
+            return 1;
+        }
+    }
+
+    array_delete(arr);
 }
-
