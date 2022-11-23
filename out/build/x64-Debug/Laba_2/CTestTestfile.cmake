a21f73509b324aa869959afa18136a0649599a35
Nechis <90858272+Nechis@users.noreply.github.com>
2022-11-23 12:26:44 +0300

    Удалено

--- out/build/x64-Debug/Laba_2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/source/repos/Algorithms3/Laba_2
-# Build directory: C:/Users/user/source/repos/Algorithms3/out/build/x64-Debug/Laba_2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
