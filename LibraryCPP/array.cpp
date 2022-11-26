d3979529838f8cc8a8c99fc4c80cedc037fef07d
Terit21 <117031460+Terit21@users.noreply.github.com>
2022-11-26 18:18:27 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,45 @@
 
 struct Array
 {
+    size_t number;
+    Data *data;
+    Array(size_t size)
+    {
+        number = size;
+        data = new Data[size];
+    }
+    ~Array()
+    {
+        delete[] data;
+    }
 };
 
 // create array
-Array *array_create(size_t size)
+Array* array_create(size_t size)
 {
-    return new Array;
+    return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
+void array_delete(Array* arr)
 {
-    delete arr;
+    delete arr->data;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
+Data array_get(const Array* arr, size_t index)
 {
-    return (Data)0;
+    return (arr->data[index]);
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+void array_set(Array* arr, size_t index, Data value)
 {
+    arr->data[index] = value;
 }
 
 // returns array size
-size_t array_size(const Array *arr)
+size_t array_size(const Array* arr)
 {
-    return 0;
+    return arr->number;
 }
