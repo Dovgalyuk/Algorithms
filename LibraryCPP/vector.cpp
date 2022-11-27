8c86d10987ed7791ba71f082f11ebf3daa76d270
julktnn <101501787+julktnn@users.noreply.github.com>
2022-11-28 00:03:09 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,33 +2,70 @@
 
 struct Vector
 {
+    size_t size;
+    size_t capacity;
+    Data* data;
 };
 
 Vector *vector_create()
 {
-    return new Vector;
+    Vector* vector = new Vector;
+    vector->size = vector->capacity = 0;
+    vector->data = new Data[vector->capacity];
+    return vector;
 }
 
-void vector_delete(Vector *vector)
+void vector_delete(Vector* vector)
 {
     // TODO: free vector internals
-    delete vector; 
+    delete[] vector->data;
+    delete vector;
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    if (index < vector_size(vector))
+    {
+        return vector->data[index];
+    }
+    else
+        return false;
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    if (index < vector->size)
+    {
+        vector->data[index] = value;
+    }
+    else
+    {
+        vector_resize(vector, index + 1);
+        vector->data[index] = value;
+    }
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size <= vector->capacity)
+    {
+        vector->size = size;
+    }
+    else {
+        size_t new_capacity = size * 2;
+        Data* new_data = new Data[new_capacity];
+        for (size_t i = 0; i < vector->size; i++)
+        {
+            new_data[i] = vector->data[i];
+        }
+        delete[] vector->data;
+        vector->data = new_data;
+        vector->size = size;
+        vector->capacity = new_capacity;
+    }
 }
