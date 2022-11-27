843e663619d7c05fd40d553104766f863b6e9196
GoroziyNikita <94207421+GoroziyNikita@users.noreply.github.com>
2022-11-27 19:27:57 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,62 @@
 
 struct Array
 {
+    size_t count ;
+    Data *arr;
+     Array(size_t n) {
+     count = n;
+     arr = new Data[count];
+
+     }
+     ~Array(){
+        delete[] arr;
+     }
+
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+return new Array(size);
+   
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
-    delete arr;
+  delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    
+    if (index >= array_size(arr))
+    {
+        return 0;
+    }
+    else 
+    {
+        return arr->arr[index];
+    }
+
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index >= array_size(arr))
+    {
+        return 0;
+    }
+    else 
+    {
+        arr->arr[index]=value;
+    }
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+   return arr->count;
 }
