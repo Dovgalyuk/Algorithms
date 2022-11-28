0104e7cadaedb962de2711c90f3ba3cfc7090f6a
MIDHEY <45918010+MIDHEY@users.noreply.github.com>
2022-11-29 00:29:06 +0300

    Add files via upload

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,143 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* pointerNext, * pointerPrev;
+
+    ListItem(Data data, ListItem* pointerNext = nullptr, ListItem* pointerPrev = nullptr) {
+        this->data = data;
+        this->pointerPrev = pointerPrev;
+        this->pointerNext = pointerNext;
+    }
 };
 
 struct List
 {
+    ListItem* ListHead, *ListTail;
+
+    List() {
+        this->ListHead = nullptr;
+        this->ListTail = nullptr;
+    }
+
 };
 
-List *list_create()
+List* list_create()
 {
     return new List;
 }
 
-void list_delete(List *list)
+void list_delete(List* list)
 {
-    // TODO: free items
+    ListItem* current = list->ListHead;
+    while (current) {
+        list->ListHead = current->pointerNext;
+        delete current;
+        current = list->ListHead;
+    }
     delete list;
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)
 {
-    return NULL;
+    return list->ListHead;
 }
 
-Data list_item_data(const ListItem *item)
+ListItem* list_last(List* list)
 {
-    return (Data)0;
+    return list->ListTail;
 }
 
-ListItem *list_item_next(ListItem *item)
+Data list_item_data(const ListItem* item)
 {
-    return NULL;
+    if (item != NULL) return item->data;  else return NULL;
 }
 
-ListItem *list_item_prev(ListItem *item)
+ListItem* list_item_next(ListItem* item)
 {
-    return NULL;
+    return item->pointerNext;
 }
 
-ListItem *list_insert(List *list, Data data)
+ListItem* list_item_prev(ListItem* item)
 {
-    return NULL;
+    return item->pointerPrev;
 }
 
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
+//Вставляем элемент в начало списка
+ListItem* list_insert(List* list, Data data)
 {
-    return NULL;
+    if (list->ListHead) return list->ListHead = new ListItem (data, list->ListHead); 
+    else {
+        list->ListHead = new ListItem(data);
+        list->ListTail = list->ListHead;
+        return list->ListHead;
+    }
 }
 
-ListItem *list_erase(List *list, ListItem *item)
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
 {
-    return NULL;
+    if (!list->ListHead)
+    {
+        list->ListHead = new ListItem(data);
+        list->ListTail = list->ListHead;
+        return list->ListHead;
+    }
+    else if (list->ListHead->pointerNext == nullptr) {
+        ListItem* add_item = new ListItem(data, nullptr, item);
+        list->ListTail = add_item;
+        return list->ListHead->pointerNext = new ListItem(data, nullptr, item);
+    } 
+    else if (item->pointerNext == nullptr) {
+        ListItem* add_item = new ListItem(data, nullptr, item);
+        list->ListTail = add_item;
+        return item->pointerNext = new ListItem(data, nullptr, item);
+    }
+    else {
+        ListItem* add_item = new ListItem(data, item->pointerNext, item);
+        item->pointerNext = add_item;
+        add_item->pointerNext->pointerPrev = add_item;
+        return add_item;
+    }
 }
 
-ListItem *list_erase_next(List *list, ListItem *item)
+ListItem* list_erase(List* list, ListItem* item)
 {
-    return NULL;
+    if (item == list_first(list)) {
+        if (item == list_last(list)) {
+            delete item;
+            return list->ListHead = list->ListTail = nullptr;
+        }
+        list->ListHead = item->pointerNext;
+        item->pointerNext->pointerPrev = nullptr;
+        delete item;
+        return list->ListHead;
+    }
+    else if (item == list_last(list)) {
+        list->ListTail = item->pointerPrev;
+        item->pointerPrev->pointerNext = nullptr;
+        delete item;
+        return list->ListTail;
+    }
+    else {
+        ListItem* temp_item = item->pointerNext;
+        item->pointerPrev->pointerNext = temp_item;
+        item->pointerNext->pointerPrev = item->pointerPrev;
+        delete item;
+        return temp_item;
+    }    
+}
+
+ListItem* list_erase_next(List* list, ListItem* item)
+{
+    if (item != list_last(list))
+    {
+        ListItem* deleted_item = item->pointerNext;
+        item->pointerNext = deleted_item->pointerNext;
+        deleted_item->pointerNext->pointerPrev = item;
+        delete deleted_item;
+        return item->pointerNext;
+    }
+    else {
+        return list->ListHead = list->ListTail = nullptr;
+    }
 }
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,52 @@
 #include "stack.h"
+#include "list.h"
 
 struct Stack
 {
+    List* list;
+    
+    Stack() {
+        this->list = list_create();
+    }
+
+    ~Stack() {
+        list_delete(this->list);
+    }
 };
 
-Stack *stack_create()
+Stack* stack_create()
 {
     return new Stack;
 }
 
-void stack_delete(Stack *stack)
+void stack_delete(Stack* stack)
 {
     // TODO: free stack elements
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
+void stack_push(Stack* stack, Data data)
 {
+    if (list_first(stack->list) == nullptr)
+    {
+        list_insert(stack->list, data);
+    }
+    else {
+        list_insert_after(stack->list, list_last(stack->list), data);
+    }
 }
 
-Data stack_get(const Stack *stack)
+Data stack_get(const Stack* stack)
 {
-    return (Data)0;
+    return list_item_data(list_last(stack->list));
 }
 
-void stack_pop(Stack *stack)
+void stack_pop(Stack* stack)
 {
+    list_erase(stack->list, list_last(stack->list));
 }
 
-bool stack_empty(const Stack *stack)
+bool stack_empty(const Stack* stack)
 {
-    return true;
+    if (list_first(stack->list) == nullptr) return true; else return false;
 }
