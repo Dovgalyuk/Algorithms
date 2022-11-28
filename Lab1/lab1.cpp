c9c1e8116946e980ae09a40461d8ed8371ec9a12
Terit21 <117031460+Terit21@users.noreply.github.com>
2022-11-28 14:52:23 +0300

    Update lab1.cpp

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -5,30 +5,6 @@ using namespace std;
 
 int main()
 {
-	//test
-	Array* arr = array_create(10);
-
-	if (array_size(arr) != 10)
-	{
-		std::cout << "Invalid array size\n";
-		return 1;
-	}
-
-	for (int i = 0; i < 10; ++i)
-		array_set(arr, i, i * 2);
-
-	for (int i = 0; i < 10; ++i)
-	{
-		if (array_get(arr, i) != i * 2)
-		{
-			std::cout << "Invalid array element " << i << "\n";
-			return 1;
-		}
-	}
-
-	array_delete(arr);
-
-	//Task
 	int a, mx = 0, sum, p;
 	Array* A = NULL;
 	cin >> a;
@@ -37,13 +13,16 @@ int main()
 	{
 		array_set(A, i, rand()%51);
 	}
-	for (int i = 0; i < a - 5; i++)
+	sum = 0;
+	for (int j = 0; j < 5; j++)
 	{
-		sum = 0;
-		for (int j = i; j < i + 5; j++)
-		{
-			sum += array_get(A, j);
-		}
+		sum += array_get(A, j);
+	}
+	mx = sum;
+	p = 0;
+	for (int i = 1; i < a - 5; i++)
+	{
+		sum += array_get(A, i + 4) - array_get(A, i - 1);
 		if (mx < sum)
 		{
 			mx = sum;
