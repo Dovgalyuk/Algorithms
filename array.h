405f709a3cd8cc5dbe02fef8570678c2e5cc7c46
Dmitriy-Sazonov <58565484+Dmitriy-Sazonov@users.noreply.github.com>
2022-11-30 15:38:37 +0300

    Add files via upload

--- array.h
+++ array.h
@@ -1,48 +1,27 @@
-#pragma once
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <cstddef>
+
+// Non-resizeable array
+// Stores integer values inside
 typedef int Data;
 
-struct Array
-{
-	Data* _arr;
-	size_t _n;
-
-	Array(size_t n)
-	{
-		_n = n;
-		_arr = new Data[_n];
-	}
-
-
-};
-
-// returns array size
-size_t array_size(const Array arr)
-{
-	//return 0;
-	return arr._n;
-}
+struct Array;
 
 // create array
-Array array_create(size_t size)
-{
-	return Array(size);
-}
+Array *array_create(size_t size);
 
 // delete array, free memory
+void array_delete(Array* arr);
+
 // returns specified array element
-Data array_get(const Array arr, size_t index)
-{// return (Data)0;
-	if (index >= array_size(arr))
-		return 0;
-	else
-		return arr._arr[index];
-}
+Data array_get(const Array* arr, size_t index);
 
 // sets the specified array element to the value
-void array_set(Array arr, size_t index, Data value)
-{
-	if (index >= array_size(arr))
-		return;
-	else
-		arr._arr[index] = value;
-}
+void array_set(Array* arr, size_t index, Data value);
+
+// returns array size
+size_t array_size(const Array* arr);
+
+#endif
