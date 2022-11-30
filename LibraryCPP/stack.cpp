b73cc06958ace3060761e60a0b1a1f36abac45d4
igor81258 <96629556+igor81258@users.noreply.github.com>
2022-12-01 00:05:15 +0300

    Add files via upload

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,50 @@
 #include "stack.h"
-
+#include "vector.h"
 struct Stack
 {
+    Vector* vector;
+
+    Stack() {
+        this->vector = vector_create();
+    }
+
+    ~Stack() {
+        vector_delete(this->vector);
+    }
 };
 
-Stack *stack_create()
+Stack* stack_create()
 {
-    return new Stack;
+    return new Stack();
 }
 
-void stack_delete(Stack *stack)
+void stack_delete(Stack* stack)
 {
     // TODO: free stack elements
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
+void stack_push(Stack* stack, Data data)
 {
+    vector_resize(stack->vector, vector_size(stack->vector) + 1);
+    vector_set(stack->vector, vector_size(stack->vector) - 1, data );
 }
 
-Data stack_get(const Stack *stack)
+Data stack_get(const Stack* stack)
 {
-    return (Data)0;
+
+    return stack_empty(stack) ? throw "Empty stack error"
+        : vector_get(stack->vector, vector_size(stack->vector) - 1);
+
 }
 
-void stack_pop(Stack *stack)
+void stack_pop(Stack* stack)
 {
+    stack_empty(stack) ? throw "Empty stack error" :
+        vector_resize(stack->vector, vector_size(stack->vector) - 1);
 }
 
-bool stack_empty(const Stack *stack)
+bool stack_empty(const Stack* stack)
 {
-    return true;
+    return vector_size(stack->vector) == 0;
 }
