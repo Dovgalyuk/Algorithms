8888f742858782d44239e690faea6a38d16c5aad
MIDHEY <45918010+MIDHEY@users.noreply.github.com>
2022-12-02 17:56:09 +0300

    Add files via upload

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,6 +2,19 @@
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t capacity;
+
+    Vector() {
+        this->size = 0;
+        this->capacity = 5;
+        this->data = new Data[capacity];
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -11,24 +24,56 @@ Vector *vector_create()
 
 void vector_delete(Vector *vector)
 {
-    // TODO: free vector internals
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    size_t size = vector_size(vector);
+    if (index<size) {
+        return vector->data[index];
+    } 
+    else {
+        throw "You entered wrong index!";
+    }
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    size_t size = vector_size(vector);
+    if (index < size) {
+        vector->data[index] = value;
+    } 
+    else {
+        throw "You entered wrong index! Use function «vector_resize» to change vector size.";
+    }
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (vector->capacity>size)
+    {
+        vector->size = size;
+    }
+    else {
+        size_t temp_capacity = vector->capacity * 2;
+        Data* temp_data = new Data[temp_capacity];
+
+        for (int i = 0; i < vector->size; i++)
+        {
+            temp_data[i] = vector->data[i];
+        }
+
+        delete[] vector->data;
+
+        vector->capacity = temp_capacity;
+        vector->size = size;
+        vector->data = temp_data;
+    }
+    
 }
