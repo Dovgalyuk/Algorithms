6883756e476e887c42663d462fd780c4533e08f9
Terit21 <117031460+Terit21@users.noreply.github.com>
2022-12-05 23:56:28 +0300

    Update list.cpp

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,86 @@
 
 struct ListItem
 {
+    ListItem* pNext;
+    Data data;
+
 };
 
 struct List
 {
+    ListItem* head;
+    List()
+    {
+        head = NULL;
+    }
 };
 
-List *list_create()
+List* list_create()
 {
-    return new List;
+    return new List();
 }
 
-void list_delete(List *list)
+void list_delete(List* list)
 {
     // TODO: free items
     delete list;
+    
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)
+{
+    return list->head;
+}
+
+Data list_item_data(const ListItem* item)
+{
+    return item->data;
+}
+
+ListItem* list_item_next(ListItem* item)
+{
+    return item->pNext;
+}
+
+// Not applicable for the singly linked lists.
+ListItem* list_item_prev(ListItem* item)
 {
     return NULL;
 }
 
-Data list_item_data(const ListItem *item)
+ListItem* list_insert(List* list, Data data)
 {
-    return (Data)0;
+    ListItem* a = new ListItem;
+    a->data = data;
+    a->pNext= list_first(list);
+    list->head = a;
+    return a;
 }
 
-ListItem *list_item_next(ListItem *item)
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
+{
+    ListItem* a = new ListItem;
+    a->data = data;
+    a->pNext = item->pNext;
+    item->pNext = a;
+    return a;
+}
+
+// Not applicable for the singly linked lists.
+ListItem* list_erase(List* list, ListItem* item)
 {
     return NULL;
 }
 
-ListItem *list_item_prev(ListItem *item)
+ListItem* list_erase_next(List* list, ListItem* item)
 {
-    return NULL;
-}
+    ListItem* erase = nullptr;
+    if (list->head)
+    {
+        erase = item->pNext;
+        item->pNext = list_item_next(list_item_next(item));
+        delete erase;
+        return item;
+    }
 
-ListItem *list_insert(List *list, Data data)
-{
-    return NULL;
-}
-
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
-{
-    return NULL;
-}
-
-ListItem *list_erase(List *list, ListItem *item)
-{
-    return NULL;
-}
-
-ListItem *list_erase_next(List *list, ListItem *item)
-{
-    return NULL;
 }
