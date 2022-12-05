6ff0bbb159b1441a26fefe5638beef9b73fbc2fb
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-05 14:15:44 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,13 +1,14 @@
 #include "queue.h"
 #include "list.h"
 #include <cstddef>
+
 struct Queue
 {
     List* list;
     size_t tail;
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
     Queue* queue = new Queue;
     queue->list = list_create();
@@ -15,24 +16,37 @@ Queue *queue_create()
     return queue;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
     list_delete(queue->list);
     queue->tail = NULL;
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
-        list_insert(queue->list, data);
-        queue->tail++;
+    list_insert(queue->list, data);
+    queue->tail++;
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-        return list_item_data(list_item_prev(list_first(queue->list)));
+    return list_item_data(list_item_prev(list_first(queue->list)));
 }
 
+void queue_remove(Queue* queue)
+{
+    if (!queue_empty(queue))
+    {
+        list_erase(queue->list, list_item_prev(list_first(queue->list)));
+        queue->tail--;
+    }
+}
+
+bool queue_empty(const Queue* queue)
+{
+    return queue->tail == 0;
+}
 void queue_remove(Queue* queue)
 {
     if (!queue_empty(queue))
