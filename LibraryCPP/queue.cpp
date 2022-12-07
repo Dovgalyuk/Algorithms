7c0adde8e5c5b139c90ab5b0512e4d52e63674ac
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-07 13:36:29 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -7,7 +7,6 @@ struct Queue
     List* list;
     size_t tail;
 };
-
 Queue* queue_create()
 {
     Queue* queue = new Queue;
@@ -15,34 +14,29 @@ Queue* queue_create()
     queue->tail = 0;
     return queue;
 }
-
 void queue_delete(Queue* queue)
 {
     list_delete(queue->list);
     queue->tail = NULL;
     delete queue;
 }
-
 void queue_insert(Queue* queue, Data data)
 {
     list_insert(queue->list, data);
     queue->tail++;
 }
-
 Data queue_get(const Queue* queue)
 {
-    return list_item_data(list_last(queue->list));
+    return list_item_data(list_item_prev(list_first((queue->list))));
 }
-
 void queue_remove(Queue* queue)
 {
     if (!queue_empty(queue))
     {
-        list_erase(queue->list, list_last(queue->list));
+        list_erase(queue->list, list_item_prev(list_first((queue->list))));
         queue->tail--;
     }
 }
-
 bool queue_empty(const Queue* queue)
 {
     return queue->tail == 0;
