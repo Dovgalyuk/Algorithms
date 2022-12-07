96df217300256eeeb27d3fc2e2b452905c98fece
pol0praktika <95435519+pol0praktika@users.noreply.github.com>
2022-12-08 02:02:46 +0300

    Add files via upload

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -1,61 +1,152 @@
 #include <cstddef>
+#include<iostream>
 #include "list.h"
 
-struct ListItem
+struct ListItem//Элемент списка
 {
+    Data data;//данные
+    ListItem* next, * prev;//следующий и предыдущий элемент
+
+    ListItem(Data data = NULL, ListItem* next = nullptr, ListItem* prev = nullptr) {
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+
+    }
 };
 
-struct List
+struct List//Список
 {
+    ListItem* Head, * Tail;//"Голова" и "Хвост"
+
+    List() {
+        this->Head = nullptr;
+        this->Tail = nullptr;
+    }
 };
 
-List *list_create()
+List* list_create()//Создать список
 {
     return new List;
 }
 
-void list_delete(List *list)
+void list_delete(List* list)//Удалить список
 {
     // TODO: free items
     delete list;
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)//Возвращает первый элемент из списка
 {
-    return NULL;
+    return list->Head;
 }
 
-Data list_item_data(const ListItem *item)
+Data list_item_data(const ListItem* item)//Возвращает данные из элемента списка
 {
-    return (Data)0;
+    if (item != nullptr) return item->data;
+    else return NULL;
 }
 
-ListItem *list_item_next(ListItem *item)
+ListItem* list_item_next(ListItem* item)// Возвращает элемент списка, следующий за указанным
 {
-    return NULL;
+    return item->next;
 }
 
-ListItem *list_item_prev(ListItem *item)
+ListItem* list_item_prev(ListItem* item)// Возвращает предыдущий элемент для указанного элемента.
 {
-    return NULL;
+    return item->prev;
 }
 
-ListItem *list_insert(List *list, Data data)
+ListItem* list_insert(List* list, Data data)// Вставляет новый элемент списка в начало
 {
-    return NULL;
+    if (list->Head == nullptr) {//Если список пустой, то в список добавляем элемент,который будет и головой и хвостом
+        return list->Head = list->Tail = new ListItem(data);
+    }
+    else {
+        ListItem* elem = new ListItem(data, list->Head);//создаем элемент вначале списка
+        list->Head->prev = elem;//Передаем голове указатель на предыдущий элемент, который создали
+        list->Head = elem;//Задаем созданный элемент, как голову
+        return list->Head;//Возвращаем первый элемент списка
+    }
 }
 
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
+ListItem* list_insert_after(List* list, ListItem* item, Data data)// Вставляет новый элемент списка после указанного элемента
 {
-    return NULL;
+    if (list->Head == nullptr) {//Если список пуст
+        std::cout << "error";
+    }
+    else {
+        ListItem* elem = new ListItem(data, item->next, item);
+        item->next = elem;
+
+        if (item != list->Tail) {
+            elem->next->prev = elem;
+        }
+        else {
+            list->Tail = elem;
+        }
+        return elem;
+    }
+    
 }
 
-ListItem *list_erase(List *list, ListItem *item)
+
+ListItem* list_erase(List* list, ListItem* item)
+// Удаляет указанный элемент списка. Возвращает следующий элемент
 {
-    return NULL;
+    if (list->Head == nullptr)//Если список пуст
+    {
+        std::cout << "Список пуст";
+    }
+
+    else if (list->Head == item && list->Tail == item)//если удаляемый элемент - это и голова и хвост
+    {
+        delete item;
+        return list->Head = list->Tail = nullptr;
+    }
+
+    else if (list->Head == item) //если удаляемый элемент - это голова списка
+    {
+        list->Head = item->next;
+        list->Head->prev = nullptr;
+        delete item;
+        return list->Head;
+    }
+
+    else if (list->Tail == item)//если удаляемый элемент - это хвост списка
+    {
+        list->Tail = item->prev;
+        list->Tail->next = nullptr;
+        delete item;
+        return list->Tail;
+    }
+        
+    else 
+    {
+        ListItem* elem = item->next;
+        item->prev->next = elem;
+        elem->prev = item->prev;
+        delete item;
+        return elem;
+    }
 }
 
-ListItem *list_erase_next(List *list, ListItem *item)
+
+ListItem* list_erase_next(List* list, ListItem* item)// Удаляет элемент списка, следующий за указанным.
+// Возвращает указатель на элемент рядом с удаленным.
 {
-    return NULL;
+
+    if (list->Tail == item) //если элемент равен хвосту,то следующий элемент пустой
+    {
+        std::cout << "Ошибка. Следующий элемент пустой";
+    }
+
+    else if (list->Head == nullptr)//Если список пуст
+    {
+        std::cout << "Ошибка.Список пуст";
+    }
+   
+    else {
+        return list_erase(list, item->next);
+    }
 }
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,47 @@
 #include "stack.h"
+#include "list.h"
 
 struct Stack
 {
+    List* list;
+
+    Stack() {
+        this->list = list_create();
+    }
+
+    ~Stack() {
+        list_delete(this->list);
+    }
 };
 
-Stack *stack_create()
+Stack* stack_create()
 {
     return new Stack;
 }
 
-void stack_delete(Stack *stack)
+void stack_delete(Stack* stack)
 {
     // TODO: free stack elements
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
+void stack_push(Stack* stack, Data data)//Записываем данные в начало стека
 {
+    list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack)
+Data stack_get(const Stack* stack)//Получает данные из последнего элемента стека
 {
-    return (Data)0;
+    return list_item_data(list_first(stack->list));
 }
 
-void stack_pop(Stack *stack)
+void stack_pop(Stack* stack) 
 {
+    list_erase(stack->list, list_first(stack->list));
 }
 
-bool stack_empty(const Stack *stack)
+bool stack_empty(const Stack* stack)//Если стек пуст, возвращает true
 {
-    return true;
+    if (list_first(stack->list) == nullptr) return true;
+    else return false;
 }
