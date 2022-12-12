3c239c22f615a1718e1098eabba0f421b78753ec
RickCodeProg <98888624+RickCodeProg@users.noreply.github.com>
2022-12-12 03:40:31 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,48 @@
 #include "stack.h"
+#include "list.h"
+#include <iostream>
 
 struct Stack
 {
+    List* stackList;
 };
 
 Stack *stack_create()
 {
-    return new Stack;
+    Stack* stack = new Stack;
+    stack->stackList = list_create();
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    list_delete(stack->stackList);
     delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    list_insert(stack->stackList, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    return list_item_data(list_first(stack->stackList));
 }
 
 void stack_pop(Stack *stack)
 {
+    if (stack_empty(stack))
+    {
+        std::cout << "Error, empty stack";
+    }
+    else
+    {
+        list_erase_top(stack->stackList);
+    }
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return list_first(stack->stackList) == nullptr;
 }
