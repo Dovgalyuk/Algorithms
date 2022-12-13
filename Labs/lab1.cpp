c4a95fc972e073e0626e3707491795736a3a9302
shkshnk <107126933+shkshnk@users.noreply.github.com>
2022-12-13 06:11:10 +0300

    Add files via upload

--- Labs/lab1.cpp
+++ Labs/lab1.cpp
@@ -3,29 +3,43 @@
 
 using namespace std;
 
+int get_size() {
+	int size;
+	cout << "input size: ";
+	cin >> size;
+	return size;
+}
+void show_array(string message, Array* arr, int size) {
+	cout << message;
+	for (size_t i = 0; i < size; i++) { cout << array_get(arr, i) << " "; }
+	cout << "\n";
+}
+void input(Array* arr, int size) {
+	for (size_t i = 0; i < size; i++) { array_set(arr, i, rand() % 100); }
+}
+void algorithm(Array* arr, int size) {
+	for (size_t i = 0; i < size / 2; i++) {
+		int value = array_get(arr, size - i - 1);
+		array_set(arr, size - i - 1, array_get(arr, i));
+		array_set(arr, i, value);
+	}
+}
+void run() {
+	int size = get_size();
+	Array* first_arr = array_create(size);
+	input(first_arr, size);
+	show_array("first array: ", first_arr, size);
+	algorithm(first_arr, size);
+	show_array("second array: ", first_arr, size);
+	cout << "\n\n";
+	array_delete(first_arr);
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-
-	cout << "input size: ";
-	int size;
-	cin >> size;
-
-	Array* first_arr = array_create(size);
-	Array* second_arr = array_create(size);
-
-	for (size_t i = 0; i < size; i++) { array_set(first_arr, i, rand() % 100); }
-
-	cout << "first array: ";
-	for (size_t i = 0; i < size; i++) { cout << array_get(first_arr, i) << " "; }
-
-	for (size_t i = 0; i < size; i++) array_set(second_arr, size - i - 1, array_get(first_arr, i));
-
-	cout << "\nsecond array: ";
-
-	for (size_t i = 0; i < size; i++) { cout << array_get(second_arr, i) << " "; }
-
-	cout << "\n\n";
+	
+	run();
 
 	return 0;
 }
