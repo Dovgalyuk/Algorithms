42d06819db8f935fb86ab3113ced810d20a0b561
IPOMPONI <93474657+IPOMPONI@users.noreply.github.com>
2022-12-13 16:14:13 +0300

    Update 6Task.cpp

--- Lab6/6Task.cpp
+++ Lab6/6Task.cpp
@@ -11,18 +11,24 @@ typedef milli Ratio;
 void Comparison(int length)
 {
 	auto* BinH = new Heap <pair<int, int>>;
-	
+	const int INF = 1000000000;
 	int n = length;
 	vector < vector < pair<int, int> > > g(n);
+	for (int i = 1; i < length; i++)           // наполняем 
+	{
+		int j = i-1 , w = j+6/2;
+		g[i].push_back(make_pair(j, w));
+		g[j].push_back(make_pair(i, w));
+	}
 	int s = 1;                               
-	vector<int> d(n, length), p(n);
+	vector<int> d(n, INF), p(n);
 	d[s] = 0;
 
 	auto start = chrono::system_clock::now();
 	priority_queue < pair<int, int> > q;
 	q.push(make_pair(0, s));
 	while (!q.empty()) {
-		int v = q.top().second, cur_d = q.top().first;
+		int v = q.top().second, cur_d = -q.top().first;
 		q.pop();
 		if (cur_d > d[v])  continue;
 
@@ -41,24 +47,39 @@ void Comparison(int length)
 	auto end = chrono::system_clock::now();
 	chrono::duration<double, Ratio> Queue = end - start;
 
+
+
+
+	int N = length;
+	vector < vector < pair<int, int> > > G(N);
+	for (int I = 1; I < length; I++)          // наполняем 
+	{
+		int J = I - 1, W = J + 6 / 2;
+		G[I].push_back(make_pair(J, W));
+		G[J].push_back(make_pair(I, W));
+	}
+	int S = 1;
+	vector<int> D(N, INF), P(N);
+	D[S] = 0;
+
 	start = chrono::system_clock::now();
-	BinH->Insert(make_pair(0, s));
+	BinH->Insert(make_pair(0, S));
 
 	while (!BinH->Empty()) 
 	{
-		int v = BinH->GetMax().second, cur_d = BinH->GetMax().first;
+		int V = BinH->GetMax().second, Cur_d = -BinH->GetMax().first;
 		BinH->RemoveMax();
-		if (cur_d > d[v])  continue;
+		if (Cur_d > D[V])  continue;
 
-		for (size_t j = 0; j < g[v].size(); ++j) 
+		for (size_t j = 0; j < G[V].size(); ++j) 
 		{
-			int to = g[v][j].first,
-				len = g[v][j].second;
-			if (d[v] + len < d[to]) 
+			int To = G[V][j].first,
+				Len = G[V][j].second;
+			if (D[V] + Len < D[To]) 
 			{
-				d[to] = d[v] + len;
-				p[to] = v;
-				BinH->Insert(make_pair(-d[to], to));
+				D[To] = D[V] + Len;
+				P[To] = V;
+				BinH->Insert(make_pair(-D[To], To));
 			}
 		}
 	}
@@ -69,9 +90,8 @@ void Comparison(int length)
 	StrQueue.replace(StrQueue.find('.'), 1, 1, ',');       // Заменяю '.' на ',' чтобы удобно было в Excel вставлять
 	string StrHeap = to_string(Heap.count());
 	StrHeap.replace(StrHeap.find('.'), 1, 1, ',');
-
-	cout << "\t" << length << "   | " << StrHeap << "\t\t| " << StrQueue << "\n";
 	delete BinH;
+	cout << "\t" << length << "   | " << StrHeap << "\t\t| " << StrQueue << "\n";
 }
 
 
@@ -86,6 +106,5 @@ int main()
 	{
 		Comparison(i);
 	}
+
 }
-
-
