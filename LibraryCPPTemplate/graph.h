1e7225e01c8a56fa4ff252a1e8f2cc6d83a2c896
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-13 19:23:34 +0300

    Add files via upload

---
+++ LibraryCPPTemplate/graph.h
@@ -0,0 +1,200 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include "list.h"
+#include <vector>
+
+template<typename Data, typename Weight>
+class Graph {
+public:
+    struct Edge; //ребро
+    struct Vertex { //вершина
+        Data data;
+        List<Edge*> edges = List<Edge*>();
+            
+        Vertex(Data data) {
+            this->data = data;
+        }
+
+        ~Vertex() {
+            auto itemDel = edges.list_first();
+
+            while(itemDel != nullptr) {
+                delete itemDel->data();
+                itemDel = itemDel->item_next();
+            }
+        }
+
+        void setData(Data data) {
+            this->data = data;
+        }
+
+        Data getData() {
+            return data;
+        }
+
+        void addEdge(Vertex* toVertex) {  // добавить ребро
+            edges.insert(new Edge(toVertex));
+        }
+
+        Edge* getEdge(Vertex* toVertex) {
+            for (auto item = edges.list_first(); item != nullptr; item = item->item_next()) {
+                if (item->data()->getToVertex() == toVertex) {
+                    return item->data();
+                }
+            }
+
+            return nullptr;
+        }
+
+        void removeEdge(Vertex* toVertex) { // удалить ребро
+            for (auto item = edges.list_first(); item != nullptr; item = item->item_next()) {
+                if (item->data()->getToVertex() == toVertex) {
+                    delete item->data();
+                    edges.erase(item);
+                    break;
+                }
+            }
+        }
+
+    };
+
+    struct Edge {
+        Vertex* toVertex;
+        Weight weight;
+
+        Edge(Vertex* toVertex) {
+            this->toVertex = toVertex;
+        }
+
+        Vertex* getToVertex() {
+            return toVertex;
+        }
+
+        void setWeight(int weight) {
+            this->weight = weight;
+        }
+
+        int getWeight() {
+            return weight;
+        }
+    };
+
+    struct EdgeIterator { //итератор
+
+        typename List<Edge*>::Item *edgeItem;
+
+        EdgeIterator(Vertex* head) {
+            edgeItem = head->edges.list_first();
+        }
+
+        void operator ++() {
+            if (edgeItem != nullptr) {
+                edgeItem = edgeItem->item_next();
+            }
+        }
+
+        Edge* operator *() {
+            if (edgeItem != nullptr) {
+                return edgeItem->data();
+            } else {
+                return nullptr;
+            }
+        }
+    };
+
+    Graph(size_t numberVertex, Data data) { //граф
+        vertexes.resize(numberVertex);
+        for (size_t i = 0; i < numberVertex; i++) {
+            vertexes[i] = new Vertex(data);
+        }
+    }
+
+    ~Graph() {
+        for (int i = 0; i < vertexes.size(); i++) {
+            delete vertexes[i];
+        }
+    }
+
+    size_t sizeVertexes() {
+        return vertexes.size();
+    }
+
+    bool checkEdge(Vertex* fromVertex, Vertex* toVertex) {
+        for (auto item = fromVertex->edges.list_first(); item != nullptr; item = item->item_next()) {
+            if (item->data()->getToVertex() == toVertex) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    Vertex* addVertex(Data data) { // добавить вершину
+        auto vertex = new Vertex(data);
+        vertexes.push_back(vertex);
+        return vertex;
+    }
+
+    Vertex* getVertex(size_t index) { 
+        return vertexes[index];
+    }
+
+    void removeVertex(size_t index) { //удалить вершину
+        if (index < vertexes.size()) {
+            for (int i = 0; i < vertexes.size(); i++) {
+                vertexes[i]->removeEdge(vertexes[index]);
+            }
+
+            delete vertexes[index];
+            for (int i = index + 1; i < vertexes.size(); i++) {
+                vertexes[i - 1] = vertexes[i];
+            }
+            vertexes.pop_back();
+        }
+    }
+
+    void setDataVertex(size_t index, Data data) { 
+        if (index >= vertexes.size()) return;
+        vertexes[index]->setData(data);
+    }
+
+    Data getDataVertex(size_t index) {
+        return vertexes[index]->getData();
+    }
+
+    void addEdge(Vertex* fromVertex, Vertex* toVertex, int weightEdge) {
+        if (!checkEdge(fromVertex, toVertex)) {
+            fromVertex->addEdge(toVertex);
+        }
+        fromVertex->getEdge(toVertex)->setWeight(weightEdge);
+    }
+
+    Edge* getEdge (Vertex* fromVertex, Vertex* toVertex) {
+        return fromVertex->getEdge(toVertex);
+    }
+
+    int getWeightEdge(Vertex* fromVertex, Vertex* toVertex) {
+        return getEdge(fromVertex, toVertex)->getWeight();
+    }
+
+    void removeEdge(Vertex* fromVertex, Vertex* toVertex) {
+        fromVertex->removeEdge(toVertex);
+    }
+
+    size_t getIndexVertex(Vertex* vertex) {
+        for (size_t i = 0; i < vertexes.size(); i++) {
+            if (vertexes[i] == vertex) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+private:
+    std::vector<Vertex*> vertexes;
+};
+
+#endif
