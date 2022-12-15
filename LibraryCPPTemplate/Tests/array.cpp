898ce32bc1917827f442af354de75e877b95678f
Pavel <90069034+Riba4koff@users.noreply.github.com>
2022-12-15 20:45:32 +0300

    Update array.cpp

--- LibraryCPPTemplate/Tests/array.cpp
+++ LibraryCPPTemplate/Tests/array.cpp
@@ -18,7 +18,7 @@ int main()
 
     for (int i = 0 ; i < 10 ; ++i)
     {
-        if (arr->get(i) != i * 2)
+        if (*arr->get(i) != i * 2)
         {
             std::cout << "Invalid array element " << i << "\n";
             return 1;
