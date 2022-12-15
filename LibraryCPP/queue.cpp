92b59ed3b0d1243fe32c38918471dde5405c0155
Terit21 <117031460+Terit21@users.noreply.github.com>
2022-12-16 01:51:12 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,59 @@
 #include "queue.h"
+#include "vector.h"
 
 struct Queue
 {
+    int id;
+    Vector* vec;
+    Queue()
+    {
+        id = 0;
+        vec = vector_create();
+        vector_resize(vec, 1);
+    }
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
-    return new Queue;
+    return new Queue();
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
-    // TODO: free queue items
+    vector_delete(queue->vec);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    vector_set(queue->vec, queue->id, data);
+    queue->id++;
+    vector_resize(queue->vec, vector_size(queue->vec)+1);
+    
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-    return (Data)0;
+    if (vector_get(queue->vec, 0))
+        return vector_get(queue->vec, 0);
+    else
+        return 0;
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+    for (int i = 0; i <= queue->id; i++)
+    {
+        vector_set(queue->vec, i, vector_get(queue->vec, i + 1));
+    }
+    queue->id--;
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    return true;
+    if (queue->id == 0)
+        return true;
+    else
+        return false;
+
 }
