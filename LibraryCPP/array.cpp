392e231f64358877efc544e7df3542721f78e5b1
Killacheckface <90393938+killacheckface@users.noreply.github.com>
2022-12-16 00:34:31 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,45 @@
 #include "array.h"
 
-struct Array
-{
+struct Array {
+    Data* ArrayData;
+    size_t ArraySize;
+    Array(size_t ArraySize) {
+        this->ArraySize = ArraySize;
+        ArrayData = new int[this->ArraySize];
+    }
 };
 
 // create array
-Array *array_create(size_t size)
-{
-    return new Array;
+Array* array_create(size_t size) {
+    return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
-    delete arr;
+void array_delete(Array* arr) {
+    delete[] arr->ArrayData;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array* arr, size_t index) {
+    if (index >= array_size(arr)) {
+        return 0;
+    }
+    else {
+        return arr->ArrayData[index];
+    }
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+    if (array_size(arr) < index) {
+        return;
+    }
+    else {
+        arr->ArrayData[index] = value;
+    }
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr) {
+    return arr->ArraySize;
 }
