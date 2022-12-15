459f7027a9985cfb22a69997c3f8dfbfc1bb7aa4
Pavel <90069034+Riba4koff@users.noreply.github.com>
2022-12-15 20:36:50 +0300

    Update Algorithms_lab_5.cpp

--- Labs/Algorithms_lab_5.cpp
+++ Labs/Algorithms_lab_5.cpp
@@ -8,14 +8,16 @@ using namespace std;
 
 const char symbols[] = { "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
 
-string random_word(){
+string random_word() {
 	int size = rand() % 10 + 1;
 	string str;
 	for (int i = 0; i < size; i++) str.push_back(symbols[rand() % 62]);
 	return str;
 }
 
-void execute(HashTable *table, unordered_map<string, string> map, int size) {
+void execute(HashTable* table, int size) {
+	unordered_map<string, string> map;
+
 	chrono::system_clock::time_point start_table;
 	chrono::system_clock::time_point table_end;
 	chrono::system_clock::time_point start_map;
@@ -43,7 +45,7 @@ void execute(HashTable *table, unordered_map<string, string> map, int size) {
 	time_map = map_end - start_map;
 
 	cout << "Added " << size << " elements. My table is " << time_table.count() << ". Unordered_map is " << time_map.count() << ".\n";
-	
+
 	delete[] random_key;
 	delete[] random_value;
 }
@@ -51,16 +53,21 @@ void execute(HashTable *table, unordered_map<string, string> map, int size) {
 int main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	
-	HashTable* table = new HashTable();
-	unordered_map<string, string> map;
 
-	execute(table, map, 100);
-	execute(table, map, 500);
-	execute(table, map, 1000);
-	execute(table, map, 5000);
-	execute(table, map, 10000);
-	execute(table, map, 50000);
-	execute(table, map, 100000);
-	execute(table, map, 200000);
+	HashTable* table = new HashTable();
+
+	execute(table, 100);
+	execute(table, 500);
+	execute(table, 1000);
+	execute(table, 5000);
+	execute(table, 10000);
+	execute(table, 50000);
+	execute(table, 100000);
+	execute(table, 200000);
+	execute(table, 500000);
+	execute(table, 800000);
+
+	delete_table(table);
+
+	return 0;
 }
