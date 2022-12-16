1e058a57c534cf1287ac685ae4c8b4f8c4a2a045
Ga-123 <55543145+Ga-123@users.noreply.github.com>
2022-12-16 15:51:13 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -21,7 +21,7 @@ Array* array_create(size_t size) {
 
 // delete array, free memory
 void array_delete(Array* arr) {
-    delete[] arr->ArrData;
+    delete arr;
 }
 
 // returns specified array element
