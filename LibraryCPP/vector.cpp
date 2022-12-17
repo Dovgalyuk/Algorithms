b5f7f12063e55f0579d9e9c79f6344aefd9d14e3
TvizEt <tvizet.inc@gmail.com>
2022-12-17 18:53:48 +0300

    Vector relis

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,7 +1,21 @@
 #include "vector.h"
+#include <memory.h>
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t capacity;
+
+    Vector() {
+        this->size = 0; //Initial size
+        this->capacity = 10;//Initial capacity
+        this->data = new Data[capacity];
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -17,18 +31,42 @@ void vector_delete(Vector *vector)
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    size_t size = vector_size(vector);
+    if (index < size)
+        return vector->data[index];
+    else
+        return Data();
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    size_t size = vector_size(vector);
+    if (index < size)
+        vector->data[index] = value;
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size <= vector->capacity) {
+        vector->size = size;
+    }
+    else {
+
+        size_t new_capacity = size << 1;// grow vector size * 2
+
+        Data* new_data = new Data[new_capacity];
+
+        memcpy(new_data, vector->data, vector_size(vector) * sizeof(Data));
+
+        delete[] vector->data;
+
+        vector->size = size;
+        vector->capacity = new_capacity;
+        vector->data = new_data;
+    }
 }
