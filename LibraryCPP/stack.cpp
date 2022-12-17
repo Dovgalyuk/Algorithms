46953e5c800b9ad2a1b91a100383cb18007fe31d
TvizEt <tvizet.inc@gmail.com>
2022-12-17 18:54:07 +0300

    Stack relis

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,7 +1,17 @@
 #include "stack.h"
+#include "vector.h"
 
 struct Stack
 {
+    Vector* vector;
+
+    Stack() {
+        this->vector = vector_create();
+    }
+
+    ~Stack() {
+        vector_delete(this->vector);
+    }
 };
 
 Stack *stack_create()
@@ -11,24 +21,29 @@ Stack *stack_create()
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
     delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    size_t size = vector_size(stack->vector);
+    vector_resize(stack->vector, size + 1);
+    vector_set(stack->vector, size, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    return stack_empty(stack) ? throw "Empty stack error"
+        : vector_get(stack->vector, vector_size(stack->vector) - 1);
 }
 
 void stack_pop(Stack *stack)
 {
+    stack_empty(stack) ? throw "Empty stack error" :
+        vector_resize(stack->vector, vector_size(stack->vector) - 1);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return vector_size(stack->vector) == 0;
 }
