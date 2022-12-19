e420035dad4af83683a450c60f97379476e86da9
1BrainStorm9 <92025895+1BrainStorm9@users.noreply.github.com>
2022-12-19 19:06:12 +0300

    Add files via upload

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,12 +2,21 @@
 
 struct Array
 {
+    Data* arrData;
+    size_t sizeOfArray;
+    Array(const size_t size) {
+        sizeOfArray = size;
+        arrData = new Data[sizeOfArray];
+    }
+    ~Array() {
+        delete[] arrData;
+    }
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    return new Array(size);
 }
 
 // delete array, free memory
@@ -19,16 +28,28 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    size_t size = array_size(arr);
+    if (size > index) {
+        return arr->arrData[index];
+    }
+    else
+    {
+        return Data();
+    }
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    size_t size = array_size(arr);
+
+    if (index < size) {
+        arr->arrData[index] = value;
+    }
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->sizeOfArray;
 }
