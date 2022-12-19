f0d35b4b3daaaf2e69b1db5d1a61dfb5fa0173cf
RickCodeProg <98888624+RickCodeProg@users.noreply.github.com>
2022-12-19 19:45:36 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -10,8 +10,8 @@ struct Queue
     {
         queueVector = vector_create();
         vector_resize(queueVector, 2);
-        first = -1;
-        last = -1;
+        first = 0;
+        last = 0;
     }
 };
 
@@ -28,30 +28,41 @@ void queue_delete(Queue *queue)
 
 void queue_insert(Queue *queue, Data data)
 {
-    if (queue->last == vector_size(queue->queueVector) - 1)
+    Vector* qVector = queue->queueVector;
+    if (queue->last >= vector_size(queue->queueVector) - 1 && queue->first==0)
     {
         vector_resize(queue->queueVector, vector_size(queue->queueVector) + 1);
     }
-    if (queue->first == -1)
+    else if (queue->last + 1 == queue->first)
     {
+        Vector* tmpV = vector_create();
+        vector_resize(tmpV, vector_size(queue->queueVector) + 1);
+        size_t s = 0;
+
+        for (int i = queue->first; i < vector_size(queue->queueVector); i++)
+        {
+            vector_set(tmpV, s, vector_get(queue->queueVector, i));
+            s++;
+        }
+        for (int i = 0; i <= queue->last; i++)
+        {
+            vector_set(tmpV, s, vector_get(queue->queueVector, i));
+            s++;
+        }
+        queue->queueVector = tmpV;
+        vector_delete(qVector);
         queue->first = 0;
+        queue->last = s + 1;
     }
-    queue->last++;
     vector_set(queue->queueVector, queue->last, data);
-    /*if (queue_empty(queue))
+    if (queue->last == vector_size(queue->queueVector) - 1)
     {
-        queue->first = 0;
         queue->last = 0;
     }
     else
     {
         queue->last++;
-        if (queue->last == vector_size(queue->queueVector) - 1)
-        {
-            vector_resize(queue->queueVector, vector_size(queue->queueVector) + 1);
-        }
     }
-    vector_set(queue->queueVector, queue->last, data);*/
 }
 
 Data queue_get(const Queue *queue)
@@ -63,10 +74,9 @@ void queue_remove(Queue *queue)
 {
     if (!queue_empty(queue))
     {
-        if (queue->first >= queue->last)
+        if (queue->first >= vector_size(queue->queueVector) - 1)
         {
-            queue->first = -1;
-            queue->last = -1;
+            queue->first = 0;
         }
         else
         {
@@ -77,7 +87,7 @@ void queue_remove(Queue *queue)
 
 bool queue_empty(const Queue *queue)
 {
-    if (queue->last == -1 && queue->first == -1)
+    if (queue->last == queue->first)
     {
         return true;
     }
