43d220d25a7c32eb9b3ffbba2a37a1694d949690
Taushicki <90393938+Taushicki@users.noreply.github.com>
2022-12-20 21:24:52 +0300

    Add files via upload

---
+++ Labs/LabLaboratornaya2.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<string>
+#include"stack.h";
+
+using namespace std;
+
+bool StringValidation(Stack* stack, string str) {
+	int count34 = 0;
+	int count39 = 0;
+	int count40 = 0;
+	int count41 = 0;
+	int counter = 0;
+	for (char c : str) {
+		switch (c) {
+		case '(': stack_push(stack, '('); break;
+		case ')': stack_push(stack, ')'); break;
+		case '\'': stack_push(stack, '\''); break;
+		case '"': stack_push(stack, '"'); break;
+		default: break;
+		}
+	}
+
+	while (!stack_empty(stack)) {
+		switch (stack_get(stack)) {
+		case '"': count34++; stack_pop(stack); break;
+		case '\'': count39++; stack_pop(stack); break;
+		case '(': count40++; stack_pop(stack); break;
+		case ')': count41++; stack_pop(stack); break;
+		default:
+			break;
+		}
+	}
+	if ((count34 != 0 && count34 % 2 == 0) && (count39 != 0 && count39 % 2 == 0) && count41 == count40) return true;
+	else return false;
+}
+
+void main() {
+	string inputString;
+	getline(cin, inputString);
+	Stack* stack = stack_create();
+	if (StringValidation(stack, inputString))
+		cout << "Yes" << endl;
+	else cout << "NO" << endl;
+	stack_delete(stack);
+}
