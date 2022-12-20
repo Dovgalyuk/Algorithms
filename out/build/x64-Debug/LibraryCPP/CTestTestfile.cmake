074bc443626517f0a1fd34ab1d22b32c1ab3bac0
TihonYakovlev <96542203+TihonYakovlev@users.noreply.github.com>
2022-12-20 15:49:30 +0300

    initial commit

---
+++ out/build/x64-Debug/LibraryCPP/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Acer/source/repos/Algorithms/LibraryCPP
+# Build directory: C:/Users/Acer/source/repos/Algorithms/out/build/x64-Debug/LibraryCPP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
