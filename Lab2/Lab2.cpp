0e6c17892d764d6a6d82d4dea4e70c470ecc9b78
Ti1234 <120597609+Ti1234@users.noreply.github.com>
2022-12-20 17:50:44 +0300

    Update Lab2.cpp

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,1 +1,37 @@
+#include "stack.h"
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{
+    int particles, cord;
+    Stack* stack = stack_create();
+    char charge;
+    //Открываем файл для чтения
+    ifstream fin("input.txt");
+    //Вводим количество частиц
+    fin >> particles;
+    for (int i = 0; i < particles; i++)
+    {
+        //Вводим координату частицы
+        fin >> cord;
+        //Вводим заряд
+        fin >> charge;
+        //Если заряд положительный, помещаем в стэк
+        if (charge == '+')
+        {
+            stack_push(stack, cord);
+        }
+        //Иначе, если заряд отрицательный и стэк не пустой, выводим элемент стэка и тот, который ввели только что
+        else if (!stack_empty(stack))
+        {
+            cout << stack_get(stack) << " " << cord << endl;
+        }
+    }
+    //Закрываем файл для чтения
+    fin.close();
+    //Удаляем стэк
+    stack_delete(stack);
+}
