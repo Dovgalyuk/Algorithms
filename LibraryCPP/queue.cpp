d841bc397bfa49835fa07338571711751cd7ef8d
Terit21 <117031460+Terit21@users.noreply.github.com>
2022-12-21 23:54:41 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -40,21 +40,14 @@ void queue_insert(Queue* queue, Data data)
     else if (queue->id_r == queue->id_l)
     {
         queue->id_r--;
-        vector_resize(queue->vec, vector_size(queue->vec) * 2);
+        vector_resize(queue->vec, vector_size(queue->vec) + queue->id_l);
         for (int i = 0; i < queue->id_l; i++)
         {
             vector_set(queue->vec, vector_size(queue->vec) - 1 - i, vector_get(queue->vec, queue->id_r));
             vector_set(queue->vec, queue->id_r, 0);
             queue->id_r--;
         }
-        for (int i = 0; i < queue->el_amount - queue->id_l; i++)
-        {
-            vector_set(queue->vec, queue->el_amount + i, vector_get(queue->vec, queue->id_l));
-            vector_set(queue->vec, queue->id_l, 0);
-            queue->id_l++;
-
-        }
-        queue->id_r = vector_size(queue->vec) - 2;
+        queue->id_r ++;
     }
     vector_set(queue->vec, queue->id_r, data);
     queue->id_r++;
