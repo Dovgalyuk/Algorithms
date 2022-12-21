dc714bdf26aa0462fff6513bb2c987732755a9dc
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-22 00:21:42 +0300

    Add files via upload

---
+++ Lab5/lb5.cpp
@@ -0,0 +1,15 @@
+﻿#include "associative_array.h"
+#include <iostream>
+
+int main()
+{
+	string one = "Ivan";
+	string second = "Filippov";
+	AssociativeArray lol(one, second);
+	lol.add(second, one);
+	lol.add("PEPA", "FAFA");
+	lol.add("PEPAf", "FAFAq");
+	lol.remove("PEPA");
+	string itog = lol.search("PEPAf");
+	cout << itog;
+}
