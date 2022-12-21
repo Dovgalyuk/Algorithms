2648bd3a60354608e6a4adcb2753149d51e1c964
RickCodeProg <98888624+RickCodeProg@users.noreply.github.com>
2022-12-21 18:04:35 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -9,90 +9,84 @@ struct Queue
     Queue()
     {
         queueVector = vector_create();
-        vector_resize(queueVector, 4);
-        first = 0;
-        last = 0;
+        vector_resize(queueVector, 2);
+        first = -1;
+        last = -1;
     }
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
     return new Queue;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
     vector_delete(queue->queueVector);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
-    Vector* qVector = queue->queueVector;
-    if (queue->last >= vector_size(queue->queueVector) - 1 && queue->first==0)
+    size_t size = vector_size(queue->queueVector);
+    if (queue_empty(queue))
     {
-        vector_resize(queue->queueVector, vector_size(queue->queueVector) + 1);
-    }
-    else if (queue->last + 1 == queue->first)
-    {
-        Vector* tmpV = vector_create();
-        vector_resize(tmpV, vector_size(queue->queueVector) + 1);
-        size_t s = 0;
-
-        for (int i = queue->first; i < vector_size(queue->queueVector); i++)
-        {
-            vector_set(tmpV, s, vector_get(queue->queueVector, i));
-            s++;
-        }
-        for (int i = 0; i <= queue->last; i++)
-        {
-            vector_set(tmpV, s, vector_get(queue->queueVector, i));
-            s++;
-        }
-        queue->queueVector = tmpV;
-        vector_delete(qVector);
         queue->first = 0;
-        queue->last = s + 1;
-    }
-    vector_set(queue->queueVector, queue->last, data);
-    if (queue->last == vector_size(queue->queueVector) - 1)
-    {
         queue->last = 0;
     }
-    else
+    else if (queue->last % size == queue->first)
     {
-        queue->last++;
+        Vector* tmpV = vector_create();
+        vector_resize(tmpV, size * 2);
+        size_t s = 0;
+
+        for (int i = queue->first; i < size; i++)
+        {
+            vector_set(tmpV, s, vector_get(queue->queueVector, i));
+            s++;
+        }
+        for (int i = 0; i < queue->last; i++)
+        {
+            vector_set(tmpV, s, vector_get(queue->queueVector, i));
+            s++;
+        }
+        vector_delete(queue->queueVector);
+        queue->queueVector = tmpV;
+        queue->first = 0;
+        queue->last = size;
+        size = vector_size(queue->queueVector);
     }
+    int l = queue->last % size;
+    vector_set(queue->queueVector, l, data);
+    queue->last = l + 1;
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
     return vector_get(queue->queueVector, queue->first);
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+    size_t size = vector_size(queue->queueVector);
+    int f = queue->first;
+
     if (!queue_empty(queue))
     {
-        if (queue->first >= vector_size(queue->queueVector) - 1)
+        f++;
+        if (queue->last == f)
         {
-            queue->first = 0;
+            queue->first = -1;
+            queue->last = -1;
         }
         else
         {
-            queue->first++;
+            queue->first = f % size;
         }
     }
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    if (queue->last == queue->first)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return (queue->last == -1 && queue->first == -1);
 }
