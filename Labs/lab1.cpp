e81e686179e71c1a5ec91965c28145cffeeceb84
TihonYakovlev <96542203+TihonYakovlev@users.noreply.github.com>
2022-12-23 01:02:17 +0300

    Update lab1.cpp

--- Labs/lab1.cpp
+++ Labs/lab1.cpp
@@ -1,52 +1,39 @@
-#include "array.h"
 #include <iostream>
 #include <ctime>
-
 using namespace std;
 
-void fill_array(Array* arr) {
-    for (int i = 0; i < array_size(arr); i++) {
-        array_set(arr, i, rand() % 100 + 1);
-    }
-}
+int main()
+{
 
-void sort_array(Array* arr) {
-    for (int i = array_size(arr) - 1; i > 0; --i) {
-        for (int j = 0; j < i; j++) {
-            Data v1 = array_get(arr, j);
-            Data v2 = array_get(arr, j + 1);
-            if (v1 > v2) {
-                array_set(arr, j, v2);
-                array_set(arr, j + 1, v1);
-            }
-        }
-    }
-}
+	int arr_size, *arr, counter = 0, num1 = 0, num2;
 
-int main() {
-    srand(time(0));
-    int size;
-    cin >> size;
-    Array* arr = array_create(size);
-    fill_array(arr);
-    sort_array(arr);
-    int n1, n2, last;
-    for (int i = 0; i < array_size(arr); i++) {
-        Data v = array_get(arr, i);
-        if (v % 2 == 0) {
-            n1 = v;
-            last = i;
-            break;
-        }
-    }
-    for (int i = last + 1; i < array_size(arr); i++) {
-        Data v = array_get(arr, i);
-        if (v % 2 == 0) {
-            n2 = v;
-            break;
-        }
-    }
-    cout << n2 - n1;
-    system("pause");
-    array_delete(arr);
+	cout << "Enter array size: ";
+	cin >> arr_size;
+	arr = new int[arr_size];
+
+	srand(time(NULL));
+	cout << "Array: ";
+	for (int i = 0; i < arr_size; i++) {
+		arr[i] = 1 + rand() % 10;
+		cout << arr[i] << " ";
+	}
+
+	cout << endl << "Element repeated 2 times: ";
+
+	for (int i = 0; i < arr_size; i++, counter = 0)
+	{
+		for (int j = 0; j < arr_size; j++)
+			if (arr[i] == arr[j])
+				counter++;
+		if (counter == 2)
+		{
+			num2 = num1;
+			num1 ^= arr[i];
+			if (num2 > num1)
+				cout<<arr[i]<<" ";
+		}
+
+	}
+	delete[] arr;
+	return 0;
 }
