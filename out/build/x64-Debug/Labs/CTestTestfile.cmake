9b496760ecbb9174a049f584351f8e41c8d4c613
MardanovAziz <114675415+MardanovAziz@users.noreply.github.com>
2022-12-22 14:05:50 +0300

    delete out

--- out/build/x64-Debug/Labs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/source/repos/Algorithms/Labs
-# Build directory: C:/Users/User/source/repos/Algorithms/out/build/x64-Debug/Labs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
