a246bf963021d0e6dce9b2091c458f67f209e077
Taushicki <90393938+Taushicki@users.noreply.github.com>
2022-12-22 19:38:44 +0300

    Update list.cpp

--- LibraryCPP/Tests/list.cpp
+++ LibraryCPP/Tests/list.cpp
@@ -28,7 +28,7 @@ int main()
         std::cout << "list_insert_after error\n";
         return 1;
     }
-    list_erase_first(List* list);
+    list_erase_first(list);
 
     if (list_item_data(list_first(list)) != 4)
     {
