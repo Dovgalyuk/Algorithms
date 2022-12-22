b0b2930600d966b4b2ce9cb11d62b2f19dcadc75
VkuhVkuhVkuh <113208222+VkuhVkuhVkuh@users.noreply.github.com>
2022-12-23 01:38:58 +0300

    Add files via upload

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,54 @@
 
 struct Array
 {
+    Data* ArrayData;
+    size_t ArraySize;
+    Array(size_t ArraySize)
+    {
+        this->ArraySize = ArraySize;
+        ArrayData = new int[this->ArraySize];
+    }
+    ~Array() {
+        delete[] ArrayData;
+    }
 };
 
-// create array
-Array *array_create(size_t size)
-{
-    return new Array;
-}
+    // create array
+    Array* array_create(size_t size)
+    {
+        return new Array(size);
+    }
 
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    delete arr;
-}
+    // delete array, free memory
+    void array_delete(Array* arr)
+    {
+        delete arr;
+    }
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
-}
+    // returns specified array element
+    Data array_get(const Array* arr, size_t index)
+    {
+        if (index >= array_size(arr)) {
+            return 0;
+        }
+        else {
+            return arr->ArrayData[index];
+        }
+    }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
-}
+    // sets the specified array element to the value
+    void array_set(Array* arr, size_t index, Data value)
+    {
+        if (array_size(arr) < index) {
+            return;
+        }
+        else {
+            arr->ArrayData[index] = value;
+        }
+    }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
-}
+    // returns array size
+    size_t array_size(const Array* arr)
+    {
+        return arr->ArraySize; 
+    }
