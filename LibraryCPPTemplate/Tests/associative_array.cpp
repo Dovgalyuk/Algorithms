0dd498ddef7876c7943de723e73dbb97515c4003
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-22 19:31:03 +0300

    Add files via upload

---
+++ LibraryCPPTemplate/Tests/associative_array.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <ctime>
+#include "associative_array.h"
+using namespace std;
+
+int main()
+{
+    AssociativeArray array(to_string(0), to_string(0));
+    int count = 1000000;
+    for (int i = 0; i < count; ++i)
+    {
+        string Str = to_string(i);
+        array.add(Str, Str);
+    }
+    for (int i = 0; i < count; ++i)
+    {
+        string Str = to_string(i);
+        auto P = array.search(Str);
+        if (P != Str)
+        {
+            cout << "Data storage error " << i << "\n\n";
+            return 1;
+        }
+    } 
+}
