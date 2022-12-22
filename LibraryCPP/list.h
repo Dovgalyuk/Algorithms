08f7cd175ba97670f8d9e651ce5e44a1115cf7fa
Taushicki <90393938+Taushicki@users.noreply.github.com>
2022-12-22 19:35:06 +0300

    Update list.h

--- LibraryCPP/list.h
+++ LibraryCPP/list.h
@@ -38,7 +38,7 @@ ListItem* list_insert_after(List* list, ListItem* item, Data data);
 // Returns pointer to the item next to the deleted one.
 // Not applicable for the singly linked lists.
 // Should be O(1)
-
+ListItem* list_erase(List* list, ListItem* item);
 ListItem* list_erase_first(List* list);
 // Deletes the list item following the specified one.
 // Returns pointer to the item next to the deleted one.
