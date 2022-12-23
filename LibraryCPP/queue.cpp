df39f910396eaeae7671519fa45633379e457d81
pol0praktika <95435519+pol0praktika@users.noreply.github.com>
2022-12-23 09:29:01 +0300

    Add files via upload

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,7 +1,26 @@
 #include "queue.h"
+#include "vector.h"
+#include <iostream>
+
+using namespace std;
 
 struct Queue
 {
+    Vector* vector;
+
+    size_t head;
+    size_t tail;
+
+    Queue() {
+        this->vector = vector_create();
+        this->head = NULL;
+        this->tail = NULL;
+    }
+
+    ~Queue() {
+        vector_delete(this->vector);
+    }
+
 };
 
 Queue *queue_create()
@@ -15,20 +34,72 @@ void queue_delete(Queue *queue)
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue *queue, Data data)//Добавление нового элемента в очереди
 {
+    if (queue_empty(queue)) { 
+        vector_resize(queue->vector, 20);
+        vector_set(queue->vector, queue->head, data);
+        queue->tail++;
+    }
+    else if (queue->tail == vector_size(queue->vector) - 1 && queue->head == 0) {
+        vector_resize(queue->vector, vector_size(queue->vector) * 2);
+        vector_set(queue->vector, queue->tail, data);
+        queue->tail++;
+    }
+    else if (queue->tail + 1 == queue->head) {
+        Vector* temp = vector_create();
+        vector_resize(temp, vector_size(queue->vector)*2);
+
+        int temp_index = 0;
+
+        for (size_t i = queue->head; i < vector_size(queue->vector); i++) 
+        {
+            vector_set(temp, temp_index, vector_get(queue->vector, i));
+            temp_index++;
+        }
+
+        for (size_t i = 0; i < queue->tail; i++)
+        {
+            vector_set(temp, temp_index, vector_get(queue->vector, i));
+            temp_index++;
+        }
+
+        vector_delete(queue->vector);
+        queue->vector = temp;
+
+        queue->head = 0;
+        queue->tail = temp_index;
+    }
+    else if (queue->tail == vector_size(queue->vector) - 1 && queue->head >= 0) {
+        vector_set(queue->vector, queue->tail, data);
+        queue->tail = 0;
+    }
+    else {
+        vector_set(queue->vector, queue->tail, data);
+        queue->tail++;
+    }
 }
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    return vector_get(queue->vector,queue->head);
 }
 
 void queue_remove(Queue *queue)
 {
+    if (queue_empty(queue)) cout<<"Очередь пуста";
+    else if (queue->head == vector_size(queue->vector) - 1) {
+        vector_set(queue->vector, queue->head, 0);
+        queue->head = 0;
+    }
+    else {
+        vector_set(queue->vector, queue->head, 0);
+        queue->head++;
+    }
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    if (queue->head == queue->tail) return true;
+    else return false;
 }
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,6 +2,19 @@
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t capacity;
+
+    Vector() {
+        this->size = 0;
+        this->capacity = 10;
+        this->data = new Data[capacity]{ NULL };
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -12,23 +25,50 @@ Vector *vector_create()
 void vector_delete(Vector *vector)
 {
     // TODO: free vector internals
+    
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    size_t size = vector_size(vector);
+    
+    if (index < size) {
+        return vector->data[index];
+    }
+
+    else return false;
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    size_t size = vector_size(vector);
+    if (index < size) {
+        vector->data[index] = value;
+    }
+  
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (vector->capacity > size) vector->size = size;
+    else {
+        size_t new_capacity = size * 2;
+        Data* new_data = new Data[new_capacity]{NULL};
+
+        //Из сторого вектора записываем занчени в новый
+        for (int i = 0; i < vector->size; i++) new_data[i] = vector->data[i]; 
+
+        delete[] vector->data;
+
+        vector->capacity = new_capacity;
+        vector->size = size;
+        vector->data = new_data;
+    }
+
 }
