a388b8df6c9b742c0fb62a31289aaa4952d32ca1
Ti1234 <120597609+Ti1234@users.noreply.github.com>
2022-12-23 17:23:58 +0300

    Update list.cpp

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -1,55 +1,135 @@
-#include <iostream>
+//Односвязный список устроен так, что каждый элемент указывает на следующий
+
+#include <cstddef>
 #include "list.h"
 
-int main()
+//Структура описывает элемент односвязного списка
+struct ListItem
 {
-List *list = list_create();
+    //Указатель на следующий элемент
+    ListItem* pNext;
+    //Данные, которые хранятся в элементе
+    Data data;
+};
 
-if (!list)
+//Структура описывает односвязный список
+struct List
 {
-std::cout « "List creation error\n";
-return 1;
+    //Указатель на первый элемент
+    ListItem* head;
+};
+
+//Функция создаёт список
+List* list_create()
+{
+    //Создаём новый список
+    List* list = new List;
+    //Присваиваем первому элементу nullptr
+    list->head = nullptr;
+    //Возвращаем этот список
+    return list;
 }
 
-list_insert(list, 1);
-list_insert(list, 2);
-list_insert(list, 3);
-
-if (list_item_data(list_first(list)) != 3)
+//Функция удаляет список
+void list_delete(List* list)
 {
-std::cout « "list_insert error\n";
-return 1;
+    //Пока есть элементы после первого выполняем цикл
+    while (list_item_next(list->head))
+    {
+        //Удаляем элемент после первого
+        list_erase_next(list, list->head);
+    }
+    //Удаляем первый элемент
+    delete list->head;
+    //Удаляем список
+    delete list;
+
 }
 
-list_insert_after(list, list_first(list), 4);
-
-if (list_item_data(list_item_next(list_first(list))) != 4)
+//Функция возвращает первый элемент списка
+ListItem* list_first(List* list)
 {
-std::cout « "list_insert_after error\n";
-return 1;
+    //Проверяем есть ли первый элемент, если есть возвращаем его, иначе NULL
+    if (list->head)
+        return list->head;
+    else
+        return NULL;
+
 }
 
-/*list_erase(list, list_first(list));
-if (list_item_data(list_first(list)) != 4)
+//Функция возвращает данные, которые хранятся в указанном элементе
+Data list_item_data(const ListItem* item)
 {
-std::cout « "list_erase error\n";
-return 1;
+    //Проверяем есть ли элемент, если есть возвращаем его значение, иначе NULL
+    if (item)
+        return item->data;
+    else
+        return NULL;
 }
-std::cout « "List: ";
-for (ListItem *item = list_first(list) ; item ; item = list_item_next(item))
+
+//Функция возвращает элемент списка, который следует за указанным
+ListItem* list_item_next(ListItem* item)
 {
-std::cout « list_item_data(item) « " ";
+    //Проверяем есть ли элемент, если есть возвращаем ссылку на следующий элемент, иначе NULL
+    if (item)
+        return item->pNext;
+    else
+        return NULL;
 }
-std::cout « "\n";*/
 
-
-std::cout « "List: ";
-for (ListItem* item = list_item_next(list_first(list)); item; item = list_item_next(item))
+//Функция не подходит для односвязного списка
+ListItem* list_item_prev(ListItem* item)
 {
-std::cout « list_item_data(item) « " ";
+    return NULL;
 }
-std::cout « "\n";
 
+//Функция вставляет новый элемент в начало списка
+ListItem* list_insert(List* list, Data data)
+{
+    //Создаём новый элемент списка
+    ListItem* a = new ListItem;
+    a->data = data;
+    //Указываем ссылку на первый элемент списка в новом элементе
+    a->pNext = list_first(list);
+    //У списка переприсваиваем первый элемент списка на новый
+    list->head = a;
+    return a;
+}
 
-list_delete(list);
+//Функция вставляет новый элемент в список после указанного элемента
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
+{
+    //Создаём новый элемент списка
+    ListItem* a = new ListItem;
+    a->data = data;
+    //Указываем ссылку на следующий за указанным элемент списка в новом элементе
+    a->pNext = item->pNext;
+    //У списка переприсваиваем указанный элемент списка на новый
+    item->pNext = a;
+    return a;
+}
+
+//Функция не подходит для односвязного списка
+ListItem* list_erase(List* list, ListItem* item)
+{
+    return NULL;
+}
+
+//Функция удаляет элемент списка после указанного
+ListItem* list_erase_next(List* list, ListItem* item)
+{
+    //Создаём новый элемент списка (Используем для удаления данных)
+    ListItem* erase = nullptr;
+    //Проверяем существует ли необходисый нам элемент
+    if (list_item_next(item))
+    {
+        //Присваиваем erase элемент, который нам нужно удалить
+        erase = item->pNext;
+        //Присваиваем ссылку на следующий за указанным элемент списка указанному элементу
+        item->pNext = list_item_next(list_item_next(item));
+        delete erase;
+        return item;
+    }
+    else
+        return NULL;
 }
