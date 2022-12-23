899a533b6524c71226b21da6090bd9013f26a33b
Ti1234 <120597609+Ti1234@users.noreply.github.com>
2022-12-23 17:24:33 +0300

    Update list.cpp

--- LibraryCPP/Tests/list.cpp
+++ LibraryCPP/Tests/list.cpp
@@ -3,46 +3,53 @@
 
 int main()
 {
-    List *list = list_create();
+List *list = list_create();
 
-    if (!list)
-    {
-        std::cout << "List creation error\n";
-        return 1;
-    }
+if (!list)
+{
+std::cout « "List creation error\n";
+return 1;
+}
 
-    list_insert(list, 1);
-    list_insert(list, 2);
-    list_insert(list, 3);
+list_insert(list, 1);
+list_insert(list, 2);
+list_insert(list, 3);
 
-    if (list_item_data(list_first(list)) != 3)
-    {
-        std::cout << "list_insert error\n";
-        return 1;
-    }
+if (list_item_data(list_first(list)) != 3)
+{
+std::cout « "list_insert error\n";
+return 1;
+}
 
-    list_insert_after(list, list_first(list), 4);
+list_insert_after(list, list_first(list), 4);
 
-    if (list_item_data(list_item_next(list_first(list))) != 4)
-    {
-        std::cout << "list_insert_after error\n";
-        return 1;
-    }
+if (list_item_data(list_item_next(list_first(list))) != 4)
+{
+std::cout « "list_insert_after error\n";
+return 1;
+}
 
-    list_erase(list, list_first(list));
+/*list_erase(list, list_first(list));
+if (list_item_data(list_first(list)) != 4)
+{
+std::cout « "list_erase error\n";
+return 1;
+}
+std::cout « "List: ";
+for (ListItem *item = list_first(list) ; item ; item = list_item_next(item))
+{
+std::cout « list_item_data(item) « " ";
+}
+std::cout « "\n";*/
 
-    if (list_item_data(list_first(list)) != 4)
-    {
-        std::cout << "list_erase error\n";
-        return 1;
-    }
 
-    std::cout << "List: ";
-    for (ListItem *item = list_first(list) ; item ; item = list_item_next(item))
-    {
-        std::cout << list_item_data(item) << " ";
-    }
-    std::cout << "\n";
+std::cout « "List: ";
+for (ListItem* item = list_item_next(list_first(list)); item; item = list_item_next(item))
+{
+std::cout « list_item_data(item) « " ";
+}
+std::cout « "\n";
 
-    list_delete(list);
+
+list_delete(list);
 }
