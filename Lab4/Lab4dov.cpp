84253cb75347e4169a2fae436752637fcfb2a3a4
Merk9ayt <90393760+Merk9ayt@users.noreply.github.com>
2022-12-24 15:08:34 +0300

    Update Lab4dov.cpp

--- Lab4/Lab4dov.cpp
+++ Lab4/Lab4dov.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "dgraph.h"
-#include <climits>
+#include <vector>
 
 using namespace std;
 
@@ -8,66 +8,61 @@ const int VERTICES_COUNT = 5;
 const int FIRST_VERTEX = 0;
 const int SECOND_VERTEX = 4;
 
-struct Data
+struct Data 
 {
-    size_t index = -1;
+    int index = -1;
     int cost = INT_MAX;
     bool was = false;
-    Data(int index) : index(index) {};
+    Data(int index) : index(index) {}
 };
 
-void fillDGraph(DGraph<Data> graph)
+void fillGraph(DGraph<Data> graph) 
 {
-    for (int i = 0; i < VERTICES_COUNT; i++)
-        graph.addVertex(Data(i));
-    graph.addEdge(FIRST_VERTEX, 1, 10);
-    graph.addEdge(FIRST_VERTEX, 3, 30);
-    graph.addEdge(FIRST_VERTEX, 4, 100);
-    graph.addEdge(1, 2, 50);
-    graph.addEdge(3, 4, 60);
-    graph.addEdge(3, 2, 20);
-    graph.addEdge(2, 4, 10);
+    for (int i = 0; i < VERTICES_COUNT; ++i) 
+    {
+        graph.add_vertex(Data(i));
+    }
+    graph.add_edge(FIRST_VERTEX, 1, 10);
+    graph.add_edge(FIRST_VERTEX, 3, 30);
+    graph.add_edge(FIRST_VERTEX, 4, 100);
+    graph.add_edge(1, 2, 50);
+    graph.add_edge(3, 4, 60);
+    graph.add_edge(3, 2, 20);
+    graph.add_edge(2, 4, 10);
 }
 
-int main()
+int main() 
 {
-    DGraph<Data> graph(0, Data(- 1));
-    fillDGraph(graph);
+    DGraph<Data> graph(0,Data(-1));
+    fillGraph(graph);
 
-    auto* vertex = graph.getVertex(FIRST_VERTEX);
-    auto get_vertex = vertex->getVertexData();
-    Data test1 = get_vertex.cost = 0;
-    vertex->setVertexData(test1);
+    auto* vertex = graph.get_vertex(FIRST_VERTEX);
+    vertex->data.cost = 0;
     DGraph<Data>::Vertex* min_cost_vertex;
-    auto get_min_cost_vertex = min_cost_vertex->getVertexData();
     do
     {
         min_cost_vertex = nullptr;
-        get_vertex.was = true;
-        for (size_t i = 0; i < graph.getVertexAmount(); ++i)
+        vertex->data.was = true;
+        for (int i = 0; i < graph.get_vertex_amount(); ++i)
         {
-            auto* new_vertex = graph.getVertex(i);
-            auto get_new_vertex = new_vertex->getVertexData();
-            if (!get_new_vertex.was)
+            DGraph<Data>::VertexIterator vertex_iter(graph);
+            auto* new_vertex = vertex_iter.graph.get_vertex(i);
+            if (!new_vertex->data.was)
             {
-                int new_cost = get_new_vertex.cost;
-                if (graph.isEdge(get_vertex.index, i))
+                int new_cost = new_vertex->data.cost;
+                if (graph.contains_edge_between_vertices(vertex->data.index, i))
                 {
-                    auto get_edge = graph.getEdge(get_vertex.index, i);
-                    auto get_data_edge = get_edge.getEdgeData();
-                    new_cost = min(new_cost, get_vertex.cost + get_data_edge.cost);
-                    get_new_vertex.cost = new_cost;
+                    new_cost = min(new_cost, vertex->data.cost + graph.get_edge_weight(vertex->data.index, i));
+                    new_vertex->data.cost = new_cost;
                 }
-                if (min_cost_vertex == nullptr || new_cost < get_min_cost_vertex.cost)
+                if (min_cost_vertex == nullptr || new_cost < min_cost_vertex->data.cost) 
                 {
-                    cout << get_vertex.index << "->";
+                    cout << vertex->data.index<<"->";
                     min_cost_vertex = new_vertex;
                 }
             }
         }
         vertex = min_cost_vertex;
     } while (min_cost_vertex == nullptr);
-    auto* second_vert = graph.getVertex(SECOND_VERTEX);
-    auto get_second_vertex = second_vert->getVertexData();
-    cout << "\n" << get_second_vertex.cost;
+    cout<<"\n" << graph.get_vertex(SECOND_VERTEX)->data.cost;
 }
