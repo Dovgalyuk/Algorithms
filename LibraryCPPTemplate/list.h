2cc96b80df1957bb015d4253ca72bb15d990dc59
Kurdova Juliana <101501787+julktnn@users.noreply.github.com>
2022-12-24 17:04:33 +0300

    Update list.h

--- LibraryCPPTemplate/list.h
+++ LibraryCPPTemplate/list.h
@@ -1,122 +1,109 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <typename Data> 
-class List
-{
+template <typename Data>
+class List {
 public:
-    class Item
-    {
+    class Item {
     public:
         Item* nextt;
         Item* prevv;
-        Item(Data data)
-        {
+        Item(Data data) {
             nextt = nullptr;
             prevv = nullptr;
             field = data;
         }
-        Item* next() {return nextt;}
-        Item* prev() {return prevv;}
-        Data& data() {return field;}
-        const Data& data() const {return field;}
+
+        Item* next() { return nextt; }
+        Item* prev() { return prevv; }
+        Data& data() { return field; }
+        const Data& data() const { return field; }
 
     private:
         Data field;
     };
 
     // Creates new list
-    List()
-    {
+    List() {
         head = nullptr;
     }
 
     // Destroys the list and frees the memory
-    ~List()
-    {
+    ~List() {
         Item* item = head;
-        Item* next;
+        Item* nextItem;
         while (item != nullptr) {
-            next = item->next();
+            nextItem = item->next();
             delete item;
-            item = next;
+            item = nextItem;
         }
     }
 
-    // Retrieves the head item from the list
-    Item *first()
-    {
+    // Retrieves the first item from the list
+    Item* first() {
         return head;
     }
 
+    // Return list items count
+    size_t getSize() const {
+        int size = 0;
+        Item* next = head;
+        while (next != nullptr) {
+            size++;
+            next = next->next();
+        }
+        return size;
+    }
+
     // Inserts new list item into the beginning
-    Item *insert(Data data)
-    {
+    Item* insert(Data data) {
         auto newItem = new Item(data);
         newItem->nextt = head;
+
         if (head != nullptr)
-        {
             head->prevv = newItem;
-        }
+
         head = newItem;
         return newItem;
     }
 
     // Inserts new list item after the specified item
-    Item *insert_after(Item *item, Data data)
-    {
+    Item* insert_after(Item* item, Data data) {
         auto* newItem = new Item(data);
         newItem->nextt = item->next();
-        newItem->prevv = item;
+
         if (item->next() != nullptr)
-        {
             item->next()->prevv = newItem;
-        }
+
+        newItem->prevv = item;
         item->nextt = newItem;
         return newItem;
     }
 
     // Deletes the specified list item.
-    // Returns pointer to the item next to the deleted one.
     // Not applicable for the singly linked lists.
     // Should be O(1)
-    Item *erase(Item *item)
-    {
-        auto *nextItem = item->next();
-        auto *prevItem = item->prev();
+    Item* erase(Item* item) {
+        auto* nextItem = item->next();
+        auto* prevItem = item->prev();
 
         if (head == item)
-        {
             head = nextItem;
-        }
+
         if (nextItem != nullptr)
-        {
-            prevItem = nextItem->prev();
-        }
+            nextItem->prevv = prevItem;
+
         if (prevItem != nullptr)
-        {
-            nextItem = prevItem->next();
-        }
+            prevItem->nextt = nextItem;
+
         delete item;
         return nextItem;
     }
 
-    // Deletes the list item following the specified one.
-    // Returns pointer to the item next to the deleted one.
+    // Deletes the list item following the specified one
     // Should be O(1)
-    Item *erase_next(Item *item)
-    {
+    Item* erase_next(Item* item) {
         return erase(item->next());
-    }
-
-    size_t get_size() const {
-        int size = 0;
-        Item* nextItem = head;
-        while (nextItem != nullptr) {
-            size++;
-            nextItem = nextItem->next();
-        }
-        return size;
     }
 protected:
     Item* head;
