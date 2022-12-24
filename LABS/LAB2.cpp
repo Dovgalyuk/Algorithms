e0dea25cab8397e5a88cfcbb20006ecfd2abddff
Andruf12 <113371649+Andruf12@users.noreply.github.com>
2022-12-24 12:14:51 +0300

    Update LAB2.cpp

--- LABS/LAB2.cpp
+++ LABS/LAB2.cpp
@@ -7,82 +7,53 @@ using namespace std;
 
 bool check(Stack* stack, string stroka)
 {
-    int count1 = 0;
-    int count2 = 0;
-    int count3 = 0;
-    int count4 = 0;
     for (char i : stroka) {
-        
         switch (i) {
-        case '(': stack_push(stack, '('); count1++;  break;
-        case '[': stack_push(stack, '['); count2++; break;
-        case '{': stack_push(stack, '{'); count3++;  break;
-        case '<': stack_push(stack, '<'); count4++;  break;
+        case '(': stack_push(stack, '(');  break;
+        case '[': stack_push(stack, '[');  break;
+        case '{': stack_push(stack, '{');  break;
+        case '<': stack_push(stack, '<');  break;
         case ')':
-            count1--;
-            if (stack_empty(stack) || stack_get(stack) != '(' || count1 ==1   )
-
+            if (stack_empty(stack) || stack_get(stack) != '('    )
         {
             return false;
         }
-            
                 stack_pop(stack); 
-
                 break;
         case ']':
-            count2--;
-            if (stack_empty(stack) || stack_get(stack) != '[' || count2 == 1)
-
+            if (stack_empty(stack) || stack_get(stack) != '[' )
         {
             return false;
         }
                 stack_pop(stack);
-
                 break;
         case '}':
-            count3--;
-            if (stack_empty(stack) || stack_get(stack) != '{' || count3 == 1)
-
+            if (stack_empty(stack) || stack_get(stack) != '{' )
         {
             return false;
         }
                 stack_pop(stack);
-
                 break;
         case '>':
-            count4--;
-
-            if (stack_empty(stack) || stack_get(stack) != '<' || count4 == 1)
-        
+            if (stack_empty(stack) || stack_get(stack) != '<')
             {
                 return false;
             }
             stack_pop(stack);
-            
             break;
         default: break;
         }
-       
-        
-        
-       
    }
     if (stack_empty(stack))
         return true;
     else
         return false;
-    
-   
-    
-    
 }
 
 
 
 
 int main() {
-
-
 	Stack* stack = stack_create();
 	string stroka;
    
@@ -92,8 +63,4 @@ int main() {
     else
         cout << "NO" << endl;
     stack_delete(stack);
-    
-   
-	
-
 }
