9b75773ddf90410d8a3714f291657640a919fc0f
SaintEfim <93579076+SaintEfim@users.noreply.github.com>
2022-12-25 21:04:43 +0300

    Update Source.cpp

--- Laba4/Source.cpp
+++ Laba4/Source.cpp
@@ -1,106 +1,67 @@
 #include <iostream>
-#include <vector>
 #include "graph.h"
+#include <cstring>
+using namespace std;
+
+
+#define INF 9999999
+
+// number of vertices in grapj
+#define V 5
+
+// create a 2d array of size 5x5
+//for adjacency matrix to represent graph 
+int G[V][V] = {
+      {0, 9, 75, 0, 0},
+      {9, 0, 95, 19, 42},
+      {75, 95, 0, 51, 66},
+      {0, 19, 51, 0, 31},
+      {0, 42, 66, 31, 0}
+};
 
 int main() {
 
-    int size = 6;
-    auto graph = new Graph();
-
-    for(int i = 0; i < size; i++){
-        graph->AddVertex(i+1);
-    }
-
-    for(int i = 0; i < size; i++){
-        graph->getVertex(i)->SetData(i+1);
-    }
-    graph->AddEdge(*graph->getVertex(0), *graph->getVertex(1), 6);// 1 -> 2
-    graph->AddEdge(*graph->getVertex(1), *graph->getVertex(0), 6);// 2 -> 1
-
-    graph->AddEdge(*graph->getVertex(0), *graph->getVertex(3), 5);// 1 -> 4
-    graph->AddEdge(*graph->getVertex(3), *graph->getVertex(0), 5);// 4 -> 1
-
-    graph->AddEdge(*graph->getVertex(0), *graph->getVertex(2), 1);// 1 -> 3
-    graph->AddEdge(*graph->getVertex(2), *graph->getVertex(0), 5);// 3 -> 1
-
-    graph->AddEdge(*graph->getVertex(1), *graph->getVertex(2), 5);// 2 -> 3
-    graph->AddEdge(*graph->getVertex(2), *graph->getVertex(1), 5);// 2 -> 3
-
-    graph->AddEdge(*graph->getVertex(1), *graph->getVertex(4), 3);// 2 -> 5
-    graph->AddEdge(*graph->getVertex(4), *graph->getVertex(1), 3);// 5 -> 2
-
-    graph->AddEdge(*graph->getVertex(2),*graph->getVertex(4), 6); // 3 -> 5
-    graph->AddEdge(*graph->getVertex(4),*graph->getVertex(2), 6); // 5 -> 3
-
-    graph->AddEdge(*graph->getVertex(2),*graph->getVertex(5), 4); // 3 -> 6
-    graph->AddEdge(*graph->getVertex(5),*graph->getVertex(2), 4); // 6 -> 3
-
-    graph->AddEdge(*graph->getVertex(2),*graph->getVertex(3), 5); // 3 -> 4
-    graph->AddEdge(*graph->getVertex(3),*graph->getVertex(2), 5); // 4 -> 3
-
-    graph->AddEdge(*graph->getVertex(3), *graph->getVertex(5), 2);// 4 -> 6
-    graph->AddEdge(*graph->getVertex(5), *graph->getVertex(3), 2);// 6 -> 4
-
-    graph->AddEdge(*graph->getVertex(4), *graph->getVertex(5), 6);// 5 -> 6
-    graph->AddEdge(*graph->getVertex(5), *graph->getVertex(4), 6);// 6 -> 5
-
-    //Prima:
+    // Graph<int> *graph = new Graph<int>(V, V);
 
 
-    auto prima = new Graph();
+    int no_edge;
+    int selected[V];
 
-    for(int i = 0; i < size; i++){
-        prima->AddVertex(i+1);
-    }
+    memset(selected, false, sizeof(selected));
 
-    for(int i = 0; i < size; i++){
-        prima->getVertex(i)->SetData(i+1);
-    }
-
-    auto vertex = graph->getVertex(0);
-    auto vertexEdges = vertex->getEdges();
-    auto minEdge = vertexEdges.first()->getData();
-
-    for(auto item = vertexEdges.first(); item != nullptr; item = item->getNext()){
-        if(item->getData()->GetWeight() < minEdge->GetWeight()){
-            minEdge = item->getData();
-        }
-    }
-
-    auto v = minEdge->GetPointV();
-
-    prima->AddEdge(*vertex, *v, minEdge->GetWeight());
-    prima->AddEdge(*v,*vertex , minEdge->GetWeight());
-
-    int countEdges = 1;
-    std::vector<Graph::Vertex*> usedVertices;
-    usedVertices.push_back(vertex);
-    vertex = minEdge->GetPointV();
-    vertexEdges = vertex->getEdges();
-    minEdge = vertexEdges.first()->getData();
+    no_edge = 0;
 
 
-    while(countEdges != prima->GetVertices().size() - 1){
-        int i = 0;
-        for(auto item = vertexEdges.first(); item != nullptr; item = item->getNext()){
-            if((item->getData()->GetWeight() < minEdge->GetWeight()) and (item->getData()->GetPointV() != usedVertices[i])){
+    selected[0] = true;
 
+    int x;
+    int y;
+
+    cout << "Edge" << " : " << "Weight";
+    cout << endl;
+    while (no_edge < V - 1) {
+
+        int min = INF;
+        x = 0;
+        y = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (selected[i]) {
+                for (int j = 0; j < V; j++) {
+                    if (!selected[j] && G[i][j]) {
+                        if (min > G[i][j]) {
+                            min = G[i][j];
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
             }
-            i++;
         }
-        v = minEdge->GetPointV();
-        prima->AddEdge(*vertex, *v, minEdge->GetWeight());
-        prima->AddEdge(*v,*vertex , minEdge->GetWeight());
-        countEdges++;
-        usedVertices.push_back(vertex);
-        vertex = minEdge->GetPointV();
-        vertexEdges = vertex->getEdges();
-        minEdge = vertexEdges.first()->getData();
+        cout << x << " - " << y << " :  " << G[x][y];
+        cout << endl;
+        selected[y] = true;
+        no_edge++;
     }
-
-    if(!prima->IsEdge(*graph->getVertex(0), *graph->getVertex(1))){
-        exit(1);
-    }
-
     return 0;
 }
