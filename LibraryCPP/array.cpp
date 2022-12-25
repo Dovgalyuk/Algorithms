dfa3ddcecd1edfff984bbc869c7a2730f2c474e5
RyazantsevNik <ryazantsevnikita8@gmail.com>
2022-12-26 01:52:30 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,52 +1,54 @@
-#pragma once
 #include "array.h"
-struct Array {
-	Data* data;
-	size_t size;
+#pragma once
+struct Array
+{
+    Data* data;
+    size_t size;
 
-	Array(int size) {
-		this->size = size;
-		this->data = new Data[size];
-	}
-
-	~Array() {
-		delete[] data;
-	}
+    Array(const size_t size) {
+        this->size = size;
+        this->data = new Data[size];
+    }
+    ~Array() {
+        delete[] data;
+    }
 };
 
-// returns array size
-size_t array_size(const Array* arr) {
-	return arr->size;
-}
-
 // create array
-Array* array_create(size_t size) {
-	return new Array(size);
+Array* array_create(size_t size)
+{
+    return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array* arr) {
-	delete arr;
+void array_delete(Array* arr)
+{
+    delete arr;
 }
 
 // returns specified array element
-
-Data array_get(const Array* arr, size_t index) {
-	size_t size = array_size(arr);
-	if (index < size && index >= 0) {
-		return arr->data[index];
-	}
-	else {
-		return Data();
-	}
+Data array_get(const Array* arr, size_t index)
+{
+    size_t size = array_size(arr);
+    if (index < size) {
+        return arr->data[index];
+    }
+    else {
+        return Data();
+    }
 }
 
 // sets the specified array element to the value
-void array_set(Array* arr, size_t index, Data value) {
-	size_t size = array_size(arr);
-	if (index < size && index >= 0) {
-		arr->data[index] = value;
-	}
+void array_set(Array* arr, size_t index, Data value)
+{
+    size_t size = array_size(arr);
+    if (index < size) {
+        arr->data[index] = value;
+    }
 }
 
 // returns array size
+size_t array_size(const Array* arr)
+{
+    return arr->size;
+}
