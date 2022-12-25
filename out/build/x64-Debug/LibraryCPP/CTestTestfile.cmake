82edc0ee80749c2a83a71020acaf404f82bb2655
TihonYakovlev <96542203+TihonYakovlev@users.noreply.github.com>
2022-12-25 18:31:49 +0300

    Я удолил гамно

--- out/build/x64-Debug/LibraryCPP/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Acer/source/repos/Algorithms/LibraryCPP
-# Build directory: C:/Users/Acer/source/repos/Algorithms/out/build/x64-Debug/LibraryCPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
