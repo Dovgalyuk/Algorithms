69a7617ab2e3c52eab32c567814ee66a14616baa
likazzz <115737695+likazzz@users.noreply.github.com>
2022-12-25 21:30:13 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,87 @@
 #include "queue.h"
+#include "vector.h"
 
 struct Queue
 {
+    Vector* vector;
+    size_t head;
+    size_t tail;
+    int kol;
+    Queue()
+    {
+        head = 0;
+        tail = 0;
+        vector = vector_create();
+        vector_resize(vector, 1);
+        kol = 0;
+    }
 };
 
-Queue *queue_create()
+Queue* queue_create()
 {
     return new Queue;
 }
 
-void queue_delete(Queue *queue)
+void queue_delete(Queue* queue)
 {
-    // TODO: free queue items
+    vector_delete(queue->vector);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    if (queue->tail == vector_size(queue->vector))
+    {
+        if (queue->head > 0)
+            queue->tail = 0;
+        else
+            vector_resize(queue->vector, vector_size(queue->vector) + 1);
+
+    }
+    else if (queue->tail == queue->head)
+    {
+        vector_resize(queue->vector, vector_size(queue->vector) + queue->head);
+        for (int i = 0; i < queue->head; i++)
+        {
+            queue->tail--;
+            vector_set(queue->vector, vector_size(queue->vector) - 1 - i, vector_get(queue->vector, queue->tail));
+            vector_set(queue->vector, queue->tail, 0);
+
+        }
+    }
+    vector_set(queue->vector, queue->tail, data);
+    queue->tail++;
+    queue->kol++;
 }
 
-Data queue_get(const Queue *queue)
+Data queue_get(const Queue* queue)
 {
-    return (Data)0;
+    if (!queue_empty(queue))
+    {
+        return vector_get(queue->vector, queue->head);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-void queue_remove(Queue *queue)
+void queue_remove(Queue* queue)
 {
+    if (!queue_empty(queue))
+    {
+        if (queue->head >= vector_size(queue->vector) - 1)
+        {
+            queue->head = 0;
+        }
+        else
+        {
+            queue->head++;
+        }
+    }
 }
 
-bool queue_empty(const Queue *queue)
+bool queue_empty(const Queue* queue)
 {
-    return true;
+    return queue->tail == queue->head;;
 }
