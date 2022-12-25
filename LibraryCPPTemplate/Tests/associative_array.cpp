dedbd6b4e418dc7da512019df4e80bb5cb197722
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-25 19:48:13 +0300

    Update associative_array.cpp

--- LibraryCPPTemplate/Tests/associative_array.cpp
+++ LibraryCPPTemplate/Tests/associative_array.cpp
@@ -1,13 +1,12 @@
-﻿#include <iostream>
-#include <ctime>
+#include <iostream>
 #include "associative_array.h"
 using namespace std;
 
 int main()
 {
-    AssociativeArray array(to_string(0), to_string(0));
+    AssociativeArray array("0", "0");
     int count = 1000000;
-    for (int i = 0; i < count; ++i)
+    for (int i = 1; i < count; ++i)
     {
         string Str = to_string(i);
         array.add(Str, Str);
@@ -21,5 +20,15 @@ int main()
             cout << "Data storage error ";
             return 1;
         }
-    } 
+    }
+    for (int i = 0; i < count; ++i)
+    {
+        string Str = to_string(i);
+        array.remove(Str);
+        if (array.search(Str) != "")
+        {
+            cout << "Error deleting an element";
+            return 1;
+        }
+    }
 }
