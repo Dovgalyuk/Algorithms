b409f1591f16a23418efddd6e8df17176f8a4386
COURAGE231 <113786856+COURAGE231@users.noreply.github.com>
2022-12-25 13:19:10 +0300

    Update lb5.cpp

--- Lab5/lb5.cpp
+++ Lab5/lb5.cpp
@@ -1,15 +1,56 @@
-﻿#include "associative_array.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include "associative_array.h"
 
-int main()
+using namespace std;
+
+typedef string Key;
+typedef string Value;
+typedef milli Ratio;
+typedef AssociativeArray AVLAssocArray;
+
+void Comparison(int length)
 {
-	string one = "Ivan";
-	string second = "Filippov";
-	AssociativeArray lol(one, second);
-	lol.add(second, one);
-	lol.add("PEPA", "FAFA");
-	lol.add("PEPAf", "FAFAq");
-	lol.remove("PEPA");
-	string itog = lol.search("PEPAf");
-	cout << itog;
+    srand(time(0));
+    AVLAssocArray arr("0", "0");
+    vector<string> Strs;
+    Strs.resize(length);
+    for (int i = 0; i < length; ++i)
+    {
+        Strs[i] = to_string(i);
+
+    }
+    auto start = chrono::system_clock::now();
+    for (int i = 0; i < length; ++i)
+    {
+        arr.add(Strs[i], Strs[i]);
+    }
+    auto end = chrono::system_clock::now();
+    chrono::duration<double, Ratio> insertToArray = end - start;
+
+    map<Key, Value> map;
+    start = chrono::system_clock::now();
+    for (int i = 0; i < length; ++i)
+    {
+        map[Strs[i]] = Strs[i];
+    }
+    end = chrono::system_clock::now();
+    chrono::duration<double, Ratio> insertToMap = end - start;
+
+    string StrArr = to_string(int (insertToArray.count()));   
+    string StrMap = to_string(int (insertToMap.count()));
+    cout << "\t" << length << "  | " << StrArr << "\t   | " << StrMap << "\n";
+}
+
+int main() {
+    const int MIN = 50000;
+    const int MAX = 1000000;
+    cout << "\tLength  | Associative array | Map\n";
+    for (int i = MIN; i <= MAX; i += MIN)
+    {
+        Comparison(i);
+    }
 }
