e43e157122f27b4783f5d22d054dcd7dd870c0ba
Nikita <81405402+Mist531@users.noreply.github.com>
2022-12-27 00:54:47 +0300

    lab 3 fix

--- Labs/Third/lab3.cpp
+++ Labs/Third/lab3.cpp
@@ -16,7 +16,15 @@ bool isPass(const char i) {
     return i == PASSED;
 }
 
-void checkPoint(Queue *Qx, Queue *Yx, const int x, const int y) {
+bool isEmpty(const char i) {
+    return i == EMPTY;
+}
+
+bool checkPoint(const char i) {
+    return (isEmpty(i) || isNumber(i)) && !isPass(i);
+}
+
+void insertPoint(Queue *Qx, Queue *Yx, const int x, const int y) {
     queue_insert(Qx, x);
     queue_insert(Yx, y);
 }
@@ -27,17 +35,17 @@ void findPoints(vector<string> &masFile, Queue *Qx, Queue *Qy, const int x, cons
     char right = masFile[x][y - 1];
     char left = masFile[x][y + 1];
 
-    if ((left == EMPTY || isNumber(left)) && !isPass(left)) {
-        checkPoint(Qx, Qy, x, y + 1);
+    if (checkPoint(left)) {
+        insertPoint(Qx, Qy, x, y + 1);
     }
-    if ((right == EMPTY || isNumber(right)) && !isPass(right)) {
-        checkPoint(Qx, Qy, x, y - 1);
+    if (checkPoint(right)) {
+        insertPoint(Qx, Qy, x, y - 1);
     }
-    if ((up == EMPTY || isNumber(up)) && !isPass(up)) {
-        checkPoint(Qx, Qy, x - 1, y);
+    if (checkPoint(up)) {
+        insertPoint(Qx, Qy, x - 1, y);
     }
-    if ((down == EMPTY || isNumber(down)) && !isPass(down)) {
-        checkPoint(Qx, Qy, x + 1, y);
+    if (checkPoint(down)) {
+        insertPoint(Qx, Qy, x + 1, y);
     }
 
     masFile[x][y] = PASSED;
@@ -72,14 +80,14 @@ int main() {
             int x1 = queue_get(x);
             int y1 = queue_get(y);
             if (isNumber(masFile[x1][y1])) {
-                cout << "x = " << x1 + 1 << " y = " << y1 + 1 << endl;
-                cout << "masFile[x][y] = " << masFile[x1][y1] << endl;
+                cout << "Closest number to the beginning = " << masFile[x1][y1] << endl;
+                cout << "The coordinates of this number: x = " << y1 + 1 << " y = " << x1 + 1 << endl;
                 queue_delete(x);
                 queue_delete(y);
                 file.close();
                 exit(0);
             }
-            findPoints(masFile, x, y, queue_get(x), queue_get(y));
+            findPoints(masFile, x, y, x1, y1);
             queue_remove(x);
             queue_remove(y);
         }
@@ -91,5 +99,4 @@ int main() {
     queue_delete(x);
     queue_delete(y);
     file.close();
-
 }
