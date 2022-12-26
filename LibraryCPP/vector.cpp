49491515c39b66ef434d2e7d134c9d10874c4af4
likazzz <115737695+likazzz@users.noreply.github.com>
2022-12-26 13:23:44 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -55,7 +55,7 @@ void vector_resize(Vector* vector, size_t size)
     }
     else {
         size_t temp_capacity = size * 2;
-        Data* temp_data = new Data[temp_capacity]{ NULL };
+        Data* temp_data = new Data[temp_capacity]{ Data() };
 
         for (int i = 0; i < vector->size; i++)
         {
