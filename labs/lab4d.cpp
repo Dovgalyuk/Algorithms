22b1c8444bc824b698671dc5c963faeaf4c9ba56
Alexey <45918010+midhey@users.noreply.github.com>
2022-12-26 12:37:55 +0300

    Add files via upload

---
+++ labs/lab4d.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>	
+#include "list.h"
+#include "graph.h"
+
+using namespace std;
+
+Graph<int>::Vertex* findVertex (Graph<int>* graph, int count) {
+	Graph<int>::vertexIterator vIterator(graph);
+	int counter = 0;
+	while (counter != count) {
+		++vIterator;
+		counter++;
+	}
+	return *vIterator;
+}
+
+int main() {
+	
+	int size = 11;
+
+	Graph<int>* graph = new Graph<int>(size);
+
+	Graph<int>::vertexIterator vIterator(graph);
+
+	int value = 0;
+	while (*vIterator != nullptr) {
+		graph->setVertexData(*vIterator, value);
+		++vIterator;
+		value++;
+	}
+	vIterator.iteratorBegin(graph);
+
+	graph->addEdgeBetween(findVertex(graph, 0), findVertex(graph, 1));
+	graph->addEdgeBetween(findVertex(graph, 0), findVertex(graph, 2), 19);
+	graph->addEdgeBetween(findVertex(graph, 0), findVertex(graph, 3));
+	graph->addEdgeBetween(findVertex(graph, 3), findVertex(graph, 2));
+	graph->addEdgeBetween(findVertex(graph, 2), findVertex(graph, 4));
+	graph->addEdgeBetween(findVertex(graph, 4), findVertex(graph, 5));
+	graph->addEdgeBetween(findVertex(graph, 6), findVertex(graph, 7));
+	graph->addEdgeBetween(findVertex(graph, 7), findVertex(graph, 8));
+	graph->addEdgeBetween(findVertex(graph, 9), findVertex(graph, 10));
+
+
+	return 0;
+}
