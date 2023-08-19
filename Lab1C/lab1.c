2556319d3ffee727e1224e752392a221d6e70d65
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2023-08-19 20:59:39 +0300

    Added first lab C template

---
+++ Lab1C/lab1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "array.h"
+
+void task1(Array *arr)
+{
+}
+
+void task2(Array *arr)
+{
+}
+
+int main()
+{
+    Array *arr = NULL;
+    task1(arr);
+    task2(arr);
+    array_delete(arr);
+}
