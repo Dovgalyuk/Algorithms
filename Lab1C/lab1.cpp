ce1f6eed18209061de4194977b5c22815f83b7c6
heheheee21 <MButovskiy@gmail.com>
2023-09-09 15:55:18 +0300

    try0

---
+++ Lab1C/lab1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+void RandArr(Array* arr)
+{
+    for (int index = 0; index < array_size(arr); index++)
+    {
+        array_set(arr, index, rand() % 100);
+    }
+}
+
+void task1(Array* arr)
+{
+    RandArr(arr);
+
+    int count = 0, minArrMember = 101, maxArrMember = -1;
+    float middle;
+
+    for (int index = 0; index < array_size(arr); index++)
+    {
+        if (array_get(arr, index) < minArrMember)
+            minArrMember = array_get(arr, index);
+
+        if (array_get(arr, index) > maxArrMember)
+            maxArrMember = array_get(arr, index);
+    }
+
+    middle = (maxArrMember + minArrMember) / 2;
+    
+    for (int index = 0; index < array_size(arr); index++)
+    {
+        if (array_get(arr, index) > middle)
+        {
+            count++;
+            cout << index << " ";
+        }
+    }
+    cout << endl;
+}
+
+void task2(Array* arr)
+{
+    RandArr(arr);
+
+    int sum = 0;
+    float middle;
+
+    for (int index = 0; index < array_size(arr); index++)
+    {
+        sum += array_get(arr, index);
+    }
+
+    middle = sum / array_size(arr);
+
+
+    for (int index = 0; index < array_size(arr); index++)
+    {
+        if (array_get(arr, index) < middle)
+        {
+            cout << array_get(arr, index) << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Array* arr = NULL;
+    size_t size;
+
+    cin >> size;
+
+    arr = array_create(size);
+    task1(arr);
+    array_delete(arr);
+    arr = NULL;
+    cin >> size;
+
+    arr = array_create(size);
+    task2(arr);
+    array_delete(arr);
+}
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,45 @@
 
 struct Array
 {
+    Data* data;
+    size_t size;
 };
 
-// create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    Array* arr = new Array;
+    arr->data = new Data[size];
+    arr->size = size;
+    return arr;
 }
 
-// delete array, free memory
 void array_delete(Array *arr)
 {
+    delete[] arr->data;
     delete arr;
 }
 
-// returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (index < arr->size)
+    {
+        return arr->data[index];
+    }
+    else
+    {
+        return (Data)0;
+    }
 }
 
-// sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index < arr->size)
+    {
+        arr->data[index] = value;
+    }
 }
 
-// returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
