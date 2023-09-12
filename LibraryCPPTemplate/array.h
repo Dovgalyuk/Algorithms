caacb1c812b237c4b76e8aa04472b6a8b759c5ac
LazyHat <sevapetrov2016p@gmail.com>
2023-09-12 14:17:54 +0300

    dynamic array

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,51 +1,64 @@
 #ifndef ARRAY_TEMPLATE_H
 #define ARRAY_TEMPLATE_H
 
-template <typename Data> class Array
+template <typename Data>
+class Array
 {
 public:
     // create array
     explicit Array(size_t size)
     {
+        ptr = new Data[size];
+        this->size = size;
     }
 
     // copy constructor
     template <typename T>
     Array(const Array<T> &a)
     {
+        delete[] ptr;
+        ptr = new Data[a.size];
+        for (size_t i = 0; i < a.size; i++)
+            ptr[i] = (Data)a.ptr[i];
     }
 
     // assignment operator
     template <typename T>
     Array &operator=(const Array<T> &a)
     {
+        this->~Array();
+        this = Array(a);
         return *this;
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] ptr;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return ptr[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        prt[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return size;
     }
 
 private:
     // private data should be here
+    Data *ptr = nullptr;
+    size_t size = 0;
 };
 
 #endif
