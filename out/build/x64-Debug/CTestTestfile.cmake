9f428158a0c2a615476b12ab49a76bbc49ad2396
Nikita Ivanov <estarese@users.noreply.github.com>
2023-09-13 18:09:16 +0300

    lab1

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Algorithms
+# Build directory: C:/Users/user/Algorithms/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("LibraryC")
+subdirs("LibraryCPP")
+subdirs("LibraryCPPClass")
+subdirs("LibraryCPPTemplate")
+subdirs("Lab1C")
