bb5213f47e49e00c3ce8fdde8b390dd3041d8a38
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2023-09-22 17:43:29 +0300

    fix errors and release stack.h

--- Labs/lab2.cpp
+++ Labs/lab2.cpp
@@ -2,90 +2,126 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include "list.h"
+#include "stack.h"
 
-void execute_operations(List* stack, int vars[4], const std::string& operations);
+int main() {
+    Stack* stack = stack_create();
+    int variables[4] = { 0, 0, 0, 0 };
 
-std::string ReadFile() {
     std::ifstream inputFile("input.txt"); //D:\VisualStudioRepo\Algorithms\out\build\x64-debug\Labs - мое расположения файла. Он должен быть там, где собирается .exe файл решения
-    std::string otv;
     if (!inputFile.is_open()) {
-        std::cerr << "Open Error" << std::endl;
-        return "ERROR";
+        std::cerr << "Failed open!" << std::endl;
+        return 1;
     }
+
     std::string line;
     while (std::getline(inputFile, line)) {
-        otv += line;
-        otv += "\n";
+        std::istringstream iss(line);
+        std::string operation;
+        iss >> operation;
+
+        if (operation == "bipush") {
+            int value;
+            if (iss >> value) {
+                stack_push(stack, value);
+            }
+        }
+        else if (operation == "pop") {
+            stack_pop(stack);
+        }
+        else if (operation == "imul") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a * b);
+        }
+        else if (operation == "iand") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a & b);
+        }
+        else if (operation == "ior") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a | b);
+        }
+        else if (operation == "ixor") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a ^ b);
+        }
+        else if (operation == "iadd") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a + b);
+        }
+        else if (operation == "isub") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, b - a);
+        }
+        else if (operation == "iload_0") {
+            stack_push(stack, variables[0]);
+        }
+        else if (operation == "iload_1") {
+            stack_push(stack, variables[1]);
+        }
+        else if (operation == "iload_2") {
+            stack_push(stack, variables[2]);
+        }
+        else if (operation == "iload_3") {
+            stack_push(stack, variables[3]);
+        }
+        else if (operation == "istore_0") {
+            variables[0] = stack_get(stack);
+            stack_pop(stack);
+        }
+        else if (operation == "istore_1") {
+            variables[1] = stack_get(stack);
+            stack_pop(stack);
+        }
+        else if (operation == "istore_2") {
+            variables[2] = stack_get(stack);
+            stack_pop(stack);
+        }
+        else if (operation == "istore_3") {
+            variables[3] = stack_get(stack);
+            stack_pop(stack);
+        }
+        else if (operation == "swap") {
+            int a = stack_get(stack);
+            stack_pop(stack);
+            int b = stack_get(stack);
+            stack_pop(stack);
+            stack_push(stack, a);
+            stack_push(stack, b);
+        }
     }
     inputFile.close();
-    return otv;
-}
 
-int main()
-{
-    List* stack = list_create();
-    int vars[4] = { 0, 0, 0, 0 };
-
-    std::string operations = ReadFile();
-    if (operations == "ERROR")
-        return 0;
-
-    execute_operations(stack, vars, operations);
-
-    std::cout << "stack:\n";
-    for (ListItem* item = list_first(stack); item != nullptr; item = list_item_next(item))
-    {
-        std::cout << list_item_data(item) << "\n";
+    std::cout << "stack:" << std::endl;
+    while (!stack_empty(stack)) {
+        std::cout << stack_get(stack) << std::endl;
+        stack_pop(stack);
     }
 
-    std::cout << "vars:\n";
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << vars[i] << "\n";
+    std::cout << "vars:" << std::endl;
+    for (int i = 0; i < 4; ++i) {
+        std::cout << variables[i] << std::endl;
     }
 
-    list_delete(stack);
+    stack_delete(stack);
 
     return 0;
-}
-
-void execute_operations(List* stack, int vars[4], const std::string& operations)
-{
-    std::istringstream iss(operations);
-    std::string operation;
-
-    while (std::getline(iss, operation, '\n'))
-    {
-        std::istringstream iss_op(operation);
-        std::string op;
-
-        iss_op >> op;
-
-        if (op == "bipush")
-        {
-            int num;
-            iss_op >> num;
-            list_insert(stack, num);
-        }
-        else if (op == "pop")
-        {
-            list_erase_first(stack);
-        }
-        else if (op == "iadd")
-        {
-            ListItem* top1 = list_first(stack);
-            ListItem* top2 = list_item_next(top1);
-            int result = list_item_data(top1) + list_item_data(top2);
-            list_erase_first(stack);
-            list_erase_first(stack);
-            list_insert(stack, result);
-        }
-        else if (op.find("istore_") != std::string::npos)
-        {
-            int var_index = op[op.length() - 1] - '0';
-            vars[var_index] = list_item_data(list_first(stack));
-            list_erase_first(stack);
-        }
-    }
 }
