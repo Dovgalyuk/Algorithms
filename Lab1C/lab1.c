5a72c5b5a0695266af407f0b6e22594b38163743
duraaach0k <volkserz@yandex.com>
2023-09-23 20:11:15 +0300

    1

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,21 +1,104 @@
-#include <stdio.h>
+﻿
+#define _CRT_SECURE_NO_WARNINGS
 #include "array.h"
+#include <stdio.h>
+#include <iostream>
 
-void task1(Array *arr)
+
+using namespace std;
+int a;
+
+
+void task1(Array* arr)
 {
+    
+    int sumosadkov = 0;
+    for (int i = 0; i <a; i++)
+    {
+        sumosadkov += *(int*)(array_get(arr, i));
+        
+        if ((i+1) % 30 == 0)
+        {
+            
+            printf("%i", sumosadkov);
+            printf("%s", "\t");
+            sumosadkov = 0;
+        }
+    }
+	
 }
 
-void task2(Array *arr)
+
+
+void task2(Array* arr)
 {
+    int c, z, x;
+    c = 0;
+    for (int i = 0; i < a; i++)
+    {
+        if (*(int*)(array_get(arr, i)) > 0)
+        {
+            for (int j = i + 1; j < a; j++)
+                if (*(int*)(array_get(arr, i)) == *(int*)(array_get(arr, j)))
+                {
+                    c += 1;
+                    z = *(int*)(array_get(arr, i));
+                    *(int*)(array_get(arr, j)) = -1;
+                }
+            if (c == 1)
+            {
+                
+                printf("%i", z);
+                printf("%s", "\t");
+                
+            }
+        }
+        c = 0;
+    }
 }
 
 int main()
 {
-    Array *arr = NULL;
+   
+    Array* arr = NULL;
+    scanf("%d", &a);
+    
+    arr = array_create(a, NULL);
+    for (int i = 0; i < a; i++)
+    {
+        int* p = (int*)malloc(sizeof(int)); 
+        *p = rand();
+        array_set(arr, i, p);
+    }
     /* Create array here */
     task1(arr);
     array_delete(arr);
     /* Create another array here */
+    scanf("%i", &a);
+    arr = array_create(a, NULL);
+    for (int i = 0; i < a; i++)
+    {
+        int* p = (int*)malloc(sizeof(int));
+        *p = rand();
+        array_set(arr, i, p);
+    }
     task2(arr);
     array_delete(arr);
+
+   
+    
+   
+
+  
+   
+   
+
+    
+
+   
+   
+    
+    
+    
+    
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,53 @@
+﻿19:06
 #include <stdlib.h>
-#include "array.h"
+#include "C:\Users\kirill\Desktop\array.h"
 
 typedef struct Array
 {
+	int size;
+	int* array;
 } Array;
 
 // create array
-Array *array_create(size_t size, FFree f)
+Array* array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+	if (size > 0) {
+		Array* newarray = (Array*)malloc(sizeof(Array));
+		newarray->size = size;
+		newarray->array = (int*)malloc(sizeof(int) * size);
+		return newarray;
+	}
+	else
+		return malloc(sizeof(Array));
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
+void array_delete(Array* arr)
 {
-    free(arr);
+	free(arr);
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
+Data array_get(const Array* arr, size_t index)
 {
-    return (Data)0;
+	if (index < arr->size) {
+		return arr->array[index];
+	}
+	return (Data)0;
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+void array_set(Array* arr, size_t index, Data value)
 {
+	if (index < arr->size) {
+		arr->array[index] = value;
+	}
+	else return;
 }
 
 // returns array size
-size_t array_size(const Array *arr)
+size_t array_size(const Array* arr)
 {
-    return 0;
+	return arr->size;
+	//return 0;
 }
