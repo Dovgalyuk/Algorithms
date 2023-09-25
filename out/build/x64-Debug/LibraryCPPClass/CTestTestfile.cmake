35ece12a75683aaf2427c12b6a63f96400369cba
Anat <62662868+chooll@users.noreply.github.com>
2023-09-26 01:26:58 +0300

    built

---
+++ out/build/x64-Debug/LibraryCPPClass/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ANAT/Desktop/Algorithms/LibraryCPPClass
+# Build directory: C:/Users/ANAT/Desktop/Algorithms/out/build/x64-Debug/LibraryCPPClass
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
