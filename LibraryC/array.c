84b03db2d3ba157e93026abfb2cfd36a9210a1c7
stmay4 <stasmayorov2004@mail.ru>
2023-10-01 17:57:07 +0300

    withoutleaks

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -23,6 +23,10 @@ Array* array_create(size_t size, FFree f)
 // delete array, free memory
 void array_delete(Array* arr)
 {
+    for (int i = 0; i < arr->size;i++) {
+        free(arr->array[i]);
+        arr->array[i] = NULL;
+    }
     free(arr->array);
     free(arr);
 }
