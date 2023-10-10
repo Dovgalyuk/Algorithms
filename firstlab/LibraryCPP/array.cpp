f0f3c54ce4d648453eb63a8373883f1cc9a217dc
ShinuiMK2092 <kihuge@yandex.ru>
2023-10-10 23:49:51 +0300

    thefirst

---
+++ firstlab/LibraryCPP/array.cpp
@@ -0,0 +1,75 @@
+#include <cstddef> 
+#include "array.h"
+#include <iostream>
+
+
+typedef int Data; 
+
+
+struct Array
+{
+	Data* data; 
+	size_t size; 
+};
+
+Array* array_create(size_t size)
+{
+	Array* arr = new Array;
+	arr->data = new Data[size]; 
+	arr->size = size;
+	return arr;
+}
+
+
+void array_delete(Array* arr)
+{
+	delete[] arr->data; 
+	delete arr;
+}
+
+Data array_get(const Array* arr, size_t index)
+{
+	if (index < arr->size)
+	{
+		return arr->data[index];
+	}
+	return Data();
+}
+
+
+void array_set(Array* arr, size_t index, Data value)
+{
+	if (index < arr->size)
+	{
+		arr->data[index] = value;
+	}
+}
+
+size_t array_size(const Array* arr)
+{
+	return arr->size;
+}
+
+
+int main()
+{
+	size_t size = 10; 
+	Array* arr = array_create(size); 
+
+	
+	for (size_t i = 0; i < size; ++i)
+	{
+		array_set(arr, i, i * 2);
+	}
+
+	
+	for (size_t i = 0; i < size; ++i)
+	{
+		std::cout << "arr[" << i << "] = " << array_get(arr, i) << std::endl;
+	}
+
+	
+	array_delete(arr);
+
+	return 0;
+}
