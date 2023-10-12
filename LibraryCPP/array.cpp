d58f1fda4c7237dcdd6fdc7868fa7257d3609d52
playnikov <playnikov@gmail.com>
2023-10-12 16:59:37 +0300

    lab1

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,12 +2,23 @@
 
 struct Array
 {
+    Data* data;
+    size_t size;
+
+    Array(size_t size) {
+        this->size = size;
+        this->data = new Data[size];
+    }
+
+    ~Array() {
+        delete[] this->data;
+    }
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    return new Array(size);
 }
 
 // delete array, free memory
@@ -19,16 +30,22 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
+    size_t size = array_size(arr);
+    if (index < size)
+        return arr->data[index];
     return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    size_t size = array_size(arr);
+    if (index < size)
+        arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
