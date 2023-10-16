b86747bcc26fff64a05474dc1a6d72fb013a6206
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-10-16 16:24:47 +0300

    Mistake
    Mistake

--- Lab1C/lab1.cpp
+++ Lab1C/lab1.cpp
@@ -10,21 +10,23 @@ using namespace std;
         }
     }
     void MinMax(Array* arr) {
-        array_set(vec, 0, array_get(arr, i));
-        int max = array_get(arr,0);
+        Array* vec = NULL;
+        int max = array_get(arr, 0);
+        
         for (int i = 0; i < array_size(arr); i++) {
             if (array_get(arr, i) > max) {
                 max = array_get(arr,i);
             }
         }
-        Array* vec = NULL;
+        
         for (int i = 0; i < array_size(arr); i++) {
             if (array_get(arr, i) == max) {
                 vec = array_create(1);
                 array_set(vec, 0, i);
                 break;
             }
-        for (int i = 0; i < array_size(arr); i++) {
+        }
+        for (int i = 0; i < array_size(arr) - 1; i++) {
             if (array_get(arr,i) == max) {
                 vec = re_size(vec, i);
             }
@@ -36,39 +38,51 @@ using namespace std;
             cout << max << endl;
         }
     }
-    void Sort() {
+    void Sort(Array* arr) {
         int peregon = 0;
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (car[j] < car[i]) {
-                    peregon = car[i];
-                    car[i] = car[j];
-                    car[j] = peregon;
+        for (int i = 0; i < array_size(arr) - 1; i++) {
+            for (int j = i + 1; j < array_size(arr); j++) {
+                if (array_get(arr,j) < array_get(arr, i)) {
+                    peregon = array_get(arr, i);
+                    array_set(arr, i, array_get(arr, j));
+                    array_set(arr, j,peregon);
                 }
             }
         }
     }
-    int Difference() {
-        Sort();
+    int Difference(Array* arr) {
+        Sort(arr);
         int min = 1000000;
-        vector<int> carChet;
-        for (int i = 0; i < size; i++) {
-            if (car[i] % 2 == 0) {
-                carChet.push_back(car[i]);
+        Array* carChet = NULL;
+        for (int i = 0; i < array_size(arr); i++) {
+            if (array_get(arr,i) % 2 == 0) {
+                carChet = array_create(1);
+                array_set(carChet,0,array_get(arr,i));
+                break;
             }
         }
-        for (int i = 1; i < carChet.size(); i++) {
-            if (carChet[i] - carChet[i - 1] < min && carChet[i] != carChet[i - 1]) {
-                min = carChet[i] - carChet[i - 1];
+        for (int i = 0; i < array_size(arr); i++) {
+            if (array_get(arr, i) % 2 == 0) {
+                carChet = re_size(carChet, array_get(arr, i));
+            }
+        }
+        for (int i = 1; i < array_size(carChet); i++) {
+            if (array_get(carChet,i) - array_get(carChet,i-1) < min && array_get(carChet,i) != array_get(carChet,i-1)) {
+                min = array_get(carChet,i) - array_get(carChet,i-1);
             }
         }
         return min;
     }
     int main() {
+        setlocale(LC_ALL, "Russian");
         Array* arr = NULL;
         size_t size;
 
+        cout << "Введите кол-во автомобилей: ";
         cin >> size;
         arr = array_create(size);
         FillRandom(arr);
+        cout << "Минимальный и максимальный элемент: ";
+        MinMax(arr);
+        cout << "Минимальная разница между скоростями автомобилей: "<<Difference(arr);
     }
