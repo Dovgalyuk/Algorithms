49d67b46a06d3d0ff14f08d55b21d7cd85775c78
heheheee21 <MButovskiy@gmail.com>
2023-10-17 13:42:35 +0300

    lab2,1

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,49 @@
 #include "stack.h"
+#include "vector.h"
 
 struct Stack
 {
+    Vector* vector;
 };
 
 Stack *stack_create()
 {
-    return new Stack;
+    Stack* stack = new Stack;
+    stack->vector = vector_create();
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    vector_delete(stack->vector);
     delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    vector_resize(stack->vector, vector_size(stack->vector) + 1);
+    vector_set(stack->vector, vector_size(stack->vector) - 1, data);
 }
 
 Data stack_get(const Stack *stack)
 {
+    size_t stackSize = vector_size(stack->vector);
+    if (stackSize > 0)
+    {
+        return vector_get(stack->vector, stackSize - 1);
+    }
     return (Data)0;
 }
 
 void stack_pop(Stack *stack)
 {
+    if (!stack_empty(stack))
+    {
+        vector_resize(stack->vector, vector_size(stack->vector) - 1);
+    }
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return vector_size(stack->vector) == 0;
 }
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,33 +2,64 @@
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t sizeMax;
 };
 
 Vector *vector_create()
 {
-    return new Vector;
+    Vector* vector = new Vector;
+    vector->data = nullptr;
+    vector->size = 0;
+    vector->sizeMax = 0;
+    return vector;
 }
 
 void vector_delete(Vector *vector)
 {
-    // TODO: free vector internals
+    if (vector->data != nullptr)
+    {
+        delete[] vector->data;
+    }
+    
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
+    if (index < vector->size)
+    {
+        return vector->data[index];
+    }
     return (Data)0;
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    if (index < vector->size)
+    {
+        vector->data[index] = value;
+    }
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size > vector->sizeMax)
+    {
+        vector->sizeMax = size * 2;
+        Data* newData = new Data[vector->sizeMax];
+        for (size_t i = 0; i < vector->size; i++)
+        {
+            newData[i] = vector->data[i];   
+        }
+        delete[] vector->data;
+        vector->data = newData;
+    }
+    vector->size = size;
 }
