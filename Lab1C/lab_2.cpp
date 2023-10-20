612c0be1ed992a943b4b02a83a6394c66c3c3500
Timofey Suryadov <timsuryadov@gmail.com>
2023-10-20 12:19:02 +0300

    lab_2 upd_7

---
+++ Lab1C/lab_2.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <cctype>
+
+#include "stack.h"
+
+using namespace std;
+
+bool is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int get_operator_priority(char op) {
+    switch (op) {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    default:
+        return 0;  // Если не оператор
+    }
+}
+
+string infix_to_RPN(string& infix) {
+    string result;
+    Stack* operators = stack_create();
+
+    for (char c : infix) {
+        if (isalnum(c))
+            result += c;  // Если символ - цифра или буква, добавляем к выходной строке
+
+        else if (is_operator(c)) {
+            while (!stack_empty(operators) && get_operator_priority(stack_get(operators)) >= get_operator_priority(c)) {
+                result += stack_get(operators);
+                stack_pop(operators);
+            }
+            stack_push(operators, c);
+        }
+        else if (c == '(') {
+            stack_push(operators, c);
+        }
+        else if (c == ')') {
+            while (!stack_empty(operators) && stack_get(operators) != '(') {
+                result += stack_get(operators);
+                stack_pop(operators);
+            }
+            stack_pop(operators);  // Убираем '(' со стека
+        }
+    }
+
+    while (!stack_empty(operators)) {
+        result += stack_get(operators);
+        stack_pop(operators);
+    }
+
+    stack_delete(operators);
+
+    return result;
+}
+
+int calculate_expression(string& postfix) {
+    Stack* intermediate_values = stack_create();
+    
+    int temp_value = 0;
+    int temp_value_2 = 0;
+
+    for (char c : postfix) {
+        if (!is_operator(c))
+            stack_push(intermediate_values, c - 48);
+        else {
+            switch (c)
+            {
+            case('+'):
+                temp_value = stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+                temp_value += stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+
+                stack_push(intermediate_values, temp_value);
+                break;
+
+            case('-'):
+                temp_value_2 = stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+                temp_value = (stack_get(intermediate_values));
+                stack_pop(intermediate_values);
+
+                stack_push(intermediate_values, temp_value - temp_value_2);
+                break;
+
+            case('*'):
+                temp_value = stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+                temp_value *= stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+
+                stack_push(intermediate_values, temp_value);
+                break;
+
+            case('/'):
+                temp_value_2 = stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+                temp_value = stack_get(intermediate_values);
+                stack_pop(intermediate_values);
+
+                stack_push(intermediate_values, temp_value / temp_value_2);
+                break;
+            }
+        }
+    }
+    temp_value = stack_get(intermediate_values);
+    stack_delete(intermediate_values);
+
+    return temp_value;
+}
+
+int main()
+{
+	string input_data;
+
+	cin >> input_data;
+
+    input_data = infix_to_RPN(input_data);
+
+    cout << input_data;
+
+    cout << endl << calculate_expression(input_data);
+}
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,54 @@
 #include "stack.h"
+#include "list.h"
 
-struct Stack
-{
+struct Stack{
+    List* list = nullptr;
+    ListItem* last_item = nullptr;
 };
 
-Stack *stack_create()
-{
-    return new Stack;
+Stack *stack_create(){
+    Stack* new_stack = new Stack;
+    List* new_list = list_create();
+
+    new_stack->list = new_list;
+
+    return new_stack;
 }
 
-void stack_delete(Stack *stack)
-{
-    // TODO: free stack elements
+void stack_delete(Stack *stack){
+    if (stack->list != nullptr)
+        list_delete(stack->list);
+
+    stack->list = nullptr;
+    stack->last_item = nullptr;
+
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
-{
+void stack_push(Stack *stack, Data data){
+    stack->last_item = list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack)
-{
-    return (Data)0;
+Data stack_get(const Stack *stack){
+    return list_item_data(stack->last_item);
 }
 
-void stack_pop(Stack *stack)
-{
+void stack_pop(Stack *stack){
+    if (stack->last_item != nullptr && list_item_prev(stack->last_item) != nullptr){
+        stack->last_item = list_item_prev(stack->last_item);
+
+        list_erase_next(stack->list, stack->last_item);
+    }
+    else if (stack->last_item != nullptr && list_item_prev(stack->last_item) == nullptr)
+    {
+        list_erase_first(stack->list);
+        stack->last_item = nullptr;
+    }
 }
 
-bool stack_empty(const Stack *stack)
-{
-    return true;
+bool stack_empty(const Stack *stack){
+    if (stack->last_item == nullptr)
+        return true;
+    else
+        return false;
 }
