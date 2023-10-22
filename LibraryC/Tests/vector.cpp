33421d40dcf75339508f6d0de37b815c29f9f113
duraaach0k <volkserz@yandex.com>
2023-10-22 19:31:39 +0300

    9

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "vector.h"
 
-void myfree(void *p)
+void myfree(void* p)
 {
     delete (int*)p;
 }
 
-int vector_get_int(Vector *v, size_t i)
+int vector_get_int(Vector* v, size_t i)
 {
     return *(int*)vector_get(v, i);
 }
 
 int main()
 {
-    Vector *vector = vector_create(myfree);
+    Vector* vector = vector_create(myfree);
 
     vector_resize(vector, 5);
     if (vector_size(vector) != 5)
@@ -22,10 +22,10 @@ int main()
         return 1;
     }
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
         vector_set(vector, i, new int(i));
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
     {
         if (vector_get_int(vector, i) != (int)i)
         {
@@ -40,9 +40,10 @@ int main()
         std::cout << "Invalid resize\n";
         return 1;
     }
-
+    for (size_t i = 0; i < vector_size(vector); ++i)
+        vector_set(vector, i, new int(i));
     std::cout << "Vector: ";
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
         std::cout << vector_get_int(vector, i) << " ";
     std::cout << "\n";
 
@@ -53,7 +54,7 @@ int main()
         return 1;
     }
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
     {
         if (vector_get_int(vector, i) != (int)i)
         {
@@ -63,19 +64,19 @@ int main()
     }
 
     std::cout << "Vector: ";
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
         std::cout << vector_get_int(vector, i) << " ";
     std::cout << "\n";
 
     // Performance test
-    for (int i = 1 ; i <= 10000000 ; ++i)
+    for (int i = 1; i <= 10000000; ++i)
     {
         vector_resize(vector, i);
         vector_set(vector, i - 1, new int(i));
     }
 
     long long sum = 0;
-    for (int i = 0 ; i < 10000000 ; ++i)
+    for (int i = 0; i < 10000000; ++i)
         sum += vector_get_int(vector, i);
 
     std::cout << sum << "\n";
