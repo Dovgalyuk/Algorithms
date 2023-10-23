4d07c51a791e30dae657bda8b6056ea06320caf6
glamYq <49494952+glamYq@users.noreply.github.com>
2023-10-23 14:51:51 +0300

    lab1 test

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "array.h"
 
-void task1(Array *arr)
+void task1(Array* arr, size_t size)
 {
+    for (size_t index = 0; index < array_size(arr); index++)
+    {
+        array_set(arr, index, rand() % 100);
+    }
+
+    int sum = 0;
+    for (size_t index = 0; index < array_size(arr); index++)
+    {
+        int num = array_get(arr, index);
+        if (num % 2 == 0 && num > 0)
+        {
+            sum += num;
+        }
+    }
+
+    printf("sum of even number %d\n", sum);
 }
 
-void task2(Array *arr)
+void task2(Array* arr, size_t size)
 {
+    for (size_t index = 0; index < array_size(arr); index++)
+    {
+        array_set(arr, index, rand() % 100);
+    }
+
+    int count = 0;
+    float sum = 0;
+
+    for (size_t index = 0; index < array_size(arr); index++)
+    {
+        sum += array_get(arr, index);
+    }
+
+    float average = sum / array_size(arr);
+
+    printf("Array elements whose value is less than the arithmetic mean:");
+    for (size_t index = 0; index < array_size(arr); index++)
+    {
+        if (array_get(arr, index) < average)
+        {
+            printf("%d ", array_get(arr, index));
+            count++;
+        }
+    }
+    printf("\n");
 }
 
-int main()
-{
-    Array *arr = NULL;
-    /* Create array here */
-    task1(arr);
-    array_delete(arr);
-    /* Create another array here */
-    task2(arr);
-    array_delete(arr);
+int main() {
+    size_t size;
+
+    scanf("%zu", &size);
+
+    Array *arr1 = array_create(size);
+    task1(arr1, size);
+    array_delete(arr1);
+
+    Array *arr2 = array_create(size);
+    task2(arr2, size);
+    array_delete(arr2);
+
+    return 0;
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,51 @@
-#include <stdlib.h>
-#include "array.h"
+#include <stdlib.h> 
 
-typedef struct Array
-{
+typedef struct {
+    int* data;
+    size_t size;
 } Array;
 
-// create array
-Array *array_create(size_t size, FFree f)
-{
-    return malloc(sizeof(Array));
+Array* array_create(size_t size) {
+    Array* arr = (Array*)malloc(sizeof(Array));
+    if (arr != NULL) {
+        arr->data = (int*)malloc(size * sizeof(int));
+        if (arr->data != NULL) {
+            arr->size = size;
+            return arr;
+        } else {
+            free(arr);
+            return NULL;
+        }
+    } else {
+        return NULL;
+    }
 }
 
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    free(arr);
+void array_delete(Array* arr) {
+    if (arr != NULL) {
+        free(arr->data);
+        free(arr);
+    }
 }
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+int array_get(const Array* arr, size_t index) {
+    if (arr != NULL && index < arr->size) {
+        return arr->data[index];
+    } else {
+        return 0;
+    }
 }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, int value) {
+    if (arr != NULL && index < arr->size) {
+        arr->data[index] = value;
+    }
 }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr) {
+    if (arr != NULL) {
+        return arr->size;
+    } else {
+        return 0;
+    }
 }
