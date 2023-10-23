f6fd5e136b257ecc11307b029c77451343e93d80
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-10-23 17:55:25 +0300

    MinMax
    MinMax

--- Lab1C/lab1.cpp
+++ Lab1C/lab1.cpp
@@ -6,31 +6,25 @@ using namespace std;
     void FillRandom(Array* arr) {
         size_t size = array_size(arr);
         for (size_t i = 0; i < size; i++) {
-            array_set(arr, i,static_cast<int>(rand() % (250 - 100 + 1) + 100));
+            array_set(arr, i, static_cast<int>(rand() % (250 - 100 + 1) + 100));
         }
     }
     void MinMax(Array* arr) {
-        int fix = 0;
 
         int max = array_get(arr, 0);
-        int max1 = 0;
-        int max2 = 0;
+        size_t max1 = 0;
+        size_t max2 = 0;
 
         for (size_t i = 0; i < array_size(arr); i++) {
             if (array_get(arr, i) > max) {
                 max = array_get(arr, i);
+                max1 = i;
             }
         }
 
         for (size_t i = 0; i < array_size(arr); i++) {
-            if (array_get(arr, i) == max && fix == 0) {
-                max1 = i;
-                fix += 1;
-            }
-            else if (array_get(arr, i) == max && fix == 1) {
-                max2 = i;
-                fix += 1;
-                break;
+            if (array_get(arr, i) == max) {
+                    max2 = i;
             }
         }
         if (max2 != 0) {
@@ -55,10 +49,10 @@ using namespace std;
     int Difference(Array* arr) {
         Sort(arr);
         int min = 1000000;
-        Array* carChet = NULL;
+        Array* carChet = NULL; 
+        carChet = array_create(1);
         for (size_t i = 0; i < array_size(arr); i++) {
             if (array_get(arr,i) % 2 == 0) {
-                carChet = array_create(1);
                 array_set(carChet,0,static_cast<int>(array_get(arr,i)));
                 break;
             }
