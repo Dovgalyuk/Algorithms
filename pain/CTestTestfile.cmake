d129ffae7068d58bf9549fd08452c9164a45f72f
BenjaminArcher13 <benjaminarcher13@mail.ru>
2023-10-24 16:38:40 +0300

    lab2.3

--- pain/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Учёба/2 курс/стукрутрные алгоритмы/git/Algorithms
-# Build directory: C:/Учёба/2 курс/стукрутрные алгоритмы/git/Algorithms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPP")
-subdirs("Lab2")
--- pain/Lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Учёба/2 курс/стукрутрные алгоритмы/git/Algorithms/Lab2
-# Build directory: C:/Учёба/2 курс/стукрутрные алгоритмы/git/Algorithms/build/Lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
