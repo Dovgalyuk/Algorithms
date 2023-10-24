083c38519c67b68e380f098c57143cd30c0ee827
Govnash213 <evogov92@gmail.com>
2023-10-24 18:54:48 +0300

    array + 1 and 2 задания

---
+++ Lab1CPP/lab1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <ctime>
+#include "array.h"
+#include "math.h"
+
+using namespace std;
+
+struct Array
+{
+    size_t size;
+    Data* data;
+};
+
+void task1(Array *arr)
+{
+    for (size_t i = 0; i < array_size(arr); i++)
+    {
+            array_set(arr, i, rand());
+    }
+
+    int count = 0;
+
+    cout << "Array: ";
+    for (size_t i = 0; i < array_size(arr); i++)
+    {
+        cout << array_get(arr, i);
+        if (i != array_size(arr)-1) cout << ' ';
+        int num = array_get(arr, i);
+        for (; num!=0; num/=10) {
+            count += num%10;
+        }
+    }
+    cout << "\nSum of all digits of all array numbers - " << count << endl;
+}
+
+void task2(Array *arr)
+{
+    for (size_t i = 0; i < array_size(arr); i++)
+        array_set(arr, i, rand() % 202 - 101);
+
+    cout << "Array: ";
+    for (size_t i = 0; i < array_size(arr); i++)
+        cout << array_get(arr, i) << ' ';
+
+    Array *tempArr = array_create(array_size(arr));
+    size_t evenElemCounter = 0;
+    for (size_t i = 0; i < array_size(arr); i++)
+        if (arr->data[i] % 2 == 0)
+            array_set(tempArr, evenElemCounter++, array_get(arr, i));
+    
+    if (evenElemCounter < 2)
+    {
+        cout << endl << "Not enough even elements.";
+        return;
+    }
+
+    int minDifference = abs(array_get(tempArr, 0) - array_get(tempArr, 1));
+    int curDifference;
+    for (size_t i = 0; i < evenElemCounter - 1; i++)
+    {
+        for (size_t j = i + 1; j < evenElemCounter; j++)
+        {
+            curDifference = abs(array_get(tempArr, i) - array_get(tempArr, j));
+            if (minDifference > curDifference)
+                minDifference = curDifference;
+        }
+    }
+    array_delete(tempArr);
+
+    cout << endl << "Minimum difference of even elements: " << minDifference;
+}
+
+int main()
+{
+    srand((int)time(NULL));
+
+    Array *arr = NULL;
+    size_t size;
+
+    /* Create array here */
+    cout << "Enter array size - ";
+    cin >> size;
+    arr = array_create(size);
+    
+    task1(arr);
+    array_delete(arr);
+
+    /* Create another array here */
+    cout << "\nEnter array size - ";
+    cin >> size;
+    arr = array_create(size);
+    
+    task2(arr);
+    array_delete(arr);
+}
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,43 @@
 
 struct Array
 {
+    size_t size;
+    Data* data;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    Array* arr = new Array;
+    arr->size = size;
+    arr->data = new Data[size];
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    delete[] arr->data;
     delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (index >= arr->size)
+        return 0;
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index < arr->size)
+        arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
