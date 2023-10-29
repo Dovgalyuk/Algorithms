24525daee4b806daff00a35996cdd167c4abc10b
Nikita Ivanov <estarese@users.noreply.github.com>
2023-10-29 16:36:38 +0300

    Rework

--- out/build/x64-Debug/LibraryCPPClass/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/Algorithms/LibraryCPPClass
-# Build directory: C:/Users/user/Algorithms/out/build/x64-Debug/LibraryCPPClass
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
