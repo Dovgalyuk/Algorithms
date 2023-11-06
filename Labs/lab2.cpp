18a969ff658b262a00457b86ed838042721b8edc
Timofey Kirikov <82898998+kirikovsky@users.noreply.github.com>
2023-11-06 13:26:17 +0300

    Update lab2.cpp

--- Labs/lab2.cpp
+++ Labs/lab2.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stack>
 #include <sstream>
-#include <unordered_map>
 #include "stack.h"
+#include "vector.h"
 
 using namespace std;
 
 int main() {
     string filename = "input.txt";
     Stack* stack = stack_create();
-    unordered_map<char, int> registers = { {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0} };
+    Vector* strings = vector_create();
+    Vector* registers = vector_create();
+    vector_resize(strings, 4);
+    vector_resize(registers, 4);
+
+    vector_set(registers, 0, 0);
+    vector_set(registers, 1, 0);
+    vector_set(registers, 2, 0);
+    vector_set(registers, 3, 0);
+
+    vector_set(strings, 0, 'A')
+    vector_set(strings, 1, 'B');
+    vector_set(strings, 2, 'C');
+    vector_set(strings, 3, 'D');
 
     ifstream inputFile(filename);
     if (!inputFile) {
-        cout << "Failed to open input file." << endl;
-        return 0;
+        cerr << "Failed to open input file." << endl;
+        return;
     }
 
     string line;
@@ -32,7 +44,8 @@ int main() {
             stringstream ss(line.substr(5));
             if (isalpha(ss.peek())) {
                 ss >> param;
-                value = registers[param];
+                for (int i = 0; i < 4; i++) if (param == vector_get(strings, i)) param = i;
+                value = vector_get(registers, param);
             }
             else {
                 ss >> value;
@@ -42,20 +55,19 @@ int main() {
         else if (line.substr(0, 3) == "POP") {
             stringstream ss(line.substr(4));
             ss >> param;
+            for (int i = 0; i < 4; i++) if (param == vector_get(strings, i)) param = i;
             value = stack_get(stack);
             stack_pop(stack);
-            registers[param] = value;
+            vector_set(registers, param, value);
         }
         else {
-            cout << "Invalid operation: " << line << endl;
-            return 0;
+            cerr << "Invalid operation: " << line << endl;
+            return;
         }
     }
 
     inputFile.close();
     stack_delete(stack);
-
-    for (std::pair<char, int> element : registers) cout << element.first << " = " << element.second << endl;
-
+    for (int i = 0; i < 4; i++) cout << vector_get(strings, i) << " = " << vector_get(registers, i)] << endl;
     return 0;
 }
