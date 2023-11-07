bc34e4b3cc775ba67bf68079cdc988162855a3ed
Ynic2 <ynic.prokopenko@yandex.ru>
2023-11-07 15:28:53 +0300

    lab3 2

--- Labs/lab3.cpp
+++ Labs/lab3.cpp
@@ -5,15 +5,15 @@
 #include "queue.h"
 
 using namespace std;
+typedef vector<vector<int>> Graph;
 
-
-int shortestPath(vector<vector<int>> graph, int start, int finish) {
+int shortestPath(const Graph& graph, int start, int finish) {
     size_t n = graph.size();
     vector<int> distance(n, INT_MAX);
     Queue* que = queue_create();
 
     distance[start] = 0;
-    queue_insert(que,(Data)start);
+    queue_insert(que, (Data)start);
 
     while (!queue_empty(que)) {
         Data i = queue_get(que);
@@ -22,14 +22,11 @@ int shortestPath(vector<vector<int>> graph, int start, int finish) {
         for (size_t j = 0; j < n; ++j) {
             if (graph[i][j] != 0 && distance[j] == INT_MAX) {
                 distance[j] = distance[i] + 1;
-                if (queue_empty(que)) {
-                    que = queue_create();
-                }
                 queue_insert(que, (Data)j);
             }
         }
     }
-
+    queue_delete(que);
     if (distance[finish] == INT_MAX) {
         return -1;
     }
@@ -38,12 +35,12 @@ int shortestPath(vector<vector<int>> graph, int start, int finish) {
 
 int main() {
     ifstream input("input.txt");
-    
+
 
     int n, start, finish;
     input >> n >> start >> finish;
 
-    vector<vector<int>> graph;
+    Graph graph;
 
     for (int i = 0; i < n; ++i) {
         graph.push_back({});
