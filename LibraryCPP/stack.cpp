7375ad0b7fa9b437a1a3b64926ef450fc227ecca
Тимофеев Иван <104462765+Morivan@users.noreply.github.com>
2023-11-09 18:53:19 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,44 +1,49 @@
 #include "stack.h"
-#include "vector.h"
+#include "list.h"
 
-struct Stack
-{
-    Vector* vector;
+struct Stack{
+    List* list = nullptr;
+    ListItem* last_item = nullptr;
 };
 
-Stack* stack_create()
-{
-    Stack* stack = new Stack;
-    stack->vector = vector_create();
-    return stack;
+Stack *stack_create(){
+    Stack* new_stack = new Stack;
+
+    new_stack->list = list_create();
+
+    return new_stack;
 }
 
-void stack_delete(Stack* stack)
-{
-    vector_delete(stack->vector);
+void stack_delete(Stack *stack){
+    if (stack->list != nullptr)
+        list_delete(stack->list);
+
+    stack->list = nullptr;
+    stack->last_item = nullptr;
+
     delete stack;
 }
 
-void stack_push(Stack* stack, Data data)
-{
-    size_t index = vector_size(stack->vector);
-    vector_resize(stack->vector, index + 1);
-    vector_set(stack->vector, index, data);
+void stack_push(Stack *stack, Data data){
+    stack->last_item = list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack* stack)
-{
-    return vector_get(stack->vector, vector_size(stack->vector) - 1);
+Data stack_get(const Stack *stack){
+    ListItem* first_item = list_first(stack->list);
+
+    if (first_item != nullptr)
+        return list_item_data(first_item);
+    else
+        return (Data)0;
 }
 
-void stack_pop(Stack* stack)
-{
-    vector_resize(stack->vector, vector_size(stack->vector) - 1);
+void stack_pop(Stack *stack){
+    stack->last_item = list_erase_first(stack->list);
 }
 
-bool stack_empty(const Stack* stack)
-{   
-    size_t size = vector_size(stack->vector);
-    return size == 0;
+bool stack_empty(const Stack *stack){
+    if (stack->last_item == nullptr)
+        return true;
+    else
+        return false;
 }
-
