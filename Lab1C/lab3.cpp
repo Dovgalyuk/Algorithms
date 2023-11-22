791414cd9e43a9bc50f0fd97941e13131822ce83
stmay4 <stasmayorov2004@mail.ru>
2023-11-22 11:33:15 +0300

    lab3Update

--- Lab1C/lab3.cpp
+++ Lab1C/lab3.cpp
@@ -7,85 +7,62 @@
 
 using namespace std;
 typedef vector<vector<size_t>> graph;
-size_t FindRootIndexForNode(const graph& g, size_t node, size_t start = 0) {
-	if (!g.empty() && (start < g.size())) {
-		for (size_t i = start; i < g.size(); i++) {
-			if (g[i].size() > 0)
-				for (size_t j = 0; j < g[i].size(); j++)
-					if (g[i][j] == node)
-						return i;
-		}
+size_t GetIndex(const string& node, vector<string>& points) {
+	size_t index = SIZE_MAX;
+
+	for (size_t j = 0;j < points.size();j++) {
+		if (points[j] == node)
+			index = j;
 	}
-	return SIZE_MAX;
+	if (index == SIZE_MAX)
+	{
+		index = points.size();
+		points.push_back(node);
+	}
+	return index;
+}
+void GetNodes(const string& str, string& node1, string& node2) {
+	node1 = "";
+	node2 = "";
+	size_t i = 0;
+	while (str[i] != ' ') {
+		node1 += str[i];
+		i++;
+	}
+	i++;
+	while (i < str.length()) {
+		if (str[i] != ' ')
+			node2 += str[i];
+		i++;
+	}
 }
 graph InputGraph(size_t* start, size_t* end, size_t* size, vector<string>& points) {
 	graph graph;
 	string str = "";
 	string node1 = "";
 	string node2 = "";
+	size_t index1, index2;
 	bool flag = true;
 	cout << "input 'Enter' for exit from Input" << endl;
 	while (flag) {
 		cout << "input node1-node2: ";
 		getline(cin, str);
 		if (str != "") {
-			node1 = "";
-			node2 = "";
-			size_t i = 0;
-			while ((str[i] != '\0') && (str[i] != ' ')) {
-				if (str[i] != ' ')
-					node1 += str[i];
-				i++;
-			}
-			i++;
-			while (i < str.length()) {
-				if (str[i] != ' ')
-					node2 += str[i];
-				i++;
-			}
+			GetNodes(str, node1, node2);
 
-			size_t index1 = SIZE_MAX;
-			size_t index2 = SIZE_MAX;
+			index1 = GetIndex(node1, points);
+			index2 = GetIndex(node2, points);
 
-			for (size_t j = 0;j < points.size();j++) {
-				if (points[j] == node1)
-					index1 = j;
-				if (points[j] == node2)
-					index2 = j;
-			}
-			if (index1 == SIZE_MAX)
-			{
-				index1 = points.size();
-				points.push_back(node1);
-			}
-			if (index2 == SIZE_MAX)
-			{
-				index2 = points.size();
-				points.push_back(node2);
-			}
 			graph.resize(points.size());
+
 			graph[index1].push_back(index2);
+			graph[index2].push_back(index1);
 		}
 		else flag = false;
 	}
-
 	cout << "input 'start' 'end' positions: ";
 	getline(cin, str);
-	node1 = "";
-	node2 = "";
-	size_t i = 0;
-	while ((str[i] != '\0') && (str[i] != ' ')) {
-		node1 += str[i];
-		i++;
-	}
-	i++;
-	while (i < str.length()) {
-		node2 += str[i];
-		i++;
-	}
-
-	size_t index1 = SIZE_MAX;
-	size_t index2 = SIZE_MAX;
+	GetNodes(str, node1, node2);
 
 	for (size_t j = 0;j < points.size();j++) {
 		if (points[j] == node1)
@@ -98,21 +75,6 @@ graph InputGraph(size_t* start, size_t* end, size_t* size, vector<string>& point
 	*end = index2;
 
 	return graph;
-}
-vector<size_t> findNodes(const graph& g, size_t node) {
-	vector<size_t> nodes;
-	size_t size = g[node].size();
-	if (size > 0) {
-		for (size_t i = 0;i < size;i++) {
-			nodes.push_back(g[node][i]);
-		}
-	}
-	size_t index = FindRootIndexForNode(g, node);
-	while (index != SIZE_MAX) {
-		nodes.push_back(index);
-		index = FindRootIndexForNode(g, node, index + 1);
-	}
-	return nodes;
 }
 vector<size_t> BFS(const graph& g, size_t node, size_t goalNode, size_t size) {
 	vector<bool> visited(size, false);
@@ -130,26 +92,23 @@ vector<size_t> BFS(const graph& g, size_t node, size_t goalNode, size_t size) {
 		if (node == goalNode) {
 			break;
 		}
-		vector<size_t> nodes = findNodes(g, node);
-		if (nodes.size() > 0) {
-			for (auto n : nodes) {
-				if (!visited[n]) {
-					visited[n] = true;
-					parent[n] = node;
-					size_t* q = (size_t*)malloc(sizeof(size_t));
-					*q = n;
-					queue_insert(queue, q);
-				}
+		for (auto n : g[node]) {
+			if (!visited[n]) {
+				visited[n] = true;
+				parent[n] = node;
+				size_t* q = (size_t*)malloc(sizeof(size_t));
+				*q = n;
+				queue_insert(queue, q);
 			}
 		}
 	}
 	queue_delete(queue);
 	return parent;
 }
-void PrintWay(const vector<size_t>& parent, const vector<string>& points, size_t node, string* message) {
+void PrintWay(const vector<size_t>& parent, const vector<string>& points, size_t node, string& message) {
 	if (parent[node] != node)
 		PrintWay(parent, points, parent[node], message);
-	*message += points[node] + " ";
+	message += points[node] + " ";
 }
 int main()
 {
@@ -159,7 +118,7 @@ int main()
 	vector<size_t> parent = BFS(g, start, end, size);
 	ofstream fout("Way.txt");
 	string message = "";
-	PrintWay(parent, points, end, &message);
+	PrintWay(parent, points, end, message);
 	fout << message;
 	fout.close();
 }
