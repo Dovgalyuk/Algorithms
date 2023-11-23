211b21cb4ed6c414cf9ba29f52d9bc6b2397baec
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2023-11-23 12:59:34 +0300

    Update binaryHeap.cpp

--- LibraryCPP/Tests/binaryHeap.cpp
+++ LibraryCPP/Tests/binaryHeap.cpp
@@ -2,25 +2,18 @@
 #include "binaryHeap.h"
 #include "huffmanTree.h"
 
-// Функция-компаратор и деструктор для тестирования
-int testHuffmanNodeComparator(const void* a, const void* b) {
-    HuffmanNode* nodeA = (HuffmanNode*)a;
-    HuffmanNode* nodeB = (HuffmanNode*)b;
-    return (int)(huffman_getNodeWeight(nodeA) - huffman_getNodeWeight(nodeB));
-}
-
-void testHuffmanNodeDestructor(void* node) {
-    huffman_deleteTree((HuffmanNode*)node);
-}
-
-void testBinaryHeap() {
+int main() {
     // Создаем кучу
-    BinaryHeap* heap = binaryHeap_create(10, testHuffmanNodeComparator, testHuffmanNodeDestructor);
+    BinaryHeap* heap = binaryHeap_create(10, huffmanNodeComparator, huffmanNodeDestructor);
 
     // Тест 1: Вставка элементов и проверка минимального элемента
-    binaryHeap_insert(heap, huffman_createLeafNode('a', 5));
-    binaryHeap_insert(heap, huffman_createLeafNode('b', 3));
-    binaryHeap_insert(heap, huffman_createLeafNode('c', 8));
+    HuffmanNode* nodeA = huffman_createLeafNode('a', 5);
+    HuffmanNode* nodeB = huffman_createLeafNode('b', 3);
+    HuffmanNode* nodeC = huffman_createLeafNode('c', 8);
+
+    binaryHeap_insert(heap, nodeA);
+    binaryHeap_insert(heap, nodeB);
+    binaryHeap_insert(heap, nodeC);
 
     std::cout << "Тест 1: Минимальный элемент после вставки - ";
     HuffmanNode* minNode = (HuffmanNode*)binaryHeap_getMin(heap);
@@ -31,10 +24,20 @@ void testBinaryHeap() {
         std::cout << "Ошибка" << std::endl;
     }
 
-    // Тест 2: Извлечение минимального элемента
-    binaryHeap_extractMin(heap);
-    minNode = (HuffmanNode*)binaryHeap_getMin(heap);
+    // Тест 2: Извлечение минимального элемента и его удаление
+    minNode = (HuffmanNode*)binaryHeap_extractMin(heap);
     std::cout << "Тест 2: Минимальный элемент после извлечения - ";
+    if (huffman_getNodeChar(minNode) == 'b' && huffman_getNodeWeight(minNode) == 3) {
+        std::cout << "Успех" << std::endl;
+    }
+    else {
+        std::cout << "Ошибка" << std::endl;
+    }
+    huffman_deleteTree(minNode); // Удаление извлеченного узла
+
+    // Тест 3: Проверка следующего минимального элемента
+    minNode = (HuffmanNode*)binaryHeap_getMin(heap);
+    std::cout << "Тест 3: Следующий минимальный элемент - ";
     if (huffman_getNodeChar(minNode) == 'a' && huffman_getNodeWeight(minNode) == 5) {
         std::cout << "Успех" << std::endl;
     }
@@ -42,8 +45,8 @@ void testBinaryHeap() {
         std::cout << "Ошибка" << std::endl;
     }
 
-    // Тест 3: Размер кучи
-    std::cout << "Тест 3: Размер кучи - ";
+    // Тест 4: Размер кучи после извлечения одного элемента
+    std::cout << "Тест 4: Размер кучи после извлечения одного элемента - ";
     if (binaryHeap_getSize(heap) == 2) {
         std::cout << "Успех" << std::endl;
     }
@@ -52,11 +55,7 @@ void testBinaryHeap() {
     }
 
     // Очищаем кучу
-    huffman_deleteTree(minNode);
     binaryHeap_delete(heap);
-}
 
-int main() {
-    testBinaryHeap();
     return 0;
 }
