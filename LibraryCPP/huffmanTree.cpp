2790a770dd20967f6cb060625ed82a932d754305
Sergey Antonov <59932798+Sergeo-Sss@users.noreply.github.com>
2023-11-23 14:08:15 +0300

    Update huffmanTree.cpp

--- LibraryCPP/huffmanTree.cpp
+++ LibraryCPP/huffmanTree.cpp
@@ -68,7 +68,7 @@ unsigned long long int huffman_getNodeWeight(HuffmanNode* node)
 
 HuffmanNode* huffman_deleteTree(HuffmanNode* node)
 {
-    if (!node)
+    if (node)
     {
         huffman_deleteTree(node->leftNode);
         huffman_deleteTree(node->rightNode);
