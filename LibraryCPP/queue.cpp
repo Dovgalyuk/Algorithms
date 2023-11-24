cc17d7cd2881c63d7909859a9d1c2ebba37d96f4
Дмитрий <Dima.Rumyantsev2017@yandex.ru>
2023-11-24 14:58:16 +0300

    upd3buf

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -24,11 +24,42 @@ Queue* queue_create() {
 void queue_delete(Queue* queue) {
     delete queue;
 }
-
 void queue_insert(Queue* queue, Data data) {
-    vector_resize(queue->vector, queue->rear + 1); // Увеличиваем размер вектора при вставке элемента
+    size_t current_size = vector_size(queue->vector);
+
+    if (current_size == 0) {
+        vector_resize(queue->vector, 1);
+        vector_set(queue->vector, queue->rear, data);
+        queue->rear++;
+        return;
+    }
+
+    size_t next_rear = (queue->rear + 1) % current_size;
+
+    if (next_rear == queue->front) {
+        size_t new_size = current_size * 2;
+        Vector* resized_vector = vector_create();
+        vector_resize(resized_vector, new_size);
+
+        for (size_t i = queue->front, j = 0; i < current_size; ++i, ++j) {
+            vector_set(resized_vector, j, vector_get(queue->vector, i));
+        }
+
+        if (queue->front != 0) {
+            for (size_t i = 0, j = current_size - queue->front; i < queue->front; ++i, ++j) {
+                vector_set(resized_vector, j, vector_get(queue->vector, i));
+            }
+        }
+
+        vector_delete(queue->vector);
+        queue->vector = resized_vector;
+        queue->front = 0;
+        queue->rear = current_size;
+        current_size = new_size;
+    }
+
     vector_set(queue->vector, queue->rear, data);
-    queue->rear++;
+    queue->rear = (queue->rear + 1) % current_size;
 }
 
 Data queue_get(const Queue* queue) {
