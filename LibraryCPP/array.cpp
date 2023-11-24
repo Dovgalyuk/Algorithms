5a758d3f90b31f97205980a00c9ef42d7089cfc6
ShinuiMK2092 <kihuge@yandex.ru>
2023-11-25 02:56:31 +0300

    labathefirst

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,30 +1,18 @@
 #include <cstddef> 
 #include "array.h"
 #include <iostream>
-
 using namespace std;
-
-typedef int Data; 
-
-
-struct Array
-{
-	Data* data; 
-	size_t size; 
-};
-
 Array* array_create(size_t size)
 {
 	Array* arr = new Array;
-	arr->data = new Data[size]; 
+	arr->data = new Data[size];
 	arr->size = size;
 	return arr;
 }
 
-
 void array_delete(Array* arr)
 {
-	delete[] arr->data; 
+	delete[] arr->data;
 	delete arr;
 }
 
@@ -36,7 +24,6 @@ Data array_get(const Array* arr, size_t index)
 	}
 	return Data();
 }
-
 
 void array_set(Array* arr, size_t index, Data value)
 {
@@ -51,25 +38,21 @@ size_t array_size(const Array* arr)
 	return arr->size;
 }
 
-
 int main()
 {
-	size_t size = 10; 
-	Array* arr = array_create(size); 
+	size_t size = 10;
+	Array* arr = array_create(size);
 
-	
 	for (size_t i = 0; i < size; ++i)
 	{
 		array_set(arr, i, i * 2);
 	}
 
-	
 	for (size_t i = 0; i < size; ++i)
 	{
 		cout << "arr[" << i << "] = " << array_get(arr, i) << endl;
 	}
 
-	
 	array_delete(arr);
 
 	return 0;
--- LibraryCPP/array.h
+++ LibraryCPP/array.h
@@ -3,25 +3,18 @@
 
 #include <cstddef>
 
-// Non-resizeable array
-// Stores integer values inside
 typedef int Data;
 
-struct Array;
+struct Array
+{
+	Data* data;
+	size_t size;
+};
 
-// create array
-Array *array_create(size_t size);
+Array* array_create(size_t size);
+void array_delete(Array* arr);
+Data array_get(const Array* arr, size_t index);
+void array_set(Array* arr, size_t index, Data value);
+size_t array_size(const Array* arr);
 
-// delete array, free memory
-void array_delete(Array *arr);
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index);
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value);
-
-// returns array size
-size_t array_size(const Array *arr);
-
-#endif
+#endif // ARRAY_H
