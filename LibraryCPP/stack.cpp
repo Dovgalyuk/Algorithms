625de87392fec5b98b6dc0f203c17a6ff438405f
Тимофеев Иван <104462765+Morivan@users.noreply.github.com>
2023-11-26 21:28:51 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,30 @@
 #include "stack.h"
 #include "list.h"
 
-struct Stack{
+struct Stack {
     List* list = nullptr;
-    ListItem* last_item = nullptr;
 };
 
-Stack *stack_create(){
+Stack* stack_create() {
     Stack* new_stack = new Stack;
-
     new_stack->list = list_create();
-
     return new_stack;
 }
 
-void stack_delete(Stack *stack){
+void stack_delete(Stack* stack) {
     if (stack->list != nullptr)
         list_delete(stack->list);
 
     stack->list = nullptr;
-    stack->last_item = nullptr;
 
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data){
-    stack->last_item = list_insert(stack->list, data);
+void stack_push(Stack* stack, Data data) {
+    list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack){
+Data stack_get(const Stack* stack) {
     ListItem* first_item = list_first(stack->list);
 
     if (first_item != nullptr)
@@ -37,13 +33,11 @@ Data stack_get(const Stack *stack){
         return (Data)0;
 }
 
-void stack_pop(Stack *stack){
-    stack->last_item = list_erase_first(stack->list);
+void stack_pop(Stack* stack) {
+    list_erase_first(stack->list);
 }
 
-bool stack_empty(const Stack *stack){
-    if (stack->last_item == nullptr)
-        return true;
-    else
-        return false;
+bool stack_empty(const Stack* stack) {
+    return list_first(stack->list) == nullptr;
 }
+
