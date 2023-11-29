be067acddaa210cfb1241d7adbfc581ac55d9abc
Alav1e <144486832+Alav1e@users.noreply.github.com>
2023-11-29 16:40:35 +0300

    lab_1

--- Lab1C/FileName.cpp
+++ Lab1C/FileName.cpp
@@ -23,14 +23,14 @@ Array* random_full() {
 
 	return newArray;
 }
-
-void bubble_sort(int* arr, int n) {
-	for (size_t i = 0; i < n - 1; i++)
-		for (int j = 0; j < n - i - 1; j++) {
-			if (arr[j] > arr[j + 1])
-				swap(arr[j], arr[j + 1]);
-		}
-}
+//
+//void bubble_sort(int* arr, int n) {
+//	for (size_t i = 0; i < n - 1; i++)
+//		for (int j = 0; j < n - i - 1; j++) {
+//			if (arr[j] > arr[j + 1])
+//				swap(arr[j], arr[j + 1]);
+//		}
+//}
 
 Array* delete_elements() {
 	Array* newArray = NULL;
