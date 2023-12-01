df4a8f75d281908efa54e76e76c1ae7f1f44ddcc
endiepq <92216179+endiepq@users.noreply.github.com>
2023-12-01 04:24:16 +0300

    Update splaytree.cpp

--- LibraryCPPTemplate/Tests/splaytree.cpp
+++ LibraryCPPTemplate/Tests/splaytree.cpp
@@ -2,35 +2,40 @@
 #include <iostream>
 #include <string>
 
-void addTest(SplayTree& tree, int numElements) {
+void addTest(SplayTree *tree, int numElements) {
     for (int i = 0; i < numElements; ++i) {
         std::string key = "key" + std::to_string(i);
         std::string value = "value" + std::to_string(i);
-        tree.add(key, value);
+        tree->add(key, value);
+        std::string beforeAdd = tree->find(key);
+        if (beforeAdd.empty()) {
+            std::cerr << "Error: addtest" << std::endl;
+            exit(1);
+        }
     }
 }
 
-void findTest(SplayTree& tree, int numElements) {
+void findTest(SplayTree *tree, int numElements) {
     int found = 0;
     for (int i = 0; i < numElements; ++i) {
         std::string key = "key" + std::to_string(rand() % numElements);
-        std::string result = tree.find(key);
+        std::string result = tree->find(key);
         if (!result.empty()) {
             found++;
         }
         else {
-            std::cerr << "Error: Key '" << key << "' not found." << std::endl;
+            std::cerr << "Error: findtest" << std::endl;
             exit(1);
         }
     }
     std::cout << "Found " << found << " elements." << std::endl;
 }
 
-void removeTest(SplayTree& tree, int numElements) {
+void removeTest(SplayTree *tree, int numElements) {
     for (int i = 0; i < numElements / 2; ++i) {
         std::string key = "key" + std::to_string(rand() % numElements);
-        tree.remove(key);
-        std::string beforeRemove = tree.find(key);
+        tree->remove(key);
+        std::string beforeRemove = tree->find(key);
         if (!beforeRemove.empty()) {
             std::cerr << "Error: removetest" << std::endl;
             exit(1);
@@ -38,26 +43,26 @@ void removeTest(SplayTree& tree, int numElements) {
     }
 }
 
-void stressTest(SplayTree& tree, int numOperations) {
+void stressTest(SplayTree *tree, int numOperations) {
     for (int i = 0; i < numOperations; ++i) {
         int op = rand() % 3;
         std::string key = "key" + std::to_string(rand() % 1000);
         std::string value = "value" + std::to_string(rand() % 1000);
 
         if (op == 0) {
-            tree.add(key, value);
+            tree->add(key, value);
         }
         else if (op == 1) {
-            tree.find(key);
+            tree->find(key);
         }
         else {
-            tree.remove(key);
+            tree->remove(key);
         }
     }
 }
 
 int main() {
-    SplayTree tree;
+    SplayTree *tree = new SplayTree;
     srand((unsigned)time(0));
     int numElements = 1000;
     int numOperations = 10000;
@@ -73,6 +78,6 @@ int main() {
 
     std::cout << "Stress test..." << std::endl;
     stressTest(tree, numOperations);
-
+    delete tree;
     return 0;
 }
