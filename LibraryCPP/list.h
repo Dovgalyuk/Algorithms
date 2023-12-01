0c20bac0129aba3bcfca9c0ef4f524904bbb3b3f
RetroBoyy <retrooboyy@mail.ru>
2023-12-02 00:03:56 +0300

    Vlasov_lab2

--- LibraryCPP/list.h
+++ LibraryCPP/list.h
@@ -33,9 +33,11 @@ ListItem *list_insert(List *list, Data data);
 // Inserts new list item after the specified item
 ListItem *list_insert_after(List *list, ListItem *item, Data data);
 
-// Deletes the first list item.
+// Deletes the specified list item.
 // Returns pointer to the item next to the deleted one.
-ListItem *list_erase_first(List *list);
+// Not applicable for the singly linked lists.
+// Should be O(1)
+ListItem *list_erase(List *list, ListItem *item);
 
 // Deletes the list item following the specified one.
 // Returns pointer to the item next to the deleted one.
