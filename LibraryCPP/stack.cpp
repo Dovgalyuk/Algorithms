500372884f437a334beda253d7a13d2079967d42
RetroBoyy <retrooboyy@mail.ru>
2023-12-01 21:50:07 +0300

    Vlasov_lab2

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -19,9 +19,9 @@ void stack_delete(Stack *stack)
     delete stack;
 }
 
-void stack_push(Stack *stack, const std::string& data)
+void stack_push(Stack *stack, const string& data)
 {
-    list_insert(stack->list, data);
+    list_insert(stack->list, data.c_str());
 }
 
 Data stack_get(const Stack *stack)
