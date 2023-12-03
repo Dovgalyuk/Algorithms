266df8e722b1b164d16e2c3e5c0d24ce613dedd3
Timofey Kirikov <82898998+portenblank@users.noreply.github.com>
2023-12-03 21:06:33 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,41 +1,50 @@
-#include <stdlib.h>
-#include "list.h"
 #include "queue.h"
+#include "list.h"
 
-struct Queue
-{
+struct Queue {
     List* list;
+    ListItem* last;
 };
 
-Queue* queue_create()
-{
+Queue* queue_create() {
     Queue* queue = new Queue;
     queue->list = list_create();
+    queue->last = nullptr;
     return queue;
 }
 
-void queue_delete(Queue* queue)
-{
+void queue_delete(Queue* queue) {
     list_delete(queue->list);
     delete queue;
 }
 
-void queue_insert(Queue* queue, Data data)
-{
-    list_insert(queue->list, data);
+
+void queue_insert(Queue* queue, Data data) {
+    if (queue->last != nullptr) {
+        queue->last = list_insert_after(queue->list, queue->last, data);
+    }
+    else {
+        queue->last = list_insert(queue->list, data);
+    }
 }
 
-Data queue_get(const Queue* queue)
-{
-    return list_item_data(front);
+Data queue_get(const Queue* queue) {
+    ListItem* item = list_first(queue->list);
+    if (item) {
+        return list_item_data(item);
+    }
+    return -1;
 }
 
-void queue_remove(Queue* queue)
-{
-    list_erase(queue->list, front);
+
+void queue_remove(Queue* queue) {
+    if (queue->last && queue->last == list_first(queue->list)) {
+        queue->last = nullptr;
+    }
+    list_erase_first(queue->list);
 }
 
-bool queue_empty(const Queue* queue)
-{
-    return list_get_length(queue->list) == 0;
+
+bool queue_empty(const Queue* queue) {
+    return list_first(queue->list) == nullptr;
 }
