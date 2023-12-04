e83e3e2aa856ed5e3c97f385b812a5ea88653ba1
Vinevskaya Masha <mv2004.ru@mail.ru>
2023-12-05 01:05:34 +0300

    up 10

---
+++ LibraryCPP/Tests/stack.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "stack.h"
+
+int main()
+{
+    Stack *stack = stack_create();
+
+    stack_push(stack, 'a');
+    stack_push(stack, 'b');
+    stack_push(stack, 'c');
+
+    if (stack_get(stack) != 'c')
+    {
+        std::cout << "Invalid stack top after push\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
+
+    if (stack_get(stack) != 'b')
+    {
+        std::cout << "Invalid stack top after pop\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
+
+    if (stack_get(stack) != 'a')
+    {
+        std::cout << "Invalid stack top after pop\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_push(stack, 'q');
+    stack_push(stack, 'w');
+
+    if (stack_get(stack) != 'w')
+    {
+        std::cout << "Invalid stack top after push\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    while (!stack_empty(stack))
+    {
+        std::cout << "Get: " << stack_get(stack) << "\n";
+        stack_pop(stack);
+    }
+
+    stack_delete(stack);
+}
