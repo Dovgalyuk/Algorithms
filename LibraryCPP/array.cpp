c1b65a51c57ef9d4b753862a87b7d5e778fc0b34
Vinevskaya Masha <mv2004.ru@mail.ru>
2023-12-04 21:50:02 +0300

    up
    перенесла структуру array в файл array.cpp и переписала код в файле lib1.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -7,7 +7,7 @@ struct Array
 {
 
     size_t size_arr;
-    Data* data;
+    Data *data;
     Array(size_t size)
     {
         this->size_arr = size;
@@ -20,31 +20,31 @@ struct Array
 };
 
 // create array
-Array* array_create(size_t size)
+Array *array_create(size_t size)
 {
     return new Array(size);
 }
 
 // delete array, free memory
-void array_delete(Array* arr)
+void array_delete(Array *arr)
 {
     delete arr;
 }
 
 // returns specified array element
-Data array_get(const Array* arr, size_t index)
+Data array_get(const Array *arr, size_t index)
 {
     return arr->data[index];
 }
 
 // sets the specified array element to the value
-void array_set(Array* arr, size_t index, Data value)
+void array_set(Array *arr, size_t index, Data value)
 {
     arr->data[index] = value;
 }
 
 // returns array size
-size_t array_size(const Array* arr)
+size_t array_size(const Array *arr)
 {
     return arr->size_arr;
 }
