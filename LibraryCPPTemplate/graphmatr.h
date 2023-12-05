e983a2fd828b3f385ac05478b8b57144c69abe3a
duraaach0k <volkserz@yandex.com>
2023-12-05 19:17:24 +0300

    1

--- LibraryCPPTemplate/graphmatr.h
+++ LibraryCPPTemplate/graphmatr.h
@@ -67,7 +67,7 @@ public:
         if (vertex >= numVertices) {
             throw std::out_of_range("Vertex does not exist");
         }
-        vector_set(vertices, vertex, data);
+        vector_set(vertices, vertex, (void*)data);
     }
 
     T getVertexMark(long unsigned int vertex) {
