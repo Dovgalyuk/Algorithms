443f5e29513e8aab09d082e63bbda8267e8ca4c9
duraaach0k <volkserz@yandex.com>
2023-12-06 22:59:31 +0300

    1

--- Lab1C/lab4.cpp
+++ Lab1C/lab4.cpp
@@ -55,13 +55,13 @@ void KruskalMST(Graph<int>cost)
 
 int main() {
     Graph<int> graph(5);
-    graph.addEdge(0, 1, (int*)10);
-    graph.addEdge(0, 2, (int*) 20);
-    graph.addEdge(1, 2, (int*) 30);
-    graph.addEdge(1, 3, (int*)40);
-    graph.addEdge(2, 3, (int*)50);
-    graph.addEdge(2, 4, (int*)60);
-    graph.addEdge(3, 4, (int*)70);
+    graph.addEdge(0, 1, 10);
+    graph.addEdge(0, 2, 20);
+    graph.addEdge(1, 2, 30);
+    graph.addEdge(1, 3, 40);
+    graph.addEdge(2, 3, 50);
+    graph.addEdge(2, 4, 60);
+    graph.addEdge(3, 4, 70);
 
     
     KruskalMST(graph);
