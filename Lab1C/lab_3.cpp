245609a31fcfd2eb4498ee40114790db3b11451e
timlissovv <timsuryadov@gmail.com>
2023-12-07 22:17:23 +0300

    lab_3 upd_6

--- Lab1C/lab_3.cpp
+++ Lab1C/lab_3.cpp
@@ -7,26 +7,26 @@
 using namespace std;
 
 int main() {
-    std::unordered_map<std::string, std::unordered_set<std::string>> reactions;
-    std::string start, reaction;
+    unordered_map<std::string, std::unordered_set<std::string>> reactions;
+    string start, reaction;
 
-    std::cin >> start;
-    while (std::cin >> reaction)
+    cin >> start;
+    while (cin >> reaction)
     {
-        std::string substance1 = reaction.substr(0, reaction.find("->"));
-        std::string substance2 = reaction.substr(reaction.find("->") + 2);
+        string substance1 = reaction.substr(0, reaction.find("->"));
+        string substance2 = reaction.substr(reaction.find("->") + 2);
         reactions[substance1].insert(substance2);
     }
 
     Queue* queue = queue_create();
-    std::unordered_set<std::string> visited;
+    unordered_set<std::string> visited;
 
     queue_insert(queue, start);
     visited.insert(start);
 
     while (!queue_empty(queue))
     {
-        std::string current = queue_get(queue);
+        string current = queue_get(queue);
         queue_remove(queue);
 
         if (reactions.count(current) > 0) 
@@ -45,7 +45,7 @@ int main() {
     for (auto substance : visited) 
     {
         if (substance != start)
-            std::cout << substance << " ";
+            cout << substance << " ";
     }
 
     queue_delete(queue);
