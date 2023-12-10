34efde07ce79be47cd8b9a5b7a2cadbbc4a3f9fe
makargravanov <121590203+makargravanov@users.noreply.github.com>
2023-12-10 05:59:05 +0300

    Update list.h

--- LibraryCPPClass/list.h
+++ LibraryCPPClass/list.h
@@ -15,13 +15,8 @@ public:
         Item *prev() { return nullptr; }
         Data data() const { return Data(); }
          
-        void setNext(Item* next) { _next = next; }
-        void setPrev(Item* prev) { _prev = prev; }
-        void setData(Data data) { _data = data; }
     private:
-        Item* _next;
-        Item* _prev;
-        Data _data;
+       
     };
 
     // Creates new list
@@ -54,8 +49,7 @@ public:
     // Should be O(1)
     Item *erase_next(Item *item);
 private:
-    Item* head;
-    Item* tail;
+   
 };
 
 #endif
