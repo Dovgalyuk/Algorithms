292d7ee98dcb1154a1cc8fcd3a5bc53176bf710c
makargravanov <121590203+makargravanov@users.noreply.github.com>
2023-12-10 06:33:56 +0300

    Update list.h
    C наш, сущий в памяти!
    да компилируется код Твой;
    да приидет царствие Софта Твоего;
    да будут действительны указатели Твои
    и в ОЗУ, как на жестком диске;
    массив наш насущный подавай нам на каждый день;
    и прости нам варнинги наши,
    как и мы избавляемся от ошибок наших;
    и не введи нас в бесконечный цикл,
    но избавь нас от винды.
    Ибо Твое есть Царство и сила и слава во веки.
    Энтер.

--- LibraryCPPClass/list.h
+++ LibraryCPPClass/list.h
@@ -8,47 +8,29 @@ typedef int Data;
 class List
 {
 public:
-    class Item
-    {
-    public:
-        Item *next() { return nullptr; }
-        Item *prev() { return nullptr; }
-        Data data() const { return Data(); }
-    private:
-        // internal data here
-    };
+   class Item
+   {
+   public:
+       Item *next() { return nextItem; }
+       Data data() const { return itemData; }
+   private:
+       Data itemData;
+       Item *nextItem;
+   };
 
-    // Creates new list
-    List();
+   List();
+   List(const List &a);
+   List &operator=(const List &a);
+   ~List();
 
-    // copy constructor
-    List(const List &a);
+   Item *first();
+   Item *insert(Data data);
+   Item *insert_after(Item *item, Data data);
+   Item *erase_first();
+   Item *erase_next(Item *item);
 
-    // assignment operator
-    List &operator=(const List &a);
-
-    // Destroys the list and frees the memory
-    ~List();
-
-    // Retrieves the first item from the list
-    Item *first();
-
-    // Inserts new list item into the beginning
-    Item *insert(Data data);
-
-    // Inserts new list item after the specified item
-    Item *insert_after(Item *item, Data data);
-
-    // Deletes the first list item.
-    // Returns pointer to the item next to the deleted one.
-    Item *erase_first();
-
-    // Deletes the list item following the specified one.
-    // Returns pointer to the item next to the deleted one.
-    // Should be O(1)
-    Item *erase_next(Item *item);
 private:
-    // private data should be here
+   Item* head;
 };
 
 #endif
