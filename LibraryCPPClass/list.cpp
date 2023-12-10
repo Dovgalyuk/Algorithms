34803fcd0328c1c15054c1a5bd05c22bf4f0daf9
makargravanov <121590203+makargravanov@users.noreply.github.com>
2023-12-10 06:04:25 +0300

    Update list.cpp

--- LibraryCPPClass/list.cpp
+++ LibraryCPPClass/list.cpp
@@ -3,53 +3,50 @@
 
 List::List() : head(nullptr), tail(nullptr) {}
 
-List::List(const List& a) {
-    head = nullptr;
-    tail = nullptr;
-    Item* temp = a.head;
+List::List(const List &a) : head(nullptr), tail(nullptr) {
+    Item *temp = a.first();
     while (temp != nullptr) {
-        this->insert(temp->data);
-        temp = temp->next;
+        this->insert(temp->data());
+        temp = temp->next();
     }
 }
 
-List& List::operator=(const List& a) {
+List &List::operator=(const List &a) {
     if (this != &a) {
-        Item* temp = head;
+        Item *temp = head;
         while (temp != nullptr) {
-            Item* next = temp->next;
+            Item *next = temp->next();
             delete temp;
             temp = next;
         }
         head = nullptr;
         tail = nullptr;
-        temp = a.head;
+        temp = a.first();
         while (temp != nullptr) {
-            this->insert(temp->data);
-            temp = temp->next;
+            this->insert(temp->data());
+            temp = temp->next();
         }
     }
     return *this;
 }
 
 List::~List() {
-    Item* temp = head;
+    Item *temp = head;
     while (temp != nullptr) {
-        Item* next = temp->next;
+        Item *next = temp->next();
         delete temp;
         temp = next;
     }
 }
 
-List::Item* List::first() {
+List::Item *List::first() {
     return head;
 }
 
-List::Item* List::insert(Data data) {
-    Item* newItem = new Item();
+List::Item *List::insert(Data data) {
+    Item *newItem = new Item();
     newItem->data = data;
     newItem->next = head;
-    newItem->prev = nullptr;
     if (head != nullptr) {
         head->prev = newItem;
     }
@@ -60,16 +57,16 @@ List::Item* List::insert(Data data) {
     return newItem;
 }
 
-List::Item* List::insert_after(Item* item, Data data) {
+List::Item *List::insert_after(Item *item, Data data) {
     if (item == nullptr) {
         return insert(data);
     }
-    Item* newItem = new Item();
+    Item *newItem = new Item();
     newItem->data = data;
-    newItem->next = item->next;
+    newItem->next = item->next();
     newItem->prev = item;
-    if (item->next != nullptr) {
-        item->next->prev = newItem;
+    if (item->next() != nullptr) {
+        item->next()->prev = newItem;
     }
     item->next = newItem;
     if (tail == item) {
@@ -78,11 +75,11 @@ List::Item* List::insert_after(Item* item, Data data) {
     return newItem;
 }
 
-List::Item* List::erase_first() {
+List::Item *List::erase_first() {
     if (head == nullptr) {
         return nullptr;
     }
-    Item* temp = head->next;
+    Item *temp = head->next();
     delete head;
     if (temp != nullptr) {
         temp->prev = nullptr;
@@ -94,18 +91,19 @@ List::Item* List::erase_first() {
     return temp;
 }
 
-List::Item* List::erase_next(Item* item) {
-    if (item == nullptr || item->next == nullptr) {
+List::Item *List::erase_next(Item *item) {
+    if (item == nullptr || item->next() == nullptr) {
         return nullptr;
     }
-    Item* temp = item->next->next;
-    delete item->next;
+    Item *temp = item->next()->next();
+    delete item->next();
     item->next = temp;
     if (temp != nullptr) {
         temp->prev = item;
     }
-    if (tail == item->next) {
+    if (tail == item->next()) {
         tail = item;
     }
     return temp;
 }
+
