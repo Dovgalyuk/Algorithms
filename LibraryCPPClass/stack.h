7d4856098f41e9411e57c318c35f44018949876b
makargravanov <121590203+makargravanov@users.noreply.github.com>
2023-12-10 22:05:33 +0300

    Update stack.h (финал)
    пам-пам

--- LibraryCPPClass/stack.h
+++ LibraryCPPClass/stack.h
@@ -1,33 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include "list.h"  // Include the header file for the List class
+#include "list.h"  
 
 class Stack
 {
 public:
-    // Creates empty stack
     Stack();
-
-    // Deletes the stack
     ~Stack();
-
-    // Pushes data on top of the stack
-    // Should be O(1) on average
     void push(Data data);
-
-    // Retrieves the last element from the stack
     Data get() const;
-
-    // Removes the last element from the stack
-    // Should be O(1)
     void pop();
-
-    // Returns true if the stack is empty
     bool empty() const;
-
 private:
-    List dataList;  // Private List member
+    List dataList; 
 };
 
 #endif
