96aeb773d9bd6f569602daed86173ed1c0cf090d
makargravanov <121590203+makargravanov@users.noreply.github.com>
2023-12-10 06:36:58 +0300

    Update list.cpp
    C Nuestro que estas en la Memoria,
    Compilado sea tu código,
    venga a nosotros tu software,
    carguense tus punteros.
    así en la RAM como en el Disco Duro,
    Danos hoy nuestro Array de cada día,
    Perdona nuestros Warnings,
    así como nosotros también los eliminamos,
    no nos dejes caer en Bucles,
    y libranos del Windows, Enter.

--- LibraryCPPClass/list.cpp
+++ LibraryCPPClass/list.cpp
@@ -1,70 +1,70 @@
 #include <cstddef>
+#include <algorithm>
 #include "list.h"
 
 List::List() : head(nullptr) {}
 
 List::List(const List &a) : head(nullptr) {
-   Item* node = a.head;
-   while (node != nullptr) {
-       insert(node->data());
-       node = node->next();
-   }
+  Item* node = a.head;
+  while (node != nullptr) {
+      insert(node->data());
+      node = node->next();
+  }
 }
 
 List &List::operator=(const List &a) {
-   if (this != &a) {
-       List temp(a);
-       std::swap(head, temp.head);
-   }
-   return *this;
+  if (this != &a) {
+      List temp(a);
+      std::swap(head, temp.head);
+  }
+  return *this;
 }
 
 List::~List() {
-   while (head != nullptr) {
-       erase_first();
-   }
+  while (head != nullptr) {
+      erase_first();
+  }
 }
 
 List::Item *List::first() {
-  return head;
+ return head;
 }
 
 List::Item *List::insert(Data data) {
-  Item* newItem = new Item;
-  newItem->itemData = data;
-  newItem->nextItem = head;
-  head = newItem;
-  return newItem;
+ Item* newItem = new Item;
+ newItem->data() = data; 
+ newItem->next() = head; 
+ head = newItem;
+ return newItem;
 }
 
 List::Item *List::insert_after(Item *item, Data data) {
-  if (item == nullptr) {
-      return nullptr;
-  }
-  Item* newItem = new Item;
-  newItem->itemData = data;
-  newItem->nextItem = item->next();
-  item->nextItem = newItem;
-  return newItem;
+ if (item == nullptr) {
+     return nullptr;
+ }
+ Item* newItem = new Item;
+ newItem->data() = data; 
+ newItem->next() = item->next();
+ item->next() = newItem;
+ return newItem;
 }
 
 List::Item *List::erase_first() {
-  if (head == nullptr) {
-      return nullptr;
-  }
-  Item* nextItem = head->next();
-  delete head;
-  head = nextItem;
-  return nextItem;
+ if (head == nullptr) {
+     return nullptr;
+ }
+ Item* nextItem = head->next();
+ delete head;
+ head = nextItem;
+ return nextItem;
 }
 
 List::Item *List::erase_next(Item *item) {
-  if (item == nullptr || item->next() == nullptr) {
-      return nullptr;
-  }
-  Item* nextItem = item->next()->next();
-  delete item->next();
-  item->nextItem = nextItem;
-  return nextItem;
+ if (item == nullptr || item->next() == nullptr) {
+     return nullptr;
+ }
+ Item* nextItem = item->next()->next();
+ delete item->next();
+ item->next() = nextItem; // используйте метод next() для установки значения
+ return nextItem;
 }
-
