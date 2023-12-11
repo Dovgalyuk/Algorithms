72cb1a60bb68e4c226ba67e1e53e65484df985a9
playnikov <playnikov@gmail.com>
2023-12-12 00:36:09 +0300

    update lab2.cpp

--- Labs/lab2.cpp
+++ Labs/lab2.cpp
@@ -5,60 +5,64 @@
 
 using namespace std;
 
-bool checkTags(const string& html);
+bool checkTags(ifstream& inputFile);
 
 int main() {
 	string inputNameFile, outputNameFile, html, line;
 
-	cout << "inputFile: ", cin >> inputNameFile;
-	cout << "outputFile: ", cin >> outputNameFile;
+	/*cout << "inputFile: ", cin >> inputNameFile;
+	cout << "outputFile: ", cin >> outputNameFile;*/
+	inputNameFile = "input.txt";
+	outputNameFile = "output.txt";
 	
 	ifstream inputFile(inputNameFile);
 	if (!inputFile.is_open()) {
 		cout << "n/a" << endl;
 	}
 	else {
-		while (getline(inputFile, line)) {
-			html += line;
-		}
-		inputFile.close();
 		ofstream outputFile(outputNameFile);
-		outputFile << (checkTags(html) ? "YES" : "NO");
+		outputFile << (checkTags(inputFile) ? "YES" : "NO");
+		inputFile.close();
 		outputFile.close();
 	}
 
 	return 0;
 }
 
-bool checkTags(const string& html) {
+bool checkTags(ifstream& inputFile) {
 	Stack* stack = stack_create();
-	for (size_t i = 0; i < html.size(); i++) {
-		if (html[i] == '<') {
-			if (html[i + 1] != '/') {
-				i++;
-				for (; html[i] != '>'; i++) {
-					stack_push(stack, tolower(html[i]));
-				}
-			}
-			else {
-				i += 2;
-				int j = 0;
-				for (; html[i] != '>'; i++) {
-					j++;
-				}
-				i -= j;
-				j--;
-				for (; j != -1; j--) {
-					if (tolower(html[i + j]) == stack_get(stack)) {
-						stack_pop(stack);
+	string html;
+	size_t i = 0;
+	while (getline(inputFile, html)) {
+		for (; i < html.size(); i++) {
+			if (html[i] == '<') {
+				if (html[i + 1] != '/') {
+					i++;
+					for (; html[i] != '>'; i++) {
+						stack_push(stack, tolower(html[i]));
 					}
-					else {
-						stack_delete(stack);
-						return false;
+				}
+				else {
+					i += 2;
+					int j = 0;
+					for (; html[i] != '>'; i++) {
+						j++;
+					}
+					i -= j;
+					j--;
+					for (; j != -1; j--) {
+						if (tolower(html[i + j]) == stack_get(stack)) {
+							stack_pop(stack);
+						}
+						else {
+							stack_delete(stack);
+							return false;
+						}
 					}
 				}
 			}
 		}
+		i=0;
 	}
 	if (stack_empty(stack))
 	{
