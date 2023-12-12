6e6c48a0a92c2dfb1226a6d97f641cf112aadfca
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-12-13 00:57:19 +0300

    Make last function

--- Labs/lab2.cpp
+++ Labs/lab2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 	 Stack* stack = stack_create();
 	 int local_var[4] = { 0, 0, 0, 0 };
 
-	 ifstream file("input.txt"); //C:\Users\user\Documents\Unreal Projects\GitSnake\Algorithms\out\build\x64-Debug\LibraryCPP - расположение файла
+	 ifstream file("input.txt"); //C:\Users\user\Documents\Unreal Projects\GitSnake\Algorithms\out\build\x64-Debug\Labs - расположение файла
 	 if (!file.is_open()) {
 		 std::cout<<"Failed open!"<<endl;
 		 return 0;
@@ -20,11 +20,93 @@ using namespace std;
 		 istringstream iss(line);
 		 string function;
 		 iss >> function;
-		 if(function == "bipush") {
-			 Data value;
-			 iss >> value;
-			 stack_push(stack, value);
-		 }
+         if (function == "bipush") {
+             int value;
+             if (iss >> value) {
+                 stack_push(stack, value);
+             }
+         }
+         else if (function == "pop") {
+             stack_pop(stack);
+         }
+         else if (function == "imul") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a * b);
+         }
+         else if (function == "iand") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a & b);
+         }
+         else if (function == "ior") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a | b);
+         }
+         else if (function == "ixor") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a ^ b);
+         }
+         else if (function == "iadd") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a + b);
+         }
+         else if (function == "isub") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, b - a);
+         }
+         else if (function == "iload_0") {
+             stack_push(stack, local_var[0]);
+         }
+         else if (function == "iload_1") {
+             stack_push(stack, local_var[1]);
+         }
+         else if (function == "iload_2") {
+             stack_push(stack, local_var[2]);
+         }
+         else if (function == "iload_3") {
+             stack_push(stack, local_var[3]);
+         }
+         else if (function == "istore_0") {
+             local_var[0] = stack_get(stack);
+             stack_pop(stack);
+         }
+         else if (function == "istore_1") {
+             local_var[1] = stack_get(stack);
+             stack_pop(stack);
+         }
+         else if (function == "istore_2") {
+             local_var[2] = stack_get(stack);
+             stack_pop(stack);
+         }
+         else if (function == "istore_3") {
+             local_var[3] = stack_get(stack);
+             stack_pop(stack);
+         }
+         else if (function == "swap") {
+             int a = stack_get(stack);
+             stack_pop(stack);
+             int b = stack_get(stack);
+             stack_pop(stack);
+             stack_push(stack, a);
+             stack_push(stack, b);
+         }
 	 }
 	 file.close();
 
