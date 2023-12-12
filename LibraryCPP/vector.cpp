60f01025b1f32440e650ae9d867de75305b6758d
Vitaliy <144813657+debcomedian@users.noreply.github.com>
2023-12-12 19:32:13 +0300

    update vector and Cmake

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,33 +2,56 @@
 
 struct Vector
 {
+    size_t size;
+    Data* data;
 };
 
 Vector *vector_create()
 {
-    return new Vector;
+    Vector* vector = new Vector;
+    vector->data = nullptr;
+    vector->size = 0;
+    return vector;
 }
 
 void vector_delete(Vector *vector)
 {
-    // TODO: free vector internals
+    delete[] vector->data;
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    return (index >= vector_size(vector))? -1 : vector->data[index];
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    if (vector == NULL)
+        return;
+
+    if (index < vector_size(vector)) {
+        vector->data[index] = value;
+    }
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (vector->size >= size) {
+        vector->size = size;
+    }
+    else {
+        Data* temp = new Data[size];
+        for (size_t i = 0; i < vector->size; i++) {
+            temp[i] = vector->data[i];
+        }
+        delete[] vector->data;
+        vector->data = temp;
+        vector->size = size;
+    }
 }
