14faa9a5d50b6cc543c48467d8c90eb33e0fabf7
Vitaliy <144813657+debcomedian@users.noreply.github.com>
2023-12-12 21:12:38 +0300

    Complete lab-2

--- Lab1CPP/lab2.cpp
+++ Lab1CPP/lab2.cpp
@@ -4,13 +4,42 @@
 
 "ab(c 'de' f)gt" --> YES
 "adjsk 'z' frr') --> NO
+
 */
 #include "stack.h"
 
 #include <ctime>
 #include <iostream>
+#include <string>
 
 int main() {
+
+    Stack* stack = stack_create();
+    std::string str; 
+    std::getline(std::cin, str);
+
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if (str[i] == '\"' || str[i] == '\'' || str[i] == '(' || str[i] == ')') {
+            char ch = str[i];
+            if (str[i] == ')') {
+                ch = '(';
+            }
+            if (stack_get(stack) == ch) {
+                stack_pop(stack);
+            }
+            else {
+                stack_push(stack, ch);
+            }
+        }
+    }
+    if (stack_empty(stack)) {
+        std::cout << "YES";
+    } else {
+        std::cout << "NO";
+    }
+
+    stack_delete(stack);
 
     return 0;
 }
