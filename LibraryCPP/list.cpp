83064afbf2f16f1e11ec96a3d17c717fde376707
Ivan_Nov <94312727+Myfasick@users.noreply.github.com>
2023-12-14 18:48:29 +0300

    FIX

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -4,18 +4,18 @@
 struct ListItem
 {
     Data data;
-    ListItem *next = nullptr;
-    ListItem *prev = nullptr;
+    ListItem *next;
+    ListItem *prev;
 };
 
 struct List
 {
-    ListItem *head = nullptr;
+    ListItem *head;
 };
 
 List *list_create()
 {
-    return new List;
+    return new List{nullptr};
 }
 
 void list_delete(List *list)
@@ -49,16 +49,17 @@ ListItem *list_item_prev(ListItem *item)
 
 ListItem *list_insert(List *list, Data data)
 {
-    ListItem *newListItem = new ListItem;
-    newListItem->data = data;
-    newListItem->next = list->head;
-    newListItem->prev = nullptr;
+    ListItem *newListItem = new ListItem{data, list->head, nullptr};
 
     if (list->head)
     {
         list->head->prev = newListItem;
     }
-
+    else
+    {
+        list->head = newListItem;
+    }
+    list->head->prev = newListItem;
     list->head = newListItem;
     return newListItem;
 }
@@ -70,10 +71,7 @@ ListItem *list_insert_after(ListItem *item, Data data)
         return nullptr;
     }
 
-    ListItem *newListItem = new ListItem;
-    newListItem->data = data;
-    newListItem->next = item->next;
-    newListItem->prev = item;
+    ListItem *newListItem = new ListItem{data, item->next, item};
 
     item->next = newListItem;
 
@@ -90,12 +88,13 @@ ListItem *list_erase_first(List *list)
     if (list->head)
     {
         ListItem *temp = list->head;
-        list->head = list->head->next;
 
-        if (list->head)
+        if (list->head->next)
         {
-            list->head->prev = nullptr;
+            list->head->next->prev = list->head->prev;
         }
+
+        list->head = list->head->next;
 
         delete temp;
         return list->head;
