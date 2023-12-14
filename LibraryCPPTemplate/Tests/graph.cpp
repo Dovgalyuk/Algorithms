95d1b4c2008b9c18c8e28a09cf0d804cb07e4b09
Timofey Kirikov <82898998+portenblank@users.noreply.github.com>
2023-12-15 00:31:55 +0300

    Update graph.cpp

--- LibraryCPPTemplate/Tests/graph.cpp
+++ LibraryCPPTemplate/Tests/graph.cpp
@@ -4,93 +4,52 @@
 typedef Graph<size_t> MyGraph;
 
 int main() {
-    std::cout << "Testing constructor..." << std::endl;
     MyGraph graph(5);
-    if (graph.vertexCount() != 5)
+    if (!graph) {
+        std::cout << "Graph creation error\n";
+        return 1;
+    }
+    if (graph.vertexCount() != 5) 
     {
-        std::cout << "Constructor test failed: wrong vertex count." << std::endl;
+        std::cout << "Graph count error\n" << std::endl;
+        return 1;
+    }
+    graph.addEdge(0, 1, 10);
+    graph.addEdge(1, 2, 20);
+    graph.addEdge(2, 3, 30);
+    graph.addEdge(3, 4, 40);
+    graph.addEdge(4, 0, 50);
+
+    if (graph.getEdgeData(2, 3) != 30) 
+    {
+        std::cout << "addEdge error\n";
         return 1;
     }
 
-    std::cout << "Testing addVertex..." << std::endl;
-    graph.addVertex();
-    if (graph.vertexCount() != 6)
+    graph.removeEdge(4, 0);
+    
+    if (graph.hasEdge(4, 0)) 
     {
-        std::cout << "Add vertex test failed." << std::endl;
+        std::cout << "removeEdge error\n";
         return 1;
     }
-
-    std::cout << "Testing addEdge with multiple edges..." << std::endl;
-    for (size_t i = 0; i < 5; i++)
+    
+    graph.setEdgeData(0, 1, 123);
+    
+    if (graph.getEdgeData(0, 1) != 123) 
     {
-        graph.addEdge(i, (i + 1) % graph.vertexCount());
-        if (!graph.hasEdge(i, (i + 1) % graph.vertexCount()))
-        {
-            std::cout << "Add edge test failed for edge " << i << " to " << (i + 1) % graph.vertexCount() << std::endl;
-            return 1;
+        std::cout << "setEdgeData error\n";
+        return 1;
+    }
+    for (size_t i = 0; i < graph.vertexCount(); i++) {
+        std::cout << "The adjacency list for a vertex " << i << ": ";
+        List<typename Graph<int>::Edge>& adjacencyList = graph.adjacencyList(i);
+        for (size_t j = 0; j < adjacencyList.size(); j++) {
+            typename Graph<int>::Edge& edge = adjacencyList[j];
+            std::cout << "(" << edge.to << ", " << edge.weight << ") ";
         }
+        std::cout << std::endl;
     }
 
-    std::cout << "Testing removeEdge with multiple edges..." << std::endl;
-    for (size_t i = 0; i < 5; i++)
-    {
-        graph.removeEdge(i, (i + 1) % graph.vertexCount());
-        if (graph.hasEdge(i, (i + 1) % graph.vertexCount()))
-        {
-            std::cout << "Remove edge test failed for edge " << i << " to " << (i + 1) % graph.vertexCount() << std::endl;
-            return 1;
-        }
-    }
-
-    std::cout << "Testing vertex data..." << std::endl;
-    graph.setVertexLabel(0, "123");
-    if (graph.getVertexLabel(0) != "123")
-    {
-        std::cout << "Vertex data test failed." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Testing edge data..." << std::endl;
-    graph.addEdge(0, 10);
-    graph.setEdgeLabel(0, 1, "20");
-    if (graph.getEdgeLabel(0, 1) != "20")
-    {
-        std::cout << "Edge data test failed." << std::endl;
-        return 1;
-    }
-
-    std::cout << "Testing add multiple edges to a single vertex..." << std::endl;
-    for (size_t i = 1; i < graph.vertexCount(); i++)
-    {
-        graph.addEdge(0, 10 * i);
-    }
-    for (size_t i = 1; i < graph.vertexCount(); i++)
-    {
-        if (!graph.hasEdge(0, i))
-        {
-            std::cout << "Multiple edges test failed for edge from 0 to " << i << std::endl;
-            return 1;
-        }
-    }
-
-
-    std::cout << "Testing removeVertex with non-empty edge set..." << std::endl;
-    size_t removeVertexIndex = 2;
-    graph.removeVertex(removeVertexIndex);
-    if (graph.vertexCount() != 5)
-    {
-        std::cout << "Remove vertex test failed: incorrect vertex count." << std::endl;
-        return 1;
-    }
-    for (size_t i = 0; i < graph.vertexCount(); i++)
-    {
-        if (graph.hasEdge(i, removeVertexIndex))
-        {
-            std::cout << "Remove vertex test failed: found edge to removed vertex." << std::endl;
-            return 1;
-        }
-    }
-
-    std::cout << "All tests passed successfully." << std::endl;
     return 0;
 }
