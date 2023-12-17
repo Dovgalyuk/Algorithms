cb1a5fc021de70300b9b2a84c216d4971fc10ec6
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-12-18 00:19:33 +0300

    Make queue_insert()

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,7 +1,20 @@
 #include "queue.h"
+#include "vector.h"
 
 struct Queue
 {
+    Vector* vector;
+    size_t last;
+    size_t* first;
+
+    Queue() {
+        vector = vector_create();
+        last = -1;
+        first = -1;
+    }
+    ~Queue() {
+        vector_delete(vector);
+    }
 };
 
 Queue *queue_create()
@@ -11,24 +24,61 @@ Queue *queue_create()
 
 void queue_delete(Queue *queue)
 {
-    // TODO: free queue items
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    size_t size = vector_size(queue->vector);
+
+    if (queue_empty(queue)) {
+        queue->first = 0;
+        queue->last= 0;
+    }
+    else if (queue->last % static_cast<int>(size) == queue->first) {
+        Vector* buff = vector_create();
+        vector_resize(buff, size * 2);
+
+        int counter = 0;
+
+        for (size_t i = queue->first; i < size; i++) {
+            vector_set(buff, counter, vector_get(queue->vector, i));
+            counter++;
+        }
+        for (int i = 0; i < queue->rear; i++) {
+            vector_set(buff, counter, vector_get(queue->vector, i));
+            counter++;
+        }
+        vector_delete(queue->vector);
+        queue->vector = buff;
+        queue->head = 0;
+        queue->last = static_cast<int>(size);
+
+        size = vector_size(queue->vector);
+    }
+    Data last = queue->last % size;
+    vector_set(queue->vector, last, data);
+    queue->last = last + 1;
 }
+
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    if (queue_empty(queue)) {
+        return NULL;
+    }
+    return vector_get(queue->vector, queue->first);
 }
 
 void queue_remove(Queue *queue)
 {
+    if (queue_empty(queue)) {
+        return;
+    }
+    queue->first++;
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    return queue->first == queue->last;
 }
