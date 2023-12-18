8ae0161ed42872c7aca3fb64d0956c2faf504c2b
turn53 <vyacheslav.ivanov.0404@mail.ru>
2023-12-19 00:46:57 +0300

    Ivanov

---
+++ Lab1C/Source.cpp
@@ -0,0 +1,103 @@
+// array.h
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <cstddef>
+
+struct Array {
+    int* data;
+    size_t size;
+};
+
+Array array_create(size_t size);
+void array_set(Array& arr, size_t index, int value);
+void array_delete(Array& arr);
+
+#endif // ARRAY_H
+
+// array.cpp
+#include "array.h"
+#include <cstdlib>
+
+Array array_create(size_t size) {
+    Array arr;
+    arr.size = size;
+    arr.data = new int[size];
+    return arr;
+}
+
+void array_set(Array& arr, size_t index, int value) {
+    if (index < arr.size) {
+        arr.data[index] = value;
+    }
+}
+
+void array_delete(Array& arr) {
+    delete[] arr.data;
+}
+
+// main.cpp
+#include "array.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void processArray(Array& arr, int m, int n) {
+    for (size_t i = 0; i < arr.size; i++) {
+        if (arr.data[i] % 2 != 0) {
+            arr.data[i] -= m;
+        }
+        if (i % 2 == 0) {
+            arr.data[i] += n;
+        }
+    }
+}
+
+void findMaxSum(const Array& arr) {
+    int maxSum = 0;
+    size_t startPos = 0;
+    for (size_t i = 0; i < arr.size - 4; i++) {
+        int sum = arr.data[i] + arr.data[i + 1] + arr.data[i + 2] + arr.data[i + 3] + arr.data[i + 4];
+        if (sum > maxSum) {
+            maxSum = sum;
+            startPos = i;
+        }
+    }
+
+    std::cout << "Максимальная сумма пяти соседних элементов: " << maxSum << std::endl;
+}
+
+void printArray(const Array& arr, const std::string& label) {
+    std::cout << label << ": ";
+    for (size_t i = 0; i < arr.size; i++) {
+        std::cout << arr.data[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int size, m, n;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    Array arr = array_create(size);
+
+    // Заполнение массива случайными числами
+    for (size_t i = 0; i < arr.size; i++) {
+        array_set(arr, i, rand() % 100); // Заполняем случайными числами от 0 до 99
+    }
+
+    printArray(arr, "Исходный массив");
+
+    std::cout << "Введите значения m и n: ";
+    std::cin >> m >> n;
+
+    processArray(arr, m, n);
+    printArray(arr, "Обработанный массив");
+
+    findMaxSum(arr);
+
+    array_delete(arr);
+
+    return 0;
+}
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,51 @@
-#include "array.h"
+#include "Array.h"
 
 struct Array
 {
+    Data data;
+    size_t size;
 };
 
 // create array
-Array *array_create(size_t size)
+Array array_create(size_t size)
 {
-    return new Array;
+    Array newArr;
+    newArr.data = new Data[size];
+    newArr.size = size;
+    return newArr;
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
+void array_delete(Array arr)
 {
-    delete arr;
+    delete[] arr.data;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
+Data array_get(const Array arr, size_t index)
 {
-    return (Data)0;
+    if (index < arr.size) {
+        return arr.data[index];
+    }
+    else {
+        // handle out of bounds error
+        return Data(); // return default value for Data type
+    }
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+void array_set(Array arr, size_t index, Data value)
 {
+    if (index < arr.size) {
+        arr.data[index] = value;
+    }
+    else {
+        // handle out of bounds error
+    }
 }
 
 // returns array size
-size_t array_size(const Array *arr)
+size_t array_size(const Array arr)
 {
-    return 0;
+    return arr->size;
 }
