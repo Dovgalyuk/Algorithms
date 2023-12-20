49a2e2c67ef257f3bd4e81ba4b163b68fef98127
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-12-20 16:58:47 +0300

    Make makeGraph

--- Labs/Lab3.cpp
+++ Labs/Lab3.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 #include<queue.h>
+#include<vector.h>
+#include<vector>
 
 using namespace std;
-int main() {
 
+vector<vector<string>> makeGraph(string input) {
+	vector<vector<string>> graph;
+
+	int indexV = (int)input[0];
+	int indexS = (int)input[3];
+	for (int i = 0; i < indexV + 1; i++) {
+		if(graph[i][indexV] != "") {
+			graph[i][indexS] = input[3];
+		}
+		else {
+			graph[i][indexV] = input[0];
+			graph[i][indexS] = input[3];
+		}
+	}
+	return graph;
+}
+int main() {
+	vector<vector<string>> graph;
+	vector<bool> used;
+
+	Queue* queue = queue_create();
 }
