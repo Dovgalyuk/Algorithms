59b3f42c9e95c5382097e9f7d844f9d74fd368bb
Mirik <kihuge@yandex.ru>
2023-12-21 12:15:51 +0300

    3lab

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,48 +1,37 @@
-
 #include "queue.h"
+#include "vector.h" 
 
-Queue *queue_create()
-{
+struct Queue {
+    Vector *vector; 
+};
+
+Queue *queue_create() {
     Queue *queue = new Queue;
     queue->vector = vector_create();
     return queue;
 }
 
-void queue_delete(Queue *queue)
-{
+void queue_delete(Queue *queue) {
     vector_delete(queue->vector);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
-{
+void queue_insert(Queue *queue, Data data) {
     vector_resize(queue->vector, vector_size(queue->vector) + 1);
     vector_set(queue->vector, vector_size(queue->vector) - 1, data);
 }
 
-Data queue_get(const Queue *queue)
-{
-    if (!queue_empty(queue))
-    {
-        return vector_get(queue->vector, 0);
-    }
-
-    return (Data)0;
+Data queue_get(const Queue *queue) {
+    return vector_get(queue->vector, 0);
 }
 
-void queue_remove(Queue *queue)
-{
-    if (!queue_empty(queue))
-    {
-        for (size_t i = 1; i < vector_size(queue->vector); ++i)
-        {
-            vector_set(queue->vector, i - 1, vector_get(queue->vector, i));
-        }
-        vector_resize(queue->vector, vector_size(queue->vector) - 1);
+void queue_remove(Queue *queue) {
+    for (size_t i = 1; i < vector_size(queue->vector); ++i) {
+        vector_set(queue->vector, i - 1, vector_get(queue->vector, i));
     }
+    vector_resize(queue->vector, vector_size(queue->vector) - 1);
 }
 
-bool queue_empty(const Queue *queue)
-{
+bool queue_empty(const Queue *queue) {
     return vector_size(queue->vector) == 0;
 }
