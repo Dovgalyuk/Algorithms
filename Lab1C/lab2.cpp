ec26e78cfe1fd3b5b747e1232993939186f9cf16
thatpel <zhenya.mitroshenko@yandex.ru>
2023-12-22 12:38:54 +0300

    lab2

---
+++ Lab1C/lab2.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <iostream>
+#include <string>
+#include "stack.h"
+#include <fstream>
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    string filename = "input.txt";
+
+	ifstream inputFile(filename);
+    if (!inputFile) {
+        printf("Не удалось открыть файл\n");
+        return 0;
+    }
+
+    Stack* stack = stack_create(); 
+
+	size_t size = 0;
+    char ch;
+	while (inputFile.get(ch)){
+		while (ch != '*'){
+			stack_push(stack, ch);
+			size += 1;
+		}
+		if (!stack_empty(stack) && ((stack_get(stack) == ch) )) {
+			stack_pop(stack);
+			size -= 1;
+		}
+	}
+
+	if (stack_empty(stack) && size == 0) { 
+		printf("YES\n");
+	}
+	else {
+		printf("NO\n");
+	}
+
+	stack_delete(stack)
+
+
+    return 0;
+}
