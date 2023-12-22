ee61c1940e48b3b7ff0cf196fccfe7a82a7e8dc3
INGAEVT <93099989+INGAEVT@users.noreply.github.com>
2023-12-22 21:56:27 +0300

    array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,13 +1,27 @@
 #include "array.h"
+#include <iostream>
+
+using namespace std;
 
 struct Array
 {
+    size_t size_arr;
+    Data *data;
+    Array(size_t size)
+    {
+        this->size_arr = size;
+        this->data = new Data[size];
+    }
+    ~Array()
+    {
+        delete[] this->data;
+    }
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    return new Array(size);
 }
 
 // delete array, free memory
@@ -19,16 +33,17 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size_arr;
 }
