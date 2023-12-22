e3bb32ebd418073406440ed350599da3419d7491
Vinevskaya Masha <mv2004.ru@mail.ru>
2023-12-22 23:27:08 +0300

    up lab_3

--- Lab1C/lab3.cpp
+++ Lab1C/lab3.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include "queue.h"
 
 using namespace std;
@@ -18,76 +17,37 @@ struct Request
     int arrivalTime;
 };
 
-void processRequests(const vector<vector<int>>& queues, ofstream& output)
+void processRequests(const vector<Queue*>& queues, ofstream& output)
 {
-    Queue* queue1 = queue_create();
-    Queue* queue2 = queue_create();
-    Queue* queue3 = queue_create();
-
     vector<Request> processedRequests;
-
-    for (int i = 1; i <= 3; i++)
-    {
-        for (int arrivalTime : queues[i - 1])
-        {
-            switch (i)
-            {
-            case 1:
-                queue_insert(queue1, arrivalTime);
-                break;
-            case 2:
-                queue_insert(queue2, arrivalTime);
-                break;
-            case 3:
-                queue_insert(queue3, arrivalTime);
-                break;
-            }
-        }
-    }
-
     int currentTime = 0;
 
-    while (!queue_empty(queue1) || !queue_empty(queue2) || !queue_empty(queue3))
+    while (true)
     {
         int minQueueNum = 0;
+        int minQueueRequest = INT_MAX;
 
-        // Find the queue with the earliest request
-        if (!queue_empty(queue1))
+        // Find the queue and request with the earliest arrival time
+        for (int i = 0; i < queues.size(); i++)
         {
-            if (queue_get(queue1) <= currentTime)
-                minQueueNum = 1;
+            if (!queue_empty(queues[i]))
+            {
+                int request = queue_front(queues[i]);
+                if (request <= currentTime && request < minQueueRequest)
+                {
+                    minQueueNum = i + 1;
+                    minQueueRequest = request;
+                }
+            }
         }
 
-        if (!queue_empty(queue2))
-        {
-            int queue2Request = queue_get(queue2);
-            if (queue2Request < queue_get(queue1) || minQueueNum == 0)
-                minQueueNum = 2;
-        }
-
-        if (!queue_empty(queue3))
-        {
-            int queue3Request = queue_get(queue3);
-            if (queue3Request < queue_get(queue1) || minQueueNum == 0 || queue3Request < queue_get(queue2))
-                minQueueNum = 3;
-        }
+        // Break the loop if all queues are empty
+        if (minQueueNum == 0)
+            break;
 
         // Process the earliest request from the selected queue
-        switch (minQueueNum)
-        {
-        case 1:
-            processedRequests.push_back({ 1, queue_get(queue1) });
-            queue_remove(queue1);
-            break;
-        case 2:
-            processedRequests.push_back({ 2, queue_get(queue2) });
-            queue_remove(queue2);
-            break;
-        case 3:
-            processedRequests.push_back({ 3, queue_get(queue3) });
-            queue_remove(queue3);
-            break;
-        }
+        processedRequests.push_back({ minQueueNum, minQueueRequest });
+        queue_remove(queues[minQueueNum - 1]);
 
         currentTime++;
     }
@@ -96,10 +56,6 @@ void processRequests(const vector<vector<int>>& queues, ofstream& output)
     {
         output << request.queueNum << " " << request.arrivalTime << endl;
     }
-
-    queue_delete(queue1);
-    queue_delete(queue2);
-    queue_delete(queue3);
 }
 
 int main()
@@ -107,24 +63,31 @@ int main()
     ifstream input("input.txt");
     ofstream output("output.txt");
 
-    vector<vector<int>> queues;
+    vector<Queue*> queues;
 
     for (int i = 0; i < 3; i++)
     {
         int requestCount;
         input >> requestCount;
 
-        vector<int> queueRequests(requestCount);
+        Queue* queue = queue_create();
 
         for (int j = 0; j < requestCount; j++)
         {
-            input >> queueRequests[j];
+            int arrivalTime;
+            input >> arrivalTime;
+            queue_insert(queue, arrivalTime);
         }
 
-        queues.push_back(queueRequests);
+        queues.push_back(queue);
     }
 
     processRequests(queues, output);
+
+    for (auto queue : queues)
+    {
+        queue_delete(queue);
+    }
 
     input.close();
     output.close();
