29fcaa41710bf383d272ee94bc088cca3e5c0bb1
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-12-22 17:42:53 +0300

    Update

--- Labs/Lab3.cpp
+++ Labs/Lab3.cpp
@@ -7,26 +7,8 @@
 
 using namespace std;
 
-vector<vector<int>> makeGraph(string input) 
-{
-	vector<vector<int>> graph;
-	graph.assign(10000, vector<int>(10000));
-
-	int indexV = (int)input[0];
-	int indexS = (int)input[3];
-	for (int i = 0; i < indexV; i++) {
-		if(graph[i][indexV] == NULL) {
-			graph[i][indexS] = indexS;
-		}
-		else {
-			graph[i][indexV] = indexV;
-			graph[i][indexS] = indexS;
-		}
-	}
-	return graph;
-}
 vector<bool> makeUsed(vector<vector<int>> graph,char Inital,Queue* queue) {
-	vector<bool> used(10000);
+	vector<bool> used(255);
 
 	int Init = (int)Inital;
 	queue_insert(queue, Init);
@@ -36,7 +18,7 @@ vector<bool> makeUsed(vector<vector<int>> graph,char Inital,Queue* queue) {
 		int cur = queue_get(queue);
 		queue_remove(queue);
 
-		for (Data neighboor : graph[cur]) {
+		for (int neighboor : graph[cur]) {
 			if (!used[neighboor]) {
 				queue_insert(queue, neighboor);
 				used[neighboor] = true;
@@ -47,28 +29,30 @@ vector<bool> makeUsed(vector<vector<int>> graph,char Inital,Queue* queue) {
 }
 int main() {
 	setlocale(LC_ALL, "Rus");
-	vector<vector<int>> graph;
-
 	
 	size_t n;
 	char Inital;
 	string input;
 	vector<bool> used;
 
+	vector<vector<int>> graph(255, vector<int>());
 	cout << "¬ведите кол-во химических элементов\n";
 	cin >> n;
 	cout << "¬ведите исходный химический элемент\n";
 	cin >> Inital;
-	cout << "¬водите химические реакции (через enter) по примеру:'A->B'";
+	cout << "¬водите химические реакции (через enter) по примеру:'A->B'\n";
 	for (size_t i = 0; i < n - 1; i++) {
 		cin >> input;
-		graph = makeGraph(input);
+		int indexV = (int)input[0];
+		int indexS = (int)input[3];
+
+		graph[indexV].push_back(indexS);
 	}
 
 	Queue* queue = queue_create();
 	used = makeUsed(graph, Inital, queue);
 	cout << "Ёлементы, которые мы можем получить из исходного\n";
-	for (int i = 0; used.size();i++) {
+	for (int i = 0; used.size(); i++) {
 		if (used[i]) {
 			cout << char(i) << "\n";
 		}
