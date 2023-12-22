4f855c1a432dc32e897d7b550759488bd50a882f
Vitaliy <144813657+debcomedian@users.noreply.github.com>
2023-12-22 18:28:47 +0300

    Update lab_3.cpp

--- Lab1CPP/lab3.cpp
+++ Lab1CPP/lab3.cpp
@@ -30,10 +30,10 @@
 using namespace std;
 
 
-void input (Data &n, Data &start, Data &finish, List* list) 
+bool input (Data &n, Data &start, Data &finish, List* list) 
 {
     ifstream fin( "..\\..\\..\\Lab1CPP\\input.txt");
-
+    bool flag = true;
     if (fin.is_open()) {
         char buf [16];
         fin >> buf;
@@ -42,24 +42,36 @@ void input (Data &n, Data &start, Data &finish, List* list)
         start = stoi(buf);
         fin >> buf;
         finish = stoi(buf);
-        while (fin >> buf) {
-            int num = stoi(buf);
-            fin >> buf;
-            int num2 = stoi(buf);
-            list_insert(list, num);
-            list_insert(list, num2);
-            list_insert(list, num2);
-            list_insert(list, num);
+        if (start > n || finish > n) {
+            flag = false;
+        }
+        if (flag) { 
+            while (fin >> buf) {
+                int num = stoi(buf);
+                fin >> buf;
+                int num2 = stoi(buf);
+                if (num > n || num2 > n) {
+                    flag = false;
+                    break;
+                }
+                list_insert(list, num);
+                list_insert(list, num2);
+                list_insert(list, num2);
+                list_insert(list, num);
+
+            }
         }
     }
     fin.close();
+    return flag;
 }
 
-void upd_queue_par(Data n, Data cnt_upd, Data upd_value, Queue* parents) 
+void upd_queue_par(Data n, Data upd_iter, Data upd_value, Queue* parents) 
 {
+    //прокрутка очереди для вставки значения
     for (Data i = 1; i <= n; i++) {
         Data num = queue_get(parents);
-        if (i == cnt_upd && num == 0) {
+        if (i == upd_iter && num == 0) {
             num = upd_value;
         } 
         queue_insert(parents, num);
@@ -67,28 +79,32 @@ void upd_queue_par(Data n, Data cnt_upd, Data upd_value, Queue* parents)
     }
 }
 
-bool check_value(Data n, Data value_get, Data value_next, List* list, Queue* parents) 
+bool check_value(Data n, Data parent_element, Data child_element, List* list, Queue* parents) 
 {
     bool flag = true;
-    for (ListItem* item = list_first(list); item; item = list_item_next(item)) {
-        if (value_next == list_item_data(item)) {
+    // проверка вершины на пройденность
+    for (ListItem* item = list_first(list); item; item = list_item_next(item)) 
+    {
+        if (child_element == list_item_data(item)) {
             flag = false;
             break;
         }
     }     
     if (flag) {
-        Data temp = -1;
-        for (Data i = 1; i <= n; i++) {
+        //проверка на одинакового родителя двух вершин
+        Data temp = 0;
+        for (Data i = 1; i <= n; i++) 
+        {
             Data num = queue_get(parents);
             if (num != 0 && temp == num) {
                 flag = false;
                 break;
             }
             
-            if (i == value_get || i == value_next) {
+            if (i == parent_element || i == child_element) {
                 temp = num;
             } 
-            
+            //прокрутка очереди для вставки значения
             queue_insert(parents, num);
             queue_remove(parents);
         }
@@ -96,67 +112,71 @@ bool check_value(Data n, Data value_get, Data value_next, List* list, Queue* par
     return flag;
 }
 
-void find_shortest_road(Data n, Data start, Data finish, List* adj_list, List* road) 
+List* find_shortest_road(Data n, Data start, Data finish, List* adj_list) 
 {
     Queue* queue = queue_create();
     Queue* parents = queue_create();
     List* visited_nodes = list_create();
+    List* road = list_create();
     //инициализация очереди родителей
     for (Data i = 1; i <= n; i++)
     {
         queue_insert(parents, 0);
     }
     queue_insert(queue, start);
-
+    //обход графа
     while (!queue_empty(queue))
     {    
-        Data value_get = queue_get(queue), value_next = 0;
-        if (value_get == finish && road && (start == finish || list_item_data(list_first(road)) != finish)) {
-            list_insert(road, finish);
+        Data parent_element = queue_get(queue), child_element = 0;
+        if (parent_element == finish ) {
+            if (road && (start == finish || list_item_data(list_first(road)) != finish)) {
+                list_insert(road, finish);
+            }
             break;
         } 
         queue_remove(queue);
-        list_insert(visited_nodes, value_get);
-        list_insert(road, value_get);
-        bool check_from_road = false;
-        //поиск следующих элементов
+        list_insert(visited_nodes, parent_element);
+        list_insert(road, parent_element);
+        bool dead_end_node = true;
+
         for( ListItem* item = list_first(adj_list); item; item = list_item_next(list_item_next(item)) ) 
         {
-            if (value_get == list_item_data(item)) 
+            if (parent_element == list_item_data(item)) 
             {
-                value_next = list_item_data(list_item_next(item));
+                child_element = list_item_data(list_item_next(item));
 
-                if (value_get == start || check_value(n, value_get, value_next, visited_nodes, parents)) {
-                    queue_insert(queue, value_next);
-                    check_from_road = true;
-                    upd_queue_par(n, value_next, value_get, parents);
+                if (parent_element == start || check_value(n, parent_element, child_element, visited_nodes, parents)) {
+                    dead_end_node = false;
+                    queue_insert(queue, child_element);
+                    upd_queue_par(n, child_element, parent_element, parents);
                 }
             }
         }
-        //если узел тупиковый, то удаляем элемент из пути
-        if (!check_from_road) {
+
+        if (dead_end_node) {
             list_erase_first(road);
         }
     }
-
+    if (list_first(road) == nullptr) {
+        road = nullptr;
+    }
     queue_delete(queue);
-    //queue_delete(distance);
     queue_delete(parents);
     list_delete(visited_nodes);
+    return road;
 }
 
-Data reverse_output(ListItem* road, ofstream& fout) {
-    if (list_item_next(road)) {
-        fout << reverse_output(list_item_next(road), fout) << ' ';
+Data reverse_output(ListItem* item, ofstream& fout) {
+    if (list_item_next(item)) {
+        fout << reverse_output(list_item_next(item), fout) << ' ';
     }
-    return list_item_data(road);
+    return list_item_data(item);
 }
 
 void output(List* road, Data start, Data finish) {
     ofstream fout;
     fout.open(("..\\..\\..\\Lab1CPP\\output.txt"));
-    if (road && (start == finish || list_item_data(list_first(road)) != start)) 
-    {
+    if (road && (start == finish || list_item_data(list_first(road)) != start)) {
         fout << reverse_output(list_first(road), fout);
     } else {
         fout << "IMPOSSIBLE";
@@ -166,18 +186,17 @@ void output(List* road, Data start, Data finish) {
 
 int main()
 {
-    
     List* adj_list = list_create();
-    List* road = list_create();
-    
     Data n = 0;
     Data start = 0, finish = 0;
-    
-    input(n, start, finish, adj_list);
-    find_shortest_road(n, start, finish, adj_list, road);
+    List* road = nullptr;
+    if (input(n, start, finish, adj_list)){
+        road = find_shortest_road(n, start, finish, adj_list);        
+    }
     output(road, start, finish);
-
     list_delete(adj_list);
-
+    if (road) {
+        list_delete(road);
+    }
     return 0;
 }
