57e0a343065f7d5b78af368ffd512d95f98e7644
Mirik <kihuge@yandex.ru>
2023-12-22 21:33:02 +0300

    3lab

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -16,38 +16,33 @@ void queue_delete(Queue *queue) {
 }
 
 void queue_enqueue(Queue *queue, const std::pair<int, int> &coordinates) {
-    vector_resize(queue->vector, vector_size(queue->vector) + 1);
-    vector_set(queue->vector, vector_size(queue->vector) - 1, coordinates);
+    vector_push_back(queue->vector, coordinates);
 }
 
 std::pair<int, int> queue_dequeue(Queue *queue) {
-    std::pair<int, int> frontValue = vector_get(queue->vector, 0);
-
-    for (size_t i = 0; i < vector_size(queue->vector) - 1; ++i) {
-        vector_set(queue->vector, i, vector_get(queue->vector, i + 1));
+    if (!queue_empty(queue)) {
+        std::pair<int, int> frontValue = vector_get(queue->vector, 0);
+        vector_erase(queue->vector, 0);
+        return frontValue;
     }
 
-    vector_resize(queue->vector, vector_size(queue->vector) - 1);
-    return frontValue;
+    return {-1, -1};  // Modify as needed
 }
 
 bool queue_empty(const Queue *queue) {
-    return vector_size(queue->vector) == 0;
+    return vector_empty(queue->vector);
 }
 
 std::pair<int, int> queue_get(const Queue *queue) {
     if (!queue_empty(queue)) {
         return vector_get(queue->vector, 0);
     }
-    return {-1, -1};  
+
+    return {-1, -1};  // Modify as needed
 }
 
 void queue_remove(Queue *queue) {
     if (!queue_empty(queue)) {
-        for (size_t i = 0; i < vector_size(queue->vector) - 1; ++i) {
-            vector_set(queue->vector, i, vector_get(queue->vector, i + 1));
-        }
-
-        vector_resize(queue->vector, vector_size(queue->vector) - 1);
+        vector_erase(queue->vector, 0);
     }
 }
