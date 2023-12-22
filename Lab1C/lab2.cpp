9bba19c63b3cc00337da4110e167f4cfd3f498f6
NastyaMorozova13 <144937667+NastyaMorozova13@users.noreply.github.com>
2023-12-22 22:51:19 +0300

    Create lab2.cpp

---
+++ Lab1C/lab2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "stack.h"
+#include <sstream>
+
+int Calc(const std::string& str) {
+    Stack* st = stack_create();
+    std::istringstream iss(expression);
+    std::string token;
+
+    while (iss >> token) {
+        if (token == "+" || token == "-" || token == "*" || token == "/") {
+            int b = stack_get(st);
+            stack_pop(st);
+            int a = stack_get(st);
+            stack_pop(st);
+
+            if (token == "+") stack_push(st, (a + b));
+            else if (token == "-") stack_push(st, (a - b));
+            else if (token == "*") stack_push(st, (a * b));
+            else if (token == "/") stack_push(st, (a / b));
+        }
+        else stack_push(st, (std::stoi(token)));
+    }
+
+    return stack_get(st);
+}
+
+int main() {
+    std::string str;
+
+    std::cout << "Input: ";
+    std::getline(std::cin, str);
+
+    int result = Calc(str);
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
