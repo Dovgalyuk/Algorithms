8449eb8f146b9fd3ddfa6cd1f00b5b8d00ecafd7
MAlutinS <gardenman0war@gmail.com>
2023-12-22 15:57:30 +0300

    lab3

---
+++ LabCPP/lab3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include "queue.h" 
+using namespace std;
+
+const int INF = 1000000000;
+
+vector<int> bfs(vector<vector<int>>& graph, int start) {
+    vector<int> dist(graph.size(), INF);
+    Queue* q = queue_create();
+
+    dist[start] = 0;
+    queue_insert(q, start);
+
+    while (!queue_empty(q)) {
+        int v = queue_get(q);
+        queue_remove(q);
+        for (int to = 0; to < graph.size(); ++to) {
+            if (graph[v][to] && dist[to] == INF) {
+                dist[to] = dist[v] + 1;
+                queue_insert(q, to);
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+    vector<vector<int>> graph = {
+        {0, 1, 0, 0},
+        {0, 0, 1, 1},
+        {1, 0, 0, 1},
+        {0, 1, 0, 0}}; 
+
+    int start = 0; 
+
+    vector<int> dist = bfs(graph, start);
+
+    for (int d : dist) {
+        if (d != INF)
+            cout << d << "\n";
+        else
+            cout << "X ";
+    }
+    return 0;
+}
+
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -2,6 +2,19 @@
 
 struct Vector
 {
+    Data* data;
+    size_t size;
+    size_t max_size;
+
+    Vector(){
+        size = 0;
+        max_size = 1;
+        data = new Data[max_size];
+    }
+
+    ~Vector(){
+        delete[] data;
+    }
 };
 
 Vector *vector_create()
@@ -17,18 +30,40 @@ void vector_delete(Vector *vector)
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    size_t size = vector->size;
+    if (size <= index){
+        throw "Error";
+    }
+    return vector->data[index];
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    size_t size = vector->size;
+    if (size <= index){
+        throw "Error";
+    }
+    vector->data[index] = value;
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    if (size <= vector->max_size){
+        vector->size = size;
+        return;
+    }
+    size_t max_max_size = size * 2;
+    Data* tmp = new Data[max_max_size];
+    for (size_t i = 0; i < vector->size; i++){
+        tmp[i] = vector->data[i];
+    }
+    delete[] vector->data;
+    vector->data = tmp;
+    vector->max_size = max_max_size;
+    vector->size = size;
 }
