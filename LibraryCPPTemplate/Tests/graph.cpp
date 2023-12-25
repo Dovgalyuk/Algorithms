caac4784c3254145291a0a06b6df718e332b7701
Gebrur <nikitamyhailov89@gmail.com>
2023-12-25 13:07:23 +0300

    fix test 2

--- LibraryCPPTemplate/Tests/graph.cpp
+++ LibraryCPPTemplate/Tests/graph.cpp
@@ -5,70 +5,64 @@ typedef Graph<int> MyGraph;
 
 int main() {
     size_t vertex_count = 9;
-    MyGraph* graph = new MyGraph(vertex_count, 1);
+    MyGraph graph(vertex_count, 1);
 
-    if (graph->getVertexAmount() != 9) {
+    if (graph.getVertexAmount() != 9) {
         std::cout << "Invalid vertex amount\n";
         return 1;
     }
 
     for (size_t i = 0; i < vertex_count; i++) {
-        graph->getVertex(i)->setVertexData(static_cast<int>(i));
+        graph.getVertex(i).setVertexData(static_cast<int>(i));
     }
 
     for (size_t i = 0; i < vertex_count; i++) {
-        if (graph->getVertex(i)->getVertexData() != static_cast<int>(i)) {
+        if (graph.getVertex(i).getVertexData() != static_cast<int>(i)) {
             std::cout << "Invalid vertex data\n";
             return 1;
         }
     }
 
-    graph->addEdge(0, 3, 15);
-    graph->addEdge(2, 1, 3);
-    graph->addEdge(3, 4, 5);
+    graph.addEdge(0, 3, 15);
+    graph.addEdge(2, 1, 3);
+    graph.addEdge(3, 4, 5);
 
-
-    if (!graph->isEdgeExist(2, 1)) {
+    if (!graph.isEdgeExist(2, 1)) {
         std::cout << "Invalid edge creation\n";
         return 1;
     }
 
-    if (graph != nullptr) {
-        auto it = graph->getIterator(0);
-        while (*it != nullptr) {
-            if ((*it)->getVertexData() != 3) {
-                std::cout << "Invalid iteration\n";
-                return 1;
-            }
-            ++it;
+    auto it = graph.getIterator(0);
+    while (*it != static_cast<size_t>(-1)) {
+        if (graph.getVertex(*it).getVertexData() != 3) {
+            std::cout << "Invalid iteration\n";
+            return 1;
         }
-    } else {
-        std::cout << "Graph does not exist\n";
-        return 1;
+        ++it;
     }
 
-    if (graph->getEdge(2, 1)->getEdgeData() != 3) {
+    if (graph.getEdge(2, 1).getEdgeData() != 3) {
         std::cout << "Invalid edge data\n";
         return 1;
     }
 
-    graph->getEdge(0, 3)->setEdgeData(30);
-    if (graph->getEdge(0, 3)->getEdgeData() != 30) {
+    graph.getEdge(0, 3).setEdgeData(30);
+    if (graph.getEdge(0, 3).getEdgeData() != 30) {
         std::cout << "Invalid edge data setting\n";
         return 1;
     }
 
-    graph->removeEdge(2, 1);
-    if (graph->isEdgeExist(2, 1)) {
+    graph.removeEdge(2, 1);
+    if (graph.isEdgeExist(2, 1)) {
         std::cout << "Invalid edge deletion\n";
         return 1;
     }
 
-    graph->removeVertex(3);
-    if (graph->getVertexAmount() != 8) {
+    graph.removeVertex(3);
+    if (graph.getVertexAmount() != 8) {
         std::cout << "Invalid vertex deletion\n";
         return 1;
     }
 
-    delete graph;
+    return 0;
 }
