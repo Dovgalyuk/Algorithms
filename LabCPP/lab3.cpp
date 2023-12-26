1f315eec3d231dd3db0faac67cc7ce6795e6159d
MAlutinS <gardenman0war@gmail.com>
2023-12-26 18:14:16 +0300

    refresh

--- LabCPP/lab3.cpp
+++ LabCPP/lab3.cpp
@@ -7,34 +7,40 @@ const int INF = 1000000000;
 
 vector<int> bfs(vector<vector<int>>& graph, int start) {
     vector<int> dist(graph.size(), INF);
-    Queue* q = queue_create();
+    Queue* que = queue_create();
 
     dist[start] = 0;
-    queue_insert(q, start);
+    queue_insert(que, start);
 
-    while (!queue_empty(q)) {
-        int v = queue_get(q);
-        queue_remove(q);
+    while (!queue_empty(que)) {
+        int CurrentV = queue_get(que);
+        queue_remove(que);
         for (size_t to = 0; to < graph.size(); ++to) {
-            if (graph[v][to] && dist[to] == INF) {
-                dist[to] = dist[v] + 1;
-                queue_insert(q, static_cast<int>(to));
+            if (graph[CurrentV][to] && dist[to] == INF) {
+                dist[to] = dist[CurrentV] + 1;
+                queue_insert(que, static_cast<int>(to));
             }
         }
     }
+    queue_delete(que);
     return dist;
 }
 
 int main() {
-    vector<vector<int>> graph = {
-        {0, 1, 0, 0},
-        {0, 0, 1, 1},
-        {1, 0, 0, 1},
-        {0, 1, 0, 0}}; 
+    int MatrixSize;
+    cout << "Enter the matrix size: ";
+    cin >> MatrixSize;
 
-    int start = 0; 
+    vector<vector<int>> graph(MatrixSize, vector<int>(MatrixSize));
 
-    vector<int> dist = bfs(graph, start);
+    cout << "Enter matrix elements:" << endl;
+    for (int Row = 0; Row < MatrixSize; ++Row){
+        for (int Column = 0; Column < MatrixSize; ++Column){
+            cin >> graph[Row][Column];
+        }
+    }
+    cout << endl;
+    vector<int> dist = bfs(graph, 0);
 
     for (int d : dist) {
         if (d != INF)
