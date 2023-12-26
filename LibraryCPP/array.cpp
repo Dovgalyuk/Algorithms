4a5102404a15196b8a69dc2c2afb57fa73679585
LyubovSat <sat.lyubov@gmail.com>
2023-12-26 12:00:15 +0300

    Update array.cpp

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,9 +1,10 @@
 #include "array.h"
+#include <stdlib.h>
 
 struct Array
 {
-    int ArrSize;
-    int ArrData;
+    size_t ArrSize;
+    Data* ArrData;
 };
 
 // create array
@@ -11,31 +12,32 @@ Array *array_create(size_t size)
 {
     Array* NewArray = new Array;
     NewArray->ArrSize = size;
-
-
-    
+    NewArray->ArrData = new Data[size];
     return new Array;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    delete[] arr->ArrData;
     delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    return arr->ArrData[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (arr->ArrSize > index && index >= 0)
+        arr->ArrData[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->ArrSize;
 }
