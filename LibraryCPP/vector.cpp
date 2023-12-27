241c6059419615b7eb3688bc09a02fd631aaab8b
Alav1e <144486832+Alav1e@users.noreply.github.com>
2023-12-27 13:16:24 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -73,7 +73,7 @@ struct Vector
     Data* data;
     size_t size;
     size_t capacity;
-  // const size_t size_max = 2;
+    const size_t size_max = 2;
 
     // Конструктор
     Vector() : data(nullptr), size(0), capacity(0) {}
@@ -99,7 +99,7 @@ Data vector_get(const Vector* vector, size_t index)
 {
     if (index < vector->size)
     {
-        return vector->data[index];
+        return *(vector->data + index);
     }
     return Data(); // Возвращаем значение по умолчанию, если индекс неверный
 }
@@ -108,7 +108,7 @@ void vector_set(Vector* vector, size_t index, Data value)
 {
     if (index < vector->size)
     {
-        vector->data[index] = value;
+        *(vector->data + index) = value;
     }
 }
 
@@ -128,7 +128,7 @@ void vector_resize(Vector* vector, size_t size)
         Data* new_data = new Data[size];
         for (size_t i = 0; i < vector->size; i++)
         {
-            new_data[i] = vector->data[i];
+            *(new_data + i) = *(vector->data + i);
         }
         delete[] vector->data;
         vector->data = new_data;
