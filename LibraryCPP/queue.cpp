dc93cb66db4d0522e21176d34a17205312f94d0a
Mirik <kihuge@yandex.ru>
2023-12-27 22:03:59 +0300

    3lab

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -2,13 +2,15 @@
 
 struct Queue {
     Vector* vector;
-    size_t front;  
+    size_t front;
+    size_t size; 
 };
 
 Queue* queue_create() {
     Queue* queue = new Queue;
     queue->vector = vector_create();
-    queue->front = 0;  
+    queue->front = 0;
+    queue->size = 0; 
     return queue;
 }
 
@@ -20,8 +22,9 @@ void queue_delete(Queue* queue) {
 void queue_insert(Queue* queue, Data data) {
     size_t size = vector_size(queue->vector);
     vector_resize(queue->vector, size + 1);
-    size_t back = (queue->front + size) % vector_size(queue->vector);
+    size_t back = (queue->front + queue->size) % vector_size(queue->vector);
     vector_set(queue->vector, back, data);
+    queue->size++;
 }
 
 Data queue_get(const Queue* queue) {
@@ -30,12 +33,12 @@ Data queue_get(const Queue* queue) {
 
 void queue_remove(Queue* queue) {
     if (!queue_empty(queue)) {
-        size_t size = vector_size(queue->vector);
-        queue->front = (queue->front + 1) % size;
-        vector_resize(queue->vector, size - 1);
+        queue->front = (queue->front + 1) % vector_size(queue->vector);
+        queue->size--;
+        vector_resize(queue->vector, queue->size); 
     }
 }
 
 bool queue_empty(const Queue* queue) {
-    return vector_size(queue->vector) == 0;
+    return queue->size == 0;
 }
