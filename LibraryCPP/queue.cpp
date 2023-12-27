5cb45b4a3ec783492df97135d6aa50d37ab2006d
Mirik <kihuge@yandex.ru>
2023-12-27 22:29:01 +0300

    3lab

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -21,8 +21,11 @@ void queue_delete(Queue* queue) {
 
 void queue_insert(Queue* queue, Data data) {
     size_t size = vector_size(queue->vector);
-    vector_resize(queue->vector, size + 1);
-    size_t back = (queue->front + queue->size) % (size + 1);
+    if (queue->size == size) {
+        vector_resize(queue->vector, (size == 0) ? 1 : size * 2);
+    }
+    size_t back = (queue->front + queue->size) % vector_size(queue->vector);
+
     vector_set(queue->vector, back, data);
     queue->size++;
 }
@@ -35,7 +38,9 @@ void queue_remove(Queue* queue) {
     if (!queue_empty(queue)) {
         queue->front = (queue->front + 1) % vector_size(queue->vector);
         queue->size--;
-        vector_resize(queue->vector, queue->size);
+        if (queue->size <= vector_size(queue->vector) / 4 && vector_size(queue->vector) > 1) {
+            vector_resize(queue->vector, vector_size(queue->vector) / 2);
+        }
     }
 }
 
