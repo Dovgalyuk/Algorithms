b4c0d6254afe023a01f918ff7ad66a4f84a5045a
IgorAndreev24 <144487334+IgorAndreev24@users.noreply.github.com>
2023-12-28 18:16:07 +0300

    Update vector.cpp

--- LibraryCPP/Tests/vector.cpp
+++ LibraryCPP/Tests/vector.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
 #include "vector.h"
 
-void myfree(void *p)
-{
-    delete (int*)p;
-}
-
-int vector_get_int(Vector *v, size_t i)
-{
-    return *(int*)vector_get(v, i);
-}
-
 int main()
 {
-    Vector *vector = vector_create(myfree);
+    Vector* vector = vector_create();
 
     vector_resize(vector, 5);
     if (vector_size(vector) != 5)
@@ -22,12 +12,12 @@ int main()
         return 1;
     }
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-        vector_set(vector, i, new int(i));
+    for (size_t i = 0; i < vector_size(vector); ++i)
+        vector_set(vector, i, (int)i);
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
     {
-        if (vector_get_int(vector, i) != (int)i)
+        if (vector_get(vector, i) != (int)i)
         {
             std::cout << "Invalid vector element " << i << "\n";
             return 1;
@@ -40,11 +30,10 @@ int main()
         std::cout << "Invalid resize\n";
         return 1;
     }
-    for (size_t i = 0; i < vector_size(vector); ++i)
-        vector_set(vector, i, new int(i));
+
     std::cout << "Vector: ";
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-        std::cout << vector_get_int(vector, i) << " ";
+    for (size_t i = 0; i < vector_size(vector); ++i)
+        std::cout << vector_get(vector, i) << " ";
     std::cout << "\n";
 
     vector_resize(vector, 3);
@@ -54,9 +43,9 @@ int main()
         return 1;
     }
 
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    for (size_t i = 0; i < vector_size(vector); ++i)
     {
-        if (vector_get_int(vector, i) != (int)i)
+        if (vector_get(vector, i) != (int)i)
         {
             std::cout << "Invalid vector element " << i << "\n";
             return 1;
@@ -64,20 +53,20 @@ int main()
     }
 
     std::cout << "Vector: ";
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-        std::cout << vector_get_int(vector, i) << " ";
+    for (size_t i = 0; i < vector_size(vector); ++i)
+        std::cout << vector_get(vector, i) << " ";
     std::cout << "\n";
 
     // Performance test
-    for (int i = 1 ; i <= 10000000 ; ++i)
+    for (int i = 1; i <= 10000000; ++i)
     {
         vector_resize(vector, i);
-        vector_set(vector, i - 1, new int(i));
+        vector_set(vector, i - 1, i);
     }
 
     long long sum = 0;
-    for (int i = 0 ; i < 10000000 ; ++i)
-        sum += vector_get_int(vector, i);
+    for (int i = 0; i < 10000000; ++i)
+        sum += vector_get(vector, i);
 
     std::cout << sum << "\n";
 
