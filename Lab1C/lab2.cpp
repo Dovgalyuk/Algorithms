64b3ae6a9d01c074388f0afeb91cfc82d22bcbd4
thatpel <zhenya.mitroshenko@yandex.ru>
2023-12-28 06:14:38 +0300

    final

--- Lab1C/lab2.cpp
+++ Lab1C/lab2.cpp
@@ -23,20 +23,29 @@ int main()
 
     Stack* stack = stack_create(); 
 
-	size_t size = 0;
+
+	size_t  size = 0, wrongsize = 0;
     char ch;
+	bool flag = 0;
 	while (inputFile.get(ch)){
-		while (ch != '*'){
+		if (ch == '*'){
+			flag = true;
+		}
+		if (flag == 0){
 			stack_push(stack, ch);
 			size += 1;
 		}
 		if (!stack_empty(stack) && ((stack_get(stack) == ch) )) {
 			stack_pop(stack);
 			size -= 1;
+			wrongsize -= 1;
+		}
+		else {
+			wrongsize += 1;
 		}
 	}
 
-	if (stack_empty(stack) && size == 0) { 
+	if ((stack_empty(stack) && size == 0 ) && (wrongsize == 1)) { 
 		printf("YES\n");
 	}
 	else {
