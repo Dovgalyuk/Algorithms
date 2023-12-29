9f5436c7a29d6b80c67ddd048b73c26019093f20
INGAEVT <93099989+INGAEVT@users.noreply.github.com>
2023-12-29 15:43:16 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,36 @@
 #include "stack.h"
 
-struct Stack
+Stack* stack_create()
 {
-};
-
-Stack *stack_create()
-{
-    return new Stack;
+    Stack* stack = new Stack;
+    stack->list = list_create();
+    return stack;
 }
 
-void stack_delete(Stack *stack)
+void stack_delete(Stack* stack)
 {
     // TODO: free stack elements
+    list_delete(stack->list);
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
+void stack_push(Stack* stack, Data data)
 {
+    list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack)
+Data stack_get(const Stack* stack)
 {
-    return (Data)0;
+    ListItem* first = list_first(stack->list);
+    return list_item_data(first);
 }
 
-void stack_pop(Stack *stack)
+void stack_pop(Stack* stack)
 {
+    list_erase_first(stack->list);
 }
 
-bool stack_empty(const Stack *stack)
+bool stack_empty(const Stack* stack)
 {
-    return true;
+    return !list_first(stack->list);
 }
