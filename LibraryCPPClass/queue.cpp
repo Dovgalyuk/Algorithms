ed8eb9f5e598f5adbe4f3b329a53d40c79ef29ab
makargravanov <121590203+makargravanov@users.noreply.github.com>
2024-01-05 02:30:38 +0300

    Update queue.cpp

--- LibraryCPPClass/queue.cpp
+++ LibraryCPPClass/queue.cpp
@@ -1,57 +1,49 @@
 #include "queue.h"
 #include "vector.h"
 
-template <typename T>
-class Queue
-{
-public:
-    Queue() : vec(Vector()), head(0), tail(0) {}
+Vector::Vector() : size_(0), max_size_(1), data_(new Data[max_size_]) {}
 
-    Queue(const Queue& a) : vec(a.vec), head(a.head), tail(a.tail) {}
+Vector::Vector(const Vector& a) : size_(a.size_), max_size_(a.max_size_), data_(new Data[a.max_size_]) {
+    std::copy(a.data_, a.data_ + a.size_, data_);
+}
 
-    Queue& operator=(const Queue& a)
-    {
-        if (this != &a)
-        {
-            vec = a.vec;
-            head = a.head;
-            tail = a.tail;
-        }
-        return *this;
+Vector& Vector::operator=(const Vector& a) {
+    if (this != &a) {
+        delete[] data_;
+        size_ = a.size_;
+        max_size_ = a.max_size_;
+        data_ = new Data[a.max_size_];
+        std::copy(a.data_, a.data_ + a.size_, data_);
     }
+    return *this;
+}
 
-    ~Queue() {}
+Vector::~Vector() { delete[] data_; }
 
-    void insert(Data data)
-    {
-        if (vec.size() == 0)
-            vec.resize(1);
-        else if (tail == vec.size())
-            vec.resize(vec.size() * 2);
-        vec.set(tail++, data);
+Data Vector::get(size_t index) const {
+    if (size_ <= index) throw "Error";
+    return data_[index];
+}
+
+void Vector::set(size_t index, Data value) {
+    if (size_ <= index) throw "Error";
+    data_[index] = value;
+}
+
+size_t Vector::size() const { return size_; }
+
+void Vector::resize(size_t size) {
+    if (size <= max_size_) {
+        size_ = size;
+        return;
     }
-
-    Data get() const
-    {
-        if (empty())
-            throw std::out_of_range("Queue is empty");
-        return vec.get(head);
+    size_t max_max_size = size * 2;
+    Data* tmp = new Data[max_max_size];
+    for (size_t i = 0; i < size_; i++) {
+        tmp[i] = data_[i];
     }
-
-    void remove()
-    {
-        if (empty())
-            throw std::out_of_range("Queue is empty");
-        head++;
-    }
-
-    bool empty() const
-    {
-        return head == tail;
-    }
-
-private:
-    Vector vec;
-    size_t head;
-    size_t tail;
-};
+    delete[] data_;
+    data_ = tmp;
+    max_size_ = max_max_size;
+    size_ = size;
+}
