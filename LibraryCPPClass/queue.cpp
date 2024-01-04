0b838997e41fda4aa2b524edf3e579f2140be6bb
makargravanov <121590203+makargravanov@users.noreply.github.com>
2024-01-05 01:43:10 +0300

    Update queue.cpp upd0

--- LibraryCPPClass/queue.cpp
+++ LibraryCPPClass/queue.cpp
@@ -1,38 +1,57 @@
 #include "queue.h"
+#include "vector.h"
 
-Queue::Queue()
+template <typename T>
+class Queue
 {
-}
+public:
+    Queue() : vec(Vector()), head(0), tail(0) {}
 
-Queue::Queue(const Queue &a)
-{
-    // implement or disable this function
-}
+    Queue(const Queue& a) : vec(a.vec), head(a.head), tail(a.tail) {}
 
-Queue &Queue::operator=(const Queue &a)
-{
-    // implement or disable this function
-    return *this;
-}
+    Queue& operator=(const Queue& a)
+    {
+        if (this != &a)
+        {
+            vec = a.vec;
+            head = a.head;
+            tail = a.tail;
+        }
+        return *this;
+    }
 
-Queue::~Queue()
-{
-}
+    ~Queue() {}
 
-void Queue::insert(Data data)
-{
-}
+    void insert(Data data)
+    {
+        if (vec.size() == 0)
+            vec.resize(1);
+        else if (tail == vec.size())
+            vec.resize(vec.size() * 2);
+        vec.set(tail++, data);
+    }
 
-Data Queue::get() const
-{
-    return Data();
-}
+    Data get() const
+    {
+        if (empty())
+            throw std::out_of_range("Queue is empty");
+        return vec.get(head);
+    }
 
-void Queue::remove()
-{
-}
+    void remove()
+    {
+        if (empty())
+            throw std::out_of_range("Queue is empty");
+        head++;
+    }
 
-bool Queue::empty() const
-{
-    return true;
-}
+    bool empty() const
+    {
+        return head == tail;
+    }
+
+private:
+    Vector vec;
+    size_t head;
+    size_t tail;
+};
