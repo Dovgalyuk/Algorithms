3018c3892804102ed94d44e66261d1b043746073
makargravanov <121590203+makargravanov@users.noreply.github.com>
2024-01-08 23:41:39 +0300

    Update vector.h

--- LibraryCPPTemplate/vector.h
+++ LibraryCPPTemplate/vector.h
@@ -10,73 +10,53 @@ class Vector
 public:
     Vector() : data_(new Data[1]), size_(0), max_size_(1) {}
 
-    template <typename T>
-    Vector(const Vector<T>& a) : data_(new Data[a.max_size_]), size_(a.size_), max_size_(a.max_size_) {
+    Vector(const Vector& a) : data_(new Data[a.max_size_]), size_(a.size_), max_size_(a.max_size_) {
         std::copy(a.data_, a.data_ + a.size_, data_);
     }
 
-    template <typename T>
-    Vector& operator=(const Vector<T>& a) {
-    if ((void*)this != (void*)&a) {
-        delete[] data_;
-        data_ = new Data[a.max_size_];
-        size_ = a.size_;
-        max_size_ = a.max_size_;
-        std::copy(a.data_, a.data_ + a.size_, data_);
+    Vector& operator=(const Vector& a) {
+        if (this != &a) {
+            Data* new_data = new Data[a.max_size_];
+            std::copy(a.data_, a.data_ + a.size_, new_data);
+            delete[] data_;
+            data_ = new_data;
+            size_ = a.size_;
+            max_size_ = a.max_size_;
+        }
+        return *this;
     }
-    return *this;
-}
-
 
     ~Vector() {
         delete[] data_;
-        data_ = nullptr;
     }
 
     Data get(size_t index) const {
-        if (size_ <= index) throw "Error";
+        if (index >= size_) throw std::out_of_range("Index out of range");
         return data_[index];
     }
 
     void set(size_t index, Data value) {
-        if (size_ <= index) throw "Error";
+        if (index >= size_) throw std::out_of_range("Index out of range");
         data_[index] = value;
     }
 
     size_t size() const { return size_; }
 
     void resize(size_t size) {
-        if (size >= max_size_) {
-           size_t new_max_size = size * 2;
-           Data* tmp = new Data[new_max_size];
-           for (size_t i = 0; i < size_; i++) {
-               tmp[i] = data_[i];
-          }
-           // Проверка на nullptr перед удалением
-           if (data_ != nullptr) {
-               delete[] data_;
-           }
-           data_ = tmp;
-           max_size_ = new_max_size;
-           }
-         size_ = size;
-    }
-
-    /*void push_back(Data value) {
-        while (size_ >= max_size_) {
-            size_t new_max_size = max_size_ * 2;
-            Data* new_data = new Data[new_max_size];
-            for (size_t i = 0; i < size_; i++) {
-                new_data[i] = data_[i];
-            }
-            delete[] data_;
-            data_ = new_data;
-            max_size_ = new_max_size;
+        if (size <= max_size_) {
+            size_ = size;
+            return;
         }
-        data_[size_] = value;
-        size_++;
-    }*/
-
+        size_t new_max_size = size * 2;
+        Data* new_data = new Data[new_max_size];
+        for (size_t i = 0; i < size_; ++i) {
+            new_data[i] = data_[i];
+        }
+        delete[] data_;
+        data_ = new_data;
+        max_size_ = new_max_size;
+        size_ = size;
+    }
 
 private:
     Data* data_;
