d589b468460b3c3283bb5bf6cd0512133d020277
makargravanov <121590203+makargravanov@users.noreply.github.com>
2024-01-08 23:23:05 +0300

    Update vector.h

--- LibraryCPPTemplate/vector.h
+++ LibraryCPPTemplate/vector.h
@@ -46,19 +46,20 @@ public:
     size_t size() const { return size_; }
 
     void resize(size_t size) {
-        if (size <= max_size_) {
-            size_ = size;
-            return;
-        }
-        size_t max_max_size = size * 2;
-        Data* tmp = new Data[max_max_size];
-        for (size_t i = 0; i < size_; i++) {
-            tmp[i] = data_[i];
-        }
-        delete[] data_;
-        data_ = tmp;
-        max_size_ = max_max_size;
-        size_ = size;
+        if (size >= max_size_) {
+           size_t new_max_size = size * 2;
+           Data* tmp = new Data[new_max_size];
+           for (size_t i = 0; i < size_; i++) {
+               tmp[i] = data_[i];
+          }
+           // Проверка на nullptr перед удалением
+           if (data_ != nullptr) {
+               delete[] data_;
+           }
+           data_ = tmp;
+           max_size_ = new_max_size;
+           }
+         size_ = size;
     }
 
     void push_back(Data value) {
