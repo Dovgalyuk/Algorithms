31d587ff1c0b384dda95ac37547a29fcc340bb9f
makargravanov <121590203+makargravanov@users.noreply.github.com>
2024-01-08 23:14:54 +0300

    Update vector.h

--- LibraryCPPTemplate/vector.h
+++ LibraryCPPTemplate/vector.h
@@ -10,12 +10,12 @@ class Vector
 public:
     Vector() : data_(new Data[1]), size_(0), max_size_(1) {}
 
-
+    template <typename T>
     Vector(const Vector<T>& a) : data_(new Data[a.max_size_]), size_(a.size_), max_size_(a.max_size_) {
         std::copy(a.data_, a.data_ + a.size_, data_);
     }
 
-
+    template <typename T>
     Vector& operator=(const Vector<T>& a) {
         if (this != &a) {
             if (data_ != nullptr) {
@@ -26,14 +26,12 @@ public:
             max_size_ = a.max_size_;
             std::copy(a.data_, a.data_ + a.size_, data_);
         }
-        return *this;
     }
 
 
     ~Vector() {
-        if (data_ != nullptr) {
-            delete[] data_;
-        }
+        delete[] data_;
+        data_ = nullptr;
     }
 
     Data get(size_t index) const {
@@ -48,21 +46,22 @@ public:
 
     size_t size() const { return size_; }
 
-    void resize(size_t size) {
-        if (size <= max_size_) {
-            size_ = size;
-            return;
+    void Vector::resize(size_t size) {
+        if (size > max_size_) {
+            size_t new_max_size = size * 2;
+            Data* tmp = new Data[new_max_size];
+            for (size_t i = 0; i < size_; i++) {
+                tmp[i] = data_[i];
+            }
+            if (data_ != nullptr) {
+                delete[] data_;
+            }
+            data_ = tmp;
+            max_size_ = new_max_size;
         }
-        size_t max_max_size = size * 2;
-        Data* tmp = new Data[max_max_size];
-        for (size_t i = 0; i < size_; i++) {
-            tmp[i] = data_[i];
-        }
-        delete[] data_;
-        data_ = tmp;
-        max_size_ = max_max_size;
         size_ = size;
     }
+
 
     void push_back(Data value) {
         while (size_ >= max_size_) {
