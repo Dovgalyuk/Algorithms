73c27b430cbd7a63858ec501757c4bd73d685c86
bvaleksch <6394008@gmail.com>
2024-09-07 11:15:04 +0300

    Implemented the first two points

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,62 @@
 
 struct Array
 {
+    size_t size;
+    Data *first;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    if (size == 0) return nullptr;
+
+    Array *arr = new Array;
+    if (!arr) return nullptr;
+
+    arr->size = size;
+    arr->first = new Data[size];
+    if (!(arr->first)){
+        delete arr;
+        return nullptr;
+    } 
+
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if (!arr) return;
+
+    delete[] arr->first;
     delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (!arr)
+        throw "Array pointer is null";
+    if (index >= arr->size)
+        throw "Index out of range";
+
+    return *(arr->first + index);
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (!arr)
+        throw "Array pointer is null";
+    if (index >= arr->size)
+        throw "Index out of range";
+
+    *(arr->first + index) = value; 
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    if (!arr) throw "Array pointer is null";
+    return arr->size;
 }
