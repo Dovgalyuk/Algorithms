c2e575e10fc5a6b8736a2278fc3665005462a34a
G-d <101927568+sYkunA1747@users.noreply.github.com>
2024-09-08 22:15:17 +0300

    Delete Lab_number_1 directory

--- Lab_number_1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/g0d/Algorithms/Lab_number_1
-# Build directory: /home/g0d/Algorithms/Lab_number_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Lab_number_1 "/home/g0d/Algorithms/Lab_number_1/build/program" "input_1.txt" "input_2.txt" "input_3.txt")
-set_tests_properties(Lab_number_1 PROPERTIES  _BACKTRACE_TRIPLES "/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;11;add_test;/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;0;")
