72b371e8c6a1aacc1d03d54420abeb635eaba108
sYkunA1747 <timoxasharik@gmail.com>
2024-09-08 12:44:49 +0300

    comment

---
+++ Lab_number_1/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/g0d/Algorithms/Lab_number_1
+# Build directory: /home/g0d/Algorithms/Lab_number_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestLab1_1C "/home/g0d/Algorithms/Lab_number_1/build/program" "/home/g0d/Algorithms/Lab_number_1/input_1.txt")
+set_tests_properties(TestLab1_1C PROPERTIES  _BACKTRACE_TRIPLES "/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;11;add_test;/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;0;")
+add_test(TestLab1_2C "/home/g0d/Algorithms/Lab_number_1/build/program" "/home/g0d/Algorithms/Lab_number_1/input_2.txt")
+set_tests_properties(TestLab1_2C PROPERTIES  _BACKTRACE_TRIPLES "/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;12;add_test;/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;0;")
+add_test(TestLab1_3C "/home/g0d/Algorithms/Lab_number_1/build/program" "/home/g0d/Algorithms/Lab_number_1/input_3.txt")
+set_tests_properties(TestLab1_3C PROPERTIES  _BACKTRACE_TRIPLES "/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;13;add_test;/home/g0d/Algorithms/Lab_number_1/CMakeLists.txt;0;")
