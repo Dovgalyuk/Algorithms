c1244733e1bf50fa4dfc2b22768421125fb2c6d2
sudo-odner <odner.main@gmail.com>
2024-09-09 09:19:29 +0300

    add fio

---
+++ cmake-build-debug/LibraryCPP/Tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/odner/GitHub/Algorithms/LibraryCPP/Tests
+# Build directory: /Users/odner/GitHub/Algorithms/cmake-build-debug/LibraryCPP/Tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build-debug/LibraryCPPTemplate/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/odner/GitHub/Algorithms/LibraryCPPTemplate
+# Build directory: /Users/odner/GitHub/Algorithms/cmake-build-debug/LibraryCPPTemplate
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
