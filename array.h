30220a028d33d40e48064abe0fd6ba3d3b412e07
tolstosup <154780185+tolstosup@users.noreply.github.com>
2024-09-10 17:25:12 +0300

    Add files via upload

---
+++ array.h
@@ -0,0 +1,34 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <cstddef>
+#include <time.h>
+
+// Non-resizeable array
+// Stores integer values inside
+typedef int Data;
+
+struct Array;
+
+// create array
+Array *array_create(size_t size);
+
+// delete array, free memory
+void array_delete(Array *arr);
+
+// returns specified array element
+Data array_get(const Array *arr, size_t index);
+
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value);
+
+// returns array size
+size_t array_size(const Array *arr);
+
+void array_erase(Array* arr, size_t index);
+
+Array* create_random_array(Data size)
+
+
+
+#endif
