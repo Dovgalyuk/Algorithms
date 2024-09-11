b274cf4cc665feda05a710c2c8c1eb1eca8b5c89
bvaleksch <6394008@gmail.com>
2024-09-11 15:21:57 +0300

    Implemented stack and list

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -1,12 +1,17 @@
 #include <cstddef>
 #include "list.h"
+#include <stdexcept>
 
 struct ListItem
 {
+    ListItem *previous_item = nullptr;
+    ListItem *next_item = nullptr;
+    Data value;
 };
 
 struct List
 {
+    ListItem *first = nullptr;
 };
 
 List *list_create()
@@ -16,46 +21,100 @@ List *list_create()
 
 void list_delete(List *list)
 {
-    // TODO: free items
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+
+    while (list->first)
+        list_erase_first(list);
+
     delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+
+    return list->first;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    if (!item) throw std::invalid_argument("The item pointer is nullptr");
+    return item->value;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    if (!item) throw std::invalid_argument("The item pointer is nullptr");
+    return item->next_item;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    if (!item) throw std::invalid_argument("The item pointer is nullptr");
+    return item->previous_item;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    ListItem *ptr = new ListItem;
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+
+    ptr->previous_item = nullptr;
+    ptr->next_item = list->first;
+    ptr->value = data;
+
+    if (list->first) {
+        list->first->previous_item = ptr;
+    }
+
+    list->first = ptr;
+
+    return ptr;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    ListItem *ptr = new ListItem;
+
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+    if (!item) throw std::invalid_argument("The item pointer is nullptr");
+
+    ptr->previous_item = item;
+    ptr->next_item = item->next_item;
+    ptr->value = data;
+
+    if (item->next_item) 
+        item->next_item->previous_item = ptr;
+    item->next_item = ptr;
+
+    return ptr;
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    ListItem *temp_ptr;
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+    if (!(list->first)) throw std::invalid_argument("The list is empty");
+
+    temp_ptr = list->first->next_item;
+    delete list->first;
+    list->first = temp_ptr;
+
+    return list->first;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    ListItem *temp_ptr;
+
+    if (!list) throw std::invalid_argument("The list pointer is nullptr");
+    if (!item) throw std::invalid_argument("The item pointer is nullptr");
+    if (!(item->next_item)) throw std::invalid_argument("The item doesn't have a next item");
+
+    temp_ptr = item->next_item->next_item;
+    if (temp_ptr) temp_ptr->previous_item = item;
+    delete item->next_item;
+    item->next_item = temp_ptr;
+    
+    return temp_ptr;
 }
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,52 @@
 #include "stack.h"
+#include "list.h"
+#include <stdexcept>
+
 
 struct Stack
 {
+    List *list = nullptr;
 };
 
 Stack *stack_create()
 {
-    return new Stack;
+    Stack *ptr = new Stack;
+    ptr->list = list_create();
+    return ptr;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    if (!stack) throw std::invalid_argument("The stack pointer is nullptr");
+
+    list_delete(stack->list);
     delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    if (!stack) throw std::invalid_argument("The stack pointer is nullptr");
+    list_insert(stack->list, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    if (!stack) throw std::invalid_argument("The stack pointer is nullptr");
+    if (stack_empty(stack)) throw std::invalid_argument("The stack is empty");
+    
+    return list_item_data(list_first(stack->list));
 }
 
 void stack_pop(Stack *stack)
 {
+    if (!stack) throw std::invalid_argument("The stack pointer is nullptr");
+    if (stack_empty(stack)) throw std::invalid_argument("The stack is empty");
+    
+    if (list_erase_first(stack->list)) return; //This construction is necessary to prevent the compiler from issuing an error about an ignored return value
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    if (!stack) throw std::invalid_argument("The stack pointer is nullptr");
+    return (list_first(stack->list) == nullptr);
 }
