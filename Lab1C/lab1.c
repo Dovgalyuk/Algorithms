455d914469aa1687565bf6a4a5dfe4e9097e522d
Igor-Porubov <149544865+Igor-Porubov@users.noreply.github.com>
2024-09-12 03:42:51 +0300

    Добавил Lab1C

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "array.h"
 
 Array *array_create_and_read(FILE *input)
@@ -8,25 +9,100 @@ Array *array_create_and_read(FILE *input)
     /* Create array */
     Array *arr = array_create(n, NULL);
     /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
+    for (size_t i = 0 ; i < (size_t)n ; ++i)
     {
-        int x;
-        fscanf(input, "%d", &x);
-        array_set(arr, i, x);
+        int* x = malloc(sizeof(int));
+        fscanf(input, "%d", x);
+        array_set(arr, i, (Data)x);
     }
     return arr;
 }
 
 void task1(Array *arr)
 {
+    int sum_of_1h = 0;
+    int sum_of_2h = 0;
+    size_t size = array_size(arr);
+    for (size_t i = 0; i < (size_t) (size/2); i++)
+    {
+        sum_of_1h += *(int*)array_get(arr, i);
+    }
+    for (size_t i = (size_t) (size/2); i < size; i++)
+    {
+        sum_of_2h += *(int*)array_get(arr, i);
+    }
+    if (sum_of_1h > sum_of_2h)
+    {
+        printf("More precipitation fell in the first half of the month/n");
+    }
+    else if (sum_of_1h < sum_of_2h)
+    {
+        printf("More precipitation fell in the second half of the month/n");
+    }
+    else
+    {
+        printf("equal amounts of precipitation fell in both halves of the month/n");
+    }
+
+    int first_decade = 0;
+    int second_decade = 0;
+    int third_decade = 0;
+    for (size_t i = 0; i < 10; i++)
+    {
+        first_decade += *(int*)array_get(arr, i);
+    }
+    for (size_t i = 10; i < 20; i++)
+    {
+        second_decade += *(int*)array_get(arr, i);
+    }
+    for (size_t i = 20; i < size; i++)
+    {
+        third_decade += *(int*)array_get(arr, i);
+    }
+    if ((first_decade > second_decade) && (first_decade > third_decade))
+    {
+        printf("More precipitation fell in the first decade of the month\n");
+    }
+    else if (second_decade > third_decade)
+    {
+        printf("More precipitation fell in the second decade of the month\n");
+    }
+    else
+    {
+        printf("More precipitation fell in the third decade of the month\n");
+    }
+    
+    
 }
 
-void task2(Array *arr)
+void task2(Array *arr, FILE *input)
 {
+    int a, b;
+    fscanf(input, "%d %d", &a, &b);
+    size_t end_of_arr = array_size(arr);
+    for (size_t i = 0; i < end_of_arr; i++)
+    {
+        if (*(int*)array_get(arr, i) >= a && *(int*)array_get(arr, i) <= b)
+        {
+            for (size_t j = i; j < end_of_arr-1; j++)
+            {
+                array_set(arr, j, array_get(arr, j+1));
+            }
+            end_of_arr--;
+            i--;
+        }
+    }
+    for (size_t i = 0; i < array_size(arr); i++)
+    {
+        printf("%d ", *(int*)array_get(arr, i));
+    }
+    printf("\n");
+    
 }
 
 int main(int argc, char **argv)
 {
+    printf("%s \n", argv[1]);
     Array *arr = NULL;
     FILE *input = fopen(argv[1], "r");
     arr = array_create_and_read(input);
@@ -34,7 +110,7 @@ int main(int argc, char **argv)
     array_delete(arr);
     /* Create another array here */
     arr = array_create_and_read(input);
-    task2(arr);
+    task2(arr, input);
     array_delete(arr);
     fclose(input);
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -3,12 +3,16 @@
 
 typedef struct Array
 {
+    size_t size;
+    Data elem_ptr;
 } Array;
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array* ptr = malloc(sizeof(Array)*size);
+    ptr->size = size;
+    return ptr;
 }
 
 // delete array, free memory
@@ -20,16 +24,17 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    return (Data) (arr + index)->elem_ptr;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    (arr + index)->elem_ptr = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return (size_t)(arr->size);
 }
