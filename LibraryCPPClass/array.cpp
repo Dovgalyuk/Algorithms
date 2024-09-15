c09b4e8baa539d9961cab19518287e249297a2f9
Ivan Tsvetkov <onixit.rar@gmail.com>
2024-09-15 20:34:44 +0300

    Update array.cpp

--- LibraryCPPClass/array.cpp
+++ LibraryCPPClass/array.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
-Array::Array(size_t size) : arrSize(size), arrData(new Data[size])
+Array::Array(size_t size) : arrSize(size), arrData(new Data[size]())
 {
+    for(size_t i = 0; i < m_size; i++) {
+        arrData[i] = 0;
+    }
 }
 
 Array::Array(const Array &a) : arrSize(a.arrSize), arrData(new Data[a.arrSize])
@@ -42,7 +45,7 @@ Data Array::get(size_t index) const
 void Array::set(size_t index, Data value)
 {
     if(index >= arrSize){
-        throw out_of_range("Index out of bounds\n");
+        throw out_of_range("Index out of bounds");
     }
     arrData[index] = value; 
 }
