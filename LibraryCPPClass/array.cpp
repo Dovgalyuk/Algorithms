c5e3eeb1aa646fb976644d6c108f0d66db48e788
onix1t <ioann.tsvetkov@gmail.com>
2024-09-15 19:11:31 +0300

    array.cpp fix and cmake test activation

--- LibraryCPPClass/array.cpp
+++ LibraryCPPClass/array.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Array::Array(size_t size) : arrSize(size), arrData(new Data[size)
+Array::Array(size_t size) : arrSize(size), arrData(new Data[size])
 {
 }
 
@@ -19,7 +19,7 @@ Array &Array::operator=(const Array &a)
         delete[] arrData;
         arrSize = a.arrSize;
         arrData = new Data[arrSize];
-        for(size_i i = 0; i < arrSize; ++i){
+        for(size_t i = 0; i < arrSize; ++i){
             arrData[i] = a.arrData[i];
         }
     }
@@ -34,7 +34,7 @@ Array::~Array()
 Data Array::get(size_t index) const
 {
     if(index >= arrSize){
-        cout throw out_of_range("Index out of bounds\n")
+        throw out_of_range("Index out of bounds\n");
     }
     return arrData[index];
 }
@@ -42,7 +42,7 @@ Data Array::get(size_t index) const
 void Array::set(size_t index, Data value)
 {
     if(index >= arrSize){
-        cout throw out_of_range("Index out of bounds\n")
+        throw out_of_range("Index out of bounds\n");
     }
     arrData[index] = value; 
 }
