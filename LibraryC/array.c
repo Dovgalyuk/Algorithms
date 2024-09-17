cfb98a888dc8b26b6afb947aca426b28d5823876
Igor Porubov <danerporubov@gmail.com>
2024-09-17 22:02:16 +0300

    4.8 try

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -4,7 +4,7 @@
 typedef struct Array
 {
     size_t size;
-    Data elem_ptr;
+    Data* elem_ptr;
 
 } Array;
 
@@ -13,7 +13,7 @@ Array *array_create(size_t size, FFree f)
 {
     Array* ptr = malloc(sizeof(Array));
     ptr->size = size;
-    ptr->elem_ptr = (Data) malloc(sizeof(int*) * size);
+    ptr->elem_ptr = (Data*) malloc(sizeof(Data) * size);
     return ptr;
 }
 
@@ -22,22 +22,22 @@ void array_delete(Array *arr)
 {
     for (size_t i = 0; i < array_size(arr); i++)
     {
-        free((void*)array_get(arr, i));
+        free((void*)arr->elem_ptr[i]);
     }
-    //free((void**)arr->elem_ptr);
+    free(arr->elem_ptr);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data) (((int**)arr->elem_ptr)[index]);
+    return (Data) arr->elem_ptr[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
-    ((int**)arr->elem_ptr)[index] = (int*) value;
+    arr->elem_ptr[index] = value;
 
 }
 
