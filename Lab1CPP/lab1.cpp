6f55630bda5a850c074802d6826bdaa1e5903b8c
Nikita Gromov <groniknov@yandex.ru>
2024-09-17 19:57:08 +0300

    Lab1

---
+++ Lab1CPP/lab1.cpp
@@ -0,0 +1,104 @@
+#include "array.h"
+#include <fstream>
+#include <iostream>
+
+
+Array* array_create_and_read(std::ifstream& input) {
+	size_t n;
+	if (!(input >> n)) {
+		throw std::invalid_argument("Failed");
+	}
+	Array* arr = array_create(n);
+	for (size_t i = 0; i < n; ++i) {
+		int x;
+		if (!(input >> x)) {
+			throw std::invalid_argument("Failed");
+		}
+		array_set(arr, i, x);
+	}
+	return arr;
+
+}
+
+void task1(std::ifstream& input)
+{
+	Array* arr = array_create_and_read(input);
+	int sum = 0;
+	for (size_t i = 0; i < array_size(arr); ++i) {
+		int val = array_get(arr, i);
+		if (val > 20) sum += val;
+	}
+	std::cout << "Does the sum of the elements (>20) exceed 100?:\n";
+	if (sum == 0) {
+		std::cout << "There in no such elements in array\n";
+	}
+	else if (sum > 100) {
+		std::cout << "Yes\n";
+	}
+	else std::cout << "No\n";
+
+	sum = 0;
+	for (size_t i = 0; i < array_size(arr); ++i) {
+		int val = array_get(arr, i);
+		if (val < 50) sum += val;
+	}
+	std::cout << "Is the sum of the elements (<50) a multiple of 2?:\n";
+	if (sum == 0) {
+		std::cout << "There in no such elements in array\n";
+	}
+	else if (sum % 2 == 0) {
+		std::cout << "Yes\n";
+	}
+	else std::cout << "No\n";
+
+	array_delete(arr);
+
+}
+
+
+void task2(std::ifstream& input) {
+	Array* arr = array_create_and_read(input);
+	int a;
+	int b;
+	if (!(input >> a)) {
+		throw std::invalid_argument("Failed");
+	}
+	if (!(input >> b)) {
+		throw std::invalid_argument("Failed");
+	}
+
+	size_t ind = 0;
+
+	for (size_t i = 0; i < array_size(arr); i++) {
+		if (array_get(arr, i) < a || array_get(arr, i) > b) {
+			array_set(arr, ind++, array_get(arr, i));
+		}
+	}
+
+	while (ind < array_size(arr)) {
+		array_set(arr, ind++, 0);
+	}
+
+	std::cout << "Compressed array:\n";
+	for (size_t i = 0; i < array_size(arr); ++i) {
+		std::cout << array_get(arr, i) << " ";
+	}
+	std::cout << std::endl;
+
+	array_delete(arr);
+}
+
+
+int main() {
+	std::ifstream input("input.txt");
+	if (!input) {
+		std::cerr << "File is not found\n";
+		return 1;
+	}
+
+	task1(input);
+	task2(input);
+
+	input.close();
+
+}
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,50 @@
 #include "array.h"
+#include <stdexcept>
 
 struct Array
 {
+    Data* values;
+    size_t size;
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array;
+    if (size == 0) {
+    throw std::invalid_argument("Size can not be 0");
+    }
+    Array* arr = new Array;
+    arr->size = size;
+    arr->values = new Data[size];
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    delete[] arr->values; 
     delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
+    if (index < arr->size) {
+    return arr->values[index];
+    }
     return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index < arr->size) {
+    arr->values[index] = value;
+    }
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
