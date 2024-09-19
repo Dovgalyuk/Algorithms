65fb37a33525b1e2b9bab57fa81f47d78ca571e1
bvaleksch <6394008@gmail.com>
2024-09-19 16:51:32 +0300

    Implemented queue and vector

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,7 +1,16 @@
 #include "queue.h"
+#include "vector.h"
+#include <stdexcept>
+#include <cmath>
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define RESIZE_FACTOR 1.2
 
 struct Queue
 {
+    size_t size = 0;
+    size_t current_index = 0;
+    Vector *vector = vector_create();
 };
 
 Queue *queue_create()
@@ -11,24 +20,49 @@ Queue *queue_create()
 
 void queue_delete(Queue *queue)
 {
-    // TODO: free queue items
+    if (!queue) throw std::invalid_argument("The queue pointer is null");
+
+    vector_delete(queue->vector);
     delete queue;
 }
 
 void queue_insert(Queue *queue, Data data)
 {
+    if (!queue) throw std::invalid_argument("The queue pointer is null");
+
+    if ((queue->size + 1) > vector_size(queue->vector))
+        vector_resize(queue->vector, max(1, std::ceil(queue->size * RESIZE_FACTOR)));
+    
+    if ((queue->size + queue->current_index) >= vector_size(queue->vector)){
+        for (size_t ind{queue->current_index}; ind < (queue->size + queue->current_index); ++ind)
+            vector_set(queue->vector, ind - queue->current_index, vector_get(queue->vector, ind));
+        queue->current_index = 0;
+    }
+
+    vector_set(queue->vector, queue->current_index + queue->size, data);
+    queue->size++;
 }
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    if (!queue) throw std::invalid_argument("The queue pointer is null");
+    if (queue_empty(queue)) throw std::invalid_argument("The queue is empty");
+
+    return vector_get(queue->vector, queue->current_index);
 }
 
 void queue_remove(Queue *queue)
 {
+    if (!queue) throw std::invalid_argument("The queue pointer is null");
+    if (queue_empty(queue)) throw std::invalid_argument("The queue is empty");
+
+    queue->current_index++;
+    queue->size--;
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    if (!queue) throw std::invalid_argument("The queue pointer is null");
+
+    return (queue->size == 0);
 }
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,7 +1,12 @@
 #include "vector.h"
+#include <new>
+#include <stdexcept>
+
 
 struct Vector
 {
+    size_t size = 0;
+    Data *pointer = nullptr;
 };
 
 Vector *vector_create()
@@ -11,24 +16,48 @@ Vector *vector_create()
 
 void vector_delete(Vector *vector)
 {
-    // TODO: free vector internals
+    if (!vector) throw std::invalid_argument("The vector pointer is null");
+
+    if (vector->pointer) free(vector->pointer);
     delete vector; 
 }
 
 Data vector_get(const Vector *vector, size_t index)
 {
-    return (Data)0;
+    if (!vector) throw std::invalid_argument("The vector pointer is null");
+    if (vector->size <= index) throw std::out_of_range("The index is out of range");
+    
+    return vector->pointer[index];
 }
 
 void vector_set(Vector *vector, size_t index, Data value)
 {
+    if (!vector) throw std::invalid_argument("The vector pointer is null");
+    if (vector->size <= index) throw std::out_of_range("The index is out of range");
+    
+    vector->pointer[index] = value;
 }
 
 size_t vector_size(const Vector *vector)
 {
-    return 0;
+    if (!vector) throw std::invalid_argument("The vector pointer is null");
+
+    return vector->size;
 }
 
 void vector_resize(Vector *vector, size_t size)
 {
+    Data *ptr;
+
+    if (!vector) throw std::invalid_argument("The vector pointer is null");
+
+    if (vector->pointer)
+        ptr = (Data*)realloc(vector->pointer, sizeof(Data) * size);
+    else
+        ptr = (Data*)malloc(sizeof(Data) * size);
+        
+    if (!ptr) throw std::bad_alloc();
+    vector->pointer = ptr;
+    vector->size = size;
 }
+
