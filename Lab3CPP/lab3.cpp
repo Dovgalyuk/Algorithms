b6aef99bcb4e63babe9ab6a81470de61fb48fb7f
bvaleksch <6394008@gmail.com>
2024-09-21 21:38:33 +0300

    Refactored lab3.cpp

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -1,59 +1,36 @@
 #include <iostream>
-#include <string>
-#include <new>
-#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <iomanip>
+#include "vector.h"
 #include "queue.h"
 
-#define abs(x) (((x) >= 0) ? (x) : (-(x)))
-#define queue_insert(queue, ptr) queue_insert((queue), (Data)(ptr))
-#define queue_get(queue) ((Item*)queue_get((queue)))
+size_t get_figure(size_t num, size_t ind){
+    num /= std::pow(10, ind);
+    return num % 10;
+}
 
-
-struct Item{
-    size_t cnt = 0;
-    std::string state;
-    Item *previous_item = nullptr;
-
-    Item(std::string state, Item *prev_item = nullptr)
-    {
-        this->state = state;
-        this->previous_item = prev_item;
-        this->cnt = 1;
-    }
-
-    Item *get_link()
-    {
-        cnt++;
-        return this;
-    }
-
-    void forget()
-    {
-        if (cnt) cnt--;
-        if (!cnt){
-            if (previous_item) previous_item->forget();
-            delete this;
-        };
-    }
-};
-
-
-int get_states(std::string current_state, std::string *states)
+size_t get_states(size_t current_state, size_t *states)
 {
-    int index = 0;
-    int zero_index = current_state.find('0');
-    int row = zero_index / 3;
-    int col = zero_index % 3;
-    int new_row, new_col;
+    size_t index = 0;
+    size_t zero_index = 0;
+
+    while (get_figure(current_state, zero_index))
+        zero_index++;
+
+    size_t row = zero_index / 3;
+    size_t col = zero_index % 3;
+    size_t new_row, new_col, new_index, new_state, figure;
 
     for (int x = -1; x < 2; ++x){
         for (int y = -1; y < 2; ++y){
-            if (abs(x) == abs(y)) continue;
+            if (std::abs(x) == std::abs(y)) continue;
 
             new_row = row + y; new_col = col + x;
-            if (((new_row < 3) && (new_row >= 0)) && ((new_col < 3) && (new_col >= 0))){
-                std::string new_state = current_state;
-                std::swap(new_state[zero_index], new_state[new_row * 3 + new_col]);
+            new_index = new_row * 3 + new_col;
+            if ((new_row < 3) && (new_col < 3)){
+                figure = get_figure(current_state, new_index);
+                new_state = current_state + figure * std::pow(10, zero_index) - figure * std::pow(10, new_index);
                 states[index++] = new_state;
             }
         }
@@ -62,84 +39,87 @@ int get_states(std::string current_state, std::string *states)
     return index;
 }
 
-void show_answer(Item *last_item)
+void show_answer(size_t first_state, size_t state, std::unordered_map<size_t, size_t> *all_states)
 {
-    std::string answer = "";
+    size_t index = 0;
+    size_t states[4];
+    size_t count = (*all_states)[state];
+    size_t cnt;
+    Vector *vec = vector_create();
+    vector_resize(vec, count);
 
-    while (last_item){
-        answer = last_item->state + '\n' + answer;
-        last_item = last_item->previous_item;
-    }
+    while (true){
+        vector_set(vec, index++, state);
+        count--;
+        if (!count) break;
 
-    std::cout << answer;
-}
-
-void delete_all_items(Queue *queue)
-{
-    Item *ptr = nullptr;
-    while (!queue_empty(queue)){
-        ptr = queue_get(queue);
-        queue_remove(queue);
-        ptr->forget();
-    }
-}
-
-Item *search_solution(Queue *queue, std::string first_state, std::string target_state)
-{
-    Item *temp_item = nullptr;
-    Item *answer_item = nullptr;
-    std::unordered_set<std::string> states;
-    std::string new_states[4];
-    std::string current_state;
-
-    queue_insert(queue, new Item(first_state));
-    states.insert(first_state);
-    while (!queue_empty(queue) && !answer_item){
-        temp_item = queue_get(queue);
-        queue_remove(queue);
-        
-        int counter = get_states(temp_item->state, new_states);
-        for (int index_state = 0; index_state < counter; ++index_state){
-            current_state = new_states[index_state];
-            
-            if (current_state == target_state){
-                answer_item = new Item(current_state, temp_item->get_link());
-                break;
-            }
-            
-            if (states.find(current_state) == states.end()){
-                Item *new_item = new Item(current_state, temp_item->get_link());
-                queue_insert(queue, new_item);
-                states.insert(current_state);
+        cnt = get_states(state, states);
+        for (size_t ind{0}; ind < cnt; ++ind){
+            if ((*all_states).count(states[ind])){
+                if ((*all_states)[states[ind]] == count)
+                    state = states[ind];
             }
         }
-
-        temp_item->forget();
     }
 
-    return answer_item;
+    for (size_t ind{vector_size(vec)}; ind > 0; --ind)
+        std::cout << std::setw(9) << std::setfill('0') << vector_get(vec, ind - 1) << std::endl;
+    vector_delete(vec);
+}
+
+int search_solution(Queue *queue, size_t target_state, std::unordered_map<size_t, size_t> *all_states)
+{
+    size_t states[4];
+    size_t cnt;
+    size_t state;
+    size_t level = 1;
+
+    while (!queue_empty(queue)){
+        state = queue_get(queue);
+        level = (*all_states)[state] + 1;
+        cnt = get_states(state, states);
+        queue_remove(queue);
+
+        for (size_t ind{0}; ind < cnt; ++ind){
+            state = states[ind];
+
+            if (state == target_state){
+                all_states->emplace(state, level);
+                return 1;
+            } else if (!(*all_states).count(state)) {
+                all_states->emplace(state, level);
+                queue_insert(queue, state);
+            }
+        }
+    }
+
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    Item *ans = nullptr;
-    std::string target_state("123456780");
-    std::string first_state(argv[1]);
+    char *ptr = argv[1];
+    size_t target_state = 123456780;
+    size_t first_state = 0;
+    size_t ans;
+    while (*ptr >= '0' && *ptr <= '9')
+        first_state = 10 * first_state + (*ptr++ - '0');
 
     Queue *queue = queue_create();
+    std::unordered_map<size_t, size_t> all_states;
+    queue_insert(queue, first_state);
+    all_states.emplace(first_state, 1);
     
     if (first_state == target_state){
         std::cout << target_state;
     } else {
-        ans = search_solution(queue, first_state, target_state);
-        if (ans){
-            show_answer(ans);
-            ans->forget();
-        }
+        ans = search_solution(queue, target_state, &all_states);
+        if (ans)
+            show_answer(first_state, target_state, &all_states);
     }
 
-    delete_all_items(queue);
     queue_delete(queue);
 
     return 0;
 }
+
