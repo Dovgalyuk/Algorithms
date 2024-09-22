4af6299630d0f56a2e885f7c11a342233140ceeb
EKATERINAelnova <katakata45882@gmail.com>
2024-09-22 20:09:19 +0300

    completed lab1

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,46 @@
 #include "array.h"
+#include <iostream>
+#include <cstdlib>
 
-struct Array
-{
+struct Array {
+    Data *data;
+    size_t capacity;
 };
 
 // create array
-Array *array_create(size_t size)
-{
-    return new Array;
+Array* array_create(size_t initCapacity){
+    Array* arr = new Array;
+    arr->data = new Data[initCapacity];
+    arr->capacity = initCapacity;
+    return arr;
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
+void array_delete(Array* arr) {
+    delete[] arr->data; // Освобождаем память под массив
     delete arr;
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+int array_get(const Array* arr, size_t index) {
+    if (index < arr->capacity) {
+        return arr->data[index]; // Возвращаем элемент
+    } else {
+        std::cerr << "Index out of bounds\n"; // Сообщение об ошибке
+        exit(EXIT_FAILURE);
+    }
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+    if (index < arr->capacity) {
+        arr->data[index] = value; // Заменяем элемент
+    } else {
+        std::cerr << "Index out of bounds\n"; // Сообщение об ошибке
+    }
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr) {
+    return arr->capacity;
 }
