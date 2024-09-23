ad26d88bb308e55201907299a1237fa9630a7550
tolstosup <154780185+tolstosup@users.noreply.github.com>
2024-09-23 14:07:16 +0300

    Update stack.cpp

--- LibraryCPP/Tests/stack.cpp
+++ LibraryCPP/Tests/stack.cpp
@@ -1,128 +1,57 @@
-#include <cstddef>
-#include "list.h"
+#include <iostream>
+#include "stack.h"
 
-struct ListItem
+int main()
 {
-    Data value;
-    ListItem* next = nullptr;
-    ListItem* prev = nullptr;
-};
+    Stack *stack = stack_create();
 
-struct List
-{
-    ListItem* node = nullptr;
-};
+    stack_push(stack, 1);
+    stack_push(stack, 2);
+    stack_push(stack, 3);
 
-List *list_create()
-{
-    List* newList = new List;
-    return newList;
-}
-
-void list_delete(List* list)
-{
-    if (list != nullptr)
+    if (stack_get(stack) != 3)
     {
-        ListItem* current = list->node;
-        while (current != nullptr)
-        {
-            ListItem* next = current->next;
-            delete current;
-            current = next;
-        }
-        delete list;
-    }
-}
-
-
-ListItem *list_first(List *list)
-{
-    if (list != nullptr)
-        return list->node;
-    return nullptr;
-}
-
-Data list_item_data(const ListItem *item)
-{
-    if (item != nullptr)
-        return item->value;
-    return (Data)0;
-}
-
-ListItem *list_item_next(ListItem *item)
-{
-    if(item != nullptr && item->next != nullptr)
-        return item->next;
-    return nullptr;
-}
-
-ListItem *list_item_prev(ListItem *item)
-{
-    if (item != nullptr && item->prev != nullptr)
-        return item->prev;
-    return nullptr;
-}
-
-ListItem* list_insert(List* list, Data data)
-{
-    ListItem* newEl = new ListItem;
-    newEl->value = data;
-    newEl->prev = nullptr;
-
-    if (list->node != nullptr)
-        newEl->next = list->node;
-    else
-        newEl->next = nullptr;
-
-    list->node = newEl;
-
-    return newEl;
-}
-
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
-{
-    if (list != nullptr && item != nullptr)
-    {
-        ListItem* newEl = new ListItem;
-        newEl->value = data;
-        newEl->prev = item;
-
-        if(item->next != nullptr)
-            newEl->next = item->next;
-
-        item->next = newEl;
-
-        return newEl;
+        std::cout << "Invalid stack top after push\n";
+        stack_delete(stack);
+        return 1;
     }
 
-    return nullptr;
-}
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
 
-ListItem *list_erase_first(List *list)
-{
-    if (list != nullptr && list->node != nullptr)
+    if (stack_get(stack) != 2)
     {
-        ListItem* temp = list->node->next;
-        if(temp != nullptr)
-            temp->prev = nullptr;
-        delete list->node;
-        list->node = temp;
-
-        return list->node;
-    }
-    return NULL;
-}
-
-ListItem *list_erase_next(List *list, ListItem *item)
-{
-    if (list != nullptr && item != nullptr && item->next != nullptr)
-    {
-        item->next->next->prev = item;
-        ListItem* temp = item->next->next;
-        delete item->next;
-        item->next = temp;
-        return temp;
+        std::cout << "Invalid stack top after pop\n";
+        stack_delete(stack);
+        return 1;
     }
 
-    return nullptr;
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_pop(stack);
+
+    if (stack_get(stack) != 1)
+    {
+        std::cout << "Invalid stack top after pop\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    std::cout << "Get: " << stack_get(stack) << "\n";
+    stack_push(stack, 4);
+    stack_push(stack, 5);
+
+    if (stack_get(stack) != 5)
+    {
+        std::cout << "Invalid stack top after push\n";
+        stack_delete(stack);
+        return 1;
+    }
+
+    while (!stack_empty(stack))
+    {
+        std::cout << "Get: " << stack_get(stack) << "\n";
+        stack_pop(stack);
+    }
+
+    stack_delete(stack);
 }
