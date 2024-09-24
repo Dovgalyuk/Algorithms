7b9021b7932dda4cd5524ac891959344bd852912
bvaleksch <6394008@gmail.com>
2024-09-24 12:54:54 +0300

    Improve code structure for better understanding

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -5,8 +5,20 @@
 #include "vector.h"
 #include "queue.h"
 
-size_t get_figure(size_t num, size_t ind){
-    num /= std::pow(10, ind);
+constexpr size_t pow10(size_t n) 
+{
+    size_t ans = 1;
+    for (size_t ind{0}; ind < n; ++ind) 
+        ans *= 10;
+    return ans;
+}
+
+size_t get_digit(size_t num, size_t ind)
+{
+    constexpr size_t data[] {pow10(0), pow10(1), pow10(2),
+                             pow10(3), pow10(4), pow10(5),
+                             pow10(6), pow10(7), pow10(8)};
+    num /= data[ind];
     return num % 10;
 }
 
@@ -15,12 +27,12 @@ size_t get_states(size_t current_state, size_t *states)
     size_t index = 0;
     size_t zero_index = 0;
 
-    while (get_figure(current_state, zero_index))
+    while (get_digit(current_state, zero_index))
         zero_index++;
 
     size_t row = zero_index / 3;
     size_t col = zero_index % 3;
-    size_t new_row, new_col, new_index, new_state, figure;
+    size_t new_row, new_col, new_index, new_state, digit;
 
     for (int x = -1; x < 2; ++x){
         for (int y = -1; y < 2; ++y){
@@ -29,8 +41,8 @@ size_t get_states(size_t current_state, size_t *states)
             new_row = row + y; new_col = col + x;
             new_index = new_row * 3 + new_col;
             if ((new_row < 3) && (new_col < 3)){
-                figure = get_figure(current_state, new_index);
-                new_state = current_state + figure * std::pow(10, zero_index) - figure * std::pow(10, new_index);
+                digit = get_digit(current_state, new_index);
+                new_state = current_state + digit * std::pow(10, zero_index) - digit * std::pow(10, new_index);
                 states[index++] = new_state;
             }
         }
@@ -39,35 +51,52 @@ size_t get_states(size_t current_state, size_t *states)
     return index;
 }
 
-void show_answer(size_t first_state, size_t state, std::unordered_map<size_t, size_t> *all_states)
+size_t search_prev_state(size_t state, size_t count, std::unordered_map<size_t, size_t> &all_states)
 {
-    size_t index = 0;
+    size_t cnt = 0;
     size_t states[4];
-    size_t count = (*all_states)[state];
-    size_t cnt;
-    Vector *vec = vector_create();
-    vector_resize(vec, count);
 
-    while (true){
-        vector_set(vec, index++, state);
-        count--;
-        if (!count) break;
-
-        cnt = get_states(state, states);
-        for (size_t ind{0}; ind < cnt; ++ind){
-            if ((*all_states).count(states[ind])){
-                if ((*all_states)[states[ind]] == count)
-                    state = states[ind];
+    cnt = get_states(state, states);
+    for (size_t ind{0}; ind < cnt; ++ind){
+        /*
+         * We go through all the states from which we could come to this state
+         * And we check:
+         * 1) If we have ever visited such a state
+         * 2) If the number of this record equals count
+         * If these checks pass, then we found the desired state
+         */
+        if (all_states.count(states[ind])){
+            if (all_states[states[ind]] == count) {
+                state = states[ind];
+                break;
             }
         }
     }
+
+    return state;
+}
+
+void show_answer(size_t first_state, size_t state, std::unordered_map<size_t, size_t> &all_states)
+{
+    size_t index = 0;
+    size_t count = all_states[state];
+    Vector *vec = vector_create();
+    vector_resize(vec, count);
+
+    // Count is responsible for "Which record by number we need to insert"
+    // If it is one, we are at the starting position, so there is no need to search further - we exit the loop
+    while (count > 1) {
+        vector_set(vec, index++, state);
+        state = search_prev_state(state, --count, all_states);
+    }
+    vector_set(vec, index, state); // Insert the first state
 
     for (size_t ind{vector_size(vec)}; ind > 0; --ind)
         std::cout << std::setw(9) << std::setfill('0') << vector_get(vec, ind - 1) << std::endl;
     vector_delete(vec);
 }
 
-int search_solution(Queue *queue, size_t target_state, std::unordered_map<size_t, size_t> *all_states)
+int search_solution(Queue *queue, size_t target_state, std::unordered_map<size_t, size_t> &all_states)
 {
     size_t states[4];
     size_t cnt;
@@ -76,7 +105,7 @@ int search_solution(Queue *queue, size_t target_state, std::unordered_map<size_t
 
     while (!queue_empty(queue)){
         state = queue_get(queue);
-        level = (*all_states)[state] + 1;
+        level = all_states[state] + 1;
         cnt = get_states(state, states);
         queue_remove(queue);
 
@@ -84,10 +113,10 @@ int search_solution(Queue *queue, size_t target_state, std::unordered_map<size_t
             state = states[ind];
 
             if (state == target_state){
-                all_states->emplace(state, level);
+                all_states.emplace(state, level);
                 return 1;
-            } else if (!(*all_states).count(state)) {
-                all_states->emplace(state, level);
+            } else if (!all_states.count(state)) {
+                all_states.emplace(state, level);
                 queue_insert(queue, state);
             }
         }
@@ -113,13 +142,12 @@ int main(int argc, char **argv)
     if (first_state == target_state){
         std::cout << target_state;
     } else {
-        ans = search_solution(queue, target_state, &all_states);
+        ans = search_solution(queue, target_state, all_states);
         if (ans)
-            show_answer(first_state, target_state, &all_states);
+            show_answer(first_state, target_state, all_states);
     }
 
     queue_delete(queue);
 
     return 0;
 }
-
