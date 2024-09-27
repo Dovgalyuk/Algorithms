d0b4c43463a5bc77be7300f76b56742cd18e1c63
Артемий Алексеев <alekseev3artem@gmail.com>
2024-09-27 15:31:21 +0300

    test

---
+++ out/build/x64-Debug/LibraryCPPClass/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aleks/source/repos/AleksArtemiy/Algorithms/LibraryCPPClass
+# Build directory: C:/Users/aleks/source/repos/AleksArtemiy/Algorithms/out/build/x64-Debug/LibraryCPPClass
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
