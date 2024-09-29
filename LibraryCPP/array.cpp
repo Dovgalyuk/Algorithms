c56e17ae3a4d620c71c0904d193efe34e6f57798
Vads <bladr2377@gmail.com>
2024-09-30 00:15:29 +0300

    Lab1

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,34 +1,51 @@
 #include "array.h"
+#include <iostream>
+#include <stdexcept>
 
-struct Array
-{
+struct Array {
+    size_t size;
+    int* data;
 };
 
-// create array
-Array *array_create(size_t size)
-{
-    return new Array;
+// Create array
+Array* array_create(size_t size) {
+    if (size == 0) {
+        throw std::invalid_argument("Size must be greater than 0");
+    }
+
+    Array* arr = new Array;
+    arr->size = size;
+    arr->data = new int[size];  // Выделяем память для целочисленного массива
+    return arr;
 }
 
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    delete arr;
+// Delete array, free memory
+void array_delete(Array* arr) {
+    if (arr) {
+        delete[] arr->data;
+        delete arr;
+    }
 }
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+// Returns specified array element
+int array_get(const Array* arr, size_t index) {
+    if (index >= arr->size) {
+        std::cerr << "Error: Index out of range." << std::endl;
+        throw std::out_of_range("Index out of range");
+    }
+    return arr->data[index];
 }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+// Sets the specified array element to the value
+void array_set(Array* arr, size_t index, int value) {
+    if (index >= arr->size) {
+        throw std::out_of_range("Index out of range");
+    }
+    arr->data[index] = value;
 }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+// Returns array size
+size_t array_size(const Array* arr) {
+    return arr->size;
 }
+
