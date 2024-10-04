97330cb50a0b167d86d7b693944b75bdd01ca623
tolstosup <154780185+tolstosup@users.noreply.github.com>
2024-10-04 09:31:34 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -2,49 +2,33 @@
 
 struct Stack
 {
-    List* list = nullptr;
 };
 
 Stack *stack_create()
 {
-    Stack* stack = new Stack;
-    stack->list = list_create();
-
-    return stack;
+    return new Stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    if(stack != nullptr)
-        list_delete(stack->list);
+    // TODO: free stack elements
+    delete stack;
 }
 
 void stack_push(Stack *stack, Data data)
 {
-    if(stack != nullptr)
-        list_insert(stack->list, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    if(stack != nullptr)
-        return list_item_data(list_first(stack->list));
     return (Data)0;
 }
 
 void stack_pop(Stack *stack)
 {
-    if(stack != nullptr)
-        list_erase_first(stack->list);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    if (stack != nullptr)
-    {
-        if (list_first(stack->list) == nullptr)
-            return true;
-        return false;
-    }
     return true;
 }
