7b0ca4ef5a52b2cfbc68db0ff826bad8e37e41ac
tolstosup <154780185+tolstosup@users.noreply.github.com>
2024-10-04 09:30:27 +0300

    Update array.h

--- LibraryCPP/array.h
+++ LibraryCPP/array.h
@@ -2,8 +2,6 @@
 #define ARRAY_H
 
 #include <cstddef>
-#include <cstdlib>
-#include <fstream>
 
 // Non-resizeable array
 // Stores integer values inside
@@ -12,21 +10,18 @@ typedef int Data;
 struct Array;
 
 // create array
-Array* array_create(size_t size);
+Array *array_create(size_t size);
 
 // delete array, free memory
-void array_delete(Array* arr);
+void array_delete(Array *arr);
 
 // returns specified array element
-Data array_get(const Array* arr, size_t index);
+Data array_get(const Array *arr, size_t index);
 
 // sets the specified array element to the value
-void array_set(Array* arr, size_t index, Data value);
+void array_set(Array *arr, size_t index, Data value);
 
 // returns array size
-size_t array_size(const Array* arr);
-
-// fill array from file
-Array* fill_array(std::ifstream &in);
+size_t array_size(const Array *arr);
 
 #endif
