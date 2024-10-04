d81901cb8d53f81dd5e0daf7315606cb2d41f2e2
frostikkkk <165842528+frostikkkk@users.noreply.github.com>
2024-10-04 17:27:40 +0300

    Update mytest_array.cpp

--- LibraryCPP/Tests/mytest_array.cpp
+++ LibraryCPP/Tests/mytest_array.cpp
@@ -4,13 +4,13 @@
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cout << "ARGC < 2";
+        std::cout << "ARGC < 2" << std::endl;
         return 1;
     }
 
     FILE *input = fopen(argv[1], "r");
     if (!input) {
-        std::cout << "input not open. Home folder is " << argv[0] << ". Trying to open " << argv[1];
+        std::cout << "input not open. Home folder is " << argv[0] << ". Trying to open " << argv[1] << std::endl;
         return 1;
     }
 
@@ -18,7 +18,11 @@ int main(int argc, char **argv) {
 
     task1(arr, input);  
     int expected_sum;
-    fscanf(input, "%d", &expected_sum);
+    if (fscanf(input, "%d", &expected_sum) != 1) {
+        array_delete(arr);
+        fclose(input);
+        return 1;
+    }
 
     Array *expected_result = array_create_and_read(input); 
 
@@ -26,7 +30,7 @@ int main(int argc, char **argv) {
     Array *result = task2(arr, result_size);
 
     if (result_size != array_size(expected_result)) {
-        std::cout << "TEST 2 FAIL: expected size = " << result_size << ", result_size = " << array_size(expected_result);
+        std::cout << "TEST 2 FAIL: expected size = " << array_size(expected_result) << ", result_size = " << result_size << std::endl;
         array_delete(arr);
         array_delete(expected_result);
         array_delete(result);
@@ -36,7 +40,7 @@ int main(int argc, char **argv) {
 
     for (size_t i = 0; i < result_size; ++i) {
         if (array_get(result, i) != array_get(expected_result, i)) {
-            std::cout << "TEST 2 FAIL: expected contents for element " << i << " = " << array_get(expected_result, i) << ", result = " << array_get(result, i);
+            std::cout << "TEST 2 FAIL: expected contents for element " << i << " = " << array_get(expected_result, i) << ", result = " << array_get(result, i) << std::endl;
             array_delete(arr);
             array_delete(expected_result);
             array_delete(result);
