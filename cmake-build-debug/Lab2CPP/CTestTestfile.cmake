e791dd9687fa047ab920a7f7e3960340c9b7a585
EKATERINAelnova <katakata45882@gmail.com>
2024-10-05 08:48:47 +0300

    minor fix3

--- cmake-build-debug/Lab2CPP/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Katya/CLionProjects/Algorithms/Lab2CPP
-# Build directory: C:/Users/Katya/CLionProjects/Algorithms/cmake-build-debug/Lab2CPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1CPP "C:/Users/Katya/CLionProjects/Algorithms/cmake-build-debug/Lab2CPP/Lab2CPP.exe" "C:/Users/Katya/CLionProjects/Algorithms/Lab2CPP/input.txt")
-set_tests_properties(TestLab1CPP PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Katya/CLionProjects/Algorithms/Lab2CPP/CMakeLists.txt;5;add_test;C:/Users/Katya/CLionProjects/Algorithms/Lab2CPP/CMakeLists.txt;0;")
