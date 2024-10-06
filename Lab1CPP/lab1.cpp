73c74988c440f432aafeb367fca3eac6e0b17490
g3ntl3man <potashov2015@gmail.com>
2024-10-06 22:16:29 +0300

    update lab1

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -1,66 +1,76 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
 #include "array.h"
 
-using namespace std;
+Array* array_create_read(std::ifstream& input);
+void total_resistance(Array* arr);
+void most_number(Array* arr);
 
-double calculateResist(Array* arr);
-int findFrequent(Array* arr);
-
-int main() {
+int main(int argc, char **argv) {
 	setlocale(LC_ALL, "RU");
-	srand(static_cast<unsigned>(time(0)));
 
-	int size;
-	cout << "Введите размер массива: ";
-	cin >> size;
+	if (argc < 2) {
+		std::cerr << "Ошибка, укажите путь к файлу" << std::endl;
+		return 1;
+	}
+
+	std::ifstream input(argv[1]);
+	if (!input.is_open()) {
+		std::cerr << "Ошибка открытия файла: " << argv[1] << "\n";
+		return 1;
+	}
+
+	Array* arr = array_create_read(input);
+	total_resistance(arr);
+	array_delete(arr);
+
+	arr = array_create_read(input);
+	most_number(arr);
+	array_delete(arr);
+
+	input.close();
+
+	return 0;
+}
+
+Array* array_create_read(std::ifstream& input) {
+	size_t size;
+	input >> size;
 
 	Array* arr = array_create(size);
 
-	for (size_t i = 0; i < array_size(arr); ++i) {
-		array_set(arr, i, rand() % 100 + 1); // случайные числа от 1 до 100
+	for (size_t i = 0; i < size; ++i) {
+		int x;
+		input >> x;
+		array_set(arr, i, x);
 	}
-
-	// Вывод массива
-	cout << "Массив: ";
-	for (size_t i = 0; i < array_size(arr); ++i) {
-		cout << array_get(arr, i) << " ";
-	}
-	cout << endl;
-
-	// Вычисление общего сопротивления цепи
-	double total = calculateResist(arr);
-	cout << "Общее сопротивление цепи: " << total << endl;
-
-	// Определение наиболее часто встречающегося числа
-	int mostFrequent = findFrequent(arr);
-	cout << "Наиболее часто встречающееся число: " << mostFrequent << endl;
-
-	// Удаление массива и освобождение памяти
-	array_delete(arr);
+	return arr;
 }
 
-double calculateResist(Array* arr) {
-	double total = 0.0f;
+void total_resistance(Array* arr) {
+	double resistance = 0.0;
 	for (size_t i = 0; i < array_size(arr); ++i) {
-		total += 1.0 / array_get(arr, i);
+		resistance += (1.0 / array_get(arr, i));
 	}
-	return 1.0 / total;
+	
+	std::cout << "Общее сопротивление: " << resistance << std::endl;
 }
 
-int findFrequent(Array* arr) {
+void most_number(Array* arr) {
 	int maxCount = 0;
-	int mostFrequet = array_get(arr, 0);
+	int result = 0;
 	for (size_t i = 0; i < array_size(arr); ++i) {
 		int count = 0;
 		for (size_t j = 0; j < array_size(arr); ++j) {
-			if (array_get(arr, j) == array_get(arr, i)) {
+			if (array_get(arr, i) == array_get(arr, j))
 				count++;
-			}
 		}
-		maxCount = max(count, maxCount);
-		mostFrequet = array_get(arr, i);
+		if (count > maxCount) {
+			maxCount = count;
+			result = array_get(arr, i);
+		}
 	}
-	return mostFrequet;
+	std::cout << "Чаще всего встречает: " << result << std::endl;
 }
