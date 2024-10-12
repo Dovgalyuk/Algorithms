474b305aa527bdc1c17f50dc299750f4ec842904
root <fastamel@student.21-school.ru>
2024-10-12 13:43:31 +0300

    first_try

---
+++ Lab2C/lab2.c
@@ -0,0 +1,111 @@
+#include "../LibraryC/stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+
+// Функция для выполнения арифметических операций
+void perform_operation(Stack *stack, char op) {
+    int a = (int)(intptr_t)stack_get(stack);
+    stack_pop(stack);
+    int b = (int)(intptr_t)stack_get(stack);
+    stack_pop(stack);
+
+    int result;
+    switch (op) {
+        case '+': result = b + a; break;
+        case '-': result = b - a; break;
+        case '*': result = b * a; break;
+        case '/': result = b / a; break;
+        case '%': result = b % a; break;
+        default: return;
+    }
+
+    stack_push(stack, (Data)(intptr_t)result);
+}
+
+// Функция для выполнения логических операций
+void perform_logical_operation(Stack *stack, char op) {
+    int a = (int)(intptr_t)stack_get(stack);
+    stack_pop(stack);
+
+    int result;
+    switch (op) {
+        case '!': result = a == 0 ? 1 : 0; break;
+        case '`': {
+            int b = (int)(intptr_t)stack_get(stack);
+            stack_pop(stack);
+            result = a > b ? 1 : 0;
+            break;
+        }
+        default: return;
+    }
+
+    stack_push(stack, (Data)(intptr_t)result);
+}
+
+// Функция для выполнения команд интерпретатора
+void execute_command(Stack *stack, char command) {
+    if (isdigit(command)) {
+        stack_push(stack, (Data)(intptr_t)(command - '0'));
+    } else {
+        switch (command) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+                perform_operation(stack, command);
+                break;
+            case '!':
+            case '`':
+                perform_logical_operation(stack, command);
+                break;
+            case ':': {
+                int a = (int)(intptr_t)stack_get(stack);
+                stack_push(stack, (Data)(intptr_t)a);
+                break;
+            }
+            case '.': {
+                int a = (int)(intptr_t)stack_get(stack);
+                stack_pop(stack);
+                printf("%d\n", a);
+                break;
+            }
+            case '&': {
+                int input;
+                printf("Введите число: ");
+                scanf("%d", &input);
+                stack_push(stack, (Data)(intptr_t)input);
+                break;
+            }
+            
+        }
+    }
+}
+
+int main() {
+    Stack *stack = stack_create(NULL);
+    if (!stack) {
+        printf("Ошибка создания стека.\n");
+        return 1;
+    }
+
+    FILE *input = fopen("input.txt", "r");
+    if (!input) {
+        printf("Ошибка открытия файла input.txt.\n");
+        stack_delete(stack);
+        return 1;
+    }
+
+    char command;
+    while ((command = fgetc(input)) != EOF) {
+        if (command == '\n') continue;
+        execute_command(stack, command);
+    }
+
+    fclose(input);
+    stack_delete(stack);
+    return 0;
+}
--- LibraryC/stack.c
+++ LibraryC/stack.c
@@ -1,35 +1,44 @@
 #include <stdlib.h>
 #include "stack.h"
+#include "list.h"
 
 typedef struct Stack
 {
+    List *list;
 } Stack;
 
-Stack *stack_create(FFree f)
+Stack *stack_create(FFree *f)
 {
-    return malloc(sizeof(Stack));
+    Stack *stack = malloc(sizeof(Stack));
+    if (stack == NULL) return NULL;
+    stack->list = list_create(f);
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    list_delete(stack->list);
     free(stack);
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    list_insert(stack->list, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    ListItem *item = list_first(stack->list);
+    if (item == NULL) return NULL;
+    return list_item_data(item);
 }
 
 void stack_pop(Stack *stack)
 {
+    list_erase_first(stack->list);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return list_first(stack->list) == NULL;
 }
