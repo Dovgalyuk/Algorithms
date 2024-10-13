448185a31db2950257f3612d5eddc0de54d0b2fc
roman@M1402 <demixhd2@gmail.com>
2024-10-13 12:00:12 +0300

    update lab3.cpp

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <fstream>
 #include "../LibraryCPP/queue.h"
 #include "../LibraryCPP/vector.h"
-#include <iostream>
 
 int main() {
+	std::ifstream in("input.txt");
+	Vector* vector = vector_create();
+	if (in) {
+		char temp = '1';
+		while (in.get(temp)) {
+			if(temp != '\n')
+				vector_push(vector, temp);
+		}
+
+		for (int i = 0; i < vector_size(vector); i++) {
+			if (i % 6 == 0 && i != 0)
+				std::cout << "\n";
+			std::cout << (char)vector_get(vector, i);
+		}
+	}
+	in.close();
 	return 0;
 }
