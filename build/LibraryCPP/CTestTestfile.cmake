ffea5b49f550385df7d3eaeb875efac3a4c133e4
AlexSipaylo <SipayloSasha@yandex.ru>
2024-10-16 16:28:20 +0300

    Lab1

--- build/LibraryCPP/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/НовГУ. Довгалюк/Algorithms/LibraryCPP
-# Build directory: D:/НовГУ. Довгалюк/Algorithms/build/LibraryCPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
--- build/LibraryCPP/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/НовГУ. Довгалюк/Algorithms/LibraryCPP/Tests
-# Build directory: D:/НовГУ. Довгалюк/Algorithms/build/LibraryCPP/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayCPP "TestArrayCPP")
-set_tests_properties(TestArrayCPP PROPERTIES  _BACKTRACE_TRIPLES "D:/НовГУ. Довгалюк/Algorithms/LibraryCPP/Tests/CMakeLists.txt;4;add_test;D:/НовГУ. Довгалюк/Algorithms/LibraryCPP/Tests/CMakeLists.txt;0;")
