7614ef4fc5147591f7ce08a625e81427f967e53b
Nikita Gromov <groniknov@yandex.ru>
2024-10-20 21:18:20 +0300

    Lab2

---
+++ Lab2CPP/lab2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "stack.h"
+
+bool sequence(const std::string& sequence) {
+    Stack* stack = stack_create();
+
+    for (char c : sequence) {
+        if (c == '(' || c == '[' || c == '{') {
+            stack_push(stack, static_cast<Data>(c)); 
+        }
+        else if (c == ')' || c == ']' || c == '}') {
+            if (stack_empty(stack)) {
+                stack_delete(stack);
+                return false; 
+            }
+            Data top = stack_get(stack); 
+            stack_pop(stack); 
+            if ((c == ')' && top != '(') ||
+                (c == ']' && top != '[') ||
+                (c == '}' && top != '{')) {
+                stack_delete(stack);
+                return false; 
+            }
+        }
+    }
+
+    bool result = stack_empty(stack);
+    stack_delete(stack); 
+    return result;
+}
+
+int main() {
+    std::ifstream infile("input.txt");
+    std::ofstream outfile("output.txt"); 
+    std::string s;
+        
+    if (!infile) {
+        std::cerr << "Error of opening input file." << std::endl;
+        return 1;
+    }
+
+    if (!outfile) {
+        std::cerr << "Error of opening output file." << std::endl;
+        return 1;
+    }
+
+    std::getline(infile, s);
+    if (sequence(s)) {
+        outfile << "YES" << std::endl; 
+    }
+    else {
+        outfile << "NO" << std::endl; 
+    }
+
+    return 0;
+}
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,34 +1,60 @@
 #include "vector.h"
+#include <cstdlib>
+#include <cstring>
 
-struct Vector
-{
+struct Vector {
+    Data* data;
+    size_t capacity;
+    size_t size;
 };
 
-Vector *vector_create()
-{
-    return new Vector;
+Vector* vector_create() {
+    Vector* vector = new Vector;
+    vector->capacity = 4;
+    vector->size = 0;
+    vector->data = new Data[vector->capacity];
+    return vector;
 }
 
-void vector_delete(Vector *vector)
-{
+void vector_delete(Vector* vector) {
     // TODO: free vector internals
-    delete vector; 
+    delete[] vector->data;
+    delete vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
-{
-    return (Data)0;
+Data vector_get(const Vector* vector, size_t index) {
+    if (index < vector->size) {
+        return vector->data[index];
+    }
+    else {
+        return -1;
+    }
+
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
-{
+void vector_set(Vector* vector, size_t index, Data value) {
+    if (index < vector->size) {
+        vector->data[index] = value;
+    }
 }
 
-size_t vector_size(const Vector *vector)
-{
-    return 0;
+size_t vector_size(const Vector* vector) {
+    return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
-{
+void vector_resize(Vector* vector, size_t size) {
+    if (size == vector->size) {
+        return;
+    }
+    if (size > vector->capacity) {
+        size_t new_capacity = (size > vector->capacity * 2) ? size : vector->capacity * 2;
+        Data* new_data = new Data[new_capacity];
+        for (size_t i = 0; i < vector->size; ++i) {
+            new_data[i] = vector->data[i];
+        }
+        delete[] vector->data;
+        vector->data = new_data;
+        vector->capacity = new_capacity;
+    }
+    vector->size = size;
 }
