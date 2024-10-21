8da6450b3ab045ebe955afdd4e1b81498892e57d
Nikita Gromov <groniknov@yandex.ru>
2024-10-21 21:21:48 +0300

    Lab2

--- out/build/x64-Debug/Lab2CPP/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/днс/source/repos/Algorithms/Lab2CPP
-# Build directory: C:/Users/днс/source/repos/Algorithms/out/build/x64-Debug/Lab2CPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab2CPP "C:/Users/днс/source/repos/Algorithms/out/build/x64-Debug/Lab2CPP/Lab2CPP.exe" "C:/Users/днс/source/repos/Algorithms/Lab2CPP/input.txt")
-set_tests_properties(TestLab2CPP PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/днс/source/repos/Algorithms/Lab2CPP/CMakeLists.txt;5;add_test;C:/Users/днс/source/repos/Algorithms/Lab2CPP/CMakeLists.txt;0;")
