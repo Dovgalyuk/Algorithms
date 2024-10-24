9f2f1c323924fbd871da4a827644d281a0c480cc
roman@M1402 <demixhd2@gmail.com>
2024-10-24 15:58:35 +0300

    update graph.h

--- LibraryCPPTemplate/graph.h
+++ LibraryCPPTemplate/graph.h
@@ -4,7 +4,8 @@
 #define GRAPH_H
 
 typedef struct Vertex {
-
+    unsigned int degree = 0;
+    unsigned int mark;
 }Vertex;
 
 template <typename Data>
@@ -23,27 +24,29 @@ class Graph {
        }
 
        void add_edge(Vertex a, Vertex b) {
-
+           relations[a.mark][a.mark] = 1;
        }
 
        void delete_vertex(Vertex a) {
 
        }
 
-       void delete_edge(Vertex a, Vertex b) {
-
+       void delete_edge(Vertex a, Vertex b) { 
+           relations[a.mark][a.mark] = 0;
        }
 
-       bool is_bounded(Vertex a, Vertex b) {
-
+       bool is_bounded(Vertex a, Vertex b) { 
+           if (relations[a.mark][b.mark] == 1)
+               return true;
+           else
+               return false;
        }
 
-       void add_mark(Vertex a) {
-
+       void add_mark(Vertex a, unsigned int mark) { 
+           a.mark = mark;
        }
 
-       Vector get_marks() {
-
+       Vector get_marks() { if (1)
        }
 
    private:
