aca41af0b55b472d9b9925c078fe24af7e423ce9
lakimovAbdulmutalib3091 <hamihe04@gmail.com>
2024-10-26 14:48:25 +0300

    update

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -2,67 +2,71 @@
 #include <stdlib.h>
 #include <limits.h>
 
-// Описание структуры динамического массива
+// Структура динамического массива фиксированного размера
 typedef struct {
-    int *data;
+    int* data;
     int size;
 } Array;
 
-// Функция для создания массива с фиксированным размером
-Array *array_create(int size, int *values) {
-    Array *arr = (Array *)malloc(sizeof(Array));
-    arr->size = size;
-    arr->data = (int *)malloc(size * sizeof(int));
-
-    if (values) {
-        for (int i = 0; i < size; ++i) {
-            arr->data[i] = values[i];
-        }
+// Создание массива фиксированного размера и заполнение данными
+Array* array_create(int size) {
+    if (size <= 0) {
+        printf("Invalid array size\n");
+        return NULL;
     }
-
+    Array* arr = (Array*)malloc(sizeof(Array));
+    arr->size = size;
+    arr->data = (int*)malloc(size * sizeof(int));
     return arr;
 }
 
-// Функция для удаления массива
-void array_delete(Array *arr) {
+// Удаление массива
+void array_delete(Array* arr) {
     if (arr) {
         free(arr->data);
         free(arr);
     }
 }
 
-// Функция для установки значения в массив
-void array_set(Array *arr, int index, int value) {
+// Установка значения в массив
+void array_set(Array* arr, int index, int value) {
     if (index >= 0 && index < arr->size) {
         arr->data[index] = value;
     }
 }
 
-// Функция для получения значения из массива
-int array_get(const Array *arr, int index) {
+// Получение значения из массива
+int array_get(const Array* arr, int index) {
     if (index >= 0 && index < arr->size) {
         return arr->data[index];
     }
-    return 0; // Можно добавить обработку ошибок
+    return 0;
 }
 
-// Функция для чтения массива из файла
-Array *array_create_and_read(FILE *input) {
+// Создание массива и чтение его из файла
+Array* array_create_and_read(FILE* input) {
     int n;
-    fscanf(input, "%d", &n);
-    Array *arr = array_create(n, NULL);
+    if (fscanf(input, "%d", &n) != 1 || n <= 0) {
+        printf("Invalid array size\n");
+        return NULL;
+    }
+    Array* arr = array_create(n);
 
     for (int i = 0; i < n; ++i) {
         int x;
-        fscanf(input, "%d", &x);
+        if (fscanf(input, "%d", &x) != 1) {
+            printf("Ошибка при чтении элементов массива\n");
+            array_delete(arr);
+            return NULL;
+        }
         array_set(arr, i, x);
     }
 
     return arr;
 }
 
-// Задача 1: Печать двузначных и трехзначных чисел
-void task1(Array *arr) {
+// Печать двузначных и трехзначных чисел
+void task1(Array* arr) {
     printf("Двузначные числа:\n");
     for (int i = 0; i < arr->size; ++i) {
         int value = array_get(arr, i);
@@ -80,13 +84,13 @@ void task1(Array *arr) {
     printf("\n");
 }
 
-// Задача 2: Сумма элементов между минимальным и максимальным элементами
-void task2(Array *arr) {
+// Сумма элементов между минимальным и максимальным
+void task2(Array* arr) {
     int min = INT_MAX;
     int max = INT_MIN;
     int min_index = 0, max_index = 0;
 
-    // Находим минимальный и максимальный элементы
+    // Поиск минимального и максимального элементов
     for (int i = 0; i < arr->size; ++i) {
         int value = array_get(arr, i);
         if (value < min) {
@@ -99,14 +103,13 @@ void task2(Array *arr) {
         }
     }
 
-    // Убедимся, что минимальный индекс меньше максимального
+    // Убедимся, что min_index < max_index
     if (min_index > max_index) {
         int temp = min_index;
         min_index = max_index;
         max_index = temp;
     }
 
-    // Считаем сумму элементов между минимальным и максимальным
     int sum = 0;
     for (int i = min_index + 1; i < max_index; ++i) {
         sum += array_get(arr, i);
@@ -115,30 +118,37 @@ void task2(Array *arr) {
     printf("Сумма элементов между минимальным и максимальным: %d\n", sum);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     if (argc != 2) {
         printf("Использование: %s <input_file>\n", argv[0]);
         return 1;
     }
 
-    FILE *input = fopen(argv[1], "r");
+    FILE* input = fopen(argv[1], "r");
     if (!input) {
         printf("Ошибка открытия файла\n");
         return 1;
     }
 
     // Первая задача
-    Array *arr = array_create_and_read(input);
+    Array* arr = array_create_and_read(input);
+    if (!arr) {
+        fclose(input);
+        return 1;
+    }
     task1(arr);
     array_delete(arr);
 
     // Вторая задача
-    rewind(input);  // Возвращаемся в начало файла
+    rewind(input);
     arr = array_create_and_read(input);
+    if (!arr) {
+        fclose(input);
+        return 1;
+    }
     task2(arr);
     array_delete(arr);
 
     fclose(input);
     return 0;
 }
-
