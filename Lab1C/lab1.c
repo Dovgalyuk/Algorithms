816166d529b11ea4ad704f085dd5d7cfa630a19b
wXwSmilewXw <mr.romanuza@mail.ru>
2024-10-27 17:53:41 +0300

    Copy lab in repository
    Установил приложение GitHub

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,40 +1,114 @@
-#include <stdio.h>
-#include "array.h"
+// 1)Реализовать контейнер - динамический массив array с неизменяемым размером (на любом из языков)
+// 2)Проверить работу соответствующей тестовой программы
+// 3)Написать две функции для обработки массива из следующих пунктов и вызвать их из функции main.
+// 4)Ввести целое число, создать массив такой размерности и заполнить его из файла. Нечетные элементы массива уменьшить на m, а элементы с нечетными номерами увеличить на n. m и n ввести из файла.
+// 5)Ввести целое число, создать массив такой размерности и заполнить его из файла. В массиве найти элементы, которые не делятся ни на какой другой элемент массива.
+// 6)Добавить пример входных данных и описать тест в CMakeLists.txt, который запускает разработанную программу и использует входные данные.
 
-Array *array_create_and_read(FILE *input)
-{
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../LibraryC/array.h"
+
+Array* array_create_and_read(FILE *input) {
     int n;
-    fscanf(input, "%d", &n);
+    scanf("%d", &n);
+
     /* Create array */
     Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
-    {
-        int x;
-        fscanf(input, "%d", &x);
-        array_set(arr, i, x);
-    }
-    return arr;
-}
 
-void task1(Array *arr)
+    /* Filling an Array */
+    int c = 0;
+    int cnt = 0;
+    while (c != EOF && cnt != n) {
+        fscanf(input, "%d", &c);
+        array_set(arr, cnt, (Data)c);
+        cnt++;
+    }
+
+    /* Read array data */
+    for (int i = 0; i < n; i++) {
+        int value = array_get(arr, i);
+        printf("%d ", value);
+    }
+    printf("\n");
+
+    return(arr);
+} 
+
+void task1(Array *arr, int n, int m)
 {
+    size_t arr_size = array_size(arr);
+    if (arr_size == 0) {
+        printf("Массив пуст.\n");
+        return;
+    }
+    for (size_t i = 0; i < array_size(arr); i++) {
+        int value = array_get(arr, i);
+        if (value % 2 != 0) {
+            value -= m;
+        }
+        if (i % 2 == 1) {
+            value += n;
+        }
+        array_set(arr, i, (Data)value);
+        printf("%d ", value);
+    }
+    printf("\n");
 }
 
 void task2(Array *arr)
 {
+    size_t arr_size = array_size(arr);
+    if (arr_size == 0) {
+        printf("Массив пуст.\n");
+        return;
+    }
+
+    for (size_t i = 0; i < array_size(arr); i++) {
+        int value_1 = array_get(arr, i);
+        int is_divisible = 0;
+
+        for (size_t j = 0; j < array_size(arr); j++) {
+            if (i != j) {
+                int value_2 = array_get(arr, j);
+                if (value_1 % value_2 == 0) {
+                    is_divisible = 1;
+                    break; 
+                }
+            }         
+        }
+        if (!is_divisible)
+            printf("%d ", value_1);  
+    }
+    printf("\n");
 }
 
-int main(int argc, char **argv)
+int main(/*int argc, char **argv*/)
 {
-    Array *arr = NULL;
-    FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
-    task1(arr);
+    int n, m;
+    FILE *input = fopen(/*argv[1]*/"input.txt", "r");
+    if (input == NULL) {
+        printf("Ошибка открытия файла");
+        return 1;
+    }
+    fscanf(input, "%d%d", &n, &m);
+
+    Array *arr = array_create_and_read(input);
+
+    task1(arr, n, m);
+    /* Removing an array */
     array_delete(arr);
+    fclose(input);
+
+    FILE *input_2 = fopen(/*argv[1]*/"input.txt", "r");
+    if (input_2 == NULL) {
+        printf("Ошибка открытия файла");
+        return 1;
+    }
     /* Create another array here */
     arr = array_create_and_read(input);
     task2(arr);
     array_delete(arr);
-    fclose(input);
+    fclose(input_2);
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,76 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "array.h"
 
-typedef struct Array
+typedef struct Array 
 {
+    size_t size;
+    Data* data;
+    FFree* distruct;
 } Array;
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array* arr = malloc(sizeof(Array));
+    if (arr == NULL) {
+        printf("Ошибка выделения памяти для структуры");
+        return NULL;
+    }
+
+    arr -> data = malloc(size * sizeof(Data));
+    if (arr -> data == NULL) {
+        printf("Ошибка выделения памяти для массива");
+        free(arr);
+        return NULL;
+    }
+
+    arr -> distruct = f;
+    arr -> size = size;
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if (arr == NULL) {
+        printf("Ошибка удаления");
+        return;
+    }
+    if (arr -> distruct != NULL) {
+        for (size_t i = 0; i < arr->size; i++){
+            void* ptr = (void*)arr->data[i];
+            arr -> distruct(ptr);
+        }
+    }
+    free(arr -> data);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (arr == NULL || index >= arr->size) {
+        printf("Ошибка: массив пуст или индекс выходит за пределы");
+        return 0;
+    }
+
+    return arr -> data[index];
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+int array_set(Array *arr, size_t index, Data value)
 {
+    if (arr == NULL || index >= arr -> size) {
+        printf("Массив пуст или индекс вышел за пределы размера");
+        return -1;
+    }
+    arr -> data[index] = value;
+    return 0;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr -> size;
 }
