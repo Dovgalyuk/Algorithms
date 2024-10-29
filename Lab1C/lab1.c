8ec9541e8d79cd8dd533d2fd5bd879965b748c45
Василина Юганова <iuganova.vasilina@yandex.ru>
2024-10-30 02:02:08 +0300

    Lab1

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,40 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "array.h"
 
-Array *array_create_and_read(FILE *input)
-{
+Array* array_create_and_read(FILE* input) {
     int n;
-    fscanf(input, "%d", &n);
-    /* Create array */
-    Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
-    {
+    fscanf_s(input, "%d", &n);
+    Array* arr = array_create(n, NULL);
+    for (int i = 0; i < n; ++i) {
         int x;
-        fscanf(input, "%d", &x);
+        fscanf_s(input, "%d", &x);
         array_set(arr, i, x);
     }
     return arr;
 }
 
-void task1(Array *arr)
-{
+void task1(Array* arr, size_t size) {
+    int min = (int)array_get(arr, 0);
+    int max = (int)array_get(arr, 0);
+    int sum = 0;
+
+    for (size_t i = 0; i < size; i++) {
+        int value = (int)array_get(arr, i);
+        if (value < min) min = value;
+        if (value > max) max = value;
+        sum += value;
+    }
+
+    double average = (double)sum / size;
+    double threshold = (average + min + max) / 3;
+
+    for (size_t i = 0; i < size; i++) {
+        if ((int)array_get(arr, i) > threshold) {
+            printf("%zu\n", i);
+        }
+    }
 }
 
-void task2(Array *arr)
-{
+void task2(Array* arr, size_t size) {
+    int* frequency = (int*)calloc(size, sizeof(int));
+    if (frequency == NULL) {
+        printf("Memory allocation failed for frequency array");
+        return;
+    }
+
+
+    if (arr != NULL) {
+        int most_frequent = (int)array_get(arr, 0);
+        int max_count = 0;
+        for (size_t i = 0; i < size; i++) {
+            int current = (int)array_get(arr, i);
+            if (current >= 0 && current < size) {
+                frequency[current]++;
+                if (frequency[current] > max_count) {
+                    max_count = frequency[current];
+                    most_frequent = current;
+                }
+
+            }
+
+        }
+        printf("%d", most_frequent);
+    }
+    free(frequency);
 }
 
-int main(int argc, char **argv)
+int main()
 {
-    Array *arr = NULL;
-    FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
-    task1(arr);
-    array_delete(arr);
-    /* Create another array here */
-    arr = array_create_and_read(input);
-    task2(arr);
-    array_delete(arr);
+    FILE* input = fopen("input.txt", "r");
+    if (!input) {
+        fprintf(stderr, "Could not open file\n");
+        return 1;
+    }
+
+    Array* arr = array_create_and_read(input);
+    size_t size = array_size(arr);
+    if (arr == NULL) {
+        printf("Array is NULL");
+        return;
+    }
+    if (size == 0) {
+        printf("Array is empty");
+        return 1;
+    }
     fclose(input);
+
+    task1(arr, size);
+    task2(arr, size);
+    array_delete(arr);
+
+    return 0;
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,68 @@
 #include <stdlib.h>
+#include <string.h>
 #include "array.h"
 
-typedef struct Array
-{
+typedef struct Array {
+    size_t size;      // Size of the array
+    Data* data;      // Pointer to the data
+    FFree freeFunc;  // Free function for user data
 } Array;
 
 // create array
-Array *array_create(size_t size, FFree f)
-{
-    return malloc(sizeof(Array));
+Array* array_create(size_t size, FFree f) {
+    Array* arr = malloc(sizeof(Array));
+    if (!arr) return NULL; // Check for allocation failure
+
+    arr->size = size;
+    arr->data = malloc(size * sizeof(Data));
+    if (!arr->data) {
+        free(arr);
+        return NULL; // Check for allocation failure
+    }
+
+    arr->freeFunc = f;
+    memset(arr->data, 0, size * sizeof(Data)); // Initialize array elements to 0
+
+    return arr;
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
-    free(arr);
+void array_delete(Array* arr) {
+    if (arr) {
+        if (arr->freeFunc) {
+            for (size_t i = 0; i < arr->size; i++) {
+                if (arr->data[i]) {
+                    arr->freeFunc((void*)arr->data[i]); // Free each element if necessary
+                }
+            }
+        }
+        free(arr->data); // Free the data array
+        free(arr);       // Free the array struct
+    }
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array* arr, size_t index) {
+    if (arr == NULL) {
+        return 1;
+    }
+    if (index < arr->size) {
+        return arr->data[index];
+    }
+    return (Data)0; // Return 0 or handle error as needed
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+    if (index < arr->size) {
+        arr->data[index] = value;
+    }
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr) {
+    if (arr == NULL) {
+        return 1;
+    }
+    return arr->size;
 }
