d1302c044c95f755154a08556bccaff1213d8c9f
roman@M1402 <demixhd2@gmail.com>
2024-10-29 09:52:22 +0300

    added iterator

--- LibraryCPPTemplate/iterator.h
+++ LibraryCPPTemplate/iterator.h
@@ -3,6 +3,27 @@
 #ifndef ITERETOR_H
 #define ITERATOR_H
 
+template <typename Data>
+class Iterator {
+public:
+    Iterator(Graph<Data>* graph, Data v_mark, size_t neib) {
+        this->graph = graph;
+        this->mark = v_mark;
+        this->neib_index = neib;
+    }
 
+    /*Vertex<Data> operator++() {
+
+    }
+    */
+    bool operator!=(Iterator<Data> iterator) {
+
+    }
+
+private:
+    Graph<Data>* graph = nullptr;
+    Data mark = Data();
+    size_t neib_index = 0;
+};
 
 #endif
