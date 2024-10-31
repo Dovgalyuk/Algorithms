7943a399f1039b57cce15663846e829ac5b12f88
Vads <bladr2377@gmail.com>
2024-10-31 22:23:25 +0300

    lab2

---
+++ Lab2CPP/src/lab2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "stack.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
+        return 1;
+    }
+
+    ifstream input(argv[1]);
+    ofstream output(argv[2]);
+
+    if (!input.is_open() || !output.is_open()) {
+        cerr << "Error opening input or output file!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(input, line)) {
+        Stack stack;
+        istringstream tokens(line);
+        string token;
+        bool error = false;
+
+        while (tokens >> token) {
+            if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)) {
+                stack.push(stoi(token));
+            } else if (token == "+" || token == "-" || token == "*" || token == "/") {
+                if (stack.empty()) {
+                    output << "UNDERFLOW" << endl;
+                    error = true;
+                    break;
+                }
+                int b = stack.get();
+                stack.pop();
+
+                if (stack.empty()) {
+                    output << "UNDERFLOW" << endl;
+                    error = true;
+                    break;
+                }
+                int a = stack.get();
+                stack.pop();
+
+                if (token == "+") stack.push(a + b);
+                else if (token == "-") stack.push(a - b);
+                else if (token == "*") stack.push(a * b);
+                else if (token == "/") {
+                    if (b == 0) {
+                        output << "ZERO" << endl;
+                        error = true;
+                        break;
+                    }
+                    stack.push(a / b);
+                }
+            } else {
+                output << "Invalid token: " << token << endl;
+                error = true;
+                break;
+            }
+        }
+
+        if (!error) {
+            if (stack.empty()) {
+                output << "UNDERFLOW" << endl;
+            } else if (stack.get() == 0 || !stack.empty()) {
+                output << "OVERFLOW" << endl;
+            } else {
+                output << stack.get() << endl;
+            }
+        }
+    }
+
+    input.close();
+    output.close();
+    return 0;
+}
--- LibraryCPPClass/list.cpp
+++ LibraryCPPClass/list.cpp
@@ -1,44 +1,55 @@
-#include <cstddef>
 #include "list.h"
 
-List::List()
+List::List(const List& a) : head_(nullptr)
 {
+    for (Item* item = a.first(); item != nullptr; item = item->next())
+        insert(item->data());
 }
 
-List::List(const List &a)
+List& List::operator=(const List& a)
 {
-}
-
-List &List::operator=(const List &a)
-{
+    if (this != &a) {
+        while (head_)
+            erase_first();
+        for (Item* item = a.first(); item != nullptr; item = item->next())
+            insert(item->data());
+    }
     return *this;
 }
 
 List::~List()
 {
+    while (head_)
+        erase_first();
 }
 
-List::Item *List::first()
+List::Item* List::insert(Data data)
 {
-    return nullptr;
+    head_ = new Item(data, head_);
+    return head_;
 }
 
-List::Item *List::insert(Data data)
+List::Item* List::insert_after(Item* item, Data data)
 {
-    return nullptr;
+    if (!item) return nullptr;
+    item->set_next(new Item(data, item->next()));
+    return item->next();
 }
 
-List::Item *List::insert_after(Item *item, Data data)
+List::Item* List::erase_first()
 {
-    return nullptr;
+    if (!head_) return nullptr;
+    Item* temp = head_;
+    head_ = head_->next();
+    delete temp;
+    return head_;
 }
 
-List::Item *List::erase_first()
+List::Item* List::erase_next(Item* item)
 {
-    return nullptr;
-}
-
-List::Item *List::erase_next(Item *item)
-{
-    return nullptr;
+    if (!item || !item->next()) return nullptr;
+    Item* temp = item->next();
+    item->set_next(temp->next());
+    delete temp;
+    return item->next();
 }
