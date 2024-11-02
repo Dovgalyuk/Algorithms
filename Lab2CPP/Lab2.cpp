62fae983b5cf799bfc72b5e21b83332c2bc85014
Roman Kachkurkin <whshcom@mail.ru>
2024-11-02 14:11:26 +0300

    upd

--- Lab2CPP/Lab2.cpp
+++ Lab2CPP/Lab2.cpp
@@ -1,103 +1,123 @@
 #include <iostream>
-#include <sstream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <map>
 #include "stack.h"
 
-const int NUM_REGISTERS = 4;
-std::map<std::string, size_t> registers;
-Stack* processorStack;  // Используем ваш самописный стек
-bool inSubroutine = false;
+enum class CommandType { PUSH, POP, CALL, RET, INVALID };
 
-void initialize_registers() {
-    registers["A"] = 0;
-    registers["B"] = 0;
-    registers["C"] = 0;
-    registers["D"] = 0;
+struct Command {
+    CommandType type;
+    size_t value;
+};
+
+Command parseCommand(const std::string& input) {
+    Command cmd = { CommandType::INVALID, 0 };
+    std::istringstream iss(input);
+    std::string operation;
+    iss >> operation;
+
+    if (operation == "push") {
+        cmd.type = CommandType::PUSH;
+        iss >> cmd.value;
+    } else if (operation == "pop") {
+        cmd.type = CommandType::POP;
+        std::string reg;
+        iss >> reg;
+        cmd.value = reg[0];
+    } else if (operation == "call") {
+        cmd.type = CommandType::CALL;
+    } else if (operation == "ret") {
+        cmd.type = CommandType::RET;
+    }
+
+    return cmd;
 }
 
-void push_value(const std::string& value) {
-    if (std::isdigit(value[0]) || (value[0] == '-' && value.size() > 1)) {
-        stack_push(processorStack, std::stoi(value));
-    } else {
-        stack_push(processorStack, registers[value]);
-    }
-}
-
-void pop_value(const std::string& reg) {
-    if (stack_empty(processorStack)) {
-        std::cout << "BAD POP\n";
-        return;
-    }
-
-    size_t top_value = stack_get(processorStack);
-
-    if (inSubroutine) {
-        std::cout << "BAD POP\n";
-        return;
-    }
-
-    registers[reg] = top_value;
-    stack_pop(processorStack);
-}
-
-void call() {
-    stack_push(processorStack, SIZE_MAX);
-    inSubroutine = true;
-}
-
-void ret() {
-    if (stack_empty(processorStack)) {
-        std::cout << "BAD RET\n";
-        return;
-    }
-
-    size_t top_value = stack_get(processorStack);
-
-    if (top_value != SIZE_MAX) {
-        std::cout << "BAD RET\n";
-        return;
-    }
-
-    stack_pop(processorStack); 
-    inSubroutine = false; 
+bool isValidRegister(char reg) {
+    return reg >= 'A' && reg <= 'D';
 }
 
 int main() {
-    initialize_registers();
+    Stack* stack = stack_create();
+    std::map<char, size_t> registers = { {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0} };
+    std::string input;
 
-    // Stack Initialization
-    processorStack = stack_create();
-
-    std::ifstream input("input2.txt");
-    std::string command;
-
-    while (std::getline(input, command)) {
-        std::istringstream iss(command);
-        std::string operation;
-        std::string arg;
-
-        iss >> operation;
-        if (operation == "push") {
-            iss >> arg;
-            push_value(arg);
-        } else if (operation == "pop") {
-            iss >> arg;
-            pop_value(arg);
-        } else if (operation == "call") {
-            call();
-        } else if (operation == "ret") {
-            ret();
-        }
+    //использование относительного пути у меня не работает, поэтому использую абсолютный путь
+    //но также оставил строку для использования относительного пути
+    //файл с входными данными находится: LibraryCPP -> "input2.txt"
+    std::ifstream inputFile("D:\\Algorithms-1\\LibraryCPP\\input2.txt");
+    //std::ifstream inputFile("../LibraryCPP/input2.txt");
+    
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
     }
 
+    while (std::getline(inputFile, input)) {
+        Command cmd = parseCommand(input);
+
+        switch (cmd.type) {
+            case CommandType::PUSH: {
+                stack_push(stack, cmd.value);
+                break;
+            }
+
+            case CommandType::POP: {
+                if (stack_empty(stack)) {
+                    std::cout << "BAD POP" << std::endl;
+                    stack_delete(stack);
+                    return 1;
+                }
+                if (stack_get(stack) == 0) {
+                    std::cout << "BAD POP" << std::endl;
+                    stack_delete(stack);
+                    return 1;
+                }
+                if (!isValidRegister(static_cast<char>(cmd.value))) {
+                    std::cout << "BAD POP" << std::endl;
+                    stack_delete(stack);
+                    return 1;
+                }
+                size_t value = stack_get(stack);
+                registers[static_cast<char>(cmd.value)] = value;
+                stack_pop(stack);
+                break;
+            }
+
+            case CommandType::CALL: {
+                stack_push(stack, 0);
+                break;
+            }
+
+            case CommandType::RET: {
+                if (stack_empty(stack)) {
+                    std::cout << "BAD RET" << std::endl;
+                    stack_delete(stack);
+                    return 1;
+                }
+                if (stack_get(stack) != 0) {
+                    std::cout << "BAD RET" << std::endl;
+                    stack_delete(stack);
+                    return 1;
+                }
+                stack_pop(stack);
+                break;
+            }
+
+            case CommandType::INVALID: {
+                std::cout << "Invalid command!" << std::endl;
+                break;
+            }
+        }
+    }
+    
     for (const auto& reg : registers) {
         std::cout << reg.first << " = " << reg.second << std::endl;
     }
 
-    // Freeing up stack memory
-    stack_delete(processorStack);
-
+    inputFile.close();
+    stack_delete(stack);
     return 0;
 }
