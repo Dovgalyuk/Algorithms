94dec7f63b94709b686940ab2e3f79ee1a3f0d09
Roman Kachkurkin <whshcom@mail.ru>
2024-11-07 21:38:38 +0300

    .

--- Lab2CPP/Lab2.cpp
+++ Lab2CPP/Lab2.cpp
@@ -40,6 +40,7 @@ bool isValidRegister(char reg) {
 }
 
 int main(int argc, char* argv[]) {
+    
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
         return 1;
