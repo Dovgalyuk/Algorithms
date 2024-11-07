012055ab573b7eb5c88236629b110f6e33f5a8da
ZwrvKrll <zverevk429@gmail.com>
2024-11-08 00:13:53 +0300

    Lab 1 try 5

--- LibraryCPP/array.h
+++ LibraryCPP/array.h
@@ -2,26 +2,39 @@
 #define ARRAY_H
 
 #include <cstddef>
+#include <stdexcept>
+#include <iostream>
 
-// Non-resizeable array
-// Stores integer values inside
 typedef int Data;
 
-struct Array;
+class Array
+{
+public:
+    // create array
+    explicit Array(size_t size);
 
-// create array
-Array *array_create(size_t size);
+    // copy constructor
+    Array(const Array &a);
 
-// delete array, free memory
-void array_delete(Array *arr);
+    // assignment operator
+    Array &operator=(const Array &a);
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index);
+    // delete array, free memory
+    ~Array();
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value);
+    // returns specified array element
+    Data get(size_t index) const;
 
-// returns array size
-size_t array_size(const Array *arr);
+    // sets the specified array element to the value
+    void set(size_t index, Data value);
+
+    // returns array size
+    size_t size() const;
+
+private:
+    // private data should be here
+    size_t m_size; //size of array
+    Data* m_data; // pointer
+};
 
 #endif
