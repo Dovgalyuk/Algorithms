96b15d3138a97c9de5c3a7fb27a5261446cfc70c
chinazes <167748451+Roman375757752@users.noreply.github.com>
2024-11-07 20:48:29 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,39 @@
 #include "queue.h"
+#include <iostream>
+#include <vector>
 
-struct Queue
-{
+using namespace std;
+
+struct Queue {
+    vector<int> data;  // Используем стандартный вектор для хранения элементов
 };
 
-Queue *queue_create()
-{
+Queue* queue_create() {
     return new Queue;
 }
 
-void queue_delete(Queue *queue)
-{
-    // TODO: free queue items
+void queue_delete(Queue* queue) {
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
-{
+void queue_insert(Queue* queue, int data) {
+    queue->data.push_back(data);  // Добавляем элемент в конец очереди
 }
 
-Data queue_get(const Queue *queue)
-{
-    return (Data)0;
+int queue_get(const Queue* queue) {
+    if (queue->data.empty()) {
+        cerr << "Ошибка: очередь пуста!" << endl;
+        return -1;  // Ошибка
+    }
+    return queue->data.front();  // Возвращаем первый элемент очереди
 }
 
-void queue_remove(Queue *queue)
-{
+void queue_remove(Queue* queue) {
+    if (!queue->data.empty()) {
+        queue->data.erase(queue->data.begin());  // Удаляем первый элемент
+    }
 }
 
-bool queue_empty(const Queue *queue)
-{
-    return true;
+bool queue_empty(const Queue* queue) {
+    return queue->data.empty();  // Проверка, пуста ли очередь
 }
