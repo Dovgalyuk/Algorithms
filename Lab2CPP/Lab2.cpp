9502e20608e15b3680fd801a76bade8c2f7cdeff
Roman Kachkurkin <whshcom@mail.ru>
2024-11-07 21:37:12 +0300

    fix user input path for file in command arguments

--- Lab2CPP/Lab2.cpp
+++ Lab2CPP/Lab2.cpp
@@ -39,25 +39,23 @@ bool isValidRegister(char reg) {
     return reg >= 'A' && reg <= 'D';
 }
 
-int main() {
-    Stack* stack = stack_create();
-    std::map<char, size_t> registers = { {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0} };
-    std::string input;
-    std::string filePath;
-
-    std::cout << "Enter the file path: ";
-    std::getline(std::cin, filePath);
-    
-    std::ifstream inputFile(filePath);
-
-    if (!inputFile.is_open()) {
-        std::cerr << "Error opening file: " << filePath << std::endl;
-        stack_delete(stack);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
         return 1;
     }
 
-    const size_t CALL_MARKER = static_cast<size_t>(-1); // Используем -1 как маркер вызова
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file: " << argv[1] << std::endl;
+        return 1;
+    }
 
+    Stack* stack = stack_create();
+    std::map<char, size_t> registers = { {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0} };
+    const size_t CALL_MARKER = static_cast<size_t>(-1);
+
+    std::string input;
     while (std::getline(inputFile, input)) {
         Command cmd = parseCommand(input);
 
@@ -66,7 +64,6 @@ int main() {
                 stack_push(stack, cmd.value);
                 break;
             }
-
             case CommandType::POP: {
                 if (stack_empty(stack)) {
                     std::cout << "BAD POP" << std::endl;
@@ -76,7 +73,6 @@ int main() {
                 }
 
                 size_t value = stack_get(stack);
-                
                 if (value == CALL_MARKER || !isValidRegister(static_cast<char>(cmd.value))) {
                     std::cout << "BAD POP" << std::endl;
                     inputFile.close();
@@ -88,12 +84,10 @@ int main() {
                 stack_pop(stack);
                 break;
             }
-
             case CommandType::CALL: {
                 stack_push(stack, CALL_MARKER);
                 break;
             }
-
             case CommandType::RET: {
                 if (stack_empty(stack) || stack_get(stack) != CALL_MARKER) {
                     std::cout << "BAD RET" << std::endl;
@@ -104,7 +98,6 @@ int main() {
                 stack_pop(stack);
                 break;
             }
-
             case CommandType::INVALID: {
                 std::cout << "Invalid command!" << std::endl;
                 inputFile.close();
