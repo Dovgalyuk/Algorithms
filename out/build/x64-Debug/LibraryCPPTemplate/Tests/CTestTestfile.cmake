571a46b6d019647e6814d97cf73281757a2a6fb4
AleksArtemiy <alekseev3artem@gmail.com>
2024-11-08 16:06:55 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/aleks/source/repos/AleksArtemiy/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: C:/Users/aleks/source/repos/AleksArtemiy/Algorithms/out/build/x64-Debug/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
