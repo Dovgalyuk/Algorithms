6880c5edbe01c8d1309f5ed631862e4693ec0e78
EKATERINAelnova <katakata45882@gmail.com>
2024-11-08 13:58:08 +0300

    try1

---
+++ Lab3CPP/lab3.cpp
@@ -0,0 +1,74 @@
+#include "queue.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int bfs(vector<vector<int>> graph, int start, int end) {
+    int n = graph.size();
+    vector<bool> visited(n, false);
+    vector<int> distance(n, -1);
+    Queue *q = queue_create();
+
+    queue_insert(q, start);
+    visited[start] = true;
+    distance[start] = 0;
+
+    while (!queue_empty(q)) {
+        int current = queue_get(q);
+        queue_remove(q);
+
+        if (current == end) {
+            return distance[current];
+        }
+
+        for (int neighbor = 0; neighbor < n; ++neighbor) {
+            if (graph[current][neighbor] == 1 && !visited[neighbor]) {
+                queue_insert(q, neighbor);
+                visited[neighbor] = true;
+                distance[neighbor] = distance[current] + 1;
+            }
+        }
+    }
+    return -1;
+}
+bool testTask(int res) {
+    if (res == 2) {
+        return true;
+    }else
+        return false;
+}
+int main(int argc, char **argv) {
+    int n, start, end;
+    ifstream input;
+    input.open(argv[1]);
+    if(!input.is_open()) {
+        cerr << "Failed to open the file." << endl;
+        return 1;
+    }
+
+    input >> n >> start >> end;
+    start--;
+    end--;
+
+    vector<vector<int>> graph(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            input >> graph[i][j];
+        }
+    }
+
+    int result = bfs(graph, start, end);
+
+    if (result == -1) {
+        cout << "IMPOSSIBLE" << endl;
+    } else {
+        if (!testTask(result)) {
+            return 1;
+        }
+        cout << result << endl;
+    }
+
+    return 0;
+}
--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,52 @@
 #include "queue.h"
+#include "vector.h"
+#include <stdexcept>
 
 struct Queue
 {
+  Vector* vec;
 };
 
 Queue *queue_create()
 {
-    return new Queue;
+  Queue *queue = new Queue;
+  queue->vec = vector_create();
+  return queue;
 }
 
 void queue_delete(Queue *queue)
 {
-    // TODO: free queue items
+    vector_delete(queue->vec);
     delete queue;
 }
 
 void queue_insert(Queue *queue, Data data)
 {
+    size_t size = vector_size(queue->vec);
+    vector_resize(queue->vec, size + 1);
+    vector_set(queue->vec, size, data);
 }
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    if (vector_size(queue->vec) == 0)
+        throw std::out_of_range("The queue is empty");
+    return vector_get(queue->vec, 0);
+
 }
 
 void queue_remove(Queue *queue)
 {
+    if (vector_size(queue->vec) == 0)
+        throw std::out_of_range("The queue is empty");
+    size_t size = vector_size(queue->vec);
+    for (size_t i = 0; i < size - 1; i++) {
+        vector_set(queue->vec, i, vector_get(queue->vec, i + 1));
+    }
+    vector_resize(queue->vec, size - 1);
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    return vector_size(queue->vec) == 0;
 }
