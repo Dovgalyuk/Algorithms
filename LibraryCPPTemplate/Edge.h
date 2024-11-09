bbc2ede9ad97c3f9912600b4c323d6d6325d9473
roman@M1402 <demixhd2@gmail.com>
2024-11-09 16:34:03 +0300

    update Edge.h

--- LibraryCPPTemplate/Edge.h
+++ LibraryCPPTemplate/Edge.h
@@ -1,12 +1,12 @@
 #pragma once
 
-template <typename E_type>
+template <typename V_type, typename E_type>
 struct Edge {
 
     Edge() {}
-    Edge(size_t a, size_t b, E_type edge_mark) : start(a), destination(b), mark(mark) {}
+    Edge(Vertex<V_type>* a, Vertex<V_type>* b, E_type mark) : start(a), destination(b), mark(mark)  {}
 
-    size_t start = std::numeric_limits<size_t>::max();
-    size_t destination = std::numeric_limits<size_t>::max();
+    Vertex<V_type> *start = nullptr;
+    Vertex<V_type> *destination = nullptr;
     E_type mark = E_type();
 };
