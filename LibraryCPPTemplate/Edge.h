1d2fd33496b20a27b43a941e2f26bd0e03a90ab1
roman@M1402 <demixhd2@gmail.com>
2024-11-09 14:50:05 +0300

    Edge.h

--- LibraryCPPTemplate/Edge.h
+++ LibraryCPPTemplate/Edge.h
@@ -2,8 +2,8 @@
 
 template <typename E_type>
 struct Edge {
-    Edge() {}
 
+    Edge() {}
     Edge(size_t a, size_t b, E_type edge_mark) : start(a), destination(b), mark(mark) {}
 
     size_t start = std::numeric_limits<size_t>::max();
