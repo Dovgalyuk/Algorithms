23ee0c596fbc93115196e62f5bb1648e73cad979
G-d <101927568+sYkunA1747@users.noreply.github.com>
2024-11-10 13:49:01 +0300

    Update stack.cpp

--- LibraryC/Tests/stack.cpp
+++ LibraryC/Tests/stack.cpp
@@ -6,20 +6,14 @@ void free_func(void* data) {
     free(data);
 }
 
+
 void free_stack_element_test(Stack** stack_head);
 void free_stack_operation_test(Stack_operation** stack_head);
 
-int main() {
 
+int main() {
     /* Тест стека элементов*/
     Stack* stack_element_head = NULL;
-    List_element* list_element_head = NULL;
-
-    Stack* new_stack_element = create_stack_element(&list_element_head, free_func);
-    if (new_stack_element == NULL) {
-        std::cerr << "Error: Failed to create stack element." << std::endl;
-        return 1;
-    }
 
     stack_push_element(&stack_element_head, 'A');
     stack_push_element(&stack_element_head, 'B');
@@ -31,18 +25,14 @@ int main() {
     }
     std::cout << std::endl;
 
-    free_stack_element_test(&stack_element_head);
-    //free_stack_element(&stack_element_head);
+    while (stack_element_head != NULL) {
+        Stack* temp = stack_element_head;
+        stack_element_head = stack_element_head->next;
+        free(temp);
+    }
 
     /*Тест стека операций*/
     Stack_operation* stack_operation_head = NULL;
-    List_operation* list_operation_head = NULL;
-
-    Stack_operation* new_stack_operation = create_stack_operation(&list_operation_head, free_func);
-    if (new_stack_operation == NULL) {
-        std::cerr << "Error: Failed to create stack operation." << std::endl;
-        return 1;
-    }
 
     stack_push_operation(&stack_operation_head, '+');
     stack_push_operation(&stack_operation_head, '-');
@@ -54,11 +44,15 @@ int main() {
     }
     std::cout << std::endl;
 
-    free_stack_operation_test(&stack_operation_head);
-    //free_stack_operation(&stack_operation_head);
+    while (stack_operation_head != NULL) {
+        Stack_operation* temp = stack_operation_head;
+        stack_operation_head = stack_operation_head->next;
+        free(temp);
+    }
 
     return 0;
 }
+
 
 void free_stack_element_test(Stack** stack_head){
     while(*stack_head != NULL){
@@ -67,6 +61,7 @@ void free_stack_element_test(Stack** stack_head){
         delete temp;
     }
 }
+
 
 void free_stack_operation_test(Stack_operation** stack_head){
     while(*stack_head != NULL){
