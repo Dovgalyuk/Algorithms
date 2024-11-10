99fb307bb191f1b72c626156f7447d7689e4e22c
G-d <101927568+sYkunA1747@users.noreply.github.com>
2024-11-10 13:48:14 +0300

    Update list.cpp

--- LibraryC/Tests/list.cpp
+++ LibraryC/Tests/list.cpp
@@ -2,15 +2,12 @@
 #include "list.h"
 
 struct List_element {
-
     Data element;
     struct List_element *next;
     FFree *distruct;
 };
 
-
 struct List_operation {
-
     Data operation;
     struct List_operation *next;
     FFree *distruct;
@@ -22,26 +19,21 @@ void delete_operation_test(List_operation **head);
 void add_element(List_element **head, Data x);
 void add_operation(List_operation **head, Data x);
 
-void  print_list_element(List_element **head);
-void  print_list_operation(List_operation **head);
+void print_list_element(List_element **head);
+void print_list_operation(List_operation **head);
 
 int main(void){
-
-    List_element *head_1 = NULL;
-    List_operation *head_2 = NULL;
-
-    create_list_element(&head_1,free);
-    create_list_operation(&head_2,free);
+    List_element *head_1 = nullptr;
+    List_operation *head_2 = nullptr;
 
     add_element(&head_1, Data('A'));
     add_element(&head_1, Data('B'));
-    add_operation(&head_2,Data('1'));
-    add_operation(&head_2,Data('2'));
+    add_operation(&head_2, Data('1'));
+    add_operation(&head_2, Data('2'));
 
-
-    std::cout << "List_elements:";
+    std::cout << "List_elements: ";
     print_list_element(&head_1);
-    std::cout << "List_operation";
+    std::cout << "List_operation: ";
     print_list_operation(&head_2);
 
     delete_element_test(&head_1);
@@ -50,9 +42,7 @@ int main(void){
     return 0;
 }
 
-
 void add_element(List_element **head, Data x){
-
     List_element *new_element = new List_element;
     if(new_element != nullptr){
         new_element -> element = x;
@@ -61,9 +51,7 @@ void add_element(List_element **head, Data x){
     }
 }
 
-
 void add_operation(List_operation **head, Data x){
-
     List_operation *new_operation = new List_operation;
     if(new_operation != nullptr){
         new_operation -> operation = x;
@@ -72,33 +60,26 @@ void add_operation(List_operation **head, Data x){
     }
 }
 
-
-void  print_list_element(List_element **head){
-
+void print_list_element(List_element **head){
     List_element *temp = *head;
-
-    while(temp != NULL){
-        std::cout << temp -> element << " ";
+    while(temp != nullptr){
+        std::cout << temp->element << " ";
         temp = temp -> next;
     }
-    std::cout << std::endl;
+    std::cout << '\n';
 }
 
-
-void  print_list_operation(List_operation **head){
-
+void print_list_operation(List_operation **head){
     List_operation *temp = *head;
-
-    while(temp != NULL){
-        std::cout << temp -> operation << " ";
+    while(temp != nullptr){
+        std::cout << temp->operation << " ";
         temp = temp -> next;
     }
-    std::cout << std::endl;
+    std::cout << '\n';
 }
-
 
 void delete_element_test(List_element **head){
-    while(*head != NULL){
+    while(*head != nullptr){
         List_element *temp = *head;
         *head = (*head) -> next;
         delete temp;
@@ -106,9 +87,10 @@ void delete_element_test(List_element **head){
 }
 
 void delete_operation_test(List_operation **head){
-    while(*head != NULL){
+    while(*head != nullptr){
         List_operation *temp = *head;
         *head = (*head) -> next;
         delete temp;
     }
 }
+
