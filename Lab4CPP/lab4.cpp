fd05f84cb57a7cae5b57a40b3ee66830dba94d90
roman@M1402 <demixhd2@gmail.com>
2024-11-10 14:19:30 +0300

    added init and output

--- Lab4CPP/lab4.cpp
+++ Lab4CPP/lab4.cpp
@@ -1,78 +1,91 @@
-#include <cassert>
 #include <iostream>
+#include <fstream>
+#include <limits>
 #include "../LibraryCPPTemplate/graph.h"
 #include "../LibraryCPPTemplate/vector.h"
 
-int main() {
+#define ERROR_VALUE std::numeric_limits<size_t>::max()
+
+std::pair<unsigned int, Vector<std::string>> find_min_path(Graph<std::string, unsigned int> graph, size_t a, size_t b);
+int init(Graph<std::string, unsigned int>& graph, std::ifstream& in);
+void output(std::pair<unsigned int, Vector<std::string>> data);
+
+int main(int argc, char** argv) {
     setlocale(0, "");
-    Graph<std::string, std::string> graph;
+    std::string filepath = argv[1];
+    std::ifstream in(filepath);
 
-    // Тест 1: Добавление вершин
-    graph.add_vertex("A");
-    graph.add_vertex("B");
-    graph.add_vertex("C");
-
-    // Проверка меток вершин
-    Vector<std::string> marks = graph.get_vertices_marks();
-    assert(marks.size() == 3);
-    assert(marks[0] == "A");
-    assert(marks[1] == "B");
-    assert(marks[2] == "C");
-    marks.clear();
-    std::cout << "Тест 1 пройден: Добавление вершин." << std::endl;
-
-    // Тест 2: Добавление рёбер
-    graph.add_edge(1, 2, "m");
-    graph.add_edge(0, 2, "a");
-    graph.add_edge(2, 0, "am");
-    graph.add_edge(2, 2, "II");
-
-    Vector<Edge<std::string, std::string>> edges = graph.get_edges();
-
-    assert(edges.size() == 4);
-    assert(edges[0].destination->number == 2 && edges[0].mark == "m");
-    assert(edges[1].start->number == 0 && edges[1].destination->number == 2);
-    assert(edges[2].start->number == 2 && edges[2].mark == "am");
-    std::cout << "Тест 2 пройден: Добавление рёбер." << std::endl;
-
-    // Тест 3: Проверка связности
-    assert(graph.is_bounded(1, 2) == true);
-    assert(graph.is_bounded(2, 2) == true);
-    assert(graph.is_bounded(2, 1) == false);
-    std::cout << "Тест 3 пройден: Проверка связности." << std::endl;
-
-    // Тест 4: Проверка работы итератора
-    Vector<Vertex<std::string>> test;
-    for (auto it = graph.begin(2); it != graph.end(2); ++it) {
-        test.push(*it);
+    if (!in.is_open()) {
+        "Не удалось открыть файл\n";
+        return 1;
     }
-    assert(test[0].mark == "A");
-    assert(test[1].mark == "C");
-    std::cout << "Тест 4 пройден: Проверка работы итератора." << std::endl;
 
-    // Тест 5: Удаление рёбер
-    graph.delete_edge(0, 2);
-    assert(graph.is_bounded(0, 2) == false); // A-B должно быть удалено
-    std::cout << "Тест 5 пройден: Удаление рёбер." << std::endl;
+    Graph<std::string, unsigned int> graph;
+    if (init(graph, in))
+        return 1;
 
-    // Тест 6: Удаление вершины
-    graph.delete_vertex(1);
-    marks = graph.get_vertices_marks();
-    assert(marks.size() == 2);
-    assert(marks[0] == "A");
-    assert(marks[1] == "C");
-    marks.clear();
-    std::cout << "Тест 6 пройден: Удаление вершины B." << std::endl;
+    size_t a = 0, b = 0;
+    in >> a >> b;
+    if (a == ERROR_VALUE || b == ERROR_VALUE) {
+        std::cout << "Некорректные индексы для нахождения расстония\n";
+        return 1;
+    }
+    output(find_min_path(graph, a, b));
 
-    // Тест 7: Удаление оставшихся вершин
-    graph.delete_vertex(0);
-    graph.delete_vertex(0);
-    //graph.delete_vertex(0);
-    marks = graph.get_vertices_marks();
-    assert(marks.size() == 0);
-    marks.clear();
-    std::cout << "Тест 7 пройден: Удаление оставшихся вершин." << std::endl;
-
-    std::cout << "Все тесты пройдены успешно!" << std::endl;
     return 0;
+}
+
+std::pair<unsigned int, Vector<std::string>> find_min_path(Graph<std::string, unsigned int> graph, size_t a, size_t b) {
+
+    return { 0, Vector<std::string>() };
+}
+
+int init(Graph<std::string, unsigned int>& graph, std::ifstream &in) {
+    int k = 0;
+    std::string temp = "";
+    in >> k;
+    if (k <= 0 || k > std::numeric_limits<int>::max()) {
+        std::cout << "Некорректный количество вершин\n";
+        return 1;
+    }
+
+    while (k) {
+        in >> temp;
+        if (temp == "") {
+            std::cout << "Некорректное значение пометки вершины\n";
+            return 1;
+        }
+        graph.add_vertex(temp);
+        k--;
+    }
+
+    size_t edge_mark = 0, a = 0, b = 0;
+    in >> k;
+    if (k <= 0 || k > std::numeric_limits<int>::max()) {
+        std::cout << "Некорректный количество ребер\n";
+        return 1;
+    }
+
+    while (k) {
+        if (a != -1) {
+            in >> a >> b >> edge_mark;
+            if (a == ERROR_VALUE || b == ERROR_VALUE || edge_mark == ERROR_VALUE) {
+                std::cout << "Некорректный индекс или пометка ребра\n";
+                return 1;
+            }
+            graph.add_edge(a, b, edge_mark);
+        }
+        k--;
+    }
+
+    return 0;
+}
+
+void output(std::pair<unsigned int, Vector<std::string>> data) {
+    std::cout << "Минимальное расстояние от " << data.second[0] << " до " << data.second[data.second.size() - 1] << " : " << data.first << '\n';
+    std::cout << "Путь: ";
+    for (size_t i = 0; i < data.second.size(); i++) {
+        std::cout << data.second[i] << " ";
+    }
+    std::cout << '\n';
 }
