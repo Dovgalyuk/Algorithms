1a14e4602988dbbeef948cd556acd11f1308ff2e
Василина Юганова <iuganova.vasilina@yandex.ru>
2024-11-11 22:13:21 +0300

    Update

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -4,11 +4,11 @@
 
 Array* array_create_and_read(FILE* input) {
     int n;
-    fscanf(input, "%d", &n);
+    fscanf_s(input, "%d", &n);
     Array* arr = array_create(n, NULL);
     for (int i = 0; i < n; ++i) {
         int x;
-        fscanf(input, "%d", &x);
+        fscanf_s(input, "%d", &x);
         array_set(arr, i, x);
     }
     return arr;
@@ -34,13 +34,21 @@ void task1(Array* arr, size_t size) {
 }
 
 void task2(Array* arr, size_t size) {
-    int* frequency = (int*)calloc(size, sizeof(int));
+    int max_value = (int)array_get(arr, 0);
+    for (size_t i = 1; i < size; i++) {
+        int current = (int)array_get(arr, i);
+        if (current > max_value) {
+            max_value = current;
+        }
+    }
+    int* frequency = (int*)calloc(max_value + 1, sizeof(int));
     if (frequency == NULL) {
         printf("Memory allocation failed for frequency array");
         return;
     }
 
     if (arr != NULL) {
+        
         int most_frequent = (int)array_get(arr, 0);
         int max_count = 0;
         for (size_t i = 0; i < size; i++) {
