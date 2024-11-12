38e48a8043067511a584baa9d174fdfc349ccf27
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-13 02:16:00 +0300

    Update vector.cpp

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -2,8 +2,7 @@
 #include <stdio.h>
 #include "vector.h"
 
-void myfree(void *p)
-{
+void myfree(void *p) {
     delete (int*)p;
 }
 
@@ -13,12 +12,11 @@ int vector_get_int(Vector *v, size_t i) {
         return -1;
     }
 
-
     if (i >= vector_size(v)) {
         std::cerr << "Ошибка: индекс " << i << " вне границ вектора.\n";
         return -1;
     }
-       
+
     Data data = vector_get(v, i);
     if (data == nullptr) {
         std::cerr << "Ошибка: данные по индексу " << i << " являются NULL!\n";
@@ -36,8 +34,7 @@ int main() {
     }
 
     vector_resize(vector, 5);
-    if (vector_size(vector) != 5)
-    {
+    if (vector_size(vector) != 5) {
         std::cout << "Invalid resize\n";
         return 1;
     }
@@ -45,21 +42,6 @@ int main() {
     for (size_t i = 0 ; i < vector_size(vector) ; ++i)
         vector_set(vector, i, new int(i));
 
-    for (size_t i = 0; i < vector_size(vector); ++i) {
-        int value = vector_get_int(vector, i);
-        if (value == -1) { // Если возникла ошибка (например, NULL)
-            break; // Завершить цикл, если элемент равен NULL
-        }
-        std::cout << value << " "; // Выводить значение, если нет ошибки
-    }
-
-    vector_resize(vector, 10);
-    if (vector_size(vector) != 10)
-    {
-        std::cout << "Invalid resize\n";
-        return 1;
-    }
-
     std::cout << "Vector: ";
     for (size_t i = 0; i < vector_size(vector); ++i) {
         int value = vector_get_int(vector, i);
@@ -70,39 +52,18 @@ int main() {
     }
     std::cout << "\n";
 
-    vector_resize(vector, 3);
-    std::cout << "Vector3: ";
-    if (vector_size(vector) != 3)
+    // Производительный тест
+    vector_resize(vector, 10000000); // Установите размер заранее
+    for (int i = 0 ; i < 10000000 ; ++i)
     {
-        std::cout << "Invalid resize\n";
-        return 1;
-    }
-    std::cout << "Vector3: ";
-    for (size_t i = 0; i < vector_size(vector); ++i) {
-        int value = vector_get_int(vector, i);
-        if (value == -1) { // Если возникла ошибка (например, NULL)
-            break; // Завершить цикл, если элемент равен NULL
-        }
-        std::cout << value << " "; // Выводить значение, если нет ошибки
-    }
-
-    std::cout << "Vector: ";
-    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-        std::cout << vector_get_int(vector, i) << " ";
-    std::cout << "\n";
-
-    // Performance test
-    for (int i = 1 ; i <= 10000000 ; ++i)
-    {
-        vector_resize(vector, i);
-        vector_set(vector, i - 1, new int(i));
+        vector_set(vector, i, new int(i));
     }
 
     long long sum = 0;
     for (int i = 0 ; i < 10000000 ; ++i)
         sum += vector_get_int(vector, i);
 
-    std::cout << sum << "\n";
+    std::cout << "Sum: " << sum << "\n";
 
     vector_delete(vector);
     return 0;
