68973bc33c457a157b0b0edd2ec9892c82f03e89
exgon <enbaevva@gmail.com>
2024-11-13 15:20:39 +0300

    lab_1
    включил предлагаемый тест

--- out/build/x64-Debug/LibraryCPP/Tests/CTestTestfile.cmake
+++ out/build/x64-Debug/LibraryCPP/Tests/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(TestArrayCPP "TestArrayCPP")
+set_tests_properties(TestArrayCPP PROPERTIES  _BACKTRACE_TRIPLES "D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/LibraryCPP/Tests/CMakeLists.txt;4;add_test;D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/LibraryCPP/Tests/CMakeLists.txt;0;")
