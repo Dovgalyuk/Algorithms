3f7ea20457f3cc4170769dd0da35091fa58f49f9
exgon <enbaevva@gmail.com>
2024-11-13 15:50:08 +0300

    lab_1
    Удалил папку out

--- out/build/x64-Debug/LibraryCPP/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/LibraryCPP
-# Build directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/out/build/x64-Debug/LibraryCPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
