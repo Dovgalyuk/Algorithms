d3b29750ec55c913b38da1b57bb04b81eec97197
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-13 19:24:49 +0300

    Update vector.cpp

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -67,12 +67,10 @@ int main()
     // Выводим значения из вектора на экран
     std::cout << "Vector: ";
     for (size_t i = 0; i < vector_size(vector); ++i) {
-        int value = vector_get_int(vector, i); // Получаем значение из вектора
-        
-        // if (value == -1) { // Если возникла ошибка, прерываем цикл
-        //     break; 
-        // }
-
+        int value = vector_get_int(vector, i); // Получаем значение из вектора       
+        if (value == -1) { // Если возникла ошибка, прерываем цикл
+            break; 
+        }
         std::cout << value << " "; // Выводим значение
     }
     std::cout << "\n";
