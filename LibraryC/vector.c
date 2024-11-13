bcc683f1bdb929ae52a4e1735f6be3f4a87d6005
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-13 23:47:06 +0300

    Update vector.c

--- LibraryC/vector.c
+++ LibraryC/vector.c
@@ -122,7 +122,7 @@ void vector_resize(Vector *v, size_t new_size) {
 
     // Увеличиваем емкость
     if (new_size > v->capacity) {
-        size_t new_capacity = v->capacity > 0 ? v->capacity * 2 : 1; 
+        size_t new_capacity = (new_size > v->capacity) ? (v->capacity * 2) : v->capacity;  
         while (new_capacity < new_size) {
             new_capacity *= 2;
         }
@@ -141,9 +141,6 @@ void vector_resize(Vector *v, size_t new_size) {
         v->data = new_data;
         v->capacity = new_capacity;
     }
-
-    // Увеличиваем размер
-    v->size = new_size;
 }
 
 // Функция для добавления элемента в конец вектора
