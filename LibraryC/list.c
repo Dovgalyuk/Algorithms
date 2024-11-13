43362b781b476883aac6cd6567c6e5077fa29a97
G-d <101927568+sYkunA1747@users.noreply.github.com>
2024-11-13 17:00:32 +0300

    Update list.c

--- LibraryC/list.c
+++ LibraryC/list.c
@@ -1,6 +1,12 @@
 #include "list.h"
 #include <stdlib.h>
 
+struct List_element {
+    Data element;
+    struct List_element *next;
+    FFree *distruct;
+};
+
 
 List_element *create_list_element (List_element **head, FFree f) {
     *head = (List_element *)malloc(sizeof(List_element));
