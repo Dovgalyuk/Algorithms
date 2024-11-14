bc8ad3305b07427f14de94f4da65808c52fc85ca
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-14 06:15:13 +0300

    Update vector.cpp

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -42,35 +42,35 @@ int main()
         return 1;
     }
 
-    // for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-    //     vector_set(vector, i, new int(i));
+    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+        vector_set(vector, i, new int(i));
 
-    // for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-    // {
-    //     if (vector_get_int(vector, i) != (int)i)
-    //     {
-    //         std::cout << "Invalid vector element " << i << "\n";
-    //         return 1;
-    //     }
-    // }
+    for (size_t i = 0 ; i < vector_size(vector) ; ++i)
+    {
+        if (vector_get_int(vector, i) != (int)i)
+        {
+            std::cout << "Invalid vector element " << i << "\n";
+            return 1;
+        }
+    }
 
-    // vector_resize(vector, 10);
-    // if (vector_size(vector) != 10)
-    // {
-    //     std::cout << "Invalid resize\n";
-    //     return 1;
-    // }
+    vector_resize(vector, 10);
+    if (vector_size(vector) != 10)
+    {
+        std::cout << "Invalid resize\n";
+        return 1;
+    }
 
 
-    // // Выводим значения из вектора на экран
-    // std::cout << "Vector: ";
-    // for (size_t i = 0; i < vector_size(vector); ++i) {
-    //     if (vector_get_int(vector, i) == -1) { // Если возникла ошибка, прерываем цикл
-    //         break; 
-    //     }
-    //     std::cout << vector_get_int(vector, i) << " "; // Выводим значение
-    // }
-    // std::cout << "\n";
+    // Выводим значения из вектора на экран
+    std::cout << "Vector: ";
+    for (size_t i = 0; i < vector_size(vector); ++i) {
+        if (vector_get_int(vector, i) == -1) { // Если возникла ошибка, прерываем цикл
+            break; 
+        }
+        std::cout << vector_get_int(vector, i) << " "; // Выводим значение
+    }
+    std::cout << "\n";
 
 
     // vector_resize(vector, 3);
