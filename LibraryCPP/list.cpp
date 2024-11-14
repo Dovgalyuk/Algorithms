e1cfed8f002a90212669273da5f5989299e15e54
SaNiokIV <sivanovbeteltrans@gmail.com>
2024-11-14 21:23:57 +0300

    Lab2 Без главного файла.

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,120 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* next;
 };
 
 struct List
 {
+    ListItem* head;
+    ListItem* tail;
 };
 
-List *list_create()
+List* list_create()
 {
-    return new List;
+    List* list = new List;
+    list->head = nullptr;
+    list->tail = nullptr;
+    return list;
 }
 
-void list_delete(List *list)
+void list_delete(List* list)
 {
-    // TODO: free items
+    ListItem* current = list->head;
+    while (current != nullptr)
+    {
+        ListItem* next = current->next;
+        delete current;
+        current = next;
+    }
     delete list;
 }
 
-ListItem *list_first(List *list)
+ListItem* list_first(List* list)
 {
-    return NULL;
+    return list->head;
 }
 
-Data list_item_data(const ListItem *item)
+Data list_item_data(const ListItem* item)
 {
-    return (Data)0;
+    return item->data;
 }
 
-ListItem *list_item_next(ListItem *item)
+ListItem* list_item_next(ListItem* item)
 {
-    return NULL;
+    return item->next;
 }
 
-ListItem *list_item_prev(ListItem *item)
+ListItem* list_insert(List* list, Data data)
 {
-    return NULL;
+    ListItem* item = new ListItem;
+    item->data = data;
+    item->next = list->head;
+
+    if (list->head == nullptr)
+    {
+        list->tail = item;
+    }
+
+    list->head = item;
+    return item;
 }
 
-ListItem *list_insert(List *list, Data data)
+ListItem* list_insert_after(List* list, ListItem* item, Data data)
 {
-    return NULL;
+    if (item == nullptr)
+    {
+        return nullptr;
+    }
+
+    ListItem* new_item = new ListItem;
+    new_item->data = data;
+    new_item->next = item->next;
+    item->next = new_item;
+
+    if (item == list->tail)
+    {
+        list->tail = new_item;
+    }
+
+    return new_item;
 }
 
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
+ListItem* list_erase_first(List* list)
 {
-    return NULL;
+    if (list->head == nullptr)
+    {
+        return nullptr;
+    }
+
+    ListItem* item = list->head;
+    list->head = item->next;
+
+    if (list->head == nullptr)
+    {
+        list->tail = nullptr;
+    }
+
+    delete item;
+    return list->head;
 }
 
-ListItem *list_erase_first(List *list)
+ListItem* list_erase_next(List* list, ListItem* item)
 {
-    return NULL;
-}
+    if (item == nullptr || item->next == nullptr)
+    {
+        return nullptr;
+    }
 
-ListItem *list_erase_next(List *list, ListItem *item)
-{
-    return NULL;
+    ListItem* to_delete = item->next;
+    item->next = to_delete->next;
+
+    if (to_delete == list->tail)
+    {
+        list->tail = item;
+    }
+
+    delete to_delete;
+    return item->next;
 }
