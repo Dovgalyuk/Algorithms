e75ec0872926a329c0a24bb0da23145eff6cec2c
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-14 03:30:11 +0300

    Update vector.c

--- LibraryC/vector.c
+++ LibraryC/vector.c
@@ -148,6 +148,8 @@ void vector_resize(Vector *v, size_t new_size) {
 
         free(v->data);
     }
+    v->size = new_size;
+    free(v->data);
 }
 
 // void vector_resize(Vector *v, size_t new_size) {
