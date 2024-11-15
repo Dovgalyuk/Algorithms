41c32e73fb6f2cb970fdb0d97e5823dcefbd44b0
exgon <enbaevva@gmail.com>
2024-11-15 13:11:55 +0300

    lab_1
    Удалил временные файлы

--- out/build/x64-Debug/LibraryC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/LibraryC
-# Build directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/out/build/x64-Debug/LibraryC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: D:/Учеба/НовГУ/Курс 2/СДИА/Algorithms/out/build/x64-Debug/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
