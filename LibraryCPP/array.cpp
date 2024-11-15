af7fb7c73e3b40943384f9a35d6df3942048ed7c
student_Novgu <nickita.gilyuck2016@yandex.ru>
2024-11-15 19:30:03 +0300

    1

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,72 +1,29 @@
+#include <iostream>
 #include "array.h"
 
-struct Array
+int main()
 {
-    Data* array;    
-    size_t size;  
-};
+    Array *arr = array_create(10);
 
-// create array
-Array *array_create(size_t size)
-{
-    if(size !=  0) {
-        Array* array = new Array;
-        array->size = size;
-        array->array = new Data[size];
-
-        return array;
-    }
-    else {
-        return nullptr;
-    }
-}
-
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    if(arr->array != nullptr)  {
-        delete[] arr->array;
-        delete arr;
-    }
-}
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    if (index < arr->size)
-        return arr->array[index];
-    else
-        return 0;
-}
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
-    if (index < arr->size)
+    if (array_size(arr) != 10)
     {
-        arr->array[index] = value;
-}
-}
-
-// returns array size
-size_t array_size(const Array *arr)
-{
-    if (arr->array != nullptr) {
-        return arr->size;
-    }
-    return 0;
-}
-Array* fill_array(std::ifstream &input)
-{
-    size_t size;
-    Data value;
-    input >> size;
-    Array* array = array_create(size);
-
-    for (size_t i = 0; i < size; i++) {
-        input >> value; 
-        array_set(array, i, value);
+        std::cout << "Invalid array size\n";
+        array_delete(arr);
+        return 1;
     }
 
-    return array;
+    for (int i = 0 ; i < 10 ; ++i)
+        array_set(arr, i, i * 2);
+
+    for (int i = 0 ; i < 10 ; ++i)
+    {
+        if (array_get(arr, i) != i * 2)
+        {
+            std::cout << "Invalid array element " << i << "\n";
+            array_delete(arr);
+            return 1;
+        }
+    }
+
+    array_delete(arr);
 }
