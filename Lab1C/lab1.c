f49e52b559c8d87a43c7228ba3ffa6f046f3be6e
student_Novgu <nickita.gilyuck2016@yandex.ru>
2024-11-16 20:55:03 +0300

    new

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,44 +1,40 @@
-#include <iostream>
-#include <fstream>
+#include <stdio.h>
 #include "array.h"
 
-using namespace std;
-Array *array_create_and_read(ifstream &input)
+Array *array_create_and_read(FILE *input)
 {
     int n;
-    input>>n;
+    fscanf(input, "%d", &n);
     /* Create array */
-    Array *arr = new Array(n);
+    Array *arr = array_create(n, NULL);
     /* Read array data */
     for (int i = 0 ; i < n ; ++i)
     {
         int x;
-        input >> x;
-        arr->set(i, x);
+        fscanf(input, "%d", &x);
+        array_set(arr, i, x);
     }
     return arr;
 }
 
 void task1(Array *arr)
 {
-
 }
 
 void task2(Array *arr)
 {
-    
 }
 
 int main(int argc, char **argv)
 {
     Array *arr = NULL;
-    ifstream input(argv[1]);
+    FILE *input = fopen(argv[1], "r");
     arr = array_create_and_read(input);
     task1(arr);
-    delete arr;
+    array_delete(arr);
     /* Create another array here */
     arr = array_create_and_read(input);
     task2(arr);
-    delete arr;
-    input.close();
+    array_delete(arr);
+    fclose(input);
 }
--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,25 +2,12 @@
 
 struct Array
 {
-    Data* data;
-    size_t size;
-
-    Array(size_t size)
-    {
-        this->size = size;
-        this->data = new Data[size];
-    }
-
-    ~Array()
-    {
-        delete[] this->data;
-    }
 };
 
 // create array
 Array *array_create(size_t size)
 {
-    return new Array(size);
+    return new Array;
 }
 
 // delete array, free memory
@@ -32,17 +19,16 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return arr->data[index];                                 
+    return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
-    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return arr->size;
+    return 0;
 }
