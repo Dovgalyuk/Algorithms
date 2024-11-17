22a6cfc5d8c16cd80cc4f536f24fa8a151a6be0d
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-17 04:47:58 +0300

    Update vector.cpp

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -71,7 +71,7 @@ int main()
 
     std::cout << "Vector: ";
     for (size_t i = 0 ; i < vector_size(vector) ; ++i)
-        std::cout << vector_get_int(vector, i) << " ";
+        std::cerr << vector_get_int(vector, i) << " ";
     std::cout << "\n";
 
     // Performance test
