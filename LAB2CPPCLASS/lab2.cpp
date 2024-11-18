92b559b1ac17b3c8aea84e20347db54a5b0153ba
knox <nikita.3.khorev@gmail.com>
2024-11-18 04:11:42 +0300

    add command types register of proccesor

--- LAB2CPPCLASS/lab2.cpp
+++ LAB2CPPCLASS/lab2.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include "stack.h"
+
+using namespace std;
+
+enum class typeCommand { PUSH, POP, CALL, RET, INVALID };
 
 int main(int argc, char* argv[])
 {
-
-
+    
     return 0;
 }
