664de1ce71160fc71cb775203fb058e0461e316f
extar17 <zfilimonenko0630@gmail.com>
2024-11-19 10:20:35 +0300

    убрал временные файлы

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dz/Algorithms/Algorithms
-# Build directory: D:/Dz/Algorithms/Algorithms/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPP")
-subdirs("Lab1CPP")
--- out/build/x64-Debug/Lab1CPP/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dz/Algorithms/Algorithms/Lab1CPP
-# Build directory: D:/Dz/Algorithms/Algorithms/out/build/x64-Debug/Lab1CPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1CPP "D:/Dz/Algorithms/Algorithms/out/build/x64-Debug/Lab1CPP/Lab1CPP.exe" "D:/Dz/Algorithms/Algorithms/Lab1CPP/input.txt")
-set_tests_properties(TestLab1CPP PROPERTIES  _BACKTRACE_TRIPLES "D:/Dz/Algorithms/Algorithms/Lab1CPP/CMakeLists.txt;5;add_test;D:/Dz/Algorithms/Algorithms/Lab1CPP/CMakeLists.txt;0;")
