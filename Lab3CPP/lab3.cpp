94f6c6d9c7ee26c6aae8be209c783ff6a14f70eb
Анна Архангельская <arhfotina@gmail.com>
2024-11-19 20:02:33 +0300

    lab3

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <stdexcept>
 #include "queue.h"
@@ -9,29 +8,21 @@
 using namespace std;
 
 struct Graph {
-    Vector* cities;
-    Vector** adjList;
+    Vector* adjList[100];
     int numCities;
 
-    Graph(int maxCities) {
-        cities = vector_create();
-        adjList = new Vector*[maxCities];
-        for (int i = 0; i < maxCities; ++i) {
+    Graph() {
+        for (int i = 0; i < 100; ++i) {
             adjList[i] = vector_create();
         }
         numCities = 0;
     }
 
     ~Graph() {
-    for (int i = 0; i < 100; ++i) {
-        if (adjList[i]) {
+        for (int i = 0; i < numCities; ++i) {
             vector_delete(adjList[i]);
         }
     }
-    delete[] adjList;
-    vector_delete(cities);
-}
-
 
     int getCityIndex(const string& city, string* cityNames, int& cityCount) {
         for (int i = 0; i < cityCount; ++i) {
@@ -49,8 +40,8 @@ struct Graph {
     void addEdge(const string& city1, const string& city2, string* cityNames, int& cityCount) {
         int index1 = getCityIndex(city1, cityNames, cityCount);
         int index2 = getCityIndex(city2, cityNames, cityCount);
-        vector_set(adjList[index1], vector_size(adjList[index1]), static_cast<int>(index2));
-        vector_set(adjList[index2], vector_size(adjList[index2]), static_cast<int>(index1));
+        vector_set(adjList[index1], vector_size(adjList[index1]), index2);
+        vector_set(adjList[index2], vector_size(adjList[index2]), index1);
     }
 };
 
@@ -70,28 +61,18 @@ void bfs(Graph& graph, const string& start, const string& end, string* cityNames
     vector_set(visited, startIdx, 1);
     queue_insert(queue, startIdx);
 
+    bool pathFound = false;
+
     while (!queue_empty(queue)) {
         int currentIdx = static_cast<int>(queue_get(queue));
         queue_remove(queue);
 
         if (currentIdx == endIdx) {
-            Vector* path = vector_create();
-            int city = endIdx;
-            while (city != -1) {
-                vector_set(path, vector_size(path), city);
-                city = vector_get(prev, city);
-            }
-
-            for (int i = 0; i < static_cast<int>(vector_size(path)); ++i) {
-                cout << cityNames[vector_get(path, i)] << " ";
-            }
-            cout << endl;
-
-            vector_delete(path);
+            pathFound = true;
             break;
         }
 
-        for (int i = 0; i < static_cast<int>(vector_size(graph.adjList[currentIdx])); ++i) {
+        for (size_t i = 0; i < vector_size(graph.adjList[currentIdx]); ++i) {
             int neighborIdx = vector_get(graph.adjList[currentIdx], i);
             if (vector_get(visited, neighborIdx) == 0) {
                 vector_set(visited, neighborIdx, 1);
@@ -101,12 +82,42 @@ void bfs(Graph& graph, const string& start, const string& end, string* cityNames
         }
     }
 
-    queue_delete(queue);
+    if (pathFound) {
+        Vector* path = vector_create();
+        int city = endIdx;
+        while (city != -1) {
+            vector_set(path, vector_size(path), city);
+            city = vector_get(prev, city);
+        }
+
+        for (int i = static_cast<int>(vector_size(path)) - 1; i >= 0; --i) {
+            cout << cityNames[vector_get(path, i)] << " ";
+        }
+        cout << endl;
+
+        vector_delete(path);
+    } else {
+        cout << "Нет пути от " << start << " до " << end << endl;
+    }
+
     vector_delete(prev);
     vector_delete(visited);
+    queue_delete(queue);
 }
 
 
+void printGraph(const Graph& graph, string* cityNames, int cityCount) {
+    cout << "Граф:" << endl;
+    for (int i = 0; i < cityCount; ++i) {
+        cout << cityNames[i] << ": ";
+        for (size_t j = 0; j < vector_size(graph.adjList[i]); ++j) {
+            int neighborIdx = vector_get(graph.adjList[i], j);
+            cout << cityNames[neighborIdx] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -120,45 +131,20 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    int maxCities = 100;
-    Graph graph(maxCities);
+    Graph graph;
+
     string cityNames[100];
     int cityCount = 0;
 
     string city1, city2;
-    string cities[100];
-    int citiesCount = 0;
 
     while (input >> city1 >> city2) {
-        if (!input.eof()) {
-            graph.addEdge(city1, city2, cityNames, cityCount);
-        }
-        if (citiesCount < 100) {
-            bool foundCity1 = false;
-            bool foundCity2 = false;
-
-            for (int i = 0; i < citiesCount; ++i) {
-                if (cities[i] == city1) {
-                    foundCity1 = true;
-                }
-                if (cities[i] == city2) {
-                    foundCity2 = true;
-                }
-            }
-
-            if (!foundCity1) {
-                cities[citiesCount++] = city1;
-            }
-            if (!foundCity2) {
-                cities[citiesCount++] = city2;
-            }
-        }
+        if (input.eof()) break;
+        graph.addEdge(city1, city2, cityNames, cityCount);
     }
 
-    if (citiesCount >= 2) {
-        string start = cities[citiesCount - 2];
-        string end = cities[citiesCount - 1];
-        bfs(graph, start, end, cityNames, cityCount);
+    if (cityCount >= 2) {
+        bfs(graph, city1, city2, cityNames, cityCount);
     } else {
         cerr << "Недостаточно данных для определения начального и конечного городов." << endl;
     }
--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,47 +1,69 @@
-#include "vector.h"
 #include "queue.h"
+#include "vector.h"
 #include <stdexcept>
 
-struct Queue {
-    Vector* vector;
-    size_t frontIndex;
+struct Queue
+{
+    Vector* vec;
+    size_t size;
+    size_t front;
+    size_t back;
 };
 
-Queue* queue_create() {
-    Queue* queue = new Queue;
-    queue->vector = vector_create();
-    queue->frontIndex = 0;
+Queue *queue_create()
+{
+    Queue *queue = new Queue;
+    queue->vec = vector_create();
+    vector_resize(queue->vec, 1);
+    queue->size = 0;
+    queue->front = 0;
+    queue->back = 0;
     return queue;
 }
 
-void queue_delete(Queue* queue) {
-    vector_delete(queue->vector);
+void queue_delete(Queue *queue)
+{
+    vector_delete(queue->vec);
     delete queue;
 }
 
-void queue_insert(Queue* queue, Data data) {
-    vector_resize(queue->vector, vector_size(queue->vector) + 1);
-    vector_set(queue->vector, vector_size(queue->vector) - 1, data);
+void queue_insert(Queue *queue, Data data)
+{
+    if (queue->size == vector_size((queue->vec))) {
+        size_t new_size = queue->size * 2;
+        Vector *new_vec = vector_create();
+        vector_resize(new_vec, new_size);
+
+        //Копируем элементы
+        for (size_t i = 0; i < queue->size; ++i) {
+            vector_set(new_vec, i, vector_get(queue->vec, (queue->front + i) % vector_size(queue->vec)));
+        }
+        vector_delete(queue->vec);
+        queue->vec = new_vec;
+        queue->front = 0;
+        queue->back = queue->size;
+    }
+    vector_set(queue->vec, queue->back, data);
+    queue->back = (queue->back + 1) % vector_size(queue->vec);
+    queue->size++;
 }
 
-Data queue_get(const Queue* queue) {
-    if (queue_empty(queue)) {
-        throw std::out_of_range("Queue is empty");
-    }
-    return vector_get(queue->vector, queue->frontIndex);
+Data queue_get(const Queue *queue)
+{
+    if (queue->size == 0)
+        throw std::out_of_range("The queue is empty");
+    return vector_get(queue->vec, queue->front);
 }
 
-void queue_remove(Queue* queue) {
-    if (queue_empty(queue)) {
-        throw std::out_of_range("Queue is empty");
-    }
-    queue->frontIndex++;
-    if (queue->frontIndex >= vector_size(queue->vector)) {
-        queue->frontIndex = 0;
-        vector_resize(queue->vector, 0);  // Очистка вектора
-    }
+void queue_remove(Queue *queue)
+{
+    if (queue->size == 0)
+        throw std::out_of_range("The queue is empty");
+    queue->front = (queue->front + 1) % vector_size(queue->vec);
+    queue->size--;
 }
 
-bool queue_empty(const Queue* queue) {
-    return queue->frontIndex >= vector_size(queue->vector);
+bool queue_empty(const Queue *queue)
+{
+    return queue->size == 0;
 }
