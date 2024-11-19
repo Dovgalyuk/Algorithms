6b2956e5908bf213aecbc1668ae0d852ba21cca6
kthk <clamtea08@gmail.com>
2024-11-19 15:04:44 +0300

    Lab1

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -2,40 +2,38 @@
 #include <fstream>
 #include <cstdlib>
 #include <cmath>
-#include "array.h"
+#include "../LibraryCPP/array.h"
 
 using namespace std; 
 
-Array *array_create_and_read(const string& filename){
-    ifstream inputFile(filename);
-    if (!inputFile) {
-        cerr << "Couldn't open this file: " << filename << endl;
+Array *array_create_and_read(ifstream& input){
+    if (!input) {
+        cerr << "Couldn't open this file." <<  endl;
         return nullptr;
     }
 
     size_t new_size;
-    inputFile >> new_size;
+    input >> new_size;
 
-    Array *arr = array_create(new_size, 0);
+    Array *arr = array_create(new_size);
     if (!arr){
         return nullptr;
     } 
 
     for (size_t i = 0; i < new_size; ++i) {
         int number;
-        if (!(inputFile >> number)) {
-            cerr << "Couldn't read file." << endl;
+        if (!(input >> number)) {
+            cerr << "Couldn't read number from file." << endl;
             array_delete(arr);
             return nullptr;
         }
         array_set(arr, i, number);
     }
-
-    inputFile.close();
     return arr;
 }
 
-void task1(Array* arr) {
+void task1(ifstream& input) {
+    Array* arr = array_create_and_read(input);
     int max = 0;
     int counter = 0;
     for(int i = 0; i<array_size(arr); i++){
@@ -55,35 +53,42 @@ void task1(Array* arr) {
     cout << "Result: " << counter;
 }
 
-void task2(Array* arr) {
+void task2(ifstream& input) {
+    Array* arr = array_create_and_read(input);
     int a;
     int b;
+
+    input >> a;
+    input >> b;
+
     size_t index = 0;
 
     for (int i = 0; i < array_size(arr); ++i) {
         int num = array_get(arr, i);
+
         if (num < a || num > b) {
             array_set(arr, index++, array_get(arr, i));
         }
     }
 
-    while(index<array_size(arr)){
+    while (index < array_size(arr)) {
         array_set(arr, index++, 0);
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < array_size(arr); i++){
+        cout << array_get(arr, i) << ' ';
     }
 }
 
 int main() {
-    Array* arr1 = array_create_and_read("input_1.txt");
-    if (arr1) {
-        task1(arr1);
-        array_delete(arr1);
-    }
+    ifstream input_1("input_1.txt");
+    task1(input_1);
+    input_1.close();
 
-    Array* arr2 = array_create_and_read("input_2.txt");
-    if (arr2) {
-        task2(arr2);
-        array_delete(arr2);
-    }
-
+    ifstream input_2("input_2.txt");
+    task2(input_2);
+    input_2.close();
     return 0;
 }
