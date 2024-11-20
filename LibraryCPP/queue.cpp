f02c8d74e6ea672f727c740b72e3c29bf6534737
AlexSipaylo <SipayloSasha@yandex.ru>
2024-11-20 04:13:36 +0300

    Lab3 fix queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -3,47 +3,55 @@
 #include <cstdlib>
 
 struct Queue {
-    Vector* vector;
-    Data front;
-    Data back;
+	Vector* vector;
+	size_t front;
+	size_t back;
+	size_t size;
 };
 
 Queue* queue_create() {
-    Queue* queue = (Queue*)malloc(sizeof(Queue));
-    if (queue != nullptr) {
-        queue->vector = vector_create();
-        queue->front = queue->back = 0;
-    }
-    return queue;
+	Queue* queue = (Queue*)malloc(sizeof(Queue));
+	if (queue != nullptr) {
+		queue->vector = vector_create();
+		vector_resize(queue->vector, 1);
+		queue->front = queue->back = queue->size = 0;
+	}
+	return queue;
 }
 
 void queue_delete(Queue* queue) {
-    vector_delete(queue->vector);
-    free(queue);
+	vector_delete(queue->vector);
+	free(queue);
 }
 
 void queue_insert(Queue* queue, Data data) {
-    vector_resize(queue->vector, vector_size(queue->vector) + 1);
-    vector_set(queue->vector, queue->back, data);
-    queue->back++;
+	if (queue->size == vector_size(queue->vector)) {
+		vector_resize(queue->vector, vector_size(queue->vector) * 2);
+		queue->front = 0;
+		queue->back = queue->size;
+	}
+	vector_set(queue->vector, queue->back, data);
+	queue->back = (queue->back + 1) % vector_size(queue->vector);
+	queue->size++;
 }
 
 Data queue_get(const Queue* queue) {
-    if (queue_empty(queue) || queue == nullptr) {
-        return (Data)0;
-    }
-    return vector_get(queue->vector, queue->front);
+	if (queue_empty(queue) || queue == nullptr) {
+		return (Data)0;
+	}
+	return vector_get(queue->vector, queue->front);
 }
 
 void queue_remove(Queue* queue) {
-    if (!queue_empty(queue)) {
-        queue->front++;
-    }
+	if (!queue_empty(queue)) {
+		queue->front = (queue->front + 1) % vector_size(queue->vector);
+		queue->size--;
+	}
 }
 
 bool queue_empty(const Queue* queue) {
-    if (queue != nullptr && queue->front == queue->back) {
-        return true;
-    }
-    return false;
+	if (queue != nullptr && queue->size == 0) {
+		return true;
+	}
+	return false;
 }
