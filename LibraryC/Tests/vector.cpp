9336bff86e92b973ad6118a691d9364fb1897eb2
wXwSmilewXw <mr.romanuza@mail.ru>
2024-11-21 19:21:54 +0300

    *

--- LibraryC/Tests/vector.cpp
+++ LibraryC/Tests/vector.cpp
@@ -21,7 +21,6 @@ int main()
     Vector *vector = vector_create(myfree);
 
     vector_resize(vector, 5);
-    std::cerr << "resize 5 end";
     if (vector_size(vector) != 5)
     {
         std::cout << "Invalid resize\n";
