14116b9b3c44d5c619ebacebd271405f2cc9496b
Bitlod <118824401+Bitlod@users.noreply.github.com>
2024-11-23 11:45:54 +0300

    Новая версия

--- array.cpp
+++ array.cpp
@@ -1,44 +1,48 @@
 #include "array.h"
-#include "stdlib.h"
-#include <iostream>
-using namespace std;
-
+#include "array.h"
 struct Array
 {
-    int* arr;
+    Data* data;
+    size_t size;
 };
 
 // create array
 Array* array_create(size_t size)
 {
-    int* arr = new int[size];
-    if (*arr == NULL) {
-        cout << "Null pointer returned";
-    }
-    return new Array;
+    Array* arr = new Array;
+    arr->data = new Data[size];
+    arr->size = size;
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array* arr)
 {
-    delete [] arr;
+    delete[] arr->data;
+    delete arr;
 }
 
 // returns specified array element
 Data array_get(const Array* arr, size_t index)
 {
-
-    return (*arr); index;
+    if (index < arr->size)
+    {
+        return arr->data[index];
+    }
+    return (Data)0;
 }
 
 // sets the specified array element to the value
 void array_set(Array* arr, size_t index, Data value)
 {
-    (*arr); index = value;
+    if (index < arr->size)
+    {
+        arr->data[index] = value;
+    }
 }
 
 // returns array size
 size_t array_size(const Array* arr)
 {
-    return sizeof(*arr)/sizeof(arr);
+    return arr->size;
 }
