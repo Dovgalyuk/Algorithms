0d237d3504334cc4bdcb298b63d509257060dd8d
Артемий Алексеев <alekseev3artem@gmail.com>
2024-11-25 08:32:08 +0300

    add library-files

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -1,61 +1,115 @@
 #include <cstddef>
 #include "list.h"
 
-struct ListItem
-{
+struct ListItem {
+    Data data;
+    ListItem *next;
+    ListItem *prev;
 };
 
-struct List
-{
+struct List {
+    ListItem *first;
+    ListItem *last;
 };
 
-List *list_create()
-{
-    return new List;
+List *list_create() {
+    List *list = new List;
+    list->first = nullptr;
+    list->last = nullptr;
+    return list;
 }
 
-void list_delete(List *list)
-{
+void list_delete(List *list) {
+    ListItem *curItem = list->first;
+
+    while(curItem != nullptr) {
+        ListItem *nextItem = curItem->next;
+        delete curItem;
+        curItem = nextItem;
+    }
     // TODO: free items
     delete list;
 }
 
-ListItem *list_first(List *list)
-{
-    return NULL;
+ListItem *list_first(List *list) {
+    return list->first;
 }
 
-Data list_item_data(const ListItem *item)
-{
-    return (Data)0;
+Data list_item_data(const ListItem *item) {
+    return item->data;
 }
 
-ListItem *list_item_next(ListItem *item)
-{
-    return NULL;
+ListItem *list_item_next(ListItem *item) {
+    return item->next;
 }
 
-ListItem *list_item_prev(ListItem *item)
-{
-    return NULL;
+ListItem *list_item_prev(ListItem *item) {
+    if (item->prev != nullptr) {
+        return item->prev;
+    } else {
+        return nullptr;
+    }
 }
 
-ListItem *list_insert(List *list, Data data)
-{
-    return NULL;
+ListItem *list_insert(List *list, Data data) {
+    ListItem *newItem = new ListItem{data, nullptr, nullptr};  
+
+    if (list->first == nullptr) {
+        list->first = newItem;
+        list->last = newItem;
+    } else {
+        newItem->next = list->first;
+        list->first->prev = newItem;
+        list->first = newItem;
+    }
+
+    return newItem;
 }
 
-ListItem *list_insert_after(List *list, ListItem *item, Data data)
-{
-    return NULL;
+ListItem *list_insert_after(List *list, ListItem *item, Data data) {
+    if (item->next == nullptr) {
+        return list_insert(list, data);
+    } else {
+        ListItem *item_insert = new ListItem{data, item, item->next};
+        item->next = item_insert;
+        item_insert->next->prev = item_insert;
+
+        return item_insert;
+    }
 }
 
-ListItem *list_erase_first(List *list)
-{
-    return NULL;
+ListItem *list_erase_first(List *list) {
+    if (list->first == nullptr) {
+        return nullptr;
+    }
+
+    ListItem *deleteItem = list->first;
+    list->first = deleteItem->next;
+
+    if (list->first != nullptr) {
+        list->first->prev = nullptr;
+    } else {
+        list->last = nullptr;
+    }
+
+    delete deleteItem;
+    return list->first;
 }
 
-ListItem *list_erase_next(List *list, ListItem *item)
-{
-    return NULL;
+ListItem *list_erase_next(List *list, ListItem *item) {
+    if (item == nullptr || item->next == nullptr) {
+        return nullptr;
+    } else if (item->next->next == nullptr) {
+        delete item->next;
+        list->last = item;
+        item->next = nullptr;
+
+        return nullptr;
+    } else {
+        ListItem *deleteItem = item->next;
+        item->next = item->next->next;
+        item->next->prev = item;
+        delete deleteItem;
+        return item->next;
+    }
 }
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,44 @@
 #include "stack.h"
+#include "list.h"
 
-struct Stack
-{
+struct Stack {
+    List *list = nullptr;
+    ListItem *last = nullptr;
 };
 
-Stack *stack_create()
-{
-    return new Stack;
+Stack *stack_create() {
+    Stack *stack = new Stack;
+    stack->list = list_create();
+    return stack;
 }
 
-void stack_delete(Stack *stack)
-{
-    // TODO: free stack elements
-    delete stack;
+void stack_delete(Stack *stack) {
+    if (stack->list!=nullptr){
+        list_delete(stack->list);
+        stack->list = nullptr;
+        stack->last = nullptr;
+        delete stack;
+    }
 }
 
-void stack_push(Stack *stack, Data data)
-{
+void stack_push(Stack *stack, Data data) {
+    stack->last = list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack)
-{
-    return (Data)0;
+Data stack_get(const Stack *stack) {
+    ListItem *item = stack->last;
+    if (item != nullptr) {
+        return list_item_data(item);
+    } else {
+        return (Data)0;
+    }
 }
 
-void stack_pop(Stack *stack)
-{
+void stack_pop(Stack *stack) {
+    stack->last = list_erase_first(stack->list);
 }
 
-bool stack_empty(const Stack *stack)
-{
-    return true;
+bool stack_empty(const Stack *stack) {
+    if (stack->last == nullptr) return true;
+    else return false;
 }
