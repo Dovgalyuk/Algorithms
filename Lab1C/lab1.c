7b6509bf58023143238b8df73dc11e353aad37ba
root <root@DESKTOP-OP76534.>
2024-11-26 16:25:37 +0300

    попытка

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -5,36 +5,85 @@ Array *array_create_and_read(FILE *input)
 {
     int n;
     fscanf(input, "%d", &n);
-    /* Create array */
     Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
+    for (int i = 0; i < n; ++i)
     {
         int x;
         fscanf(input, "%d", &x);
-        array_set(arr, i, x);
+        array_set(arr, i, (Data)x);
     }
     return arr;
 }
 
 void task1(Array *arr)
 {
+    int positive = 0, negative = 0, zero = 0;
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        int value = (int)array_get(arr, i);
+        if (value > 0)
+            positive++;
+        else if (value < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Array elements:\n");
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        printf("%d ", (int)array_get(arr, i));
+    }
+    printf("\nPositive: %d, Negative: %d, Zero: %d\n", positive, negative, zero);
 }
 
 void task2(Array *arr)
 {
+    int min_diff = __INT_MAX__;
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        int value1 = (int)array_get(arr, i);
+        if (value1 % 2 != 0)
+            continue;
+        for (size_t j = i + 1; j < array_size(arr); ++j)
+        {
+            int value2 = (int)array_get(arr, j);
+            if (value2 % 2 != 0)
+                continue;
+            int diff = value1 - value2;
+            if (diff < 0)
+                diff = -diff;
+            if (diff < min_diff)
+                min_diff = diff;
+        }
+    }
+    printf("Minimum difference between different even elements: %d\n", min_diff);
 }
 
 int main(int argc, char **argv)
 {
-    Array *arr = NULL;
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
     FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
+    if (input == NULL)
+    {
+        perror("Failed to open input file");
+        return 1;
+    }
+
+    Array *arr = array_create_and_read(input);
     task1(arr);
     array_delete(arr);
-    /* Create another array here */
+
+    rewind(input);
     arr = array_create_and_read(input);
     task2(arr);
     array_delete(arr);
+
     fclose(input);
+    return 0;
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,62 @@
-#include <stdlib.h>
 #include "array.h"
+#include <stdlib.h>
 
 typedef struct Array
 {
+    size_t size;
+    Data *data;
+    FFree *distruct;
 } Array;
 
-// create array
-Array *array_create(size_t size, FFree f)
+Array *array_create(size_t size, FFree field)
 {
-    return malloc(sizeof(Array));
+    Array *arr = malloc(sizeof(Array));
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    arr->data = malloc(size * sizeof(Data));
+    if (arr->data == NULL) {
+        free(arr);
+        return NULL;
+    }
+
+    arr->distruct = field;
+    arr->size = size;
+    return arr;
 }
 
-// delete array, free memory
 void array_delete(Array *arr)
 {
+    if (arr == NULL) {
+        return;
+    }
+
+    if (arr->distruct != NULL) {
+        for (size_t i = 0; i < arr->size; i++) {
+            arr->distruct((void*)arr->data[i]);
+        }
+    }
+    free(arr->data);
     free(arr);
 }
 
-// returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    return arr->data[index];
 }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+int array_set(Array *arr, size_t index, Data value)
 {
-}
-
-// returns array size
-size_t array_size(const Array *arr)
-{
+    if (arr == NULL || index >= arr->size) {
+        return -1;
+    }
+    arr->data[index] = value;
     return 0;
 }
+
+size_t array_size(const Array *arr)
+{
+    return arr->size;
+}
+
