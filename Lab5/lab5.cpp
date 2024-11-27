dd2acda314ebffb155ff54ea357987a324ae44d6
roman@M1402 <demixhd2@gmail.com>
2024-11-27 20:45:31 +0300

    lab5

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -1,9 +1,102 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <chrono>
 #include <set>
-#include "../LibraryCPPClass/Set.h"
+#include "Set.h"
 
-//int main() {
-//	std::cout << "LAB_5 TEST\n";
-//	return 0;
-//}
+struct data {
+    double insert, find, remove;
+    size_t number;
+};
+
+data stdSet(int n) {
+    std::set<std::string> set;
+    data temp;
+    temp.number = n;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.insert(std::to_string(i));
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> res = end - start;
+    temp.insert = res.count();
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.find(std::to_string(i));
+    }
+    end = std::chrono::high_resolution_clock::now();
+    res = end - start;
+    temp.find = res.count();
+
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.erase(std::to_string(i));
+    }
+    end = std::chrono::high_resolution_clock::now();
+    res = end - start;
+    temp.remove = res.count();
+
+    return temp;
+}
+
+data mySet(int n) {
+    Set set;
+    data temp;
+    temp.number = n;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.insert(std::to_string(i));
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> res = end - start;
+    temp.insert = res.count();
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.find(std::to_string(i));
+    }
+    end = std::chrono::high_resolution_clock::now();
+    res = end - start;
+    temp.find =  res.count();
+
+
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; ++i) {
+        set.remove(std::to_string(i));
+    }
+    end = std::chrono::high_resolution_clock::now();
+    res = end - start;
+    temp.remove = res.count();
+
+    return temp;
+}
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::ofstream outMy("mySet", std::ios::binary);
+    std::ofstream outStd("stdSet", std::ios::binary);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < n; i += 100) {
+        system("cls");
+        std::cout << "Current n value: " << i << '\n';
+        data res;
+        res = mySet(i + 1);
+        outMy.write(reinterpret_cast<char*>(&res), sizeof(data));
+        res = stdSet(i + 1);
+        outStd.write(reinterpret_cast<char*>(&res), sizeof(data));
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> res = end - start;
+    std::cout << "Time left: " << res.count() << " seconds" << std::endl;
+
+    outMy.close();
+    outStd.close();
+
+	return 0;
+}
+
--- LibraryCPPClass/Tests/SetTest.cpp
+++ LibraryCPPClass/Tests/SetTest.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
-#include "../Set.h"
+#include <string>
+#include "Set.h"
 
-int main() {
-	std::cout << "SET TEST\n";
-	Set set;
-	set.insert("c");
-	set.insert("a");
-	set.insert("c");
-	set.insert("b");
-	std::cout << set.find("c") << '\n';
-	std::cout << set.find("a") << '\n';
-	std::cout << set.find("b") << '\n';
-	return 0;
+int basicTest() {
+    Set set;
+
+    set.insert("A");
+    set.insert("B");
+    set.insert("C");
+
+    if (set.find("A") == nullptr) {
+        std::cout << "Invalid insertion - 1" << std::endl;
+        return 1;
+    }
+
+    if (set.find("B") == nullptr) {
+        std::cout << "Invalid insertion - 2" << std::endl;
+        return 2;
+    }
+
+    if (set.find("C") == nullptr) {
+        std::cout << "Invalid insertion - 3" << std::endl;
+        return 3;
+    }
+
+    if (set.find("D") != nullptr) {
+        std::cout << "Invalid insertion and seraching - 4" << std::endl;
+        return 4;
+    }
+
+    set.remove("B");
+    if (set.find("B") != nullptr) {
+        std::cout << "Invalid removing - 5" << std::endl;
+        return 5;
+    }
+
+    return 0;
+}
+
+void performanceTest() {
+    Set set;
+
+    for (int i = 1; i <= 1e6; ++i) {
+        set.insert(std::to_string(i));
+    }
+
+    for (int i = 1; i <= 1e6; ++i) {
+        set.find(std::to_string(i));
+    }
+
+    for (int i = 1; i <= 1e6; ++i) {
+        set.remove(std::to_string(i));
+    }
+}
+
+int main()
+{
+    int res = basicTest();
+    if (res) return res;
+    performanceTest();
+
+    std::cout << "All tests was passed!" << std::endl;
+
+    return 0;
 }
