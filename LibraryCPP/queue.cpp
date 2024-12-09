d2103a82c578e5291be839cffd15812abf8c0fe4
SaNiokIV <sivanovbeteltrans@gmail.com>
2024-12-09 22:14:55 +0300

    Библиотеки без проги

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,59 @@
 #include "queue.h"
+#include "vector.h"
 
-struct Queue
-{
+struct Queue {
+    Vector *vector;
+    size_t size;
+    size_t front;
+    size_t back;
 };
 
-Queue *queue_create()
-{
-    return new Queue;
+Queue *queue_create() {
+    Queue *queue = new Queue;
+    queue->vector = vector_create();
+    vector_resize(queue->vector, 1);
+    queue->size = 0;
+    queue->front = 0;
+    queue->back = 0;
+    return queue;
 }
 
-void queue_delete(Queue *queue)
-{
-    // TODO: free queue items
+void queue_delete(Queue *queue) {
+    vector_delete(queue->vector);
     delete queue;
 }
 
-void queue_insert(Queue *queue, Data data)
+void queue_insert(Queue* queue, Data data)
 {
+    if (queue->size == vector_size((queue->vector))) {
+        size_t new_size = queue->size * 2;
+        Vector* new_vec = vector_create();
+        vector_resize(new_vec, new_size);
+
+        for (size_t i = 0; i < queue->size; ++i) {
+            vector_set(new_vec, i, vector_get(queue->vector, (queue->front + i) % vector_size(queue->vector)));
+        }
+        vector_delete(queue->vector);
+        queue->vector = new_vec;
+        queue->front = 0;
+        queue->back = queue->size;
+    }
+    vector_set(queue->vector, queue->back, data);
+    queue->back = (queue->back + 1) % vector_size(queue->vector);
+    queue->size++;
 }
 
-Data queue_get(const Queue *queue)
-{
-    return (Data)0;
+Data queue_get(const Queue *queue) {
+    if (queue->size == 0) return 0;
+    return vector_get(queue->vector, queue->front);
 }
 
-void queue_remove(Queue *queue)
-{
+void queue_remove(Queue *queue) {
+    if (queue->size == 0) return;
+    queue->front = (queue->front + 1) % vector_size(queue->vector);
+    queue->size--;
 }
 
-bool queue_empty(const Queue *queue)
-{
-    return true;
+bool queue_empty(const Queue *queue){
+    return queue->size == 0;
 }
--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,34 +1,45 @@
 #include "vector.h"
 
-struct Vector
-{
+struct Vector {
+    Data *data;
+    size_t size;
+    size_t capacity;
 };
 
-Vector *vector_create()
-{
-    return new Vector;
+Vector *vector_create() {
+    Vector *vector = new Vector;
+    vector->capacity = 10;
+    vector->capacity = 0;
+    vector->data = new Data[vector->capacity];
+    return vector;
 }
 
-void vector_delete(Vector *vector)
-{
-    // TODO: free vector internals
+void vector_delete(Vector *vector) {
+    if (vector->data != nullptr) delete[] vector->data;
     delete vector; 
 }
 
-Data vector_get(const Vector *vector, size_t index)
-{
-    return (Data)0;
+Data vector_get(const Vector *vector, size_t index) {
+    if (vector->size <= index) return (Data)0;
+    return vector->data[index];
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
-{
+void vector_set(Vector *vector, size_t index, Data value) {
+    if (vector->size <= index) return;
+    vector->data[index] = value;
 }
 
-size_t vector_size(const Vector *vector)
-{
-    return 0;
+size_t vector_size(const Vector *vector) {
+    return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
-{
+void vector_resize(Vector *vector, size_t size) {
+    if (size > vector->capacity) {
+        while (vector->capacity < size) vector->capacity *= 2;
+        Data *new_data = new Data[vector->capacity];
+        for (size_t i = 0 ; i < vector->size ; ++i) new_data[i] = vector->data[i];
+        delete[] vector->data;
+        vector->data = new_data;
+    }
+    vector->size = size;
 }
