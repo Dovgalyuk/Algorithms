a4945ef767efbbfd3e6034187f91a258280d8245
Roman <roma.gego@mail.ru>
2024-12-12 00:32:57 +0300

    Update lab3.cpp

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -6,34 +6,32 @@ using namespace std;
 
 void bfs(int start, int finish, int n, int** graph, int* parent) {
     Queue* q = queue_create();
-    bool* visited = new bool[n + 1];
-
+    
+    
     for (int i = 1; i <= n; ++i) {
-        visited[i] = false;
+        parent[i] = -1;
     }
 
     queue_insert(q, start);
-    visited[start] = true;
+    parent[start] = start;  
 
     while (!queue_empty(q)) {
         int current = queue_get(q);
         queue_remove(q);
 
         for (int i = 1; i <= n; ++i) {
-            if (graph[current][i] == 1 && !visited[i]) {
-                visited[i] = true;
+            if (graph[current][i] == 1 && parent[i] == -1) { 
                 parent[i] = current;
                 queue_insert(q, i);
 
                 if (i == finish) {
-                    delete[] visited;
+                    queue_delete(q);
                     return;
                 }
             }
         }
     }
     queue_delete(q);
-    delete[] visited;
 }
 
 int main(__attribute__((unused)) int argc, char** argv  ) {
@@ -42,12 +40,10 @@ int main(__attribute__((unused)) int argc, char** argv  ) {
     int n, start, finish;
     input >> n >> start >> finish;
 
-    
     int** graph = new int*[n + 1];
     for (int i = 0; i <= n; ++i) {
         graph[i] = new int[n + 1]();
     }
-
 
     int u, v;
     while (input >> u >> v) {
@@ -56,10 +52,7 @@ int main(__attribute__((unused)) int argc, char** argv  ) {
     }
 
     int* parent = new int[n + 1];
-    for (int i = 1; i <= n; ++i) {
-        parent[i] = -1;
-    }
-
+    
     bfs(start, finish, n, graph, parent);
 
     if (parent[finish] == -1) {
@@ -72,7 +65,6 @@ int main(__attribute__((unused)) int argc, char** argv  ) {
             path[path_length++] = at;
         }
 
-        
         for (int i = path_length - 1; i >= 1; --i) {
             cout << path[i] << " ";
         }
