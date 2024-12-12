23e7230d1b8d0493f20a9cfa0e6557820ccbdf88
EKATERINAelnova <katakata45882@gmail.com>
2024-12-12 08:57:10 +0300

    fix41

--- LibraryCPPTemplate/Tests/graph.cpp
+++ LibraryCPPTemplate/Tests/graph.cpp
@@ -3,10 +3,9 @@
 #include "vector.h"
 #include <algorithm>
 
-typedef Graph<int, int> MyGraph;
 
 int main() {
-    MyGraph graph(7);
+    Graph<int, int> graph(7);
 
     // Test 1: get all vertex marks
     Vector<int> vertices = graph.getAllVertexMarks();
@@ -14,7 +13,7 @@ int main() {
         for (size_t i = 0; i < vertices.size(); ++i) {
             std::cout << vertices.get(i) << " ";
         }
-        std::cout << "\nTest1: Incorrect graph\n";
+        std::cout << "\ntest1.Incorrect graph\n";
         return 1;
     }
 
@@ -25,7 +24,7 @@ int main() {
         for (size_t i = 0; i < vertices2.size(); ++i) {
             std::cout << vertices2.get(i) << " ";
         }
-        std::cout << "\nTest2: Incorrect graph\n";
+        std::cout << "\ntest2.Incorrect graph\n";
         return 1;
     }
 
@@ -36,7 +35,7 @@ int main() {
         for (size_t i = 0; i < vertices3.size(); ++i) {
             std::cout << vertices3.get(i) << " ";
         }
-        std::cout << "\nTest3: Incorrect graph\n";
+        std::cout << "\ntest3.Incorrect graph\n";
         return 1;
     }
 
@@ -47,65 +46,59 @@ int main() {
     graph.addEdge(7, 2, 255);
 
     if (!graph.hasEdge(7, 4)) {
-        std::cout << "\nTest4: Graph does not have 7 --> 4 edge\n";
+        std::cout << "\ntest4.Graph doesn't have 7 to 4 edge\n";
         for (auto i = graph.begin(7); i != graph.end(7); ++i) {
             std::cout << "To: " << (*i).path << " Mark: " << (*i).sign << '\n';
         }
         return 1;
     }
     if (!graph.hasEdge(7, 1)) {
-        std::cout << "\nTest4: Graph does not have 7 --> 1 edge\n";
+        std::cout << "\ntest4.Graph doesn't have 7 to 1 edge\n";
         for (auto i = graph.begin(7); i != graph.end(7); ++i) {
             std::cout << "To: " << (*i).path << " Mark: " << (*i).sign << '\n';
         }
         return 1;
     }
     if (!graph.hasEdge(7, 2)) {
-        std::cout << "\nTest4: Graph does not have 7 --> 2 edge\n";
+        std::cout << "\ntest4.Graph doesn't have 7 to 2 edge\n";
         for (auto i = graph.begin(7); i != graph.end(7); ++i) {
             std::cout << "To: " << (*i).path << " Mark: " << (*i).sign << '\n';
         }
         return 1;
     }
 
-    // Test 5: Removing edge
+    //test 5.removing edge
     graph.removeEdge(7, 1);
     if (graph.hasEdge(7, 1)) {
-        std::cout << "\nTest5: Graph still has 10 --> 1 edge\n";
-        for (auto i = graph.begin(10); i != graph.end(10); ++i) {
+        std::cout << "\ntest5.Graph hasn't 7 to 1 edge\n";
+        for (auto i = graph.begin(7); i != graph.end(7); ++i) {
             std::cout << "To: " << (*i).path << " Mark: " << (*i).sign << std::endl;
         }
         return 1;
     }
 
-    // Test 6: Removing vertex
-    graph.addVertex();
-    graph.addEdge(3, 4, -15);
-    graph.removeVertex(7);
-    if (!graph.hasEdge(3, 4)) {
-        std::cout << "\nTest6: Graph does not have 3 --> 4 edge\n";
-        for (auto it = graph.begin(3); it != graph.end(3); ++it) {
-            std::cout << "To: " << (*it).path << " Mark: " << (*it).sign << std::endl;
-        }
-        return 1;
-    }
-
-    // Test 7: Setting edge sign
+    //test 6.setting edge sign
     graph.addVertex();
     graph.addEdge(1, 2, 1000);
     graph.setEdgeMark(1, 2, 1000);
     if (graph.getEdgeMark(1, 2) != 1000) {
-        std::cout << "\nTest7: Graph edge sign: " << graph.getEdgeMark(1, 2) << '\n';
+        std::cout << "\ntest7. Graph edge sign. \n";
         return 1;
     }
 
-    // Test 8: Getting vertex sign
+    //test 7. getting vertex sign
     graph.setVertexMark(5, 11);
     if (graph.getVertexMark(5) != 11) {
-        std::cout << "Test8: Graph vertex sign: " << graph.getVertexMark(9) << '\n';
+        std::cout << "test8.Graph vertex sign.\n";
         return 1;
     }
 
-    std::cout << "All tests passed successfully!\n";
+    //test 8. getting size
+    if (graph.getSize() != 9) {
+        std::cout << "test9.Graph size.\n";
+        return 1;
+    }
+
+    std::cout << "all tests passed. success.\n";
     return 0;
 }
--- LibraryCPPTemplate/vector.h
+++ LibraryCPPTemplate/vector.h
@@ -2,125 +2,88 @@
 #define VECTOR_TEMPLATE_H
 
 #include <cstddef>
+#include <stdexcept>
 
-
-template <typename Data> class Vector
-{
+template <typename Data>
+class Vector {
 public:
     // Creates vector
-    Vector();
+    Vector() : data(nullptr), capacity(0), sizee(0) {}
 
-    Vector(size_t size);
+    Vector(size_t size) : data(new Data[size]), capacity(size), sizee(0) {}
 
-    // copy constructor
-    Vector(const Vector &vec);
+    // Copy constructor
+    Vector(const Vector &vec)
+        : data(new Data[vec.capacity]), capacity(vec.capacity), sizee(vec.sizee) {
+        for (size_t i = 0; i < vec.sizee; i++) {
+            data[i] = vec.data[i];
+        }
+    }
 
-    // assignment operator
-    Vector &operator=(const Vector &vec);
+    // Assignment operator
+    Vector &operator=(const Vector &vec) {
+        if (this == &vec) {
+            return *this;
+        }
+
+        delete[] data;
+
+        capacity = vec.capacity;
+        sizee = vec.sizee;
+        data = new Data[capacity];
+        for (size_t i = 0; i < sizee; i++) {
+            data[i] = vec.data[i];
+        }
+
+        return *this;
+    }
 
     // Deletes vector structure and internal data
-    ~Vector();
+    ~Vector() {
+        delete[] data;
+    }
 
     // Retrieves vector element with the specified index
-    Data& get(size_t index) const;
+    Data &get(size_t index) const {
+        if (index >= sizee) {
+            throw std::out_of_range("out of range");
+        }
+        return data[index];
+    }
 
     // Sets vector element with the specified index
-    void set(size_t index, Data value);
+    void set(size_t index, Data value) {
+        if (index >= sizee) {
+            throw std::out_of_range("out of range set");
+        }
+        data[index] = value;
+    }
 
     // Retrieves current vector size
-    size_t size() const;
+    size_t size() const {
+        return sizee;
+    }
 
-    void resize(size_t new_size);
+    // Change capacity
+    void resize(size_t new_size) {
+        if (new_size > capacity) {
+            size_t new_capacity = new_size * 2;
+            Data *newData = new Data[new_capacity];
+            for (size_t i = 0; i < sizee; ++i) {
+                newData[i] = data[i];
+            }
+
+            delete[] data;
+            data = newData;
+            capacity = new_capacity;
+        }
+        sizee = new_size;
+    }
 
 private:
-    // private data should be here
-    Data* data;
+    Data *data;
     size_t capacity;
     size_t sizee;
 };
 
-//Constructors
-template<typename Data>
-Vector<Data>::Vector():data(nullptr), capacity(0), sizee(0) {}
-
-template<typename Data>
-Vector<Data>::Vector(size_t initCap):data(new Data[initCap]), capacity(initCap), sizee(0) {}
-
-template<typename Data>
-Vector<Data>::Vector(const Vector& vec)
-        :data(new Data[vec.capacity])
-        , capacity(vec.capacity)
-        , sizee(vec.sizee)
-{
-    for(size_t i = 0; i < vec.sizee; i++) {
-        data[i] = vec.data[i];
-    }
-}
-
-// assignment operator
-template<typename Data>
-Vector<Data>& Vector<Data>::operator=(const Vector<Data>& vec) {
-    if (this == &vec) {
-        return *this;
-    }
-
-    delete[] data;
-
-    capacity = vec.capacity;
-    sizee = vec.sizee;
-    data = new Data[capacity];
-    for (size_t i = 0; i < sizee; i++) {
-        data[i] = vec.data[i];
-    }
-
-    return *this;
-}
-
-
-// Destructor
-template<typename Data>
-Vector<Data>::~Vector() {
-    delete[] data;
-}
-
-template<typename Data>
-Data& Vector<Data>::get(size_t index) const {
-    if (index >= size()) {
-        std::out_of_range("out of range");
-    }
-    return data[index];
-}
-
-template<typename Data>
-void Vector<Data>::set(size_t index, Data value) {
-    if (index >= size()) {
-        std::out_of_range("out of range set");
-    }
-    data[index] = value;
-}
-
-template<typename Data>
-size_t Vector<Data>::size() const{
-    return sizee;
-}
-
-// Change capacity
-template<typename Data>
-void Vector<Data>::resize(size_t new_size) {
-    if (new_size > capacity) {
-        size_t new_capacity = new_size * 2;
-        Data* newData = new Data[new_capacity];
-        for (size_t i = 0; i < sizee; ++i) {
-            newData[i] = data[i];
-        }
-
-        delete[] data;
-        data = newData;
-        capacity = new_capacity;
-    }
-    sizee = new_size;
-}
-
-
-
-#endif
+#endif // VECTOR_TEMPLATE_H
