fabf887f3d0a44c8b2184bef668401ae559adea6
wendanoa <wendanoa@student.21-school.ru>
2024-12-13 21:21:29 +0300

    log3

--- Lab4CPP/lab4.cpp
+++ Lab4CPP/lab4.cpp
@@ -4,15 +4,17 @@
 
 void dfs(Graph<int, int>& graph, size_t vertex, std::vector<int>& visited) {
     if (vertex >= visited.size()) {
-        throw std::invalid_argument("vertex");
+        throw std::invalid_argument("vertex out of bounds");
     }
     visited[vertex] = 1;
 
     for (auto it = graph.begin(vertex); it != graph.end(vertex); it++) {
         size_t neighbor = (*it).get_mark();
-        if (neighbor > visited.size()) {
-            throw std::invalid_argument("neighbor");
+        
+        if (neighbor >= visited.size()) {
+            throw std::invalid_argument("neighbor out of bounds");
         }
+        
         if (visited[neighbor] == 0) {
             dfs(graph, neighbor, visited);
         }
@@ -26,8 +28,8 @@ int countConnectedComponents(Graph<int, int>& graph, size_t numVertices) {
     int components = 0;
 
     for (size_t i = 0; i < numVertices; i++) {
-        if (i > visited.size()) {
-            throw std::invalid_argument("i");
+        if (i >= visited.size()) {
+            throw std::invalid_argument("i out of bounds");
         }
         if (visited[i] == 0) {
             components++;
@@ -47,17 +49,20 @@ int main(int argc, char **argv) {
     }
 
     int numVertices, numEdges;
-    if (fscanf(input, "%d %d", &numVertices, &numEdges) < 1) {
-        throw std::invalid_argument("Invalid argument");
+    if (fscanf(input, "%d %d", &numVertices, &numEdges) < 2) {
+        throw std::invalid_argument("Invalid input format");
     }
 
     Graph<int, int> graph(numVertices);
 
     for (int i = 0; i < numEdges; i++) {
         int from, to;
-        if (fscanf(input, "%d %d", &from, &to) < 1) {
-            throw std::invalid_argument("Invalid argument");
-        }   
+        if (fscanf(input, "%d %d", &from, &to) < 2) {
+            throw std::invalid_argument("Invalid edge input");
+        }
+        if (from < 0 || to < 0 || from >= numVertices || to >= numVertices) {
+            throw std::out_of_range("Edge indices out of bounds");
+        }
         graph.add_edge(from, to, 1);
     }
 
