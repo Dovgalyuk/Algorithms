97e01028f82cb6030e98e5c3422228ca0e552aed
Василина Юганова <iuganova.vasilina@yandex.ru>
2024-12-16 02:20:49 +0300

    Lab3

---
+++ out/build/x64-Debug/Lab3C/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/User/source/repos/Algorithms/Lab3C
+# Build directory: C:/Users/User/source/repos/Algorithms/out/build/x64-Debug/Lab3C
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestLab3C "C:/Users/User/source/repos/Algorithms/out/build/x64-Debug/Lab3C/Lab3C.exe" "C:/Users/User/source/repos/Algorithms/Lab3C/input.txt")
+set_tests_properties(TestLab3C PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/source/repos/Algorithms/Lab3C/CMakeLists.txt;6;add_test;C:/Users/User/source/repos/Algorithms/Lab3C/CMakeLists.txt;0;")
