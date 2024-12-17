86318bfba2b6741e4a2ee7509b64d9aefd90c949
Василина Юганова <iuganova.vasilina@yandex.ru>
2024-12-18 02:15:32 +0300

    Lab3CPP

--- out/build/x64-Debug/LibraryCPPTemplate/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/source/repos/Algorithms/LibraryCPPTemplate
-# Build directory: C:/Users/User/source/repos/Algorithms/out/build/x64-Debug/LibraryCPPTemplate
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
