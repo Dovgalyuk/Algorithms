0f52f11ddf90800edbeef73f430ff94f3c02d5a6
Василина Юганова <iuganova.vasilina@yandex.ru>
2024-12-19 22:50:51 +0300

    Lab3CPP

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,18 +1,21 @@
 #include "queue.h"
 #include "vector.h"
+#include <iostream>
 struct Queue
 {
     Vector* vector;
     size_t front;
     size_t back;
+    size_t size;
 };
 
 Queue *queue_create()
 {
-    Queue *queue = new Queue;
+    Queue* queue = new Queue;
     queue->vector = vector_create();
     queue->front = 0;
     queue->back = 0;
+    queue->size = 0;
     return queue;
 }
 
@@ -24,28 +27,31 @@ void queue_delete(Queue *queue)
 
 void queue_insert(Queue *queue, Data data)
 {
-    if (queue->back == vector_size(queue->vector)) {
-        vector_resize(queue->vector, queue->back + 1);
+    if (queue->size == vector_size(queue->vector)) {
+        vector_resize(queue->vector, queue->size == 0 ? 4 : queue->size * 2);
     }
-    vector_set(queue->vector, queue->back++, data);
+    vector_set(queue->vector, queue->back, data);
+    queue->back = (queue->back + 1) % vector_size(queue->vector);
+    queue->size++;
 }
 
 Data queue_get(const Queue* queue) {
+    if (queue_empty(queue)) {
+        throw std::out_of_range("Queue is empty");
+    }
     return vector_get(queue->vector, queue->front);
 }
 
 void queue_remove(Queue *queue)
 {
-    if (!queue_empty(queue)) {
-        queue->front++;
-        if (queue->front == queue->back) {
-            queue->front = 0;
-            queue->back = 0;
-        }
+    if (queue_empty(queue)) {
+        throw std::out_of_range("Queue is empty");
     }
+    queue->front = (queue->front + 1) % vector_size(queue->vector);
+    queue->size--;
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return queue->front == queue->back;
+    return queue->size == 0;
 }
