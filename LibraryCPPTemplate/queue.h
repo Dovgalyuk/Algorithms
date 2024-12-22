79acf300d2da8179d9054499cfa10101cef4d91a
xlurr <aborigen.nm@gmail.com>
2024-12-23 00:37:59 +0300

    data fix

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -2,117 +2,111 @@
 #define QUEUE_TEMPLATE_H
 
 #include "vector.h"
-#include <type_traits>
+#include <stdexcept>
 
-template <typename Data> class Queue
-{
+template <typename Data>
+class Queue {
 public:
     // Create empty queue
     Queue();
 
-    // copy constructor
-    Queue(const Queue &a);
+    // Copy constructor
+    Queue(const Queue& a);
 
-    // assignment operator
-    Queue &operator=(const Queue &a);
+    // Assignment operator
+    Queue& operator=(const Queue& a);
 
     // Deletes queue
     ~Queue();
 
     // Includes new element into the queue
-    // Should be O(1) on average
     void insert(Data value);
 
     // Retrieves first element from the queue
     Data& get() const;
 
     // Removes first element from the queue
-    // Should be O(1) on average
     void remove();
 
     // Returns true if the queue is empty
     bool empty() const;
 
 private:
-    // private data should be here
-    Vector<Data> data;
+    Vector<Data>* data; // Pointer to the data
     size_t frontIndex;
     size_t queueSize;
 };
 
 // Constructor
 template <typename Data>
-Queue<Data>::Queue() : data(), frontIndex(0), queueSize(0) {}
+Queue<Data>::Queue() : data(new Vector<Data>()), frontIndex(0), queueSize(0) {}
 
 // Copy constructor
 template <typename Data>
-Queue<Data>::Queue(const Queue<Data>& other): data(other.data), frontIndex(other.frontIndex), queueSize(other.queueSize) {}
+Queue<Data>::Queue(const Queue<Data>& other)
+    : data(new Vector<Data>(*other.data)),
+      frontIndex(other.frontIndex),
+      queueSize(other.queueSize) {}
 
 // Assignment operator
 template <typename Data>
 Queue<Data>& Queue<Data>::operator=(const Queue<Data>& other) {
-    if (this == &other) {
-        return *this;
+    if (this != &other) {
+        delete data;
+
+        data = new Vector<Data>(*other.data);
+        frontIndex = other.frontIndex;
+        queueSize = other.queueSize;
     }
-
-    data = other.data; 
-    frontIndex = other.frontIndex;
-    queueSize = other.queueSize;
-
     return *this;
 }
 
+// Destructor
 template <typename Data>
 Queue<Data>::~Queue() {
-    // destruct Vector
+    delete data;
 }
 
+// Insert a new element
 template <typename Data>
 void Queue<Data>::insert(Data value) {
-    if (queueSize == data.size()) {
+    if (queueSize == data->size()) {
         size_t new_size = (queueSize == 0) ? 1 : queueSize * 2;
-        data.resize(new_size);
+        Vector<Data>* newData = new Vector<Data>();
+        newData->resize(new_size);
 
-        size_t count = 0; 
-        size_t current = frontIndex;
-        Data temp = data.get(current);
-
-        while (count < queueSize) {
-            size_t next = (current + queueSize - frontIndex) % queueSize;
-            Data swapping =  data.get(next);
-            data.set(next, temp);
-            temp = swapping;
-            current = next; 
-            ++count;
+        for (size_t i = 0; i < queueSize; ++i) {
+            newData->set(i, data->get((frontIndex + i) % data->size()));
         }
 
+        delete data;
+        data = newData;
         frontIndex = 0;
     }
-    data.set((frontIndex + queueSize) % data.size(), value);
+    data->set((frontIndex + queueSize) % data->size(), value);
     queueSize++;
 }
 
-
+// Get the front element
 template <typename Data>
 Data& Queue<Data>::get() const {
     if (empty()) {
         throw std::out_of_range("Queue is empty");
     }
-    return data.get(frontIndex);
+    return data->get(frontIndex);
 }
 
-// Dequeue: Remove element from the front
+// Remove the front element
 template <typename Data>
 void Queue<Data>::remove() {
     if (empty()) {
         throw std::out_of_range("Queue is empty");
     }
-
-    
-    frontIndex = (frontIndex + 1) % data.size();
+    frontIndex = (frontIndex + 1) % data->size();
     queueSize--;
 }
 
+// Check if the queue is empty
 template <typename Data>
 bool Queue<Data>::empty() const {
     return queueSize == 0;
