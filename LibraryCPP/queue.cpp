08ce3feebf36044476dd902264cc8fb1e53d7174
AlenaMaks <deia1704092@mail.ru>
2024-12-24 06:23:06 +0300

    dream

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,34 +1,65 @@
-#include "queue.h"
+#include "../LibraryCPP/queue.h"
+#include "vector.h"
 
 struct Queue
 {
+    Vector* vector;
+    size_t front;
+    size_t rear;
+    size_t size;
 };
 
 Queue *queue_create()
 {
-    return new Queue;
+    Queue* queue = new Queue;
+    queue->vector = vector_create();
+    queue->front = 0;
+    queue->rear = 0;
+    queue->size = 0;
+    return queue;
 }
 
 void queue_delete(Queue *queue)
 {
     // TODO: free queue items
+    vector_delete(queue->vector);
     delete queue;
 }
 
 void queue_insert(Queue *queue, Data data)
 {
+    if (queue->size == vector_size(queue->vector)) {
+        size_t new_size = vector_size(queue->vector) * 2 + 1;
+        Vector* new_vector = vector_create();
+        vector_resize(new_vector, new_size);
+        
+        for (size_t i = 0; i < queue->size; i++) {
+            vector_set(new_vector, i, vector_get(queue->vector, (queue->front + i) % vector_size(queue->vector)));
+        }
+
+        vector_delete(queue->vector);
+        queue->vector = new_vector;
+        queue->front = 0;
+        queue->rear = queue->size;
+    }
+    vector_set(queue->vector, queue->rear, data);
+    queue->rear = (queue->rear + 1) % vector_size(queue->vector);
+    queue->size++;
 }
 
 Data queue_get(const Queue *queue)
 {
-    return (Data)0;
+    return vector_get(queue->vector, queue->front);
 }
 
 void queue_remove(Queue *queue)
 {
+    queue->front = (queue->front + 1) % vector_size(queue->vector);
+    queue->size - 1;
 }
 
 bool queue_empty(const Queue *queue)
 {
-    return true;
+    if (queue->size == 0) return true;
+    else return false;
 }
--- lab_three/lab.cpp
+++ lab_three/lab.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-main(int argc, char* argv[]) {
+void search(int** graph, int num_tops, const string &filename, int &start, int &end);
+
+int main(int argc, char* argv[]) {
     if (argc != 3) {
         cerr << "Usage: " << argv[0] << " <file_path>" << endl;
         return 1;
     }
-
     ifstream input(argv[1]);
 
     if (!input) {
@@ -18,5 +19,54 @@ main(int argc, char* argv[]) {
         return 1;
     }
 
+    int num_tops, start, end;
+    input >> num_tops >> start >> end;
+
+    int** graph = new int*[num_tops];
+
+    for (int i = 0; i < num_tops; i++) {
+        graph[i] = new int[num_tops];
+        for (int j = 0; j < num_tops; j++) input >> graph[i][j];
+    }
+
+    search(graph, num_tops, argv[2], start, end);
+
+    for (int i = 0; i < num_tops; i++) delete[] graph[i];
+    delete[] graph;
+
+    input.close();
     
+    return 0;
+}
+
+void search(int** graph, int num_tops, const string &filename, int &start, int &end) {
+
+    int* distances = new int[num_tops];
+    for (int i = 0; i < num_tops; i++) distances[i] = -1; //0 -1 -1 -1
+    distances[start-1] = 0;
+
+    Queue* queue = queue_create();
+    queue_insert(queue, start-1);
+
+    while (!queue_empty(queue)) {
+        int now = queue_get(queue);
+        queue_remove(queue);
+
+        if (now == (end - 1)) break;
+
+        for (int i = 0; i < num_tops; i++) {
+            if (graph[now][i] == 1 && distances[i] == -1) {
+                distances[i] = distances[now] + 1;
+                queue_insert(queue, i);
+            }
+            else queue_insert(queue, i);
+        }
+    }
+
+    ofstream output(filename);
+    if (distances[end-1] != -1) output << distances[end-1];
+    else output << "IMPOSSIBLE";
+    output.close();
+
+    delete[] distances;
 }
