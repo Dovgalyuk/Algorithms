04d6d220c6e4b1c35828c00b35bd19dc249bf34c
Артемий Алексеев <alekseev3artem@gmail.com>
2025-01-07 18:05:36 +0300

    lab3 v16

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -38,39 +38,31 @@ size_t vector_size(const Vector *vector) {
 void vector_resize(Vector *vector, size_t size) {
     // Если новый размер равен 0, освобождаем память и обнуляем вектор
     if (size == 0) {
-        delete[] vector->data; // Освобождаем память
-        vector->data = nullptr; // Устанавливаем указатель на nullptr
-        vector->size = 0;       // Устанавливаем размер вектора в 0
-        vector->capacity = 0;   // Устанавливаем емкость вектора в 0
+        delete[] vector->data;
+        vector->data = nullptr;
+        vector->size = 0;       
+        vector->capacity = 0;  
         return;
     }
 
-    // Увеличиваем емкость, если новый размер больше текущей
     if (size > vector->capacity) {
         size_t new_capacity = vector->capacity;
 
-        // Увеличиваем емкость до ближайшей степени двойки
         while (new_capacity < size) {
             new_capacity = (new_capacity == 0) ? 1 : new_capacity * 2;
         }
 
-        // Выделяем новую память
         Data *new_data = new Data[new_capacity];
-
-        // Копируем старые данные в новый массив
         for (size_t i = 0; i < vector->size; ++i) {
-            new_data[i] = vector->data[i]; // Копируем существующие данные
+            new_data[i] = vector->data[i];
         }
 
-        // Освобождаем старую память
         delete[] vector->data;
 
-        // Обновляем указатели и размеры
         vector->data = new_data;
         vector->capacity = new_capacity;
     }
 
-    // Устанавливаем новый размер
     vector->size = size;
     for (size_t i = vector->size; i < size; ++i) {
         vector->data[i] = Data();
