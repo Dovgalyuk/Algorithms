14b7fb9f93d8c8562ab4267456c29b3359cbbd6c
Alakazjab <98467762+Alakazjab@users.noreply.github.com>
2025-02-14 16:58:02 +0300

    Update lab1.cpp

--- Lab1Templates/lab1.cpp
+++ Lab1Templates/lab1.cpp
@@ -1,1 +1,13 @@
+#include "array.h"
+
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char* argv[])
+{
+    size_t size = 10;
+    Array<int>* array = new Array<int>(size);
+    std::cout << array->size();
+    return 0;
+}
 
