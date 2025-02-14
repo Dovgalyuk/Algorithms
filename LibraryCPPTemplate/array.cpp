b63b0eed8aecae4a7f27d1b9b251de97473e97b8
Alakazjab <98467762+Alakazjab@users.noreply.github.com>
2025-02-14 16:56:48 +0300

    Update array.cpp

--- LibraryCPPTemplate/array.cpp
+++ LibraryCPPTemplate/array.cpp
@@ -1,12 +1,1 @@
 #include "array.h"
-
-#include <iostream>
-#include <ostream>
-
-int main(int argc, char* argv[])
-{
-    size_t size = 10;
-    Array<int>* array = new Array<int>(size);
-    std::cout << array->size();
-    return 0;
-}
