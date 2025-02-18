56bb41fc98917ba2022997d3f6027eefd8acabb4
YuzkovRepository <118473182+programeblan00@users.noreply.github.com>
2025-02-18 08:59:01 +0300

    create array

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,35 +1,32 @@
 #include <stdlib.h>
 #include "array.h"
 
-typedef struct Array
-{
+typedef struct Array {
+    size_t size;
+    Data *data;
 } Array;
 
 // create array
-Array *array_create(size_t size, FFree f)
-{
-    return malloc(sizeof(Array));
+Array *array_create(size_t size, FFree f) {
+    Array *arr = (Array *)malloc(sizeof(Array));
+    arr->size = size;
+    arr->data = (Data *)malloc(size * sizeof(Data));  // Выделяем память под элементы
+    return arr;
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
-{
-    free(arr);
+void array_delete(Array *arr) {
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array *arr, size_t index) {
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array *arr, size_t index, Data value) {
+
 }
 
 // returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array *arr) {
 }
