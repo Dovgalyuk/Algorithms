2af990a714b77853f0a0ce77adc4a91883a8d98f
YuzkovRepository <118473182+programeblan00@users.noreply.github.com>
2025-02-23 18:59:50 +0300

    error correction

--- LibraryC/Tests/array.cpp
+++ LibraryC/Tests/array.cpp
@@ -3,31 +3,37 @@
 
 int main()
 {
-    Array *arr = array_create(10, [](void *p) { delete (int*)p; });
+    Array* arr = array_create(10, [](void* p) { delete (int*)p; });
 
     if (array_size(arr) != 10)
     {
-        std::cout << "Invalid array size\n";
+        std::cout << "Неверный размер массива\n";
         array_delete(arr);
         return 1;
     }
 
-    for (int i = 0 ; i < 10 ; ++i)
+    for (int i = 0; i < 10; ++i)
     {
-        int *d = new int;
+        int* d = new int;
         *d = i * 2;
         array_set(arr, i, (Data)d);
     }
 
-    for (int i = 0 ; i < 10 ; ++i)
+    for (int i = 0; i < 10; ++i)
     {
         if (*(int*)array_get(arr, i) != i * 2)
         {
-            std::cout << "Invalid array element " << i << "\n";
+            std::cout << "Неверный элемент массива " << i << "\n";
             array_delete(arr);
             return 1;
         }
     }
 
+    for (int i = 0; i < 10; ++i)
+    {
+        delete (int*)array_get(arr, i);
+    }
+
     array_delete(arr);
+    return 0;
 }
