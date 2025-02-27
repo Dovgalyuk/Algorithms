fe45fd8eabcf66dfde7ff04a717fd524c60f38fc
YuzkovRepository <118473182+programeblan00@users.noreply.github.com>
2025-02-27 08:34:36 +0300

    deleted files

--- out/build/x64-Debug/LibraryC/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/serez/source/repos/Algorithms/LibraryC/Tests
-# Build directory: C:/Users/serez/source/repos/Algorithms/out/build/x64-Debug/LibraryC/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayC "TestArrayC")
-set_tests_properties(TestArrayC PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/serez/source/repos/Algorithms/LibraryC/Tests/CMakeLists.txt;4;add_test;C:/Users/serez/source/repos/Algorithms/LibraryC/Tests/CMakeLists.txt;0;")
