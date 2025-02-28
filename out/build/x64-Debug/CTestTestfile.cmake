79afb916d3607f9cdfb9f456118e8eadddf37bbd
Conral11 <anastasianovsu.ru@mail.ru>
2025-02-28 15:08:35 +0300

    main

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vladislav-b/Desktop/Algorithms
-# Build directory: C:/Users/Anastasia/Desktop/Algorithms/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPP")
-subdirs("Lab1CPP")
