feef7e56953a65b52e28e2e771e6dfb2b78a88b8
Muzafarov06 <muzafarovfazliddin468@gmail.com>
2025-03-03 22:37:58 +0300

    Added fixes to Lab1CPP

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,50 +1,48 @@
 #include "array.h"
+#include <stdexcept> 
 
-struct Array
-{
-    Data* data;
-    size_t size;
+struct Array {
+    size_t size;  
+    Data* data;   
 
-    Array(size_t size)
-    {
+   
+    Array(size_t size) {
         this->size = size;
-        this->data = new Data[size];
-        for (size_t i = 0; i < size; ++i)
-        {
-            this->data[i] = Data();
-        }
+        this->data = new Data[size]; 
+    }
+
+    
+    ~Array() {
+        delete[] data;
     }
 };
 
-// create array
-Array* array_create(size_t size)
-{
-    Array* arr = new Array(size);
-    return arr;
+
+Array* array_create(size_t size) {
+    return new Array(size);
 }
 
-// delete array, free memory
-void array_delete(Array* arr)
-{
-    delete[] arr->data;
+
+void array_delete(Array* arr) {
     delete arr;
 }
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    if (index >= arr->size) return 0;
+
+Data array_get(const Array* arr, size_t index) {
+    if (index >= arr->size) {
+        throw std::out_of_range("Index out of range in array_get()");
+    }
     return arr->data[index];
 }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
-    if (index < arr->size) arr->data[index] = value;
+
+void array_set(Array* arr, size_t index, Data value) {
+    if (index >= arr->size) {
+        throw std::out_of_range("Index out of range in array_set()");
+    }
+    arr->data[index] = value;
 }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
+size_t array_size(const Array* arr) {
     return arr->size;
 }
