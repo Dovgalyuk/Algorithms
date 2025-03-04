54c61b8202e4fc0dfb0254b814ecbd98a3710db7
DarjaGus <dar4wei@yandex.ru>
2025-03-04 20:53:52 +0300

    Update array.h

--- LibraryCPP/array.h
+++ LibraryCPP/array.h
@@ -1,27 +1,28 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include <cstddef>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
 
-// Non-resizeable array
-// Stores integer values inside
-typedef int Data;
+template <typename T>
+class Array{
+private:
+    std::vector<T> data;
+    size_t size_;
+public:
+    Array(size_t size);
+    Array(const FixedArray& other);
+    Array& operator=(const Array& other);
+    Array(Array&& other) noexcept;
+    Array& operator=(Array&& other) noexcept;
 
-struct Array;
-
-// create array
-Array *array_create(size_t size);
-
-// delete array, free memory
-void array_delete(Array *arr);
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index);
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value);
-
-// returns array size
-size_t array_size(const Array *arr);
+    size_t size() const;
+    T& at(size_t index);
+    const T& at(size_t index) const;
+    T& operator[](size_t index);
+    const T& operator[](size_t index) const;
+    void fill(const T& value);
+};
 
 #endif
