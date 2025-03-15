c0ddfbc1607e90d8503796ad14eb583f508db711
Muzafarov06 <muzafarovfazliddin468@gmail.com>
2025-03-15 16:30:10 +0300

    Added fixes to Lab2CPP

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,40 +1,41 @@
 #include "stack.h"
-#include <stdexcept>
+#include "vector.h"
 
-struct Stack {
-    Vector* vector;  
+struct Stack
+{
+    Vector* vector;
 };
 
-Stack* stack_create() {
+Stack* stack_create()
+{
     Stack* stack = new Stack;
     stack->vector = vector_create();
     return stack;
 }
 
-void stack_delete(Stack* stack) {
-    vector_delete(stack->vector);  
-    delete stack;                  
+void stack_delete(Stack* stack)
+{
+    vector_delete(stack->vector);
+    delete stack;
 }
 
-void stack_push(Stack* stack, Data data) {
-    vector_resize(stack->vector, vector_size(stack->vector) + 1);  // Увеличиваем размер вектора
-    vector_set(stack->vector, vector_size(stack->vector) - 1, data);  // Добавляем элемент
+void stack_push(Stack* stack, Data data)
+{
+    vector_resize(stack->vector, vector_size(stack->vector) + 1);
+    vector_set(stack->vector, vector_size(stack->vector) - 1, data);
 }
 
-Data stack_get(const Stack* stack) {
-    if (stack_empty(stack)) {
-        throw std::underflow_error("Stack is empty");
-    }
-    return vector_get(stack->vector, vector_size(stack->vector) - 1);  // Возвращаем последний элемент
+Data stack_get(const Stack* stack)
+{
+    return vector_get(stack->vector, vector_size(stack->vector) - 1);
 }
 
-void stack_pop(Stack* stack) {
-    if (stack_empty(stack)) {
-        throw std::underflow_error("Stack is empty");
-    }
-    vector_resize(stack->vector, vector_size(stack->vector) - 1);  // Уменьшаем размер вектора
+void stack_pop(Stack* stack)
+{
+    vector_resize(stack->vector, vector_size(stack->vector) - 1);
 }
 
-bool stack_empty(const Stack* stack) {
+bool stack_empty(const Stack* stack)
+{
     return vector_size(stack->vector) == 0;
 }
