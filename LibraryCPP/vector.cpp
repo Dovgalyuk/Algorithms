d9ea8daa1c0bb61978624da3bf236f1dcacad11f
Conral11 <anastasianovsu.ru@mail.ru>
2025-03-22 23:16:21 +0300

    main2_2

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,16 +1,13 @@
 #include "vector.h"
 #include <stdexcept>
 
-struct Vector
-{
+struct Vector {
     Data *data;
     size_t size;
     size_t capacity;
-    
 };
 
-Vector *vector_create()
-{
+Vector *vector_create() {
     Vector *vector = new Vector;
     vector->data = nullptr;
     vector->size = 0;
@@ -18,41 +15,33 @@ Vector *vector_create()
     return vector;
 }
 
-void vector_delete(Vector *vector)
-{
+void vector_delete(Vector *vector) {
     delete[] vector->data;
-    delete vector; 
+    delete vector;
 }
 
-Data vector_get(const Vector *vector, size_t index)
-{
+Data vector_get(const Vector *vector, size_t index) {
     if (index >= vector->size) {
         throw std::out_of_range("Index out of range");
     }
-    return (Data)0;
+    return vector->data[index]; 
 }
 
-void vector_set(Vector *vector, size_t index, Data value)
-{
-    if (index >= vector->capacity) {
-        vector_resize(vector, index + 1);
-    }
-    if (index >= vector->size) {
-        vector->size = index + 1;
+void vector_set(Vector *vector, size_t index, Data value) {
+    if (index >= vector->size) { 
+        vector_resize(vector, index + 1);  
     }
     vector->data[index] = value;
 }
 
-size_t vector_size(const Vector *vector)
-{
+size_t vector_size(const Vector *vector) {
     return vector->size;
 }
 
-void vector_resize(Vector *vector, size_t size)
-{
-    if (size > vector->capacity) {
-        size_t new_capacity = (size > vector->capacity * 2) ? size : vector->capacity * 2;
-        Data *new_data = new Data[new_capacity];
+void vector_resize(Vector *vector, size_t new_size) {
+    if (new_size > vector->capacity) {
+        size_t new_capacity = (new_size > vector->capacity * 2) ? new_size : vector->capacity * 2;
+        Data* new_data = new Data[new_capacity];
         for (size_t i = 0; i < vector->size; ++i) {
             new_data[i] = vector->data[i];
         }
@@ -60,4 +49,6 @@ void vector_resize(Vector *vector, size_t size)
         vector->data = new_data;
         vector->capacity = new_capacity;
     }
+
+    vector->size = new_size; 
 }
