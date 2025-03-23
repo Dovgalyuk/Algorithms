5570e17cf72aa94515304dbddf83add596d12ee4
darGus <darGus@DESKTOP-BGT97TI>
2025-03-23 22:42:54 +0300

    *

--- Lab1CPP/main.cpp
+++ Lab1CPP/main.cpp
@@ -27,6 +27,7 @@ Data findMaxEvenIndex(const Array* arr) {
 
     // Проходим по элементам массива с четными индексами
     for (size_t i = 0; i < size; i += 2) {
+        std::cerr << "findMaxEvenIndex: i = " << i << std::endl;
         Data val = array_get(arr, i);
         if (val > maxVal) {
             maxVal = val;
