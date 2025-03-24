077004bd6c74ee41fd2da302343c12e7e489a2a9
u3pauLb <vadim12020novikor5550@gmail.com>
2025-03-24 23:54:49 +0300

    Lab2

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -1,14 +1,12 @@
 #include "list.h"
 #include <cstddef>
 
-// Создание нового списка
 List* list_create() {
     List* list = new List;
     list->head = nullptr;
     return list;
 }
 
-// Удаление списка и освобождение памяти
 void list_delete(List* list) {
     while (list->head != nullptr) {
         ListItem* temp = list->head;
@@ -18,22 +16,18 @@ void list_delete(List* list) {
     delete list;
 }
 
-// Получение первого элемента списка
 ListItem* list_first(List* list) {
     return list->head;
 }
 
-// Получение данных из элемента списка
 Data list_item_data(const ListItem* item) {
     return item->data;
 }
 
-// Получение следующего элемента списка
 ListItem* list_item_next(ListItem* item) {
     return item->next;
 }
 
-// Вставка элемента в начало списка
 ListItem* list_insert(List* list, Data data) {
     ListItem* newItem = new ListItem;
     newItem->data = data;
@@ -42,10 +36,9 @@ ListItem* list_insert(List* list, Data data) {
     return newItem;
 }
 
-// Вставка элемента после указанного элемента
-ListItem* list_insert_after(List* /*list*/, ListItem* item, Data data) {
-    if (item == nullptr) {
-        return nullptr; 
+ListItem* list_insert_after(List* list, ListItem* item, Data data) {
+    if (!list || !item) {
+        return nullptr;
     }
     ListItem* newItem = new ListItem;
     newItem->data = data;
@@ -54,10 +47,9 @@ ListItem* list_insert_after(List* /*list*/, ListItem* item, Data data) {
     return newItem;
 }
 
-// Удаление первого элемента списка
 ListItem* list_erase_first(List* list) {
     if (list->head == nullptr) {
-        return nullptr; 
+        return nullptr;
     }
     ListItem* temp = list->head;
     list->head = list->head->next;
@@ -65,10 +57,9 @@ ListItem* list_erase_first(List* list) {
     return list->head;
 }
 
-// Удаление элемента, следующего за указанным
 ListItem* list_erase_next(ListItem* item) {
     if (item == nullptr || item->next == nullptr) {
-        return nullptr; 
+        return nullptr;
     }
     ListItem* temp = item->next;
     item->next = item->next->next;
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,38 +1,31 @@
 #include "stack.h"
 
-// Создание пустого стека
+
 Stack* stack_create() {
     Stack* stack = new Stack;
     stack->list = list_create();
     return stack;
 }
 
-// Удаление стека
 void stack_delete(Stack* stack) {
     list_delete(stack->list);
     delete stack;
 }
 
-// Добавление элемента в стек
 void stack_push(Stack* stack, Data data) {
+    
     list_insert(stack->list, data);
 }
 
-// Получение верхнего элемента стека
 Data stack_get(const Stack* stack) {
     ListItem* first = list_first(stack->list);
-    if (first == nullptr) {
-        return 0; 
-    }
-    return list_item_data(first);
+    return first ? list_item_data(first) : "";
 }
 
-// Удаление верхнего элемента стека
 void stack_pop(Stack* stack) {
     list_erase_first(stack->list);
 }
 
-// Проверка, пуст ли стек
 bool stack_empty(const Stack* stack) {
     return list_first(stack->list) == nullptr;
 }
