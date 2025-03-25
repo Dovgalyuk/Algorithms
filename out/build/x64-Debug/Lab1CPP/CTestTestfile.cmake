2416fdf73c68356adbb251dcddf92b0ced4e2838
darGus <darGus@DESKTOP-BGT97TI>
2025-03-25 13:51:28 +0300

    *

--- out/build/x64-Debug/Lab1CPP/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dovgaluk/Lab1CPP
-# Build directory: C:/Dovgaluk/out/build/x64-Debug/Lab1CPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1CPP "C:/Dovgaluk/out/build/x64-Debug/Lab1CPP/Lab1CPP.exe" "C:/Dovgaluk/Lab1CPP/input.txt")
-set_tests_properties(TestLab1CPP PROPERTIES  _BACKTRACE_TRIPLES "C:/Dovgaluk/Lab1CPP/CMakeLists.txt;5;add_test;C:/Dovgaluk/Lab1CPP/CMakeLists.txt;0;")
