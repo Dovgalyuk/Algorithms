14b1f02ba491e0d3785ce9c60cbfe9d58fabb293
YuzkovRepository <118473182+programeblan00@users.noreply.github.com>
2025-04-03 18:44:41 +0300

    test

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -137,10 +137,12 @@ int main(int argc, char** argv) {
     }
 
     try {
-        solve_puzzle(start);
-    } catch (const std::exception& e) {
-        cerr << "Error: " << e.what() << endl;
-    }
+    	Queue* q = queue_create();
+    	unique_ptr<Queue, decltype(&queue_delete)> queue_guard(q, queue_delete);
+    	solve_puzzle(start);
+	} catch (const std::exception& e) {
+    	cerr << "Error: " << e.what() << endl;
+	}
 
     return 0;
 }
