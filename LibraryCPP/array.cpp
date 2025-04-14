7bfc11c6d02529578ef1995a95a7eac42e18779b
Muzafarov06 <muzafarovfazliddin468@gmail.com>
2025-04-14 22:59:12 +0300

    Added fixes to Lab3CPP

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,48 +1,50 @@
 #include "array.h"
-#include <stdexcept> 
 
-struct Array {
-    size_t size;  
-    Data* data;   
+struct Array
+{
+    Data* data;
+    size_t size;
 
-   
-    Array(size_t size) {
+    Array(size_t size)
+    {
         this->size = size;
-        this->data = new Data[size]; 
-    }
-
-    
-    ~Array() {
-        delete[] data;
+        this->data = new Data[size];
+        for (size_t i = 0; i < size; ++i)
+        {
+            this->data[i] = Data();
+        }
     }
 };
 
-
-Array* array_create(size_t size) {
-    return new Array(size);
+// create array
+Array* array_create(size_t size)
+{
+    Array* arr = new Array(size);
+    return arr;
 }
 
-
-void array_delete(Array* arr) {
+// delete array, free memory
+void array_delete(Array* arr)
+{
+    delete[] arr->data;
     delete arr;
 }
 
-
-Data array_get(const Array* arr, size_t index) {
-    if (index >= arr->size) {
-        throw std::out_of_range("Index out of range in array_get()");
-    }
+// returns specified array element
+Data array_get(const Array *arr, size_t index)
+{
+    if (index >= arr->size) return 0;
     return arr->data[index];
 }
 
-
-void array_set(Array* arr, size_t index, Data value) {
-    if (index >= arr->size) {
-        throw std::out_of_range("Index out of range in array_set()");
-    }
-    arr->data[index] = value;
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value)
+{
+    if (index < arr->size) arr->data[index] = value;
 }
 
-size_t array_size(const Array* arr) {
+// returns array size
+size_t array_size(const Array *arr)
+{
     return arr->size;
 }
--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -3,46 +3,41 @@
 
 struct Stack
 {
-    Vector* vector;
+    Vector *vector;
 };
 
-Stack* stack_create()
+Stack *stack_create()
 {
-    Stack* stack = new Stack;
+    Stack *stack = new Stack;
     stack->vector = vector_create();
     return stack;
 }
 
-void stack_delete(Stack* stack)
+void stack_delete(Stack *stack)
 {
     vector_delete(stack->vector);
     delete stack;
 }
 
-void stack_push(Stack* stack, Data data)
+void stack_push(Stack *stack, Data data)
 {
-    vector_resize(stack->vector, vector_size(stack->vector) + 1);
-    vector_set(stack->vector, vector_size(stack->vector) - 1, data);
+    vector_set(stack->vector, vector_size(stack->vector), data);
 }
 
-Data stack_get(const Stack* stack)
+Data stack_get(const Stack *stack)
 {
     return vector_get(stack->vector, vector_size(stack->vector) - 1);
 }
 
-void stack_pop(Stack* stack)
+void stack_pop(Stack *stack)
 {
-    if (!stack_empty(stack)) {
-        vector_resize(stack->vector, vector_size(stack->vector) - 1);
+    if (vector_size(stack->vector) > 0) {
+        size_t new_size = vector_size(stack->vector) - 1;
+        vector_resize(stack->vector, new_size);
     }
 }
 
-bool stack_empty(const Stack* stack)
+bool stack_empty(const Stack *stack)
 {
     return vector_size(stack->vector) == 0;
-}
-
-size_t stack_size(const Stack* stack)
-{
-    return vector_size(stack->vector);
 }
