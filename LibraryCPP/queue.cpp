49856d262967bd277131381858070eeb3e506977
Zooooof <105545024+Zooooof@users.noreply.github.com>
2025-04-14 22:38:17 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -28,40 +28,33 @@ void queue_delete(Queue *queue)
     }
 }
 
-void queue_insert(Queue *queue, Data data)
-{
-    size_t capacity = vector_size(queue->vector);
-    if (queue->size == capacity) {
-        size_t new_size = capacity == 0 ? 1 : capacity * 2;
-        if (queue->head != 0) {
-            
-            Vector* new_vector = vector_create();
-            vector_resize(new_vector, new_size);
+void queue_insert(Queue *queue, Data data) {
+    if (!queue) {
+        cerr << "Error: Queue is null\n";
+        exit(EXIT_FAILURE); 
+    }
 
-            for (size_t i = 0; i < queue->size; ++i) {
-                Data elem = vector_get(queue->vector, (queue->head + i) % capacity);
-                vector_set(new_vector, i, elem);
-            }
-            
-            vector_set(new_vector, queue->size, data);
-            vector_delete(queue->vector); 
-            queue->vector = new_vector; 
-            queue->head = 0;
-            queue->size++; 
+    size_t capacity = vector_size(queue->vector);
+
+    if (queue->size == capacity) {
+        size_t new_capacity = (capacity == 0) ? 1 : capacity * 2; 
+
+        Vector* new_vector = vector_create();
+        vector_resize(new_vector, new_capacity);
+
+        for (size_t i = 0; i < queue->size; ++i) {
+            Data elem = vector_get(queue->vector, (queue->head + i) % capacity);
+            vector_set(new_vector, i, elem);
         }
-        else
-        {
-            vector_resize(queue->vector, new_size);
-        }
+
+        vector_delete(queue->vector);
+        queue->vector = new_vector;
+        queue->head = 0;
         capacity = vector_size(queue->vector);
     }
-    if (capacity==0){
-        vector_resize(queue->vector, 1);
-        capacity = vector_size(queue->vector);
-    }
+
     size_t insert_index = (queue->head + queue->size) % capacity;
     vector_set(queue->vector, insert_index, data);
-
     queue->size++;
 }
 
