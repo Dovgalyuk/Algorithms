705ea1b0655b4bb123814c0b68d388eedd9011e5
Conral11 <anastasianovsu.ru@mail.ru>
2025-05-01 00:21:16 +0300

    Практика 4

--- Lab4CPP/main.cpp
+++ Lab4CPP/main.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
-#include <fstream>
 #include <climits>
+#include <windows.h> // Include Windows API for console settings
 #include "graph.h"
+#include <cassert>
+
+const int INF = INT_MAX;
 
 template<typename Data>
-void modifiedFloydWarshall(Graph<Data>& graph, size_t vertex_amount) {
+void modifiedFloydWarshall(const Graph<Data>& graph, size_t vertex_amount) {
     Graph<Data> mutableGraph = graph;
 
     for (size_t k = 0; k < vertex_amount; ++k) {
         for (size_t i = 0; i < vertex_amount; ++i) {
             for (size_t j = 0; j < vertex_amount; ++j) {
-                if (mutableGraph.isEdgeExist(i, k) && mutableGraph.isEdgeExist(k, j)) {
-                    Data weightIK = mutableGraph.getEdgeData(i, k);
-                    Data weightKJ = mutableGraph.getEdgeData(k, j);
+                typename Graph<Data>::Edge* edgeIK = mutableGraph.getEdge(i, k);
+                typename Graph<Data>::Edge* edgeKJ = mutableGraph.getEdge(k, j);
 
-                    if (!mutableGraph.isEdgeExist(i, j) || mutableGraph.getEdgeData(i, j) < weightIK + weightKJ) {
-                        mutableGraph.addEdge(i, j, weightIK + weightKJ);
+                if (edgeIK && edgeKJ) {
+                    int weightIK = edgeIK->getEdgeData();
+                    int weightKJ = edgeKJ->getEdgeData();
+
+                    if (mutableGraph.getEdge(i, j) == nullptr ||
+                        (weightIK != INF && weightKJ != INF &&
+                        mutableGraph.getVertex(i).getVertexData() != INF &&
+                        mutableGraph.getVertex(k).getVertexData() != INF &&
+                        mutableGraph.getVertex(j).getVertexData() != INF &&
+                        mutableGraph.getEdge(i, j)->getEdgeData() != INF &&
+                        mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ >
+                        mutableGraph.getVertex(j).getVertexData())) {
+                        if (mutableGraph.getEdge(i, j) == nullptr) {
+                            mutableGraph.addEdge(i, j, mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ);
+                        } else {
+                            mutableGraph.getEdge(i, j)->setEdgeData(mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ);
+                        }
                     }
                 }
             }
         }
     }
 
-    std::cout << "Матрица смежности (1 — если ребро существует, 0 — если нет ребра):\n";
+    std::cout << "Матрица смежности:\n"; 
     for (size_t i = 0; i < vertex_amount; ++i) {
         for (size_t j = 0; j < vertex_amount; ++j) {
-            if (mutableGraph.isEdgeExist(i, j)) {
+            if (mutableGraph.getEdge(i, j)) {
                 std::cout << "1 ";
             } else {
                 std::cout << "0 ";
@@ -35,67 +52,37 @@ void modifiedFloydWarshall(Graph<Data>& graph, size_t vertex_amount) {
     }
 
     size_t start_vertex = 0;
-    size_t end_vertex = vertex_amount - 1;
-    if (mutableGraph.isEdgeExist(start_vertex, end_vertex)) {
-        std::cout << "Самый длинный кратчайший путь от вершины " << start_vertex << " до вершины " << end_vertex << " равен "
-                  << mutableGraph.getEdgeData(start_vertex, end_vertex) << "\n";
+    size_t end_vertex = vertex_amount - 1; 
+    if (mutableGraph.getEdge(start_vertex, end_vertex) != nullptr) {
+        std::cout << "Самый длинный путь от вершины " << start_vertex << " до вершины " << end_vertex << " равен "
+                  << mutableGraph.getEdge(start_vertex, end_vertex)->getEdgeData() << "\n"; 
     } else {
-        std::cout << "Путь между вершинами " << start_vertex << " и " << end_vertex << " не существует\n";
+        std::cout << "Самый длинный путь от вершины " << start_vertex << " до вершины " << end_vertex << " отсутствует\n"; 
     }
 }
 
-int main(int argc, char* argv[]) {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
-    size_t vertex_amount = 0;
-    int N = 0;
-    Graph<int> graph;
+int main() {
+    // Set console output to UTF-8
+    SetConsoleOutputCP(CP_UTF8);
 
-    if (argc > 1) {
-        std::ifstream infile(argv[1]);
-        if (infile) {
-            infile >> vertex_amount >> N;
-            for (int i = 0; i < N; i++) {
-                size_t start_vertex_index, end_vertex_index;
-                int edge_data;
-                infile >> start_vertex_index >> end_vertex_index >> edge_data;
-                graph.addEdge(start_vertex_index, end_vertex_index, edge_data);
-            }
-            infile.close();
-            std::cout << "Данные прочитаны из файла: " << argv[1] << "\n";
-        } else {
-            std::cerr << "Файл не найден. Переходим к ручному вводу.\n";
-            std::cout << "Введите количество вершин> ";
-            std::cin >> vertex_amount;
+    size_t vertex_amount = 0; 
+    int N = 0; 
 
-            std::cout << "Введите количество данных для чтения> ";
-            std::cin >> N;
+    std::cout << "Введите количество вершин> "; 
+    std::cin >> vertex_amount;
 
-            for (int i = 0; i < N; i++) {
-                size_t start_vertex_index, end_vertex_index;
-                int edge_data;
-                std::cout << "Введите " << i << " данные (size_t start_vertex_index, size_t end_vertex_index, Data edge_data)> ";
-                std::cin >> start_vertex_index >> end_vertex_index >> edge_data;
-                graph.addEdge(start_vertex_index, end_vertex_index, edge_data);
-            }
-        }
-    } else {
-        std::cout << "Введите количество вершин> ";
-        std::cin >> vertex_amount;
+    Graph<int> graph(vertex_amount);
 
-        std::cout << "Введите количество данных для чтения> ";
-        std::cin >> N;
+    std::cout << "Введите количество данных для чтения> ";
+    std::cin >> N;
 
-        for (int i = 0; i < N; i++) {
-            size_t start_vertex_index, end_vertex_index;
-            int edge_data;
-            std::cout << "Введите " << i << " данные (size_t start_vertex_index, size_t end_vertex_index, Data edge_data)> ";
-            std::cin >> start_vertex_index >> end_vertex_index >> edge_data;
-            graph.addEdge(start_vertex_index, end_vertex_index, edge_data);
-        }
+    for (int i = 0; i < N; i++) {
+        size_t start_vertex_index, end_vertex_index;
+        int edge_data;
+        std::cout << "Введите данные " << i << " (start_vertex_index, end_vertex_index, edge_data)> "; 
+        std::cin >> start_vertex_index >> end_vertex_index >> edge_data;
+        graph.addEdge(start_vertex_index, end_vertex_index, edge_data);
     }
-
-    std::cout << "Исходный граф:\n";
-    graph.printGraph();
 
     modifiedFloydWarshall(graph, vertex_amount);
 
--- LibraryCPPTemplate/graph.h
+++ LibraryCPPTemplate/graph.h
@@ -1,47 +1,243 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <unordered_map>
-#include <iostream>
-#include <vector>
-#include <climits>
+#include "vector.h"
 
 template<typename Data>
 class Graph {
-private:
-    std::unordered_map<size_t, std::unordered_map<size_t, Data>> edges;
-
 public:
-    void addEdge(size_t start_vertex, size_t end_vertex, Data edge_data) {
-        edges[start_vertex][end_vertex] = edge_data;
+   Graph(size_t vertexAmount) {
+		vertexes.resize(vertexAmount);
+		for (size_t i = 0; i < vertexAmount; i++) {
+			vertexes.set(i, Vertex( static_cast<int>(i) ));
+		}
+		edgeMatrix.resize(vertexAmount * vertexAmount);
+		for (size_t i = 0;i<edgeMatrix.size();i++){
+			edgeMatrix.set(i, nullptr);
+		}
+	}
+	//деструктор графа
+	~Graph() {
+		for (size_t i = 0; i < edgeMatrix.size(); i++) {
+			delete edgeMatrix.get(i);
+		}
+	}
+    Graph(const Graph& a) {
+    // Копирование вершин
+    vertexes = a.vertexes;
+
+    // Создаем временную копию объекта Vector<Edge>
+    Vector<Edge*> tempEdgeMatrix = a.edgeMatrix;
+
+    // Используем swap для обмена данными между объектами Vector<Edge>
+    edgeMatrix.swap(tempEdgeMatrix);
+}
+
+    Graph& operator=(const Graph& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        // Освобождаем память от существующих объектов Edge
+        for (size_t i = 0; i < edgeMatrix.size(); i++) {
+            delete edgeMatrix.get(i);
+        }
+
+        // Создаем временные копии объектов
+        Vector<Vertex> tempVertices = other.vertexes;
+        Vector<Edge> tempEdgeMatrix = other.edgeMatrix;
+
+        // Используем swap для обмена данными между объектами Vector
+        vertexes.swap(tempVertices);
+        edgeMatrix.swap(tempEdgeMatrix);
+
+        return *this;
     }
 
-    bool isEdgeExist(size_t start_vertex, size_t end_vertex) const {
-        return edges.find(start_vertex) != edges.end() && edges.at(start_vertex).find(end_vertex) != edges.at(start_vertex).end();
-    }
 
-    Data getEdgeData(size_t start_vertex, size_t end_vertex) const {
-        return edges.at(start_vertex).at(end_vertex);
-    }
+    struct Vertex {
+	private:
+		Data vertexData;
+	public:
+		Vertex() : vertexData(Data()) {}
 
-    const std::unordered_map<size_t, Data>& getEdges(size_t vertex) const {
-        return edges.at(vertex);
-    }
+		Vertex(Data vertexData) {
+			this->vertexData = vertexData;
+		}
+		void setVertexData(Data vertexData) {
+			this->vertexData = vertexData;
+		}
+		Data getVertexData() {
+			return this->vertexData;
+		}
+	};
 
-    size_t getVertexAmount() const {
-        return edges.size();
-    }
+	struct Edge {
+	private:
+		Data edgeData;
+	public:
+		Edge(Data data) {
+			this->edgeData = data;
+		}
+		void setEdgeData(Data data) {
+			this->edgeData = data;
+		}
+		Data getEdgeData() {
+			return edgeData;
+		}
+	};
 
-    void printGraph() const {
-        for (const auto& pair : edges) {
-            size_t vertex = pair.first;
-            std::cout << "Vertex " << vertex << " has edges: ";
-            for (const auto& edge : pair.second) {
-                std::cout << "(" << edge.first << ", " << edge.second << ") ";
+size_t getVertexAmount() const {
+    return vertexes.size();
+}
+
+    // Вложенная структура Iterator для обхода вершин графа(не используется)
+struct Iterator {
+        private:
+            Graph* graph;  // Указатель на граф
+            size_t start;  // Индекс начальной вершины
+            int end = -1;   // Индекс следующей смежной вершины
+
+            // метод для поиска индекса ближайшей вершины, смежной с текущей
+            size_t getNearVertexIndex() {
+                for (size_t i = end + 1; i < graph->getVertexAmount(); i++) {
+				if (graph->isEdgeExist(start, i)) {
+					return static_cast<int>(i);
+				}
+			}
+			return -1;
+		}
+
+        public:
+            // Конструктор итератора
+            Iterator(Graph* graph, size_t start) {
+			this->graph = graph;
+			this->start = start;
+			this->end = getNearVertexIndex();
+		}
+            // Оператор разыменования
+           bool operator *() {
+			if (end != -1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+            // Префиксный оператор инкремента для перехода к следующей смежной вершине
+            void operator ++() {
+                end = getNearVertexIndex();
             }
-            std::cout << "\n";
+
+            size_t getIndex() const {
+			return end;
+		}
+
+		    int getStart() const {
+			return start;
+		}
+        };
+
+    Iterator getIterator(size_t start) {
+        return Iterator(this, start);
+    }
+
+size_t addVertex(Data vertex_data) {
+    size_t index = vertexes.size();
+    vertexes.resize(index + 1);
+    vertexes.set(index, Vertex(vertex_data));
+
+    size_t vertex_amount = getVertexAmount();
+
+    Vector<Edge*> buffMatrix;
+    buffMatrix.resize(vertex_amount * vertex_amount);
+    for (size_t i = 0; i < vertex_amount; i++) {
+        for (size_t j = 0; j < vertex_amount; j++) {
+            buffMatrix.set((i * vertex_amount) + j, edgeMatrix.get(i * index + j));
         }
     }
+
+    edgeMatrix.swap(buffMatrix);
+
+
+    return index;
+}
+void removeVertex(size_t index) {
+		size_t _vertexAmount = getVertexAmount();
+
+		if (index >= _vertexAmount) {
+			return;
+		}
+
+		for (size_t i = index; i < _vertexAmount - 1; i++) {
+			vertexes.set(i, vertexes.get(i + 1));
+		}
+		vertexes.resize(_vertexAmount - 1);
+
+		for (size_t i = 0; i < _vertexAmount; i++) {
+			Edge* edge = edgeMatrix.get(index * _vertexAmount + i);
+			if (edge) {
+				delete edge;
+			}
+			edge = edgeMatrix.get(i * _vertexAmount + index);
+			if (edge) {
+				delete edge;
+			}
+		}
+
+		size_t vertex_amount = getVertexAmount();
+		Vector<Edge*> buffMatrix;
+		buffMatrix.resize(vertex_amount * vertex_amount);
+		for (size_t i = 0; i < vertex_amount; i++) {
+			for (size_t j = 0; j < vertex_amount; j++) {
+				Edge* edge = edgeMatrix.get(((i + (i >= index)) * _vertexAmount) + (j + (j >= index)));
+				buffMatrix.set((i * vertex_amount) + j, edge);
+			}
+		}
+
+
+    edgeMatrix.swap(buffMatrix);
+
+}
+
+    Vertex getVertex(size_t index) {
+        return vertexes.get(index);
+    }
+
+    void addEdge(size_t start_vertex_index, size_t end_vertex_index, Data edge_data) {
+    size_t vertex_amount = getVertexAmount();
+
+    Edge* existingEdge = edgeMatrix.get(start_vertex_index * vertex_amount + end_vertex_index);
+
+    if (existingEdge == nullptr) {
+        Edge* newEdge = new Edge(edge_data);
+        edgeMatrix.set(start_vertex_index * vertex_amount + end_vertex_index, newEdge);
+    }
+    else {
+        existingEdge->setEdgeData(edge_data);
+    }
+}
+
+    void removeEdge(size_t start_vertex_index, size_t end_vertex_index) {
+        size_t vertex_amount = getVertexAmount();
+        Edge* edge = edgeMatrix.get(start_vertex_index * vertex_amount + end_vertex_index);
+        delete edge;
+        edgeMatrix.set(start_vertex_index * vertex_amount + end_vertex_index, nullptr);
+    }
+
+    Edge* getEdge(size_t start_vertex_index, size_t end_vertex_index) {
+    size_t vertex_amount = getVertexAmount();
+    return edgeMatrix.get(start_vertex_index * vertex_amount + end_vertex_index);
+}
+
+
+    bool isEdgeExist(size_t start_vertex_index, size_t end_vertex_index) {
+        return getEdge(start_vertex_index, end_vertex_index) != nullptr;
+    }
+private:
+    Vector<Vertex> vertexes;
+    Vector<Edge*> edgeMatrix;
 };
 
-#endif 
+#endif
