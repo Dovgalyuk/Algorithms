d5129416c97c4ca8adaa27900ed50aee07f25353
Conral11 <anastasianovsu.ru@mail.ru>
2025-05-05 11:05:11 +0300

    Практика 4

--- Lab4CPP/main.cpp
+++ Lab4CPP/main.cpp
@@ -6,7 +6,7 @@
 const int INF = INT_MAX;
 
 template<typename Data>
-void modifiedFloydWarshall(const Graph<Data>& graph, size_t vertex_amount) {
+void modifiedFloydWarshall(Graph<Data>& graph, size_t vertex_amount) {
     Graph<Data> mutableGraph = graph;
 
     for (size_t k = 0; k < vertex_amount; ++k) {
@@ -19,18 +19,15 @@ void modifiedFloydWarshall(const Graph<Data>& graph, size_t vertex_amount) {
                     int weightIK = edgeIK->getEdgeData();
                     int weightKJ = edgeKJ->getEdgeData();
 
-                    if (mutableGraph.getEdge(i, j) == nullptr ||
-                        (weightIK != INF && weightKJ != INF &&
-                        mutableGraph.getVertex(i).getVertexData() != INF &&
-                        mutableGraph.getVertex(k).getVertexData() != INF &&
-                        mutableGraph.getVertex(j).getVertexData() != INF &&
-                        mutableGraph.getEdge(i, j)->getEdgeData() != INF &&
-                        mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ >
-                        mutableGraph.getVertex(j).getVertexData())) {
+                    // Обновляем вес рёбер на основе Флойда-Уоршелла
+                    if (mutableGraph.getEdge(i, j) == nullptr || 
+                        (weightIK != INF && weightKJ != INF && 
+                         mutableGraph.getEdge(i, j)->getEdgeData() != INF &&
+                         mutableGraph.getEdge(i, j)->getEdgeData() > weightIK + weightKJ)) {
                         if (mutableGraph.getEdge(i, j) == nullptr) {
-                            mutableGraph.addEdge(i, j, mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ);
+                            mutableGraph.addEdge(i, j, weightIK + weightKJ);
                         } else {
-                            mutableGraph.getEdge(i, j)->setEdgeData(mutableGraph.getVertex(i).getVertexData() + weightIK + weightKJ);
+                            mutableGraph.getEdge(i, j)->setEdgeData(weightIK + weightKJ);
                         }
                     }
                 }
@@ -38,25 +35,27 @@ void modifiedFloydWarshall(const Graph<Data>& graph, size_t vertex_amount) {
         }
     }
 
+    // Выводим матрицу смежности
     std::cout << "Adjacency Matrix:\n";
     for (size_t i = 0; i < vertex_amount; ++i) {
         for (size_t j = 0; j < vertex_amount; ++j) {
             if (mutableGraph.getEdge(i, j)) {
-                std::cout << "1 ";
+                std::cout << "1 "; // Есть ребро
             } else {
-                std::cout << "0 ";
+                std::cout << "0 "; // Нет рёбер
             }
         }
         std::cout << "\n";
     }
 
+    // Для примера выводим кратчайший путь от первой вершины к последней
     size_t start_vertex = 0;
     size_t end_vertex = vertex_amount - 1; 
     if (mutableGraph.getEdge(start_vertex, end_vertex) != nullptr) {
-        std::cout << "Longest path from vertex " << start_vertex << " to vertex " << end_vertex << " is "
+        std::cout << "Shortest path from vertex " << start_vertex << " to vertex " << end_vertex << " is "
                   << mutableGraph.getEdge(start_vertex, end_vertex)->getEdgeData() << "\n";
     } else {
-        std::cout << "Longest path from vertex " << start_vertex << " to vertex " << end_vertex << " is NULL\n";
+        std::cout << "No path from vertex " << start_vertex << " to vertex " << end_vertex << "\n";
     }
 }
 
@@ -67,19 +66,22 @@ int main() {
     std::cout << "Input vertex_amount> ";
     std::cin >> vertex_amount;
 
+    // Инициализация графа
     Graph<int> graph(vertex_amount);
 
-    std::cout << "Input size read data> ";
+    std::cout << "Input number of edges> ";
     std::cin >> N;
 
+    // Ввод рёбер
     for (int i = 0; i < N; i++) {
         size_t start_vertex_index, end_vertex_index;
         int edge_data;
-        std::cout << "input " << i << " data (size_t start_vertex_index, size_t end_vertex_index, Data edge_data)> ";
+        std::cout << "Input " << i << " data (size_t start_vertex_index, size_t end_vertex_index, Data edge_data)> ";
         std::cin >> start_vertex_index >> end_vertex_index >> edge_data;
         graph.addEdge(start_vertex_index, end_vertex_index, edge_data);
     }
 
+    // Запуск модифицированного алгоритма Флойда-Уоршелла
     modifiedFloydWarshall(graph, vertex_amount);
 
     return 0;
