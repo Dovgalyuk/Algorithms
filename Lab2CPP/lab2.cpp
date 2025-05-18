5c4e57c97d25ee8bbe3d7481a57ca7f8f230df2d
SriXer <anisimovilya3012005@gmail.com>
2025-05-19 00:56:54 +0300

    lab2

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -10,10 +10,17 @@ private:
     Stack* stack;
     int variables[4] = {0};
     std::vector<std::string> call_log;
+    std::vector<bool> is_return_address_stack;
 
     void check_stack_size(size_t required) {
         if (stack_size(stack) < required) {
             throw std::runtime_error("Stack underflow");
+        }
+    }
+
+    void check_return_address() {
+        if (!is_return_address_stack.empty() && is_return_address_stack.back()) {
+            throw std::runtime_error("Invalid operation with return address");
         }
     }
 
@@ -25,131 +32,155 @@ public:
     void bipush(int value) {
         call_log.push_back("bipush " + std::to_string(value));
         stack_push(stack, value);
+        is_return_address_stack.push_back(false);  // Это не адрес возврата
     }
 
     void pop() {
         call_log.push_back("pop");
         check_stack_size(1);
+        check_return_address();
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void imul() {
         call_log.push_back("imul");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a * b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, a * b); is_return_address_stack.push_back(false);
     }
 
     void iand() {
         call_log.push_back("iand");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a & b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, a & b); is_return_address_stack.push_back(false);
     }
 
     void ior() {
         call_log.push_back("ior");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a | b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, a | b); is_return_address_stack.push_back(false);
     }
 
     void ixor() {
         call_log.push_back("ixor");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a ^ b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, a ^ b); is_return_address_stack.push_back(false);
     }
 
     void iadd() {
         call_log.push_back("iadd");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a + b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, a + b); is_return_address_stack.push_back(false);
     }
 
     void isub() {
         call_log.push_back("isub");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, b - a);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); is_return_address_stack.pop_back();
+        stack_push(stack, b - a); is_return_address_stack.push_back(false);
     }
 
     void iload_0() {
         call_log.push_back("iload_0");
         stack_push(stack, variables[0]);
+        is_return_address_stack.push_back(false);
     }
 
     void iload_1() {
         call_log.push_back("iload_1");
         stack_push(stack, variables[1]);
+        is_return_address_stack.push_back(false);
     }
 
     void iload_2() {
         call_log.push_back("iload_2");
         stack_push(stack, variables[2]);
+        is_return_address_stack.push_back(false);
     }
 
     void iload_3() {
         call_log.push_back("iload_3");
         stack_push(stack, variables[3]);
+        is_return_address_stack.push_back(false);
     }
 
     void istore_0() {
         call_log.push_back("istore_0");
         check_stack_size(1);
+        check_return_address();
         variables[0] = stack_get(stack);
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void istore_1() {
         call_log.push_back("istore_1");
         check_stack_size(1);
+        check_return_address();
         variables[1] = stack_get(stack);
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void istore_2() {
         call_log.push_back("istore_2");
         check_stack_size(1);
+        check_return_address();
         variables[2] = stack_get(stack);
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void istore_3() {
         call_log.push_back("istore_3");
         check_stack_size(1);
+        check_return_address();
         variables[3] = stack_get(stack);
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void swap() {
         call_log.push_back("swap");
         check_stack_size(2);
-        int a = stack_get(stack); stack_pop(stack);
-        int b = stack_get(stack); stack_pop(stack);
-        stack_push(stack, a);
-        stack_push(stack, b);
+        check_return_address();
+        int a = stack_get(stack); stack_pop(stack); bool a_flag = is_return_address_stack.back(); is_return_address_stack.pop_back();
+        int b = stack_get(stack); stack_pop(stack); bool b_flag = is_return_address_stack.back(); is_return_address_stack.pop_back();
+        stack_push(stack, a); is_return_address_stack.push_back(b_flag);
+        stack_push(stack, b); is_return_address_stack.push_back(a_flag);
     }
 
     void invokestatic(int address) {
         call_log.push_back("invokestatic " + std::to_string(address));
-        stack_push(stack, -address); // Адрес как отрицательное значение
+        stack_push(stack, -address);  // адрес как отрицательное число
+        is_return_address_stack.push_back(true);  // это именно адрес возврата
     }
 
     void return_op() {
         call_log.push_back("return");
         check_stack_size(1);
-        if (stack_get(stack) >= 0) {
+        if (!is_return_address_stack.back()) {
             throw std::runtime_error("Return with non-address value");
         }
         stack_pop(stack);
+        is_return_address_stack.pop_back();
     }
 
     void print_state() {
@@ -162,17 +193,24 @@ public:
             std::cout << std::endl;
         } else {
             Stack* temp_stack = stack_create();
+            std::vector<bool> temp_flags;
+
             while (!stack_empty(stack)) {
-                stack_push(temp_stack, stack_get(stack));
+                int val = stack_get(stack);
                 stack_pop(stack);
+                stack_push(temp_stack, val);
+                temp_flags.push_back(is_return_address_stack.back());
+                is_return_address_stack.pop_back();
             }
 
-            while (!stack_empty(temp_stack)) {
+            for (int i = temp_flags.size() - 1; i >= 0; --i) {
                 int val = stack_get(temp_stack);
                 std::cout << val << std::endl;
-                stack_push(stack, val);
                 stack_pop(temp_stack);
+                stack_push(stack, val);
+                is_return_address_stack.push_back(temp_flags[i]);
             }
+
             stack_delete(temp_stack);
         }
 
@@ -182,6 +220,7 @@ public:
         }
     }
 };
+
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
