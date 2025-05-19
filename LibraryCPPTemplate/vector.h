4bd8f66d865d47bea25a7718e7d9d7ac39168cb0
Zooooof <105545024+Zooooof@users.noreply.github.com>
2025-05-20 01:05:29 +0300

    Update vector.h

--- LibraryCPPTemplate/vector.h
+++ LibraryCPPTemplate/vector.h
@@ -2,56 +2,73 @@
 #define VECTOR_TEMPLATE_H
 
 #include <cstddef>
+#include <stdexcept>
+#include <algorithm>
 
 template <typename Data> class Vector
 {
 public:
-    // Creates vector
-    Vector()
-    {
+    Vector() : data(nullptr), _size(0), _capacity(0) {}
+
+    Vector(const Vector& other) : _size(other._size), _capacity(other._size) {
+        data = new Data[_capacity];
+        for (size_t i = 0; i < _size; ++i) {
+            data[i] = other.data[i];
+        }
     }
 
-    // copy constructor
-    Vector(const Vector &a)
-    {
-    }
-
-    // assignment operator
-    Vector &operator=(const Vector &a)
-    {
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] data;
+            _size = other._size;
+            _capacity = other._size;
+            data = new Data[_capacity];
+            for (size_t i = 0; i < _size; ++i) {
+                data[i] = other.data[i];
+            }
+        }
         return *this;
     }
 
-    // Deletes vector structure and internal data
-    ~Vector()
-    {
+    ~Vector() {
+        delete[] data;
     }
 
-    // Retrieves vector element with the specified index
-    Data get(size_t index) const
-    {
-        return Data();
+    Data get(size_t index) const {
+        if (index >= _size) throw std::out_of_range("Index out of range");
+        return data[index];
     }
 
-    // Sets vector element with the specified index
-    void set(size_t index, Data value)
-    {
+    void set(size_t index, const Data& value) {
+        if (index >= _size) throw std::out_of_range("Index out of range");
+        data[index] = value;
     }
 
-    // Retrieves current vector size
-    size_t size() const
-    {
-        return 0;
+    size_t size() const { return _size; }
+
+    void resize(size_t new_size) {
+        if (new_size > _capacity) {
+            reserve(new_size * 2);
+        }
+        _size = new_size;
     }
 
-    // Changes the vector size (may increase or decrease)
-    // Should be O(1) on average
-    void resize(size_t size)
-    {
+    void reserve(size_t new_capacity) {
+        if (new_capacity > _capacity) {
+            Data* new_data = new Data[new_capacity];
+            for (size_t i = 0; i < _size; ++i) {
+                new_data[i] = data[i];
+            }
+            delete[] data;
+            data = new_data;
+            _capacity = new_capacity;
+        }
     }
 
 private:
-    // private data should be here
+    Data* data;
+    size_t _size;
+    size_t _capacity;
 };
 
-#endif
+#endif // VECTOR_TEMPLATE_H
