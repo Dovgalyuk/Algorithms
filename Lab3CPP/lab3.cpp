a30837caf57bd469645e5baedffe08b420909c44
SriXer <anisimovilya3012005@gmail.com>
2025-06-02 22:58:02 +0300

    lab3

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -3,10 +3,6 @@
 #include <vector>
 #include "../LibraryCPP/queue.h"
 
-struct Cell {
-    int row, col, intensity;
-};
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
@@ -28,14 +24,19 @@ int main(int argc, char* argv[]) {
     int x, y;
     while (fin >> x >> y) {
         x--; y--;
+        if (x < 0 || x >= n || y < 0 || y >= m) {
+            std::cerr << "Invalid coordinates: " << x+1 << " " << y+1 << std::endl;
+            continue;
+        }
         board[x][y] = 1;
-        queue_insert(q, x * m + y);  // Храним координаты как одно число
+        queue_insert(q, x * m + y);
     }
 
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
     int maxIntensity = 1;
 
+    // Обработка очереди
     while (!queue_empty(q)) {
         int pos = queue_get(q);
         queue_remove(q);
@@ -44,19 +45,25 @@ int main(int argc, char* argv[]) {
         int y = pos % m;
         int current = board[x][y];
 
+        // Обрабатываем соседей
         for (int d = 0; d < 4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
+            
+            // Проверяем границы и что клетка еще не окрашена
             if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
                 board[nx][ny] = current + 1;
-                if (current + 1 > maxIntensity)
+                if (current + 1 > maxIntensity) {
                     maxIntensity = current + 1;
+                }
                 queue_insert(q, nx * m + ny);
             }
         }
     }
 
     queue_delete(q);
+
+    // Выводим результат
     std::cout << maxIntensity << std::endl;
     return 0;
 }
--- LibraryCPP/list.h
+++ LibraryCPP/list.h
@@ -1,25 +1,23 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdexcept>
-
 typedef int Data;
 
-struct List;
 struct ListItem;
+struct List;
 
-List *list_create();
-void list_delete(List *list);
+List* list_create();
+void list_delete(List* list);
 
-ListItem *list_first(List *list);
-Data list_item_data(const ListItem *item);
-ListItem *list_item_next(ListItem *item);
-ListItem *list_item_prev(ListItem *item);
+ListItem* list_first(List* list);
+Data list_item_data(const ListItem* item);
+ListItem* list_item_next(ListItem* item);
+ListItem* list_item_prev(ListItem* item);
 
-ListItem *list_insert(List *list, Data data);
-ListItem *list_insert_after(List *list, ListItem *item, Data data);
+ListItem* list_insert(List* list, Data data);
+ListItem* list_insert_after(List* list, ListItem* item, Data data);
 
-ListItem *list_erase_first(List *list);
-ListItem *list_erase_next(List *list, ListItem *item);
+ListItem* list_erase_first(List* list);
+ListItem* list_erase_next(List* list, ListItem* item);
 
 #endif
