c55dc96886f401b3316995ca49101d204c575b65
crunchydeer30 <crunchydeer30@gmail.com>
2025-09-16 23:05:24 +0300

    lab1

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,40 +1,80 @@
-#include <stdio.h>
 #include "array.h"
+#include <stdio.h>
 
-Array *array_create_and_read(FILE *input)
-{
-    int n;
-    fscanf(input, "%d", &n);
-    /* Create array */
-    Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
-    {
-        int x;
-        fscanf(input, "%d", &x);
-        array_set(arr, i, x);
+Array *array_create_and_read(FILE *input) {
+  int n;
+  fscanf(input, "%d", &n);
+
+  Array *arr = array_create(n, NULL);
+
+  for (int i = 0; i < n; ++i) {
+    int x;
+    fscanf(input, "%d", &x);
+    array_set(arr, i, x);
+  }
+
+  return arr;
+}
+
+void task1(Array *arr) {
+  int remainder = 1;
+  const int divisor = 17;
+
+  for (size_t i = 0; i < array_size(arr); i++) {
+    remainder *= 10;
+    int digit = remainder / divisor;
+    remainder %= divisor;
+    array_set(arr, i, digit);
+  }
+
+  for (size_t i = 0; i < array_size(arr); i++) {
+    printf("%d ", (int)array_get(arr, i));
+  }
+  printf("\n");
+}
+
+void task2(Array *arr) {
+  size_t n = array_size(arr);
+
+  for (size_t i = 0; i < n; i++) {
+    int a = (int)array_get(arr, i);
+    int is_divisible = 0;
+
+    for (size_t j = 0; j < n; j++) {
+      if (i == j) {
+        continue;
+      }
+      int b = (int)array_get(arr, j);
+
+      if (b != 0 && a % b == 0) {
+        is_divisible = 1;
+        break;
+      }
     }
-    return arr;
+
+    if (!is_divisible) {
+      printf("%d ", a);
+    }
+  }
+  printf("\n");
 }
 
-void task1(Array *arr)
-{
-}
+int main(int argc, char **argv) {
+  Array *arr = NULL;
+  FILE *input = fopen(argv[1], "r");
 
-void task2(Array *arr)
-{
-}
+  if (input == NULL) {
+    printf("Error: Failed to open input file\n");
+    return 1;
+  }
 
-int main(int argc, char **argv)
-{
-    Array *arr = NULL;
-    FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
-    task1(arr);
-    array_delete(arr);
-    /* Create another array here */
-    arr = array_create_and_read(input);
-    task2(arr);
-    array_delete(arr);
-    fclose(input);
+  arr = array_create_and_read(input);
+  task1(arr);
+  array_delete(arr);
+
+  arr = array_create_and_read(input);
+  task2(arr);
+  array_delete(arr);
+
+  fclose(input);
 }
