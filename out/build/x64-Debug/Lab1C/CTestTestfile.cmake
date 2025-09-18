695a670ed6435f33eb3bcd768e1b0359eff4f90a
Дроздов Виталий <hitic2004@mail.ru>
2025-09-19 01:40:10 +0300

    ...

--- out/build/x64-Debug/Lab1C/CTestTestfile.cmake
+++ out/build/x64-Debug/Lab1C/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TestLab1C "C:/Users/drozd/source/repos/Algorithms/out/build/x64-Debug/Lab1C/Lab1C.exe" "C:/Users/drozd/source/repos/Algorithms/Lab1C/input.txt")
-set_tests_properties(TestLab1C PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/drozd/source/repos/Algorithms/Lab1C/CMakeLists.txt;5;add_test;C:/Users/drozd/source/repos/Algorithms/Lab1C/CMakeLists.txt;0;")
+set_tests_properties(TestLab1C PROPERTIES  PASS_REGULAR_EXPRESSION "1 2 3 4 5" _BACKTRACE_TRIPLES "C:/Users/drozd/source/repos/Algorithms/Lab1C/CMakeLists.txt;5;add_test;C:/Users/drozd/source/repos/Algorithms/Lab1C/CMakeLists.txt;0;")
