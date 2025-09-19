ab33aceb5214b3f5e58c9402a7aefdcfb1e73b6c
Дроздов Виталий <hitic2004@mail.ru>
2025-09-19 18:37:51 +0300

    ...

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,4 +1,4 @@
-#include "array.h"
+/*#include "array.h"
 
 struct Array
 {
@@ -31,4 +31,34 @@ void array_set(Array *arr, size_t index, Data value)
 size_t array_size(const Array *arr)
 {
     return 0;
+}
+*/
+
+#include "array.h"
+struct Array {
+    Data* data;
+    size_t size;
+};
+Array* array_create(size_t size) {
+    if (size == 0) {
+        return nullptr;
+    }
+    Array* arr = new Array;
+    arr->size = size;
+    arr->data = new Data[size];
+    return arr;
+}
+void array_delete(Array* arr) {
+    if (!arr) return;
+    delete[] arr->data;
+    delete arr;
+}
+Data array_get(const Array* arr, size_t index) {
+    return arr->data[index];
+}
+void array_set(Array* arr, size_t index, Data value) {
+    arr->data[index] = value;
+}
+size_t array_size(const Array* arr) {
+    return arr->size;
 }
