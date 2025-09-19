c8cd2342aefb51b106514cc3917ae62a05de85f5
Дроздов Виталий <hitic2004@mail.ru>
2025-09-19 19:51:46 +0300

    ...

--- Lab1C_PR/main.cpp
+++ Lab1C_PR/main.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include <fstream> // Для файлов.
 #include <vector> // Для vector.
 #include <climits> // Для INT_MIN.
@@ -114,72 +114,5 @@ int main(int argc, char* argv[]) {
     task4(filename);
     task5(filename);
 
-    return 0;
-}*/
-
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <climits>
-#include "array.h"
-#include <queue.h>
-
-using namespace std;
-
-bool readFile(const string& filename, size_t& n, Data*& data) {
-    ifstream file(filename);
-    if (!file.is_open()) { cout << "File not opened" << endl; return false; }
-    file >> n;
-    if (n <= 0) { cout << "Size <=0" << endl; return false; }
-    data = new Data[n];
-    for (size_t i = 0; i < n; i++) file >> data[i];
-    file.close();
-    return true;
-}
-
-void task4(const string& filename) {
-    size_t n = 0;
-    Data* data = nullptr;
-    if (!readFile(filename, n, data)) return;
-    Array arr(n);
-    for (size_t i = 0; i < n; i++) arr.set(i, data[i]);
-    Data sum = 0;
-    for (size_t i = 0; i < arr.getSize(); i++) sum += arr.get(i);
-    int count = 0;
-    vector<size_t> indices;
-    for (size_t i = 0; i < arr.getSize(); i++) {
-        if (arr.get(i) > sum) { count++; indices.push_back(i); }
-    }
-    cout << "Task 4: count > sum (" << sum << "): " << count << endl;
-    if (count > 0) { cout << "Indices: "; for (size_t idx : indices) cout << idx << " "; cout << endl; }
-    delete[] data;
-}
-
-void task5(const string& filename) {
-    size_t n = 0;
-    Data* data = nullptr;
-    if (!readFile(filename, n, data)) return;
-    Array arr(n);
-    for (size_t i = 0; i < n; i++) arr.set(i, data[i]);
-    if (n < 5) { cout << "Task 5: array <5" << endl; delete[] data; return; }
-    Data maxSum = INT_MIN;
-    size_t start = 0;
-    for (size_t i = 0; i <= n - 5; i++) {
-        Data cur = arr.get(i) + arr.get(i + 1) + arr.get(i + 2) + arr.get(i + 3) + arr.get(i + 4);
-        if (cur > maxSum) { maxSum = cur; start = i; }
-    }
-    cout << "Task 5: max sum: " << maxSum << endl;
-    cout << "Elements: ";
-    for (size_t j = start; j < start + 5; j++) cout << arr.get(j) << " ";
-    cout << endl;
-    delete[] data;
-}
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) { cout << "Specify file!" << endl; return 1; }
-    string filename = argv[1];
-    task4(filename);
-    task5(filename);
     return 0;
 }
--- LibraryCPP/array.h
+++ LibraryCPP/array.h
@@ -1,55 +1,5 @@
 #ifndef ARRAY_H
 #define ARRAY_H
-#include <cstddef>
-
-typedef int Data; // Тип данных
-
-struct Array {
-    Data* data; // Указатель
-    size_t size; // Размер
-
-    Array(size_t s); // Создать
-    ~Array(); // Удалить
-    size_t getSize() const; // Размер
-    Data get(size_t index) const; // Получить
-    void set(size_t index, Data value); // Установить
-};
-
-#endif
-
-/*#ifndef ARRAY_H
-#define ARRAY_H
-
-#include <cstddef>
-
-// Non-resizeable array
-
-// Stores integer values inside
-// Change it to desired type
-typedef int Data;
-
-struct Array;
-
-// create array
-Array *array_create(size_t size);
-
-// delete array, free memory
-void array_delete(Array *arr);
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index);
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value);
-
-// returns array size
-size_t array_size(const Array *arr);
-
-#endif
-*/
-
-/*#ifndef ARRAY_H
-#define ARRAY_H
 
 #include <stdexcept>
 
@@ -97,4 +47,4 @@ public:
     }
 };
 
-#endif*/
+#endif
