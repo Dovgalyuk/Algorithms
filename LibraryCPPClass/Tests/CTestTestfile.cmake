8accefee18d0abdce5b15c762afde8361f1b7835
Gogoxa <igor.nikolaev0@yandex.ru>
2025-09-21 15:39:58 +0300

    Delete LibraryCPPClass/Tests/CTestTestfile.cmake

--- LibraryCPPClass/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/IgorE/OneDrive/Desktop/Учеба/СДИА/Algorithms/LibraryCPPClass/Tests
-# Build directory: C:/Users/IgorE/OneDrive/Desktop/Учеба/СДИА/Algorithms/LibraryCPPClass/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
