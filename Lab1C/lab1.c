ee6341d13db98c5a61132a17abaa7789141f3bda
Roman Kuliev <culievroman9@gmail.com>
2025-09-21 13:39:49 +0300

    Started from scratch, removed handwritten libraries, modified the existing array.c, completely rewritten lab1.c and cmakelists.txt

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,118 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "array.h"
 
-Array *array_create_and_read(FILE *input, int read_data)
-{
-    int n;
-    
-    if (fscanf(input, "%d", &n) != 1) {
-        return NULL;
-    }
-    
-    if (n < 0) {
-        return NULL;
-    }
-    
-    Array *arr = array_create(n, NULL);
-    if (!arr) {
-        return NULL;
-    }
-    
-    if (!read_data) {
-        return arr;
-    }
-    
-    for (int i = 0 ; i < n ; ++i)
-    {
-        int x;
-        if (fscanf(input, "%d", &x) != 1) {
-            array_delete(arr);
-            return NULL;
-        }
-        array_set(arr, i, x);
-    }
-    
-    return arr;
-}
-
-void task1(Array *arr)
-{
-    if (!arr || arr->size == 0) {
-        return;
-    }
+void fill_with_1_over_17_digits(Array* arr) {
+    size_t size = array_size(arr);
+    if (size == 0) return;
     
     int remainder = 1;
-    for (int i = 0; i < arr->size; i++) {
+    for (size_t i = 0; i < size; i++) {
         remainder *= 10;
         int digit = remainder / 17;
-        remainder %= 17;
-        array_set(arr, i, digit);
+        remainder = remainder % 17;
+        array_set(arr, i, (Data)digit);
     }
-    
-    array_print(arr);
 }
 
-void task2(Array *arr)
-{
-    if (!arr) {
+void find_max_sum_of_five(Array* arr) {
+    size_t size = array_size(arr);
+    if (size < 5) {
+        printf("Array size is less than 5\n");
         return;
     }
     
-    if (arr->size < 5) {
-        array_print(arr);
-        return;
+    long long max_sum = 0;
+    size_t max_start_index = 0;
+    
+    for (size_t i = 0; i < 5; i++) {
+        max_sum += (long long)array_get(arr, i);
     }
     
-    int max_sum = 0;
-    int max_start_index = 0;
+    long long current_sum = max_sum;
     
-    for (int i = 0; i <= arr->size - 5; i++) {
-        int current_sum = 0;
-        for (int j = 0; j < 5; j++) {
-            current_sum += array_get(arr, i + j);
-        }
-        
+    for (size_t i = 5; i < size; i++) {
+        current_sum = current_sum - (long long)array_get(arr, i - 5) + (long long)array_get(arr, i);
         if (current_sum > max_sum) {
             max_sum = current_sum;
-            max_start_index = i;
+            max_start_index = i - 4;
         }
     }
     
-    for (int i = 0; i < 5; i++) {
-        printf("%d", array_get(arr, max_start_index + i));
-        if(i<4){
+    for (size_t i = max_start_index; i < max_start_index + 5; i++) {
+        printf("%d", (int)array_get(arr, i));
+        if (i < max_start_index + 4) {
             printf(" ");
         }
     }
+    printf("\n");
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2) {
+int read_int_from_file(FILE* file) {
+    int value;
+    fscanf(file, "%d", &value);
+    return value;
+}
+
+void read_array_from_file(Array* arr, FILE* file) {
+    size_t size = array_size(arr);
+    for (size_t i = 0; i < size; i++) {
+        int value;
+        fscanf(file, "%d", &value);
+        array_set(arr, i, (Data)value);
+    }
+}
+
+void print_array(Array* arr) {
+    size_t size = array_size(arr);
+    for (size_t i = 0; i < size; i++) {
+        printf("%d", (int)array_get(arr, i));
+        if (i < size - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
         return 1;
     }
     
-    FILE *input = fopen(argv[1], "r");
-    if (!input) {
-        perror("fopen error");
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening file: %s\n", argv[1]);
         return 1;
     }
+
+    int size1 = read_int_from_file(file);
+    Array* arr1 = array_create(size1, NULL);
     
-    Array *arr = NULL;
+    fill_with_1_over_17_digits(arr1);
+    print_array(arr1);
     
-    arr = array_create_and_read(input, 0); 
-    if (arr) {
-        task1(arr);
-        array_delete(arr);
-    }
+    array_delete(arr1);
     
-    arr = array_create_and_read(input, 1);
-    if (arr) {
-        task2(arr);
-        array_delete(arr);
-    }
+    int size2 = read_int_from_file(file);
+    Array* arr2 = array_create(size2, NULL);
     
-    fclose(input);
+    read_array_from_file(arr2, file);
+    find_max_sum_of_five(arr2);
+    
+    array_delete(arr2);
+    fclose(file);
+    
     return 0;
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,36 +1,80 @@
 #include <stdlib.h>
+#include <string.h>
 #include "array.h"
 
 typedef struct Array {
-    // remove this
-    Data d;
+    size_t size;
+    Data* data;
+    FFree* free_func;
 } Array;
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array* arr = malloc(sizeof(Array));
+    if (arr == NULL) {
+        return NULL;
+    }
+    
+    arr->size = size;
+    arr->free_func = f;
+    arr->data = malloc(size * sizeof(Data));
+    
+    if (arr->data == NULL) {
+        free(arr);
+        return NULL;
+    }
+    
+    // Инициализируем нулями
+    memset(arr->data, 0, size * sizeof(Data));
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if (arr == NULL) return;
+    
+    if (arr->free_func != NULL) {
+        for (size_t i = 0; i < arr->size; i++) {
+            if (arr->data[i] != 0) {
+                arr->free_func((void*)arr->data[i]);
+            }
+        }
+    }
+    
+    free(arr->data);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (arr == NULL || index >= arr->size) {
+        return (Data)0;
+    }
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (arr == NULL || index >= arr->size) {
+        return;
+    }
+    
+    if (arr->free_func != NULL && arr->data[index] != 0) {
+        arr->free_func((void*)arr->data[index]);
+    }
+    
+    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    if (arr == NULL) {
+        return 0;
+    }
+    return arr->size;
 }
