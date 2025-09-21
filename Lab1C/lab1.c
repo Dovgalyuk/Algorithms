25fd9ae596cd9f8d81a3798282b4af60a327cc96
va1ker <dead1nside.inside@yandex.ru>
2025-09-21 18:59:51 +0300

    lab: implement two tasks and fix the output of tests

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -19,10 +19,44 @@ Array *array_create_and_read(FILE *input)
 
 void task1(Array *arr)
 {
+    int min = (int)array_get(arr, 0);
+    int max = (int)array_get(arr, 0);
+    for (size_t i = 1; i < array_size(arr); i++) {
+        int val = (int)array_get(arr, i);
+        if (val < min) min = val;
+        if (val > max) max = val;
+    }
+    double avg = (min + max) / 2.0;
+        for (size_t i = 0; i < array_size(arr); i++) {
+        int val = (int)array_get(arr, i);
+        if (val < avg) {
+            printf("%zu ", i);
+        }
+    }
+    return;
 }
 
 void task2(Array *arr)
 {
+    int maxNumber = (int)array_get(arr, 0);
+    int maxFrequent = 1;
+    int maxFrequentIdx = 0;
+    for (size_t i = 0; i < array_size(arr); i++) {
+        int count = 0;
+        int val = (int)array_get(arr, i);
+        for (size_t j = 0; j < array_size(arr); j++) {
+            if (val == (int)array_get(arr, j)) {
+                count++;
+            }
+        }
+        if (count > maxFrequent) {
+            maxFrequent = count;
+            maxNumber = val;
+        }
+
+    }
+    printf("%d %d %d\n", maxNumber, maxFrequent, maxFrequentIdx);
+    return;
 }
 
 int main(int argc, char **argv)
