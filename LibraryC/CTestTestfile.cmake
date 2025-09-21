48b13df637c30c9eb60d6aa873dd5d46e408b59a
Gogoxa <igor.nikolaev0@yandex.ru>
2025-09-21 15:35:11 +0300

    Delete LibraryC/CTestTestfile.cmake

--- LibraryC/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/IgorE/OneDrive/Desktop/Учеба/СДИА/Algorithms/LibraryC
-# Build directory: C:/Users/IgorE/OneDrive/Desktop/Учеба/СДИА/Algorithms/LibraryC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
