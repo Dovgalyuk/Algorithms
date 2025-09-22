7ab39ac104051bd2217228413efe2993d605313b
Gogoxa <igor.nikolaev0@yandex.ru>
2025-09-22 19:39:13 +0300

    lab1

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "array.h"
 
 Array *array_create_and_read(FILE *input)
 {
     int n;
-    fscanf(input, "%d", &n);
+    if(fscanf(input, "%d", &n) != 1) return NULL; 
     /* Create array */
-    Array *arr = array_create(n, NULL);
+    Array *arr = array_create((size_t)n, NULL);
+    if(!arr) return NULL;
     /* Read array data */
     for (int i = 0 ; i < n ; ++i)
     {
         int x;
         fscanf(input, "%d", &x);
-        array_set(arr, i, x);
+        array_set(arr, i, (Data)x);
     }
     return arr;
 }
 
 void task1(Array *arr)
 {
+    size_t size = array_size(arr);
+    for(size_t i = 0; i < size / 2; i++){
+        Data t1 = array_get(arr, i);
+        Data t2 = array_get(arr, size - i - 1);
+        array_set(arr, i, t2);
+        array_set(arr, size - i - 1, t1);
+    }
+    for(size_t i = 0; i < size; i++){
+        printf("%d ", (int)array_get(arr, i));
+    }
+    printf("\n");
 }
 
 void task2(Array *arr)
 {
+    if(array_size(arr) < 5){
+        return;
+    }
+    int s1 = 0;
+    size_t i1 = 0;
+    for(size_t i = 0; i < 5; i++){
+        s1 += (int)array_get(arr,i);
+    }
+    int s2 = s1;
+    for(size_t i = 1; i <= array_size(arr) - 5; i++){
+        s2 = s2 - (int)array_get(arr, i - 1) + (int)array_get(arr,i + 4);
+        if(s2 > s1){
+            s1 = s2;
+            i1 = i;
+        }
+    }
+
+    for(size_t i = 0; i < 5; i++){
+        printf("%d ", (int)array_get(arr, i1 + i));
+    }
+    printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -33,8 +67,8 @@ int main(int argc, char **argv)
     task1(arr);
     array_delete(arr);
     /* Create another array here */
-    arr = array_create_and_read(input);
-    task2(arr);
-    array_delete(arr);
+    Array *arr1 = array_create_and_read(input);
+    task2(arr1);
+    array_delete(arr1);
     fclose(input);
 }
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -2,35 +2,67 @@
 #include "array.h"
 
 typedef struct Array {
-    // remove this
-    Data d;
+    size_t size;
+    Data *d;
+    FFree *ff;
 } Array;
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array *arr = (Array*)malloc(sizeof(Array));
+    if(!arr){
+        return NULL;
+    }
+    arr->size = size;
+    arr->ff = f;
+    arr->d = (Data*)calloc(size, sizeof(Data));
+
+    if(!arr->d){
+        free(arr);
+        return NULL;
+    }
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if(!arr) return;
+    if(arr->ff){
+        for(size_t i = 0; i < arr->size; i++){
+            if(arr->d[i] != 0){
+                arr->ff((void*)arr->d[i]);
+            }
+        }
+    }
+    free(arr->d);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if(!arr || index >= arr->size){
+        return (Data)0;
+    }
+    return arr->d[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if(!arr || index >= arr->size){
+        return;
+    }
+    if(arr->ff && arr->d[index] != 0){
+        arr->ff((void*)arr->d[index]);
+    }
+    arr->d[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr ? arr->size : 0;
 }
