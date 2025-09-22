4e0704ff153431180409e20d81361b825743bba1
Abubakr0220 <peregar1122@gmail.com>
2025-09-22 08:08:34 +0300

    Очистка репозитория от временных файлов

--- LibraryC/LibraryC/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/magnito/proga/Algorithms/LibraryC
-# Build directory: /home/magnito/proga/Algorithms/LibraryC/LibraryC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
--- LibraryC/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/magnito/proga/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: /home/magnito/proga/Algorithms/LibraryC/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
