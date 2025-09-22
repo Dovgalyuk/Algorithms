6aad677f964572444463b63ac25f3b2c5fb131dd
Дроздов Виталий <hitic2004@mail.ru>
2025-09-22 15:20:13 +0300

    Исправил

--- out/build/x64-Debug/Lab1C_PR/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/drozd/source/repos/Algorithms/Lab1C_PR
-# Build directory: C:/Users/drozd/source/repos/Algorithms/out/build/x64-Debug/Lab1C_PR
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1C_PR "C:/Users/drozd/source/repos/Algorithms/out/build/x64-Debug/Lab1C_PR/Lab1C_PR.exe" "C:/Users/drozd/source/repos/Algorithms/Lab1C_PR/input.txt")
-set_tests_properties(TestLab1C_PR PROPERTIES  PASS_REGULAR_EXPRESSION "Task 4: Количество элементов > суммы \\(.*\\): 0;Task 5: Максимальная сумма 5 соседних: 114" _BACKTRACE_TRIPLES "C:/Users/drozd/source/repos/Algorithms/Lab1C_PR/CMakeLists.txt;9;add_test;C:/Users/drozd/source/repos/Algorithms/Lab1C_PR/CMakeLists.txt;0;")
--- out/build/x64-Debug/LibraryC/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/drozd/source/repos/Algorithms/LibraryC/Tests
-# Build directory: C:/Users/drozd/source/repos/Algorithms/out/build/x64-Debug/LibraryC/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayC "TestArrayC")
-set_tests_properties(TestArrayC PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/drozd/source/repos/Algorithms/LibraryC/Tests/CMakeLists.txt;4;add_test;C:/Users/drozd/source/repos/Algorithms/LibraryC/Tests/CMakeLists.txt;0;")
