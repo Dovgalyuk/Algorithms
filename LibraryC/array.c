f309ed7355d0850769ba2e4ae56e578d4aafb7cb
Smole17 <slavapnv@gmail.com>
2025-09-22 04:52:59 +0300

    Final refactoring to lab1

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -35,17 +35,24 @@ void array_delete(Array *arr)
 Data array_get(const Array *arr, size_t index)
 {
     if (index >= arr->size)
+    {
         return 0;
+    }
+
     return arr->data[index];
 }
 
 void array_set(Array *arr, size_t index, Data value)
 {
     if (index >= arr->size)
+    {
         return;
+    }
 
     if (arr->data[index] && arr->free_func)
+    {
         arr->free_func((void*) arr->data[index]);
+    }
 
     arr->data[index] = value;
 }
