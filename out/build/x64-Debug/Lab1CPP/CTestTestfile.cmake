f66ccf09055ae799a2210fb937675016ed7c4edd
Даниил Сорока <mamulia737@gmail.com>
2025-09-25 23:41:53 +0300

    Add new test fiels

--- out/build/x64-Debug/Lab1CPP/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Пользователь/source/repos/Algorithms/Lab1CPP
-# Build directory: C:/Users/Пользователь/source/repos/Algorithms/out/build/x64-Debug/Lab1CPP
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1CPPClass "C:/Users/Пользователь/source/repos/Algorithms/out/build/x64-Debug/Lab1CPP/Lab1CPP.exe" "C:/Users/Пользователь/source/repos/Algorithms/Lab1CPP/input.txt")
-set_tests_properties(TestLab1CPPClass PROPERTIES  PASS_REGULAR_EXPRESSION "Found: 9" _BACKTRACE_TRIPLES "C:/Users/Пользователь/source/repos/Algorithms/Lab1CPP/CMakeLists.txt;5;add_test;C:/Users/Пользователь/source/repos/Algorithms/Lab1CPP/CMakeLists.txt;0;")
--- out/build/x64-Debug/LibraryCPPClass/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Пользователь/source/repos/Algorithms/LibraryCPPClass/Tests
-# Build directory: C:/Users/Пользователь/source/repos/Algorithms/out/build/x64-Debug/LibraryCPPClass/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayCPPClass "TestArrayCPPClass")
-set_tests_properties(TestArrayCPPClass PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Пользователь/source/repos/Algorithms/LibraryCPPClass/Tests/CMakeLists.txt;4;add_test;C:/Users/Пользователь/source/repos/Algorithms/LibraryCPPClass/Tests/CMakeLists.txt;0;")
