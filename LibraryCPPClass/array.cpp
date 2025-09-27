451a0c6d9cd6439f4719938d7640d31864ea141f
ilichppshka <paturin.ilya@gmail.com>
2025-09-26 22:19:56 -0700

    feat: implementing reverse and shift func for container (array)

--- LibraryCPPClass/array.cpp
+++ LibraryCPPClass/array.cpp
@@ -1,17 +1,15 @@
-#include "array.h"
 #include <stdexcept>
+#include <iostream>
+#include "array.h"
 
-Array::Array(size_t size) : _size(size), _data(new Data[size])
+Array::Array(size_t size) : _data(new Data[size]), _size(size)
 {
-    //if (_data == nullptr) {
-      //  throw std::bad_alloc();
-    //}
-    for (size_t i = 0; i < size; i++) {
-        _data[i] = 0;
+    if (_data == nullptr) {
+        throw std::bad_alloc();
     }
 }
 
-Array::Array(const Array& a) : _size(a._size), _data(new Data[a._size])
+Array::Array(const Array& a) : _data(new Data[a._size]), _size(a._size)
 {
     for (size_t i = 0; i < _size; i++) {
         _data[i] = a._data[i];
@@ -43,7 +41,7 @@ Array::~Array()
 Data Array::get(size_t index) const
 {
     if (index >= _size) {
-        throw std::out_of_range("Index out of range");
+        throw std::out_of_range("Индекс вне диапазона");
     }
 
     return _data[index];
@@ -52,7 +50,7 @@ Data Array::get(size_t index) const
 void Array::set(size_t index, Data value)
 {
     if (index >= _size) {
-        throw std::out_of_range("Index out of range");
+        throw std::out_of_range("Индекс вне диапазона");
     }
 
     _data[index] = value;
@@ -61,4 +59,11 @@ void Array::set(size_t index, Data value)
 size_t Array::size() const
 {
     return _size;
+}
+
+void Array::print() {
+    for (size_t i = 0; i < _size; i++) {
+        std::cout << _data[i] << " ";
+    }
+    std::cout << std::endl;
 }
