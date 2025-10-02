1cc263acdbb01b516225c420e91bff6fa2019f33
YungPlane <assassincreed13570@gmail.com>
2025-10-02 19:31:26 +0300

    feat(array): реализован класс Array

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,49 +1,68 @@
 #ifndef ARRAY_TEMPLATE_H
 #define ARRAY_TEMPLATE_H
 
-template <typename Data> class Array
+template <typename T> class Array
 {
 public:
     // create array
-    explicit Array(size_t size)
+    explicit Array(size_t size) : m_size(size), m_data(new T[size])
     {
     }
 
     // copy constructor
-    Array(const Array &a)
+    Array(const Array &a) : m_size(a.m_size), m_data(new T[a.m_size])
     {
+        for (size_t i = 0; i < m_size; ++i)
+            m_data[i] = a.m_data[i];
     }
 
     // assignment operator
     Array &operator=(const Array &a)
     {
+        if (this != &a)
+        {
+            delete[] m_data;
+
+            m_size = a.m_size;
+            m_data = new T[m_size];
+            for (size_t i = 0; i < m_size; ++i)
+                m_data[i] = a.m_data[i];
+        }
         return *this;
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] m_data;
     }
 
     // returns specified array element
-    Data get(size_t index) const
+    T get(size_t index) const
     {
-        return Data(0);
+        if (index >= m_size)
+            throw std::out_of_range("Выход за пределы массива");
+        return m_data[index];
     }
 
     // sets the specified array element to the value
-    void set(size_t index, Data value)
+    void set(size_t index, T value)
     {
+        if (index >= m_size)
+            throw std::out_of_range("Выход за пределы массива");
+        m_data[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return m_size;
     }
 
 private:
-    // private data should be here
+    // private T should be here
+    size_t m_size; 
+    T *m_data;  
 };
 
 #endif
