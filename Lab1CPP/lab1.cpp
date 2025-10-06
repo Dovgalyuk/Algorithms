e2ef9deaee15f1c9b6ec20c499bcf8d3bf7f1df5
besiha <annadoberman2006@gmail.com>
2025-10-07 02:28:46 +0300

    лаба1

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -25,6 +25,7 @@ void task1(Array *arr)
     if(n!=30){
         return ;
     }
+    //a
     int firstHalf=0,secondHalf=0;
     for(int i=0;i<15;i++){
         firstHalf += array_get(arr,i);
@@ -34,22 +35,53 @@ void task1(Array *arr)
     }
 
     if(firstHalf>secondHalf){
-        cout<<"Больше осадков выпало в первую половину июня"<<endl;
+        cout<<1<<endl;
     } else if(firstHalf<secondHalf){
-        cout<<"Больше осадков выпало во вторую половину июня"<<endl;
+        cout<<2<<endl;
     } 
+    //b
+    int dec1=0, dec2=0, dec3=0;
+    for(int i=0;i<10;i++){
+        dec1 += array_get(arr,i);
+    }
+    for(int i=10;i<20;i++){
+        dec2 += array_get(arr,i);
+    }
+    for(int i=20;i<30;i++){
+        dec3 += array_get(arr,i);
+    }
+
+    if(dec1>dec2 && dec1>dec3){
+        cout<<1<<endl;
+    }else if(dec2>dec1 && dec2>dec3){
+        cout<<2<<endl;
+    }else if(dec3>dec2 && dec3>dec1){
+        cout<<3<<endl;
+    }
 
 }
 
 void task2(Array *arr)
 {
+    int n=array_size(arr);
+    for(int i=0;i<n;i++){
+        int num = array_get(arr,i);
+        bool unique = true;
+        for(int j=0;j<n;j++){
+            if(i!=j && array_get(arr,j)==num){
+            unique = false;
+            break;
+            }
+        }
+        if(unique){
+            cout<<num<<endl;
+        }
+    }
 
 }
 
 int main(int argc, char **argv)
 { 
-    setlocale(LC_ALL,"ru");
-
     Array *arr = NULL;
     std::ifstream input(argv[1]);
     arr = array_create_and_read(input);
