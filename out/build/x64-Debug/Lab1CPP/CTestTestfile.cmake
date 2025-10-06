2f44d10acbc2a2d91982d7b3956b439a3e5c1abe
Полина <125036748+polinavarfolomeeva@users.noreply.github.com>
2025-10-06 14:46:18 +0300

    Реализация лабораторной работы 1

---
+++ out/build/x64-Debug/Lab1CPP/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/Lab1CPP
+# Build directory: D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/out/build/x64-Debug/Lab1CPP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestLab1CPP "D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/out/build/x64-Debug/Lab1CPP/Lab1CPP.exe" "D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/Lab1CPP/input.txt")
+set_tests_properties(TestLab1CPP PROPERTIES  PASS_REGULAR_EXPRESSION "Factorial array: 1 1 2 6 24.*Elements appearing twice: 2 4" _BACKTRACE_TRIPLES "D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/Lab1CPP/CMakeLists.txt;21;add_test;D:/MI/Ynik/CDIA_D/Algorithms/Algorithms/Lab1CPP/CMakeLists.txt;0;")
