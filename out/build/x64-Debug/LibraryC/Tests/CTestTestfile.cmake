e32bff3c1fdc380b95357f5586306dea44434fe8
ekim0w <ekimovkirill94@gmail.com>
2025-10-08 19:43:21 +0300

    Delete temporary files

--- out/build/x64-Debug/LibraryC/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Кирилл/source/repos/ekim0w/Algorithms/LibraryC/Tests
-# Build directory: C:/Users/Кирилл/source/repos/ekim0w/Algorithms/out/build/x64-Debug/LibraryC/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
