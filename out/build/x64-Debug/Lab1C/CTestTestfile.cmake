150b5bb4a3fada72083018dd7a7c93a5aa13a868
Kirill Ekimov <ekimovkirill94@gmail.com>
2025-10-08 15:33:36 +0300

    add \n in expected output...

--- out/build/x64-Debug/Lab1C/CTestTestfile.cmake
+++ out/build/x64-Debug/Lab1C/CTestTestfile.cmake
@@ -6,6 +6,7 @@
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TestLab1C "C:/Users/Кирилл/source/repos/ekim0w/Algorithms/out/build/x64-Debug/Lab1C/Lab1C.exe" "C:/Users/Кирилл/source/repos/ekim0w/Algorithms/Lab1C/input.txt")
 set_tests_properties(TestLab1C PROPERTIES  PASS_REGULAR_EXPRESSION "Precipitation per month:
-78 0 0 0 0 0 0 0 0 0 0 0
+78 0 0 0 0 0 0 0 0 0 0 0 
 Compressed array:
-1 6 7 8 9 10 11 12 0 0 0 0" _BACKTRACE_TRIPLES "C:/Users/Кирилл/source/repos/ekim0w/Algorithms/Lab1C/CMakeLists.txt;5;add_test;C:/Users/Кирилл/source/repos/ekim0w/Algorithms/Lab1C/CMakeLists.txt;0;")
+1 6 7 8 9 10 11 12 0 0 0 0 
+" _BACKTRACE_TRIPLES "C:/Users/Кирилл/source/repos/ekim0w/Algorithms/Lab1C/CMakeLists.txt;5;add_test;C:/Users/Кирилл/source/repos/ekim0w/Algorithms/Lab1C/CMakeLists.txt;0;")
