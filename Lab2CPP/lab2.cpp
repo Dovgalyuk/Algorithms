15c4eb40c0475a2dcd2ab9ccfd56bccc2c755d5e
Баранов Даниил <danilkabaranov2104@gmail.com>
2025-10-10 23:26:31 +0300

    2_лабораторная

---
+++ Lab2CPP/lab2.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <map>
+#include<fstream>
+#include "stack.h"
+
+
+using namespace std;
+
+bool qwer(string& s, Stack* stack, map<char, int>& Arr , ifstream& file) {
+
+	int value;
+
+	if (s == "push") {
+		file >> value;
+		stack_push(stack, value);
+	}
+	else if (s == "pop") {
+		char c;
+		file >> c;
+
+		if (stack_empty(stack)) {
+			cout << "BAD POP";
+			return false;
+		}
+
+		Arr[c] = stack_get(stack);
+		stack_pop(stack);
+	}
+	else if (s == "add"){
+		if (stack_empty(stack)) {
+			cout << "BAD ADD";
+			return false;
+		}
+
+		int a = stack_get(stack);
+		stack_pop(stack);
+
+		if(stack_empty(stack)) {
+			cout << "BAD ADD";
+			return false;
+		}
+
+		int b = stack_get(stack);
+		stack_pop(stack);
+
+		stack_push(stack, a + b);
+	}
+	else if (s == "sub") {
+		if (stack_empty(stack)) {
+			cout << "BAD SUB";
+			return false;
+		}
+
+		int a = stack_get(stack);
+		stack_pop(stack);
+
+		if (stack_empty(stack)) {
+			cout << "BAD SUB";
+			return false;
+		}
+
+		int b = stack_get(stack);
+		stack_pop(stack);
+
+		stack_push(stack, a - b);
+	}
+	else if (s == "mul") {
+		if (stack_empty(stack)) {
+			cout << "BAD MUL";
+			return false;
+		}
+
+		int a = stack_get(stack);
+		stack_pop(stack);
+
+		if (stack_empty(stack)) {
+			cout << "BAD MUL";
+			return false;
+		}
+
+		int b = stack_get(stack);
+		stack_pop(stack);
+
+		stack_push(stack, a * b);
+	}
+	else if (s == "call"){
+		stack_push(stack , 9999);
+	}
+	else if (s == "ret") {
+		if (stack_empty(stack)) {
+			cout << "BAD RET";
+			return false;
+		}
+
+		int qwer = stack_get(stack);
+
+		if (qwer != 9999) {
+			cout << "BAD RET";
+			return false;
+		}
+		stack_pop(stack);
+	}
+
+	return true;
+}
+
+
+
+int main(int argc, char** argv) {
+
+	ifstream file(argv[1]);
+
+	if (!file.is_open()) {
+		return 1;
+	}
+
+	Stack* stack = stack_create();
+	map<char, int> Arr = {
+		{'A',0},{'B',0},{'C',0},{'D',0}
+	};
+	string s;
+
+	while (file >> s) {
+		if (!qwer(s, stack, Arr, file)) {
+			stack_delete(stack);
+			file.close();
+			return 0;
+		}
+	}
+	for (auto a : { 'A', 'B', 'C', 'D' }) {
+		cout << a << " = " << Arr[a] << "\n";
+	}
+	return 0;
+}
--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,10 +3,13 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* next;
 };
 
 struct List
 {
+    ListItem* head = nullptr;
 };
 
 List *list_create()
@@ -16,46 +19,73 @@ List *list_create()
 
 void list_delete(List *list)
 {
+    while (list->head) {
+        ListItem* temp = list->head;
+        list->head = list->head->next;
+        delete temp;
+    }
     // TODO: free items
     delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return list->head;;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    return item->data;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return nullptr;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    ListItem* item = new ListItem{ data, list->head };
+    list->head = item;
+    return item;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (!item) {
+        return list_insert(list, data);
+    }
+    ListItem* newItem = new ListItem{ data, item->next };
+    item->next = newItem;
+    return newItem;
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    if (!list->head) {
+        return nullptr;
+    }
+    ListItem* temp = list->head;
+    list->head = temp->next;
+    delete temp;
+    return list->head;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    if (!item) {
+        return list_erase_first(list);
+    }
+    if (!item->next) {
+        return nullptr;
+    }
+    ListItem* temp = item->next;
+    item->next = temp->next;
+    delete temp;
+    return item->next;
 }
