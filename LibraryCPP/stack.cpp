2edc2bb79053d8ef65729ce9e8d112bb4d04bf9e
Dmitry <mr.romanuza@gmail.com>
2025-10-11 23:17:36 +0300

    Update queue.cpp and stack.cpp
    Реализация стека и очереди на базе созданного ранее вектора (Тесты пройдены)

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,50 @@
 #include "stack.h"
+#include "vector.h"
 
 struct Stack
 {
+    Vector *vector;
 };
 
 Stack *stack_create()
 {
-    return new Stack;
+    Stack *stack = new Stack;
+    stack -> vector = vector_create();
+
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
     // TODO: free stack elements
-    delete stack;
+    if(stack)
+    {
+        vector_delete(stack -> vector);
+        delete stack;
+    }
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    vector_resize(stack -> vector, vector_size(stack -> vector) + 1);
+    vector_set(stack -> vector,vector_size(stack -> vector) - 1, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    size_t size = vector_size(stack -> vector);
+    
+    return vector_get(stack -> vector, size - 1);
 }
 
 void stack_pop(Stack *stack)
 {
+    size_t size = vector_size(stack -> vector);
+
+    vector_resize(stack -> vector, size - 1);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    return vector_size(stack -> vector) == 0;
 }
