9110ceb69088497be536573b740c486afa6a5884
OlesaTropnikova <olesatropnikova351@gmail.com>
2025-10-11 19:00:20 +0300

    create lab1 v2

--- out/build/x64-Debug/LibraryC/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GitHub/Algorithms/LibraryC
-# Build directory: D:/GitHub/Algorithms/out/build/x64-Debug/LibraryC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Tests")
--- out/build/x64-Debug/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GitHub/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: D:/GitHub/Algorithms/out/build/x64-Debug/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
