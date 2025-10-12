9483f0746d22da31e5beb9cf5a6dfe730d637f64
Egorverd <egorverderecsky@mail.ru>
2025-10-12 19:24:44 +0300

    fixed

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -21,9 +21,13 @@
 
     void task1(Array *arr)
     { 
+        int arrSize = array_size(arr);
+        int decade = arrSize / 3;
         int firstHalf = 0;
         int secondHalf = 0;
-        int arrSize = array_size(arr);
+        int firstDecade = 0;
+        int secondDecade = 0;
+        int thirdDecade = 0;
         for(int i = 0; i < arrSize/2; i++)
         {
             firstHalf += array_get(arr, i);
@@ -32,26 +36,6 @@
         {
             secondHalf += array_get(arr, i);
         }
-        std::cout << "In the first half of June, " <<  firstHalf <<" mm of precipitation fell.\n";
-        std::cout << "In the second half of June, " <<  secondHalf <<" mm of precipitation fell.\n";
-        if(firstHalf>secondHalf){
-            std::cout << "As a result, more precipitation fell in the first half of June.\n";
-        }
-        else if(firstHalf<secondHalf){
-            std::cout << "As a result, more precipitation fell in the second half of June.\n";
-        }
-        else{
-            std::cout << "The same amount of precipitation fell in each half of the month.\n";
-        }
-    }
-
-    void task2(Array *arr)
-    {
-        int arrSize = array_size(arr);
-        int decade = arrSize / 3;
-        int firstDecade = 0;
-        int secondDecade = 0;
-        int thirdDecade = 0;
         for(int i = 0; i < decade; i++){
             firstDecade += array_get(arr, i);
         }
@@ -61,10 +45,21 @@
         for(int i = decade*2; i < decade*3; i++){
             thirdDecade += array_get(arr, i);
         }
+        std::cout << "In the first half of June, " <<  firstHalf <<" mm of precipitation fell.\n";
+        std::cout << "In the second half of June, " <<  secondHalf <<" mm of precipitation fell.\n";
         std::cout << firstDecade << " mm of precipitation fell in the first decade of June.\n";
         std::cout << secondDecade << " mm of precipitation fell in the second decade of June.\n";
         std::cout << thirdDecade << " mm of precipitation fell in the third decade of June.\n";
-        if (firstDecade > secondDecade && firstDecade > thirdDecade)
+        if(firstHalf>secondHalf){
+            std::cout << "As a result, more precipitation fell in the first half of June.\n";
+        }
+        else if(firstHalf<secondHalf){
+            std::cout << "As a result, more precipitation fell in the second half of June.\n";
+        }
+        else{
+            std::cout << "The same amount of precipitation fell in each half of the month.\n";
+        }
+         if (firstDecade > secondDecade && firstDecade > thirdDecade)
         {
             std::cout << "The first decade of June saw the most precipitation.\n";
         }
@@ -82,6 +77,50 @@
         }
     }
 
+void task2(Array *arr)
+{
+    size_t size = array_size(arr);
+    bool found = false;
+    std::cout << "Elements that occur 2 times: ";
+    for (size_t i = 0; i < size; i++)
+    {
+        Data current = array_get(arr, i);
+        int count = 0;
+        for (size_t j = 0; j < size; j++)
+        {
+            if (array_get(arr, j) == current)
+            {
+                count++;
+            }
+        }
+        if (count == 2)
+        {
+            bool alreadyPrinted = false;
+            for (size_t k = 0; k < i; k++)
+            {
+                if (array_get(arr, k) == current)
+                {
+                    alreadyPrinted = true;
+                    break;
+                }
+            }
+            if (!alreadyPrinted)
+            {
+                if (found)
+                {
+                    std::cout << " ";
+                }
+                std::cout << current;
+                found = true;
+            }
+        }
+    }
+    if (!found)
+    {
+        std::cout << "no such elements.";
+    }
+}
+
     int main(int argc, char **argv)
     { 
         Array *arr = NULL;
@@ -89,8 +128,6 @@
         arr = array_create_and_read(input);
         task1(arr);
         array_delete(arr);
-        input.close();
-        input.open(argv[1]);
         /* Create another array here */
         arr = array_create_and_read(input);
         task2(arr);
