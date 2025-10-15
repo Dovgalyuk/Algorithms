db85c1e9c53017d1856769fb1fc68e12049c0d91
PrintsMomoe <ninakiller328@gmail.com>
2025-10-15 15:14:02 +0300

    Update task2

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
 #include "array.h"
 
 using namespace std;
@@ -40,7 +39,15 @@ void task1(Array *arr){
 void task2(Array *arr){
     if (arr == nullptr) return;
 
-    sort(begin(arr), end(arr));
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 
     for (size_t i = 0; i < n; i++) {
         if (arr[i]==arr[i++]) {
