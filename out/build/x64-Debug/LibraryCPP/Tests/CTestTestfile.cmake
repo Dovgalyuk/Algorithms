f23590e0ed610b9e84a90b3edad0f55fa225c321
IvanGrig123 <vahn60-70@mail.ru>
2025-10-16 09:01:39 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/LibraryCPP/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Vanek/source/repos/Algorithms3/LibraryCPP/Tests
-# Build directory: C:/Users/Vanek/source/repos/Algorithms3/out/build/x64-Debug/LibraryCPP/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayCPP "TestArrayCPP")
-set_tests_properties(TestArrayCPP PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Vanek/source/repos/Algorithms3/LibraryCPP/Tests/CMakeLists.txt;4;add_test;C:/Users/Vanek/source/repos/Algorithms3/LibraryCPP/Tests/CMakeLists.txt;0;")
