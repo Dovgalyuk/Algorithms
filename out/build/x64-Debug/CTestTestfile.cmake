35a7a3a81d961168cca6160684657301c77602b2
Manashka737 <mamulia737@gmail.com>
2025-10-19 20:54:41 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Пользователь/source/repos/Algorithms
-# Build directory: C:/Users/Пользователь/source/repos/Algorithms/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPPClass")
-subdirs("Lab2CPP")
