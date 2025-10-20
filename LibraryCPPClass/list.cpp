000a31e536ebbdc3fafed628b5763553a2fb00cd
ilichppshka <paturin.ilya@gmail.com>
2025-10-19 18:17:05 -0700

    implemented linked list

--- LibraryCPPClass/list.cpp
+++ LibraryCPPClass/list.cpp
@@ -1,44 +1,104 @@
 #include <cstddef>
 #include "list.h"
 
-List::List()
+List::List() : _head(nullptr), _tail(nullptr) {}
+
+List::List(const List& a) : _head(nullptr), _tail(nullptr)
 {
+    Item* last = nullptr;
+    for (Item* it = a._head; it; it = it->_next)
+    {
+        last = insert_after(last, it->_data);
+    }
 }
 
-List::List(const List &a)
+List& List::operator=(const List& a)
 {
-}
-
-List &List::operator=(const List &a)
-{
-    return *this;
+    while (_head)
+    {
+        erase_first();
+    }
+    Item* last = nullptr;
+    for (Item* it = a._head; it; it = it->_next)
+    {
+        last = insert_after(last, it->_data);
+    }
+    return*this;
 }
 
 List::~List()
 {
+    while (_head)
+    {
+        erase_first();
+    }
 }
 
-List::Item *List::first()
+List::Item* List::first()
 {
-    return nullptr;
+    return _head;
 }
 
-List::Item *List::insert(Data data)
+List::Item* List::insert(Data data)
 {
-    return nullptr;
+    return insert_after(nullptr, data);
 }
 
-List::Item *List::insert_after(Item *item, Data data)
+List::Item* List::insert_after(Item* item, Data data)
 {
-    return nullptr;
+    Item* new_item = new Item(data);
+    if (!item)
+    {
+        if (_head)
+        {
+            _head->_prev = new_item;
+            new_item->_next = _head;
+        }
+        _head = new_item;
+        if (!_tail) _tail = new_item;
+    }
+    else
+    {
+        new_item->_next = item->_next;
+        new_item->_prev = item;
+
+        if (item->_next) item->_next->_prev = new_item;
+
+        item->_next = new_item;
+        if (item == _tail) _tail = new_item;
+    }
+
+    return new_item;
 }
 
-List::Item *List::erase_first()
+List::Item* List::erase_first()
 {
-    return nullptr;
+    return erase_next(nullptr);
 }
 
-List::Item *List::erase_next(Item *item)
+List::Item* List::erase_next(Item* item)
 {
-    return nullptr;
+    Item* to_delete;
+    if (!item)
+    {
+        if (!_head) return nullptr;
+
+        to_delete = _head;
+        _head = _head->_next;
+        if (_head) _head->_prev = nullptr;
+        if (to_delete == _tail) _tail = nullptr;
+    }
+    else
+    {
+        to_delete = item->_next;
+        if (!to_delete) return nullptr;
+
+        item->_next = to_delete->_next;
+        if (to_delete->_next) to_delete->_next->_prev = item;
+        if (to_delete == _tail) _tail = item;
+    }
+
+    delete to_delete;
+
+    return item ? item->_next : _head;
 }
--- LibraryCPPClass/list.h
+++ LibraryCPPClass/list.h
@@ -12,46 +12,52 @@ public:
     class Item
     {
     public:
-        Item *next() { return nullptr; }
-        Item *prev() { return nullptr; }
+        Item* next() { return nullptr; }
+        Item* prev() { return nullptr; }
         Data data() const { return Data(); }
     private:
-        // internal data here
+        Item* _next;
+        Item* _prev;
+        Data _data;
+
+        Item(Data d) : _data(d), _next(nullptr), _prev(nullptr) {}
+        friend class List;
     };
 
     // Creates new list
     List();
 
     // copy constructor
-    List(const List &a);
+    List(const List& a);
 
     // assignment operator
-    List &operator=(const List &a);
+    List& operator=(const List& a);
 
     // Destroys the list and frees the memory
     ~List();
 
     // Retrieves the first item from the list
-    Item *first();
+    Item* first();
 
     // Inserts new list item into the beginning
-    Item *insert(Data data);
+    Item* insert(Data data);
 
     // Inserts new list item after the specified item
     // Inserts first element if item is null
-    Item *insert_after(Item *item, Data data);
+    Item* insert_after(Item* item, Data data);
 
     // Deletes the first list item.
     // Returns pointer to the item next to the deleted one.
-    Item *erase_first();
+    Item* erase_first();
 
     // Deletes the list item following the specified one.
     // Deletes the first element when item is null.
     // Returns pointer to the item next to the deleted one.
     // Should be O(1)
-    Item *erase_next(Item *item);
+    Item* erase_next(Item* item);
 private:
-    // private data should be here
+    Item* _head = nullptr;
+    Item* _tail = nullptr;
 };
 
 #endif
