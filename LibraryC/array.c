7cd5ec0acb0a3ad45ca6460d34f32dbddedfd113
Eugene <nikiforovzena53@gmail.com>
2025-10-21 18:13:26 +0300

    syntax refinement

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -11,8 +11,9 @@ typedef struct Array {
 Array *array_create(size_t size, FFree f)
 {
     Array *arr = (Array*)malloc(sizeof(Array));
-    if (arr == NULL) 
+    if (arr == NULL) {
         return NULL;
+    }
     arr->size = size;
     arr->freeFunc = f;
     arr->d = (Data*)calloc(size, sizeof(Data));
@@ -26,8 +27,9 @@ Array *array_create(size_t size, FFree f)
 // delete array, free memory
 void array_delete(Array *arr)
 {
-    if ( arr == NULL ) 
+    if ( arr == NULL ) { 
         return;
+    }
     if (arr->d == NULL) {
         free(arr);
         return;
@@ -47,8 +49,9 @@ void array_delete(Array *arr)
 // returns specified array element
 Data array_get(const Array *arr, const size_t index)
 {
-    if ( arr == NULL || arr->d == NULL || index >= arr->size) 
+    if ( arr == NULL || arr->d == NULL || index >= arr->size) {
         return (Data)0;
+    }
  
     return (arr->d[index]);
 }
@@ -56,8 +59,9 @@ Data array_get(const Array *arr, const size_t index)
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
-    if (arr == NULL || arr->d == NULL || index >= arr->size) 
+    if (arr == NULL || arr->d == NULL || index >= arr->size) {
         return;
+    }
         
     arr->d[index] = value;  
    
