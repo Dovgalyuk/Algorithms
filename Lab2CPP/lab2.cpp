d0128e37259f02dc0fc67978948c80c4582b4fc1
S0NIC-the-hedgehog <s41322126@gmail.com>
2025-10-22 18:12:07 +0300

    LAB2 (update 4)

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <fstream>
-#include <cmath> 
+#include <cmath>
 #include <string>
+#include "array.h"
 #include "stack.h"
+
+std::ifstream inputnum("inputnum");
 
 enum Command {
     cmd_none = 0,
-    cmd_add = 1, //done
-    cmd_sub = 2, //done
-    cmd_mul = 3, //done
-    cmd_div = 4, //done
-    cmd_sqrt = 5, //done
-    cmd_sq = 6, //done
-    cmd_get = 7, //done
-    cmd_peek = 8, //done
+    cmd_add = 1,
+    cmd_sub = 2,
+    cmd_mul = 3,
+    cmd_div = 4,
+    cmd_sqrt = 5,
+    cmd_sq = 6,
+    cmd_get = 7,
+    cmd_peek = 8,
     cmd_cond = 9,
     cmd_end = 10,
-    cmd_setr = 11, //half done
+    cmd_setr = 11,
     cmd_repeat = 12
 };
 
@@ -36,10 +39,13 @@ Command command_to_num(const std::string& cmd) {
     return cmd_none;
 }
 
-void read_numbers_and_commands_from_file(const char* filename, Stack* nums, Stack* cmds) {
+void read_numbers_and_commands_from_file(const char* filename, Stack* nums, Array* cmds_array, size_t& cmds_count) {
     std::ifstream file(filename);
     std::string token;
     bool reading_commands = false;
+
+    const size_t max_cmds = array_size(cmds_array);
+    cmds_count = 0;
 
     while (file >> token) {
         if (!reading_commands) {
@@ -49,34 +55,24 @@ void read_numbers_and_commands_from_file(const char* filename, Stack* nums, Stac
             }
             catch (...) {
                 reading_commands = true;
-                Command cmd_code = command_to_num(token);
-                if (cmd_code != cmd_none) {
-                    stack_push(cmds, static_cast<Data>(cmd_code));
-                }
             }
         }
-        else {
+        if (reading_commands) {
             Command cmd_code = command_to_num(token);
-            if (cmd_code != cmd_none) {
-                stack_push(cmds, static_cast<Data>(cmd_code));
+            if (cmd_code != cmd_none && cmds_count < max_cmds) {
+                array_set(cmds_array, cmds_count, static_cast<Data>(cmd_code));
+                cmds_count++;
             }
         }
     }
 }
 
-Stack* copy_stack_with_reverse(Stack* original) {
-    Stack* temp = stack_create();
-    while (!stack_empty(original)) {
-        stack_push(temp, stack_get(original));
-        stack_pop(original);
-    }
-    return temp;
-}
+void execute_commands(Array* cmds, size_t count, Stack* nums) {
+    int loop_var = 0;
+    size_t i = 0;
 
-void execute_commands(Stack* temp_cmds, Stack* nums) {
-    while (!stack_empty(temp_cmds)) {
-        Command cmd = static_cast<Command>(stack_get(temp_cmds));
-        stack_pop(temp_cmds);
+    while (i < count) {
+        Command cmd = static_cast<Command>(array_get(cmds, i));
 
         switch (cmd) {
         case cmd_peek:
@@ -84,116 +80,215 @@ void execute_commands(Stack* temp_cmds, Stack* nums) {
                 Data val = stack_get(nums);
                 std::cout << val << " ";
             }
+            ++i;
             break;
+
         case cmd_setr:
             if (!stack_empty(nums)) {
+                loop_var = stack_get(nums);
                 stack_pop(nums);
             }
+            ++i;
             break;
+
         case cmd_add:
             if (!stack_empty(nums)) {
-                int a = stack_get(nums); 
+                int a = stack_get(nums);
                 stack_pop(nums);
                 if (!stack_empty(nums)) {
-                    int b = stack_get(nums); stack_pop(nums);
+                    int b = stack_get(nums);
+                    stack_pop(nums);
                     stack_push(nums, a + b);
                 }
             }
+            ++i;
             break;
+
         case cmd_sub:
             if (!stack_empty(nums)) {
-                int a = stack_get(nums); 
+                int a = stack_get(nums);
                 stack_pop(nums);
                 if (!stack_empty(nums)) {
-                    int b = stack_get(nums); stack_pop(nums);
+                    int b = stack_get(nums);
+                    stack_pop(nums);
                     stack_push(nums, b - a);
                 }
             }
+            ++i;
             break;
+
         case cmd_mul:
             if (!stack_empty(nums)) {
-                int a = stack_get(nums); 
+                int a = stack_get(nums);
                 stack_pop(nums);
                 if (!stack_empty(nums)) {
-                    int b = stack_get(nums); stack_pop(nums);
+                    int b = stack_get(nums);
+                    stack_pop(nums);
                     stack_push(nums, a * b);
                 }
             }
+            ++i;
             break;
+
         case cmd_div:
             if (!stack_empty(nums)) {
                 int a = stack_get(nums);
                 stack_pop(nums);
                 if (!stack_empty(nums) && a != 0) {
-                    int b = stack_get(nums); stack_pop(nums);
+                    int b = stack_get(nums);
+                    stack_pop(nums);
                     stack_push(nums, b / a);
                 }
             }
+            ++i;
             break;
+
         case cmd_sqrt:
             if (!stack_empty(nums)) {
-                int a = stack_get(nums); 
+                int a = stack_get(nums);
                 stack_pop(nums);
                 stack_push(nums, (int)sqrt(a));
             }
+            ++i;
             break;
+
         case cmd_sq:
             if (!stack_empty(nums)) {
-                int a = stack_get(nums); 
+                int a = stack_get(nums);
                 stack_pop(nums);
                 stack_push(nums, a * a);
             }
+            ++i;
             break;
-        case cmd_get:
-        {
+
+        case cmd_get: {
             int val;
-            std::cout << "Enter number: ";
-            std::cin >> val;
-            stack_push(nums, val);
+            if (inputnum >> val) {
+                stack_push(nums, val);
+            }
+            else {
+                return;
+            }
+            ++i;
+            break;
         }
-        break;
+
+        case cmd_cond: {
+            if (!stack_empty(nums)) {
+                int a = stack_get(nums);
+                stack_pop(nums);
+                if (!stack_empty(nums)) {
+                    int b = stack_get(nums);
+                    stack_pop(nums);
+                    if (a == b) {
+                        ++i;
+                    }
+                    else {
+                        size_t nested = 0;
+                        do {
+                            ++i;
+                            if (i >= count) break;
+                            Command cur = static_cast<Command>(array_get(cmds, i));
+                            if (cur == cmd_cond) nested++;
+                            if (cur == cmd_end) {
+                                if (nested == 0) break;
+                                else nested--;
+                            }
+                        } while (true);
+                        ++i;
+                    }
+                }
+                else {
+                    size_t nested = 0;
+                    do {
+                        ++i;
+                        if (i >= count) break;
+                        Command cur = static_cast<Command>(array_get(cmds, i));
+                        if (cur == cmd_cond) nested++;
+                        if (cur == cmd_end) {
+                            if (nested == 0) break;
+                            else nested--;
+                        }
+                    } while (true);
+                    ++i;
+                }
+            }
+            else {
+                size_t nested = 0;
+                do {
+                    ++i;
+                    if (i >= count) break;
+                    Command cur = static_cast<Command>(array_get(cmds, i));
+                    if (cur == cmd_cond) nested++;
+                    if (cur == cmd_end) {
+                        if (nested == 0) break;
+                        else nested--;
+                    }
+                } while (true);
+                ++i;
+            }
+            break;
+        }
+
+        case cmd_end:
+            ++i;
+            break;
+
+        case cmd_repeat:
+            if (loop_var > 0) {
+                loop_var--;
+                ssize_t j = i - 1;
+                while (j >= 0) {
+                    Command c = static_cast<Command>(array_get(cmds, j));
+                    if (c == cmd_setr)
+                        break;
+                    j--;
+                }
+                if (j >= 0) {
+                    i = j + 1;
+                }
+                else {
+                    ++i;
+                }
+            }
+            else {
+                ++i;
+            }
+            break;
+
         default:
+            ++i;
             break;
         }
     }
 }
 
 int main(int argc, char* argv[]) {
-    const char* input_file = argc > 1 ? argv[1] : "input.txt";
+  
+    const char* input_files[3] = {
+        argc > 1 ? argv[1] : "input.txt",
+        argc > 2 ? argv[2] : "input2.txt",
+        argc > 3 ? argv[3] : "input3.txt"
+    };
 
-    Stack* nums = stack_create();
-    Stack* cmds = stack_create();
+    for (int t = 0; t < 3; ++t) {
+        Stack* nums = stack_create();
+        Array* cmds = array_create(64);
+        size_t cmds_count = 0;
 
-    read_numbers_and_commands_from_file(input_file, nums, cmds);
+        read_numbers_and_commands_from_file(input_files[t], nums, cmds, cmds_count);
 
-    Stack* temp_cmds = copy_stack_with_reverse(cmds);
+        execute_commands(cmds, cmds_count, nums);
 
-    execute_commands(temp_cmds, nums);
+        stack_delete(nums);
+        array_delete(cmds);
+    }
 
-    stack_delete(nums);
-    stack_delete(cmds);
-    stack_delete(temp_cmds);
-
-
-
-    const char* input_file2 = argc > 1 ? argv[1] : "input2.txt";
-
-    Stack* nums2 = stack_create();
-    Stack* cmds2 = stack_create();
-
-    read_numbers_and_commands_from_file(input_file2, nums2, cmds2);
-
-    Stack* temp_cmds2 = copy_stack_with_reverse(cmds2);
-
-    execute_commands(temp_cmds2, nums2);
-
-    stack_delete(nums2);
-    stack_delete(cmds2);
-    stack_delete(temp_cmds2);
-
-
+    inputnum.close();
     return 0;
 }
+
+
 
 
 
