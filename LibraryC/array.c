5d8a6dd79eb11791839489688579b856a7108836
IvanGrig123 <vahn60-70@mail.ru>
2025-10-24 00:39:03 +0300

    .

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdlib.h>
 #include "array.h"
 
@@ -35,41 +34,3 @@ size_t array_size(const Array *arr)
 {
     return 0;
 }
-=======
-#include <stdlib.h>
-#include "array.h"
-
-typedef struct Array {
-    // remove this
-    Data d;
-} Array;
-
-// create array
-Array *array_create(size_t size, FFree f)
-{
-    return malloc(sizeof(Array));
-}
-
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    free(arr);
-}
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
-}
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
-}
-
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
-}
->>>>>>> 1bfc6b8b4b18bf3bcbae8f6a4e143ec7da3dacea
