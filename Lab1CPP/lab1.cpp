b6afbcf195bbed114eca6879330525235c8c1f50
Fog-xWost <fogwostfox@gmail.com>
2025-10-24 19:32:10 +0300

    Fix Lab1CPP

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -1,5 +1,5 @@
-#include <stdio>
-#include <algorithm>
+#include <iostream>
+#include <fstream>
 #include "array.h"
 
 using namespace std;
@@ -7,7 +7,7 @@ using namespace std;
 template <typename T>
 Array<T> array_create_and_read(istream& input)
 {
-    int n;
+    size_t n;
     input >> n;
     /* Create array */
     Array<T> arr(n);
@@ -24,61 +24,55 @@ Array<T> array_create_and_read(istream& input)
 template <typename T>
 void task1(Array<T> &arr)
 {
+
+    T sumOneDecade = 0;
+    T sumTwoDecade = 0;
+    T sumThreeDecade = 0;
+
     for (size_t i = 0; i < 30; ++i)
     {
-        int sumOneDecade = 0;
-        int sumTwoDecade = 0;
-        int sumThreeDecade = 0;
-
         if (i < 10)
             sumOneDecade += arr.get(i);
-        if (i < 19 && i > 9)
+        if (i <= 19 && i > 9)
             sumTwoDecade += arr.get(i);
-        if (i < 29 && i > 19)
+        if (i <= 29 && i > 19)
             sumThreeDecade += arr.get(i);
     }
 
-    cout << double(sumOneDecade / 10) << endl;
-    cout << double(sumTwoDecade / 10) << endl;
-    cout << double(sumThreeDecade / 10) << endl;
+    cout << sumOneDecade / 10 << endl;
+    cout << sumTwoDecade / 10 << endl;
+    cout << sumThreeDecade / 10 << endl;
 }
 
 template <typename T>
 void task2(Array<T> &arr)
 {
-    for (size_t k = 0; k < arr.size(); ++i)
+    bool unique = true;
+
+    for (size_t i = 0; i < arr.size(); ++i)
     {
-        for (size_t i = 0; i < arr.size() - 1; i++)
+        unique = true;
+        for (size_t j = 0; j < arr.size(); ++j)
         {
-            if (arr.get(i) > arr.get(i + 1))
-                swap(arr.get(i), arr.get(i + 1));
+            if (i != j && arr.get(i) == arr.get(j))
+            {
+                unique = false;
+                break;
+            }
         }
+        if (unique)
+            cout << arr.get(i) << " ";
     }
-
-    int temp = 0;
-
-    for (size_t i = 0; i < arr.size() - 1; ++i)
-    {
-        temp = arr.get(i);
-        if (arr.get(i + 1) != arr.get(i) && arr.get(i) != temp)
-        {
-            cout << temp << " ";
-            count = 0;
-        }
-        else
-            temp = arr.get(i);
-
-    }
-
+    cout << endl;
 }
 
 int main(int argc, char **argv)
 {
     ifstream inputFile(argv[1]);
 
-    Array<int> arr = array_create_and_read(inputFile);
+    Array<double> arr = array_create_and_read<double>(inputFile);
     task1(arr);
 
-    Array<int> arr2 = array_create_and_read(inputFile);
-    task2(arr);
+    Array<int> arr2 = array_create_and_read<int>(inputFile);
+    task2(arr2);
 }
