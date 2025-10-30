35ba42f041697aba49239bb3b8f17bcc22375e0e
Hahaazaza <marknor@yandex.ru>
2025-10-31 02:31:26 +0300

    correction2

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -4,53 +4,46 @@
 #include <cstddef>
 #include <stdexcept>
 #include <algorithm>
+#include <utility>
 
 template<typename T>
 class Array {
 public:
-    explicit Array(size_t size) : size_(size) {  // конструктор
-        if (size == 0) {
-            data_ = nullptr;
-        }
-        else {
-            data_ = new T[size]();
-        }
-    }
+    explicit Array(size_t size) : size_(size), data_(size ? new T[size]() : nullptr) {}
 
-    ~Array() {  // деструктор
+    ~Array() {
         delete[] data_;
     }
 
-    Array(const Array& other) : size_(other.size_) {  // копирующий конструктор
-        if (size_ == 0) {
-            data_ = nullptr;
-        }
-        else {
-            data_ = new T[size_];
+    Array(const Array& other) : size_(other.size_), data_(size_ ? new T[size_] : nullptr) {
+        if (data_) {
             std::copy(other.data_, other.data_ + size_, data_);
         }
     }
 
-    Array& operator=(const Array& other) {  // оператор присваивания
+    Array& operator=(const Array& other) {
         if (this != &other) {
-            delete[] data_;
-            size_ = other.size_;
-            if (size_ == 0) {
-                data_ = nullptr;
-            }
-            else {
-                data_ = new T[size_];
-                std::copy(other.data_, other.data_ + size_, data_);
-            }
+            Array temp(other);
+            std::swap(data_, temp.data_);
+            std::swap(size_, temp.size_);
         }
         return *this;
     }
 
-    size_t size() const { return size_; }
+    size_t size() const noexcept { return size_; }
 
-    T& operator[](size_t index) {
+    void set(size_t index, const T& value) {
+        if (index >= size_) throw std::out_of_range("Index out of range");
+        data_[index] = value;
+    }
+
+    const T& get(size_t index) const {
         if (index >= size_) throw std::out_of_range("Index out of range");
         return data_[index];
+    }
+
+    T& operator[](size_t index) {
+        return const_cast<T&>(static_cast<const Array&>(*this)[index]);
     }
 
     const T& operator[](size_t index) const {
@@ -59,8 +52,8 @@ public:
     }
 
 private:
-    T* data_;
-    size_t size_;
+    size_t size_ = 0;
+    T* data_ = nullptr;
 };
 
 #endif // ARRAY_H
