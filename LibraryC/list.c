1d7d2e6effb43e861ca91cdb4cdbcee054b8079d
muba <muba@kali.mubinshokirov17@gmail.com>
2025-10-31 17:39:41 -0400

    laba2

--- LibraryC/list.c
+++ LibraryC/list.c
@@ -2,62 +2,149 @@
 #include "list.h"
 
 typedef struct ListItem {
-    // remove this
-    Data d;
+    Data data;
+	struct ListItem *next;
+	struct ListItem *prev;
 } ListItem;
 
 typedef struct List {
-    // remove this
-    Data d;
+    ListItem *head;
+	ListItem *tail;
+	FFree *freeFn;
 } List;
 
 List *list_create(FFree f)
 {
-    return malloc(sizeof(List));
+    List *list = (List*)malloc(sizeof(List));
+	if (!list) return NULL;
+	list->head = NULL;
+	list->tail = NULL;
+	list->freeFn = f;
+	return list;
 }
 
 void list_delete(List *list)
 {
-    // TODO: free items
-    free(list);
+    if (!list) return;
+	ListItem *cur = list->head;
+	while (cur) {
+		ListItem *next = cur->next;
+        free(cur);
+		cur = next;
+	}
+	free(list);
 }
 
-ListItem *list_first(List *list)
-{
-    return NULL;
+ListItem *list_first(List *list) {
+    if (list) {
+        return list->head;
+    } else {
+        return NULL;
+    }
 }
 
-Data list_item_data(const ListItem *item)
-{
-    return (Data)0;
+Data list_item_data(const ListItem *item) {
+    if (item) {
+        return item->data;
+    } else {
+        return (Data)0;
+    }
 }
 
-ListItem *list_item_next(ListItem *item)
-{
-    return NULL;
+ListItem *list_item_next(ListItem *item) {
+    if (item) {
+        return item->next;
+    } else {
+        return NULL;
+    }
 }
 
-ListItem *list_item_prev(ListItem *item)
-{
-    return NULL;
+ListItem *list_item_prev(ListItem *item) {
+    if (item) {
+        return item->prev;
+    } else {
+        return NULL;
+    }
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    if (!list) return NULL;
+	ListItem *node = (ListItem*)malloc(sizeof(ListItem));
+    node->data = data;
+    node->next = list->head;
+    node->prev = NULL;
+
+    if (list->head) {
+        list->head->prev = node;
+    } else {
+        list->tail = node;
+    }
+
+    list->head = node;
+    return node;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (!list) return NULL;
+	ListItem *node = (ListItem*)malloc(sizeof(ListItem));
+    node->data = data;
+    node->next = item->next;
+    node->prev = item;
+    if (item->next) {
+        item->next->prev = node;
+    } else {
+        list->tail = node;
+    }
+	item->next = node;
+
+	return node;
+
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    if (!list || !list->head) return NULL;
+    
+    ListItem* fordelete = list->head;
+    ListItem* newfirst = fordelete->next;
+
+    list->head = newfirst;
+
+    if(newfirst){
+        newfirst->prev=NULL;
+    } else {
+        list->tail = NULL;
+    }
+
+	if (list->freeFn) {
+		list->freeFn((void*)fordelete->data);
+	}
+	free(fordelete);
+    return list->head;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+	if (!list) return NULL;
+	if (item == NULL) return list_erase_first(list);
+	if (!item->next) return NULL;
+
+    ListItem* fordelete = item->next;
+    ListItem* newnext = fordelete->next;
+
+    item->next = newnext;
+
+    if (newnext) {
+        newnext->prev = item;
+    } else {
+        list->tail = item;
+    }
+
+	if (list->freeFn) {
+		list->freeFn((void*)fordelete->data);
+	}
+	free(fordelete);
+    return item->next;
 }
--- LibraryC/stack.c
+++ LibraryC/stack.c
@@ -1,36 +1,58 @@
 #include <stdlib.h>
 #include "stack.h"
+#include "list.h"
 
 typedef struct Stack {
-    // remove this
-    Data d;
+    List *list;
 } Stack;
 
 Stack *stack_create(FFree f)
 {
-    return malloc(sizeof(Stack));
+    Stack *stack = (Stack*)malloc(sizeof(Stack));
+    stack->list = list_create(f);
+    if (!stack->list) {
+        free(stack);
+        return NULL;
+    }
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    if (!stack) return;
+    
+    list_delete(stack->list);
     free(stack);
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    if(!stack) return;
+
+    list_insert(stack->list, data);
 }
 
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    if (!stack) return (Data)0;
+    if (!stack->list) return (Data)0;
+    
+    ListItem *num = list_first(stack->list);
+    if (!num) return (Data)0;
+    
+    return list_item_data(num);
 }
 
 void stack_pop(Stack *stack)
 {
+    if (!stack || !stack->list) return;
+
+    list_erase_first(stack->list);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    if (!stack) return true;
+    if (!stack->list) return true;
+    return list_first(stack->list) == NULL;
 }
