b16d684dead4dfd60375558b135e6e1ab5bafe88
besiha <annadoberman2006@gmail.com>
2025-11-04 02:54:28 +0300

    лаба2

---
+++ Lab2CPP/lab2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "stack.h"
+using namespace std;
+
+int priority_operations(char ch){
+    if(ch == '+' || ch == '-'){
+        return 1;
+    } else if(ch == '*' || ch == '/'){
+        return 2;
+    }
+    return 0;
+}
+
+void task_1(string input){
+    Stack *stack = stack_create();
+    string output;
+
+    for(size_t i = 0; i < input.size(); i++){
+        char ch = input[i];
+
+        if(ch >= 'A' && ch <= 'Z'){
+            output += ch;
+
+        } else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+            while(!stack_empty(stack) && priority_operations(stack_get(stack)) >= priority_operations(ch) && stack_get(stack) != '('){
+                output += stack_get(stack);
+                stack_pop(stack);
+            }
+            stack_push(stack, ch);
+
+        } else if(ch == '('){
+            stack_push(stack, ch);
+
+        } else if(ch == ')'){
+            while(!stack_empty(stack) && stack_get(stack) != '('){
+                output += stack_get(stack);
+                stack_pop(stack);
+            }
+            if(!stack_empty(stack) && stack_get(stack) == '('){
+                stack_pop(stack);
+            }
+        } 
+    }
+
+    while(!stack_empty(stack)){
+        output += stack_get(stack);
+        stack_pop(stack);
+    }
+    stack_delete(stack);
+    cout << output;
+}
+
+
+int main(int argc, char **argv)
+{
+    std::ifstream in(argv[1]);
+    std::string input;
+    getline(in, input);
+    task_1(input);
+    in.close();
+}
--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,59 +3,138 @@
 
 struct ListItem
 {
+    ListItem *prev;
+    Data data;
+    ListItem *next; 
 };
 
 struct List
 {
+    ListItem *head;
+    ListItem *tail;
 };
 
 List *list_create()
 {
-    return new List;
+    List *list = new List;
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
 }
 
 void list_delete(List *list)
 {
-    // TODO: free items
+    if (!list) return;
+    while(list->head != NULL){
+        list_erase_first(list);
+    }
     delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    if(!list){
+        return NULL;
+    }
+    return list->head;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    return item->data;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return item->prev;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    if(!list){
+        return NULL;
+    }
+    ListItem *newitem = new ListItem;
+    newitem->data = data;
+    newitem->next = list->head;
+    newitem->prev = NULL;
+
+    if(list->head!=NULL){
+        list->head->prev = newitem;
+    } else{
+        list->tail = newitem;
+    }
+    list->head = newitem;
+    return newitem;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (!item){
+        return list_insert(list, data); 
+    }
+
+    ListItem *newitem = new ListItem;
+    newitem->data = data;
+    newitem->prev = item;
+    newitem->next = item->next;
+
+    if (item->next != NULL) {
+        item->next->prev = newitem;
+    } else {
+        list->tail = newitem;
+    }
+    item->next = newitem;
+    return newitem;
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    if (!list || !list->head) {
+        return NULL;
+    }
+
+    ListItem *oldhead = list->head;
+    ListItem *newhead = oldhead->next;
+
+    if(newhead != NULL){
+        newhead->prev = NULL;
+    }else{
+        list->tail = NULL;
+    }
+    list->head = newhead;
+    delete oldhead;
+    return newhead;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    if (!list || !list->head){
+        return NULL;
+    }
+    if (!item){
+        return list_erase_first(list); 
+    }
+    if (!item->next){
+        return NULL;
+    }
+
+    ListItem *olditem = item->next;
+    ListItem *newitem = olditem->next;
+
+    item->next = newitem;
+    if (newitem != NULL) {
+        newitem->prev = item;
+    }else {
+        list->tail = item;
+    }
+
+    delete olditem;
+    return newitem;
 }
+
