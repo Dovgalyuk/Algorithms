1f1a7f7e0fa1ddeab4b96a5f51819b32fc57423e
BadWolfy11 <97262433+BadWolfy11@users.noreply.github.com>
2025-11-11 02:32:52 +0300

    -

---
+++ Lab1Cpp/LibraryCPP/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/LibraryCPP
+# Build directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1Cpp/LibraryCPP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
---
+++ Lab1Cpp/LibraryCPPTemplate/Tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/LibraryCPPTemplate/Tests
+# Build directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1Cpp/LibraryCPPTemplate/Tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
