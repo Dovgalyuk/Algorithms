62a7f88ff9b98c32bb9ba0cabe86695deb1062ed
BadWolfy11 <97262433+BadWolfy11@users.noreply.github.com>
2025-11-11 02:00:28 +0300

    -

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,38 +1,60 @@
-#ifndef ARRAY_H
-#define ARRAY_H
+#include "array.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <stddef.h>
-#include <stdint.h>
+// ОПРЕДЕЛЕНИЕ СТРУКТУРЫ ТОЛЬКО В C-ФАЙЛЕ
+struct Array {
+    Data* data;
+    size_t size;
+    FFree* free_fn;
+};
 
-// Non-resizeable array
-// Stores integer or pointer to custom user data
-typedef uintptr_t Data;
-// Custom function to free user pointers on delete
-typedef void (FFree)(void*);
-
-typedef struct Array Array;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// create array
-Array *array_create(size_t size, FFree f);
-
-// delete array, free memory
-void array_delete(Array *arr);
-
-// returns specified array element
-Data array_get(const Array *arr, size_t index);
-
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value);
-
-// returns array size
-size_t array_size(const Array *arr);
-
-#ifdef __cplusplus
+Array* array_create(size_t size, FFree f)
+{
+    Array* arr = (Array*)malloc(sizeof(Array));
+    if (!arr) return NULL;
+    
+    arr->data = (Data*)malloc(size * sizeof(Data));
+    if (!arr->data) {
+        free(arr);
+        return NULL;
+    }
+    
+    arr->size = size;
+    arr->free_fn = f;
+    return arr;
 }
-#endif
 
-#endif
+void array_delete(Array* arr)
+{
+    if (!arr) return;
+    
+    if (arr->free_fn) {
+        for (size_t i = 0; i < arr->size; i++) {
+            arr->free_fn((void*)arr->data[i]);
+        }
+    }
+    
+    free(arr->data);
+    free(arr);
+}
+
+Data array_get(const Array* arr, size_t index)
+{
+    if (!arr || index >= arr->size) {
+        printf("Invalid array or index\n");
+        return (Data)0;
+    }
+    return arr->data[index];
+}
+
+void array_set(Array* arr, size_t index, Data value)
+{
+    if (!arr || index >= arr->size) return;
+    arr->data[index] = value;
+}
+
+size_t array_size(const Array* arr)
+{
+    return arr ? arr->size : 0;
+}
