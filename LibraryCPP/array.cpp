5831669ba18cd57b6ac5cfdf7c8a78108666e7e3
BadWolfy11 <97262433+BadWolfy11@users.noreply.github.com>
2025-11-11 02:54:07 +0300

    -

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -1,47 +1,62 @@
 #include "array.h"
-#include <cstdlib>
+#include <cassert>
+#include <cstddef>
 
-Array* array_create(size_t size)
+struct Array
+{
+    size_t size;  
+    Data* data;
+    FFree* free_fn;  
+};
+
+// create array 
+Array *array_create(size_t size, FFree f)
 {
     Array* arr = new Array;
     arr->size = size;
     arr->data = new Data[size];
-    arr->free_fn = nullptr;
-
-    for (size_t i = 0; i < size; i++)
-        arr->data[i] = nullptr;
-
+    arr->free_fn = f;  
+    
+    for (size_t i = 0; i < size; ++i)
+        arr->data[i] = (Data)0;
     return arr;
 }
 
-void array_delete(Array* arr)
+// delete array, free memory
+void array_delete(Array *arr)
 {
     if (!arr) return;
-
-    if (arr->free_fn)
-    {
-        for (size_t i = 0; i < arr->size; i++)
-        {
-            if (arr->data[i])
-                arr->free_fn(arr->data[i]);
+    
+   
+    if (arr->free_fn) {
+        for (size_t i = 0; i < arr->size; i++) {
+            arr->free_fn((void*)arr->data[i]);
         }
     }
-
+    
     delete[] arr->data;
     delete arr;
 }
 
-Data array_get(const Array* arr, size_t index)
+// returns specified array element
+Data array_get(const Array *arr, size_t index)
 {
+    assert(arr != nullptr);
+    assert(index < arr->size);
     return arr->data[index];
 }
 
-void array_set(Array* arr, size_t index, Data value)
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value)
 {
+    assert(arr != nullptr);
+    assert(index < arr->size);
     arr->data[index] = value;
 }
 
-size_t array_size(const Array* arr)
+// returns array size
+size_t array_size(const Array *arr)
 {
+    assert(arr != nullptr);
     return arr->size;
 }
