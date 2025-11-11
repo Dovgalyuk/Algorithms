fd8d41e65a92a6af3ec6a7381baca833f2ae68ed
Blowball666 <krezhowa@gmail.com>
2025-11-11 10:21:03 +0300

    2 task

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -54,12 +54,10 @@ void vector_resize(Vector *vector, size_t size)
         size_t new_capacity = size * 2;
         Data* new_data = new Data[new_capacity];
         
-        // Копируем существующие данные
         for (size_t i = 0; i < vector->size; ++i) {
             new_data[i] = vector->data[i];
         }
         
-        // Инициализируем новые элементы нулями
         for (size_t i = vector->size; i < size; ++i) {
             new_data[i] = 0;
         }
@@ -67,19 +65,6 @@ void vector_resize(Vector *vector, size_t size)
         delete[] vector->data;
         vector->data = new_data;
         vector->capacity = new_capacity;
-    }
-    else if (size < vector->size) {
-        // Просто уменьшаем размер, память не освобождаем
-        for (size_t i = size; i < vector->size; ++i) {
-            vector->data[i] = 0;
-        }
-    }
-    else if (size > vector->size) {
-        // Инициализируем новые элементы нулями
-        for (size_t i = vector->size; i < size; ++i) {
-            vector->data[i] = 0;
-        }
-    }
-    
+    }    
     vector->size = size;
 }
