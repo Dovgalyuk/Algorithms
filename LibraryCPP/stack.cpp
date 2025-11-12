1c4cc30c3d22aeb4b951cd96d0b694f375491037
Shiku2889 <107749199+Shiku2889@users.noreply.github.com>
2025-11-13 00:19:26 +0300

    Update stack.cpp

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -1,34 +1,38 @@
-#include "stack.h"
+#include "Stack.h"
+#include "List.h"
+#include <cstdlib>
 
-struct Stack
-{
+struct Stack {
+    List* list;
 };
 
-Stack *stack_create()
-{
-    return new Stack;
+Stack* stack_create() {
+    Stack* stack = new Stack;
+    stack->list = list_create();
+    return stack;
 }
 
-void stack_delete(Stack *stack)
-{
-    // TODO: free stack elements
+void stack_delete(Stack* stack) {
+    list_delete(stack->list);
     delete stack;
 }
 
-void stack_push(Stack *stack, Data data)
-{
+void stack_push(Stack* stack, Data data) {
+    list_insert(stack->list, data);
 }
 
-Data stack_get(const Stack *stack)
-{
-    return (Data)0;
+Data stack_get(const Stack* stack) {
+    ListItem* first = list_first(stack->list);
+    if (first == nullptr) {
+        return '\0';
+    }
+    return list_item_data(first);
 }
 
-void stack_pop(Stack *stack)
-{
+void stack_pop(Stack* stack) {
+    list_erase_first(stack->list);
 }
 
-bool stack_empty(const Stack *stack)
-{
-    return true;
+bool stack_empty(const Stack* stack) {
+    return list_first(stack->list) == nullptr;
 }
