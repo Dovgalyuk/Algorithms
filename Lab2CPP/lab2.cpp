7b3f54bc29eae45b9b540e144392b9d6b5495c1f
Egorverd <egorverderecsky@mail.ru>
2025-11-12 14:05:20 +0300

    No stack for analyzing string

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -4,80 +4,43 @@
     #include "vector.h"
     #include "stack.h"
 
-    Stack *stack_create_and_read(std::ifstream &input)
-    {
-        std::string line;
-        std::getline(input, line);
-
-        Stack* stack = stack_create();
-        for(char c : line) {
-            if (c == '(' || c == ')' || c == '\"' || c == '\'') 
-            {
-                stack_push(stack, c);
-            }
-        }
-        return stack;
-    }
-
-    bool check(Stack* stack) {
-        Stack* new_stack = stack_create();
-        bool flag = true;
-        Stack* temp_stack = stack_create();
-        while (!stack_empty(stack)) {
-            char c = stack_get(stack);
-            stack_pop(stack);
-            stack_push(temp_stack, c);
-        }
-        while (!stack_empty(temp_stack)) {
-            char c = stack_get(temp_stack);
-            stack_pop(temp_stack);
-            stack_push(stack, c);
+    bool check(const std::string& line) {
+        int round_brackets = 0;
+        bool double_quotes = false;
+        bool single_quotes = false;
+        
+        for (char c : line) {
             if (c == '(') {
-                stack_push(new_stack, '(');
+                round_brackets++;
             }
             else if (c == ')') {
-                if (stack_empty(new_stack) || stack_get(new_stack) != '(') {
-                    flag = false;
-                    break;
+                if (round_brackets <= 0) {
+                    return false;
                 }
-                stack_pop(new_stack);
+                round_brackets--;
             }
             else if (c == '\"') {
-                if (!stack_empty(new_stack) && stack_get(new_stack) == '\"') {
-                    stack_pop(new_stack);
-                } else {
-                    stack_push(new_stack, '\"');
-                }
+                double_quotes = !double_quotes;
             }
             else if (c == '\'') {
-                if (!stack_empty(new_stack) && stack_get(new_stack) == '\'') {
-                    stack_pop(new_stack);
-                } else {
-                    stack_push(new_stack, '\'');
-                }
+                single_quotes = !single_quotes;
             }
         }
-        if (!stack_empty(new_stack)) {
-            flag = false;
-        }
-        stack_delete(new_stack);
-        stack_delete(temp_stack);
-        return flag;
+        return (round_brackets == 0) && !double_quotes && !single_quotes;
     }
 
     int main(int argc, char **argv)
     { 
-        Stack* stack = NULL;
         std::ifstream input(argv[1]);
-        stack = stack_create_and_read(input);
-        if(check(stack)){
+        std::string line;
+        std::getline(input, line);
+        if(check(line)){
             std::cout << "YES";
         }
         else
         {
             std::cout << "NO";
         }
-        stack_delete(stack);
         input.close();
         
     }
