8bcf85e5a10ac7e2efb2b8781d4e8f70984453a1
BadWolfy11 <97262433+BadWolfy11@users.noreply.github.com>
2025-11-13 00:30:45 +0300

    -

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -4,36 +4,22 @@
 Array *array_create_and_read(FILE *input)
 {
     int n;
-    if(fscanf(input, "%d", &n) != 1)
-    {
-        printf("Не удалось прочитать число\n");
-        return NULL;
-        
-    }
-    else
-    {
-        Array *arr = array_create(n, NULL);
-        /* Read array data */
-        for (int i = 0 ; i < n ; ++i)
-        {
-            int x;
-            fscanf(input, "%d", &x);
-            array_set(arr, i, x);
-        }
-        return arr;
-    }
-    
+    fscanf(input, "%d", &n);
     /* Create array */
-   
+    Array *arr = array_create(n, NULL);
+    /* Read array data */
+    for (int i = 0 ; i < n ; ++i)
+    {
+        int x;
+        fscanf(input, "%d", &x);
+        array_set(arr, i, x);
+    }
+    return arr;
 }
 
 void task1(Array *arr)
 {
-    for (size_t i = 0; i < array_size(arr); i++) {
-            printf("%d ", (int)array_get(arr, i));
-        }
-        printf("\n");
-    }
+}
 
 void task2(Array *arr)
 {
@@ -43,11 +29,6 @@ int main(int argc, char **argv)
 {
     Array *arr = NULL;
     FILE *input = fopen(argv[1], "r");
-    if(!input)
-    {
-        printf("Случилась ошибка при открытии файла");
-        return 1;
-    }
     arr = array_create_and_read(input);
     task1(arr);
     array_delete(arr);
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,60 +1,36 @@
-#include "array.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "array.h"
 
-// ОПРЕДЕЛЕНИЕ СТРУКТУРЫ ТОЛЬКО В C-ФАЙЛЕ
-struct Array {
-    Data* data;
-    size_t size;
-    FFree* free_fn;
-};
+typedef struct Array {
+    // remove this
+    Data d;
+} Array;
 
-Array* array_create(size_t size, FFree f)
+// create array
+Array *array_create(size_t size, FFree f)
 {
-    Array* arr = (Array*)malloc(sizeof(Array));
-    if (!arr) return NULL;
-    
-    arr->data = (Data*)malloc(size * sizeof(Data));
-    if (!arr->data) {
-        free(arr);
-        return NULL;
-    }
-    
-    arr->size = size;
-    arr->free_fn = f;
-    return arr;
+    return malloc(sizeof(Array));
 }
 
-void array_delete(Array* arr)
+// delete array, free memory
+void array_delete(Array *arr)
 {
-    if (!arr) return;
-    
-    if (arr->free_fn) {
-        for (size_t i = 0; i < arr->size; i++) {
-            arr->free_fn((void*)arr->data[i]);
-        }
-    }
-    
-    free(arr->data);
     free(arr);
 }
 
-Data array_get(const Array* arr, size_t index)
+// returns specified array element
+Data array_get(const Array *arr, size_t index)
 {
-    if (!arr || index >= arr->size) {
-        printf("Invalid array or index\n");
-        return (Data)0;
-    }
-    return arr->data[index];
+    return (Data)0;
 }
 
-void array_set(Array* arr, size_t index, Data value)
+// sets the specified array element to the value
+void array_set(Array *arr, size_t index, Data value)
 {
-    if (!arr || index >= arr->size) return;
-    arr->data[index] = value;
 }
 
-size_t array_size(const Array* arr)
+// returns array size
+size_t array_size(const Array *arr)
 {
-    return arr ? arr->size : 0;
+    return 0;
 }
