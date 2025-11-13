ba8a3336c3a83e0044c28a92ca7478c2638ca892
AlinaZhukova27 <evgeni1979cerge@gmail.com>
2025-11-13 11:01:20 +0300

    Lab4

--- LibraryCPPTemplate/Tests/graph.cpp
+++ LibraryCPPTemplate/Tests/graph.cpp
@@ -1,14 +1,37 @@
 #include "graph.h"
 #include <iostream>
 using namespace std;
+
 int main() {
-    Graph< string, int> graph(3);
+    Graph<string, int> graph(3);
     graph.setVertexLabel(0, "A");
+    graph.setVertexLabel(1, "B");
+    graph.setVertexLabel(2, "C");
+
     graph.addEdge(0, 1, 5);
     graph.addEdge(1, 2, 3);
 
-    if (graph.getVertexCount() == 3 && graph.hasEdge(0, 1)) {
-         cout << "Basic functionality: OK" <<  endl;
+    bool allTestsPassed = true;
+
+    if (graph.getVertexCount() != 3) allTestsPassed = false;
+    if (!graph.hasEdge(0, 1)) allTestsPassed = false;
+    if (graph.getEdgeLabel(0, 1) != 5) allTestsPassed = false;
+    if (graph.hasEdge(1, 0)) allTestsPassed = false;
+    if (graph.getVertexLabel(0) != "A") allTestsPassed = false;
+
+    auto it = graph.getIterator(0);
+    int neighborCount = 0;
+    while (it.hasNext()) {
+        it.next();
+        neighborCount++;
+    }
+    if (neighborCount != 1) allTestsPassed = false;
+
+    if (allTestsPassed) {
+        cout << "All graph tests passed!" << endl;
+    }
+    else {
+        cout << "Some tests failed!" << endl;
     }
 
     return 0;
