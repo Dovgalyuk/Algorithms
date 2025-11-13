c95c9ba8ae88e47ef26a665eb284ad7de805e5d9
akixurx <duma881luch@gmail.com>
2025-11-13 22:10:42 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -6,7 +6,6 @@ struct Queue
 {
     Vector* data;
     size_t head;
-    size_t tail;
     size_t count;
 };
 
@@ -16,11 +15,9 @@ Queue* queue_create()
     queue->data = vector_create();
     vector_resize(queue->data, 4);
     queue->head = 0;
-    queue->tail = 0;
     queue->count = 0;
     return queue;
 }
-
 void queue_delete(Queue* queue)
 {
     if (queue) {
@@ -28,30 +25,29 @@ void queue_delete(Queue* queue)
         delete queue;
     }
 }
-
 void queue_insert(Queue* queue, Data data)
 {
     if (!queue) return;
+
     if (queue->count >= vector_size(queue->data)) {
         size_t old_size = vector_size(queue->data);
         size_t new_size = old_size * 2;
+        Data* temp = new Data[queue->count];
 
-        Vector* new_vector = vector_create();
-        vector_resize(new_vector, new_size);
-        for (size_t i = 0; i < old_size - queue->head; i++) {
-            vector_set(new_vector, i, vector_get(queue->data, queue->head + i));
+        for (size_t i = 0; i < queue->count; i++) {
+            size_t index = (queue->head + i) % old_size;
+            temp[i] = vector_get(queue->data, index);
         }
-        for (size_t i = 0; i < queue->tail; i++) {
-            vector_set(new_vector, old_size - queue->head + i, vector_get(queue->data, i));
+        vector_resize(queue->data, new_size);
+        for (size_t i = 0; i < queue->count; i++) {
+            vector_set(queue->data, i, temp[i]);
         }
-        vector_delete(queue->data);
-        queue->data = new_vector;
+
+        delete[] temp;
         queue->head = 0;
-        queue->tail = old_size;
     }
-
-    vector_set(queue->data, queue->tail, data);
-    queue->tail = (queue->tail + 1) % vector_size(queue->data);
+    size_t tail = (queue->head + queue->count) % vector_size(queue->data);
+    vector_set(queue->data, tail, data);
     queue->count++;
 }
 
@@ -71,7 +67,6 @@ void queue_remove(Queue* queue)
     queue->head = (queue->head + 1) % vector_size(queue->data);
     queue->count--;
 }
-    
 bool queue_empty(const Queue* queue)
 {
     return !queue || queue->count == 0;
