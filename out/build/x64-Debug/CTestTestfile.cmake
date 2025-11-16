c80d61c7ba16e75f3874a262ca2ff57c4fa479d1
PrintsMomoe <ninakiller328@gmail.com>
2025-11-16 20:20:41 +0300

    3

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Momoe/Desktop/labD/2/Algorithms
-# Build directory: C:/Users/Momoe/Desktop/labD/2/Algorithms/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPP")
-subdirs("Lab2CPP")
