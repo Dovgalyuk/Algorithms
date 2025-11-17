8f645ae8e68f234582dad78f0a6a85b26e6e6f86
Kulebyak291 <oleg_pavlov291999@mail.com>
2025-11-17 04:47:04 +0300

    776

--- LibraryCPP/stack.cpp
+++ LibraryCPP/stack.cpp
@@ -30,9 +30,8 @@ void stack_pop(Stack *stack) {
 
 bool stack_empty(const Stack *stack) {
     return vector_size(stack->vec) == 0;
-
+}
     // Реализация stack_size
 size_t stack_size(const Stack *stack) {
     return vector_size(stack->vec);
-}
 }
--- LibraryCPP/stack.h
+++ LibraryCPP/stack.h
@@ -1,6 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
-
+#include <cstddef>
 // Stack
 
 // Stores integer values inside
