1726aa49e561052246debd6dd39f5218d849c8e1
creezed <glamber3@mail.ru>
2025-11-17 09:28:39 +0300

    feat(LibraryCPPClass): rewritten to singly linked list

--- LibraryCPPClass/list.cpp
+++ LibraryCPPClass/list.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include "list.h"
 
 List::List() = default;
@@ -39,13 +38,12 @@ List::Item *List::insert(Data data)
 {
     Item *new_item = new Item(data);
     new_item->_next = _head;
+    _head = new_item;
     
-    if (_head) {
-        _head->_prev = new_item;
-    } else {
+    if (!_tail) {
         _tail = new_item;
     }
-    _head = new_item;
+    
     return new_item;
 }
 
@@ -54,11 +52,11 @@ List::Item *List::insert_end(Data data) {
     
     if (_tail) {
         _tail->_next = new_item;
-        new_item->_prev = _tail;
         _tail = new_item;
     } else {
         _head = _tail = new_item;
     }
+    
     return new_item;
 }
 
@@ -67,37 +65,39 @@ List::Item *List::insert_after(Item *item, Data data)
     if (!item) {
         return insert(data);
     }
-
+    
     Item *new_item = new Item(data);
     new_item->_next = item->_next;
-    new_item->_prev = item;
+    item->_next = new_item;
     
-    if (item->_next) {
-        item->_next->_prev = new_item;
-    } else {
+    if (item == _tail) {
         _tail = new_item;
     }
-    item->_next = new_item;
+    
     return new_item;
 }
 
 List::Item *List::erase(Item *item) {
     if (!item) return nullptr;
     
+    if (item == _head) {
+        return erase_first();
+    }
+    
+    Item* prev = _head;
+    while (prev && prev->_next != item) {
+        prev = prev->_next;
+    }
+    
+    if (!prev) return nullptr;
+    
+    prev->_next = item->_next;
+    
+    if (item == _tail) {
+        _tail = prev;
+    }
+    
     Item* result = item->_next;
-    
-    if (item->_prev) {
-        item->_prev->_next = item->_next;
-    } else {
-        _head = item->_next;
-    }
-    
-    if (item->_next) {
-        item->_next->_prev = item->_prev;
-    } else {
-        _tail = item->_prev;
-    }
-    
     delete item;
     return result;
 }
@@ -111,9 +111,31 @@ List::Item *List::erase_next(Item *item)
 {
     if (item == nullptr)
     {
-        return erase(_head);
+        if (!_head) return nullptr;
+        
+        Item* to_delete = _head;
+        _head = _head->_next;
+        
+        if (!_head) {
+            _tail = nullptr;
+        }
+        
+        Item* result = _head;
+        delete to_delete;
+        return result;
     } else {
-        return erase(item -> _next);
+        if (!item->_next) return nullptr;
+        
+        Item* to_delete = item->_next;
+        item->_next = to_delete->_next;
+        
+        if (to_delete == _tail) {
+            _tail = item;
+        }
+        
+        Item* result = item->_next;
+        delete to_delete;
+        return result;
     }
 }
 
@@ -122,24 +144,22 @@ void List::copy(const List &a) {
         _head = _tail = nullptr;
         return;
     }
-
+    
     _head = new Item(a._head->_data);
     Item *current = _head;
     Item *src = a._head->_next;
-
+    
     while (src) {
         current->_next = new Item(src->_data);
-        current->_next->_prev = current;
         current = current->_next;
         src = src->_next;
     }
-
+    
     _tail = current;
 }
 
 void List::clear() {
-    while (_head)
-    {
+    while (_head) {
         erase_first();
-    } 
+    }
 }
--- LibraryCPPClass/list.h
+++ LibraryCPPClass/list.h
@@ -1,8 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <cstddef>
-
 // Change it to desired type
 typedef int Data;
 
@@ -13,48 +11,46 @@ public:
     {
     public:
         Item *next() { return _next; }
-        Item *prev() { return _prev; }
         Data data() const { return _data; }
+        
     private:
         Item* _next;
-        Item* _prev;
         Data _data;
-
-        Item(Data d): _next(nullptr), _prev(nullptr), _data(d) {}
+        Item(Data d): _next(nullptr), _data(d) {}
         friend class List;
     };
 
     // Creates new list
     List();
-
+    
     // copy constructor
     List(const List &a);
-
+    
     // assignment operator
     List &operator=(const List &a);
-
+    
     // Destroys the list and frees the memory
     ~List();
 
     void clear();
-
     void copy(const List &a);
 
     // Retrieves the first item from the list
     Item *first();
-
     Item *last();
-    const Item *last() const;  
+    const Item *last() const;
 
     // Inserts new list item into the beginning
     Item *insert(Data data);
-
     Item *insert_end(Data data);
 
     // Inserts new list item after the specified item
     // Inserts first element if item is null
     Item *insert_after(Item *item, Data data);
 
+    // Erases the specified item
+    // Returns pointer to the item next to the deleted one
+    // O(n) complexity for singly-linked list
     Item *erase(Item *item);
 
     // Deletes the first list item.
@@ -64,8 +60,9 @@ public:
     // Deletes the list item following the specified one.
     // Deletes the first element when item is null.
     // Returns pointer to the item next to the deleted one.
-    // Should be O(1)
+    // O(1)
     Item *erase_next(Item *item);
+
 private:
     Item* _head = nullptr;
     Item* _tail = nullptr;
