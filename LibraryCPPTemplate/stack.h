7756c6df3665a34d59c7543ae3080f7266c3885b
Fog-xWost <fogwostfox@gmail.com>
2025-11-19 03:14:35 +0300

    Add realisation stack template cpp

--- LibraryCPPTemplate/stack.h
+++ LibraryCPPTemplate/stack.h
@@ -1,41 +1,54 @@
 #ifndef STACK_TEMPLATE_H
 #define STACK_TEMPLATE_H
 
+#include "vector.h"
+
 template <typename Data> class Stack
 {
 public:
     // Creates empty stack
     Stack()
     {
+        // Вектор сам создает контейнер по дефолту
     }
 
     // copy constructor
     Stack(const Stack &a)
     {
-        // implement or disable this function
+        mContainer = a.mContainer;
     }
 
     // assignment operator
     Stack &operator=(const Stack &a)
     {
-        // implement or disable this function
+        if (this != &a)
+            mContainer = a.mContainer;
         return *this;
     }
 
     // Deletes the stack
     ~Stack()
     {
+        // Деструктор использующегося вектора сам очищает память
     }
 
     // Pushes data on top of the stack
     // Should be O(1) on average
     void push(Data data)
     {
+        size_t currentSize = mContainer.size();
+        mContainer.resize(currentSize + 1);
+
+        mContainer.set(currentSize, data);
     }
 
     // Retrieves the last element from the stack
     Data get() const
     {
+        size_t currentSize = mContainer.size();
+        if (currentSize > 0)
+            return mContainer.get(currentSize - 1);
+
         return Data();
     }
 
@@ -43,16 +56,19 @@ public:
     // Should be O(1)
     void pop()
     {
+        size_t currentSize = mContainer.size();
+        if (currentSize > 0)
+           mContainer.resize(currentSize - 1);
     }
 
     // Returns true if the stack is empty
     bool empty() const
     {
-        return true;
+        return mContainer.size() == 0;
     }
 
 private:
-    // private data should be here
+    Vector<Data> mContainer;
 };
 
 #endif
