ec7ff76574c1747e2dc3f3bce72b8e734e6ec8ce
besiha <annadoberman2006@gmail.com>
2025-11-19 17:44:28 +0300

    лаба3_исправления

--- Lab3CPP/laba3.cpp
+++ Lab3CPP/laba3.cpp
@@ -5,11 +5,11 @@
 #include "vector.h"
 using namespace std;
 
-void bfs(int start, Vector* adjacency_matrix, int n, Vector* visited, Vector* component) {
+void bfs(int start, Vector* adjacency_matrix, int n, Vector* visited, int component_id, Vector* component_ids) {
     Queue* queue = queue_create();
     
     vector_set(visited, start, 1);
-    vector_set(component, start, 1);
+    vector_set(component_ids, start, component_id);
     queue_insert(queue, start);
     
     while (!queue_empty(queue)) {
@@ -20,16 +20,15 @@ void bfs(int start, Vector* adjacency_matrix, int n, Vector* visited, Vector* co
             int is_adjacent = vector_get(adjacency_matrix, current * n + j);
             if (is_adjacent && !vector_get(visited, j)) {
                 vector_set(visited, j, 1);
-                vector_set(component, j, 1);
+                vector_set(component_ids, j, component_id);
                 queue_insert(queue, j);
             }
         }
-    }
-    
+    }    
     queue_delete(queue);
 }
 
-void task(std::ifstream& in) {
+void task(ifstream& in) {
     int n;
     in >> n;
 
@@ -46,49 +45,38 @@ void task(std::ifstream& in) {
     
     Vector* visited = vector_create();
     vector_resize(visited, n);
+
+    Vector* component_ids = vector_create();
+    vector_resize(component_ids, n);
    
     int component_count = 0;
-
-    Vector* temp_visited = vector_create();
-    vector_resize(temp_visited, n);  
+ 
     for (int i = 0; i < n; i++) {
-        if (!vector_get(temp_visited, i)) {
-            Vector* temp_component = vector_create();
-            vector_resize(temp_component, n);
-            bfs(i, adjacency_matrix, n, temp_visited, temp_component);
-            vector_delete(temp_component);
+        if (!vector_get(visited, i)) {
+            bfs(i, adjacency_matrix, n, visited, component_count + 1, component_ids);
             component_count++;
         }
     }
     cout << component_count << endl;
-    vector_delete(temp_visited);
 
-    for (int i = 0; i < n; i++) {
-        if (!vector_get(visited, i)) {
-
-            Vector* component = vector_create();
-            vector_resize(component, n);
-
-            bfs(i, adjacency_matrix, n, visited, component);
-
-            bool first = true;
-            for (int j = 0; j < n; j++) {
-                if (vector_get(component, j)) {
-                    if (!first) cout << " ";
-                    cout << (j + 1);
-                    first = false;
-                }
+    for (int comp_id = 1; comp_id <= component_count; comp_id++) {
+        bool first = true;
+        for (int j = 0; j < n; j++) {
+            if (vector_get(component_ids, j) == comp_id) {
+                if (!first) cout << " ";
+                cout << (j + 1);
+                first = false;
             }
-            cout << endl;
-            vector_delete(component);          
         }
+        cout << endl;          
     }
     vector_delete(adjacency_matrix);
     vector_delete(visited);
+    vector_delete(component_ids);
 }
 
 int main(int argc, char **argv) {
-    std::ifstream in(argv[1]);
+    ifstream in(argv[1]);
     task(in);
     in.close();
 }
