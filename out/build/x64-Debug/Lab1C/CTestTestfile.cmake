7a82b44ce939bab36eb3ea82c1fa98b66a39eeda
BadWolfy11 <97262433+BadWolfy11@users.noreply.github.com>
2025-11-20 23:02:49 +0300

    clean

--- out/build/x64-Debug/Lab1C/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1C
-# Build directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/out/build/x64-Debug/Lab1C
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestLab1C "C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/out/build/x64-Debug/Lab1C/Lab1C.exe" "C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1C/input.txt")
-set_tests_properties(TestLab1C PROPERTIES  PASS_REGULAR_EXPRESSION "1 2 3 4 5" _BACKTRACE_TRIPLES "C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1C/CMakeLists.txt;5;add_test;C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/Lab1C/CMakeLists.txt;0;")
--- out/build/x64-Debug/LibraryCPP/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/LibraryCPP/Tests
-# Build directory: C:/Users/badvo/OneDrive/Документы/GitHub/Algorithms/out/build/x64-Debug/LibraryCPP/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
