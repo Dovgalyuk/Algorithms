aa99a425ef1fff5a889034153db5af01b3c3643e
Fog-xWost <fogwostfox@gmail.com>
2025-11-20 03:08:39 +0300

    Add task realisation without output.txt

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -13,10 +13,50 @@ bool isPair(char open, char close) {
 }
 
 bool isOpening(char c) {
-    return c == '('  c == '['  c == '{' || c == '<';
+    return c == '(' || c == '[' || c == '{' || c == '<';
+}
+
+void task(string &input) {
+    Stack<char> stack;
+    bool isSuccess = true;
+
+    for (size_t i = 0; i < input.length(); i++) {
+        char current = input[i];
+
+        if (isOpening(current))
+            stack.push(current);
+        else {
+            if (stack.empty()) {
+                isSuccess = false;
+                break;
+            }
+
+            char top = stack.get();
+
+            if (isPair(top, current))
+                stack.pop();
+            else {
+                isSuccess = false;
+                break;
+            }
+        }
+    }
+
+    if (isSuccess && stack.empty()) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 }
 
 int main(int argc, char **argv) {
+
+    ifstream inputFile(argv[1]);
+
+    string line;
+    getline(inputFile, line);
+
+    task(line);
 
     return 0;
 }
