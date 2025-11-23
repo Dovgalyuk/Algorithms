d8869fe36840fda5b62c02928ad657cae9dea5a4
Dehveyn <evseenko-daniil@yandex.ru>
2025-11-24 01:50:51 +0300

    add ReadStringFromFile and AnnoyingScriptInterpreter

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -2,11 +2,27 @@
 #include <iostream>
 #include <unordered_map>
 #include "list.h"
-#include "stack.h"
+#include "LibraryCPPClass/stack.h"
 
 
 using namespace std;
 
+string ReadStringFromFile(const string& fileName){
+    ifstream file(fileName);
+    if (!file.is_open())
+    {
+        cerr << "Error reading the file" << endl;
+        return;
+    }
+    
+    string s;
+
+    file >> s;
+
+    file.close();
+
+    return s;
+}
 
 void TestList(int element_count)
 {
@@ -31,18 +47,7 @@ void TestList(int element_count)
 
 void TestStack(const string& fileName)
 {
-    ifstream file(fileName);
-    if (!file.is_open())
-    {
-        cerr << "Error reading the file" << endl;
-        return;
-    }
-    
-    string s;
-
-    file >> s;
-
-    file.close();
+    string s = ReadStringFromFile(fileName);
 
     Stack st; 
     unordered_map<char, char> pairs = {
@@ -78,16 +83,109 @@ void TestStack(const string& fileName)
     }
 }
 
+void AnnoyingScriptInterpreter(const string& fileNameScript, const string& fileNameInput)
+{
+    string script = ReadStringFromFile(fileNameScript);
+    string input = ReadStringFromFile(fileNameInput);
+
+    size_t inputIndex = 0;
+    Stack stack;
+
+    for(size_t i = 0; i< script.length(); i++)
+    {
+        char command = script[i];
+
+        switch (command)
+        {
+        case '+':
+        {
+            i++;
+            string str;
+
+            while (i < script.length())
+            {
+                char c = script[i];
+                if (c == '+' || c == '-' || c == '<' || c == '>' || c == '?' || c == '!')
+                {
+                    i--;
+                    break;
+                }
+                str += c;
+                i++;
+            }
+
+            for (int j = str.length()-1; j >= 0; j--)
+            {
+                stack.push(str[j]);
+            }            
+            break;
+        }
+        case '-':
+        {
+            if (!stack.empty())
+            {
+                stack.pop();
+            }
+            break;
+        }
+        case '<':
+        {
+            if(!stack.empty())
+            {
+                //char top = stack.get();
+                //stack.pop();
+                Stack temp;
+                while (!stack.empty())
+                {
+                    temp.push(stack.get());
+                    stack.pop();
+                }
+               // stack.push(top);
+                while (!temp.empty())
+                {
+                    stack.push(temp.get());
+                    temp.pop();
+                }
+                
+            }
+            break;
+        }
+        case '>':
+        {
+            if(!stack.empty())
+            {
+                Stack temp;
+                while (!stack.empty())
+                {
+                    temp.push(stack.get());
+                    stack.pop();
+                }
+                while (!temp.empty())
+                {
+                    stack.push(temp.get());
+                    temp.pop();
+                }
+                
+            }
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+}
+
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
         cout << "Help: "<<argv[0]<<"\n -list <5> -- test list\n"
-                                   " -stack <fileName> -- test stack"<<endl;
+                                   " -stack <fileName> -- test stack\n"
+                                   " -inter <scriptFile> <inputFile>"<<endl;
         
         return 1;
     }
-    //TestStack("D:\\Программирование\\с++\\ALab\\Lab2CPP\\test.txt");
     
     string mode = argv[1];
 
@@ -98,6 +196,10 @@ int main(int argc, char* argv[])
     else if (mode == "-stack")
     {
         TestStack(argv[2]);
+    }
+    else if (mode == "-inter")
+    {
+        AnnoyingScriptInterpreter(argv[2], argv[3]);
     }
     else
     {
