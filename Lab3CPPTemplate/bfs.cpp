4c13b631404eb96e2409a129e00933c627971e64
Prodix <prodixfg@gmail.com>
2025-11-24 23:40:06 +0300

    fix

--- Lab3CPPTemplate/bfs.cpp
+++ Lab3CPPTemplate/bfs.cpp
@@ -4,13 +4,13 @@
 
 #include "queue.h"
 
-void bfs(const std::vector<std::vector<int>>& graph, int source) {
+typedef std::vector<std::vector<int>> Graph;
+
+void bfs(const Graph& graph, int source) {
     size_t n = graph.size();
     std::vector<int> d(graph.size(), -1);
-    std::vector<bool> visited(graph.size(), false);
     
     d[source] = 0;
-    visited[source] = true;
 
     Queue<int> queue;
     queue.insert(source);
@@ -19,10 +19,9 @@ void bfs(const std::vector<std::vector<int>>& graph, int source) {
         int v = queue.get();
         queue.remove();
         for (size_t e = 0; e < n; e++) {
-            if (graph[v][e] != 0 && !visited[e]) {
+            if (graph[v][e] != 0 && d[e] == -1) {
                 queue.insert(e);
                 d[e] = d[v] + 1;
-                visited[e] = true;
             }
         }
     }
@@ -43,7 +42,7 @@ int main(int argc, char** argv) {
     int v;
     inputFile >> v;
 
-    std::vector<std::vector<int>> matrix(v, std::vector<int>(v));
+    Graph matrix(v, std::vector<int>(v));
 
     for (int i = 0; i < v; i++) {
         for (int j = 0; j < v; j++) {
