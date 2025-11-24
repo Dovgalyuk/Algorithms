dd2d07121d85eb3a54690709c0d90985978b4edb
qComanche <ef.artem2019@gmail.com>
2025-11-24 21:26:42 +0300

    Исправлена реализация очереди в queue.cpp; Добавлен тест на IMPOSSIBLE

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -5,7 +5,6 @@ struct Queue
 {
     Vector* buf;
     size_t head;
-    size_t tail;
     size_t count;
 };
 
@@ -13,51 +12,66 @@ Queue* queue_create()
 {
     Queue* q = new Queue;
     q->buf = vector_create();
+
     vector_resize(q->buf, 4);
+
     q->head = 0;
-    q->tail = 0;
     q->count = 0;
     return q;
 }
 
-void queue_delete(Queue* queue) 
+void queue_delete(Queue* queue)
 {
+    if (!queue)
+        return;
+
     vector_delete(queue->buf);
     delete queue;
 }
 
 void queue_insert(Queue* queue, Data data)
 {
-    if (queue->count == vector_size(queue->buf))
+    size_t capacity = vector_size(queue->buf);
+
+    if (queue->count == capacity)
     {
-        size_t oldcap = vector_size(queue->buf);
-        vector_resize(queue->buf, oldcap * 2);
+        size_t old_capacity = capacity;
+
+        Vector* new_buf = vector_create();
+        vector_resize(new_buf, old_capacity * 2);
 
         for (size_t i = 0; i < queue->count; i++)
         {
-            vector_set(queue->buf, i, vector_get(queue->buf, (queue->head + i) % oldcap));
+            size_t idx = (queue->head + i) % old_capacity;
+            vector_set(new_buf, i, vector_get(queue->buf, idx));
         }
+
+        vector_delete(queue->buf);
+        queue->buf = new_buf;
         queue->head = 0;
-        queue->tail = queue->count;
+        capacity = vector_size(queue->buf);
     }
 
-    vector_set(queue->buf, queue->tail, data);
-    queue->tail = (queue->tail + 1) % vector_size(queue->buf);
-    queue->count++;
+    size_t tail = (queue->head + queue->count) % capacity;
+    vector_set(queue->buf, tail, data);
+    ++queue->count;
 }
 
-Data queue_get(const Queue* queue) 
+Data queue_get(const Queue* queue)
 {
     return vector_get(queue->buf, queue->head);
 }
 
 void queue_remove(Queue* queue)
 {
+    if (queue->count == 0)
+        return;
+
     queue->head = (queue->head + 1) % vector_size(queue->buf);
-    queue->count--;
+    --queue->count;
 }
 
-bool queue_empty(const Queue* queue) 
+bool queue_empty(const Queue* queue)
 {
     return queue->count == 0;
 }
