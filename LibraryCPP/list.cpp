6c67b69f5e51a2928ea127546824eb5ab7cb525d
presslord <vad311205@gmail.com>
2025-11-24 23:26:21 +0300

    lab3

--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -9,9 +9,8 @@ struct ListItem {
 
 struct List {
   ListItem *head;
-  ListItem *tail;
 
-  List() : head(nullptr), tail(nullptr) {}
+  List() : head(nullptr) {}
 };
 
 List *list_create() { return new List; }
@@ -32,18 +31,14 @@ Data list_item_data(const ListItem *item) { return item->data; }
 
 ListItem *list_item_next(ListItem *item) { return item->next; }
 
-ListItem *list_item_prev(ListItem *item) { return nullptr; }
+ListItem *list_item_prev(ListItem *item) {
+  return nullptr; // односвязный список
+}
 
 ListItem *list_insert(List *list, Data data) {
   ListItem *new_item = new ListItem(data);
-
-  if (list->head == nullptr) {
-    list->head = new_item;
-    list->tail = new_item;
-  } else {
-    new_item->next = list->head;
-    list->head = new_item;
-  }
+  new_item->next = list->head;
+  list->head = new_item;
   return new_item;
 }
 
@@ -51,15 +46,9 @@ ListItem *list_insert_after(List *list, ListItem *item, Data data) {
   if (item == nullptr) {
     return list_insert(list, data);
   }
-
   ListItem *new_item = new ListItem(data);
   new_item->next = item->next;
   item->next = new_item;
-
-  if (item == list->tail) {
-    list->tail = new_item;
-  }
-
   return new_item;
 }
 
@@ -67,14 +56,8 @@ ListItem *list_erase_first(List *list) {
   if (list->head == nullptr) {
     return nullptr;
   }
-
   ListItem *old_head = list->head;
   list->head = list->head->next;
-
-  if (old_head == list->tail) {
-    list->tail = nullptr;
-  }
-
   delete old_head;
   return list->head;
 }
@@ -83,14 +66,8 @@ ListItem *list_erase_next(List *list, ListItem *item) {
   if (item == nullptr || item->next == nullptr) {
     return nullptr;
   }
-
   ListItem *to_delete = item->next;
   item->next = to_delete->next;
-
-  if (to_delete == list->tail) {
-    list->tail = item;
-  }
-
   delete to_delete;
   return item->next;
 }
