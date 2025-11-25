b0e8418dbabe2d4bdc2eacc3af53c90f3efa57f9
PrintsMomoe <ninakiller328@gmail.com>
2025-11-26 01:09:56 +0300

    3l

---
+++ Lab3CPP/lab3.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <queue.h>
+
+
+void lab3(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    int rowscol = 0, rows =0;
+    int startX = 0, startY = 0;
+
+    while (std::getline(file, line)) {
+        if (rowscol == 0) rowscol = line.length();
+        for (int i = 0; i < rowscol; i++){
+            if (line[i] == 'X') {
+                startX = rows;
+                startY = i;
+            }
+        }
+        rows++;
+    }
+
+    file.clear();
+    file.seekg(0);
+
+    char** pole = new char* [rowscol];
+    bool** visited = new bool* [rowscol];
+
+    for (int i = 0; i < rowscol; i++) {
+        pole[i] = new char[rowscol+1];
+        visited[i] = new char[rowscol];
+        std::getline(file, line);
+        for (int j = 0; j < cols; j++) {
+            maze[i][j] = line[j];
+            visited[i][j] = false;
+        }
+        maze[i][cols] = '\0';
+    }
+    file.close();
+
+    int dx[] = { -1, 0, 1, 0 };
+    int dy[] = { 0, 1, 0, -1 };
+
+    Queue* queue = queue_create();
+    queue_insert(queue, startX * rowscol + startY);
+    visited[startX][startY] = true;
+    int o = 1;
+
+    while (!queue_empty(queue)){
+        int pos = queue_get(queue);
+        queue_remove(queue);
+
+        int x = pos / cols;  
+        int y = pos % cols;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx >= 0 && nx < rowscol && ny >= 0 && ny < cols && !volatile [nx][ny] && pole[nx][ny] != '#') {
+                visited[nx][ny] = true;
+                queue_insert(queue, nx * rowscol + ny);
+                o++;
+            }
+
+        }
+
+    }
+
+    std::cout << o << std::endl;
+
+    for (int i = 0; i < rowscol; i++) {
+        for (int j = 0; j < rowscol; j++) {
+            if (visited[i][j] && pole[i][j] == '.') {
+                std::cout << 'x';
+            }
+            else {
+                std::cout << pole[i][j];
+            }
+        }
+
+    }
+
+    queue_delete(queue);
+
+    for (int i = 0; i < rowscol; i++) {
+        delete[] pole[i];
+        delete[] visited[i];
+    }
+    delete[] pole;
+    delete[] visited;
+
+   
+}
+
+
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            execute_instructions(argv[i]);
+        }
+    }
+    else {
+        std::string filenames[] = { "input.txt", "input2.txt"};
+        for (int i = 0; i < 2; i++) {
+            execute_instructions(filenames[i]);
+        }
+    }
+    return 0;
+}
+
--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,10 +3,19 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* next;
+    ListItem* previous;
+
+    ListItem(Data d) : data(d), next(nullptr), previous(nullptr) {}
 };
 
 struct List
 {
+    ListItem* head;
+    ListItem* tail;
+
+    List() : head(nullptr), tail(nullptr) {}
 };
 
 List *list_create()
@@ -16,46 +25,105 @@ List *list_create()
 
 void list_delete(List *list)
 {
-    // TODO: free items
+    ListItem* current = list->head;
+    while (current != nullptr) {
+        ListItem* next = current->next;
+        delete current;
+        current = next;
+    }
     delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return item->head;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    return item->data;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return item->previous;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    ListItem* new_item = new ListItem(data);
+
+    if (list->head == nullptr) {
+        list->head = new_item;
+        list->tail = new_item;
+    }
+    else {
+        new_item->next = list->head;
+        list->head->previous = new_item;
+        list->head = new_item;
+    }
+    return new_item;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (item == nullptr) {
+        return list_insert(list, data);
+    }
+    ListItem* new_item = item->next;
+
+    new_item->next = item->next;
+    new_item->previous = item;
+
+    if (item->next != nullptr) {
+        item->next->previous = new_item;
+    }
+    else {
+        list->tail = new_item;
+    }
+    item->next = new_item;
+    return new_item;
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    if (list->head == nullptr)
+        return nullptr;
+    ListItem* to_delete = list->head;
+    list->head = to_delete->next;
+
+    if (list->head != nullptr) {
+        list->head->previous = nullptr;
+    }
+    else {
+        list->tail = nullptr;
+    }
+    ListItem* result = list->head;
+    delete to_delete;
+    return result;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    if (item == nullptr)
+        return list_erase_first(list);
+    if (item->next == nullptr)
+        return nullptr;
+
+    ListItem* to_delete = item->next;
+    item->next = to_delete->next;
+
+    if (to_delete->next != nullptr)
+        to_delete->next->previous = item;
+    else
+        list->tail = item;
+
+    ListItem* result = item->next;
+    delete to_delete;
+    return result;
 }
