9c404e082e9a5eeb5c4cb2bfe85511c810dbf7ed
Kulebyak291 <oleg_pavlov291999@mail.com>
2025-11-27 19:19:52 +0300

    lab3.1

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -17,7 +17,12 @@ void queue_delete(Queue *queue) {
 }
 
 void queue_insert(Queue *queue, Data data) {
-    list_insert_after(queue->list, nullptr, data);  // Вставка в конец
+    List *lst = queue->list;
+    if (queue_empty(queue)) {
+        list_insert(lst, data);  // Вставка в начало для пустого
+    } else {
+        list_insert_after(lst, lst->tail, data);  // Вставка после tail для конца
+    }
 }
 
 Data queue_get(const Queue *queue) {
