dfff3437b08c482d2c4d4a93c74f6b7852acf4fa
OlesaTropnikova <olesatropnikova351@gmail.com>
2025-11-30 19:47:55 +0300

    v3.2

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -4,9 +4,20 @@
 #include <map>
 #include <string>
 #include <algorithm>
+#include <stack>
 #include "queue.h"
 
 using namespace std;
+
+bool parseRoad(const string& last, string& start_name, string& end_name) {
+    size_t pos = last.find(' ');
+    if (pos == string::npos) {
+        return false;
+    }
+    start_name = last.substr(0, pos);
+    end_name = last.substr(pos + 1);
+    return true;
+}
 
 int main(int argc, char **argv) {
     ifstream input(argv[1]);
@@ -28,65 +39,55 @@ int main(int argc, char **argv) {
     lines.pop_back();
 
     string start_name, end_name;
-    size_t pos = last.find(' ');
-    if (pos == string::npos) return 1;
-    start_name = last.substr(0, pos);
-    end_name = last.substr(pos + 1);
+    if (!parseRoad(last, start_name, end_name)) {
+        return 1;
+    }
 
     map<string, int> city_to_id;
     vector<string> id_to_city;
 
     auto get_id = [&](const string& name) -> int {
-        if (city_to_id.count(name) == 0) {
+        if (!city_to_id.contains(name)) {
             city_to_id[name] = id_to_city.size();
             id_to_city.push_back(name);
         }
         return city_to_id[name];
     };
 
-    get_id(start_name);
-    get_id(end_name);
-
-    vector<pair<string, string>> roads;
+    int start_id = get_id(start_name);
+    int end_id = get_id(end_name);
 
     for (const auto& ln : lines) {
-        pos = ln.find(' ');
-        if (pos == string::npos) continue;
-        string a = ln.substr(0, pos);
-        string b = ln.substr(pos + 1);
-        roads.emplace_back(a, b);
+        string a, b;
+        if (!parseRoad(ln, a, b)) continue;
         get_id(a);
         get_id(b);
     }
 
+
     int n = id_to_city.size();
     vector<vector<int>> graph(n);
 
-    for (const auto& road : roads) {
-        int u = city_to_id[road.first];
-        int v = city_to_id[road.second];
+    for (const auto& ln : lines) {
+        string a, b;
+        if (!parseRoad(ln, a, b)) continue;
+        int u = city_to_id[a];
+        int v = city_to_id[b];
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
 
-    int start_id = city_to_id[start_name];
-    int end_id = city_to_id[end_name];
-
     vector<int> parent(n, -1);
-    vector<bool> visited(n, false);
     Queue* q = queue_create();
-
     queue_insert(q, start_id);
-    visited[start_id] = true;
-    parent[start_id] = -1;
+    parent[start_id] = -2;
 
     while (!queue_empty(q)) {
         int current = queue_get(q);
         queue_remove(q);
         if (current == end_id) break;
         for (int neighbor : graph[current]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
+            if (parent[neighbor] == -1) {
                 parent[neighbor] = current;
                 queue_insert(q, neighbor);
             }
@@ -95,22 +96,22 @@ int main(int argc, char **argv) {
 
     queue_delete(q);
 
-    if (!visited[end_id]) {
+    if (parent[end_id] == -1) {
         return 1;
     }
 
-    vector<int> path;
+    stack<string> path;
     int cur = end_id;
-    while (cur != -1) {
-        path.push_back(cur);
+    while (cur != -2) {
+        path.push(id_to_city[cur]);
         cur = parent[cur];
     }
 
-    reverse(path.begin(), path.end());
-
-    for (size_t i = 0; i < path.size(); ++i) {
-        if (i > 0) cout << " ";
-        cout << id_to_city[path[i]];
+    cout << path.top();
+    path.pop();
+    while (!path.empty()) {
+        cout << " " << path.top();
+        path.pop();
     }
     cout << endl;
 }
