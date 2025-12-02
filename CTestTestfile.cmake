7dde04a092d6a1c5ee593cc70c9c70ac298bdfec
Manashka737 <mamulia737@gmail.com>
2025-12-02 23:10:59 +0300

    Чистка ветки

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /c/Users/mamul/OneDrive/Desktop/Программирование/C++/Dovgaluk/Algorithms
-# Build directory: /c/Users/mamul/OneDrive/Desktop/Программирование/C++/Dovgaluk/Algorithms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LibraryCPPTemplate")
-subdirs("LAB3CPP")
