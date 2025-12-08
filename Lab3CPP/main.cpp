d5509fb200eaa6f51791ae3e6b462aa0f3b17f81
Kulebyak291 <oleg_pavlov291999@mail.com>
2025-12-08 04:09:17 +0300

    ...

--- Lab3CPP/main.cpp
+++ Lab3CPP/main.cpp
@@ -9,21 +9,16 @@
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
-
 using namespace std;
-
 using Maze = vector<string>;
 using DistMap = vector<vector<int>>;
-
 int encode(int q, int r) {
     return q * 1000 + r;
 }
-
 void decode(int code, int& q, int& r) {
     q = code / 1000;
     r = code % 1000;
 }
-
 Maze read_maze(const string& filename, int& start_q, int& start_r) {
     Maze maze;
     ifstream input(filename);
@@ -31,15 +26,13 @@ Maze read_maze(const string& filename, int& start_q, int& start_r) {
         exit(1);
     }
     string line;
-    while (getline(input, line) ) {
+    while (getline(input, line)) {
         if (!line.empty() && line.back() == '\r') line.pop_back();
         maze.push_back(line);
     }
     if (maze.empty()) return maze;
-
     int rows = maze.size();
     int cols = maze[0].size();
-
     for (int r = 0; r < rows; ++r) {
         for (int q = 0; q < cols; ++q) {
             if (maze[r][q] == 'S') {
@@ -50,51 +43,49 @@ Maze read_maze(const string& filename, int& start_q, int& start_r) {
     }
     return maze;
 }
-
 void get_neighbors(int r, int dirs[6][2]) {
-    dirs[0][0] = 1;  dirs[0][1] = 0;
+    dirs[0][0] = 1; dirs[0][1] = 0;
     dirs[1][0] = -1; dirs[1][1] = 0;
-
     if (r % 2 == 0) {
         dirs[2][0] = 0; dirs[2][1] = -1;
         dirs[3][0] = 1; dirs[3][1] = -1;
         dirs[4][0] = 0; dirs[4][1] = 1;
         dirs[5][0] = 1; dirs[5][1] = 1;
-    } else {
+    }
+    else {
         dirs[2][0] = -1; dirs[2][1] = -1;
         dirs[3][0] = 0; dirs[3][1] = -1;
         dirs[4][0] = -1; dirs[4][1] = 1;
         dirs[5][0] = 0; dirs[5][1] = 1;
     }
 }
-
 bool is_slit_blocked(const Maze& maze, int cq, int cr, int nq, int nr) {
     if (abs(cq - nq) != 1 || abs(cr - nr) != 1) return false;
-
     int rows = maze.size();
     int cols = maze[0].size();
-
     int r_wall1, q_wall1;
     int r_wall2, q_wall2;
-
     if (cr % 2 == 0) {
         r_wall1 = cr;
-        q_wall1 = min(cq, nq) + 1;
+        q_wall1 = nq;
         r_wall2 = nr;
-        q_wall2 = min(cq, nq);
-    } else {
-        r_wall1 = cr;
-        q_wall1 = min(cq, nq);
-        r_wall2 = nr;
-        q_wall2 = min(cq, nq) + 1;
+        q_wall2 = cq;
     }
-
-    bool wall1_blocked = (r_wall1 >= 0 && r_wall1 < rows && q_wall1 >= 0 && q_wall1 < cols) && maze[r_wall1][q_wall1] == '#';
-    bool wall2_blocked = (r_wall2 >= 0 && r_wall2 < rows && q_wall2 >= 0 && q_wall2 < cols) && maze[r_wall2][q_wall2] == '#';
-
-    return wall1_blocked && wall2_blocked;
+    else {
+        r_wall1 = cr;
+        q_wall1 = cq;
+        r_wall2 = nr;
+        q_wall2 = nq;
+    }
+    auto is_wall = [&](int r, int q) {
+        if (r < 0 || r >= rows || q < 0 || q >= cols) return false;
+        return maze[r][q] == '#';
+        };
+    if (is_wall(r_wall1, q_wall1) && is_wall(r_wall2, q_wall2)) {
+        return true;
+    }
+    return false;
 }
-
 bool is_valid(const Maze& maze, int q, int r, const DistMap& dist) {
     int rows = maze.size();
     if (rows == 0) return false;
@@ -104,15 +95,13 @@ bool is_valid(const Maze& maze, int q, int r, const DistMap& dist) {
     if (dist[r][q] != -1) return false;
     return true;
 }
-
 vector<pair<int, int>> reconstruct_path(const DistMap& dist, int end_q, int end_r) {
     vector<pair<int, int>> path;
     int cq = end_q, cr = end_r;
     while (dist[cr][cq] != -1) {
         path.emplace_back(cq, cr);
         if (dist[cr][cq] == 0) break;
-        int dirs[6][2];
-        get_neighbors(cr, dirs);
+        int dirs[6][2] = { {1,0},{0,-1},{-1,1},{-1,0},{0,1},{1,-1} };
         bool found = false;
         for (int d = 0; d < 6; ++d) {
             int nq = cq + dirs[d][0];
@@ -129,7 +118,35 @@ vector<pair<int, int>> reconstruct_path(const DistMap& dist, int end_q, int end_
     reverse(path.begin(), path.end());
     return path;
 }
-
+vector<pair<int, int>> get_path(int end_q, int end_r, const DistMap& dist) {
+    vector<pair<int, int>> path;
+    int curr_q = end_q;
+    int curr_r = end_r;
+    int curr_dist = dist[curr_r][curr_q];
+    int dirs[6][2];
+    while (curr_dist > 0) {
+        path.push_back({ curr_q, curr_r });
+        get_neighbors(curr_r, dirs);
+        bool found_prev = false;
+        for (int d = 0; d < 6; ++d) {
+            int nq = curr_q + dirs[d][0];
+            int nr = curr_r + dirs[d][1];
+            if (nr >= 0 && nr < (int)dist.size() && nq >= 0 && nq < (int)dist[0].size()) {
+                if (dist[nr][nq] == curr_dist - 1) {
+                    curr_q = nq;
+                    curr_r = nr;
+                    curr_dist--;
+                    found_prev = true;
+                    break;
+                }
+            }
+        }
+        if (!found_prev) break;
+    }
+    path.push_back({ curr_q, curr_r });
+    reverse(path.begin(), path.end());
+    return path;
+}
 void print_path(const vector<pair<int, int>>& path) {
     cout << "Path:";
     for (size_t i = 0; i < path.size(); ++i) {
@@ -138,48 +155,44 @@ void print_path(const vector<pair<int, int>>& path) {
     }
     cout << endl;
 }
-
 void print_hex_maze(const Maze& maze, const vector<pair<int, int>>& path) {
     int rows = maze.size();
     if (rows == 0) return;
     int cols = maze[0].size();
+    // Создаем матрицу для отображения
     vector<vector<char>> display(rows, vector<char>(cols));
     for (int r = 0; r < rows; ++r) {
         for (int q = 0; q < cols; ++q) {
             display[r][q] = maze[r][q];
-        }
-    }
-    for (const auto& p : path) {
-        int q = p.first;
-        int r = p.second;
-        if (display[r][q] != 'S' && display[r][q] != 'E') {
-            display[r][q] = 'x';
+            auto pos = make_pair(q, r);
+            if (find(path.begin(), path.end(), pos) != path.end() &&
+                maze[r][q] != 'S' && maze[r][q] != 'E') {
+                display[r][q] = 'x';
+            }
         }
     }
     // Верхняя граница
-    cout << " ";
     for (int q = 0; q < cols; ++q) {
-        cout << "/ \\ ";
+        cout << " / \\";
     }
-    cout << endl;
+    cout << '\n';
     // Основное содержимое
     for (int r = 0; r < rows; ++r) {
-        // Отступ
-        string indent(r, ' ');
-        cout << indent << " /";
+        string indent(2 * r, ' ');
+        // Строка с ячейками
+        cout << indent;
         for (int q = 0; q < cols; ++q) {
-            cout << " " << display[r][q] << " ";
-            cout << "\\";
+            cout << "| " << display[r][q] << " ";
         }
-        cout << endl;
-        cout << indent << "  /";
+        cout << "|\n";
+        // Нижняя граница
+        cout << indent;
         for (int q = 0; q < cols; ++q) {
             cout << " \\ /";
         }
-        cout << endl;
+        cout << '\n';
     }
 }
-
 void bfs_hex(const Maze& maze, int start_q, int start_r) {
     int rows = maze.size();
     if (rows == 0) {
@@ -221,17 +234,18 @@ void bfs_hex(const Maze& maze, int start_q, int start_r) {
     queue_delete(q);
     if (found) {
         cout << dist[end_r][end_q] << endl;
-        auto path = reconstruct_path(dist, end_q, end_r);
+        auto path = get_path(end_q, end_r, dist);
         print_path(path);
         cout << "Maze with path:" << endl;
         print_hex_maze(maze, path);
-    } else {
+    }
+    else {
         cout << -1 << endl;
     }
 }
 
 int main(int argc, char* argv[]) {
-    const char* test_output = getenv("TEST_OUTPUT");
+const char* test_output = getenv("TEST_OUTPUT");
     if (test_output) {
         FILE* result = freopen(test_output, "w", stdout);
         if (!result) {
