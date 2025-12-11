2558578413d1835d8722cd64f22d57323cb54698
Shiku2889 <107749199+Shiku2889@users.noreply.github.com>
2025-12-12 01:48:18 +0300

    Update vector.cpp

--- LibraryCPP/vector.cpp
+++ LibraryCPP/vector.cpp
@@ -1,12 +1,12 @@
 #include "vector.h"
-#include <algorithm> // для std::min
+#include <algorithm>
 
 struct Vector
 {
     Data* data;
     size_t size;
     size_t capacity;
-    
+
     Vector() : data(nullptr), size(0), capacity(0) {}
 };
 
@@ -24,7 +24,7 @@ void vector_delete(Vector* vector)
 Data vector_get(const Vector* vector, size_t index)
 {
     if (index >= vector->size) {
-        return (Data)0; // или можно бросить исключение
+        return (Data)0;
     }
     return vector->data[index];
 }
@@ -32,7 +32,7 @@ Data vector_get(const Vector* vector, size_t index)
 void vector_set(Vector* vector, size_t index, Data value)
 {
     if (index >= vector->size) {
-        return; // или можно бросить исключение
+        return;
     }
     vector->data[index] = value;
 }
@@ -45,27 +45,23 @@ size_t vector_size(const Vector* vector)
 void vector_resize(Vector* vector, size_t new_size)
 {
     if (new_size > vector->capacity) {
-        // Увеличиваем capacity с коэффициентом 2
         size_t new_capacity = std::max(new_size, vector->capacity * 2);
         if (new_capacity < 4) new_capacity = 4;
-        
+
         Data* new_data = new Data[new_capacity];
-        
-        // Копируем существующие данные
+
         for (size_t i = 0; i < vector->size; ++i) {
             new_data[i] = vector->data[i];
         }
-        
-        // Инициализируем новые элементы нулями
+
         for (size_t i = vector->size; i < new_capacity; ++i) {
             new_data[i] = (Data)0;
         }
-        
+
         delete[] vector->data;
         vector->data = new_data;
         vector->capacity = new_capacity;
     }
-    
-    // Если уменьшаем размер, то просто меняем size
+
     vector->size = new_size;
 }
