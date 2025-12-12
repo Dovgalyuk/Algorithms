82d9577e3c2aa367b4577328246ffe2599025dcd
Fog-xWost <fogwostfox@gmail.com>
2025-12-12 19:39:28 +0300

    add queue implement

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -1,41 +1,59 @@
 #ifndef QUEUE_TEMPLATE_H
 #define QUEUE_TEMPLATE_H
 
+#include "list.h"
+
 template <typename Data> class Queue
 {
 public:
     // Create empty queue
     Queue()
     {
+        mTail = nullptr;
+        // mList создается сам
     }
 
     // copy constructor
     Queue(const Queue &a)
     {
         // implement or disable this function
+        mList = a.mList;
+        updateTail();
     }
 
     // assignment operator
     Queue &operator=(const Queue &a)
     {
         // implement or disable this function
+        if (this != &a) {
+            mList = a.mList;
+            updateTail();
+        }
         return *this;
     }
 
     // Deletes queue
     ~Queue()
     {
+        // Деструктор листа вызывается сам
     }
 
     // Includes new element into the queue
     // Should be O(1) on average
     void insert(Data data)
     {
+        if (mList.first() == nullptr) {
+            mList.insert(data);
+            mTail = mList.first();
+        }
+        else mTail = mList.insert_after(mTail, data); 
     }
 
     // Retrieves first element from the queue
     Data get() const
     {
+        if (mList.first() != nullptr) return mList.first()->data();
+
         return Data();
     }
 
@@ -43,16 +61,32 @@ public:
     // Should be O(1) on average
     void remove()
     {
+        if (mList.first() != nullptr) {
+            mList.erase_first();
+            if (mList.first() == nullptr) mTail = nullptr;
+        }
     }
 
     // Returns true if the queue is empty
     bool empty() const
     {
-        return true;
+        return mList.first() == nullptr;
     }
 
 private:
-    // private data should be here
+    List<Data> mList;
+    typename List<Data>::Item *mTail;
+
+    void updateTail() {
+        if (mList.first() == nullptr) mTail = nullptr;
+        else {
+            typename List<Data>::Item *current = mList.first();
+
+            while (current->next() != nullptr) current = current->next();
+            
+            mTail = current;
+        }
+    }
 };
 
 #endif
