cc82c36ac2799d3f854f036035395facf2ffca8f
TheCh0sen11 <sashik0708@gmail.com>
2025-12-15 04:28:21 +0300

    lab3.3cpp

--- Lab3CPP/lab3.cpp
+++ Lab3CPP/lab3.cpp
@@ -10,47 +10,46 @@ int bfs(int n, int start, int finish, int** graph) {
     Queue* q = queue_create();
     int* distances = new int[n];
     
-    for (int i = 0; i < n; i++) {
-        distances[i] = -1;
-    }
+    for (int i = 0; i < n; i++) 
+        distances[i] = -1;  
     
     distances[start - 1] = 0;
     queue_insert(q, start);
     
-    while (!queue_empty(q)) {
+    bool found = false;
+    
+
+    while (!queue_empty(q) && !found) {
         int current = queue_get(q);
         queue_remove(q);
         
         for (int i = 0; i < n; i++) {
-            if (graph[current-1][i] == 1 && distances[i] == -1) {
+            if (graph[current - 1][i] == 1 && distances[i] == -1) {
                 distances[i] = distances[current - 1] + 1;
                 
-                if (i+1 == finish) {
-                    int result = distances[i];
-                    queue_delete(q);
-                     delete[] distances;
-                    return result;
+                if (i + 1 == finish) {
+                    found = true;
+                    break;
                 }
-                queue_insert(q, i+1);
+                
+                queue_insert(q, i + 1);
             }
         }
     }
     
+    int result = distances[finish - 1 ];
     queue_delete(q);
     delete[] distances;
-    return -1;
+    
+    return result;
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2)
-        return 1;
+    if (argc < 2) return 1;
     
     ifstream input(argv[1]);
     
-    if (!input.is_open()) {
-        cout << "Error opening input file" << endl;
-        return 1;
-    }
+    if (!input.is_open()) return 1;
     
     int n, start, finish;
     input >> n >> start >> finish;
@@ -60,20 +59,19 @@ int main(int argc, char** argv) {
         graph[i] = new int[n];
         for (int j = 0; j < n; j++)
             input >> graph[i][j];
-
     }
     
     int result = bfs(n, start, finish, graph);
     
-    if (result == -1) {
+    if (result == -1)
         cout << "IMPOSSIBLE" << endl;
-    } else {
+    else 
         cout << result << endl;
-    }
     
-    for (int i = 0; i < n; i++) {
-        delete[] graph[i];
-    }
+    
+    for (int i = 0; i < n; i++) 
+         delete[] graph[i];
+    
     delete[] graph;
     
     input.close();
