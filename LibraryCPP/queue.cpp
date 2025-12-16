e00610bb09a31b32c4a51e23487be69d2ee6fc63
Shiku2889 <107749199+Shiku2889@users.noreply.github.com>
2025-12-16 22:57:50 +0300

    Update queue.cpp

--- LibraryCPP/queue.cpp
+++ LibraryCPP/queue.cpp
@@ -1,72 +1,61 @@
 #include "queue.h"
 #include "vector.h"
 
-struct Queue
-{
-    Vector* data;  // кольцевой буфер на основе vector
-    size_t front;  // индекс начала очереди
-    size_t back;   // индекс следующего свободного места
-    size_t count;  // количество элементов в очереди
+struct Queue {
+    Vector* data;   // кольцевой буфер
+    size_t front;   // индекс первого элемента
+    size_t count;   // количество элементов
 };
 
-Queue* queue_create()
-{
+Queue* queue_create() {
     Queue* queue = new Queue;
     queue->data = vector_create();
     queue->front = 0;
-    queue->back = 0;
     queue->count = 0;
-    vector_resize(queue->data, 4); // начальная емкость
+    vector_resize(queue->data, 4);
     return queue;
 }
 
-void queue_delete(Queue* queue)
-{
+void queue_delete(Queue* queue) {
     vector_delete(queue->data);
     delete queue;
 }
 
-bool queue_empty(const Queue* queue)
-{
+bool queue_empty(const Queue* queue) {
     return queue->count == 0;
 }
 
-void queue_insert(Queue* queue, Data data)
-{
-    // Если буфер полный, увеличиваем его
-    if (queue->count == vector_size(queue->data)) {
-        size_t old_capacity = vector_size(queue->data);
-        size_t new_capacity = old_capacity * 2;
-        vector_resize(queue->data, new_capacity);
-
-        // Если очередь "обернулась", перераспределяем элементы
-        if (queue->back <= queue->front && queue->count > 0) {
-            for (size_t i = 0; i < old_capacity - queue->front; ++i) {
-                vector_set(queue->data, new_capacity - i - 1,
-                    vector_get(queue->data, old_capacity - i - 1));
-            }
-            queue->front = new_capacity - (old_capacity - queue->front);
+void queue_insert(Queue* queue, Data data) {
+    size_t capacity = vector_size(queue->data);
+    
+    if (queue->count == capacity) {
+        size_t new_capacity = capacity * 2;
+        Vector* new_data = vector_create();
+        vector_resize(new_data, new_capacity);
+        
+        for (size_t i = 0; i < queue->count; ++i) {
+            size_t old_index = (queue->front + i) % capacity;
+            vector_set(new_data, i, vector_get(queue->data, old_index));
         }
+        
+        vector_delete(queue->data);
+        queue->data = new_data;
+        queue->front = 0;
+        capacity = new_capacity;
     }
-
-    vector_set(queue->data, queue->back, data);
-    queue->back = (queue->back + 1) % vector_size(queue->data);
+    
+    size_t insert_pos = (queue->front + queue->count) % capacity;
+    vector_set(queue->data, insert_pos, data);
     queue->count++;
 }
 
-Data queue_get(const Queue* queue)
-{
-    if (queue_empty(queue)) {
-        return (Data)0;
-    }
-    return vector_get(queue->data, queue->front);
+Data queue_get(const Queue* queue) {
+    return queue_empty(queue) ? (Data)0 : vector_get(queue->data, queue->front);
 }
 
-void queue_remove(Queue* queue)
-{
-    if (queue_empty(queue)) {
-        return;
+void queue_remove(Queue* queue) {
+    if (!queue_empty(queue)) {
+        queue->front = (queue->front + 1) % vector_size(queue->data);
+        queue->count--;
     }
-    queue->front = (queue->front + 1) % vector_size(queue->data);
-    queue->count--;
 }
