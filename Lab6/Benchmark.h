1435f3f8c76e39e64976d79a287dcf241715e8c7
Tosoret <tosoret@mail.ru>
2025-12-17 04:07:29 +0300

    add benchmarks runs

--- Lab6/Benchmark.h
+++ Lab6/Benchmark.h
@@ -12,5 +12,5 @@ constexpr int BENCHMARK_MAX_N = 20;
 class Benchmark {
 public:
     static Matrix generateChain(int n);
-    static void run(bool writeToFile = true, int repeats = 5, int benchmarkRuns = 3, const std::string& outputFileName = "results.csv");
+    static void run(bool writeToFile = true, int repeats = 5, int benchmarkRuns = 10, const std::string& outputFileName = "results.csv");
 };
