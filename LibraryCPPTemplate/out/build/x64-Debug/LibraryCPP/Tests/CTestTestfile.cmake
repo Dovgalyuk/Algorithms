d00931b54dba35c6478d462f6d6d7a9adc50750f
Vsevolod <workbrauzmanvsevolod@mail.ru>
2025-12-18 13:58:25 +0300

    убрал временные файлы

--- LibraryCPPTemplate/out/build/x64-Debug/LibraryCPP/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Profiles/Acer/Desktop/Algorithms/LibraryCPP/Tests
-# Build directory: D:/Profiles/Acer/Desktop/Algorithms/out/build/x64-Debug/LibraryCPP/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayCPP "TestArrayCPP")
-set_tests_properties(TestArrayCPP PROPERTIES  _BACKTRACE_TRIPLES "D:/Profiles/Acer/Desktop/Algorithms/LibraryCPP/Tests/CMakeLists.txt;4;add_test;D:/Profiles/Acer/Desktop/Algorithms/LibraryCPP/Tests/CMakeLists.txt;0;")
