275e7f8a2c76775286a10f2ca139dfa269fb725a
Ilia Bogdanov <90547748+Brikka23@users.noreply.github.com>
2025-12-18 11:08:15 +0300

    add test
    Added tests for edge removal and vertex removal in graph.

--- LibraryCPPTemplate/Tests/graph.cpp
+++ LibraryCPPTemplate/Tests/graph.cpp
@@ -61,4 +61,65 @@ int main()
     assert(graph3.getVertexLabel(1) == "C");
     assert(graph3.hasEdge(1, 0));
     assert(!graph3.hasEdge(0, 1));
+
+    GraphType graph4(3);
+    graph4.setVertexLabel(0, "A");
+    graph4.setVertexLabel(1, "B");
+    graph4.setVertexLabel(2, "C");
+
+    graph4.addEdge(0, 1, 10);
+    graph4.addEdge(0, 2, 20);
+    graph4.addEdge(0, 0, 30);
+
+    graph4.removeEdge(0, 2);
+
+    auto neighboursBeforeRemoval = graph4.neighbours(0);
+    int neighbourCount = 0;
+    bool hasSelfLoop = false;
+    bool hasEdgeToOne = false;
+    while (neighboursBeforeRemoval.valid())
+    {
+        const auto vertex = neighboursBeforeRemoval.vertex();
+        if (vertex == 0)
+        {
+            hasSelfLoop = neighboursBeforeRemoval.edgeLabel() == 30;
+        }
+        else if (vertex == 1)
+        {
+            hasEdgeToOne = neighboursBeforeRemoval.edgeLabel() == 10;
+        }
+        ++neighbourCount;
+        neighboursBeforeRemoval.next();
+    }
+    assert(neighbourCount == 2);
+    assert(hasSelfLoop);
+    assert(hasEdgeToOne);
+
+    graph4.removeVertex(1);
+
+    assert(graph4.verticesCount() == 2);
+    assert(graph4.getVertexLabel(1) == "C");
+
+    auto neighboursAfterRemoval = graph4.neighbours(0);
+    neighbourCount = 0;
+    hasSelfLoop = false;
+    hasEdgeToOne = false;
+    while (neighboursAfterRemoval.valid())
+    {
+        const auto vertex = neighboursAfterRemoval.vertex();
+        if (vertex == 0)
+        {
+            hasSelfLoop = neighboursAfterRemoval.edgeLabel() == 30;
+        }
+        else if (vertex == 1)
+        {
+            hasEdgeToOne = true;
+        }
+        ++neighbourCount;
+        neighboursAfterRemoval.next();
+    }
+
+    assert(neighbourCount == 1);
+    assert(hasSelfLoop);
+    assert(!hasEdgeToOne);
 }
