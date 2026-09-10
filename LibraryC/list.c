17758336649818937af10f566b9ccf9e74a577e5
bikakuka <bikakukavn@gmail.com>
2026-09-11 00:53:15 +0300

    realize list logic

--- LibraryC/list.c
+++ LibraryC/list.c
@@ -2,67 +2,144 @@
 #include "list.h"
 
 typedef struct ListItem {
-    // remove this
+    struct ListItem *prev;
+    struct ListItem *next;
     Data d;
 } ListItem;
 
 typedef struct List {
-    // remove this
-    Data d;
+    ListItem *head;
+    ListItem *tail;
+    size_t size;
+    FFree *freefunc;
 } List;
 
 List *list_create(FFree f)
 {
-    return malloc(sizeof(List));
+    List *list = malloc(sizeof(List)); 
+    if (!list) return NULL;
+    list->head = NULL;
+    list->tail = NULL;
+    list->freefunc = f;
+    list->size = 0;
+    return list;
 }
 
 void list_delete(List *list)
 {
-    // TODO: free items
+    if (!list) return;
+    ListItem *point = list->head;
+    while (point){
+        ListItem *next = point->next;
+        if (list->freefunc) {
+            list->freefunc((void*)point->d);
+        }
+        free(point);
+        point = next;
+    }
+    
     free(list);
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    if (!list) return NULL;
+
+    return list->head;
 }
 
 ListItem *list_last(List *list)
 {
-    return NULL;
+    if (!list) return NULL;
+
+    return list->tail;
 }
 
+/* проверка существования item перед вызовом обязтельна.
+у нас 0 может хранится как данные, потому надо
+*/
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    if (!item) return (Data)0; // я бы не вставлял это вообще, но по шаблону выглядит что нужно так
+    return item->d;
 }
 
 ListItem *list_item_next(ListItem *item)
 {
-    return NULL;
+    if (!item) return NULL;
+
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    if (!item) return NULL;
+
+    return item->prev;
 }
 
-ListItem *list_insert(List *list, Data data)
+ListItem *list_insert(List *list, Data data) // new head
 {
-    return NULL;
+    if (!list) return NULL;
+
+    ListItem *item = malloc(sizeof(ListItem));
+    if (!item) return NULL;
+    item->d = data;
+    item->prev = NULL;
+    item->next = list->head;
+    if (list->head) list->head->prev = item;
+    else list->tail = item;
+    
+    list->head = item;
+    list->size++;
+    return item;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (!list) return NULL;
+    if (!item) return list_insert(list, data);
+    ListItem *newitem = malloc(sizeof(ListItem));
+    if (!newitem) return NULL;
+    newitem->d = data;
+    newitem->prev = item;
+    newitem->next = item->next;
+    item->next = newitem;
+    if (newitem->next) newitem->next->prev = newitem;
+    else list->tail = newitem;
+    list->size++;
+    return newitem;
 }
 
-ListItem *list_erase_first(List *list)
+ListItem *list_erase_first(List *list) // del head of list, return new head
 {
-    return NULL;
+    if (!list || list->size == 0) return NULL;
+    ListItem *del = list->head;
+    list->head = del->next;
+    if (list->freefunc) {
+        list->freefunc((void*)del->d);
+    }
+    if (list->head) list->head->prev = NULL;
+    else list->tail = NULL;
+    free(del);
+    list->size--;
+    return list->head;
 }
 
-ListItem *list_erase_next(List *list, ListItem *item)
+ListItem *list_erase_next(List *list, ListItem *item) // del next to given, return new next
 {
-    return NULL;
+    if (!list) return NULL;
+    if (!item) return list_erase_first(list);
+    ListItem *del = item->next;
+    if (del){ 
+        item->next = del->next;
+        if (del->next) del->next->prev = item;
+        else list->tail = item;
+        if (list->freefunc) {
+            list->freefunc((void*)del->d);
+        }
+        free(del);
+        list->size--;
+    }
+    return item->next;
 }
