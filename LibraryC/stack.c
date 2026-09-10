9b7ddd79ecf0873dbd2f11333600e06219b455a1
bikakuka <bikakukavn@gmail.com>
2026-09-11 01:24:43 +0300

    realize stack logic

--- LibraryC/stack.c
+++ LibraryC/stack.c
@@ -1,36 +1,54 @@
 #include <stdlib.h>
 #include "stack.h"
+#include "list.h"
 
 typedef struct Stack {
-    // remove this
-    Data d;
+    List *list;
 } Stack;
 
 Stack *stack_create(FFree f)
 {
-    return malloc(sizeof(Stack));
+    Stack *stack = malloc(sizeof(Stack));
+    if (!stack) return NULL;
+    stack->list = list_create(f);
+    if (!stack->list) {
+        free(stack);
+        return NULL;
+    }
+    return stack;
 }
 
 void stack_delete(Stack *stack)
 {
-    // TODO: free stack elements
+    if (!stack) return;
+    list_delete(stack->list);
     free(stack);
 }
 
 void stack_push(Stack *stack, Data data)
 {
+    if (!stack) return;
+    list_insert(stack->list, data);
 }
 
+/* проверка непустоты стека перед вызовом обязтельна.
+у нас 0 может хранится как данные, потому надо
+*/
 Data stack_get(const Stack *stack)
 {
-    return (Data)0;
+    if (!stack) return (Data)0; // я бы не вставлял это вообще, но по шаблону выглядит что нужно так
+    ListItem *data = list_first(stack->list);
+    return list_item_data(data);
 }
 
 void stack_pop(Stack *stack)
 {
+    if (!stack) return;
+    list_erase_first(stack->list);
 }
 
 bool stack_empty(const Stack *stack)
 {
-    return true;
+    if (!stack || !stack->list) return true;
+    return list_first(stack->list) == NULL;
 }
