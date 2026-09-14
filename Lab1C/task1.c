82c3f25f1d5cb2a9c0938df8edcb00eed60a66e7
Goga <gahan.sudakov@list.ru>
2026-09-14 15:46:18 +0300

    Разделил задания на две программы

--- Lab1C/task1.c
+++ Lab1C/task1.c
@@ -5,21 +5,22 @@ Array *array_create_and_read(FILE *input)
 {
     int n;
     fscanf(input, "%d", &n);
-    
+
     Array *arr = array_create(n, NULL);
-    
-    for (int i = 0 ; i < n ; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
         int x;
         fscanf(input, "%d", &x);
         array_set(arr, i, x);
     }
+
     return arr;
 }
 
 void task1(Array *arr)
 {
-     size_t n = array_size(arr);
+    size_t n = array_size(arr);
 
     if (n == 0)
     {
@@ -62,53 +63,22 @@ void task1(Array *arr)
     printf("\n");
 }
 
-void task2(Array *arr, Data a, Data b)
-{
-    size_t n = array_size(arr);
-    size_t write = 0;
-
-    for (size_t i = 0; i < n; ++i)
-    {
-        Data x = array_get(arr, i);
-
-        if (x < a || x > b)
-        {
-            array_set(arr, write, x);
-            write++;
-        }
-    }
-
-    while (write < n)
-    {
-        array_set(arr, write, 0);
-        write++;
-    }
-
-    for (size_t i = 0; i < n; ++i)
-    {
-        printf("%zu ", (size_t)array_get(arr, i));
-    }
-
-    printf("\n");
-}
-
 int main(int argc, char **argv)
 {
-    Array *arr = NULL;
+    if (argc < 2)
+        return 1;
+
     FILE *input = fopen(argv[1], "r");
 
-    arr = array_create_and_read(input);
+    if (input == NULL)
+        return 1;
+
+    Array *arr = array_create_and_read(input);
+
     task1(arr);
-    array_delete(arr);
-
-    /* Create another array here */
-    arr = array_create_and_read(input);
-
-    size_t a, b;
-    fscanf(input, "%zu %zu", &a, &b);
-
-    task2(arr, (Data)a, (Data)b);
 
     array_delete(arr);
     fclose(input);
+
+    return 0;
 }
