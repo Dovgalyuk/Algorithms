931bf43053973d2c4e940227ed943cf8b19d242f
Ilia Vaskov <vii.pixel.71@gmail.com>
2026-09-15 22:57:34 +0300

    Lab1 Vaskov Ilia 5093

--- out/build/x64-Debug/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ilava/OneDrive/Рабочий стол/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: C:/Users/ilava/OneDrive/Рабочий стол/Algorithms/out/build/x64-Debug/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
