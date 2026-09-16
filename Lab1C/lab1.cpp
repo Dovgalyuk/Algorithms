9323c95b7d65ecce4415cfc2077ef5a84cc060fb
Ivan Ivanov <maksim.ru1405@gmail.com>
2026-09-16 15:09:49 +0300

    Fix warnings in C++ implementation

--- Lab1C/lab1.cpp
+++ Lab1C/lab1.cpp
@@ -1,15 +1,11 @@
-#include <stdio.h>
+#include <cstdio>
 #include "array.h"
 
-Array *array_create_and_read(FILE *input)
-{
+Array* array_create_and_read(FILE* input) {
     int n;
     fscanf(input, "%d", &n);
-
-    Array *arr = array_create(n, NULL);
-
-    for (int i = 0 ; i < n ; ++i)
-    {
+    Array* arr = array_create(n);
+    for (int i = 0; i < n; ++i) {
         int x;
         fscanf(input, "%d", &x);
         array_set(arr, i, x);
@@ -19,59 +15,39 @@ Array *array_create_and_read(FILE *input)
 
 void task1(Array* arr) {
     size_t n = array_size(arr);
-    if (n == 0) 
-    { 
-        printf("0.000000\n"); 
-        return; 
-    }
-
+    if (n == 0) { printf("0.000000\n"); return; }
     double sum = 0.0;
     for (size_t i = 0; i < n; ++i) {
-        int r = (int)array_get(arr, i);
-        if (r <= 0) 
-        { 
-            printf("Error\n"); 
-            return; 
-        }
-
+        int r = array_get(arr, i);
+        if (r <= 0) { printf("Error\n"); return; }
         sum += 1.0 / r;
     }
-
     printf("%.6f\n", 1.0 / sum);
 }
 
 void task2(Array* arr) {
     size_t n = array_size(arr);
-    if (n == 0) 
-    { 
-        printf("\n"); 
-        return; 
-    }
-
+    if (n == 0) { printf("\n"); return; }
     size_t i = 0;
     while (i < n) {
-        int val = (int)array_get(arr, i);
+        int val = array_get(arr, i);
         int count = 1;
-        while (i + count < n && (int)array_get(arr, i + count) == val)
+        while (i + count < n && array_get(arr, i + count) == val)
             count++;
-        
         printf("(%d,%d) ", val, count);
         i += count;
     }
-
     printf("\n");
 }
 
-int main(int argc, char **argv)
-{
-    Array *arr = NULL;
-    FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
+int main(int argc, char** argv) {
+    FILE* input = fopen(argv[1], "r");
+    Array* arr = array_create_and_read(input);
     task1(arr);
     array_delete(arr);
-
     arr = array_create_and_read(input);
     task2(arr);
     array_delete(arr);
     fclose(input);
+    return 0;
 }
---
+++ out/build/x64-Debug/LibraryC/Tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Algorithms/LibraryC/Tests
+# Build directory: C:/Algorithms/out/build/x64-Debug/LibraryC/Tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
