a86942c74e7ae4d09a51eef029382037878ac19b
deathbyoverthinking <pashka201099@gmail.com>
2026-09-17 10:37:10 +0300

    feat: done

---
+++ Lab1C/above_sum.c
@@ -0,0 +1,83 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array.h"
+
+static Array *read_array(FILE *input)
+{
+    long long input_size;
+    if (fscanf(input, "%lld", &input_size) != 1 || input_size < 0
+        || (unsigned long long)input_size > SIZE_MAX)
+    {
+        return NULL;
+    }
+
+    const size_t size = (size_t)input_size;
+    Array *arr = array_create(size, NULL);
+    if (arr == NULL)
+        return NULL;
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        int value;
+        if (fscanf(input, "%d", &value) != 1)
+        {
+            array_delete(arr);
+            return NULL;
+        }
+        array_set(arr, i, (Data)(intptr_t)value);
+    }
+
+    return arr;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        fprintf(stderr, "Cannot open input file: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    Array *arr = read_array(input);
+    fclose(input);
+    if (arr == NULL)
+    {
+        fprintf(stderr, "Invalid input data\n");
+        return EXIT_FAILURE;
+    }
+
+    int64_t sum = 0;
+    for (size_t i = 0; i < array_size(arr); ++i)
+        sum += (int)(intptr_t)array_get(arr, i);
+
+    size_t count = 0;
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        if ((int64_t)(int)(intptr_t)array_get(arr, i) > sum)
+            ++count;
+    }
+
+    printf("%zu\n", count);
+    int first = 1;
+    for (size_t i = 0; i < array_size(arr); ++i)
+    {
+        if ((int64_t)(int)(intptr_t)array_get(arr, i) > sum)
+        {
+            printf("%s%zu", first ? "" : " ", i + 1);
+            first = 0;
+        }
+    }
+    putchar('\n');
+
+    array_delete(arr);
+    return EXIT_SUCCESS;
+}
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,36 +1,81 @@
-#include <stdlib.h>
 #include "array.h"
+#include <stdlib.h>
 
-typedef struct Array {
-    // remove this
-    Data d;
-} Array;
+struct Array {
+    Data *data;
+    size_t size;
+    FFree *free_value;
+};
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array *arr = malloc(sizeof(*arr));
+    if (arr == NULL)
+        return NULL;
+
+    arr->data = NULL;
+    arr->size = size;
+    arr->free_value = f;
+
+    if (size != 0)
+    {
+        arr->data = calloc(size, sizeof(*arr->data));
+        if (arr->data == NULL)
+        {
+            free(arr);
+            return NULL;
+        }
+    }
+
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if (arr == NULL)
+        return;
+
+    if (arr->free_value != NULL)
+    {
+        for (size_t i = 0; i < arr->size; ++i)
+        {
+            if (arr->data[i] != (Data)0)
+                arr->free_value((void *)arr->data[i]);
+        }
+    }
+
+    free(arr->data);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (arr == NULL || index >= arr->size)
+        return (Data)0;
+
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (arr == NULL || index >= arr->size)
+        return;
+
+    if (arr->free_value != NULL && arr->data[index] != (Data)0
+        && arr->data[index] != value)
+    {
+        arr->free_value((void *)arr->data[index]);
+    }
+
+    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr == NULL ? 0 : arr->size;
 }
