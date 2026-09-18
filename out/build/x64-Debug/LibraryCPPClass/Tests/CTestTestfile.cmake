399e202cbafbaf0eb3c762b6334087e3489ee09b
timofeikrylov <timofei.krylov@gmail.com>
2026-09-18 22:30:01 +0300

    Remove build and IDE folders from repository, update .gitignore

--- out/build/x64-Debug/LibraryCPPClass/Tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/timof/Algorithms/LibraryCPPClass/Tests
-# Build directory: C:/Users/timof/Algorithms/out/build/x64-Debug/LibraryCPPClass/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
