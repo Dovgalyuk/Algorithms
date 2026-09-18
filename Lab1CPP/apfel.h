5f8c010bd6df61e6872145caa02120793e244954
Ilia Vaskov <vii.pixel.71@gmail.com>
2026-09-18 12:28:56 +0300

    try3

---
+++ Lab1CPP/apfel.h
@@ -0,0 +1,14 @@
+#ifndef APFEL_H
+#define APFEL_H
+
+#include <iostream>
+#include <fstream>
+#include "array.h"
+
+using namespace std;
+
+void print_array(ostream& out, Array* arr);
+Array* array_create_and_read(ifstream& input);
+void print_error(const char* message, ofstream& output);
+
+#endif
