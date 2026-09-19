dd31e8793a840ae229bc49f1ba954bca23130a92
ZeroTangens <kovalenkoa372@gmail.com>
2026-09-19 17:23:02 +0300

    Array

--- LibraryCPP/array.cpp
+++ LibraryCPP/array.cpp
@@ -2,33 +2,54 @@
 
 struct Array
 {
+    Data* data;
+    size_t size;
 };
 
 // create array
-Array *array_create(size_t size)
+Array* array_create(size_t size)
 {
-    return new Array;
+    Array* arr = new Array;
+    arr->size = size;
+    try
+    {
+        arr->data = (size > 0) ? new Data[size] : nullptr;
+    }
+    catch (...)
+    {
+        delete arr;
+        throw;
+    }
+    return arr;
 }
 
 // delete array, free memory
-void array_delete(Array *arr)
+void array_delete(Array* arr)
 {
-    delete arr;
+    if (arr != nullptr)
+    {
+        if (arr->data) delete[] arr->data;
+        delete arr;
+    }
+
 }
 
 // returns specified array element
-Data array_get(const Array *arr, size_t index)
+Data array_get(const Array* arr, size_t index)
 {
-    return (Data)0;
+    if (arr != nullptr && index < arr->size) return arr->data[index];
+    return Data{};
 }
 
 // sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
+void array_set(Array* arr, size_t index, Data value)
 {
+    if (arr != nullptr && index < arr->size) arr->data[index] = value;
 }
 
 // returns array size
-size_t array_size(const Array *arr)
+size_t array_size(const Array* arr)
 {
+    if (arr != nullptr) return arr->size;
     return 0;
 }
