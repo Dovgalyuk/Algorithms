18916add0fd5e8584c650351c13515406040f8b3
Nekilaev4 <nekilaev4@mail.ru>
2026-09-21 09:01:30 +0300

    LAB1NikolaevVersion3

--- LAB1C++/LAB1C++_TASK1.cpp
+++ LAB1C++/LAB1C++_TASK1.cpp
@@ -12,28 +12,33 @@ int main(int argc, char *argv[])
     ifstream filetask1(argv[1]);
     if(!filetask1) {cout << "Error!"; return 1;}
    
-    size_t n; 
+	int n;
+    size_t y; 
     filetask1 >> n;
-    vector<int> Arre;
-    Arre.resize(n);
-	for (int i = 0; i < n; ++i)
-		filetask1 >> Arre[i];
 
+	if (n < 0) {y = 0;}
+	else {y = static_cast<size_t>(n);}
+
+    vector<int> Arre;
+    Arre.resize(y);
+	for (int i = 0; i < n; ++i){
+		filetask1 >> Arre[i];
+	}
     short int count2 = 0, count3 = 0, count4 = 0, count5 = 0;
-    for (int num : Arre)
-        if (num == 2) count2++;
+    for (int num : Arre){
+		if (num == 2) count2++;}
 	Arre.erase(remove(Arre.begin(), Arre.end(), 2), Arre.end());
 
-	for (int num : Arre)
-		if (num == 3) count3++;
+	for (int num : Arre){
+		if (num == 3) count3++;}
 	Arre.erase(remove(Arre.begin(), Arre.end(), 3), Arre.end());
 
-	for (int num : Arre)
-		if (num == 4) count4++;
+	for (int num : Arre){
+		if (num == 4) count4++;}
 	Arre.erase(remove(Arre.begin(), Arre.end(), 4), Arre.end());
 
-	for (int num : Arre)
-		if (num == 5) count5++;
+	for (int num : Arre){
+		if (num == 5) count5++;}
 	Arre.erase(remove(Arre.begin(), Arre.end(), 5), Arre.end());
 
 	cout << "Quantity 2: " << count2 << endl;
--- LAB1C++/LAB1C++_TASK2.cpp
+++ LAB1C++/LAB1C++_TASK2.cpp
@@ -11,22 +11,28 @@ int main(int argc, char *argv[])
     ifstream filetask1(argv[1]);
     if(!filetask1) {cout << "Error!"; return 1;}
    
-    size_t n; 
+    int n;
+    size_t y; 
     filetask1 >> n;
-    vector<int> Arre;
-    Arre.resize(n);
-	for (int i = 0; i < n; ++i)
-		filetask1 >> Arre[i];
 
+	if (n < 0) {y = 0;}
+	else {y = static_cast<size_t>(n);}
+
+    vector<int> Arre;
+    Arre.resize(y);
+	for (int i = 0; i < n; ++i){
+		filetask1 >> Arre[i];
+	}
     int maxSum = 0, startIndex = 0;
 	for (int i = 0; i + 4 < Arre.size(); ++i) {
 		int currentSum = 0;
-		for (int j = i; j < i + 5; ++j)
+		for (int j = i; j < i + 5; ++j){
 			currentSum += Arre[j];
-			if (currentSum > maxSum) {
-				maxSum = currentSum;
-				startIndex = i;
-			}
+		}
+		if (currentSum > maxSum) {
+			maxSum = currentSum;
+			startIndex = i;
+		}
 		}
 	cout << "The maximum sum of 5 adjacent elements: " << maxSum << endl;
 }
