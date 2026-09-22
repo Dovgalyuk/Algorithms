9bbaf192aa855d9110ed74eedd19dca7e63bf9cd
puldice <sofashikova101@gmail.com>
2026-09-22 22:25:12 +0300

    array test

--- LibraryCPP/Tests/array.cpp
+++ LibraryCPP/Tests/array.cpp
@@ -26,4 +26,43 @@ int main()
     }
 
     array_delete(arr);
+
+    Array *arr2 = array_create(0);
+    if (array_size(arr2) != 0) {
+        std::cout << "Invalid array size\n";
+        array_delete(arr2);
+        return 1;
+    }
+    if (array_get(arr2, 0) != Data(0))
+    {
+        std::cout << "Invalid array get\n";
+        array_delete(arr2);
+        return 1;
+    }
+    array_delete(arr2);
+
+    Array *arr3 = array_create(5);
+    if (array_get(arr3, 100) != Data(0)) {
+        std::cout << "Invalid array get"<< "\n";
+        array_delete(arr3);
+        return 1;
+    }
+
+    array_set(arr3, 0, 7);
+    array_set(arr3, 5, 70);
+    if (array_get(arr3, 0) != 7)
+    {
+        std::cout << "Invalid array set\n";
+        array_delete(arr3);
+        return 1;
+    }
+    array_delete(arr3);
+
+    if (array_size(nullptr) != 0)
+    {
+        std::cout << "Invalid size for nullptr\n";
+        return 1;
+    }
+
+    std::cout << "All tests passed";
 }
