65c2e82952812aa4e7381550cec72722618f95d9
Danil Safin <safin.danil.2007@mail.ru>
2026-09-22 18:51:51 +0300

    lab2

---
+++ Lab2CPP/task.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <fstream>
+#include "stack.h"
+
+static bool is_number(const std::string& str)
+{
+    if (str.empty()) return false;
+    size_t start = 0;
+    if (str[0] == '-' || str[0] == '+') {
+        if (str.length() == 1) return false;
+        start = 1;
+    }
+    for (size_t i = start; i < str.length(); i++) {
+        if (!std::isdigit(static_cast<unsigned char>(str[i]))) return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        return 1;
+    }
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        return 1;
+    }
+    Stack* stack = stack_create();
+    std::string str;
+    while (file >> str)
+    {
+        if (is_number(str)) {
+            Data val = static_cast<Data>(std::stoi(str));
+            stack_push(stack, val);
+        }
+        else if (str == "+")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack); 
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, x + y);
+                }
+            }
+        }
+        else if (str == "-")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, x - y);
+                }
+            }
+        }
+        else if (str == "*")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, x * y);
+                }
+            }
+        }
+        else if (str == "/")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack) && y != 0)
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, x / y);
+                }
+            }
+        }
+        else if (str == "%")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack) && y != 0)
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, x % y);
+                }
+            }
+        }
+        else if (str == "dup")
+        {
+            if (!stack_empty(stack)) stack_push(stack, stack_get(stack));
+        }
+        else if (str == "drop")
+        {
+            if (!stack_empty(stack)) stack_pop(stack);
+        }
+        else if (str == "swap")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data x = stack_get(stack);
+                    stack_pop(stack);
+                    stack_push(stack, y);
+                    stack_push(stack, x);
+                }
+            }
+        }
+        else if (str == "over")
+        {
+            if (!stack_empty(stack))
+            {
+                Data y = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data x = stack_get(stack);
+                    stack_push(stack, y);
+                    stack_push(stack, x);
+                }
+            }
+        }
+        else if (str == "rot")
+        {
+            if (!stack_empty(stack))
+            {
+                Data c = stack_get(stack);
+                stack_pop(stack);
+                if (!stack_empty(stack))
+                {
+                    Data y = stack_get(stack);
+                    stack_pop(stack);
+                    if (!stack_empty(stack))
+                    {
+                        Data x = stack_get(stack);
+                        stack_pop(stack);
+                        stack_push(stack, y);
+                        stack_push(stack, c);
+                        stack_push(stack, x);
+                    }
+                }
+            }
+        }
+        else if (str == ".")
+        {
+            if (!stack_empty(stack))
+            {
+                std::cout << stack_get(stack) << std::endl;
+                stack_pop(stack);
+            }
+        }
+    }
+
+    stack_delete(stack);
+    return 0;
+}
--- LibraryCPP/list.cpp
+++ LibraryCPP/list.cpp
@@ -3,64 +3,119 @@
 
 struct ListItem
 {
+    Data data;
+    ListItem* next = nullptr;
+    ListItem* prev = nullptr;
 };
 
 struct List
 {
+    ListItem* head = nullptr;
+    ListItem* tail = nullptr;
 };
 
 List *list_create()
 {
-    return new List;
+    List* list = new List;
+    list->head;
+    list->tail;
+    return list;
 }
 
 void list_delete(List *list)
 {
-    // TODO: free items
+    ListItem* current = list->head;
+    while (current)
+    {
+        ListItem* next = current->next;
+        delete current;
+        current = next;
+    }
     delete list;
 }
 
 ListItem *list_first(List *list)
 {
-    return NULL;
+    return list->head;
 }
 
 ListItem *list_last(List *list)
 {
-    return NULL;
+    return list->tail;
 }
 
 Data list_item_data(const ListItem *item)
 {
-    return (Data)0;
+    return item->data;
 }
 
-ListItem *list_item_next(ListItem *item)
+ListItem* list_item_next(ListItem *item)
 {
-    return NULL;
+    return item->next;
 }
 
 ListItem *list_item_prev(ListItem *item)
 {
-    return NULL;
+    return item->prev;
 }
 
 ListItem *list_insert(List *list, Data data)
 {
-    return NULL;
+    ListItem* new_item = new ListItem;
+    new_item->data = data;
+    new_item->next = list->head;
+    new_item->prev = nullptr;
+
+    if (list->head) list->head->prev = new_item;
+    else
+        list->tail = new_item;
+
+    list->head = new_item;
+    return new_item;
 }
 
 ListItem *list_insert_after(List *list, ListItem *item, Data data)
 {
-    return NULL;
+    if (!item) return list_insert(list, data);
+
+    ListItem* new_item = new ListItem;
+    new_item->data = data;
+    new_item->prev = item;
+    new_item->next = item->next;
+
+    if (item->next) item->next->prev = new_item;
+    else
+        list->tail = new_item;
+
+    item->next = new_item;
+    return new_item;
 }
 
 ListItem *list_erase_first(List *list)
 {
-    return NULL;
+    ListItem* f_delete = list->head;
+    list->head = f_delete->next;
+
+    if (list->head) list->head->prev = nullptr;
+    else
+        list->tail = nullptr;
+
+    delete f_delete;
+    return list->head;
 }
 
 ListItem *list_erase_next(List *list, ListItem *item)
 {
-    return NULL;
+    if (!item) return list_erase_first(list);
+
+    ListItem* n_delete = item->next;
+    if (!n_delete) return nullptr;
+
+    item->next = n_delete->next;
+    if (n_delete->next) n_delete->next->prev = item;
+    else
+        list->tail = item;
+
+    delete n_delete;
+    return item->next;
 }
