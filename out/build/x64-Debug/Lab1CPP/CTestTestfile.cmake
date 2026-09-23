081cee98bc319eb41d15cd1d1da81bc4c89d85e0
Andr3y Malyshev <Andr3y044@gmail.com>
2026-09-23 23:36:43 +0300

    add array functions

---
+++ out/build/x64-Debug/Lab1CPP/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Andr3/source/repos/Algorithms/Lab1CPP
+# Build directory: C:/Users/Andr3/source/repos/Algorithms/out/build/x64-Debug/Lab1CPP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Tests")
