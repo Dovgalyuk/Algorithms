294511528b3e065c4f3ac377f85ebc6118e4fd61
Lev Groshev <levgroshev8@gmail.com>
2026-09-24 02:33:19 +0300

    N1

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -4,14 +4,16 @@
 Array *array_create_and_read(FILE *input)
 {
     int n;
-    fscanf(input, "%d", &n);
+    if (fscanf(input, "%d", &n) != 1)
+        n = 0;
     /* Create array */
     Array *arr = array_create(n, NULL);
     /* Read array data */
     for (int i = 0 ; i < n ; ++i)
     {
         int x;
-        fscanf(input, "%d", &x);
+        if (fscanf(input, "%d", &x) != 1)
+            x = 0;
         array_set(arr, i, x);
     }
     return arr;
@@ -19,10 +21,26 @@ Array *array_create_and_read(FILE *input)
 
 void task1(Array *arr)
 {
+    int n = (int)array_size(arr);
+    for (int i = 0 ; i < n ; ++i)
+    {
+        printf("%d", (int)array_get(arr, i));
+        if (i + 1 < n)
+            printf(" ");
+    }
+    printf("\n");
 }
 
 void task2(Array *arr)
 {
+    int n = (int)array_size(arr);
+    for (int i = 0 ; i < n ; ++i)
+    {
+        printf("%d", (int)array_get(arr, i));
+        if (i + 1 < n)
+            printf(" ");
+    }
+    printf("\n");
 }
 
 int main(int argc, char **argv)
--- LibraryC/array.c
+++ LibraryC/array.c
@@ -2,35 +2,57 @@
 #include "array.h"
 
 typedef struct Array {
-    // remove this
-    Data d;
+    Data *data;
+    size_t size;
+    FFree *free_func;
 } Array;
 
 // create array
 Array *array_create(size_t size, FFree f)
 {
-    return malloc(sizeof(Array));
+    Array *arr = (Array*)malloc(sizeof(Array));
+    arr->size = size;
+    arr->free_func = f;
+    arr->data = size > 0 ? (Data*)calloc(size, sizeof(Data)) : NULL;
+    return arr;
 }
 
 // delete array, free memory
 void array_delete(Array *arr)
 {
+    if (arr->free_func)
+    {
+        size_t i;
+        for (i = 0 ; i < arr->size ; ++i)
+        {
+            if (arr->data[i])
+                arr->free_func((void*)arr->data[i]);
+        }
+    }
+    free(arr->data);
     free(arr);
 }
 
 // returns specified array element
 Data array_get(const Array *arr, size_t index)
 {
-    return (Data)0;
+    if (index >= arr->size)
+        return (Data)0;
+    return arr->data[index];
 }
 
 // sets the specified array element to the value
 void array_set(Array *arr, size_t index, Data value)
 {
+    if (index >= arr->size)
+        return;
+    if (arr->free_func && arr->data[index])
+        arr->free_func((void*)arr->data[index]);
+    arr->data[index] = value;
 }
 
 // returns array size
 size_t array_size(const Array *arr)
 {
-    return 0;
+    return arr->size;
 }
