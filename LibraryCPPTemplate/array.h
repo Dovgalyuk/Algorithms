86ab6d0ff2d421583d123b843e613c041446fb2b
Данила <clevercc1001@gmail.com>
2026-09-25 04:08:28 +0300

    Implement C++ template array

--- LibraryCPPTemplate/array.h
+++ LibraryCPPTemplate/array.h
@@ -1,49 +1,78 @@
 #ifndef ARRAY_TEMPLATE_H
 #define ARRAY_TEMPLATE_H
 
+#include <cstddef>
+
 template <typename Data> class Array
 {
 public:
     // create array
     explicit Array(size_t size)
     {
+        length = size;
+        items = new Data[length];
     }
 
     // copy constructor
     Array(const Array &a)
     {
+        length = a.length;
+        items = new Data[length];
+
+        for (size_t i = 0; i < length; i++)
+        {
+            items[i] = a.items[i];
+        }
     }
 
     // assignment operator
     Array &operator=(const Array &a)
     {
+        if (this == &a)
+        {
+            return *this;
+        }
+
+        delete[] items;
+
+        length = a.length;
+        items = new Data[length];
+
+        for (size_t i = 0; i < length; i++)
+        {
+            items[i] = a.items[i];
+        }
+
         return *this;
     }
 
     // delete array, free memory
     ~Array()
     {
+        delete[] items;
     }
 
     // returns specified array element
     Data get(size_t index) const
     {
-        return Data(0);
+        return items[index];
     }
 
     // sets the specified array element to the value
     void set(size_t index, Data value)
     {
+        items[index] = value;
     }
 
     // returns array size
     size_t size() const
     {
-        return 0;
+        return length;
     }
 
 private:
-    // private data should be here
+    size_t length;
+    Data *items;
 };
 
 #endif
