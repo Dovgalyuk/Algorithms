d721a75b8e6a7c109c049950cad77b3e3aeea9bd
zheniaregbl <evgenieregbl@gmail.com>
2026-09-26 09:42:30 +0300

    Delete 'out' folder

--- out/build/x64-Debug/LibraryCPPTemplate/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/admin/source/repos/Algorithms/LibraryCPPTemplate/Tests
-# Build directory: C:/Users/admin/source/repos/Algorithms/out/build/x64-Debug/LibraryCPPTemplate/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestArrayCPPTemplate "TestArrayCPPTemplate")
-set_tests_properties(TestArrayCPPTemplate PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/admin/source/repos/Algorithms/LibraryCPPTemplate/Tests/CMakeLists.txt;3;add_test;C:/Users/admin/source/repos/Algorithms/LibraryCPPTemplate/Tests/CMakeLists.txt;0;")
